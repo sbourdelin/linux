9757c5ef6ef667434edc61a64f486b43770c0538
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-03-05 15:25:27 -0500

    rtl8xxxu: Reduce indented code
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

--- drivers/net/wireless/rtl8xxxu.c
+++ drivers/net/wireless/rtl8xxxu.c
@@ -1883,73 +1883,74 @@ static void rtl8xxxu_fill_iqk_matrix_a(struct rtl8xxxu_priv *priv,
 	int y, tx0_c;
 	u32 val32;
 
-	if (iqk_ok) {
-		val32 = rtl8723au_read32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE);
-		oldval = (val32 >> 22) & 0x3FF;
+	if (!iqk_ok)
+		return;
 
-		x = result[final_candidate][0];
-		if ((x & 0x00000200) != 0)
-			x = x | 0xfffffc00;
-		tx0_a = (x * oldval) >> 8;
+	val32 = rtl8723au_read32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE);
+	oldval = (val32 >> 22) & 0x3FF;
 
-		val32 = rtl8723au_read32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE);
-		val32 &= ~0x3ff;
-		val32 |= tx0_a;
-		rtl8723au_write32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE, val32);
+	x = result[final_candidate][0];
+	if ((x & 0x00000200) != 0)
+		x = x | 0xfffffc00;
+	tx0_a = (x * oldval) >> 8;
 
-		val32 = rtl8723au_read32(priv, REG_OFDM0_ENERGY_CCA_THRES);
-		val32 &= ~BIT(31);
-		if ((x * oldval >> 7) & 0x1)
-			val32 |= BIT(31);
-		rtl8723au_write32(priv, REG_OFDM0_ENERGY_CCA_THRES, val32);
+	val32 = rtl8723au_read32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE);
+	val32 &= ~0x3ff;
+	val32 |= tx0_a;
+	rtl8723au_write32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE, val32);
 
-		y = result[final_candidate][1];
-		if ((y & 0x00000200) != 0)
-			y = y | 0xfffffc00;
-		tx0_c = (y * oldval) >> 8;
+	val32 = rtl8723au_read32(priv, REG_OFDM0_ENERGY_CCA_THRES);
+	val32 &= ~BIT(31);
+	if ((x * oldval >> 7) & 0x1)
+		val32 |= BIT(31);
+	rtl8723au_write32(priv, REG_OFDM0_ENERGY_CCA_THRES, val32);
 
-		val32 = rtl8723au_read32(priv, REG_OFDM0_XC_TX_AFE);
-		val32 &= ~0xf0000000;
-		val32 |= (((tx0_c & 0x3c0) >> 6) << 28);
-		rtl8723au_write32(priv, REG_OFDM0_XC_TX_AFE, val32);
+	y = result[final_candidate][1];
+	if ((y & 0x00000200) != 0)
+		y = y | 0xfffffc00;
+	tx0_c = (y * oldval) >> 8;
 
-		val32 = rtl8723au_read32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE);
-		val32 &= ~0x003f0000;
-		val32 |= ((tx0_c & 0x3f) << 16);
-		rtl8723au_write32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE, val32);
+	val32 = rtl8723au_read32(priv, REG_OFDM0_XC_TX_AFE);
+	val32 &= ~0xf0000000;
+	val32 |= (((tx0_c & 0x3c0) >> 6) << 28);
+	rtl8723au_write32(priv, REG_OFDM0_XC_TX_AFE, val32);
 
-		val32 = rtl8723au_read32(priv, REG_OFDM0_ENERGY_CCA_THRES);
-		val32 &= ~BIT(29);
-		if ((y * oldval >> 7) & 0x1)
-			val32 |= BIT(29);
-		rtl8723au_write32(priv, REG_OFDM0_ENERGY_CCA_THRES, val32);
+	val32 = rtl8723au_read32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE);
+	val32 &= ~0x003f0000;
+	val32 |= ((tx0_c & 0x3f) << 16);
+	rtl8723au_write32(priv, REG_OFDM0_XA_TX_IQ_IMBALANCE, val32);
 
-		if (tx_only) {
-			printk(KERN_DEBUG "%s: only TX\n", __func__);
-			return;
-		}
+	val32 = rtl8723au_read32(priv, REG_OFDM0_ENERGY_CCA_THRES);
+	val32 &= ~BIT(29);
+	if ((y * oldval >> 7) & 0x1)
+		val32 |= BIT(29);
+	rtl8723au_write32(priv, REG_OFDM0_ENERGY_CCA_THRES, val32);
 
-		reg = result[final_candidate][2];
-
-		val32 = rtl8723au_read32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE);
-		val32 &= ~0x3ff;
-		val32 |= (reg & 0x3ff);
-		rtl8723au_write32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE, val32);
-
-		reg = result[final_candidate][3] & 0x3F;
-
-		val32 = rtl8723au_read32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE);
-		val32 &= ~0xfc00;
-		val32 |= ((reg << 10) & 0xfc00);
-		rtl8723au_write32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE, val32);
-
-		reg = (result[final_candidate][3] >> 6) & 0xF;
-
-		val32 = rtl8723au_read32(priv, REG_OFDM0_RX_IQ_EXT_ANTA);
-		val32 &= ~0xf0000000;
-		val32 |= (reg << 28);
-		rtl8723au_write32(priv, REG_OFDM0_RX_IQ_EXT_ANTA, val32);
+	if (tx_only) {
+		printk(KERN_DEBUG "%s: only TX\n", __func__);
+		return;
 	}
+
+	reg = result[final_candidate][2];
+
+	val32 = rtl8723au_read32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE);
+	val32 &= ~0x3ff;
+	val32 |= (reg & 0x3ff);
+	rtl8723au_write32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE, val32);
+
+	reg = result[final_candidate][3] & 0x3F;
+
+	val32 = rtl8723au_read32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE);
+	val32 &= ~0xfc00;
+	val32 |= ((reg << 10) & 0xfc00);
+	rtl8723au_write32(priv, REG_OFDM0_XA_RX_IQ_IMBALANCE, val32);
+
+	reg = (result[final_candidate][3] >> 6) & 0xF;
+
+	val32 = rtl8723au_read32(priv, REG_OFDM0_RX_IQ_EXT_ANTA);
+	val32 &= ~0xf0000000;
+	val32 |= (reg << 28);
+	rtl8723au_write32(priv, REG_OFDM0_RX_IQ_EXT_ANTA, val32);
 }
 
 #define MAX_TOLERANCE		5
