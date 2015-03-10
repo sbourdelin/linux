41d9b6ab5ad2e704dc08ed8aad9e51f1523683a3
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-03-10 09:41:24 -0400

    rtl8xxxu: Use the correctly sized byte conversion macro for the csum word.
    The csum field is 16 bits, so use cpu_to_le16().
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

--- drivers/net/wireless/rtl8xxxu.c
+++ drivers/net/wireless/rtl8xxxu.c
@@ -3613,12 +3613,12 @@ static void rtl8xxxu_calc_tx_desc_csum(struct rtl8xxxu_tx_desc *tx_desc)
 	 * Clear csum field before calculation, as the csum field is
 	 * in the middle of the struct.
 	 */
-	tx_desc->csum = cpu_to_le32(0);
+	tx_desc->csum = cpu_to_le16(0);
 
 	for (i = 0; i < (sizeof(struct rtl8xxxu_tx_desc) / sizeof(u16)); i++)
 		csum = csum ^ le16_to_cpu(ptr[i]);
 
-	tx_desc->csum |= cpu_to_le32(csum);
+	tx_desc->csum |= cpu_to_le16(csum);
 }
 
 static void rtl8xxxu_tx_complete(struct urb *urb)
