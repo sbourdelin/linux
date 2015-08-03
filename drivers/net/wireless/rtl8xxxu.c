a6c3bd7ee756b3dcfa7ef7bfa75cd569cdf28794
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-08-03 12:55:04 -0400

    rtl8xxxu: rtl8xxxu_disabled_to_emu(): Use register names instead of hard coded values
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

--- drivers/net/wireless/rtl8xxxu.c
+++ drivers/net/wireless/rtl8xxxu.c
@@ -2750,19 +2750,19 @@ static void rtl8xxxu_disabled_to_emu(struct rtl8xxxu_priv *priv)
 	u8 val8;
 
 	/* Clear suspend enable and power down enable*/
-	val8 = rtl8xxxu_read8(priv, 0x05);
+	val8 = rtl8xxxu_read8(priv, REG_APS_FSMCO + 1);
 	val8 &= ~(BIT(3) | BIT(7));
-	rtl8xxxu_write8(priv, 0x05, val8);
+	rtl8xxxu_write8(priv, REG_APS_FSMCO + 1, val8);
 
 	/* 0x48[16] = 0 to disable GPIO9 as EXT WAKEUP*/
-	val8 = rtl8xxxu_read8(priv, 0x4a);
+	val8 = rtl8xxxu_read8(priv, REG_GPIO_INTM + 2);
 	val8 &= ~BIT(0);
-	rtl8xxxu_write8(priv, 0x4a, val8);
+	rtl8xxxu_write8(priv, REG_GPIO_INTM + 2, val8);
 
 	/* 0x04[12:11] = 11 enable WL suspend*/
-	val8 = rtl8xxxu_read8(priv, 0x05);
+	val8 = rtl8xxxu_read8(priv, REG_APS_FSMCO + 1);
 	val8 &= ~(BIT(3) | BIT(4));
-	rtl8xxxu_write8(priv, 0x05, val8);
+	rtl8xxxu_write8(priv, REG_APS_FSMCO + 1, val8);
 }
 
 static int rtl8xxxu_emu_to_active(struct rtl8xxxu_priv *priv)
