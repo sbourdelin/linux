bdb4814fd63328731f86f11d5104278ae5d21c60
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-08-12 14:38:15 -0400

    rtl8xxxu: Handle 1T2R pieces
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

--- drivers/net/wireless/rtl8xxxu.c
+++ drivers/net/wireless/rtl8xxxu.c
@@ -2036,69 +2036,67 @@ static int rtl8xxxu_init_phy_bb(struct rtl8xxxu_priv *priv)
 	val8 = RF_ENABLE | RF_RSTB | RF_SDMRSTB;
 	rtl8xxxu_write8(priv, REG_RF_CTRL, val8);
 
-	if (priv->chip == 8192 || priv->chip == 8191) {
+	if (priv->tx_paths == 2)
 		rtl8xxxu_init_phy_regs(priv, rtl8192cu_phy_2t_init_table);
-		if (priv->tx_paths == 1) {
-			/*
-			 * For 1T2R boards, patch the registers.
-			 *
-			 * It looks like 8191/2 1T2R boards use path B for TX
-			 */
-			val32 = rtl8xxxu_read32(priv, REG_FPGA0_TX_INFO);
-			val32 &= ~(BIT(0) | BIT(1));
-			val32 |= BIT(1);
-			rtl8xxxu_write32(priv, REG_FPGA0_TX_INFO, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_FPGA1_TX_INFO);
-			val32 &= ~0x300033;
-			val32 |= 0x200022;
-			rtl8xxxu_write32(priv, REG_FPGA1_TX_INFO, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_CCK0_AFE_SETTING);
-			val32 &= 0xff000000;
-			val32 |= 0x45000000;
-			rtl8xxxu_write32(priv, REG_CCK0_AFE_SETTING, val32);
-
-			val32 = rtl8xxxu_read32(priv,
-						REG_OFDM0_TRX_PATH_ENABLE);
-			val32 &= ~(OFDM_RF_PATH_RX_MASK | OFDM_RF_PATH_TX_MASK);
-			val32 |= (OFDM_RF_PATH_RX_A | OFDM_RF_PATH_RX_B |
-				  OFDM_RF_PATH_TX_B);
-			rtl8xxxu_write32(priv,
-					 REG_OFDM0_TRX_PATH_ENABLE, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_OFDM0_AGC_PARM1);
-			val32 &= ~(BIT(4) | BIT(5));
-			val32 |= BIT(4);
-			rtl8xxxu_write32(priv, REG_OFDM0_AGC_PARM1, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_TX_CCK_RFON);
-			val32 &= ~(BIT(27) | BIT(26));
-			val32 |= BIT(27);
-			rtl8xxxu_write32(priv, REG_TX_CCK_RFON, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_TX_CCK_BBON);
-			val32 &= ~(BIT(27) | BIT(26));
-			val32 |= BIT(27);
-			rtl8xxxu_write32(priv, REG_TX_CCK_BBON, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_TX_OFDM_RFON);
-			val32 &= ~(BIT(27) | BIT(26));
-			val32 |= BIT(27);
-			rtl8xxxu_write32(priv, REG_TX_OFDM_RFON, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_TX_OFDM_BBON);
-			val32 &= ~(BIT(27) | BIT(26));
-			val32 |= BIT(27);
-			rtl8xxxu_write32(priv, REG_TX_OFDM_BBON, val32);
-
-			val32 = rtl8xxxu_read32(priv, REG_TX_TO_TX);
-			val32 &= ~(BIT(27) | BIT(26));
-			val32 |= BIT(27);
-			rtl8xxxu_write32(priv, REG_TX_TO_TX, val32);
-		}
-	} else {
+	else
 		rtl8xxxu_init_phy_regs(priv, rtl8723a_phy_1t_init_table);
+
+	if (priv->tx_paths == 1 && priv->rx_paths == 2) {
+		/*
+		 * For 1T2R boards, patch the registers.
+		 *
+		 * It looks like 8191/2 1T2R boards use path B for TX
+		 */
+		val32 = rtl8xxxu_read32(priv, REG_FPGA0_TX_INFO);
+		val32 &= ~(BIT(0) | BIT(1));
+		val32 |= BIT(1);
+		rtl8xxxu_write32(priv, REG_FPGA0_TX_INFO, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_FPGA1_TX_INFO);
+		val32 &= ~0x300033;
+		val32 |= 0x200022;
+		rtl8xxxu_write32(priv, REG_FPGA1_TX_INFO, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_CCK0_AFE_SETTING);
+		val32 &= 0xff000000;
+		val32 |= 0x45000000;
+		rtl8xxxu_write32(priv, REG_CCK0_AFE_SETTING, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_OFDM0_TRX_PATH_ENABLE);
+		val32 &= ~(OFDM_RF_PATH_RX_MASK | OFDM_RF_PATH_TX_MASK);
+		val32 |= (OFDM_RF_PATH_RX_A | OFDM_RF_PATH_RX_B |
+			  OFDM_RF_PATH_TX_B);
+		rtl8xxxu_write32(priv, REG_OFDM0_TRX_PATH_ENABLE, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_OFDM0_AGC_PARM1);
+		val32 &= ~(BIT(4) | BIT(5));
+		val32 |= BIT(4);
+		rtl8xxxu_write32(priv, REG_OFDM0_AGC_PARM1, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_TX_CCK_RFON);
+		val32 &= ~(BIT(27) | BIT(26));
+		val32 |= BIT(27);
+		rtl8xxxu_write32(priv, REG_TX_CCK_RFON, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_TX_CCK_BBON);
+		val32 &= ~(BIT(27) | BIT(26));
+		val32 |= BIT(27);
+		rtl8xxxu_write32(priv, REG_TX_CCK_BBON, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_TX_OFDM_RFON);
+		val32 &= ~(BIT(27) | BIT(26));
+		val32 |= BIT(27);
+		rtl8xxxu_write32(priv, REG_TX_OFDM_RFON, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_TX_OFDM_BBON);
+		val32 &= ~(BIT(27) | BIT(26));
+		val32 |= BIT(27);
+		rtl8xxxu_write32(priv, REG_TX_OFDM_BBON, val32);
+
+		val32 = rtl8xxxu_read32(priv, REG_TX_TO_TX);
+		val32 &= ~(BIT(27) | BIT(26));
+		val32 |= BIT(27);
+		rtl8xxxu_write32(priv, REG_TX_TO_TX, val32);
 	}
 
 	rtl8xxxu_init_phy_regs(priv, rtl8723a_agc_1t_init_table);
