32bf9c3a105ee714d668141c582f410732daba50
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-08-26 21:23:38 -0400

    rtl8xxxu: Make sure to enable/disble path B support for 2T modules
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

--- drivers/net/wireless/rtl8xxxu.c
+++ drivers/net/wireless/rtl8xxxu.c
@@ -987,10 +987,14 @@ static void rtl8723a_enable_rf(struct rtl8xxxu_priv *priv)
 	val8 |= BIT(0) | BIT(3);
 	rtl8xxxu_write8(priv, REG_SPS0_CTRL, val8);
 
-	val32 = rtl8xxxu_read32(priv, REG_FPGA0_XA_RF_PARM);
+	val32 = rtl8xxxu_read32(priv, REG_FPGA0_XAB_RF_PARM);
 	val32 &= ~(BIT(4) | BIT(5));
 	val32 |= BIT(3);
-	rtl8xxxu_write32(priv, REG_FPGA0_XA_RF_PARM, val32);
+	if (priv->rf_paths == 2) {
+		val32 &= ~(BIT(20) | BIT(21));
+		val32 |= BIT(19);
+	}
+	rtl8xxxu_write32(priv, REG_FPGA0_XAB_RF_PARM, val32);
 
 	val32 = rtl8xxxu_read32(priv, REG_OFDM0_TRX_PATH_ENABLE);
 	val32 &= ~OFDM_RF_PATH_TX_MASK;
@@ -1006,9 +1010,14 @@ static void rtl8723a_enable_rf(struct rtl8xxxu_priv *priv)
 	val32 &= ~FPGA_RF_MODE_JAPAN;
 	rtl8xxxu_write32(priv, REG_FPGA0_RF_MODE, val32);
 
-	rtl8xxxu_write32(priv, REG_RX_WAIT_CCA, 0x631B25A0);
+	if (priv->rf_paths == 2)
+		rtl8xxxu_write32(priv, REG_RX_WAIT_CCA, 0x63db25a0);
+	else
+		rtl8xxxu_write32(priv, REG_RX_WAIT_CCA, 0x631b25a0);
 
 	rtl8xxxu_write_rfreg(priv, RF_A, RF6052_REG_AC, 0x32d95);
+	if (priv->rf_paths == 2)
+		rtl8xxxu_write_rfreg(priv, RF_B, RF6052_REG_AC, 0x32d95);
 
 #if 0
 	rtl8xxxu_write8(priv, REG_SYS_FUNC, 0xE3);
@@ -1029,9 +1038,11 @@ static void rtl8723a_disable_rf(struct rtl8xxxu_priv *priv)
 	sps0 = rtl8xxxu_read8(priv, REG_SPS0_CTRL);
 
 	/* RF RX code for preamble power saving */
-	val32 = rtl8xxxu_read32(priv, REG_FPGA0_XA_RF_PARM);
+	val32 = rtl8xxxu_read32(priv, REG_FPGA0_XAB_RF_PARM);
 	val32 &= ~(BIT(3) | BIT(4) | BIT(5));
-	rtl8xxxu_write32(priv, REG_FPGA0_XA_RF_PARM, val32);
+	if (priv->rf_paths == 2)
+		val32 &= ~(BIT(19) | BIT(20) | BIT(21));
+	rtl8xxxu_write32(priv, REG_FPGA0_XAB_RF_PARM, val32);
 
 	/* Disable TX for four paths */
 	val32 = rtl8xxxu_read32(priv, REG_OFDM0_TRX_PATH_ENABLE);
@@ -1044,10 +1055,15 @@ static void rtl8723a_disable_rf(struct rtl8xxxu_priv *priv)
 	rtl8xxxu_write32(priv, REG_FPGA0_RF_MODE, val32);
 
 	/* AFE control register to power down bits [30:22] */
-	rtl8xxxu_write32(priv, REG_RX_WAIT_CCA, 0x001b25a0);
+	if (priv->rf_paths == 2)
+		rtl8xxxu_write32(priv, REG_RX_WAIT_CCA, 0x00db25a0);
+	else
+		rtl8xxxu_write32(priv, REG_RX_WAIT_CCA, 0x001b25a0);
 
 	/* Power down RF module */
 	rtl8xxxu_write_rfreg(priv, RF_A, RF6052_REG_AC, 0);
+	if (priv->rf_paths == 2)
+		rtl8xxxu_write_rfreg(priv, RF_B, RF6052_REG_AC, 0);
 
 	sps0 &= ~(BIT(0) | BIT(3));
 	rtl8xxxu_write8(priv, REG_SPS0_CTRL, sps0);
