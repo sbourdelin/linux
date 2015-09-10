9aac2640680fec100e7f63a5e0777aa6462a1e0c
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-09-10 14:43:35 -0400

    rtl8xxxu: Use usb_free_urb() to free tx urbs
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

--- drivers/net/wireless/rtl8xxxu.c
+++ drivers/net/wireless/rtl8xxxu.c
@@ -4605,14 +4605,14 @@ static void rtl8xxxu_calc_tx_desc_csum(struct rtl8xxxu_tx_desc *tx_desc)
 
 static void rtl8xxxu_free_tx_resources(struct rtl8xxxu_priv *priv)
 {
-	struct rtl8xxxu_tx_urb *entry, *tmp;
+	struct rtl8xxxu_tx_urb *tx_urb, *tmp;
 	unsigned long flags;
 
 	spin_lock_irqsave(&priv->tx_urb_lock, flags);
-	list_for_each_entry_safe(entry, tmp, &priv->tx_urb_free_list, list) {
-		list_del(&entry->list);
+	list_for_each_entry_safe(tx_urb, tmp, &priv->tx_urb_free_list, list) {
+		list_del(&tx_urb->list);
 		priv->tx_urb_free_count--;
-		kfree(entry);
+		usb_free_urb(&tx_urb->urb);
 	}
 	spin_unlock_irqrestore(&priv->tx_urb_lock, flags);
 }
