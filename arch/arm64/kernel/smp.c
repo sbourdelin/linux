a471b0ecaa83b58bceeb8382c7e903d8a51d014c
Suzuki K. Poulose <suzuki.poulose@arm.com>
2015-10-13 18:22:21 +0100

    arm64: Populate cpuinfo after notify_cpu_starting
    This patch delays populating the cpuinfo for a new (hotplugged)
    CPU until the notifiers have executed. This will enable us to verify
    if the new (hotplugged) CPU has all the capabilities which the system
    already has. If it doesn't, we could prevent it from turning online and
    also modifying the system wide feature register status.
    
    Also delays advertising that the CPU has booted until we complete
    the notifiers, when we are ready to mark it online. This would avoid
    confusing the user if the CPU fails to boot due to a missing capability.
    
    Signed-off-by: Suzuki K. Poulose <suzuki.poulose@arm.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -145,9 +145,6 @@ asmlinkage void secondary_start_kernel(void)
 	cpumask_set_cpu(cpu, mm_cpumask(mm));
 
 	set_my_cpu_offset(per_cpu_offset(smp_processor_id()));
-	pr_info("CPU%u: Booted secondary processor [%08x]\n",
-					 cpu, read_cpuid_id());
-
 	/*
 	 * TTBR0 is only used for the identity mapping at this stage. Make it
 	 * point to zero page to avoid speculatively fetching new entries.
@@ -163,14 +160,14 @@ asmlinkage void secondary_start_kernel(void)
 		cpu_ops[cpu]->cpu_postboot();
 
 	/*
-	 * Log the CPU info before it is marked online and might get read.
-	 */
-	cpuinfo_store_cpu();
-
-	/*
 	 * Enable GIC and timers.
 	 */
 	notify_cpu_starting(cpu);
+
+	/*
+	 * Log the CPU info before it is marked online and might get read.
+	 */
+	cpuinfo_store_cpu();
 
 	smp_store_cpu_info(cpu);
 
@@ -179,6 +176,10 @@ asmlinkage void secondary_start_kernel(void)
 	 * the CPU migration code to notice that the CPU is online
 	 * before we continue.
 	 */
+
+	pr_info("CPU%u: Booted secondary processor [%08x]\n",
+					 cpu, read_cpuid_id());
+
 	set_cpu_online(cpu, true);
 	complete(&cpu_running);
 
