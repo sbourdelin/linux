505dfed721b70b9bbabff404d179e59e5a433895
Andi Kleen <ak@linux.intel.com>
2015-10-15 16:38:00 -0700

    x86, perf: Use INST_RETIRED.PREC_DIST for cycles:pp on Skylake
    Switch the cycles:pp alias from UOPS_RETITRED to INST_RETIRED.PREC_DIST.
    The basic mechanism of abusing the inverse cmask to get all cycles
    works the same as before.
    
    PREC_DIST has special support for avoiding shadow effects, which
    can give better results compare to UOPS_RETIRED. The drawback is
    that PREC_DIST can only schedule on counter 1, but that is ok for
    cycle sampling, as there is normally no need to do multiple cycle
    sampling runs in parallel. It is still possible to run perf top
    in parallel, as that doesn't use precise mode. Also of course
    the multiplexing can still allow parallel operation.
    
    The UOPS_RETIRED old alias is still available in raw form.
    
    Example:
    
    Sample a loop with 10 sqrt with old cycles:pp
    
      0.14 │10:   sqrtps %xmm1,%xmm0     <--------------
      9.13 │      sqrtps %xmm1,%xmm0
     11.58 │      sqrtps %xmm1,%xmm0
     11.51 │      sqrtps %xmm1,%xmm0
      6.27 │      sqrtps %xmm1,%xmm0
     10.38 │      sqrtps %xmm1,%xmm0
     12.20 │      sqrtps %xmm1,%xmm0
     12.74 │      sqrtps %xmm1,%xmm0
      5.40 │      sqrtps %xmm1,%xmm0
     10.14 │      sqrtps %xmm1,%xmm0
     10.51 │    ↑ jmp    10
    
    We expect all 10 sqrt to get roughly the sample number of samples.
    
    But you can see that the instruction directly after the jmp is
    systematically underestimated in the result, due to sampling shadow
    effects.
    
    With the new PREC_DIST based sampling this problem is gone
    and all instructions show up roughly evenly:
    
      9.51 │10:   sqrtps %xmm1,%xmm0
     11.74 │      sqrtps %xmm1,%xmm0
     11.84 │      sqrtps %xmm1,%xmm0
      6.05 │      sqrtps %xmm1,%xmm0
     10.46 │      sqrtps %xmm1,%xmm0
     12.25 │      sqrtps %xmm1,%xmm0
     12.18 │      sqrtps %xmm1,%xmm0
      5.26 │      sqrtps %xmm1,%xmm0
     10.13 │      sqrtps %xmm1,%xmm0
     10.43 │      sqrtps %xmm1,%xmm0
      0.16 │    ↑ jmp    10
    
    Even with PREC_DIST there is still sampling skid and the result
    is not completely even, but systematic shadow effects are
    significantly reduced.
    
    The improvements are mainly expected to make a difference
    in high IPC code. With low IPC it should be similar.
    
    The PREC_DIST event is supported back to IvyBridge, but I only tested
    it on Skylake for now, so I only enabled it on Skylake.
    
    On earlier parts there were various hardware bugs in it
    (but no show stopper on IvyBridge and up I believe),
    so it could be enabled there after sufficient testing.
    On Sandy Bridge PREC_DIST can only be scheduled as a single
    event on the PMU, which is too limiting. Before Sandy
    Bridge it was not supported.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/cpu/perf_event_intel.c
+++ arch/x86/kernel/cpu/perf_event_intel.c
@@ -2565,6 +2565,31 @@ static void intel_pebs_aliases_snb(struct perf_event *event)
 	}
 }
 
+static void intel_pebs_aliases_skl(struct perf_event *event)
+{
+	if ((event->hw.config & X86_RAW_EVENT_MASK) == 0x003c) {
+		/*
+		 * Use an alternative encoding for CPU_CLK_UNHALTED.THREAD_P
+		 * (0x003c) so that we can use it with PEBS.
+		 *
+		 * The regular CPU_CLK_UNHALTED.THREAD_P event (0x003c) isn't
+		 * PEBS capable. However we can use INST_RETIRED.PREC_DIST
+		 * (0x01c0), which is a PEBS capable event, to get the same
+		 * count.
+		 *
+		 * The PREC_DIST event has special support to minimize sample
+		 * shadowing effects. One drawback is that it can be
+		 * only programmed on counter 1, but that seems like an
+		 * acceptable trade off.
+		 */
+		u64 alt_config = X86_CONFIG(.event=0xc0, .umask=0x01, .inv=1, .cmask=16);
+
+		alt_config |= (event->hw.config & ~X86_RAW_EVENT_MASK);
+		event->hw.config = alt_config;
+	}
+}
+
+
 static unsigned long intel_pmu_free_running_flags(struct perf_event *event)
 {
 	unsigned long flags = x86_pmu.free_running_flags;
@@ -3617,7 +3642,7 @@ __init int intel_pmu_init(void)
 		x86_pmu.event_constraints = intel_skl_event_constraints;
 		x86_pmu.pebs_constraints = intel_skl_pebs_event_constraints;
 		x86_pmu.extra_regs = intel_skl_extra_regs;
-		x86_pmu.pebs_aliases = intel_pebs_aliases_snb;
+		x86_pmu.pebs_aliases = intel_pebs_aliases_skl;
 		/* all extra regs are per-cpu when HT is on */
 		x86_pmu.flags |= PMU_FL_HAS_RSP_1;
 		x86_pmu.flags |= PMU_FL_NO_HT_SHARING;
