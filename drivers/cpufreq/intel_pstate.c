0b8774ff1332f98986b5ef361a85e4d91e398e1e
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2015-10-15 11:32:48 -0700

    cpufreq: intel_pstate: divide by zero on Knights Landing (KNL)
    This is a workaround for KNL platform, where in some cases MPERF counter
    will not have updated value before next read of MSR_IA32_MPERF. In this
    case divide by zero will occur. This change ignores current sample for
    busy calculation in this case.
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -776,6 +776,9 @@ static inline void intel_pstate_sample(struct cpudata *cpu)
 	local_irq_save(flags);
 	rdmsrl(MSR_IA32_APERF, aperf);
 	rdmsrl(MSR_IA32_MPERF, mperf);
+	if (cpu->prev_mperf == mperf)
+		return;
+
 	tsc = rdtsc();
 	local_irq_restore(flags);
 
