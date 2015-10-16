f68fd79f573300be43dad1f4dc49c6d03d72dea4
Jan Kaisrlik <kaisrja1@fel.cvut.cz>
2015-10-16 13:28:05 +0200

    ath9k: Add support for ITS-G5 band (5.9 GHz)
    The patch adds support for Inteligent Transportation System (ITS-G5)
    band to the ath9k drivers.
    
    Signed-off-by: Jan Kaisrlik <kaisrja1@fel.cvut.cz>
    Cc: Michal Sojka <sojkam1@fel.cvut.cz>

--- drivers/net/wireless/ath/ath9k/hw.h
+++ drivers/net/wireless/ath/ath9k/hw.h
@@ -73,7 +73,11 @@
 
 #define ATH9K_RSSI_BAD			-128
 
-#define ATH9K_NUM_CHANNELS	38
+#ifdef CONFIG_CFG80211_REG_ITSG5_BAND
+#define ATH9K_NUM_CHANNELS		53
+#else
+#define ATH9K_NUM_CHANNELS		38
+#endif
 
 /* Register read/write primitives */
 #define REG_WRITE(_ah, _reg, _val) \
