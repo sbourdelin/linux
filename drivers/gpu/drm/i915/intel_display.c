d7b7eb535f691358ba51b3e6bdea33d60c3b0b30
Shashank Sharma <shashank.sharma@intel.com>
2015-10-16 19:59:05 +0530

    drm/i915: Attach color properties to CRTC
    Function intel_attach_color_properties_to_crtc attaches a
    color property to its CRTC object. This patch calls this
    function from crtc initialization sequence.
    
    Signed-off-by: Shashank Sharma <shashank.sharma@intel.com>
    Signed-off-by: Kausal Malladi <kausalmalladi@gmail.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -13852,6 +13852,7 @@ static void intel_crtc_init(struct drm_device *dev, int pipe)
 	intel_crtc->cursor_size = ~0;
 
 	intel_crtc->wm.cxsr_allowed = true;
+	intel_attach_color_properties_to_crtc(dev, &intel_crtc->base);
 
 	BUG_ON(pipe >= ARRAY_SIZE(dev_priv->plane_to_crtc_mapping) ||
 	       dev_priv->plane_to_crtc_mapping[intel_crtc->plane] != NULL);
