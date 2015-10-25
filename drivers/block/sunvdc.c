24d3c8380aeee809110d9dfdecb9a42c1086f026
Punit Vara <punitvara@gmail.com>
2015-10-26 00:18:34 +0530

    block: Remove unnedded semicolon
    This patch is to the sunvdc.c file that removes unnecessary
    semicolon reported by coccicheck
    
    Signed-off-by: Punit Vara <punitvara@gmail.com>

--- drivers/block/sunvdc.c
+++ drivers/block/sunvdc.c
@@ -603,7 +603,7 @@ static int generic_request(struct vdc_port *port, u8 op, void *buf, int len)
 	case VD_OP_SET_EFI:
 		return -EOPNOTSUPP;
 		break;
-	};
+	}
 
 	map_perm |= LDC_MAP_SHADOW | LDC_MAP_DIRECT | LDC_MAP_IO;
 
