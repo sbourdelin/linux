dd6902aa59bbdf538677a4abb73ff47e11526f64
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2015-10-27 09:18:55 +0900

    powerpc/dma: Basic DMA operations shouldn't be GPL only
    When turning these from inline to exported functions I was a bit
    over-eager and made them GPL only. This basically prevent use of
    any non-GPL PCI driver which is a bit over the top. Bring them
    in line with other architecture.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kernel/dma.c
+++ arch/powerpc/kernel/dma.c
@@ -303,7 +303,7 @@ int dma_set_coherent_mask(struct device *dev, u64 mask)
 	dev->coherent_dma_mask = mask;
 	return 0;
 }
-EXPORT_SYMBOL_GPL(dma_set_coherent_mask);
+EXPORT_SYMBOL(dma_set_coherent_mask);
 
 #define PREALLOC_DMA_DEBUG_ENTRIES (1 << 16)
 
@@ -362,7 +362,7 @@ u64 dma_get_required_mask(struct device *dev)
 
 	return __dma_get_required_mask(dev);
 }
-EXPORT_SYMBOL_GPL(dma_get_required_mask);
+EXPORT_SYMBOL(dma_get_required_mask);
 
 static int __init dma_init(void)
 {
