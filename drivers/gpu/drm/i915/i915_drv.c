7647e014d70843bca6fecae0505e411333eb52cc
Deepak S <deepak.s@intel.com>
2015-10-27 10:14:56 -0700

    drm/i915/kbl: Add Kabylake PCI ID
    v2: separate out device info into different GT (Damien)
    v3: Add is_kabylake to the KBL gt3 structuer (Damien)
        Sort the platforms in older -> newer order (Damien)
    
    v4: Split platform definition since is_skylake=1 on
        kabylake structure was Nacked. (Rodrigo)
    
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Deepak S <deepak.s@intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -465,7 +465,10 @@ static const struct intel_device_info intel_kabylake_gt3_info = {
 	INTEL_SKL_GT1_IDS(&intel_skylake_info),	\
 	INTEL_SKL_GT2_IDS(&intel_skylake_info),	\
 	INTEL_SKL_GT3_IDS(&intel_skylake_gt3_info),	\
-	INTEL_BXT_IDS(&intel_broxton_info)
+	INTEL_BXT_IDS(&intel_broxton_info),		\
+	INTEL_KBL_GT1_IDS(&intel_kabylake_info),	\
+	INTEL_KBL_GT2_IDS(&intel_kabylake_info),	\
+	INTEL_KBL_GT3_IDS(&intel_kabylake_gt3_info)
 
 static const struct pci_device_id pciidlist[] = {		/* aka */
 	INTEL_PCI_IDS,
