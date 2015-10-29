834c9b74140d037c399e4b388be7ee3b5c169c01
Pranith Kumar <bobby.prani@gmail.com>
2015-10-29 15:19:29 -0400

    llist: Use for() initializer and conditional
    Simplify the code a bit by using the initializer and conditional
    check in for() instead of a separate if() statement in the loop.
    
    Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>

--- lib/llist.c
+++ lib/llist.c
@@ -66,10 +66,7 @@ struct llist_node *llist_del_first(struct llist_head *head)
 {
 	struct llist_node *entry, *old_entry, *next;
 
-	entry = smp_load_acquire(&head->first);
-	for (;;) {
-		if (entry == NULL)
-			return NULL;
+	for (entry = smp_load_acquire(&head->first); entry ; ) {
 		old_entry = entry;
 		next = READ_ONCE(entry->next);
 		entry = cmpxchg(&head->first, old_entry, next);
