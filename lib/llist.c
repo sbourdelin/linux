4c519a8053edc652b1ef23feed9fff284c9e7a7d
Pranith Kumar <bobby.prani@gmail.com>
2015-10-28 23:53:42 -0400

    llist: Use for conditional in llist_del_first()
    Simplify the code a bit by using the initializer and conditional
    check in for() instead of a separate if() statement in the loop.
    
    Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>

--- lib/llist.c
+++ lib/llist.c
@@ -66,10 +66,7 @@ struct llist_node *llist_del_first(struct llist_head *head)
 {
 	struct llist_node *entry, *old_entry, *next;
 
-	entry = head->first;
-	for (;;) {
-		if (entry == NULL)
-			return NULL;
+	for (entry = head->first; entry ;) {
 		old_entry = entry;
 		next = entry->next;
 		entry = cmpxchg(&head->first, old_entry, next);
