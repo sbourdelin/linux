6868c8d3c0653ad03fa969e3ca942d9f641c62d5
Thomas F Herbert <thomasfherbert@gmail.com>
2015-10-30 13:08:21 -0400

    Check for vlan ethernet types for 8021.q or 802.1ad
    This is to simplify using double tagged vlans. This function allows all
    valid vlan ethertypes to be checked in a single function call.
    
    Signed-off-by: Thomas F Herbert <thomasfherbert@gmail.com>

--- include/linux/if_vlan.h
+++ include/linux/if_vlan.h
@@ -627,6 +627,22 @@ static inline netdev_features_t vlan_features_check(const struct sk_buff *skb,
 
 	return features;
 }
+/**
+ * eth_type_vlan - check for valid vlan ether type.
+ * @ethertype: ether type to check
+ *
+ * Returns true if the ether type is a vlan ether type.
+ */
+static inline bool eth_type_vlan(__be16 ethertype)
+{
+	switch (ethertype) {
+	case htons(ETH_P_8021Q):
+	case htons(ETH_P_8021AD):
+		return true;
+	default:
+		return false;
+	}
+}
 
 /**
  * compare_vlan_header - Compare two vlan headers
