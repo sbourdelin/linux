6d292b5475868e58312fe3a35627e41334124e78
Aaron Conole <aconole@bytheb.org>
2015-11-02 12:01:59 -0500

    af_unix: optimize unix_writable by inlining
    unix_writable() originally was inlined, but was changed as part of
    commit 1586a5877db9 ("af_unix: do not report POLLOUT on
    listeners"). Re-enable the inline flag.
    
    Signed-off-by: Aaron Conole <aconole@bytheb.org>
    Cc: Eric Dumazet <edumazet@google.com>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -326,7 +326,7 @@ found:
 	return s;
 }
 
-static int unix_writable(const struct sock *sk)
+static inline int unix_writable(const struct sock *sk)
 {
 	return sk->sk_state != TCP_LISTEN &&
 	       (atomic_read(&sk->sk_wmem_alloc) << 2) <= sk->sk_sndbuf;
