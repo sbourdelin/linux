0744a6b9c27b5284c13ab91e4be7f8d690dbc83d
Abdul Hussain <hussain.abdul@outlook.com>
2015-11-02 07:00:14 +0000

    Staging: rtl8188eu: checkpatch cleanup for core/rtw_xmit.c
    This patch will remove the error reported by checkpatch.pl script
    
    Signed-off-by: Abdul Hussain <hussain.abdul@outlook.com>

--- drivers/staging/rtl8188eu/core/rtw_xmit.c
+++ drivers/staging/rtl8188eu/core/rtw_xmit.c
@@ -1702,12 +1702,12 @@ u32 rtw_get_ff_hwaddr(struct xmit_frame *pxmitframe)
 	return addr;
 }
 
-static void do_queue_select(struct adapter	*padapter, struct pkt_attrib *pattrib)
+static void do_queue_select(struct adapter *padapter, struct pkt_attrib *pattrib)
 {
 	u8 qsel;
 
 	qsel = pattrib->priority;
-	RT_TRACE(_module_rtl871x_xmit_c_, _drv_info_, ("### do_queue_select priority=%d , qsel = %d\n", pattrib->priority , qsel));
+	RT_TRACE(_module_rtl871x_xmit_c_, _drv_info_, ("### do_queue_select priority=%d , qsel = %d\n", pattrib->priority, qsel));
 
 	pattrib->qsel = qsel;
 }
