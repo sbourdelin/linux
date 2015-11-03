aeba933993de83ad68bc29a436eb4763c44c0e0f
Palmer Dabbelt <palmer@dabbelt.com>
2015-11-03 11:46:23 -0800

    Use sys_ni.c instead of #ifdef to disable fork on CONFIG_NOMMU
    I think this change actually doesn't do anything: __NR_fork was still
    being defined either way, and on my machine fork() in <unistd.h> comes
    from libc.
    
    This just moves to the standard mechanism for defining syscalls that
    aren't implemented instead, which has the side-effect of no longer
    having an #ifdef CONFIG_* in a user-visible header.
    
    Signed-off-by: Palmer Dabbelt <palmer@dabbelt.com>
    Reviewed-by: Andrew Waterman <waterman@eecs.berkeley.edu>
    Reviewed-by: Albert Ou <aou@eecs.berkeley.edu>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -869,11 +869,7 @@ __SYSCALL(__NR_uselib, sys_uselib)
 __SYSCALL(__NR__sysctl, sys_sysctl)
 
 #define __NR_fork 1079
-#ifdef CONFIG_MMU
 __SYSCALL(__NR_fork, sys_fork)
-#else
-__SYSCALL(__NR_fork, sys_ni_syscall)
-#endif /* CONFIG_MMU */
 
 #undef __NR_syscalls
 #define __NR_syscalls (__NR_fork+1)
