5216fdc02c79f1111c0a990fdd76713a958755bd
Ivan Safonov <insafonov@gmail.com>
2015-11-04 10:13:29 +0700

    staging: rtl8188eu: constant in both sides of loop condition added
    Now it is possible to replace (*pfwlen + remain) to fwlen
    and (*pfwlen + n) to roundup(pfwlen, 4).
    
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>

--- drivers/staging/rtl8188eu/hal/fw.c
+++ drivers/staging/rtl8188eu/hal/fw.c
@@ -89,7 +89,7 @@ static void _rtl88e_fill_dummy(u8 *pfwbuf, u32 *pfwlen)
 	remain = (remain == 0) ? 0 : (4 - remain);
 	n = remain;
 
-	for (remain = 0; remain < n; fwlen++, remain++) {
+	for (remain = 0; *pfwlen + remain < *pfwlen + n; fwlen++, remain++) {
 		pfwbuf[fwlen] = 0;
 	}
 
