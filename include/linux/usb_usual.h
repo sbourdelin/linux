5b94dda7430cd5be44dae98fef381ea967cf6452
Felipe Balbi <balbi@ti.com>
2015-11-04 14:48:32 -0600

    usb: storage: scsiglue: limit USB3 devices to 2048 sectors
    USB3 devices, because they are much newer, have much
    less chance of having issues with larger transfers.
    
    We still keep a limit because anything above 2048
    sectors really rendered negligible speed
    improvements, so we will simply ignore
    that. Transferring 1MiB should already give us
    pretty good performance.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- include/linux/usb_usual.h
+++ include/linux/usb_usual.h
@@ -79,6 +79,8 @@
 		/* Cannot handle MI_REPORT_SUPPORTED_OPERATION_CODES */	\
 	US_FLAG(MAX_SECTORS_240,	0x08000000)		\
 		/* Sets max_sectors to 240 */			\
+	US_FLAG(USB3,		0x10000000)			\
+		/* This is a USB3 Storage Device */		\
 
 #define US_FLAG(name, value)	US_FL_##name = value ,
 enum { US_DO_ALL_FLAGS };
