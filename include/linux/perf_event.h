0533af1e7b8a801f7895825435cb93816a3e893e
Stephane Eranian <eranian@google.com>
2015-11-04 05:12:19 +0100

    perf: fix RCU issues with cgroup monitoring mode
    This patch eliminates RCU violations detected by the RCU
    checker (PROVE_RCU). The impact code paths were all related
    to cgroup mode monitoring and involved access a task's cgrp.
    
    V2 is updated to include comments from PeterZ to eliminate
    some of the warnings without grabbing the rcu_read lock because
    we know we are already holding th ctx->lock which prevents
    the cgroup from disappearing while we are accessing it.
    The trick, as suggested by Peter, is to modify the
    perf_cgroup_from_task() to take an extra boolean parameter
    to allow bypassing the lockdep test in the task_subsys_cstate()
    macros. This patch uses this approach to update all calls the
    perf_cgroup_from_task().
    
    In V3, we change the boolean parameter for a pointer to a
    perf_event_context so we can check the ctx->lock explicitely.
    This is more robust, than passing the boolean to express that
    we know the lock is held. The code can change, and thus the
    locking assumption, checking lockdep_is_held() ensures,
    the proper locking is in place. Patch relative to tip.git
    at commit 57ef9fc.
    
    Signed-off-by: Stephane Eranian <eranian@google.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -697,9 +697,10 @@ struct perf_cgroup {
  * if there is no cgroup event for the current CPU context.
  */
 static inline struct perf_cgroup *
-perf_cgroup_from_task(struct task_struct *task)
+perf_cgroup_from_task(struct task_struct *task, struct perf_event_context *ctx)
 {
-	return container_of(task_css(task, perf_event_cgrp_id),
+	bool safe = ctx ? lockdep_is_held(&ctx->lock) : true;
+	return container_of(task_css_check(task, perf_event_cgrp_id, safe),
 			    struct perf_cgroup, css);
 }
 #endif /* CONFIG_CGROUP_PERF */
