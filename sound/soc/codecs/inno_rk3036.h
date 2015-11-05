bf5edf39591ff8b856b7f215b9aed2995b74ccfc
ZhengShunQian <zhengsq@rock-chips.com>
2015-11-05 17:53:13 +0800

    ASoC: codec: Inno codec driver for RK3036 SoC
    RK3036 SoC integrated with an Inno audio codec.
    This driver implements the functions of it.
    
    There is not need a special machine driver, since the
    simple-card machine driver works perfect in this case.
    
    Signed-off-by: ZhengShunQian <zhengsq@rock-chips.com>

---
+++ sound/soc/codecs/inno_rk3036.h
@@ -0,0 +1,120 @@
+/*
+ * Driver of Inno Codec for rk3036 by Rockchip Inc.
+ *
+ * Author: Zheng ShunQian<zhengsq@rock-chips.com>
+ */
+
+#ifndef _INNO_RK3036_CODEC_H
+#define _INNO_RK3036_CODEC_H
+
+/* codec registers */
+#define INNO_R00	0x00
+#define INNO_R01	0x0c
+#define INNO_R02	0x10
+#define INNO_R03	0x14
+#define INNO_R04	0x88
+#define INNO_R05	0x8c
+#define INNO_R06	0x90
+#define INNO_R07	0x94
+#define INNO_R08	0x98
+#define INNO_R09	0x9c
+#define INNO_R10	0xa0
+
+/* register bit filed */
+#define INNO_R00_CSR_RESET		BIT(0) /*codec system reset*/
+#define INNO_R00_CSR_WORK		BIT(0)
+#define INNO_R00_CDCR_RESET		BIT(1) /*codec digital core reset*/
+#define INNO_R00_CDCR_WORK		BIT(1)
+#define INNO_R00_PRB_DISABLE		BIT(6) /*power reset bypass*/
+#define INNO_R00_PRB_ENABLE		BIT(6)
+
+#define INNO_R01_I2SMODE_MSK		BIT(4)
+#define INNO_R01_I2SMODE_SLAVE		BIT(4)
+#define INNO_R01_I2SMODE_MASTER		BIT(4)
+#define INNO_R01_PINDIR_MSK		BIT(5)
+#define INNO_R01_PINDIR_IN_SLAVE	BIT(5) /*direction of pin*/
+#define INNO_R01_PINDIR_OUT_MASTER	BIT(5)
+
+#define INNO_R02_LRS_MSK		BIT(2)
+#define INNO_R02_LRS_NORMAL		BIT(2) /*DAC Left Right Swap*/
+#define INNO_R02_LRS_SWAP		BIT(2)
+#define INNO_R02_DACM_MSK		BIT(3)
+#define INNO_R02_DACM_PCM		BIT(3) /*DAC Mode*/
+#define INNO_R02_DACM_I2S		BIT(3)
+#define INNO_R02_DACM_LJM		BIT(3)
+#define INNO_R02_DACM_RJM		BIT(3)
+#define INNO_R02_VWL_MSK		BIT(5)
+#define INNO_R02_VWL_32BIT		BIT(5) /*1/2Frame Valid Word Length*/
+#define INNO_R02_VWL_24BIT		BIT(5)
+#define INNO_R02_VWL_20BIT		BIT(5)
+#define INNO_R02_VWL_16BIT		BIT(5)
+#define INNO_R02_LRCP_MSK		BIT(7)
+#define INNO_R02_LRCP_NORMAL		BIT(7) /*Left Right Polarity*/
+#define INNO_R02_LRCP_REVERSAL		BIT(7)
+
+#define INNO_R03_BCP_MSK		BIT(0)
+#define INNO_R03_BCP_NORMAL		BIT(0) /*DAC bit clock polarity*/
+#define INNO_R03_BCP_REVERSAL		BIT(0)
+#define INNO_R03_DACR_MSK		BIT(1)
+#define INNO_R03_DACR_RESET		BIT(1) /*DAC Reset*/
+#define INNO_R03_DACR_WORK		BIT(1)
+#define INNO_R03_FWL_MSK		BIT(2)
+#define INNO_R03_FWL_32BIT		BIT(2) /*1/2Frame Word Length*/
+#define INNO_R03_FWL_24BIT		BIT(2)
+#define INNO_R03_FWL_20BIT		BIT(2)
+#define INNO_R03_FWL_16BIT		BIT(2)
+
+#define INNO_R04_DACR_SW_SHIFT		0
+#define INNO_R04_DACL_SW_SHIFT		1
+#define INNO_R04_DACR_CLK_SHIFT		2
+#define INNO_R04_DACL_CLK_SHIFT		3
+#define INNO_R04_DACR_VREF_SHIFT	4
+#define INNO_R04_DACL_VREF_SHIFT	5
+
+#define INNO_R05_HPR_EN_SHIFT		0
+#define INNO_R05_HPL_EN_SHIFT		1
+#define INNO_R05_HPR_WORK_SHIFT		2
+#define INNO_R05_HPL_WORK_SHIFT		3
+
+#define INNO_R06_VOUTR_CZ_SHIFT		0
+#define INNO_R06_VOUTL_CZ_SHIFT		1
+#define INNO_R06_DACR_HILO_VREF_SHIFT	2
+#define INNO_R06_DACL_HILO_VREF_SHIFT	3
+#define INNO_R06_DAC_EN_SHIFT		5
+
+#define INNO_R06_DAC_PRECHARGE		BIT(4) /*PreCharge control for DAC*/
+#define INNO_R06_DAC_DISCHARGE		BIT(4)
+
+#define INNO_HP_GAIN_SHIFT		0
+/* Gain of output, 1.5db step: -39db(0x0) ~ 0db(0x1a) ~ 6db(0x1f) */
+#define INNO_HP_GAIN_0DB		0x1a
+#define INNO_HP_GAIN_N39DB		0x0
+
+#define INNO_R09_HPR_ANITPOP_SHIFT	0
+#define INNO_R09_HPL_ANITPOP_SHIFT	2
+#define INNO_R09_HPR_MUTE_SHIFT		4
+#define INNO_R09_HPL_MUTE_SHIFT		5
+#define INNO_R09_DACR_SWITCH_SHIFT	6
+#define INNO_R09_DACL_SWITCH_SHIFT	7
+
+#define INNO_R10_CHARGE_SEL_CUR_400I_YES	BIT(0)
+#define INNO_R10_CHARGE_SEL_CUR_400I_NO		BIT(0)
+#define INNO_R10_CHARGE_SEL_CUR_260I_YES	BIT(1)
+#define INNO_R10_CHARGE_SEL_CUR_260I_NO		BIT(1)
+#define INNO_R10_CHARGE_SEL_CUR_130I_YES	BIT(2)
+#define INNO_R10_CHARGE_SEL_CUR_130I_NO		BIT(2)
+#define INNO_R10_CHARGE_SEL_CUR_100I_YES	BIT(3)
+#define INNO_R10_CHARGE_SEL_CUR_100I_NO		BIT(3)
+#define INNO_R10_CHARGE_SEL_CUR_050I_YES	BIT(4)
+#define INNO_R10_CHARGE_SEL_CUR_050I_NO		BIT(4)
+#define INNO_R10_CHARGE_SEL_CUR_027I_YES	BIT(5)
+#define INNO_R10_CHARGE_SEL_CUR_027I_NO		BIT(5)
+
+#define INNO_R10_MAX_CUR (INNO_R10_CHARGE_SEL_CUR_400I_YES | \
+			  INNO_R10_CHARGE_SEL_CUR_260I_YES | \
+			  INNO_R10_CHARGE_SEL_CUR_130I_YES | \
+			  INNO_R10_CHARGE_SEL_CUR_100I_YES | \
+			  INNO_R10_CHARGE_SEL_CUR_050I_YES | \
+			  INNO_R10_CHARGE_SEL_CUR_027I_YES)
+
+#endif
