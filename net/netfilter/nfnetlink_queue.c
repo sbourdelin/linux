ab3c2860f8a8a3003aa4f4b3c4ec0fa95e1d0558
Dmitry Safonov <0x7f454c46@gmail.com>
2015-11-06 22:35:44 +0300

    net: netfilter: fix false positive GCC warnings
    With x86_64_defconfig:
    GCC thinks that in nfulnl_recv_config flags parameter is not inited but
    it was under the same condition (nfula[NFULA_CFG_FLAGS] == true).
    Suppress this warning:
            net/netfilter/nfnetlink_log.c: In function ‘nfulnl_recv_config’:
            net/netfilter/nfnetlink_log.c:320:14: warning: ‘flags’ may be used uninitialized in this function [-Wmaybe-uninitialized]
            inst->flags = flags;
                        ^
    GCC thinks that nfnl_ct wasn't inited, but it is used only under
    	if (ct && diff)
    where ct is not NULL only when nfnl_ct is inited.
    
    Reported-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Dmitry Safonov <0x7f454c46@gmail.com>

--- net/netfilter/nfnetlink_queue.c
+++ net/netfilter/nfnetlink_queue.c
@@ -313,7 +313,7 @@ nfqnl_build_packet_message(struct net *net, struct nfqnl_instance *queue,
 	struct net_device *outdev;
 	struct nf_conn *ct = NULL;
 	enum ip_conntrack_info uninitialized_var(ctinfo);
-	struct nfnl_ct_hook *nfnl_ct;
+	struct nfnl_ct_hook *uninitialized_var(nfnl_ct);
 	bool csum_verify;
 	char *secdata = NULL;
 	u32 seclen = 0;
@@ -1041,7 +1041,7 @@ nfqnl_recv_verdict(struct sock *ctnl, struct sk_buff *skb,
 	unsigned int verdict;
 	struct nf_queue_entry *entry;
 	enum ip_conntrack_info uninitialized_var(ctinfo);
-	struct nfnl_ct_hook *nfnl_ct;
+	struct nfnl_ct_hook *uninitialized_var(nfnl_ct);
 	struct nf_conn *ct = NULL;
 
 	struct net *net = sock_net(ctnl);
