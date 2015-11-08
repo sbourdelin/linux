e740c2dc3d324ceae866481a889fed6a695f293e
Sinan Kaya <okaya@codeaurora.org>
2015-11-07 23:52:57 -0500

    dma: qcom_bam_dma: move to qcom directory
    Creating a QCOM directory for all QCOM DMA
    source files.
    
    Signed-off-by: Sinan Kaya <okaya@codeaurora.org>

--- drivers/dma/qcom/bam_dma.c
+++ drivers/dma/qcom/bam_dma.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -49,8 +49,8 @@
 #include <linux/clk.h>
 #include <linux/dmaengine.h>
 
-#include "dmaengine.h"
-#include "virt-dma.h"
+#include "../dmaengine.h"
+#include "../virt-dma.h"
 
 struct bam_desc_hw {
 	u32 addr;		/* Buffer physical address */
