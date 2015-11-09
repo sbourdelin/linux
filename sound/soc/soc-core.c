12efb4da6648fc1add0e27b3aa38322bd2a71f6b
Jeeja KP <jeeja.kp@intel.com>
2015-11-09 23:19:59 +0530

    ASoC: core: Adds support for cpu loopback dai_link
    soc_link_dai_widgets() creates codec-codec loopback link which
    allows DPCM to trigger these links and program the parameters for
    these links These links are:
    
    	Codec1 Tx (Pb)  ----------> Rx Codec2 (Cap)
    	Codec2 Tx (Cap) <---------- Tx Codec1 (Pb)
    
    But as we try to model a CPU based loops for non host links like
    modem to codec loop, the above mapping gets wrong as:
    
    	CPU Tx (Pb)  -----------> Codec Tx (Pb)
    	CPU Rx (Cap) <----------- Codec Rx (Cap)
    
    So we add a new flag to handle above scenario and link widgets as
    depecited above
    
    Signed-off-by: Jeeja KP <jeeja.kp@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1290,8 +1290,14 @@ static int soc_link_dai_widgets(struct snd_soc_card *card,
 		dev_warn(card->dev, "ASoC: Multiple codecs not supported yet\n");
 
 	/* link the DAI widgets */
-	sink = codec_dai->playback_widget;
-	source = cpu_dai->capture_widget;
+	if (!dai_link->cpu_loopback) {
+		sink = codec_dai->playback_widget;
+		source = cpu_dai->capture_widget;
+	} else {
+		sink = codec_dai->playback_widget;
+		source = cpu_dai->playback_widget;
+	}
+
 	if (sink && source) {
 		ret = snd_soc_dapm_new_pcm(card, dai_link->params,
 					   dai_link->num_params,
@@ -1303,8 +1309,14 @@ static int soc_link_dai_widgets(struct snd_soc_card *card,
 		}
 	}
 
-	sink = cpu_dai->playback_widget;
-	source = codec_dai->capture_widget;
+	if (!dai_link->cpu_loopback) {
+		sink = cpu_dai->playback_widget;
+		source = codec_dai->capture_widget;
+	} else {
+		sink = cpu_dai->capture_widget;
+		source = codec_dai->capture_widget;
+	}
+
 	if (sink && source) {
 		ret = snd_soc_dapm_new_pcm(card, dai_link->params,
 					   dai_link->num_params,
