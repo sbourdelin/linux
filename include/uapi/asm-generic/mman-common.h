22930fa9fa6fc988cdbd9112d8b7298a79d812d1
Palmer Dabbelt <palmer@dabbelt.com>
2015-11-09 17:31:01 -0800

    Always expose MAP_UNINITIALIZED to userspace
    This used to be hidden behind CONFIG_MMAP_ALLOW_UNINITIALIZED, so
    userspace wouldn't actually ever see it be non-zero.  While I had
    originally hoped to avoid hiding it, it looks like this conflicts with
    MAP_HUGE_SHIFT so I think it's safer to just keep this 0.
    
    Architectures that want to define this can still override it.  In
    fact, the Xtensa port already overrides it in a very similar manner to
    the previously broken one (but due to lots of conflicting opinions on
    how to solve this correctly, I'm just taking the easy way out and
    letting their arch maintainers deal with it -- sorry).
    
    Signed-off-by: Palmer Dabbelt <palmer@dabbelt.com>
    Reviewed-by: Andrew Waterman <waterman@eecs.berkeley.edu>
    Reviewed-by: Albert Ou <aou@eecs.berkeley.edu>

--- include/uapi/asm-generic/mman-common.h
+++ include/uapi/asm-generic/mman-common.h
@@ -19,9 +19,7 @@
 #define MAP_TYPE	0x0f		/* Mask for type of mapping */
 #define MAP_FIXED	0x10		/* Interpret addr exactly */
 #define MAP_ANONYMOUS	0x20		/* don't use a file */
-#ifdef CONFIG_MMAP_ALLOW_UNINITIALIZED
-# define MAP_UNINITIALIZED 0x4000000	/* For anonymous mmap, memory could be uninitialized */
-#else
+#ifndef MAP_UNINITIALIZED
 # define MAP_UNINITIALIZED 0x0		/* Don't support this flag */
 #endif
 
