484566e4a6af856a0862fcf604e2dc05ae5dd474
Chris Brandt <chris.brandt@renesas.com>
2015-11-11 09:17:35 -0500

    ARM: xip: Use correct symbol for end of ROM marker
    For an XIP build, _edata_loc, not _etext, represents the end of the
    binary image that will be programmed into ROM and mapped into the
    MODULES_VADDR area.
    With an XIP kernel, nothing is loaded into RAM before boot, meaning
    you have to take into account the size of the entire binary image
    that was programmed, including the init data values that will be copied
    to RAM during kernel boot.
    
    This fixes the bug where you might lose the end of your kernel area
    after page table setup is complete.
    
    Signed-off-by: Chris Brandt <chris.brandt@renesas.com>

--- include/asm-generic/sections.h
+++ include/asm-generic/sections.h
@@ -26,7 +26,7 @@
  *	__ctors_start, __ctors_end
  */
 extern char _text[], _stext[], _etext[];
-extern char _data[], _sdata[], _edata[];
+extern char _data[], _sdata[], _edata[], _edata_loc[];
 extern char __bss_start[], __bss_stop[];
 extern char __init_begin[], __init_end[];
 extern char _sinittext[], _einittext[];
