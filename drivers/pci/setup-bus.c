1ac206d24250251fcc88771d9141ac2b7034cbc0
Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
2015-11-17 17:03:56 +0000

    drivers: pci: add generic code to claim bus resources
    PCI core code contains a set of functions, eg:
    
    pci_assign_unassigned_bus_resources()
    
    that allow to assign the PCI resources for a given bus after
    enumeration.
    
    On systems where the PCI BARs are immutable (ie they must not and can
    not be assigned), PCI resources must be claimed in order to be
    validated and inserted in the PCI resources tree, but there is no generic
    PCI kernel function for that purpose and the resource claiming is
    implemented in an arch specific fashion which resulted in arches
    implementations that contain duplicated code.
    
    This patch, based on the ia64 resource claiming arch implementation,
    implements a set of functions in core PCI code that provides a PCI core
    interface for resources claiming for a given PCI bus hierarchy, paving
    the way for further resource claiming consolidation across architectures.
    
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Yinghai Lu <yinghai@kernel.org>

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -1424,6 +1424,48 @@ void pci_bus_assign_resources(const struct pci_bus *bus)
 }
 EXPORT_SYMBOL(pci_bus_assign_resources);
 
+static void pci_claim_device_resources(struct pci_dev *dev)
+{
+	int i;
+
+	for (i = 0; i < PCI_BRIDGE_RESOURCES; i++) {
+		struct resource *r = &dev->resource[i];
+
+		if (!r->flags || r->parent)
+			continue;
+
+		pci_claim_resource(dev, i);
+	}
+}
+
+static void pci_claim_bridge_resources(struct pci_dev *dev)
+{
+	int i;
+
+	for (i = PCI_BRIDGE_RESOURCES; i < PCI_NUM_RESOURCES; i++) {
+		struct resource *r = &dev->resource[i];
+
+		if (!r->flags || r->parent)
+			continue;
+
+		pci_claim_bridge_resource(dev, i);
+	}
+}
+
+void pci_bus_claim_resources(struct pci_bus *b)
+{
+	struct pci_dev *dev;
+
+	if (b->self) {
+		pci_read_bridge_bases(b);
+		pci_claim_bridge_resources(b->self);
+	}
+
+	list_for_each_entry(dev, &b->devices, bus_list)
+		pci_claim_device_resources(dev);
+}
+EXPORT_SYMBOL(pci_bus_claim_resources);
+
 static void __pci_bridge_assign_resources(const struct pci_dev *bridge,
 					  struct list_head *add_head,
 					  struct list_head *fail_head)
