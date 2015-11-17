dfa13a7d16a5bc9720a88d61fbd0b86820e9fc3d
Alexander Aring <alex.aring@gmail.com>
2015-11-17 23:33:14 +0100

    6lowpan: iphc: add support for stateful compression
    This patch introduce support for IPHC stateful address compression. It
    will offer three debugfs per interface entries, which are:
    
     - dci_table: destination context indentifier table
     - sci_table: source context indentifier table
     - mcast_sci_table: multicast context identifier table
    
    Example to setup a context id:
    
    A "cat /sys/kernel/debug/6lowpan/lowpan0/dci_table" will display all
    contexts which are available. Example:
    
    ID ipv6-address/prefix-length                  state
    0  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    1  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    2  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    3  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    4  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    5  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    6  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    7  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    8  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    9  0000:0000:0000:0000:0000:0000:0000:0000/000 0
    10 0000:0000:0000:0000:0000:0000:0000:0000/000 0
    11 0000:0000:0000:0000:0000:0000:0000:0000/000 0
    12 0000:0000:0000:0000:0000:0000:0000:0000/000 0
    13 0000:0000:0000:0000:0000:0000:0000:0000/000 0
    14 0000:0000:0000:0000:0000:0000:0000:0000/000 0
    15 0000:0000:0000:0000:0000:0000:0000:0000/000 0
    
    For setting a context e.g. context id 0, context 2001::, prefix-length
    64.
    
    Hint: Simple copy one line and then maniuplate it.
    
    echo "0 2001:0000:0000:0000:0000:0000:0000:0000/64 1" >
    /sys/kernel/debug/6lowpan/lowpan0/dci_table
    
    The state will display currently if the context is disabled(0) or
    enabled(1).
    
    On transmit side:
    
    The IPHC code will automatically search for a context which would be
    match for the address. Then it will be use the context which the
    best compression, means the longest prefix which match will be used.
    
    Example:
    
    2001::/126 vs 2001::/127 - the 2001::/127 can be full compressed if the
    last bit of the address which has the prefix 2001::/127 is the same like
    the IID from the Encapsulating Header. A context ID can also be a
    2001::1/128, which is then a full ipv6 address.
    
    On Receive side:
    
    If there is a context defined (when CID not available then it's the
    default context 0) then it will be used, if the header doesn't set
    SAC or DAC bit thens, it will be dropped.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>

--- net/6lowpan/core.c
+++ net/6lowpan/core.c
@@ -19,12 +19,29 @@
 
 int lowpan_netdev_setup(struct net_device *dev, enum lowpan_lltypes lltype)
 {
+	int i;
+
 	dev->addr_len = EUI64_ADDR_LEN;
 	dev->type = ARPHRD_6LOWPAN;
 	dev->mtu = IPV6_MIN_MTU;
 	dev->priv_flags |= IFF_NO_QUEUE;
 
 	lowpan_priv(dev)->lltype = lltype;
+
+	spin_lock_init(&lowpan_priv(dev)->iphc_dci.lock);
+	lowpan_priv(dev)->iphc_dci.ops = &iphc_ctx_unicast_ops;
+
+	spin_lock_init(&lowpan_priv(dev)->iphc_sci.lock);
+	lowpan_priv(dev)->iphc_sci.ops = &iphc_ctx_unicast_ops;
+
+	spin_lock_init(&lowpan_priv(dev)->iphc_mcast_dci.lock);
+	lowpan_priv(dev)->iphc_mcast_dci.ops = &iphc_ctx_mcast_ops;
+
+	for (i = 0; i < LOWPAN_IPHC_CI_TABLE_SIZE; i++) {
+		lowpan_priv(dev)->iphc_dci.table[i].id = i;
+		lowpan_priv(dev)->iphc_sci.table[i].id = i;
+		lowpan_priv(dev)->iphc_mcast_dci.table[i].id = i;
+	}
 
 	return lowpan_dev_debugfs_init(dev);
 }
