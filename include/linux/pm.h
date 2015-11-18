f0105ae40c45cc17745515ed1ed558e3bfd1860e
Imre Deak <imre.deak@intel.com>
2015-11-18 11:16:18 +0200

    PCI / PM: tune down RPM suspend error message with EBUSY and EAGAIN retval
    The runtime PM core doesn't treat EBUSY and EAGAIN retvals from the driver
    suspend hooks as errors, but they still show up as errors in dmesg. Tune
    them down.
    
    One problem caused by this was noticed by Daniel: the i915 driver
    returns EAGAIN to signal a temporary failure to suspend and as a request
    towards the RPM core for scheduling a suspend again. This is a normal
    event, but the resulting error message flags a breakage during the
    driver's automated testing which parses dmesg and picks up the error.
    
    Reported-by: Daniel Vetter <daniel.vetter@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -702,11 +702,17 @@ extern int dpm_suspend_late(pm_message_t state);
 extern int dpm_suspend(pm_message_t state);
 extern int dpm_prepare(pm_message_t state);
 
-extern void __suspend_report_result(const char *function, void *fn, int ret);
+extern void __suspend_report_result(const char *function, void *fn, int ret,
+				    bool runtime_pm);
 
 #define suspend_report_result(fn, ret)					\
 	do {								\
-		__suspend_report_result(__func__, fn, ret);		\
+		__suspend_report_result(__func__, fn, ret, false);	\
+	} while (0)
+
+#define rpm_suspend_report_result(fn, ret)				\
+	do {								\
+		__suspend_report_result(__func__, fn, ret, true);	\
 	} while (0)
 
 extern int device_pm_wait_for_dev(struct device *sub, struct device *dev);
