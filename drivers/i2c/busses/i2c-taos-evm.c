8baa84aa08ae19b6916798a369c1ee81909e1b13
LABBE Corentin <clabbe.montjoie@gmail.com>
2015-11-18 09:00:53 +0100

    i2c: taos-evm: replace simple_strtoul by kstrtou8
    The simple_strtoul function is marked as obsolete.
    This patch replace it by kstrtou8.
    
    Signed-off-by: LABBE Corentin <clabbe.montjoie@gmail.com>

--- drivers/i2c/busses/i2c-taos-evm.c
+++ drivers/i2c/busses/i2c-taos-evm.c
@@ -70,6 +70,7 @@ static int taos_smbus_xfer(struct i2c_adapter *adapter, u16 addr,
 	struct serio *serio = adapter->algo_data;
 	struct taos_data *taos = serio_get_drvdata(serio);
 	char *p;
+	int err;
 
 	/* Encode our transaction. "@" is for the device address, "$" for the
 	   SMBus command and "#" for the data. */
@@ -130,7 +131,14 @@ static int taos_smbus_xfer(struct i2c_adapter *adapter, u16 addr,
 			return 0;
 	} else {
 		if (p[0] == 'x') {
-			data->byte = simple_strtol(p + 1, NULL, 16);
+			/*
+			 * voluntary dropping error code of kstrtou8 since all
+			 * error code that it could return are invalid according
+			 * to Documentation/i2c/fault-codes
+			 */
+			err = kstrtou8(p + 1, 16, &data->byte);
+			if (err)
+				return -EPROTO;
 			return 0;
 		}
 	}
