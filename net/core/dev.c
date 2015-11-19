c149ff46675edffc11f85c2f4de0d816f1448989
Eric Dumazet <edumazet@google.com>
2015-11-19 11:42:50 -0800

    net: remove useless check in napi_gro_frags()
    Checking if skb is NULL in napi_gro_frags() is too late.
    
    If skb was NULL, we would crash earlier in napi_frags_skb()
    
    Drivers normally catch napi_get_frags() NULL return value
    before calling napi_gro_frags()
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4465,9 +4465,6 @@ gro_result_t napi_gro_frags(struct napi_struct *napi)
 {
 	struct sk_buff *skb = napi_frags_skb(napi);
 
-	if (!skb)
-		return GRO_DROP;
-
 	trace_napi_gro_frags_entry(skb);
 
 	return napi_frags_finish(napi, skb, dev_gro_receive(napi, skb));
