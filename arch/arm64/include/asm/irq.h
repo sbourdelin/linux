e569894fb68bf9fbba343d511a3ab8fd95ecf890
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2015-11-20 11:25:04 -0500

    xen/events: Always allocate legacy interrupts on PV guests
    After commit 8c058b0b9c34 ("x86/irq: Probe for PIC presence before
    allocating descs for legacy IRQs") early_irq_init() will no longer
    preallocate descriptors for legacy interrupts if PIC does not
    exist, which is the case for Xen PV guests.
    
    Therefore we may need to allocate those descriptors ourselves.
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/arm64/include/asm/irq.h
+++ arch/arm64/include/asm/irq.h
@@ -7,4 +7,10 @@ struct pt_regs;
 
 extern void set_handle_irq(void (*handle_irq)(struct pt_regs *));
 
+#define NR_IRQS_LEGACY  0
+static inline int nr_legacy_irqs(void)
+{
+	return NR_IRQS_LEGACY;
+}
+
 #endif
