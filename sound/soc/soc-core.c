29755ff880632d00e56f1e15b68e788671059e4d
Mengdong Lin <mengdong.lin@linux.intel.com>
2015-11-23 11:04:04 -0500

    ASoC: Implement DAI links in a list & define API to add/remove a link
    Implement a dai link list for the soc card.
    
    Add APIs to add/remove a DAI links dynamically, e.g. by topology.
    
    And a dobj is embedded into the struct snd_soc_dai_link. Topology can
    use the dobj to find the links created by it and remove them when the
    topology component is unloaded.
    
    The predefined DAI links are reserved to keep backward compatibility.
    And they will also be added to the list.
    
    Signed-off-by: Mengdong Lin <mengdong.lin@linux.intel.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1120,6 +1120,7 @@ static void soc_remove_dai_links(struct snd_soc_card *card)
 {
 	int order;
 	struct snd_soc_pcm_runtime *rtd;
+	struct snd_soc_dai_link *link, *_link;
 
 	for (order = SND_SOC_COMP_ORDER_FIRST; order <= SND_SOC_COMP_ORDER_LAST;
 			order++) {
@@ -1131,6 +1132,14 @@ static void soc_remove_dai_links(struct snd_soc_card *card)
 			order++) {
 		list_for_each_entry(rtd, &card->rtd_list, list)
 			soc_remove_link_components(card, rtd, order);
+	}
+
+	list_for_each_entry_safe(link, _link, &card->dai_link_list, list) {
+		if (link->dobj.type)
+			dev_warn(card->dev, "Forgot to remove link %s?\n",
+				link->name);
+		list_del(&link->list);
+		card->num_dai_links--;
 	}
 }
 
@@ -1227,6 +1236,52 @@ static int soc_init_dai_link(struct snd_soc_card *card,
 
 	return 0;
 }
+
+/**
+ * snd_soc_add_dai_link - Add a DAI link dynamically
+ * @card: The ASoC card to which the DAI link is added
+ * @dai_link: The new DAI link to add
+ *
+ * This function adds a DAI link to the ASoC card's link list.
+ *
+ * Note: Topology can use this API to add DAI links when probing the
+ * topology component. And machine drivers can still define static
+ * DAI links in dai_link array.
+ */
+void snd_soc_add_dai_link(struct snd_soc_card *card,
+		struct snd_soc_dai_link *dai_link)
+{
+	lockdep_assert_held(&client_mutex);
+	list_add_tail(&dai_link->list, &card->dai_link_list);
+	card->num_dai_links++;
+}
+EXPORT_SYMBOL_GPL(snd_soc_add_dai_link);
+
+/**
+ * snd_soc_remove_dai_link - Remove a DAI link from the list
+ * @card: The ASoC card that owns the link
+ * @dai_link: The DAI link to remove
+ *
+ * This function removes a DAI link from the ASoC card's link list.
+ *
+ * For DAI links previously added by topology, topology should
+ * remove them by using the dobj embedded in the link.
+ */
+void snd_soc_remove_dai_link(struct snd_soc_card *card,
+			     struct snd_soc_dai_link *dai_link)
+{
+	struct snd_soc_dai_link *link, *_link;
+
+	lockdep_assert_held(&client_mutex);
+	list_for_each_entry_safe(link, _link, &card->dai_link_list, list) {
+		if (link == dai_link) {
+			list_del(&link->list);
+			card->num_dai_links--;
+			return;
+		}
+	}
+}
+EXPORT_SYMBOL_GPL(snd_soc_remove_dai_link);
 
 static void soc_set_name_prefix(struct snd_soc_card *card,
 				struct snd_soc_component *component)
@@ -1721,6 +1776,10 @@ static int snd_soc_instantiate_card(struct snd_soc_card *card)
 		if (ret != 0)
 			goto base_error;
 	}
+
+	/* add predefined DAI links to the list */
+	for (i = 0; i < card->num_links; i++)
+		snd_soc_add_dai_link(card, card->dai_link+i);
 
 	/* initialize the register cache for each available codec */
 	list_for_each_entry(codec, &codec_list, list) {
@@ -2478,6 +2537,9 @@ int snd_soc_register_card(struct snd_soc_card *card)
 	dev_set_drvdata(card->dev, card);
 
 	snd_soc_initialize_card_lists(card);
+
+	INIT_LIST_HEAD(&card->dai_link_list);
+	card->num_dai_links = 0;
 
 	INIT_LIST_HEAD(&card->rtd_list);
 	card->num_rtd = 0;
