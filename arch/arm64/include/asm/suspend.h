1b84a61a32ef9a10329cdcb98d602391749b9f6b
James Morse <james.morse@arm.com>
2015-11-26 17:32:44 +0000

    arm64: Change cpu_resume() to enable mmu early then access sleep_sp by va
    By enabling the MMU early in cpu_resume(), the sleep_save_sp and stack can
    be accessed by VA, which avoids the need to convert-addresses and clean to
    PoC on the suspend path.
    
    MMU setup is shared with the boot path, meaning the swapper_pg_dir is
    restored directly: ttbr1_el1 is no longer saved/restored.
    
    struct sleep_save_sp is removed, replacing it with a single array of
    pointers.
    
    cpu_do_{suspend,resume} could be further reduced to not restore: cpacr_el1,
    mdscr_el1, tcr_el1, vbar_el1 and sctlr_el1, all of which are set by
    __cpu_setup(). However these values all contain res0 bits that may be used
    to enable future features.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/arm64/include/asm/suspend.h
+++ arch/arm64/include/asm/suspend.h
@@ -1,7 +1,7 @@
 #ifndef __ASM_SUSPEND_H
 #define __ASM_SUSPEND_H
 
-#define NR_CTX_REGS 11
+#define NR_CTX_REGS 10
 #define NR_CALLEE_SAVED_REGS 12
 
 /*
@@ -16,11 +16,6 @@ struct cpu_suspend_ctx {
 	u64 ctx_regs[NR_CTX_REGS];
 	u64 sp;
 } __aligned(16);
-
-struct sleep_save_sp {
-	phys_addr_t *save_ptr_stash;
-	phys_addr_t save_ptr_stash_phys;
-};
 
 /*
  * Memory to save the cpu state is allocated on the stack by
