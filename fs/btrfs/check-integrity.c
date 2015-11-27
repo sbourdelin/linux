0484bd8ea6734af5a0a28c81568e86f188785908
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2015-11-27 09:11:31 +0100

    btrfs: use kbasename in btrfsic_mount
    This is more readable.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>

--- fs/btrfs/check-integrity.c
+++ fs/btrfs/check-integrity.c
@@ -95,6 +95,7 @@
 #include <linux/genhd.h>
 #include <linux/blkdev.h>
 #include <linux/vmalloc.h>
+#include <linux/string.h>
 #include "ctree.h"
 #include "disk-io.h"
 #include "hash.h"
@@ -3136,11 +3137,7 @@ int btrfsic_mount(struct btrfs_root *root,
 		ds->state = state;
 		bdevname(ds->bdev, ds->name);
 		ds->name[BDEVNAME_SIZE - 1] = '\0';
-		for (p = ds->name; *p != '\0'; p++);
-		while (p > ds->name && *p != '/')
-			p--;
-		if (*p == '/')
-			p++;
+		p = kbasename(ds->name);
 		strlcpy(ds->name, p, sizeof(ds->name));
 		btrfsic_dev_state_hashtable_add(ds,
 						&btrfsic_dev_state_hashtable);
