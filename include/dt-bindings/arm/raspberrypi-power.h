62f947bff47639b67708fd950357adae04ce9b29
Alexander Aring <alex.aring@gmail.com>
2015-11-29 17:59:31 +0100

    ARM: bcm2835: add rpi power domain driver
    This patch adds support for RPi several Power Domains and enable support
    to enable the USB Power Domain when it's not enabled before.
    
    This patch based on Eric Anholt's patch to support Power Domains. He had
    an issue about -EPROBE_DEFER inside the power domain subsystem, this
    issue was solved by commit <311fa6a> ("PM / Domains: Return -EPROBE_DEFER
    if we fail to init or turn-on domain").
    
    Cc: Stephen Warren <swarren@wwwdotorg.org>
    Cc: Lee Jones <lee@kernel.org>
    Cc: Eric Anholt <eric@anholt.net>
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Eric Anholt <eric@anholt.net>

---
+++ include/dt-bindings/arm/raspberrypi-power.h
@@ -0,0 +1,14 @@
+/*
+ *  Copyright © 2015 Broadcom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_ARM_BCM2835_RPI_POWER_H
+#define _DT_BINDINGS_ARM_BCM2835_RPI_POWER_H
+
+#define RPI_POWER_DOMAIN_USB	3
+
+#endif /* _DT_BINDINGS_ARM_BCM2835_RPI_POWER_H */
