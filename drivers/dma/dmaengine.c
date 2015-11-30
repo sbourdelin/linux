6055436b513b88eb4b13dc7c6ed43e22db308c40
Peter Ujfalusi <peter.ujfalusi@ti.com>
2015-11-30 15:45:31 +0200

    dmaengine: core: Allow NULL mask pointer in __dma_device_satisfies_mask()
    Treat as true condition the case when the mask is NULL.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- drivers/dma/dmaengine.c
+++ drivers/dma/dmaengine.c
@@ -184,6 +184,9 @@ __dma_device_satisfies_mask(struct dma_device *device,
 {
 	dma_cap_mask_t has;
 
+	if (!want)
+		return true;
+
 	bitmap_and(has.bits, want->bits, device->cap_mask.bits,
 		DMA_TX_TYPE_END);
 	return bitmap_equal(want->bits, has.bits, DMA_TX_TYPE_END);
