4fbe957451f6f0a4c3279a39cfa0e3fad39e42ce
Moise Gergaud <moise.gergaud@st.com>
2015-12-02 15:22:04 +0100

    ALSA: pcm: add IEC958 channel status update
    Add a helper to update only the IEC958 channel status sampling freq and
    word length parameters from an ALSA snd_pcm_runtime structure, taking
    account of the sample rate and sample size.
    
    Signed-off-by: Moise Gergaud <moise.gergaud@st.com>
    Acked-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

--- include/sound/pcm_iec958.h
+++ include/sound/pcm_iec958.h
@@ -3,7 +3,24 @@
 
 #include <linux/types.h>
 
+#ifdef CONFIG_SND_PCM_IEC958
 int snd_pcm_create_iec958_consumer(struct snd_pcm_runtime *runtime, u8 *cs,
-	size_t len);
+				   size_t len);
+
+int snd_pcm_update_iec958_consumer(struct snd_pcm_runtime *runtime, u8 *cs,
+				   size_t len);
+#else
+int snd_pcm_create_iec958_consumer(struct snd_pcm_runtime *runtime, u8 *cs,
+				   size_t len)
+{
+	return len;
+}
+
+int snd_pcm_update_iec958_consumer(struct snd_pcm_runtime *runtime, u8 *cs,
+				   size_t len)
+{
+	return len;
+}
+#endif
 
 #endif
--- sound/core/pcm_iec958.c
+++ sound/core/pcm_iec958.c
@@ -11,6 +11,84 @@
 #include <sound/pcm.h>
 #include <sound/pcm_iec958.h>
 
+/*
+ * snd_pcm_update_iec958_consumer - update consumer format IEC958 channel status
+ * @runtime: pcm runtime structure with ->rate filled in
+ * @cs: channel status buffer, at least four bytes
+ * @len: length of channel status buffer
+ *
+ * Update sampling frequency and word length parameters of the consumer format
+ * channel status data in @cs of maximum size @len.
+ * Values correspond to the rate and format parameters of the PCM runtime
+ * @runtime.
+ *
+ * Returns: length of buffer, or negative error code if something failed.
+ */
+int snd_pcm_update_iec958_consumer(struct snd_pcm_runtime *runtime, u8 *cs,
+				   size_t len)
+{
+	if (len < 4)
+		return -EINVAL;
+
+	cs[3] &= ~IEC958_AES3_CON_FS;
+
+	switch (runtime->rate) {
+	case 22050:
+		cs[3] |= IEC958_AES3_CON_FS_22050;
+		break;
+	case 32000:
+		cs[3] |= IEC958_AES3_CON_FS_32000;
+		break;
+	case 44100:
+		cs[3] |= IEC958_AES3_CON_FS_44100;
+		break;
+	case 48000:
+		cs[3] |= IEC958_AES3_CON_FS_48000;
+		break;
+	case 88200:
+		cs[3] |= IEC958_AES3_CON_FS_88200;
+		break;
+	case 96000:
+		cs[3] |= IEC958_AES3_CON_FS_96000;
+		break;
+	case 176400:
+		cs[3] |= IEC958_AES3_CON_FS_176400;
+		break;
+	case 192000:
+		cs[3] |= IEC958_AES3_CON_FS_192000;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	if (len > 4) {
+		cs[4] &= ~IEC958_AES4_CON_WORDLEN;
+
+		switch (snd_pcm_format_width(runtime->format)) {
+		case 16:
+			cs[4] |= IEC958_AES4_CON_WORDLEN_20_16;
+			break;
+		case 18:
+			cs[4] |= IEC958_AES4_CON_WORDLEN_22_18;
+			break;
+		case 20:
+			cs[4] |= IEC958_AES4_CON_WORDLEN_20_16 |
+			     IEC958_AES4_CON_MAX_WORDLEN_24;
+			break;
+		case 24:
+			cs[4] |= IEC958_AES4_CON_WORDLEN_24_20 |
+			     IEC958_AES4_CON_MAX_WORDLEN_24;
+			break;
+
+		default:
+			return -EINVAL;
+		}
+	}
+
+	return len;
+}
+EXPORT_SYMBOL(snd_pcm_update_iec958_consumer);
+
 /**
  * snd_pcm_create_iec958_consumer - create consumer format IEC958 channel status
  * @runtime: pcm runtime structure with ->rate filled in
@@ -25,70 +103,20 @@
  * Returns: length of buffer, or negative error code if something failed.
  */
 int snd_pcm_create_iec958_consumer(struct snd_pcm_runtime *runtime, u8 *cs,
-	size_t len)
+				   size_t len)
 {
-	unsigned int fs, ws;
-
-	if (len < 4)
-		return -EINVAL;
-
-	switch (runtime->rate) {
-	case 32000:
-		fs = IEC958_AES3_CON_FS_32000;
-		break;
-	case 44100:
-		fs = IEC958_AES3_CON_FS_44100;
-		break;
-	case 48000:
-		fs = IEC958_AES3_CON_FS_48000;
-		break;
-	case 88200:
-		fs = IEC958_AES3_CON_FS_88200;
-		break;
-	case 96000:
-		fs = IEC958_AES3_CON_FS_96000;
-		break;
-	case 176400:
-		fs = IEC958_AES3_CON_FS_176400;
-		break;
-	case 192000:
-		fs = IEC958_AES3_CON_FS_192000;
-		break;
-	default:
-		return -EINVAL;
-	}
-
-	if (len > 4) {
-		switch (snd_pcm_format_width(runtime->format)) {
-		case 16:
-			ws = IEC958_AES4_CON_WORDLEN_20_16;
-			break;
-		case 18:
-			ws = IEC958_AES4_CON_WORDLEN_22_18;
-			break;
-		case 20:
-			ws = IEC958_AES4_CON_WORDLEN_20_16 |
-			     IEC958_AES4_CON_MAX_WORDLEN_24;
-			break;
-		case 24:
-			ws = IEC958_AES4_CON_WORDLEN_24_20 |
-			     IEC958_AES4_CON_MAX_WORDLEN_24;
-			break;
-
-		default:
-			return -EINVAL;
-		}
-	}
+	int ret;
 
 	memset(cs, 0, len);
+
+	ret = snd_pcm_update_iec958_consumer(runtime, cs, len);
+	if (ret < 0)
+		return ret;
 
 	cs[0] = IEC958_AES0_CON_NOT_COPYRIGHT | IEC958_AES0_CON_EMPHASIS_NONE;
 	cs[1] = IEC958_AES1_CON_GENERAL;
 	cs[2] = IEC958_AES2_CON_SOURCE_UNSPEC | IEC958_AES2_CON_CHANNEL_UNSPEC;
-	cs[3] = IEC958_AES3_CON_CLOCK_1000PPM | fs;
-
-	if (len > 4)
-		cs[4] = ws;
+	cs[3] |= IEC958_AES3_CON_CLOCK_1000PPM;
 
 	return len;
 }
