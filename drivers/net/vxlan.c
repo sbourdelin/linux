2ce662246f9b80aaab3b69fd9ea72f92c2a50ab6
Jiri Benc <jbenc@redhat.com>
2015-12-03 11:41:23 +0100

    vxlan: support ndo_fill_metadata_dst also for IPv6
    Fill the metadata correctly even when tunneling over IPv6. Also, check that
    the provided metadata is of an address family that is supported by the
    tunnel.
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -2413,15 +2413,37 @@ static int vxlan_fill_metadata_dst(struct net_device *dev, struct sk_buff *skb)
 {
 	struct vxlan_dev *vxlan = netdev_priv(dev);
 	struct ip_tunnel_info *info = skb_tunnel_info(skb);
+#if IS_ENABLED(CONFIG_IPV6)
+	struct dst_entry *ndst;
+#endif
 	__be16 sport, dport;
 
 	sport = udp_flow_src_port(dev_net(dev), skb, vxlan->cfg.port_min,
 				  vxlan->cfg.port_max, true);
 	dport = info->key.tp_dst ? : vxlan->cfg.dst_port;
 
-	if (ip_tunnel_info_af(info) == AF_INET)
+	if (ip_tunnel_info_af(info) == AF_INET) {
+		if (!vxlan->vn4_sock)
+			return -EINVAL;
 		return egress_ipv4_tun_info(dev, skb, info, sport, dport);
-	return -EINVAL;
+	} else {
+		if (!IS_ENABLED(CONFIG_IPV6))
+			return -EPFNOSUPPORT;
+
+#if IS_ENABLED(CONFIG_IPV6)
+		if (!vxlan->vn6_sock)
+			return -EINVAL;
+		ndst = vxlan6_get_route(vxlan, skb, 0,
+					&info->key.u.ipv6.dst,
+					&info->key.u.ipv6.src);
+		if (IS_ERR(ndst))
+			return PTR_ERR(ndst);
+		dst_release(ndst);
+#endif
+		info->key.tp_src = sport;
+		info->key.tp_dst = dport;
+	}
+	return 0;
 }
 
 static const struct net_device_ops vxlan_netdev_ops = {
