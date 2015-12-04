12aa508b1ab2515f8f4e25f565282d718d479ff7
Du, Changbin <changbin.du@intel.com>
2015-12-04 15:21:16 +0800

    usb: gadget: forbid queuing request to a disabled ep
    Queue a request to disabled ep  doesn't make sense, and induce caller
    make mistakes.
    
    Here is a example for the android mtp gadget function driver. A mem
    corruption can happen on below senario.
    1) On disconnect, mtp driver disable its EPs,
    2) During send_file_work and receive_file_work, mtp queues a request
       to ep. (The mtp driver need improve its synchronization logic!)
    3) mtp_function_unbind is invoked and all mtp requests are freed.
    4) when udc process the request queued on step 2, will cause kernel
       NULL pointer dereference exception.
    
    Signed-off-by: Du, Changbin <changbin.du@intel.com>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -402,6 +402,9 @@ static inline void usb_ep_free_request(struct usb_ep *ep,
 static inline int usb_ep_queue(struct usb_ep *ep,
 			       struct usb_request *req, gfp_t gfp_flags)
 {
+	if (!ep->enabled)
+		return -ESHUTDOWN;
+
 	return ep->ops->queue(ep, req, gfp_flags);
 }
 
