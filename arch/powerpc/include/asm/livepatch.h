f84c40e7175d50168afeb078b218cd698df2bc84
Torsten Duwe <duwe@lst.de>
2015-12-04 15:11:37 +0100

    Implement kernel live patching for ppc64le (ABIv2)
      * create the appropriate files+functions
        arch/powerpc/include/asm/livepatch.h
            klp_check_compiler_support,
            klp_arch_set_pc
        arch/powerpc/kernel/livepatch.c with a stub for
            klp_write_module_reloc
        This is architecture-independent work in progress.
      * introduce a fixup in arch/powerpc/kernel/entry_64.S
        for local calls that are becoming global due to live patching.
        And of course do the main KLP thing: return to a maybe different
        address, possibly altered by the live patching ftrace op.
    
    Signed-off-by: Torsten Duwe <duwe@suse.de>

---
+++ arch/powerpc/include/asm/livepatch.h
@@ -0,0 +1,45 @@
+/*
+ * livepatch.h - powerpc-specific Kernel Live Patching Core
+ *
+ * Copyright (C) 2015 SUSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _ASM_POWERPC64_LIVEPATCH_H
+#define _ASM_POWERPC64_LIVEPATCH_H
+
+#include <linux/module.h>
+#include <linux/ftrace.h>
+
+#ifdef CONFIG_LIVEPATCH
+static inline int klp_check_compiler_support(void)
+{
+#if !defined(_CALL_ELF) || _CALL_ELF != 2 || !defined(CC_USING_MPROFILE_KERNEL)
+	return 1;
+#endif
+	return 0;
+}
+
+extern int klp_write_module_reloc(struct module *mod, unsigned long type,
+				   unsigned long loc, unsigned long value);
+
+static inline void klp_arch_set_pc(struct pt_regs *regs, unsigned long ip)
+{
+	regs->nip = ip;
+}
+#else
+#error Live patching support is disabled; check CONFIG_LIVEPATCH
+#endif
+
+#endif /* _ASM_POWERPC64_LIVEPATCH_H */
