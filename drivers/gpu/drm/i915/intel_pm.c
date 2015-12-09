8216f1df6081702901a2d5bfd5d4e70e96644739
Chris Wilson <chris@chris-wilson.co.uk>
2015-12-09 17:10:17 +0000

    drm/i915: Flush the RPS bottom-half when the GPU idles
    Make sure that the RPS bottom-half is flushed before we set the idle
    frequency when we decide the GPU is idle. This should prevent any races
    with the bottom-half and setting the idle frequency, and ensures that
    the bottom-half is bounded by the GPU's rpm reference taken for when it
    is active (i.e. between gen6_rps_busy() and gen6_rps_idle()).
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -4401,11 +4401,15 @@ void gen6_rps_busy(struct drm_i915_private *dev_priv)
 
 void gen6_rps_idle(struct drm_i915_private *dev_priv)
 {
-	struct drm_device *dev = dev_priv->dev;
+	/* Flush our bottom-half so that it does not race with us
+	 * setting the idle frequency and so that it is bounded by
+	 * our rpm wakeref.
+	 */
+	flush_work(&dev_priv->rps.work);
 
 	mutex_lock(&dev_priv->rps.hw_lock);
 	if (dev_priv->rps.enabled) {
-		if (IS_VALLEYVIEW(dev))
+		if (IS_VALLEYVIEW(dev_priv))
 			vlv_set_rps_idle(dev_priv);
 		else
 			gen6_set_rps(dev_priv->dev, dev_priv->rps.idle_freq);
