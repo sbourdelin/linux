a66ac46cf6731e31bd4a2b2370b0b38635d2ac39
Andreas Gruenbacher <agruenba@redhat.com>
2015-12-11 16:14:19 +0100

    nfs: Fix listxattr regression (2)
    Al,
    
    here is another fix for the same botched nfs commit.  Could you please
    also merge / fold that?  Sorry for the mess.
    
    Thanks,
    Andreas
    
    --
    
    Fix another regression introduced in d77ae742: listxattr ended up always
    failing with -ERANGE on NFSv4.2 mounts with security label support.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -6296,8 +6296,8 @@ nfs4_listxattr_nfs4_label(struct inode *inode, char *list, size_t list_len)
 	int len = 0;
 
 	if (nfs_server_capable(inode, NFS_CAP_SECURITY_LABEL)) {
-		len = security_inode_listsecurity(inode, list, len);
-		if (len > list_len)
+		len = security_inode_listsecurity(inode, list, list_len);
+		if (list_len && len > list_len)
 			return -ERANGE;
 	}
 	return len;
