57e1417b043f910450934ba7f68cac03b3cecd78
Chen Gang <chengang@emindsoft.com.cn>
2015-12-15 22:15:36 +0800

    include: asm-generic: Notice about 80 columns in pgtable-no*.h
    It is a trivial patch.
    
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>

--- include/asm-generic/pgtable-nopmd.h
+++ include/asm-generic/pgtable-nopmd.h
@@ -38,7 +38,8 @@ static inline void pud_clear(pud_t *pud)	{ }
  * (pmds are folded into puds so this doesn't get actually called,
  * but the define is needed for a generic inline function.)
  */
-#define set_pud(pudptr, pudval)			set_pmd((pmd_t *)(pudptr), (pmd_t) { pudval })
+#define set_pud(pudptr, pudval)			(set_pmd((pmd_t *)(pudptr), \
+							(pmd_t) { pudval }))
 
 static inline pmd_t * pmd_offset(pud_t * pud, unsigned long address)
 {
