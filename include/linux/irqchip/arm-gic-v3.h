412ab41097454866227d26de8c1ee9e4bbf5afc9
Tomasz Nowicki <tn@semihalf.com>
2015-12-17 12:52:09 +0100

    irqchip/GICv3/ITS: Refator ITS dt init code to prepare for ACPI.
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Tomasz Nowicki <tn@semihalf.com>

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -331,7 +331,7 @@ struct rdists {
 
 struct irq_domain;
 int its_cpu_init(void);
-int its_init(struct device_node *node, struct rdists *rdists,
+int its_init(struct fwnode_handle *handle, struct rdists *rdists,
 	     struct irq_domain *domain);
 
 static inline bool gic_enable_sre(void)
