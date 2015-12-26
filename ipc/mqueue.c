24dfd47fe1fb3a7625bd6c94c4ee2cdc42b887c6
Julia Lawall <Julia.Lawall@lip6.fr>
2015-12-26 22:57:43 +0100

    ipc/mqueue: remove assignment from IS_ERR argument
    The semantic patch that makes this change is as follows:
    (http://coccinelle.lip6.fr/)
    
    // <smpl>
    @@
    expression e1,e2;
    statement S1,S2;
    @@
    
    +e1 = e2;
    if (IS_ERR(
        e1
    -   = e2
       )) S1 else S2
    // </smpl>
    
    Signed-off-by: Julia Lawall <Julia.Lawall@lip6.fr>

--- ipc/mqueue.c
+++ ipc/mqueue.c
@@ -786,7 +786,8 @@ SYSCALL_DEFINE4(mq_open, const char __user *, u_name, int, oflag, umode_t, mode,
 
 	audit_mq_open(oflag, mode, u_attr ? &attr : NULL);
 
-	if (IS_ERR(name = getname(u_name)))
+	name = getname(u_name);
+	if (IS_ERR(name))
 		return PTR_ERR(name);
 
 	fd = get_unused_fd_flags(O_CLOEXEC);
