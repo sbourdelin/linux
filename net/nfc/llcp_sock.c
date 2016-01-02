032e4f0e531d6ff16f84c5c9de8ff70b6c66859e
Cong Wang <xiyou.wangcong@gmail.com>
2016-01-01 16:34:17 -0800

    nfc: check sock state in llcp_sock_getname()
    llcp_sock_getname() checks llcp_sock->dev to make sure
    llcp_sock is already connected or bound, however, we could
    be in the middle of llcp_sock_bind() where llcp_sock->dev
    is bound and llcp_sock->service_name_len is set,
    but llcp_sock->service_name is not, in this case we would
    lead to copy some bytes from a NULL pointer.
    
    We should just check if sk->sk_state is still closed since
    both connect() and bind() will update this state at the end.
    
    Reported-by: Dmitry Vyukov <dvyukov@google.com>
    Cc: Lauro Ramos Venancio <lauro.venancio@openbossa.org>
    Cc: Aloisio Almeida Jr <aloisio.almeida@openbossa.org>
    Cc: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>

--- net/nfc/llcp_sock.c
+++ net/nfc/llcp_sock.c
@@ -500,7 +500,7 @@ static int llcp_sock_getname(struct socket *sock, struct sockaddr *uaddr,
 	struct nfc_llcp_sock *llcp_sock = nfc_llcp_sock(sk);
 	DECLARE_SOCKADDR(struct sockaddr_nfc_llcp *, llcp_addr, uaddr);
 
-	if (llcp_sock == NULL || llcp_sock->dev == NULL)
+	if (llcp_sock == NULL || sk->sk_state == LLCP_CLOSED)
 		return -EBADFD;
 
 	pr_debug("%p %d %d %d\n", sk, llcp_sock->target_idx,
