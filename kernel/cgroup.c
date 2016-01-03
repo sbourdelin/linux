b16a2aeaac9dfa1ee58e855e6a8c6e53042f5244
Geliang Tang <geliangtang@163.com>
2016-01-04 00:08:23 +0800

    cgroup: make /proc/cgroups aligned
    This patch makes /proc/cgroups aligned like this:
    $ cat /proc/cgroups
    #subsys_name    hierarchy       num_cgroups     enabled
          cpuset           11                 1           1
             cpu            2                 1           1
         cpuacct            2                 1           1
           blkio           12                 1           1
          memory            3                 1           1
         devices            4                83           1
         freezer           10                 1           1
      perf_event            9                 1           1
         hugetlb            7                 1           1
       intel_rdt            6                 1           1
            pids            8                 1           1
           debug            5                 1           1
    
    Signed-off-by: Geliang Tang <geliangtang@163.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -5450,7 +5450,7 @@ static int proc_cgroupstats_show(struct seq_file *m, void *v)
 	mutex_lock(&cgroup_mutex);
 
 	for_each_subsys(ss, i)
-		seq_printf(m, "%s\t%d\t%d\t%d\n",
+		seq_printf(m, "%12s\t%9d\t%11d\t%7d\n",
 			   ss->legacy_name, ss->root->hierarchy_id,
 			   atomic_read(&ss->root->nr_cgrps),
 			   cgroup_ssid_enabled(i));
