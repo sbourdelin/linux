4aebfe64c48f4d19a2d67e453ecec7a4df1e9447
Milo Kim <milo.kim@ti.com>
2016-01-04 13:28:26 +0900

    irqchip: atmel-aic: clean up RTC interrupt code
    Atmel AIC disables RTC interrupt on initialization. However, the controller
    doesn't need to do it. There are two reasons.
    
    * RTC driver disables interrupts on _probe()
      RTC_IDR is set to 0x1f which means all RTC interrupts are disabled.
      (drivers/rtc/rtc-at91rm9200.c)
      at91_rtc_write_idr(AT91_RTC_ACKUPD | AT91_RTC_ALARM |
    		AT91_RTC_SECEV | AT91_RTC_TIMEV |
    		AT91_RTC_CALEV);
    
    * All RTC interrupts are disabled by default.
      If we try to read RTC_IMR(interrup mask status), then read value is 0
      by default. It means all RTC interrupts are disabled by default.
      We can also check the default status in the datasheet. Please note that
      all devices have same RTC registers.
      AT91RM9200
      AT91SAM9N12
      AT91SAM9X5
      AT91SAM9G45
      AT91SAM9RL
      SAMA5D3
      SAMA5D4
    
    So, aic_common_rtc_irq_fixup() helper can be removed.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Cc: Boris BREZILLON <boris.brezillon@free-electrons.com>
    Cc: Ludovic Desroches <ludovic.desroches@atmel.com>
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Milo Kim <milo.kim@ti.com>

--- drivers/irqchip/irq-atmel-aic5.c
+++ drivers/irqchip/irq-atmel-aic5.c
@@ -285,17 +285,6 @@ static const struct irq_domain_ops aic5_irq_ops = {
 	.xlate	= aic5_irq_domain_xlate,
 };
 
-static void __init sama5d3_aic_irq_fixup(struct device_node *root)
-{
-	aic_common_rtc_irq_fixup(root);
-}
-
-static const struct of_device_id aic5_irq_fixups[] __initconst = {
-	{ .compatible = "atmel,sama5d3", .data = sama5d3_aic_irq_fixup },
-	{ .compatible = "atmel,sama5d4", .data = sama5d3_aic_irq_fixup },
-	{ /* sentinel */ },
-};
-
 static int __init aic5_of_init(struct device_node *node,
 			       struct device_node *parent,
 			       int nirqs)
@@ -315,8 +304,6 @@ static int __init aic5_of_init(struct device_node *node,
 				    nirqs);
 	if (IS_ERR(domain))
 		return PTR_ERR(domain);
-
-	aic_common_irq_fixup(aic5_irq_fixups);
 
 	aic5_domain = domain;
 	nchips = aic5_domain->revmap_size / 32;
