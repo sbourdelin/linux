610de7e0f1fc2033e78fcfc5382e57cfc5832299
Wengang Wang <wen.gang.wang@oracle.com>
2016-01-06 17:03:17 +0800

    net: take care of bonding in build_skb_flow_key (v2)
    In a bonding setting, we determines fragment size according to MTU and
    PMTU associated to the bonding master. If the slave finds the fragment
    size is too big, it drops the fragment and calls ip_rt_update_pmtu(),
    passing _skb_ and _pmtu_, trying to update the path MTU.
    Problem is that the target device that function ip_rt_update_pmtu actually
    tries to update is the slave (skb->dev), not the master. Thus since no
    PMTU change happens on master, the fragment size for later packets doesn't
    change so all later fragments/packets are dropped too.
    
    The fix is letting build_skb_flow_key() take care of the transition of
    device index from bonding slave to the master. That makes the master become
    the target device that ip_rt_update_pmtu tries to update PMTU to.
    
    Signed-off-by: Wengang Wang <wen.gang.wang@oracle.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -523,10 +523,18 @@ static void build_skb_flow_key(struct flowi4 *fl4, const struct sk_buff *skb,
 			       const struct sock *sk)
 {
 	const struct iphdr *iph = ip_hdr(skb);
-	int oif = skb->dev->ifindex;
 	u8 tos = RT_TOS(iph->tos);
+	struct net_device *master;
 	u8 prot = iph->protocol;
 	u32 mark = skb->mark;
+	int oif;
+
+	if (skb->dev->flags & IFF_SLAVE) {
+		master = netdev_master_upper_dev_get(skb->dev);
+		oif = master->ifindex;
+	} else {
+		oif = skb->dev->ifindex;
+	}
 
 	__build_flow_key(fl4, sk, iph, oif, tos, prot, mark, 0);
 }
