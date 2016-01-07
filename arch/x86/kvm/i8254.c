b5355c1c0b266eaa2aa988a546a7090332f03bb3
P J P <pjp@fedoraproject.org>
2016-01-07 18:02:30 +0530

    kvm: use PIT channel index in hpet_legacy_start mode
    While setting the KVM PIT counters in 'kvm_pit_load_count', if
    'hpet_legacy_start' is set, the function disables the timer on
    channel[0], instead of the respective index 'channel'. Update it
    to use 'channel' index parameter.
    
    Signed-off-by: P J P <pjp@fedoraproject.org>

--- arch/x86/kvm/i8254.c
+++ arch/x86/kvm/i8254.c
@@ -420,10 +420,11 @@ void kvm_pit_load_count(struct kvm *kvm, int channel, u32 val, int hpet_legacy_s
 	u8 saved_mode;
 	if (hpet_legacy_start) {
 		/* save existing mode for later reenablement */
-		saved_mode = kvm->arch.vpit->pit_state.channels[0].mode;
-		kvm->arch.vpit->pit_state.channels[0].mode = 0xff; /* disable timer */
+		saved_mode = kvm->arch.vpit->pit_state.channels[channel].mode;
+		/* disable timer */
+		kvm->arch.vpit->pit_state.channels[channel].mode = 0xff;
 		pit_load_count(kvm, channel, val);
-		kvm->arch.vpit->pit_state.channels[0].mode = saved_mode;
+		kvm->arch.vpit->pit_state.channels[channel].mode = saved_mode;
 	} else {
 		pit_load_count(kvm, channel, val);
 	}
