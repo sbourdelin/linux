04cea64771a7be0f7ca274682fa6c72347244aa9
Huw Davies <huw@codeweavers.com>
2016-01-08 09:52:41 +0000

    netlabel: Add support for enumerating the CALIPSO DOI list.
    Enumerate the DOI list through the NLBL_CALIPSO_C_LISTALL command.
    It takes no attributes.
    
    Signed-off-by: Huw Davies <huw@codeweavers.com>

--- net/netlabel/netlabel_calipso.h
+++ net/netlabel/netlabel_calipso.h
@@ -126,5 +126,8 @@ int calipso_doi_add(struct calipso_doi *doi_def,
 void calipso_doi_free(struct calipso_doi *doi_def);
 struct calipso_doi *calipso_doi_getdef(u32 doi);
 void calipso_doi_putdef(struct calipso_doi *doi_def);
+int calipso_doi_walk(u32 *skip_cnt,
+		     int (*callback)(struct calipso_doi *doi_def, void *arg),
+		     void *cb_arg);
 
 #endif
