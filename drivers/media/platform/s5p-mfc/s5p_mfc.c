a2a4a01407f8a8050f239412b5955ae92342e095
Douglas Anderson <dianders@chromium.org>
2016-01-08 15:05:32 -0800

    s5p-mfc: Set DMA_ATTR_NO_HUGE_PAGE
    We do video allocation all the time and we need it to be fast.  Plus TLB
    efficiency isn't terribly important for video.
    
    That means we want to set DMA_ATTR_NO_HUGE_PAGE.
    
    See also the previous change ("ARM: dma-mapping: Use
    DMA_ATTR_NO_HUGE_PAGE hint to optimize allocation").
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>

--- drivers/media/platform/s5p-mfc/s5p_mfc.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc.c
@@ -1074,6 +1074,7 @@ static int s5p_mfc_alloc_memdevs(struct s5p_mfc_dev *dev)
 /* MFC probe function */
 static int s5p_mfc_probe(struct platform_device *pdev)
 {
+	DEFINE_DMA_ATTRS(attrs);
 	struct s5p_mfc_dev *dev;
 	struct video_device *vfd;
 	struct resource *res;
@@ -1143,12 +1144,20 @@ static int s5p_mfc_probe(struct platform_device *pdev)
 		}
 	}
 
-	dev->alloc_ctx[0] = vb2_dma_contig_init_ctx(dev->mem_dev_l);
+	/*
+	 * We'll do mostly sequential access, so sacrifice TLB efficiency for
+	 * faster allocation.
+	 */
+	dma_set_attr(DMA_ATTR_NO_HUGE_PAGE, &attrs);
+
+	dev->alloc_ctx[0] = vb2_dma_contig_init_ctx_attrs(dev->mem_dev_l,
+							  &attrs);
 	if (IS_ERR(dev->alloc_ctx[0])) {
 		ret = PTR_ERR(dev->alloc_ctx[0]);
 		goto err_res;
 	}
-	dev->alloc_ctx[1] = vb2_dma_contig_init_ctx(dev->mem_dev_r);
+	dev->alloc_ctx[1] = vb2_dma_contig_init_ctx_attrs(dev->mem_dev_r,
+							  &attrs);
 	if (IS_ERR(dev->alloc_ctx[1])) {
 		ret = PTR_ERR(dev->alloc_ctx[1]);
 		goto err_mem_init_ctx_1;
