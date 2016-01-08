15b5a992cf45f5ccf0f691d990d475ed1e8cbf1a
Tim Gardner <tim.gardner@canonical.com>
2016-01-08 05:32:54 -0700

    isdn: eicon: diddfunc: Silence frame size warning
    drivers/isdn/hardware/eicon/diddfunc.c: In function 'connect_didd':
    drivers/isdn/hardware/eicon/diddfunc.c:77:1: warning: the frame size of 1152 bytes is larger than 1024 bytes [-Wframe-larger-than=]
    
    gcc version 5.3.1 20151219 (Ubuntu 5.3.1-4ubuntu1)
    
    Cc: Armin Schindler <mac@melware.de>
    Cc: Karsten Keil <isdn@linux-pingi.de>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/isdn/hardware/eicon/diddfunc.c
+++ drivers/isdn/hardware/eicon/diddfunc.c
@@ -52,9 +52,13 @@ static int __init connect_didd(void)
 	int x = 0;
 	int dadapter = 0;
 	IDI_SYNC_REQ req;
-	DESCRIPTOR DIDD_Table[MAX_DESCRIPTORS];
+	DESCRIPTOR *DIDD_Table;
 
-	DIVA_DIDD_Read(DIDD_Table, sizeof(DIDD_Table));
+	DIDD_Table = kcalloc(MAX_DESCRIPTORS, sizeof(*DIDD_Table), GFP_KERNEL);
+	if (!DIDD_Table)
+		goto out;
+
+	DIVA_DIDD_Read(DIDD_Table, MAX_DESCRIPTORS * sizeof(*DIDD_Table));
 
 	for (x = 0; x < MAX_DESCRIPTORS; x++) {
 		if (DIDD_Table[x].type == IDI_DADAPTER) {	/* DADAPTER found */
@@ -66,13 +70,18 @@ static int __init connect_didd(void)
 			req.didd_notify.info.callback = (void *)didd_callback;
 			req.didd_notify.info.context = NULL;
 			_DAdapter.request((ENTITY *)&req);
-			if (req.didd_notify.e.Rc != 0xff)
-				return (0);
+			if (req.didd_notify.e.Rc != 0xff) {
+				dadapter = 0;
+				goto out;
+			}
 			notify_handle = req.didd_notify.info.handle;
 		} else if (DIDD_Table[x].type == IDI_DIMAINT) {	/* MAINT found */
 			DbgRegister("DIDD", DRIVERRELEASE_DIDD, DBG_DEFAULT);
 		}
 	}
+
+out:
+	kfree(DIDD_Table);
 	return (dadapter);
 }
 
