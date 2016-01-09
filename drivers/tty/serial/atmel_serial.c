449e4b151a5b7a277d4799e4aad57cf3474f9cd7
Guenter Roeck <linux@roeck-us.net>
2016-01-09 10:15:35 -0800

    tty/serial: atmel: Include module.h to fix build failure
    If serial/atmel_serial.c is compiled with devicetree enabled, the
    following build error is observed.
    
    drivers/tty/serial/atmel_serial.c:192:1: warning:
    	data definition has no type or storage class
    drivers/tty/serial/atmel_serial.c:192:1: error:
    	type defaults to 'int' in declaration of 'MODULE_DEVICE_TABLE'
    drivers/tty/serial/atmel_serial.c:192:1: warning:
    	parameter names (without types) in function declaration
    
    MODULE_DEVICE_TABLE is used to specify devicetree compatibilities.
    
    Fixes: c39dfebc7798 ("drivers/tty/serial: make serial/atmel_serial.c explicitly non-modular")
    Cc: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/tty/serial/atmel_serial.c
+++ drivers/tty/serial/atmel_serial.c
@@ -22,6 +22,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+#include <linux/module.h>
 #include <linux/tty.h>
 #include <linux/ioport.h>
 #include <linux/slab.h>
