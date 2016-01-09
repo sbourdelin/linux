a33d06d6c52c45fe28ce7d2a15bf78c2d1a4fa49
Nicholas Mc Guire <hofrat@osadl.org>
2016-01-09 12:01:04 +0100

    timer: drop the unnecessary while loop in msleep
     The while loop in msleep does not seem necessary as
    timeout is unsigned long and no larger than MAX_JIFFY_OFFSET (which is
    LONG_MAX/2 - 1) so the while-loop condition is always true at the beginning
    (msecs_to_jiffies will return >=0 always and with the +1 timeout is >= 1 so
    the while condition is always true at the start) and
    schedule_timeout_uninterruptible always returns 0, so the while loop always
    terminates after the first loop.
    
    So msleep really should be a pure wrapper to
      schedule_timeout_uninterruptible(msecs_to_jiffies(timeout));
    
    Signed-off-by: Nicholas Mc Guire <hofrat@osadl.org>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1674,8 +1674,7 @@ void msleep(unsigned int msecs)
 {
 	unsigned long timeout = msecs_to_jiffies(msecs) + 1;
 
-	while (timeout)
-		timeout = schedule_timeout_uninterruptible(timeout);
+	timeout = schedule_timeout_uninterruptible(timeout);
 }
 
 EXPORT_SYMBOL(msleep);
