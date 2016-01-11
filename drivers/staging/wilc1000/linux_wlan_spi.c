261470218f77ffdecccf5f3a9b56e3dfde90eb6e
Janosch Frank <kernel@janosch-frank.de>
2016-01-11 21:13:35 +0100

    staging/wilc1000: Remove unneeded imports
    Apart from spi.h and module.h, none of the imports are used anymore.
    
    Signed-off-by: Janosch Frank <kernel@janosch-frank.de>

--- drivers/staging/wilc1000/linux_wlan_spi.c
+++ drivers/staging/wilc1000/linux_wlan_spi.c
@@ -1,12 +1,4 @@
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/slab.h>
-#include <linux/types.h>
-#include <linux/cdev.h>
-#include <linux/uaccess.h>
-#include <linux/device.h>
 #include <linux/spi/spi.h>
 
 #include "linux_wlan_common.h"
