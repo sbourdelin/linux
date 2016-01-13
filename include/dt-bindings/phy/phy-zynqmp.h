c4f2a46f28a73bd4bb33641b36726de341824cfa
Subbaraya Sundeep Bhatta <subbaraya.sundeep.bhatta@xilinx.com>
2016-01-13 14:52:30 +0530

    phy: zynqmp: Add PHY driver for ZynqMP PHY
    ZynMP SoC has a Gigabit Transceiver with four lanes.
    All the high speed peripherals USB, SATA, PCIE, Display
    Port and Ethernet SGMII rely on GT for PHY layer.
    This patch adds driver for ZynqMP GT.
    
    Signed-off-by: Subbaraya Sundeep Bhatta <sbhatta@xilinx.com>

---
+++ include/dt-bindings/phy/phy-zynqmp.h
@@ -0,0 +1,19 @@
+#ifndef _DT_BINDINGS_PHY_ZYNQMP
+#define _DT_BINDINGS_PHY_ZYNQMP
+
+#define XPSGTR_TYPE_USB0	0 /* USB controller 0 */
+#define XPSGTR_TYPE_USB1	1 /* USB controller 1 */
+#define XPSGTR_TYPE_SATA_0	2 /* SATA controller lane 0 */
+#define XPSGTR_TYPE_SATA_1	3 /* SATA controller lane 1 */
+#define XPSGTR_TYPE_PCIE_0	4 /* PCIE controller lane 0 */
+#define XPSGTR_TYPE_PCIE_1	5 /* PCIE controller lane 1 */
+#define XPSGTR_TYPE_PCIE_2	6 /* PCIE controller lane 2 */
+#define XPSGTR_TYPE_PCIE_3	7 /* PCIE controller lane 3 */
+#define XPSGTR_TYPE_DP_0	8 /* Display Port lane 0 */
+#define XPSGTR_TYPE_DP_1	9 /* Display Port lane 1 */
+#define XPSGTR_TYPE_SGMII0	10 /* SGMMI controller 0 */
+#define XPSGTR_TYPE_SGMII1	11 /* SGMII controller 1 */
+#define XPSGTR_TYPE_SGMII2	12 /* SGMII controller 2 */
+#define XPSGTR_TYPE_SGMII3	13 /* SGMII controller 3 */
+
+#endif
