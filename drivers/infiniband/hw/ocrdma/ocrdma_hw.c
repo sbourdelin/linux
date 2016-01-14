3b164b3368e99edd4f4c6d0f161b1fcaba507b06
Markus Elfring <elfring@users.sourceforge.net>
2016-01-14 17:54:45 +0100

    InfiniBand-ocrdma: Returning only value constants in ocrdma_qp_state_change()
    Return zero at the end without using the local variable "status".
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- drivers/infiniband/hw/ocrdma/ocrdma_hw.c
+++ drivers/infiniband/hw/ocrdma/ocrdma_hw.c
@@ -2138,7 +2138,6 @@ int ocrdma_qp_state_change(struct ocrdma_qp *qp, enum ib_qp_state new_ib_state,
 			   enum ib_qp_state *old_ib_state)
 {
 	unsigned long flags;
-	int status = 0;
 	enum ocrdma_qp_state new_state;
 	new_state = get_ocrdma_qp_state(new_ib_state);
 
@@ -2163,7 +2162,7 @@ int ocrdma_qp_state_change(struct ocrdma_qp *qp, enum ib_qp_state new_ib_state,
 	qp->state = new_state;
 
 	spin_unlock_irqrestore(&qp->q_lock, flags);
-	return status;
+	return 0;
 }
 
 static u32 ocrdma_set_create_qp_mbx_access_flags(struct ocrdma_qp *qp)
