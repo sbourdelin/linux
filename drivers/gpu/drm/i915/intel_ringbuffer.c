f871214496a5d50005d80f0328caa5f51f3fd90e
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2016-01-15 15:10:28 +0000

    drm/i915: Cache ringbuffer GTT VMA
    Purpose is to avoid calling i915_gem_obj_ggtt_offset from the
    interrupt context without the big lock held.
    
    v2: Renamed gtt_start to gtt_offset. (Daniel Vetter)
    v3: Cache the VMA instead of address. (Chris Wilson)
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_ringbuffer.c
+++ drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -1988,6 +1988,7 @@ void intel_unpin_ringbuffer_obj(struct intel_ringbuffer *ringbuf)
 	else
 		iounmap(ringbuf->virtual_start);
 	ringbuf->virtual_start = NULL;
+	ringbuf->vma = NULL;
 	i915_gem_object_ggtt_unpin(ringbuf->obj);
 }
 
@@ -2053,6 +2054,8 @@ int intel_pin_and_map_ringbuffer_obj(struct drm_device *dev,
 			return -EINVAL;
 		}
 	}
+
+	ringbuf->vma = i915_gem_obj_to_ggtt(obj);
 
 	return 0;
 }
