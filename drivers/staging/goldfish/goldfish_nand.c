8cbc17b5588c4720f8d3d7f19279af6864afc1dd
Shraddha Barke <shraddha.6596@gmail.com>
2016-01-18 22:37:08 +0530

    Staging: goldfish: Add DMA support using dma_alloc_coherent
    Add support for accessing the device through DMA because CPU is
    unnecessarily used when DMA can do the job. Coherent mapping is allocated
    by means of dma_alloc_coherent so that the device and CPU are in sync.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>

--- drivers/staging/goldfish/goldfish_nand.c
+++ drivers/staging/goldfish/goldfish_nand.c
@@ -27,6 +27,7 @@
 #include <linux/mutex.h>
 #include <linux/goldfish.h>
 #include <asm/div64.h>
+#include <linux/dma-mapping.h>
 
 #include "goldfish_nand_reg.h"
 
@@ -284,7 +285,8 @@ invalid_arg:
 static int nand_setup_cmd_params(struct platform_device *pdev,
 				 struct goldfish_nand *nand)
 {
-	u64 paddr;
+	void *addr;
+	dma_addr_t dma_handle;
 	unsigned char __iomem  *base = nand->base;
 
 	nand->cmd_params = devm_kzalloc(&pdev->dev,
@@ -292,9 +294,14 @@ static int nand_setup_cmd_params(struct platform_device *pdev,
 	if (!nand->cmd_params)
 		return -1;
 
-	paddr = __pa(nand->cmd_params);
-	writel((u32)(paddr >> 32), base + NAND_CMD_PARAMS_ADDR_HIGH);
-	writel((u32)paddr, base + NAND_CMD_PARAMS_ADDR_LOW);
+	addr = dma_alloc_coherent(&pdev->dev, sizeof(struct cmd_params),
+				  &dma_handle, GFP_KERNEL | GFP_DMA);
+	if (!addr) {
+		dev_err(&pdev->dev, "allocate buffer failed\n");
+		return -ENOMEM;
+	}
+	writel((u32)(dma_handle >> 32), base + NAND_CMD_PARAMS_ADDR_HIGH);
+	writel((u32)dma_handle, base + NAND_CMD_PARAMS_ADDR_LOW);
 	return 0;
 }
 
