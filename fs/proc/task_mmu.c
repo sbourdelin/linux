47c961bf1f68cd089c49f6f075ef4d9357b3f9d3
Michael Holzheu <holzheu@linux.vnet.ibm.com>
2016-01-20 19:17:59 +0100

    numa: fix /proc/<pid>/numa_maps on s390
    When working with huge page pmds in general is not valid to directly
    use pte functions like pte_present() because the hardware bit layout
    of pmds and ptes can be different. This is the case on s390. Therefore
    we have to convert the pmds first into a valid pte encoding with
    huge_ptep_get(). So add the two missing functions calls to do this.
    
    Reviewed-by: Gerald Schaefer <gerald.schaefer@de.ibm.com>
    Signed-off-by: Michael Holzheu <holzheu@linux.vnet.ibm.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1520,7 +1520,7 @@ static int gather_pte_stats(pmd_t *pmd, unsigned long addr,
 	pte_t *pte;
 
 	if (pmd_trans_huge_lock(pmd, vma, &ptl)) {
-		pte_t huge_pte = *(pte_t *)pmd;
+		pte_t huge_pte = huge_ptep_get((pte_t *)pmd);
 		struct page *page;
 
 		page = can_gather_numa_stats(huge_pte, vma, addr);
@@ -1548,18 +1548,19 @@ static int gather_pte_stats(pmd_t *pmd, unsigned long addr,
 static int gather_hugetlb_stats(pte_t *pte, unsigned long hmask,
 		unsigned long addr, unsigned long end, struct mm_walk *walk)
 {
+	pte_t huge_pte = huge_ptep_get(pte);
 	struct numa_maps *md;
 	struct page *page;
 
-	if (!pte_present(*pte))
+	if (!pte_present(huge_pte))
 		return 0;
 
-	page = pte_page(*pte);
+	page = pte_page(huge_pte);
 	if (!page)
 		return 0;
 
 	md = walk->private;
-	gather_stats(page, md, pte_dirty(*pte), 1);
+	gather_stats(page, md, pte_dirty(huge_pte), 1);
 	return 0;
 }
 
