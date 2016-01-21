5951c21d0ca02d027cf9f00429cf62066fcf2170
Cyril Bur <cyrilbur@gmail.com>
2016-01-21 11:55:46 +1100

    powerpc: Prepare for splitting giveup_{fpu, altivec, vsx} in two
    This prepares for the decoupling of saving {fpu,altivec,vsx} registers and
    marking {fpu,altivec,vsx} as being unused by a thread.
    
    Currently giveup_{fpu,altivec,vsx}() does both however optimisations to
    task switching can be made if these two operations are decoupled.
    save_all() will permit the saving of registers to thread structs and leave
    threads MSR with bits enabled.
    
    This patch introduces no functional change.
    
    Signed-off-by: Cyril Bur <cyrilbur@gmail.com>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -349,6 +349,14 @@ void flush_spe_to_thread(struct task_struct *tsk)
 		preempt_enable();
 	}
 }
+#else
+/*
+ * save_all() is going to test MSR_SPE, rather than pull in all the
+ * booke definitions all the time on a books kernel just ensure it exists
+ * but acts as a nop.
+ */
+
+#define MSR_SPE 0
 #endif /* CONFIG_SPE */
 
 static unsigned long msr_all_available;
@@ -440,12 +448,41 @@ void restore_math(struct pt_regs *regs)
 	regs->msr = msr;
 }
 
+void save_all(struct task_struct *tsk)
+{
+	unsigned long usermsr;
+
+	if (!tsk->thread.regs)
+		return;
+
+	usermsr = tsk->thread.regs->msr;
+
+	if ((usermsr & msr_all_available) == 0)
+		return;
+
+	msr_check_and_set(msr_all_available);
+
+	if (usermsr & MSR_FP)
+		__giveup_fpu(tsk);
+
+	if (usermsr & MSR_VEC)
+		__giveup_altivec(tsk);
+
+	if (usermsr & MSR_VSX)
+		__giveup_vsx(tsk);
+
+	if (usermsr & MSR_SPE)
+		__giveup_spe(tsk);
+
+	msr_check_and_clear(msr_all_available);
+}
+
 void flush_all_to_thread(struct task_struct *tsk)
 {
 	if (tsk->thread.regs) {
 		preempt_disable();
 		BUG_ON(tsk != current);
-		giveup_all(tsk);
+		save_all(tsk);
 
 #ifdef CONFIG_SPE
 		if (tsk->thread.regs->msr & MSR_SPE)
