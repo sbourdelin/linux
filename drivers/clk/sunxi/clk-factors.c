a52270665f6ce1239992eea5412458c3e32844a0
Chen-Yu Tsai <wens@csie.org>
2016-01-25 21:15:40 +0800

    clk: sunxi: factors: Add unregister function
    sunxi's factors clk did not have an unregister function. This means
    multiple structs were leaked whenever a factors clk was unregistered.
    
    Add an unregister function for it. Also keep pointers to the mux and
    gate structs so they can be freed.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>

--- drivers/clk/sunxi/clk-factors.c
+++ drivers/clk/sunxi/clk-factors.c
@@ -202,6 +202,8 @@ struct clk *sunxi_factors_register(struct device_node *node,
 		if (!gate)
 			goto err_gate;
 
+		factors->gate = gate;
+
 		/* set up gate properties */
 		gate->reg = reg;
 		gate->bit_idx = data->enable;
@@ -214,6 +216,8 @@ struct clk *sunxi_factors_register(struct device_node *node,
 		mux = kzalloc(sizeof(struct clk_mux), GFP_KERNEL);
 		if (!mux)
 			goto err_mux;
+
+		factors->mux = mux;
 
 		/* set up gate properties */
 		mux->reg = reg;
@@ -253,4 +257,24 @@ err_gate:
 	kfree(factors);
 err_factors:
 	return NULL;
+}
+
+void sunxi_factors_unregister(struct device_node *node, struct clk *clk)
+{
+	struct clk_hw *hw = __clk_get_hw(clk);
+	struct clk_factors *factors;
+	const char *name;
+
+	if (!hw)
+		return;
+
+	factors = to_clk_factors(hw);
+	name = clk_hw_get_name(hw);
+
+	/* No unregister call for clkdev_* */
+	of_clk_del_provider(node);
+	clk_unregister_composite(clk);
+	kfree(factors->mux);
+	kfree(factors->gate);
+	kfree(factors);
 }
