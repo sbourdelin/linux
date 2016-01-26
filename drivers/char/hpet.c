a83531fb7890a34ba9d2bae888979a1c05e100f0
Michael S. Tsirkin <mst@redhat.com>
2016-01-26 18:43:28 +0200

    hpet: update the link
    Looks like the HPET spec at intel.com got moved.
    Update the link.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- drivers/char/hpet.c
+++ drivers/char/hpet.c
@@ -42,7 +42,7 @@
 /*
  * The High Precision Event Timer driver.
  * This driver is closely modelled after the rtc.c driver.
- * http://www.intel.com/hardwaredesign/hpetspec_1.pdf
+ * http://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/software-developers-hpet-spec-1-0a.pdf
  */
 #define	HPET_USER_FREQ	(64)
 #define	HPET_DRIFT	(500)
