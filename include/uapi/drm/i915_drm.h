e563386ef30e1da99dbdbd1c74f405a998f5da0b
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2016-01-26 14:53:33 +0000

    drm/i915: Announce the new DRM_IOCTL_I915_GEM_MMAP_GTT capabilities
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>

--- include/uapi/drm/i915_drm.h
+++ include/uapi/drm/i915_drm.h
@@ -357,6 +357,7 @@ typedef struct drm_i915_irq_wait {
 #define I915_PARAM_HAS_GPU_RESET	 35
 #define I915_PARAM_HAS_RESOURCE_STREAMER 36
 #define I915_PARAM_HAS_EXEC_SOFTPIN	 37
+#define I915_PARAM_MMAP_GTT_VERSION	 38
 
 typedef struct drm_i915_getparam {
 	__s32 param;
