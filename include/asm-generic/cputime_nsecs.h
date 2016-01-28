5254796a4560ed25e2ab1697f9e88cf420f9e8fc
zengtao <prime.zeng@huawei.com>
2016-01-28 15:02:26 +0800

    cputime: Fix timeval-->cputime conversion
    The structure:
    struct timeval {
    	__kernel_time_t		tv_sec;		/* seconds */
    	__kernel_suseconds_t	tv_usec;	/* microseconds */
    };
    both __kernel_time_t and __kernel_suseconds_t are short than u64
    when it is 32bit platform, so force u64 conversion here.
    
    Signed-off-by: zengtao <prime.zeng@huawei.com>

--- include/asm-generic/cputime_nsecs.h
+++ include/asm-generic/cputime_nsecs.h
@@ -91,7 +91,8 @@ static inline void cputime_to_timespec(const cputime_t ct, struct timespec *val)
  */
 static inline cputime_t timeval_to_cputime(const struct timeval *val)
 {
-	u64 ret = val->tv_sec * NSEC_PER_SEC + val->tv_usec * NSEC_PER_USEC;
+	u64 ret = (u64)val->tv_sec * NSEC_PER_SEC +
+			val->tv_usec * NSEC_PER_USEC;
 	return (__force cputime_t) ret;
 }
 static inline void cputime_to_timeval(const cputime_t ct, struct timeval *val)
