e678e4593249c78156f80a18d23012c5b7abe8fb
David Gibson <david@gibson.dropbear.id.au>
2016-01-29 16:23:58 +1100

    arch/powerpc: Clean up memory hotplug failure paths
    This makes a number of cleanups to handling of mapping failures during
    memory hotplug on Power:
    
    For errors creating the linear mapping for the hot-added region:
      * This is now reported with EFAULT which is more appropriate than the
        previous EINVAL (the failure is unlikely to be related to the
        function's parameters)
      * An error in this path now prints a warning message, rather than just
        silently failing to add the extra memory.
      * Previously a failure here could result in the region being partially
        mapped.  We now clean up any partial mapping before failing.
    
    For errors creating the vmemmap for the hot-added region:
       * This is now reported with EFAULT instead of causing a BUG() - this
         could happen for external reason (e.g. full hash table) so it's better
         to handle this non-fatally
       * An error message is also printed, so the failure won't be silent
       * As above a failure could cause a partially mapped region, we now
         clean this up.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- arch/powerpc/mm/hash_utils_64.c
+++ arch/powerpc/mm/hash_utils_64.c
@@ -635,9 +635,16 @@ static unsigned long __init htab_get_table_size(void)
 #ifdef CONFIG_MEMORY_HOTPLUG
 int create_section_mapping(unsigned long start, unsigned long end)
 {
-	return htab_bolt_mapping(start, end, __pa(start),
-				 pgprot_val(PAGE_KERNEL), mmu_linear_psize,
-				 mmu_kernel_ssize);
+	int rc = htab_bolt_mapping(start, end, __pa(start),
+				   pgprot_val(PAGE_KERNEL), mmu_linear_psize,
+				   mmu_kernel_ssize);
+
+	if (rc < 0) {
+		int rc2 = htab_remove_mapping(start, end, mmu_linear_psize,
+					      mmu_kernel_ssize);
+		BUG_ON(rc2 && (rc2 != -ENOENT));
+	}
+	return rc;
 }
 
 int remove_section_mapping(unsigned long start, unsigned long end)
