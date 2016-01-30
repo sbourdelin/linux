935336da6a271d7f7a0fbf6eda416ba1c29f65cc
Rik van Riel <riel@redhat.com>
2016-01-29 22:36:02 -0500

    sched,time: remove non-power-of-two divides from __acct_update_integrals
    When running a microbenchmark calling an invalid syscall number
    in a loop, on a nohz_full CPU, we spend a full 9% of our CPU
    time in __acct_update_integrals.
    
    This function converts cputime_t to jiffies, to a timeval, only to
    convert the timeval back to microseconds before discarding it.
    
    This patch leaves __acct_update_integrals functionally equivalent,
    but speeds things up by about 12%, with 10 million calls to an
    invalid syscall number dropping from 3.7 to 3.25 seconds.
    
    Signed-off-by: Rik van Riel <riel@redhat.com>

--- kernel/tsacct.c
+++ kernel/tsacct.c
@@ -93,9 +93,9 @@ void xacct_add_tsk(struct taskstats *stats, struct task_struct *p)
 {
 	struct mm_struct *mm;
 
-	/* convert pages-usec to Mbyte-usec */
-	stats->coremem = p->acct_rss_mem1 * PAGE_SIZE / MB;
-	stats->virtmem = p->acct_vm_mem1 * PAGE_SIZE / MB;
+	/* convert pages-nsec/KB to Mbyte-usec, see __acct_update_integrals */
+	stats->coremem = p->acct_rss_mem1 * PAGE_SIZE / (1000 * KB);
+	stats->virtmem = p->acct_vm_mem1 * PAGE_SIZE / (1000 * KB);
 	mm = get_task_mm(p);
 	if (mm) {
 		/* adjust to KB unit */
@@ -125,22 +125,21 @@ static void __acct_update_integrals(struct task_struct *tsk,
 {
 	if (likely(tsk->mm)) {
 		cputime_t time, dtime;
-		struct timeval value;
 		unsigned long flags;
 		u64 delta;
 
 		local_irq_save(flags);
 		time = stime + utime;
 		dtime = time - tsk->acct_timexpd;
-		jiffies_to_timeval(cputime_to_jiffies(dtime), &value);
-		delta = value.tv_sec;
-		delta = delta * USEC_PER_SEC + value.tv_usec;
+		delta = cputime_to_nsecs(dtime);
 
-		if (delta == 0)
+		if (delta < TICK_NSEC)
 			goto out;
+
 		tsk->acct_timexpd = time;
-		tsk->acct_rss_mem1 += delta * get_mm_rss(tsk->mm);
-		tsk->acct_vm_mem1 += delta * tsk->mm->total_vm;
+		/* The final unit will be Mbyte-usecs, see xacct_add_tsk */
+		tsk->acct_rss_mem1 += delta * get_mm_rss(tsk->mm) / 1024;
+		tsk->acct_vm_mem1 += delta * tsk->mm->total_vm / 1024;
 	out:
 		local_irq_restore(flags);
 	}
