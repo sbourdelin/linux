5d59cdf14ecb7d5d1bd0cea8df1c142f8b3f1be2
John Youn <John.Youn@synopsys.com>
2016-02-01 17:42:54 -0800

    usb: dwc3: Enable SuperSpeedPlus
    Enable SuperSpeedPlus by programming the DCFG.speed and after
    enumerating, set gadget->speed appropriately.
    
    Signed-off-by: John Youn <johnyoun@synopsys.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1629,8 +1629,13 @@ static int dwc3_gadget_start(struct usb_gadget *g,
 		case USB_SPEED_HIGH:
 			reg |= DWC3_DSTS_HIGHSPEED;
 			break;
-		case USB_SPEED_SUPER:	/* FALLTHROUGH */
-		case USB_SPEED_UNKNOWN:	/* FALTHROUGH */
+		case USB_SPEED_SUPER:
+			reg |= DWC3_DSTS_SUPERSPEED;
+			break;
+		case USB_SPEED_SUPER_PLUS:
+			reg |= DWC3_DSTS_SUPERSPEED_PLUS;
+			break;
+		case USB_SPEED_UNKNOWN:
 		default:
 			reg |= DWC3_DSTS_SUPERSPEED;
 		}
@@ -2337,6 +2342,11 @@ static void dwc3_gadget_conndone_interrupt(struct dwc3 *dwc)
 	dwc3_update_ram_clk_sel(dwc, speed);
 
 	switch (speed) {
+	case DWC3_DCFG_SUPERSPEED_PLUS:
+		dwc3_gadget_ep0_desc.wMaxPacketSize = cpu_to_le16(512);
+		dwc->gadget.ep0->maxpacket = 512;
+		dwc->gadget.speed = USB_SPEED_SUPER_PLUS;
+		break;
 	case DWC3_DCFG_SUPERSPEED:
 		/*
 		 * WORKAROUND: DWC3 revisions <1.90a have an issue which
