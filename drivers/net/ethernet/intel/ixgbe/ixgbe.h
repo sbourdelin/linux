de6eb2c847a87b67336627e2df97fea79610f80a
John Fastabend <john.fastabend@gmail.com>
2016-02-03 01:29:59 -0800

    net: ixgbe: add support for tc_u32 offload
    This adds initial support for offloading the u32 tc classifier. This
    initial implementation only implements a few base matches and actions
    to illustrate the use of the infrastructure patches.
    
    However it is an interesting subset because it handles the u32 next
    hdr logic to correctly map tcp packets from ip headers using the ihl
    and protocol fields. After this is accepted we can extend the match
    and action fields easily by updating the model header file.
    
    Also only the drop action is supported initially.
    
    Here is a short test script,
    
     #tc qdisc add dev eth4 ingress
     #tc filter add dev eth4 parent ffff: protocol ip \
    	u32 ht 800: order 1 \
    	match ip dst 15.0.0.1/32 match ip src 15.0.0.2/32 action drop
    
    <-- hardware has dst/src ip match rule installed -->
    
     #tc filter del dev eth4 parent ffff: prio 49152
     #tc filter add dev eth4 parent ffff: protocol ip prio 99 \
    	handle 1: u32 divisor 1
     #tc filter add dev eth4 protocol ip parent ffff: prio 99 \
    	u32 ht 800: order 1 link 1: \
    	offset at 0 mask 0f00 shift 6 plus 0 eat match ip protocol 6 ff
     #tc filter add dev eth4 parent ffff: protocol ip \
    	u32 ht 1: order 3 match tcp src 23 ffff action drop
    
    <-- hardware has tcp src port rule installed -->
    
     #tc qdisc del dev eth4 parent ffff:
    
    <-- hardware cleaned up -->
    
    Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe.h
+++ drivers/net/ethernet/intel/ixgbe/ixgbe.h
@@ -925,6 +925,9 @@ s32 ixgbe_fdir_erase_perfect_filter_82599(struct ixgbe_hw *hw,
 					  u16 soft_id);
 void ixgbe_atr_compute_perfect_hash_82599(union ixgbe_atr_input *input,
 					  union ixgbe_atr_input *mask);
+int ixgbe_update_ethtool_fdir_entry(struct ixgbe_adapter *adapter,
+				    struct ixgbe_fdir_filter *input,
+				    u16 sw_idx);
 void ixgbe_set_rx_mode(struct net_device *netdev);
 #ifdef CONFIG_IXGBE_DCB
 void ixgbe_set_rx_drop_en(struct ixgbe_adapter *adapter);
