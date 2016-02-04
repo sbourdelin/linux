4b2b2d409c186759ea2eb1ce66aa7668f7d5548e
Steffen Klassert <steffen.klassert@secunet.com>
2016-02-04 07:37:06 +0100

    net: Allow IPsec GSO for locally sent traffic.
    This patch finally allows locally sent IPsec packets to
    use the GSO codepath.
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1583,13 +1583,9 @@ void sk_setup_caps(struct sock *sk, struct dst_entry *dst)
 		sk->sk_route_caps |= NETIF_F_GSO_SOFTWARE;
 	sk->sk_route_caps &= ~sk->sk_route_nocaps;
 	if (sk_can_gso(sk)) {
-		if (dst->header_len) {
-			sk->sk_route_caps &= ~NETIF_F_GSO_MASK;
-		} else {
-			sk->sk_route_caps |= NETIF_F_SG | NETIF_F_HW_CSUM;
-			sk->sk_gso_max_size = dst->dev->gso_max_size;
-			max_segs = max_t(u32, dst->dev->gso_max_segs, 1);
-		}
+		sk->sk_route_caps |= NETIF_F_SG | NETIF_F_HW_CSUM;
+		sk->sk_gso_max_size = dst->dev->gso_max_size;
+		max_segs = max_t(u32, dst->dev->gso_max_segs, 1);
 	}
 	sk->sk_gso_max_segs = max_segs;
 }
