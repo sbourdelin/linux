526743800284f9bf1c3487754abdd0a04689986a
Waiman Long <Waiman.Long@hpe.com>
2016-02-09 14:47:42 -0500

    locking/mutex: Enable optimistic spinning of woken task in wait queue
    Ding Tianhong reported a live-lock situation where a constant stream
    of incoming optimistic spinners blocked a task in the wait list from
    getting the mutex.
    
    This patch attempts to fix this live-lock condition by enabling the
    woken task in the wait queue to enter into an optimistic spinning
    loop itself in parallel with the regular spinners in the OSQ. This
    should prevent the live-lock condition from happening.
    
    Running the AIM7 benchmarks on a 4-socket E7-4820 v3 system (with ext4
    filesystem), the additional spinning of the waiter-spinning improved
    performance for the following workloads at high user count:
    
      Workload	% Improvement
      --------	-------------
      alltests	    3.9%
      disk		    3.4%
      fserver	    2.0%
      long		    3.8%
      new_fserver	   10.5%
    
    The other workloads were about the same as before.
    
    Signed-off-by: Waiman Long <Waiman.Long@hpe.com>

--- kernel/locking/mutex.c
+++ kernel/locking/mutex.c
@@ -531,6 +531,7 @@ __mutex_lock_common(struct mutex *lock, long state, unsigned int subclass,
 	struct task_struct *task = current;
 	struct mutex_waiter waiter;
 	unsigned long flags;
+	bool  acquired = false;	/* True if the lock is acquired */
 	int ret;
 
 	preempt_disable();
@@ -561,7 +562,7 @@ __mutex_lock_common(struct mutex *lock, long state, unsigned int subclass,
 
 	lock_contended(&lock->dep_map, ip);
 
-	for (;;) {
+	while (!acquired) {
 		/*
 		 * Lets try to take the lock again - this is needed even if
 		 * we get here for the first time (shortly after failing to
@@ -596,6 +597,12 @@ __mutex_lock_common(struct mutex *lock, long state, unsigned int subclass,
 		/* didn't get the lock, go to sleep: */
 		spin_unlock_mutex(&lock->wait_lock, flags);
 		schedule_preempt_disabled();
+
+		/*
+		 * Optimistically spinning on the mutex without the wait lock
+		 */
+		acquired = mutex_optimistic_spin(lock, ww_ctx, use_ww_ctx,
+						 true);
 		spin_lock_mutex(&lock->wait_lock, flags);
 	}
 	__set_task_state(task, TASK_RUNNING);
@@ -605,6 +612,9 @@ __mutex_lock_common(struct mutex *lock, long state, unsigned int subclass,
 	if (likely(list_empty(&lock->wait_list)))
 		atomic_set(&lock->count, 0);
 	debug_mutex_free_waiter(&waiter);
+
+	if (acquired)
+		goto unlock;
 
 skip_wait:
 	/* got the lock - cleanup and rejoice! */
@@ -616,6 +626,7 @@ skip_wait:
 		ww_mutex_set_context_slowpath(ww, ww_ctx);
 	}
 
+unlock:
 	spin_unlock_mutex(&lock->wait_lock, flags);
 	preempt_enable();
 	return 0;
