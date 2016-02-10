379b20b31f14d8a7490866d77f3ff4fcb68b10b4
Colin Vidal <colin@cvidal.org>
2016-02-10 22:09:28 +0100

    Staging: fix coding style in rtl8188eu/core
    Set constant operand on right of test, and refactor the code in a more
    compact and readable way.
    
    Signed-off-by: Colin Vidal <colin@cvidal.org>

--- drivers/staging/rtl8188eu/core/rtw_iol.c
+++ drivers/staging/rtl8188eu/core/rtw_iol.c
@@ -18,14 +18,11 @@
  *
  ******************************************************************************/
 
-#include<rtw_iol.h>
+#include <rtw_iol.h>
 
-bool rtw_IOL_applied(struct adapter  *adapter)
+bool rtw_IOL_applied(struct adapter *adapter)
 {
-	if (1 == adapter->registrypriv.fw_iol)
-		return true;
-
-	if ((2 == adapter->registrypriv.fw_iol) && (!adapter_to_dvobj(adapter)->ishighspeed))
-		return true;
-	return false;
+	return adapter->registrypriv.fw_iol == 1 ||
+		(adapter->registrypriv.fw_iol == 2 &&
+		 !adapter_to_dvobj(adapter)->ishighspeed);
 }
