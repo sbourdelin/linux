37de4560f02d5a684252add3b07613de5e05cd99
Shuah Khan <shuahkh@osg.samsung.com>
2016-02-11 16:41:20 -0700

    media: Add ALSA Media Controller function entities
    Add ALSA Media Controller capture, playback, and mixer
    function entity defines.
    
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>

--- include/uapi/linux/media.h
+++ include/uapi/linux/media.h
@@ -98,6 +98,13 @@ struct media_device_info {
 #define MEDIA_ENT_F_IF_AUD_DECODER	(MEDIA_ENT_F_BASE + 42)
 
 /*
+ * Audio Entity Functions
+ */
+#define MEDIA_ENT_F_AUDIO_CAPTURE	(MEDIA_ENT_F_BASE + 200)
+#define MEDIA_ENT_F_AUDIO_PLAYBACK	(MEDIA_ENT_F_BASE + 201)
+#define MEDIA_ENT_F_AUDIO_MIXER		(MEDIA_ENT_F_BASE + 202)
+
+/*
  * Don't touch on those. The ranges MEDIA_ENT_F_OLD_BASE and
  * MEDIA_ENT_F_OLD_SUBDEV_BASE are kept to keep backward compatibility
  * with the legacy v1 API.The number range is out of range by purpose:
