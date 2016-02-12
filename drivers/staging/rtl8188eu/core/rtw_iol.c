b5f810097359e0ecc29144e7a80a7c3a2310fa00
Colin Vidal <colin@cvidal.org>
2016-02-12 19:05:53 +0100

    Staging: rtl8188eu/core: make core more readable
    Signed-off-by: Colin Vidal <colin@cvidal.org>

--- drivers/staging/rtl8188eu/core/rtw_iol.c
+++ drivers/staging/rtl8188eu/core/rtw_iol.c
@@ -18,11 +18,7 @@
 
 bool rtw_IOL_applied(struct adapter *adapter)
 {
-	if (adapter->registrypriv.fw_iol == 1)
-		return true;
-
-	if ((adapter->registrypriv.fw_iol == 2) &&
-	    (!adapter_to_dvobj(adapter)->ishighspeed))
-		return true;
-	return false;
+	return adapter->registrypriv.fw_iol == 1 ||
+		(adapter->registrypriv.fw_iol == 2 &&
+		 !adapter_to_dvobj(adapter)->ishighspeed);
 }
