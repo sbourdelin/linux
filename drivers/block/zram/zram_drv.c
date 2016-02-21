0dd46ff9eee51f5277f36bd633761a8167f452c1
Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
2016-02-21 22:27:53 +0900

    zram: use zs_get_huge_class_size_watermark()
    zram should stop enforcing its own 'bad' object size watermark,
    and start using zs_get_huge_class_size_watermark(). zsmalloc
    really knows better.
    
    Drop `max_zpage_size' and use zs_get_huge_class_size_watermark()
    instead.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -711,7 +711,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 		goto out;
 	}
 	src = zstrm->buffer;
-	if (unlikely(clen > max_zpage_size)) {
+	if (unlikely(clen > zs_get_huge_class_size_watermark())) {
 		clen = PAGE_SIZE;
 		if (is_partial_io(bvec))
 			src = uncmem;
