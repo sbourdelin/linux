8dc70afd6c364a8eeb6460324f3242b5fc3daf63
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2016-02-22 18:03:42 +0200

    dmaengine: dw: revisit data_width property
    There are several changes are done here:
    
     - Convert the property to be in bytes
    
       Much more convenient than keeping encoded value.
    
     - Use one value for all AHB masters for now
    
       It seems in practice we have no controllers where masters have different
       data bus width, we still might return to distinct values when there is a use
       case.
    
     - Rename data_width to data-width in the device tree bindings.
    
     - While here, replace dwc_fast_ffs() by __ffs().
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Mans Rullgard <mans@mansr.com>

--- include/linux/platform_data/dma-dw.h
+++ include/linux/platform_data/dma-dw.h
@@ -42,8 +42,7 @@ struct dw_dma_slave {
  * @chan_priority: Set channel priority increasing from 0 to 7 or 7 to 0.
  * @block_size: Maximum block size supported by the controller
  * @nr_masters: Number of AHB masters supported by the controller
- * @data_width: Maximum data width supported by hardware per AHB master
- *		(0 - 8bits, 1 - 16bits, ..., 5 - 256bits)
+ * @data_width: Maximum data width supported by hardware (in bytes)
  */
 struct dw_dma_platform_data {
 	unsigned int	nr_channels;
@@ -57,7 +56,7 @@ struct dw_dma_platform_data {
 	unsigned char	chan_priority;
 	unsigned short	block_size;
 	unsigned char	nr_masters;
-	unsigned char	data_width[DW_DMA_MAX_NR_MASTERS];
+	unsigned char	data_width;
 };
 
 #endif /* _PLATFORM_DATA_DMA_DW_H */
