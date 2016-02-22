b1e9c8b527953de5ebc654e7bb34de51f736ae57
Sakari Ailus <sakari.ailus@iki.fi>
2016-02-22 22:47:02 +0200

    media: Rearrange the fields in the G_TOPOLOGY IOCTL argument
    This avoids having multiple reserved fields in the struct. Reserved fields
    are added in order to align the struct size to a power of two as well.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- include/uapi/linux/media.h
+++ include/uapi/linux/media.h
@@ -341,21 +341,16 @@ struct media_v2_link {
 struct media_v2_topology {
 	__u64 topology_version;
 
-	__u32 num_entities;
-	__u32 reserved1;
 	__u64 ptr_entities;
-
-	__u32 num_interfaces;
-	__u32 reserved2;
 	__u64 ptr_interfaces;
-
-	__u32 num_pads;
-	__u32 reserved3;
 	__u64 ptr_pads;
-
-	__u32 num_links;
-	__u32 reserved4;
 	__u64 ptr_links;
+
+	__u32 num_entities;
+	__u32 num_interfaces;
+	__u32 num_pads;
+	__u32 num_links;
+	__u32 reserved[18];
 };
 
 static inline void __user *media_get_uptr(__u64 arg)
