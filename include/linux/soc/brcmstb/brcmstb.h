85a2cc5df1a7706c79381db8f63d82931f848757
Justin Chen <justin.chen@broadcom.com>
2016-02-25 16:19:07 -0800

    bsp: add SoC device to brcmstb
    Added SoC driver at drivers/soc/brcmstb/common.c. Added two
    helper functions for getting family/product ID located at
    include/linux/soc/brcmstb/brcmstb.h.
    
    Assigned the values of soc_dev_attribute accordingly:
    family = chip family id
    soc_id = product id
    revision = product revision
    
    Signed-off-by: Justin Chen <justin.chen@broadcom.com>

--- include/linux/soc/brcmstb/brcmstb.h
+++ include/linux/soc/brcmstb/brcmstb.h
@@ -7,4 +7,11 @@
  */
 void brcmstb_biuctrl_init(void);
 
+/*
+ * Helper functions for getting family or product id from the
+ * SoC driver.
+ */
+u32 brcmstb_get_family_id(void);
+u32 brcmstb_get_product_id(void);
+
 #endif /* __BRCMSTB_SOC_H */
