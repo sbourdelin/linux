a1c9def8c95886fbf0cc855c063119c84dca07d2
John Crispin <blogic@openwrt.org>
2016-02-26 15:21:35 +0100

    net-next: mediatek: add embedded switch driver (ESW)
    The ESW is found in many of the old 100mbit MIPS based SoCs. it has 5
    external ports, 1 cpu port and 1 further port that the internal HW
    offloading engine connects to.
    
    This driver is very basic and only provides basic init and irq support.
    The SoC and switch core both have support for a special tag making DSA
    support possible.
    
    I have managed to work out all the names of the registers of the ESW,
    however the init for the phys uses lots of voodoo magic which is based
    on the SDK driver code. It will be hard to find out the real names for
    the phy registers and their bits.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>

---
+++ drivers/net/ethernet/mediatek/esw_rt3050.h
@@ -0,0 +1,21 @@
+/*   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   Copyright (C) 2009-2016 John Crispin <blogic@openwrt.org>
+ *   Copyright (C) 2009-2016 Felix Fietkau <nbd@openwrt.org>
+ *   Copyright (C) 2013-2016 Michael Lee <igvtee@gmail.com>
+ */
+
+#ifndef _RALINK_ESW_RT3052_H__
+#define _RALINK_ESW_RT3052_H__
+
+int __init mtk_esw_init(struct mtk_eth *eth);
+void mtk_switch_exit(struct mtk_eth *eth);
+
+#endif
