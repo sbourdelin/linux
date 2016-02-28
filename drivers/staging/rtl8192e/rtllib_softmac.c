0c21bf380c29c625a20de8a37f94f5efddd6d852
Tapan Prakash T <tapanprakasht@gmail.com>
2016-02-28 22:35:48 +0530

    staging: rtl8192e: rtllib_softmac.c: Add necessary blank lines
    Fixed blank lines related issues reported by checkpatch.pl
    
    Signed-off-by: Tapan Prakash T <tapanprakasht@gmail.com>

--- drivers/staging/rtl8192e/rtllib_softmac.c
+++ drivers/staging/rtl8192e/rtllib_softmac.c
@@ -1521,6 +1521,7 @@ static void rtllib_associate_complete_wq(void *data)
 				     struct rtllib_device,
 				     associate_complete_wq);
 	struct rt_pwr_save_ctrl *pPSC = &(ieee->PowerSaveControl);
+
 	netdev_info(ieee->dev, "Associated successfully\n");
 	if (!ieee->is_silent_reset) {
 		netdev_info(ieee->dev, "normal associate\n");
@@ -2755,6 +2756,7 @@ static void rtllib_link_change_wq(void *data)
 				     struct rtllib_device, link_change_wq);
 	ieee->link_change(ieee->dev);
 }
+
 /* called only in userspace context */
 void rtllib_disassociate(struct rtllib_device *ieee)
 {
