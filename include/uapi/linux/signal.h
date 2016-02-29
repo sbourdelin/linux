eb98f712d3de733f2429614c41584b2299c66130
Stas Sergeev <stsp@list.ru>
2016-03-01 00:29:04 +0300

    sigaltstack: implement SS_AUTODISARM flag
    This patch implements the SS_AUTODISARM flag that can be ORed with
    SS_ONSTACK when forming ss_flags.
    When this flag is set, sigaltstack will be disabled when entering
    the signal handler; more precisely, after saving sas to uc_stack.
    When leaving the signal handler, the sigaltstack is restored by
    uc_stack.
    When this flag is used, it is safe to switch from sighandler with
    swapcontext(). Without this flag, the subsequent signal will corrupt
    the state of the switched-away sighandler.
    
    CC: Ingo Molnar <mingo@redhat.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: Richard Weinberger <richard@nod.at>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: Oleg Nesterov <oleg@redhat.com>
    CC: Tejun Heo <tj@kernel.org>
    CC: Heinrich Schuchardt <xypron.glpk@gmx.de>
    CC: Jason Low <jason.low2@hp.com>
    CC: Andrea Arcangeli <aarcange@redhat.com>
    CC: Frederic Weisbecker <fweisbec@gmail.com>
    CC: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    CC: Josh Triplett <josh@joshtriplett.org>
    CC: "Eric W. Biederman" <ebiederm@xmission.com>
    CC: Aleksa Sarai <cyphar@cyphar.com>
    CC: "Amanieu d'Antras" <amanieu@gmail.com>
    CC: Paul Moore <pmoore@redhat.com>
    CC: Sasha Levin <sasha.levin@oracle.com>
    CC: Palmer Dabbelt <palmer@dabbelt.com>
    CC: Vladimir Davydov <vdavydov@parallels.com>
    CC: linux-kernel@vger.kernel.org
    CC: linux-api@vger.kernel.org
    CC: Andy Lutomirski <luto@amacapital.net>
    
    Signed-off-by: Stas Sergeev <stsp@users.sourceforge.net>

--- include/uapi/linux/signal.h
+++ include/uapi/linux/signal.h
@@ -6,5 +6,8 @@
 
 #define SS_ONSTACK	1
 #define SS_DISABLE	2
+#define SS_VALMASK	0xf
+/* bit-flags */
+#define SS_AUTODISARM	(1 << 4)	/* disable sas during sighandling */
 
 #endif /* _UAPI_LINUX_SIGNAL_H */
