96dbea4a5a63f9ba7c2e536fd53cf709e64059b4
Brian Bloniarz <brian.bloniarz@gmail.com>
2016-02-28 19:56:25 -0800

    n_tty: Check the other end of pty pair before returning EAGAIN on a read()
    (Take 3, fix compile error in n_hdlc.c)
    
    Hi Peter, I saw Marc Aurele La France's proposed patch to n_tty to fix
    OpenSSH, and your feedback. Patch below is an attempt to address that
    feedback. Please let me know if this is the change you envisioned;
    (see Marc's excellent original writeup for details on the issue).
    
    [PATCH] n_tty: wait for buffer work in read() and poll().
    
    Undoes the following four changes:
    
    1) f95499c3030fe1bfad57745f2db1959c5b43dca8
        n_tty: Don't wait for buffer work in read() loop
    
    2) f8747d4a466ab2cafe56112c51b3379f9fdb7a12
        tty: Fix pty master read() after slave closes
    
    3) 52bce7f8d4fc633c9a9d0646eef58ba6ae9a3b73
        pty, n_tty: Simplify input processing on final close
    
    4) 1a48632ffed61352a7810ce089dc5a8bcd505a60
        pty: Fix input race when closing
    
    These changes caused a regression in OpenSSH, as it assumes that the
    first read() to return EAGAIN after a SIGCHLD means that all the child's
    output has been returned.
    
    Inspired by analysis and patch from Marc Aurele La France <tsi@tuyoix.net>
    
    Reported-by: Volth <openssh@volth.com>
    Reported-by: Marc Aurele La France <tsi@tuyoix.net>
    BugLink: https://bugzilla.mindrot.org/show_bug.cgi?id=52
    BugLink: https://bugzilla.mindrot.org/show_bug.cgi?id=2492
    Signed-off-by: Brian Bloniarz <brian.bloniarz@gmail.com>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -338,7 +338,6 @@ struct tty_file_private {
 #define TTY_EXCLUSIVE 		3	/* Exclusive open mode */
 #define TTY_DEBUG 		4	/* Debugging */
 #define TTY_DO_WRITE_WAKEUP 	5	/* Call write_wakeup after queuing new */
-#define TTY_OTHER_DONE		6	/* Closed pty has completed input processing */
 #define TTY_LDISC_OPEN	 	11	/* Line discipline is open */
 #define TTY_PTY_LOCK 		16	/* pty private */
 #define TTY_NO_WRITE_SPLIT 	17	/* Preserve write boundaries to driver */
