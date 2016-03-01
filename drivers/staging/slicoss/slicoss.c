e022611da65373926bbea9db9bb6d497dcf48bae
Ben Marsh <bmarsh94@gmail.com>
2016-03-01 11:22:48 +0100

    Staging: slicoss: removes unnecessary blank lines in slicoss.c
    This is a patch to slicoss.c to remove unnecessary blank lines as found
    by checkpatch.pl
    
    Signed-off-by: Ben Marsh <bmarsh94@gmail.com>

--- drivers/staging/slicoss/slicoss.c
+++ drivers/staging/slicoss/slicoss.c
@@ -792,7 +792,6 @@ static bool slic_mac_filter(struct adapter *adapter,
 		return true;
 	}
 	return false;
-
 }
 
 static int slic_mac_set_address(struct net_device *dev, void *ptr)
@@ -1769,7 +1768,6 @@ static void slic_init_cleanup(struct adapter *adapter)
 	if (adapter->intrregistered) {
 		adapter->intrregistered = 0;
 		free_irq(adapter->netdev->irq, adapter->netdev);
-
 	}
 	if (adapter->pshmem) {
 		pci_free_consistent(adapter->pcidev,
