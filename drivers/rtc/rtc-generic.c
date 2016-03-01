8bd7f836a53d4c8be4db6efeb45082df8cd5fe49
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2016-03-01 18:16:40 +0530

    rtc: rtc-generic: use correct header file
    The allmodconfig build of m32r fails with errors like:
    
    In file included from drivers/rtc/rtc-generic.c:12:0:
    arch/m32r/include/asm/rtc.h:46:8: error: redefinition of 'struct rtc_time'
    
    drivers/rtc/rtc-generic.c: In function 'generic_get_time':
    drivers/rtc/rtc-generic.c:16:21: error: implicit declaration of function 'get_rtc_time'
    
    drivers/rtc/rtc-generic.c:18:12: error: 'RTC_BATT_BAD' undeclared
    
    drivers/rtc/rtc-generic.c: In function 'generic_set_time':
    drivers/rtc/rtc-generic.c:26:6: error: implicit declaration of function 'set_rtc_time'
    
    Apart from m32r, many other arch like xtensa, sparc are failing with:
    drivers/rtc/rtc-generic.c:12:21: fatal error: asm/rtc.h: No such file or directory
    
    Other rtc drivers are using asm-generic/rtc.h, so use it here.
    
    Fixes: 2a32c792f1c9 ("rtc: enable COMPILE_TEST")
    Signed-off-by: Sudip Mukherjee <sudip.mukherjee@codethink.co.uk>

--- drivers/rtc/rtc-generic.c
+++ drivers/rtc/rtc-generic.c
@@ -9,7 +9,7 @@
 #include <linux/platform_device.h>
 #include <linux/rtc.h>
 
-#include <asm/rtc.h>
+#include <asm-generic/rtc.h>
 
 static int generic_get_time(struct device *dev, struct rtc_time *tm)
 {
