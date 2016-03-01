42815562f1a2b610d5217cd563b23cfda2f08e04
Thierry Reding <thierry.reding@gmail.com>
2016-03-01 15:55:58 +0100

    rtc: rtc-vr41xx: remove unused function
    On Tue, Mar 01, 2016 at 06:26:21PM +0530, Sudip Mukherjee wrote:
    > We were getting build warning about:
    > drivers/rtc/rtc-vr41xx.c:228:12: warning: ‘vr41xx_rtc_alarm_irq_enable’
    > defined but not used
    >
    > The function vr41xx_rtc_alarm_irq_enable() was not used anywhere.
    >
    > Signed-off-by: Sudip Mukherjee <sudip.mukherjee@codethink.co.uk>
    > ---
    >
    > build log is at:
    > https://travis-ci.org/sudipm-mukherjee/parport/jobs/112780206
    >
    >  drivers/rtc/rtc-vr41xx.c | 18 ------------------
    >  1 file changed, 18 deletions(-)
    >
    > diff --git a/drivers/rtc/rtc-vr41xx.c b/drivers/rtc/rtc-vr41xx.c
    > index f64c282..68cb63e 100644
    > --- a/drivers/rtc/rtc-vr41xx.c
    > +++ b/drivers/rtc/rtc-vr41xx.c
    > @@ -225,24 +225,6 @@ static int vr41xx_rtc_ioctl(struct device *dev, unsigned int cmd, unsigned long
    >  	return 0;
    >  }
    >
    > -static int vr41xx_rtc_alarm_irq_enable(struct device *dev, unsigned int enabled)
    > -{
    > -	spin_lock_irq(&rtc_lock);
    > -	if (enabled) {
    > -		if (!alarm_enabled) {
    > -			enable_irq(aie_irq);
    > -			alarm_enabled = 1;
    > -		}
    > -	} else {
    > -		if (alarm_enabled) {
    > -			disable_irq(aie_irq);
    > -			alarm_enabled = 0;
    > -		}
    > -	}
    > -	spin_unlock_irq(&rtc_lock);
    > -	return 0;
    > -}
    > -
    
    I don't think it's supposed to be unused, rather back at the time it
    simply wasn't hooked up to the rtc_class_ops.
    
    Here's what I used.
    
    Thierry

--- drivers/rtc/rtc-vr41xx.c
+++ drivers/rtc/rtc-vr41xx.c
@@ -278,6 +278,7 @@ static const struct rtc_class_ops vr41xx_rtc_ops = {
 	.set_time	= vr41xx_rtc_set_time,
 	.read_alarm	= vr41xx_rtc_read_alarm,
 	.set_alarm	= vr41xx_rtc_set_alarm,
+	.alarm_irq_enable = vr41xx_rtc_alarm_irq_enable,
 };
 
 static int rtc_probe(struct platform_device *pdev)
