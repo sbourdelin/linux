d3cc6e1c2da9c0ecb8512220d4e24a054503b6a7
Parav Pandit <pandit.parav@gmail.com>
2016-03-01 19:56:38 +0530

    rdmacg: Added rdma cgroup controller
    Added rdma cgroup controller that does accounting, limit enforcement
    on rdma/IB verbs and hw resources.
    
    Added rdma cgroup header file which defines its APIs to perform
    charing/uncharing functionality and device registration which will
    participate in controller functions of accounting and limit
    enforcements. It also define rdmacg_device structure to bind IB stack
    and RDMA cgroup controller.
    
    RDMA resources are tracked using resource pool. Resource pool is per
    device, per cgroup entity which allows setting up accounting limits
    on per device basis.
    
    Resources are not defined by the RDMA cgroup, instead they are defined
    by the external module IB stack. This allows extending IB stack
    without changing kernel, as IB stack is going through changes
    and enhancements.
    
    Resource pool is created/destroyed dynamically whenever
    charging/uncharging occurs respectively and whenever user
    configuration is done. Its a tradeoff of memory vs little more code
    space that creates resource pool whenever necessary,
    instead of creating them during cgroup creation and device registration
    time.
    
    Signed-off-by: Parav Pandit <pandit.parav@gmail.com>
    Reviewed-by: Haggai Eran <haggaie@mellanox.com>

---
+++ include/linux/cgroup_rdma.h
@@ -0,0 +1,51 @@
+#ifndef _CGROUP_RDMA_H
+#define _CGROUP_RDMA_H
+
+#include <linux/cgroup.h>
+
+#ifdef CONFIG_CGROUP_RDMA
+
+struct rdma_cgroup {
+	struct cgroup_subsys_state	css;
+
+	spinlock_t rpool_list_lock;	/* protects resource pool list */
+	struct list_head rpool_head;	/* head to keep track of all resource
+					 * pools that belongs to this cgroup.
+					 */
+};
+
+struct rdmacg_pool_info {
+	const char **resource_name_table;
+	int table_len;
+};
+
+struct rdmacg_device {
+	struct rdmacg_pool_info pool_info;
+	struct list_head	rdmacg_list;
+	struct list_head	rpool_head;
+	spinlock_t		rpool_lock;	/*
+						 * protects resource pool list
+						 */
+	char			*name;
+};
+
+/* APIs for RDMA/IB stack to publish when a device wants to
+ * participate in resource accounting
+ */
+int rdmacg_register_device(struct rdmacg_device *device);
+void rdmacg_unregister_device(struct rdmacg_device *device);
+
+/* APIs for RDMA/IB stack to charge/uncharge pool specific resources */
+int rdmacg_try_charge(struct rdma_cgroup **rdmacg,
+		      struct rdmacg_device *device,
+		      int resource_index,
+		      int num);
+void rdmacg_uncharge(struct rdma_cgroup *cg,
+		     struct rdmacg_device *device,
+		     int resource_index,
+		     int num);
+void rdmacg_query_limit(struct rdmacg_device *device,
+			int *limits);
+
+#endif	/* CONFIG_CGROUP_RDMA */
+#endif	/* _CGROUP_RDMA_H */
