688ffb2f62d0996b453fbce1984beeec5d155e65
Subhransu S. Prusty <subhransu.s.prusty@intel.com>
2016-03-02 17:33:23 +0530

    ALSA: hda - Register chmap obj as priv data instead of codec
    With this chmap object is added as private data and new ops are
    added to access driver specific chmap.
    
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- include/sound/hdmi_chmap.h
+++ include/sound/hdmi_chmap.h
@@ -30,6 +30,12 @@ struct hdmi_chmap_ops {
 
 	/* check that the user-given chmap is supported */
 	int (*chmap_validate)(int ca, int channels, unsigned char *chmap);
+
+	void (*get_chmap)(struct hdac_device *hdac, int pcm_idx,
+					unsigned char *chmap);
+	void (*set_chmap)(struct hdac_device *hdac, int pcm_idx,
+			unsigned char *chmap, int prepared);
+	bool (*is_monitor_connected)(struct hdac_device *hdac, int pcm_idx);
 };
 
 struct hdmi_chmap {
