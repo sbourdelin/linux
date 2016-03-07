8f27c456895040fdb3fc54d4945c2ec0885efc0f
Yetunde Adebisi <yetundex.adebisi@intel.com>
2016-03-07 12:58:19 +0000

    drm/i915: Add Backlight Control using DPCD for eDP connectors (v7)
    This patch adds support for eDP backlight control using DPCD registers to
    backlight hooks in intel_panel.
    
    It checks for backlight control over AUX channel capability and sets up
    function pointers to get and set the backlight brightness level if
    supported.
    
    v2: Moved backlight functions from intel_dp.c into a new file
    intel_dp_aux_backlight.c. Also moved reading of eDP display control
    registers to intel_dp_get_dpcd
    
    v3: Correct some formatting mistakes
    
    v4: Updated to use AUX backlight control if PWM control is not possible
    	(Jani)
    v5: Moved call to initialize backlight registers to dp_aux_setup_backlight
    v6: Check DP_EDP_BACKLIGHT_PIN_ENABLE_CAP is disabled before setting up AUX
    backlight control. To fix BLM_PWM_ENABLE igt test warnings on bdw_ultra
    v7: Add enable_dpcd_backlight module parameter.
    
    This patch depends on http://patchwork.freedesktop.org/patch/64253/
    
    Cc: Bob Paauwe <bob.j.paauwe@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Yetunde Adebisi <yetundex.adebisi@intel.com>

--- drivers/gpu/drm/i915/i915_params.c
+++ drivers/gpu/drm/i915/i915_params.c
@@ -56,6 +56,7 @@ struct i915_params i915 __read_mostly = {
 	.edp_vswing = 0,
 	.enable_guc_submission = false,
 	.guc_log_level = -1,
+	.enable_dpcd_backlight = false,
 };
 
 module_param_named(modeset, i915.modeset, int, 0400);
@@ -201,3 +202,7 @@ MODULE_PARM_DESC(enable_guc_submission, "Enable GuC submission (default:false)")
 module_param_named(guc_log_level, i915.guc_log_level, int, 0400);
 MODULE_PARM_DESC(guc_log_level,
 	"GuC firmware logging level (-1:disabled (default), 0-3:enabled)");
+
+module_param_named(enable_dpcd_backlight, i915.enable_dpcd_backlight, bool, 0600);
+MODULE_PARM_DESC(enable_dpcd_backlight,
+	"Enable support for DPCD backlight control (default:false)");
