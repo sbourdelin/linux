fc64e1ae71ac91db3d1c44a6510fb63f7e43bde4
Jacob Keller <jacob.e.keller@intel.com>
2016-03-11 09:52:31 -0800

    rework of "fm10k: prevent RCU issues during AER events
    This is a rework of the original patch 308f3d5aad0e ("fm10k: prevent RCU
    issues during AER events", 2016-03-09), and the following email is a
    direct rework which replaces it in the queue. Thus, the previously
    mentioned version should be dropped from the queue.
    
    Patch interdiff for review:

--- drivers/net/ethernet/intel/fm10k/fm10k_pci.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_pci.c
@@ -2337,11 +2337,13 @@ static void fm10k_io_resume(struct pci_dev *pdev)
 	/* reset statistics starting values */
 	hw->mac.ops.rebind_hw_stats(hw, &interface->stats);
 
+	rtnl_lock();
+
 	err = fm10k_init_queueing_scheme(interface);
 	if (err) {
 		dev_err(&interface->pdev->dev,
 			"init_queueing_scheme failed: %d\n", err);
-		return;
+		goto unlock;
 	}
 
 	/* reassociate interrupts */
@@ -2358,6 +2360,9 @@ static void fm10k_io_resume(struct pci_dev *pdev)
 
 	if (!err)
 		netif_device_attach(netdev);
+
+unlock:
+	rtnl_unlock();
 }
 
 static const struct pci_error_handlers fm10k_err_handler = {
