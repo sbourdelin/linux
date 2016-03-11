332838de9974bb343fa7ce4941d1bd307fc59699
Shawn Lin <shawn.lin@rock-chips.com>
2016-03-11 10:29:44 +0800

    f2fs: remove redundant checking of inline data flag
    Remove the f2fs_has_inline_data for f2fs_read_data_page,
    and let f2fs_read_inline_data take over gatekeeper of
    checking inline data flag.
    
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1047,9 +1047,9 @@ static int f2fs_read_data_page(struct file *file, struct page *page)
 
 	trace_f2fs_readpage(page, DATA);
 
-	/* If the file has inline data, try to read it directly */
-	if (f2fs_has_inline_data(inode))
-		ret = f2fs_read_inline_data(inode, page);
+	/* Firstly, let's try to read inline data directly */
+	ret = f2fs_read_inline_data(inode, page);
+
 	if (ret == -EAGAIN)
 		ret = f2fs_mpage_readpages(page->mapping, NULL, page, 1);
 	return ret;
