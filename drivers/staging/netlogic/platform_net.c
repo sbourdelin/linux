6e1ed20b76df5bb96df672bf6f232e8ef1a6f588
Parth Sane <laerdevstudios@gmail.com>
2016-03-15 16:23:12 +0000

    staging: netlogic: removed 1 multiple assignment checkpatch warning

--- drivers/staging/netlogic/platform_net.c
+++ drivers/staging/netlogic/platform_net.c
@@ -86,7 +86,8 @@ static void xlr_resource_init(struct resource *res, int offset, int irq)
 
 	res++;
 	res->name = "gmac";
-	res->start = res->end = irq;
+	res->end = irq;
+	res->start = res->end;
 	res->flags = IORESOURCE_IRQ;
 }
 
