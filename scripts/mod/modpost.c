0fb8d3b24206061788d526ecc242f3068e8cab56
Chris Metcalf <cmetcalf@mellanox.com>
2016-03-16 13:02:13 -0400

    nmi_backtrace: generate one-line reports for idle cpus
    When doing an nmi backtrace of many cores, most of which are idle,
    the output is a little overwhelming and very uninformative.  Suppress
    messages for cpus that are idling when they are interrupted and just
    emit one line, "NMI backtrace for N skipped: idling at pc 0xNNN".
    
    We do this by grouping all the cpuidle code together into a new
    .cpuidle.text section, and then checking the address of the
    interrupted PC to see if it lies within that section.
    
    Signed-off-by: Chris Metcalf <cmetcalf@mellanox.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -887,8 +887,8 @@ static void check_section(const char *modname, struct elf_info *elf,
 #define ALL_EXIT_SECTIONS EXIT_SECTIONS, ALL_XXXEXIT_SECTIONS
 
 #define DATA_SECTIONS ".data", ".data.rel"
-#define TEXT_SECTIONS ".text", ".text.unlikely", ".sched.text", \
-		".kprobes.text"
+#define TEXT_SECTIONS ".text", ".text.unlikely", \
+		".kprobes.text", ".cpuidle.text"
 #define OTHER_TEXT_SECTIONS ".ref.text", ".head.text", ".spinlock.text", \
 		".fixup", ".entry.text", ".exception.text", ".text.*", \
 		".coldtext"
