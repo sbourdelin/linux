b8d93af7f563e51222dba6b223cbdf260d5ded45
Ebru Akagunduz <ebru.akagunduz@gmail.com>
2016-03-20 20:07:39 +0200

    mm, thp: avoid unnecessary swapin in khugepaged
    Currently khugepaged makes swapin readahead to improve
    THP collapse rate. This patch checks vm statistics
    to avoid workload of swapin, if unnecessary. So that
    when system under pressure, khugepaged won't consume
    resources to swapin.
    
    The patch was tested with a test program that allocates
    800MB of memory, writes to it, and then sleeps. The system
    was forced to swap out all. Afterwards, the test program
    touches the area by writing, it skips a page in each
    20 pages of the area. When waiting to swapin readahead
    left part of the test, the memory forced to be busy
    doing page reclaim. There was enough free memory during
    test, khugepaged did not swapin readahead due to business.
    
    Test results:
    
                            After swapped out
    -------------------------------------------------------------------
                  | Anonymous | AnonHugePages | Swap      | Fraction  |
    -------------------------------------------------------------------
    With patch    | 217888 kB |  217088 kB    | 582112 kB |    %99    |
    -------------------------------------------------------------------
    Without patch | 351308 kB | 350208 kB     | 448692 kB |    %99    |
    -------------------------------------------------------------------
    
                            After swapped in (waiting 10 minutes)
    -------------------------------------------------------------------
                  | Anonymous | AnonHugePages | Swap      | Fraction  |
    -------------------------------------------------------------------
    With patch    | 604440 kB | 348160 kB     | 195560 kB |    %57    |
    -------------------------------------------------------------------
    Without patch | 586816 kB | 464896 kB     | 213184 kB |    %79    |
    -------------------------------------------------------------------
    
    Signed-off-by: Ebru Akagunduz <ebru.akagunduz@gmail.com>
    Fixes: 363cd76e5b11c ("mm: make swapin readahead to improve thp collapse rate")

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -102,6 +102,7 @@ static DECLARE_WAIT_QUEUE_HEAD(khugepaged_wait);
  */
 static unsigned int khugepaged_max_ptes_none __read_mostly;
 static unsigned int khugepaged_max_ptes_swap __read_mostly;
+static unsigned long allocstall;
 
 static int khugepaged(void *none);
 static int khugepaged_slab_init(void);
@@ -2438,7 +2439,7 @@ static void collapse_huge_page(struct mm_struct *mm,
 	struct page *new_page;
 	spinlock_t *pmd_ptl, *pte_ptl;
 	int isolated = 0, result = 0;
-	unsigned long hstart, hend;
+	unsigned long hstart, hend, swap, curr_allocstall;
 	struct mem_cgroup *memcg;
 	unsigned long mmun_start;	/* For mmu_notifiers */
 	unsigned long mmun_end;		/* For mmu_notifiers */
@@ -2493,7 +2494,14 @@ static void collapse_huge_page(struct mm_struct *mm,
 		goto out;
 	}
 
-	__collapse_huge_page_swapin(mm, vma, address, pmd);
+	swap = get_mm_counter(mm, MM_SWAPENTS);
+	curr_allocstall = sum_vm_event(ALLOCSTALL);
+	/*
+	 * When system under pressure, don't swapin readahead.
+	 * So that avoid unnecessary resource consuming.
+	 */
+	if (allocstall == curr_allocstall && swap != 0)
+		__collapse_huge_page_swapin(mm, vma, address, pmd);
 
 	anon_vma_lock_write(vma->anon_vma);
 
@@ -2905,6 +2913,7 @@ static int khugepaged(void *none)
 	set_user_nice(current, MAX_NICE);
 
 	while (!kthread_should_stop()) {
+		allocstall = sum_vm_event(ALLOCSTALL);
 		khugepaged_do_scan();
 		khugepaged_wait_work();
 	}
