d2746dd8a360f6d60b15d404e3155b352c84d8d4
Lee Jones <lee.jones@linaro.org>
2016-03-23 14:43:41 +0000

    mailbox: mailbox-test: Use more consistent format for calling copy_from_user()
    While we're at it, ensure copy-to location is NULL'ed in the error path.
    
    Suggested-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mailbox/mailbox-test.c
+++ drivers/mailbox/mailbox-test.c
@@ -45,7 +45,6 @@ static ssize_t mbox_test_signal_write(struct file *filp,
 				       size_t count, loff_t *ppos)
 {
 	struct mbox_test_device *tdev = filp->private_data;
-	int ret;
 
 	if (!tdev->tx_channel) {
 		dev_err(tdev->dev, "Channel cannot do Tx\n");
@@ -63,13 +62,13 @@ static ssize_t mbox_test_signal_write(struct file *filp,
 	if (!tdev->signal)
 		return -ENOMEM;
 
-	ret = copy_from_user(tdev->signal, userbuf, count);
-	if (ret) {
+	if (copy_from_user(tdev->signal, userbuf, count)) {
 		kfree(tdev->signal);
+		tdev->signal = NULL;
 		return -EFAULT;
 	}
 
-	return ret < 0 ? ret : count;
+	return count;
 }
 
 static const struct file_operations mbox_test_signal_ops = {
