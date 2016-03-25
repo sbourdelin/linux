95c613fa6a9be976047935637cbd2f62efa967b4
Yaowei Bai <baiyaowei@cmss.chinamobile.com>
2016-03-25 10:11:59 +0800

    drivers/ata: delete unused ata_using_mwdma function
    There's no user of ata_using_mwdma since introduced by commit
    b15b3ebae ("libata: Fix a large collection of DMA mode mismatches")
    in 2008, so remove it.
    
    Signed-off-by: Yaowei Bai <baiyaowei@cmss.chinamobile.com>

--- include/linux/libata.h
+++ include/linux/libata.h
@@ -1759,13 +1759,6 @@ static inline unsigned long ata_deadline(unsigned long from_jiffies,
    change in future hardware and specs, secondly 0xFF means 'no DMA' but is
    > UDMA_0. Dyma ddreigiau */
 
-static inline int ata_using_mwdma(struct ata_device *adev)
-{
-	if (adev->dma_mode >= XFER_MW_DMA_0 && adev->dma_mode <= XFER_MW_DMA_4)
-		return 1;
-	return 0;
-}
-
 static inline int ata_using_udma(struct ata_device *adev)
 {
 	if (adev->dma_mode >= XFER_UDMA_0 && adev->dma_mode <= XFER_UDMA_7)
