f38c147f49aac60391cdff92d5b8353f8d38eb4b
Bart Van Assche <bart.vanassche@sandisk.com>
2016-03-28 14:36:07 -0700

    Move two declarations from scsi_scan.c into scsi_priv.h
    Consistency of the declarations that have been moved can only be
    checked by the compiler if these are present in a header file that
    is included from both the source file with the definition and the
    source file with the caller(s). Hence move these two declarations.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>

--- drivers/scsi/scsi_priv.h
+++ drivers/scsi/scsi_priv.h
@@ -83,6 +83,8 @@ int scsi_noretry_cmd(struct scsi_cmnd *scmd);
 extern int scsi_maybe_unblock_host(struct scsi_device *sdev);
 extern void scsi_device_unbusy(struct scsi_device *sdev);
 extern void scsi_queue_insert(struct scsi_cmnd *cmd, int reason);
+extern void scsi_requeue_run_queue(struct work_struct *work);
+extern void scsi_evt_thread(struct work_struct *work);
 extern void scsi_io_completion(struct scsi_cmnd *, unsigned int);
 extern void scsi_run_host_queues(struct Scsi_Host *shost);
 extern struct request_queue *scsi_alloc_queue(struct scsi_device *sdev);
