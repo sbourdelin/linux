97b6e3cdfea275a29a8219def8fdfb6dbd333535
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2016-04-05 11:24:33 -0400

    net: dsa: make the STP state function return void
    The DSA layer doesn't care about the return code of the port_stp_update
    routine, so make it void in the layer and the DSA drivers.
    
    Replace the useless dsa_slave_stp_update function with a
    dsa_slave_stp_state function used to reply to the switchdev
    SWITCHDEV_ATTR_ID_PORT_STP_STATE attribute.
    
    In the meantime, rename port_stp_update to port_stp_state to explicit
    the state change.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -299,8 +299,7 @@ struct dsa_switch_driver {
 	int	(*port_bridge_join)(struct dsa_switch *ds, int port,
 				    struct net_device *bridge);
 	void	(*port_bridge_leave)(struct dsa_switch *ds, int port);
-	int	(*port_stp_update)(struct dsa_switch *ds, int port,
-				   u8 state);
+	void	(*port_stp_state)(struct dsa_switch *ds, int port, u8 state);
 
 	/*
 	 * VLAN support
