62b5ec50222eff5bc494968f042647d7c1e634c5
Waiman Long <Waiman.Long@hpe.com>
2016-04-14 19:32:28 -0400

    lib/dlock-list: Distributed and lock-protected lists
    Linked list is used everywhere in the Linux kernel. However, if many
    threads are trying to add or delete entries into the same linked list,
    it can create a performance bottleneck.
    
    This patch introduces a new list APIs that provide a set of distributed
    lists (one per CPU), each of which is protected by its own spinlock.
    To the callers, however, the set of lists acts like a single
    consolidated list.  This allows list entries insertion and deletion
    operations to happen in parallel instead of being serialized with a
    global list and lock.
    
    List entry insertion is strictly per cpu. List deletion, however, can
    happen in a cpu other than the one that did the insertion. So we still
    need lock to protect the list. Because of that, there may still be
    a small amount of contention when deletion is being done.
    
    A new header file include/linux/dlock-list.h will be added with the
    associated dlock_list_head and dlock_list_node structures. The following
    functions are provided to manage the per-cpu list:
    
     1. int init_dlock_list_head(struct dlock_list_head **pdlock_head)
     2. void dlock_list_add(struct dlock_list_node *node,
    		        struct dlock_list_head *head)
     3. void dlock_list_del(struct dlock_list *node)
    
    Iteration of all the list entries within a group of per-cpu
    lists is done by calling either the dlock_list_iterate() or
    dlock_list_iterate_safe() functions in a while loop. They correspond
    to the list_for_each_entry() and list_for_each_entry_safe() macros
    respectively. The iteration states are keep in a dlock_list_state
    structure that is passed to the iteration functions.
    
    Signed-off-by: Waiman Long <Waiman.Long@hpe.com>
    Reviewed-by: Jan Kara <jack@suse.cz>

---
+++ lib/dlock-list.c
@@ -0,0 +1,100 @@
+/*
+ * Distributed/locked list
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * (C) Copyright 2016 Hewlett-Packard Enterprise Development LP
+ *
+ * Authors: Waiman Long <waiman.long@hpe.com>
+ */
+#include <linux/dlock-list.h>
+#include <linux/lockdep.h>
+
+/*
+ * The dlock list lock needs its own class to avoid warning and stack
+ * trace when lockdep is enabled.
+ */
+static struct lock_class_key dlock_list_key;
+
+/*
+ * Initialize the per-cpu list head
+ */
+int init_dlock_list_head(struct dlock_list_head **pdlock_head)
+{
+	struct dlock_list_head *dlock_head;
+	int cpu;
+
+	dlock_head = alloc_percpu(struct dlock_list_head);
+	if (!dlock_head)
+		return -ENOMEM;
+
+	for_each_possible_cpu(cpu) {
+		struct dlock_list_head *head = per_cpu_ptr(dlock_head, cpu);
+
+		INIT_LIST_HEAD(&head->list);
+		head->lock = __SPIN_LOCK_UNLOCKED(&head->lock);
+		lockdep_set_class(&head->lock, &dlock_list_key);
+	}
+
+	*pdlock_head = dlock_head;
+	return 0;
+}
+
+/*
+ * List selection is based on the CPU being used when the dlock_list_add()
+ * function is called. However, deletion may be done by a different CPU.
+ * So we still need to use a lock to protect the content of the list.
+ */
+void dlock_list_add(struct dlock_list_node *node, struct dlock_list_head *head)
+{
+	struct dlock_list_head *myhead;
+
+	/*
+	 * Disable preemption to make sure that CPU won't gets changed.
+	 */
+	myhead = get_cpu_ptr(head);
+	spin_lock(&myhead->lock);
+	node->lockptr = &myhead->lock;
+	list_add(&node->list, &myhead->list);
+	spin_unlock(&myhead->lock);
+	put_cpu_ptr(head);
+}
+
+/*
+ * Delete a node from a dlock list
+ *
+ * We need to check the lock pointer again after taking the lock to guard
+ * against concurrent delete of the same node. If the lock pointer changes
+ * (becomes NULL or to a different one), we assume that the deletion was done
+ * elsewhere.
+ */
+void dlock_list_del(struct dlock_list_node *node)
+{
+	spinlock_t *lock = READ_ONCE(node->lockptr);
+
+	if (unlikely(!lock)) {
+		WARN(1, "dlock_list_del: node 0x%lx has no associated lock\n",
+			(unsigned long)node);
+		return;
+	}
+
+	spin_lock(lock);
+	if (likely(lock == node->lockptr)) {
+		list_del_init(&node->list);
+		node->lockptr = NULL;
+	} else {
+		/*
+		 * This path should never be executed.
+		 */
+		WARN_ON(1);
+	}
+	spin_unlock(lock);
+}
