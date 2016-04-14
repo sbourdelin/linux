a96da7871113040bda71e45f0c16baeb0bf3c9ba
Ming Lei <tom.leiming@gmail.com>
2016-04-14 20:02:29 +0800

    bcache: io.c: use bio_set_vec_table
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- drivers/md/bcache/io.c
+++ drivers/md/bcache/io.c
@@ -26,8 +26,7 @@ struct bio *bch_bbio_alloc(struct cache_set *c)
 
 	bio_init(bio);
 	bio->bi_flags		|= BIO_POOL_NONE << BIO_POOL_OFFSET;
-	bio->bi_max_vecs	 = bucket_pages(c);
-	bio->bi_io_vec		 = bio->bi_inline_vecs;
+	bio_set_vec_table(bio, bio->bi_inline_vecs, bucket_pages(c));
 
 	return bio;
 }
