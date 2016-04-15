93615e18d8383c1b2dd7a392e42257ce2919cdee
Yang Shi <yang.shi@linaro.org>
2016-04-15 11:10:05 -0700

    mm: thp: correct split_huge_pages file permission
    split_huge_pages doesn't support get method at all, so the read permission
    sounds confusing, change the permission to write only.
    
    And, add "\n" to the output of set method to make it more readable.
    
    Signed-off-by: Yang Shi <yang.shi@linaro.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -3454,7 +3454,7 @@ next:
 		}
 	}
 
-	pr_info("%lu of %lu THP split", split, total);
+	pr_info("%lu of %lu THP split\n", split, total);
 
 	return 0;
 }
@@ -3465,7 +3465,7 @@ static int __init split_huge_pages_debugfs(void)
 {
 	void *ret;
 
-	ret = debugfs_create_file("split_huge_pages", 0644, NULL, NULL,
+	ret = debugfs_create_file("split_huge_pages", 0200, NULL, NULL,
 			&split_huge_pages_fops);
 	if (!ret)
 		pr_warn("Failed to create split_huge_pages in debugfs");
