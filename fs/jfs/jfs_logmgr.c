7a943962de2bd9d2bb33612f782f40ee9117bbb4
Mike Christie <mchristi@redhat.com>
2016-04-15 14:15:38 -0500

    block, fs, mm, drivers: set bi_op to REQ_OP
    This patch converts the simple bi_rw use cases in the block,
    drivers, mm and fs code to set the bio->bi_op to a REQ_OP.
    
    These should be simple one liner cases, so I just did them
    in one patch. The next patches handle the more complicated
    cases in a module per patch.
    
    v5:
    1. Add missed crypto call.
    2. Change nfs bi_rw check to bi_op.
    
    Signed-off-by: Mike Christie <mchristi@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Hannes Reinecke <hare@suse.com>

--- fs/jfs/jfs_logmgr.c
+++ fs/jfs/jfs_logmgr.c
@@ -2002,6 +2002,7 @@ static int lbmRead(struct jfs_log * log, int pn, struct lbuf ** bpp)
 
 	bio->bi_end_io = lbmIODone;
 	bio->bi_private = bp;
+	bio->bi_op = REQ_OP_READ;
 	bio->bi_rw = READ_SYNC;
 	/*check if journaling to disk has been disabled*/
 	if (log->no_integrity) {
@@ -2146,6 +2147,7 @@ static void lbmStartIO(struct lbuf * bp)
 
 	bio->bi_end_io = lbmIODone;
 	bio->bi_private = bp;
+	bio->bi_op = REQ_OP_WRITE;
 	bio->bi_rw = WRITE_SYNC;
 
 	/* check if journaling to disk has been disabled */
