dd5cd42a3e9d937753cc00858f71124304af16da
Jia Hongtao <hongtao.jia@nxp.com>
2016-04-18 15:59:31 +0800

    cpufreq: qoriq: Fix cooling device registration issue during suspend
    Cooling device is registered by ready callback. It's also invoked while
    system resuming from sleep (Enabling non-boot cpus). Thus cooling device
    may be multiple registered. Stop_cpu callback is invoked during suspend
    (Disabling non-boot cpus). So matchable unregistration is added to fix
    this issue.
    
    Signed-off-by: Jia Hongtao <hongtao.jia@nxp.com>

--- drivers/cpufreq/qoriq-cpufreq.c
+++ drivers/cpufreq/qoriq-cpufreq.c
@@ -305,6 +305,7 @@ static int __exit qoriq_cpufreq_cpu_exit(struct cpufreq_policy *policy)
 {
 	struct cpu_data *data = policy->driver_data;
 
+	cpufreq_cooling_unregister(data->cdev);
 	kfree(data->pclk);
 	kfree(data->table);
 	kfree(data);
@@ -323,6 +324,12 @@ static int qoriq_cpufreq_target(struct cpufreq_policy *policy,
 	return clk_set_parent(policy->clk, parent);
 }
 
+static void qoriq_cpufreq_stop_cpu(struct cpufreq_policy *policy)
+{
+	struct cpu_data *cpud = policy->driver_data;
+
+	cpufreq_cooling_unregister(cpud->cdev);
+}
 
 static void qoriq_cpufreq_ready(struct cpufreq_policy *policy)
 {
@@ -352,6 +359,7 @@ static struct cpufreq_driver qoriq_cpufreq_driver = {
 	.verify		= cpufreq_generic_frequency_table_verify,
 	.target_index	= qoriq_cpufreq_target,
 	.get		= cpufreq_generic_get,
+	.stop_cpu	= qoriq_cpufreq_stop_cpu,
 	.ready		= qoriq_cpufreq_ready,
 	.attr		= cpufreq_generic_attr,
 };
