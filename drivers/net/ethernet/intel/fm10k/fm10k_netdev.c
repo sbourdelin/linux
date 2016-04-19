7b0bd1d5cad6953bccdfe9e9e7e357acefb1e815
Alexander Duyck <aduyck@mirantis.com>
2016-04-19 15:06:21 -0400

    fm10k: Add support for UDP tunnel segmentation with outer checksum offload
    This patch assumes that the fm10k hardware will ignore existing IPv4/v6
    header fields for length and checksum as well as the length and checksum
    fields for outer UDP headers.
    
    I have no means of testing this as I do not have any mlx5 hardware but
    thought I would submit it as an RFC to see if anyone out there wants to
    test this and see if this does in fact enable this functionality allowing
    us to to segment UDP tunneled frames that have an outer checksum.
    
    Signed-off-by: Alexander Duyck <aduyck@mirantis.com>

--- drivers/net/ethernet/intel/fm10k/fm10k_netdev.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_netdev.c
@@ -1443,15 +1443,21 @@ struct net_device *fm10k_alloc_netdev(const struct fm10k_info *info)
 
 	/* Only the PF can support VXLAN and NVGRE tunnel offloads */
 	if (info->mac == fm10k_mac_pf) {
-		dev->hw_enc_features = NETIF_F_IP_CSUM |
+		dev->hw_enc_features = NETIF_F_SG |
+				       NETIF_F_IP_CSUM |
+				       NETIF_F_IPV6_CSUM |
 				       NETIF_F_TSO |
 				       NETIF_F_TSO6 |
 				       NETIF_F_TSO_ECN |
 				       NETIF_F_GSO_UDP_TUNNEL |
-				       NETIF_F_IPV6_CSUM |
-				       NETIF_F_SG;
+				       NETIF_F_GSO_UDP_TUNNEL_CSUM |
+				       NETIF_F_GSO_PARTIAL;
 
-		dev->features |= NETIF_F_GSO_UDP_TUNNEL;
+		dev->features |= NETIF_F_GSO_UDP_TUNNEL |
+				 NETIF_F_GSO_UDP_TUNNEL_CSUM |
+				 NETIF_F_GSO_PARTIAL;
+
+		dev->gso_partial_features = NETIF_F_GSO_UDP_TUNNEL_CSUM;
 	}
 
 	/* all features defined to this point should be changeable */
