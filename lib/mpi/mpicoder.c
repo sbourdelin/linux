4a3576309d617961466936ebfc6f41e757cd0dd2
Sowmini Varadhan <sowmini.varadhan@oracle.com>
2016-04-21 07:07:36 -0400

    lib/mpi: Fix kernel unaligned acces in mpi_write_to_sgl
    Commit 2d4d1eea540b ("lib/mpi: Add mpi sgl helpers") added
    mpi_write_to_sgl() which generates traps due to unaligned
    access on some platforms like sparc. Fix this by using
    the get_unaligned* and put_unaligned* functions.
    
    Fixes: 2d4d1eea540b ("lib/mpi: Add mpi sgl helpers")
    Signed-off-by: Sowmini Varadhan <sowmini.varadhan@oracle.com>

--- lib/mpi/mpicoder.c
+++ lib/mpi/mpicoder.c
@@ -21,6 +21,7 @@
 #include <linux/bitops.h>
 #include <linux/count_zeros.h>
 #include "mpi-internal.h"
+#include <asm/unaligned.h>
 
 #define MAX_EXTERN_MPI_BITS 16384
 
@@ -405,10 +406,13 @@ int mpi_write_to_sgl(MPI a, struct scatterlist *sgl, unsigned *nbytes,
 				p -= sizeof(alimb);
 				continue;
 			} else {
-				mpi_limb_t *limb1 = (void *)p - sizeof(alimb);
-				mpi_limb_t *limb2 = (void *)p - sizeof(alimb)
-							+ lzeros;
-				*limb1 = *limb2;
+				mpi_limb_t tmp;
+
+				tmp = get_unaligned_be32((void *)p -
+							 sizeof(alimb) +
+							 lzeros);
+				put_unaligned_be32(tmp,
+						   (void *)p - sizeof(alimb));
 				p -= lzeros;
 				y = lzeros;
 			}
