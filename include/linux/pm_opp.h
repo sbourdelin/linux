924e0d5a02dc5ebc10e6647d6d88d282c7b6bcef
Viresh Kumar <viresh.kumar@linaro.org>
2016-04-22 08:46:51 +0530

    PM / OPP: -ENOSYS is applicable only to syscalls
    Some of the routines have use -ENOSYS, which is supposed to be used only
    for syscalls. Replace that with -EINVAL.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -199,7 +199,7 @@ static inline void dev_pm_opp_of_remove_table(struct device *dev)
 
 static inline int dev_pm_opp_of_cpumask_add_table(cpumask_var_t cpumask)
 {
-	return -ENOSYS;
+	return -EINVAL;
 }
 
 static inline void dev_pm_opp_of_cpumask_remove_table(cpumask_var_t cpumask)
@@ -208,12 +208,12 @@ static inline void dev_pm_opp_of_cpumask_remove_table(cpumask_var_t cpumask)
 
 static inline int dev_pm_opp_of_get_sharing_cpus(struct device *cpu_dev, cpumask_var_t cpumask)
 {
-	return -ENOSYS;
+	return -EINVAL;
 }
 
 static inline int dev_pm_opp_set_sharing_cpus(struct device *cpu_dev, cpumask_var_t cpumask)
 {
-	return -ENOSYS;
+	return -EINVAL;
 }
 #endif
 
