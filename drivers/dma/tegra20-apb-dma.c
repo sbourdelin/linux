c5d7d37edca74078f733fad2abe73920cc528b32
Shardar Shariff Md <smohammed@nvidia.com>
2016-04-22 19:44:39 +0530

    dmaengine: tegra-apb: proper default init of channel slave_id
    Initialize default channel slave_id(req_sel) to invalid id
    (i.e max supported slave id + 1) to avoid overwriting of slave_id
    during tegra_dma_slave_config() with client data if slave_id
    is not initialized through DT
    
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>

--- drivers/dma/tegra20-apb-dma.c
+++ drivers/dma/tegra20-apb-dma.c
@@ -54,6 +54,7 @@
 #define TEGRA_APBDMA_CSR_ONCE			BIT(27)
 #define TEGRA_APBDMA_CSR_FLOW			BIT(21)
 #define TEGRA_APBDMA_CSR_REQ_SEL_SHIFT		16
+#define TEGRA_APBDMA_CSR_REQ_SEL_MASK		0x1F
 #define TEGRA_APBDMA_CSR_WCOUNT_MASK		0xFFFC
 
 /* STATUS register */
@@ -113,6 +114,9 @@
 
 /* Channel base address offset from APBDMA base address */
 #define TEGRA_APBDMA_CHANNEL_BASE_ADD_OFFSET	0x1000
+
+#define TEGRA_APBDMA_SLAVE_ID_MAX		TEGRA_APBDMA_CSR_REQ_SEL_MASK
+#define TEGRA_APBDMA_SLAVE_ID_INVALID		(TEGRA_APBDMA_SLAVE_ID_MAX + 1)
 
 struct tegra_dma;
 
@@ -353,7 +357,7 @@ static int tegra_dma_slave_config(struct dma_chan *dc,
 	}
 
 	memcpy(&tdc->dma_sconfig, sconfig, sizeof(*sconfig));
-	if (!tdc->slave_id)
+	if (tdc->slave_id == TEGRA_APBDMA_SLAVE_ID_INVALID)
 		tdc->slave_id = sconfig->slave_id;
 	tdc->config_init = true;
 	return 0;
@@ -1236,7 +1240,7 @@ static void tegra_dma_free_chan_resources(struct dma_chan *dc)
 	}
 	pm_runtime_put(tdma->dev);
 
-	tdc->slave_id = 0;
+	tdc->slave_id = TEGRA_APBDMA_SLAVE_ID_INVALID;
 }
 
 static struct dma_chan *tegra_dma_of_xlate(struct of_phandle_args *dma_spec,
@@ -1245,13 +1249,20 @@ static struct dma_chan *tegra_dma_of_xlate(struct of_phandle_args *dma_spec,
 	struct tegra_dma *tdma = ofdma->of_dma_data;
 	struct dma_chan *chan;
 	struct tegra_dma_channel *tdc;
+	unsigned int slave_id;
+
+	slave_id = dma_spec->args[0];
+	if (slave_id > TEGRA_APBDMA_SLAVE_ID_MAX) {
+		dev_err(tdma->dev, "Invalid slave id: %d\n", slave_id);
+		return NULL;
+	}
 
 	chan = dma_get_any_slave_channel(&tdma->dma_dev);
 	if (!chan)
 		return NULL;
 
 	tdc = to_tegra_dma_chan(chan);
-	tdc->slave_id = dma_spec->args[0];
+	tdc->slave_id = slave_id;
 
 	return chan;
 }
@@ -1389,6 +1400,7 @@ static int tegra_dma_probe(struct platform_device *pdev)
 				&tdma->dma_dev.channels);
 		tdc->tdma = tdma;
 		tdc->id = i;
+		tdc->slave_id = TEGRA_APBDMA_SLAVE_ID_INVALID;
 
 		tasklet_init(&tdc->tasklet, tegra_dma_tasklet,
 				(unsigned long)tdc);
