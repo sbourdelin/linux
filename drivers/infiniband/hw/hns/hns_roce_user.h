8d8b52c71099d8d67d356ec2267f1acdbefb1b89
Lijun Ou <oulijun@huawei.com>
2016-04-23 18:26:52 +0800

    IB/hns: Add operations support for IB device and port
    This patch mainly registered some relative verbs for the kernel.
    These operation funtions will be called by user. For example:
        1. modify device
        2. query device
        3. query_port
        4. modify_port
    and so on.
    
    Signed-off-by: Lijun Ou <oulijun@huawei.com>
    Signed-off-by: Wei Hu(Xavier) <xavier.huwei@huawei.com>

---
+++ drivers/infiniband/hw/hns/hns_roce_user.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Hisilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef _HNS_ROCE_USER_H
+#define _HNS_ROCE_USER_H
+
+struct hns_roce_ib_alloc_ucontext_resp {
+	__u32	qp_tab_size;
+};
+
+#endif /*_HNS_ROCE_USER_H */
