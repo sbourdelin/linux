14876cc4bbe07fa37f66893d52ae152efff854b7
Julius Werner <jwerner@chromium.org>
2016-04-25 18:06:46 -0700

    block: partitions: efi: Always check for alternative GPT at end of drive
    The GUID Partiton Table layout maintains two synonymous partition tables
    on a block device, one starting in sector 1 and one in the very last
    sectors of the block device. This is useful if one of the tables gets
    accidentally corrupted (e.g. through a partial write because of an
    unexpected power loss).
    
    Linux normally only boots if the primary GPT is valid. It will not even
    try to find the alternative GPT to an invalid primary one unless the
    "gpt" command line option forces more aggressive detection. This doesn't
    really make any sense... if the "gpt" option is not set, the code
    validates the protective or hybrid MBR in sector 0 anyway before it even
    starts looking for the actual GPTs. If we get to the point where a valid
    proctective or hybrid MBR was found but the primary GPT was not found
    (valid), checking the alternative GPT is our best bet: we know that this
    block device is meant to use GPT (because any other partitioning system
    would've presumably overwritten sector 0), and we know that if the
    alternative GPT is valid it should contain more accurate information
    than parsing the protective/hybrid MBR with msdos_partition() would
    yield (which would otherwise be what happens next).
    
    Signed-off-by: Julius Werner <jwerner@chromium.org>

--- block/partitions/efi.c
+++ block/partitions/efi.c
@@ -625,7 +625,7 @@ static int find_valid_gpt(struct parsed_partitions *state, gpt_header **gpt,
 		good_agpt = is_gpt_valid(state,
 					 le64_to_cpu(pgpt->alternate_lba),
 					 &agpt, &aptes);
-        if (!good_agpt && force_gpt)
+        if (!good_agpt)
                 good_agpt = is_gpt_valid(state, lastlba, &agpt, &aptes);
 
         /* The obviously unsuccessful case */
