84da0753b279c6f7ea3bf7f613476d26dd202c34
Laxman Dewangan <ldewangan@nvidia.com>
2016-05-02 17:47:13 +0530

    pinctrl: tegra: Add DT binding for io pads control
    NVIDIA Tegra210 supports the IO pads which can operate at 1.8V
    or 3.3V I/O voltage levels. Also IO pads can be configured for
    power down state if it is not in used. SW needs to configure the
    voltage level of IO pads based on IO rail voltage and its power
    state based on platform usage.
    
    Add DT binding document for detailing the DT properties for
    configuring IO pads voltage levels and its power state.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>

---
+++ include/dt-bindings/pinctrl/pinctrl-tegra210-io-pad.h
@@ -0,0 +1,20 @@
+/*
+ * This header provides constants for Tegra210 IO pads pinctrl bindings.
+ *
+ * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Author: Laxman Dewangan <ldewangan@nvidia.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_PINCTRL_TEGRA210_IO_PAD_H
+#define _DT_BINDINGS_PINCTRL_TEGRA210_IO_PAD_H
+
+/* Power source voltage for Tegra IO Pad. */
+#define TEGRA_IO_PAD_POWER_SOURCE_1800000UV		0
+#define TEGRA_IO_PAD_POWER_SOURCE_3300000UV		1
+
+#endif
