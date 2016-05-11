cdfa8cdd1ed39b7a0ba1153339fe31bfb300f653
Zhaoxiu Zeng <zhaoxiu.zeng@gmail.com>
2016-05-11 17:24:43 +0800

    crypto: use parity_long is sahara.c
    Signed-off-by: Zhaoxiu Zeng <zhaoxiu.zeng@gmail.com>

--- drivers/crypto/sahara.c
+++ drivers/crypto/sahara.c
@@ -784,7 +784,7 @@ static u32 sahara_sha_init_hdr(struct sahara_dev *dev,
 	if (rctx->last)
 		hdr |= SAHARA_HDR_MDHA_PDATA;
 
-	if (hweight_long(hdr) % 2 == 0)
+	if (!parity_long(hdr))
 		hdr |= SAHARA_HDR_PARITY_BIT;
 
 	return hdr;
