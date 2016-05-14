d14cc988f87ad85b6cae79d0e35b8f9e59ce4575
Liu Bo <bo.li.liu@oracle.com>
2016-05-13 17:06:57 -0700

    Btrfs: replace BUG_ON with WARN_ONCE in cow_file_range
    This BUG_ON is more like a warning since an invalid
    btrfs_super_total_bytes() doesn't affect other stuff.
    
    Thus, use WARN_ONCE for warning purpose and don't leave a possible
    kernel panic here.
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -974,8 +974,11 @@ static noinline int cow_file_range(struct inode *inode,
 		}
 	}
 
-	BUG_ON(disk_num_bytes >
-	       btrfs_super_total_bytes(root->fs_info->super_copy));
+	WARN_ONCE(disk_num_bytes >
+		  btrfs_super_total_bytes(root->fs_info->super_copy),
+	  KERN_WARNING "disk_num_bytes(%llu) > btrfs_super_total_bytes(%llu)\n",
+		  disk_num_bytes,
+		  btrfs_super_total_bytes(root->fs_info->super_copy));
 
 	alloc_hint = get_extent_allocation_hint(inode, start, num_bytes);
 	btrfs_drop_extent_cache(inode, start, start + num_bytes - 1, 0);
