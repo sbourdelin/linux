e9a10811c6c5ddce2d21c404e2e328c1b2af80c2
Hans de Goede <hdegoede@redhat.com>
2016-05-14 19:55:45 +0200

    regulator: axp20x: Add support for the (external) drivebus regulator
    The axp20x pmics have 2 power inputs, one called ACIN which is intended
    for to be supplied via a powerbarrel on the board and one called VBUS
    which is intended to be supplied via an otg connector.
    
    In the VBUS case the pmic needs to know if the board is supplying power
    to the otg connector, because then it should not take any power from
    its VBUS pin. The axp209 pmic has a N_VBUSEN input pin via which the
    board can signal to the pmic whether the board is supplying power to the
    otg connector or not.
    
    On the axp221/axp223 this pin can alternatively be used as an output
    which controls an external regulator which (optionally) supplies
    power to the otg connector from the board. When the pin is used as
    output it is called DRIVEBUS in the datasheet.
    
    This commit adds support for the DRIVEBUS pin as an extra pmic
    controlled regulator. Since this is optional a new x-powers,drivebus dt
    property is added. When this is present the misc-control register is
    written to change the N_VBUSEN input pin to DRIVEBUS output pin mode and
    the extra drivebus regulator is registered with the regulator subsystem.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/regulator/axp20x-regulator.c
+++ drivers/regulator/axp20x-regulator.c
@@ -36,6 +36,8 @@
 
 #define AXP20X_FREQ_DCDC_MASK		0x0f
 
+#define AXP22X_MISC_N_VBUSEN_FUNC	BIT(4)
+
 #define AXP_DESC_IO(_family, _id, _match, _supply, _min, _max, _step, _vreg,	\
 		    _vmask, _ereg, _emask, _enable_val, _disable_val)		\
 	[_family##_##_id] = {							\
@@ -230,6 +232,18 @@ static const struct regulator_desc axp22x_regulators[] = {
 	AXP_DESC_FIXED(AXP22X, RTC_LDO, "rtc_ldo", "ips", 3000),
 };
 
+static const struct regulator_desc axp22x_drivebus_regulator = {
+	.name		= "drivebus",
+	.supply_name	= "ips",
+	.of_match	= of_match_ptr("drivebus"),
+	.regulators_node = of_match_ptr("regulators"),
+	.type		= REGULATOR_VOLTAGE,
+	.owner		= THIS_MODULE,
+	.enable_reg	= AXP20X_VBUS_IPSOUT_MGMT,
+	.enable_mask	= BIT(2),
+	.ops		= &axp20x_ops_sw,
+};
+
 static int axp20x_set_dcdc_freq(struct platform_device *pdev, u32 dcdcfreq)
 {
 	struct axp20x_dev *axp20x = dev_get_drvdata(pdev->dev.parent);
@@ -354,6 +368,7 @@ static int axp20x_regulator_probe(struct platform_device *pdev)
 	u32 workmode;
 	const char *axp22x_dc1_name = axp22x_regulators[AXP22X_DCDC1].name;
 	const char *axp22x_dc5_name = axp22x_regulators[AXP22X_DCDC5].name;
+	bool drivebus = false;
 
 	switch (axp20x->variant) {
 	case AXP202_ID:
@@ -365,6 +380,8 @@ static int axp20x_regulator_probe(struct platform_device *pdev)
 	case AXP223_ID:
 		regulators = axp22x_regulators;
 		nregulators = AXP22X_REG_ID_MAX;
+		drivebus = of_property_read_bool(pdev->dev.parent->of_node,
+						 "x-powers,drivebus");
 		break;
 	default:
 		dev_err(&pdev->dev, "Unsupported AXP variant: %ld\n",
@@ -436,6 +453,19 @@ static int axp20x_regulator_probe(struct platform_device *pdev)
 				of_property_read_string(rdev->dev.of_node,
 							"regulator-name",
 							&axp22x_dc5_name);
+		}
+	}
+
+	if (drivebus) {
+		/* Change N_VBUSEN sense pin to DRIVEBUS output pin */
+		regmap_update_bits(axp20x->regmap, AXP20X_OVER_TMP,
+				   AXP22X_MISC_N_VBUSEN_FUNC, 0);
+		rdev = devm_regulator_register(&pdev->dev,
+					       &axp22x_drivebus_regulator,
+					       &config);
+		if (IS_ERR(rdev)) {
+			dev_err(&pdev->dev, "Failed to register drivebus\n");
+			return PTR_ERR(rdev);
 		}
 	}
 
