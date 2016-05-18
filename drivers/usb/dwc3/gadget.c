a17e19e05c2bff1af2dd79ce6125b57597b45c26
Felipe Balbi <felipe.balbi@linux.intel.com>
2016-05-18 11:32:37 +0300

    usb: dwc3: gadget: switch over to spin_lock()
    we *know* that our dwc3 interrupt is masked because
    we masked it in the hardirq handler. This means we
    don't need to disable all of current cpu's
    interrupts.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2762,12 +2762,11 @@ static irqreturn_t dwc3_thread_interrupt(int irq, void *_evt)
 {
 	struct dwc3_event_buffer *evt = _evt;
 	struct dwc3 *dwc = evt->dwc;
-	unsigned long flags;
 	irqreturn_t ret = IRQ_NONE;
 
-	spin_lock_irqsave(&dwc->lock, flags);
+	spin_lock(&dwc->lock);
 	ret = dwc3_process_event_buf(evt);
-	spin_unlock_irqrestore(&dwc->lock, flags);
+	spin_unlock(&dwc->lock);
 
 	return ret;
 }
