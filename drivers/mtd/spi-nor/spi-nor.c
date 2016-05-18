0bbc9875dcc2b6fe2d2158de1092e385aa3b08d6
Matthias Schiffer <mschiffer@universe-factory.net>
2016-05-18 15:32:33 +0200

    mtd: spi-nor: disable software protection for Macronix flash at startup
    This patch has been tested in OpenWrt for a few months and seems to work
    correctly.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1304,6 +1304,7 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 
 	if (JEDEC_MFR(info) == SNOR_MFR_ATMEL ||
 	    JEDEC_MFR(info) == SNOR_MFR_INTEL ||
+	    JEDEC_MFR(info) == SNOR_MFR_MACRONIX ||
 	    JEDEC_MFR(info) == SNOR_MFR_SST ||
 	    info->flags & SPI_NOR_HAS_LOCK) {
 		write_enable(nor);
