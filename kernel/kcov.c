f868fc7e97aae3f7f7fae4ee2b7aeefb22e932e3
Nicolai Stange <nicstange@gmail.com>
2016-05-23 15:45:47 +0200

    kernel/kcov: unproxify debugfs file's fops
    Since commit 49d200deaa68 ("debugfs: prevent access to removed files'
    private data"), a debugfs file's file_operations methods get proxied
    through lifetime aware wrappers.
    
    However, only a certain subset of the file_operations members is supported
    by debugfs and ->mmap isn't among them -- it appears to be NULL from the
    VFS layer's perspective.
    
    This behaviour breaks the /sys/kernel/debug/kcov file introduced
    concurrently with commit 5c9a8750a640 ("kernel: add kcov code coverage").
    
    Since that file never gets removed, there is no file removal race and thus,
    a lifetime checking proxy isn't needed.
    
    Avoid the proxying for /sys/kernel/debug/kcov by creating it via
    debugfs_create_file_unsafe() rather than debugfs_create_file().
    
    Fixes: 49d200deaa68 ("debugfs: prevent access to removed files' private
                          data")
    Fixes: 5c9a8750a640 ("kernel: add kcov code coverage")
    Signed-off-by: Nicolai Stange <nicstange@gmail.com>

--- kernel/kcov.c
+++ kernel/kcov.c
@@ -264,7 +264,7 @@ static const struct file_operations kcov_fops = {
 
 static int __init kcov_init(void)
 {
-	if (!debugfs_create_file("kcov", 0600, NULL, NULL, &kcov_fops)) {
+	if (!debugfs_create_file_unsafe("kcov", 0600, NULL, NULL, &kcov_fops)) {
 		pr_err("failed to create kcov in debugfs\n");
 		return -ENOMEM;
 	}
