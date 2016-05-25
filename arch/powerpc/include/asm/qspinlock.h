b5872be86d4dc757eaafbc561cc49a2d12902624
Pan Xinhui <xinhui.pan@linux.vnet.ibm.com>
2016-05-25 16:18:07 +0800

    pv-qspinlock: powerpc support pv-qspinlock
    As we need let pv-qspinlock-kernel run on all environment which might
    have no powervm, we should runtime choose which qspinlock version to
    use.
    
    The default pv-qspinlock use native version. pv_lock initialization
    should be done in bootstage with irq disabled. And if there is PHYP,
    restore pv_lock_ops callbacks to pv version.
    
    Signed-off-by: Pan Xinhui <xinhui.pan@linux.vnet.ibm.com>

--- arch/powerpc/include/asm/qspinlock.h
+++ arch/powerpc/include/asm/qspinlock.h
@@ -12,10 +12,27 @@ static inline void native_queued_spin_unlock(struct qspinlock *lock)
 	smp_store_release((u8 *)lock, 0);
 }
 
+#ifdef CONFIG_PARAVIRT_SPINLOCKS
+
+#define __ARCH_NEED_PV_HASH_LOOKUP
+
+#include <asm/qspinlock_paravirt.h>
+
+static inline void queued_spin_lock_slowpath(struct qspinlock *lock, u32 val)
+{
+	pv_queued_spin_lock(lock, val);
+}
+
+static inline void queued_spin_unlock(struct qspinlock *lock)
+{
+	pv_queued_spin_unlock(lock);
+}
+#else
 static inline void queued_spin_unlock(struct qspinlock *lock)
 {
 	native_queued_spin_unlock(lock);
 }
+#endif
 
 #include <asm-generic/qspinlock.h>
 
