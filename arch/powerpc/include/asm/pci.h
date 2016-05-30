87843d4e44ff852c596ce9034999b7eb50e4bef5
Yongji Xie <xyjxie@linux.vnet.ibm.com>
2016-05-30 21:00:18 +0800

    PCI: Add support for enforcing all MMIO BARs to be page aligned
    When vfio passthrough a PCI device of which MMIO BARs are
    smaller than PAGE_SIZE, guest will not handle the mmio
    accesses to the BARs which leads to mmio emulations in host.
    
    This is because vfio will not allow to passthrough one BAR's
    mmio page which may be shared with other BARs. Otherwise,
    there will be a backdoor that guest can use to access BARs
    of other guest.
    
    To solve this issue, this patch modifies resource_alignment
    to support syntax where multiple devices get the same
    alignment. So we can use something like
    "pci=resource_alignment=*:*:*.*:noresize" to enforce the
    alignment of all MMIO BARs to be at least PAGE_SIZE so that
    one BAR's mmio page would not be shared with other BARs.
    
    And we also define a macro PCIBIOS_MIN_ALIGNMENT to enable this
    automatically on PPC64 platform which can easily hit this issue
    because its PAGE_SIZE is 64KB.
    
    Note that this would not be applied to VFs whose BARs are always
    page aligned and should be never reassigned according to SRIOV
    spec.
    
    Signed-off-by: Yongji Xie <xyjxie@linux.vnet.ibm.com>

--- arch/powerpc/include/asm/pci.h
+++ arch/powerpc/include/asm/pci.h
@@ -28,6 +28,8 @@
 #define PCIBIOS_MIN_IO		0x1000
 #define PCIBIOS_MIN_MEM		0x10000000
 
+#define PCIBIOS_MIN_ALIGNMENT  PAGE_SIZE
+
 struct pci_dev;
 
 /* Values for the `which' argument to sys_pciconfig_iobase syscall.  */
