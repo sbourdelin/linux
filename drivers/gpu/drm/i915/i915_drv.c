49fbc8d11d2312773bb2b98ee3f624646d9e454c
Chris Wilson <chris@chris-wilson.co.uk>
2016-05-31 11:49:22 +0530

    drm/i915: Migrate stolen objects before hibernation
    Ville reminded us that stolen memory is not preserved across
    hibernation, and a result of this was that context objects now being
    allocated from stolen were being corrupted on S4 and promptly hanging
    the GPU on resume.
    
    We want to utilise stolen for as much as possible (nothing else will use
    that wasted memory otherwise), so we need a strategy for handling
    general objects allocated from stolen and hibernation. A simple solution
    is to do a CPU copy through the GTT of the stolen object into a fresh
    shmemfs backing store and thenceforth treat it as a normal objects. This
    can be refined in future to either use a GPU copy to avoid the slow
    uncached reads (though it's hibernation!) and recreate stolen objects
    upon resume/first-use. For now, a simple approach should suffice for
    testing the object migration.
    
    v2:
    Swap PTE for pinned bindings over to the shmemfs. This adds a
    complicated dance, but is required as many stolen objects are likely to
    be pinned for use by the hardware. Swapping the PTEs should not result
    in externally visible behaviour, as each PTE update should be atomic and
    the two pages identical. (danvet)
    
    safe-by-default, or the principle of least surprise. We need a new flag
    to mark objects that we can wilfully discard and recreate across
    hibernation. (danvet)
    
    Just use the global_list rather than invent a new stolen_list. This is
    the slowpath hibernate and so adding a new list and the associated
    complexity isn't worth it.
    
    v3: Rebased on drm-intel-nightly (Ankit)
    
    v4: Use insert_page to map stolen memory backed pages for migration to
    shmem (Chris)
    
    v5: Acquire mutex lock while copying stolen buffer objects to shmem (Chris)
    
    v6: Handled file leak, Splitted object migration function, added kerneldoc
    for migrate_stolen_to_shmemfs() function (Tvrtko)
    Use i915 wrapper function for drm_mm_insert_node_in_range()
    
    v7: Keep the object in cpu domain after get_pages, remove the object from
    the unbound list only when marked PURGED, Corrected split of object migration
    function (Chris)
    
    v8: Split i915_gem_freeze(), removed redundant use of barrier, corrected
    use of set_to_cpu_domain() (Chris)
    
    v9: Replaced WARN_ON by BUG_ON and added a comment explaining it
    (Daniel/Tvrtko)
    
    v10: Document use of barriers (Chris)
    
    v11: Resolved list corruption due to not removing obj from global_list
    if no reference to pages is held, Rebased (Ankit)
    
    v12: Rebase (Ankit)
    
    Signed-off-by: Chris Wilson <chris at chris-wilson.co.uk>
    Signed-off-by: Ankitprasad Sharma <ankitprasad.r.sharma@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1066,6 +1066,22 @@ static int i915_pm_suspend(struct device *dev)
 	return i915_drm_suspend(drm_dev);
 }
 
+/* freeze: before creating the hibernation_image */
+static int i915_pm_freeze(struct device *dev)
+{
+	int ret;
+
+	ret = i915_gem_freeze(pci_get_drvdata(to_pci_dev(dev)));
+	if (ret)
+		return ret;
+
+	ret = i915_pm_suspend(dev);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
 static int i915_pm_suspend_late(struct device *dev)
 {
 	struct drm_device *drm_dev = dev_to_i915(dev)->dev;
@@ -1113,12 +1129,6 @@ static int i915_pm_resume(struct device *dev)
 		return 0;
 
 	return i915_drm_resume(drm_dev);
-}
-
-/* freeze: before creating the hibernation_image */
-static int i915_pm_freeze(struct device *dev)
-{
-	return i915_pm_suspend(dev);
 }
 
 static int i915_pm_freeze_late(struct device *dev)
--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2180,6 +2180,12 @@ struct drm_i915_gem_object {
 	 * Advice: are the backing pages purgeable?
 	 */
 	unsigned int madv:2;
+	/**
+	 * Whereas madv is for userspace, there are certain situations
+	 * where we want I915_MADV_DONTNEED behaviour on internal objects
+	 * without conflating the userspace setting.
+	 */
+	unsigned int internal_volatile:1;
 
 	/**
 	 * Current tiling mode for the object.
@@ -3317,6 +3323,9 @@ int __must_check i915_gem_init_hw(struct drm_device *dev);
 void i915_gem_init_swizzling(struct drm_device *dev);
 void i915_gem_cleanup_engines(struct drm_device *dev);
 int __must_check i915_gpu_idle(struct drm_device *dev);
+int __must_check i915_gem_freeze(struct drm_device *dev);
+int __must_check
+i915_gem_object_migrate_stolen_to_shmemfs(struct drm_i915_gem_object *obj);
 int __must_check i915_gem_suspend(struct drm_device *dev);
 void __i915_add_request(struct drm_i915_gem_request *req,
 			struct drm_i915_gem_object *batch_obj,
@@ -3542,6 +3551,7 @@ i915_gem_object_create_stolen_for_preallocated(struct drm_device *dev,
 					       u32 stolen_offset,
 					       u32 gtt_offset,
 					       u32 size);
+int __must_check i915_gem_stolen_freeze(struct drm_i915_private *i915);
 
 /* i915_gem_shrinker.c */
 unsigned long i915_gem_shrink(struct drm_i915_private *dev_priv,
