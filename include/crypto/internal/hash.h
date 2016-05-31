69a0fedcd5a3ab54b74ea4a5c24f38d4d50c1d5d
Megha Dey <megha.dey@linux.intel.com>
2016-05-31 14:42:21 -0700

    crypto : async implementation for sha1-mb
    Herbert wants the sha1-mb algorithm to have an async implementation:
    https://lkml.org/lkml/2016/4/5/286.
    Currently, sha1-mb uses an async interface for the outer algorithm
    and a sync interface for the inner algorithm. This patch introduces
    a async interface for even the inner algorithm.
    
    Signed-off-by: Megha Dey <megha.dey@linux.intel.com>
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>

--- include/crypto/internal/hash.h
+++ include/crypto/internal/hash.h
@@ -115,13 +115,13 @@ int shash_ahash_finup(struct ahash_request *req, struct shash_desc *desc);
 int shash_ahash_digest(struct ahash_request *req, struct shash_desc *desc);
 
 int shash_ahash_mcryptd_update(struct ahash_request *req,
-			       struct shash_desc *desc);
+			       struct ahash_request *desc);
 int shash_ahash_mcryptd_final(struct ahash_request *req,
-			      struct shash_desc *desc);
+			      struct ahash_request *desc);
 int shash_ahash_mcryptd_finup(struct ahash_request *req,
-			      struct shash_desc *desc);
+			      struct ahash_request *desc);
 int shash_ahash_mcryptd_digest(struct ahash_request *req,
-			       struct shash_desc *desc);
+			       struct ahash_request *desc);
 
 int crypto_init_shash_ops_async(struct crypto_tfm *tfm);
 
