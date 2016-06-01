8463312a416c31ade3a4f5abe27a07ec2c1cee5a
Jungseung Lee <js07.lee@samsung.com>
2016-06-01 17:54:41 +0900

    posix-clock: remove unneeded posix_clock_compat_ioctl()
    posix_clock_compat_ioctl() is identical to posix_clock_ioctl().
    We don't need additional compat_ioctl in this time.
    Remove posix_clock_compat_ioctl() and let posix_clock_ioctl() is in charge.
    
    Signed-off-by: Jungseung Lee <js07.lee@samsung.com>

--- kernel/time/posix-clock.c
+++ kernel/time/posix-clock.c
@@ -131,25 +131,6 @@ static long posix_clock_ioctl(struct file *fp,
 	return err;
 }
 
-#ifdef CONFIG_COMPAT
-static long posix_clock_compat_ioctl(struct file *fp,
-				     unsigned int cmd, unsigned long arg)
-{
-	struct posix_clock *clk = get_posix_clock(fp);
-	int err = -ENOTTY;
-
-	if (!clk)
-		return -ENODEV;
-
-	if (clk->ops.ioctl)
-		err = clk->ops.ioctl(clk, cmd, arg);
-
-	put_posix_clock(clk);
-
-	return err;
-}
-#endif
-
 static int posix_clock_open(struct inode *inode, struct file *fp)
 {
 	int err;
@@ -201,9 +182,6 @@ static const struct file_operations posix_clock_file_operations = {
 	.release	= posix_clock_release,
 	.fasync		= posix_clock_fasync,
 	.mmap		= posix_clock_mmap,
-#ifdef CONFIG_COMPAT
-	.compat_ioctl	= posix_clock_compat_ioctl,
-#endif
 };
 
 int posix_clock_register(struct posix_clock *clk, dev_t devid)
