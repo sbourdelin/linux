fabcf0fc680c94450c6e1ec07bcb15779d7c59ef
Binoy Jayan <binoy.jayan@linaro.org>
2016-06-01 10:52:06 +0530

    rtl8712: pwrctrl_priv: Replace semaphore lock with mutex
    The semaphore 'lock' in 'pwrctrl_priv' is used as a simple mutex,
    so it should be written as one. Semaphores are going away in the future.
    
    Signed-off-by: Binoy Jayan <binoy.jayan@linaro.org>

--- drivers/staging/rtl8712/rtl871x_pwrctrl.h
+++ drivers/staging/rtl8712/rtl871x_pwrctrl.h
@@ -93,7 +93,7 @@ static inline void _enter_pwrlock(struct semaphore *plock)
 }
 
 struct	pwrctrl_priv {
-	struct semaphore lock;
+	struct mutex mutex_lock;
 	/*volatile*/ u8 rpwm; /* requested power state for fw */
 	/* fw current power state. updated when 1. read from HCPWM or
 	 * 2. driver lowers power level */
