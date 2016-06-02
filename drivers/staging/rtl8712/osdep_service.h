eda415cb5e8ba71eaa608c2a9415172bac9ce2a6
Binoy Jayan <binoy.jayan@linaro.org>
2016-06-02 09:54:10 +0530

    rtl8712: pwrctrl_priv: Replace semaphore lock with mutex
    The semaphore 'lock' in 'pwrctrl_priv' is used as a simple mutex, so it
    should be written as one. Semaphores are going away in the future.
    _enter_pwrlock was using down_interruptible(), so the lock could be broken
    by sending a signal. This could be a bug, because nothing checks the return
    code here. Hence, using mutex_lock instead of the interruptible version.
    Removing the now unused _enter_pwrlock and _down_sema.
    
    Signed-off-by: Binoy Jayan <binoy.jayan@linaro.org>

--- drivers/staging/rtl8712/osdep_service.h
+++ drivers/staging/rtl8712/osdep_service.h
@@ -60,13 +60,6 @@ struct	__queue	{
 #define LIST_CONTAINOR(ptr, type, member) \
 	((type *)((char *)(ptr)-(SIZE_T)(&((type *)0)->member)))
 
-static inline u32 _down_sema(struct semaphore *sema)
-{
-	if (down_interruptible(sema))
-		return _FAIL;
-	return _SUCCESS;
-}
-
 static inline u32 end_of_queue_search(struct list_head *head,
 		struct list_head *plist)
 {
