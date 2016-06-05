95e7b6febe043e51afc93f8fb726b45d116058bb
Fabian Frederick <fabf@skynet.be>
2016-06-05 15:11:00 +0200

    ASoC: cs53l30: include gpio/consumer.h
    cs53l30 breaks kernel compilation when CONFIG_GPIOLIB is disabled.
    
    sound/soc/codecs/cs53l30.c:931:2: error: implicit declaration of
    function devm_gpiod_get_optional [-Werror=implicit-function-declaration]
      cs53l30->reset_gpio = devm_gpiod_get_optional(dev, "reset",
      ^
    sound/soc/codecs/cs53l30.c:932:13: error: GPIOD_OUT_LOW undeclared
    (first use in this function)
    
    Including gpio/consumer.h declares minimal functions in that case.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>

--- sound/soc/codecs/cs53l30.c
+++ sound/soc/codecs/cs53l30.c
@@ -15,6 +15,7 @@
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/i2c.h>
+#include <linux/gpio/consumer.h>
 #include <linux/module.h>
 #include <linux/of_gpio.h>
 #include <linux/regulator/consumer.h>
