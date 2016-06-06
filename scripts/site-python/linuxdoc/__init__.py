b2b1ef17fbd93758ed78c5051b06dce5878c077e
Heiser, Markus <markus.heiser@darmarIT.de>
2016-06-06 18:32:22 +0200

    linuxdoc: add python package linuxdoc
    The linuxdoc package includes python extensions related to the linux
    documentation processes.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarIT.de>

---
+++ scripts/site-python/linuxdoc/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8; mode: python -*-
+u"""
+    linuxdoc
+    ~~~~~~~~
+
+    The linuxdoc package includes python extensions related to the linux
+    documentation processes.
+"""
