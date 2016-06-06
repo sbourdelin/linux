12e29e68f7f2aba3d98e2a85fe9c7d3ae5dbaff9
Pali Rohár <pali.rohar@gmail.com>
2016-06-06 13:24:01 +0200

    Input: alps - cleanup ALPS_PROTO_V2 detection
    Table alps_model_data is now used only for ALPS_PROTO_V2, so add "v2"
    indentifier to all relevant functions/variables/structures. Sort all
    devices by signature and remove command_mode_resp which is not used
    anymore.
    
    Signed-off-by: Pali Rohár <pali.rohar@gmail.com>

--- drivers/input/mouse/alps.h
+++ drivers/input/mouse/alps.h
@@ -139,21 +139,16 @@ struct alps_protocol_info {
 };
 
 /**
- * struct alps_model_info - touchpad ID table
+ * struct alps_v2_model_info - touchpad ID table
  * @signature: E7 response string to match.
- * @command_mode_resp: For V3/V4 touchpads, the final byte of the EC response
- *   (aka command mode response) identifies the firmware minor version.  This
- *   can be used to distinguish different hardware models which are not
- *   uniquely identifiable through their E7 responses.
  * @protocol_info: information about protcol used by the device.
  *
  * Many (but not all) ALPS touchpads can be identified by looking at the
  * values returned in the "E7 report" and/or the "EC report."  This table
  * lists a number of such touchpads.
  */
-struct alps_model_info {
+struct alps_v2_model_info {
 	u8 signature[3];
-	u8 command_mode_resp;
 	struct alps_protocol_info protocol_info;
 };
 
