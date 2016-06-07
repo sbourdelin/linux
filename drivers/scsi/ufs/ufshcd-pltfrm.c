ad27e62af5a8672c119a7b518adcc5d435697182
Tiezhu Yang <kernelpatch@126.com>
2016-06-08 07:22:11 +0800

    scsi: ufs: fix potential memory leak
    If the function ufshcd_parse_clock_info returns an error, the memory clkfreq
    allocated by devm_kzalloc will be freed at that time. But when the function
    ufshcd_parse_clock_info returns 0 on success, there exists potential memory
    leak, this patch fixes it.
    
    Signed-off-by: Tiezhu Yang <kernelpatch@126.com>

--- drivers/scsi/ufs/ufshcd-pltfrm.c
+++ drivers/scsi/ufs/ufshcd-pltfrm.c
@@ -122,6 +122,10 @@ static int ufshcd_parse_clock_info(struct ufs_hba *hba)
 				clki->min_freq, clki->max_freq, clki->name);
 		list_add_tail(&clki->list, &hba->clk_list_head);
 	}
+
+	devm_kfree(dev, clkfreq);
+	clkfreq = NULL;
+
 out:
 	return ret;
 }
