e7f414d2b4de0d2b292103a783281f87b226c0f9
Masami Hiramatsu <mhiramat@kernel.org>
2016-06-08 18:32:41 +0900

    perf build: Add sdt feature detection
    Will be used to define SDT events in perf test code.

--- tools/build/feature/test-all.c
+++ tools/build/feature/test-all.c
@@ -137,6 +137,10 @@
 # include "test-libcrypto.c"
 #undef main
 
+#define main main_test_sdt
+# include "test-sdt.c"
+#undef main
+
 int main(int argc, char *argv[])
 {
 	main_test_libpython();
@@ -168,6 +172,7 @@ int main(int argc, char *argv[])
 	main_test_get_cpuid();
 	main_test_bpf();
 	main_test_libcrypto();
+	main_test_sdt();
 
 	return 0;
 }
