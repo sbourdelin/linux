493662214e7e102bc6c2b11399bbe4f69adc32b7
Adrian Hunter <adrian.hunter@intel.com>
2016-06-09 14:52:23 +0300

    mmc: queue: Fix queue thread wake-up
    The only time the driver sleeps expecting to be woken upon the arrival of
    a new request, is when the dispatch queue is empty. The only time that it
    is known whether the dispatch queue is empty is after NULL is returned
    from blk_fetch_request() while under the queue lock.
    
    Recognizing those facts, simplify the synchronization between the queue
    thread and the request function. A couple of flags tell the request
    function what to do, and the queue lock and barriers associated with
    wake-ups ensure synchronization.
    
    The result is simpler and allows the removal of the context_info lock.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2146,8 +2146,6 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 	int ret;
 	struct mmc_blk_data *md = mq->data;
 	struct mmc_card *card = md->queue.card;
-	struct mmc_host *host = card->host;
-	unsigned long flags;
 	unsigned int cmd_flags = req ? req->cmd_flags : 0;
 
 	if (req && !mq->mqrq_prev->req)
@@ -2178,11 +2176,6 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 			mmc_blk_issue_rw_rq(mq, NULL);
 		ret = mmc_blk_issue_flush(mq, req);
 	} else {
-		if (!req && host->areq) {
-			spin_lock_irqsave(&host->context_info.lock, flags);
-			host->context_info.is_waiting_last_req = true;
-			spin_unlock_irqrestore(&host->context_info.lock, flags);
-		}
 		ret = mmc_blk_issue_rw_rq(mq, req);
 	}
 
