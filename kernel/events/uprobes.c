263a13d15924072b26cdc23992af53fbb6a57257
Andrea Parri <parri.andrea@gmail.com>
2016-06-09 20:18:41 +0200

    events/uprobes: move smp_read_barrier_depends() where needed
    There is no need to use the barrier if there is no dereference/
    memory access; move it where needed (currently, affecting only
    Alpha). While touching this, also make the reads _ONCE().
    
    Signed-off-by: Andrea Parri <parri.andrea@gmail.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -1520,10 +1520,11 @@ static unsigned long get_trampoline_vaddr(void)
 	struct xol_area *area;
 	unsigned long trampoline_vaddr = -1;
 
-	area = current->mm->uprobes_state.xol_area;
-	smp_read_barrier_depends();
-	if (area)
-		trampoline_vaddr = area->vaddr;
+	area = READ_ONCE(current->mm->uprobes_state.xol_area);
+	if (area) {
+		smp_read_barrier_depends();
+		trampoline_vaddr = READ_ONCE(area->vaddr);
+	}
 
 	return trampoline_vaddr;
 }
