5d052c267ca31cef563e9e2c3700b57a1d1cabbd
Lianwei Wang <lianwei.wang@gmail.com>
2016-06-09 23:43:28 -0700

    cpu/hotplug: handle unbalanced hotplug enable/disable
    Currently it just print a warning message but did not
    reset cpu_hotplug_disabled when the enable/disable is
    unbalanced. The unbalanced enable/disable will lead
    the cpu hotplug work abnormally.
    
    Do nothing if an unablanced hotplug enable detected.
    
    Signed-off-by: Lianwei Wang <lianwei.wang@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -245,6 +245,14 @@ void cpu_hotplug_done(void)
 	cpuhp_lock_release();
 }
 
+static void __cpu_hotplug_enable(void)
+{
+	if (WARN_ONCE(!cpu_hotplug_disabled, "Unbalanced cpu hotplug enable\n"))
+		return;
+
+	cpu_hotplug_disabled--;
+}
+
 /*
  * Wait for currently running CPU hotplug operations to complete (if any) and
  * disable future CPU hotplug (from sysfs). The 'cpu_add_remove_lock' protects
@@ -263,7 +271,7 @@ EXPORT_SYMBOL_GPL(cpu_hotplug_disable);
 void cpu_hotplug_enable(void)
 {
 	cpu_maps_update_begin();
-	WARN_ON(--cpu_hotplug_disabled < 0);
+	__cpu_hotplug_enable();
 	cpu_maps_update_done();
 }
 EXPORT_SYMBOL_GPL(cpu_hotplug_enable);
@@ -1074,7 +1082,7 @@ void enable_nonboot_cpus(void)
 
 	/* Allow everyone to use the CPU hotplug again */
 	cpu_maps_update_begin();
-	WARN_ON(--cpu_hotplug_disabled < 0);
+	__cpu_hotplug_enable();
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
