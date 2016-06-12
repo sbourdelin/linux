325ce9dafac2b3f6c82276638be167603cf3fa17
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2016-06-12 21:25:04 +0100

    dma: remove bad_dma_ops to fix build fail
    m32r allmodconfig is failng with errors like:
    ERROR: "bad_dma_ops" [sound/soc/bcm/snd-soc-cygnus.ko] undefined!
    
    On checking the code it turns out that struct bad_dma_ops has been
    declared as extern but no one has actually defined struct bad_dma_ops.
    Lets remove that and return NULL from get_dma_ops() if HAS_DMA is not
    defined.
    
    Signed-off-by: Sudip Mukherjee <sudip.mukherjee@codethink.co.uk>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -113,10 +113,9 @@ int dma_mmap_from_coherent(struct device *dev, struct vm_area_struct *vma,
  * dma dependent code.  Code that depends on the dma-mapping
  * API needs to set 'depends on HAS_DMA' in its Kconfig
  */
-extern struct dma_map_ops bad_dma_ops;
 static inline struct dma_map_ops *get_dma_ops(struct device *dev)
 {
-	return &bad_dma_ops;
+	return NULL;
 }
 #endif
 
