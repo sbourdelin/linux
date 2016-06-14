5e234dc4672c8519ebaf1249fe77ba4d7011b907
Ben Hutchings <ben@decadent.org.uk>
2016-06-14 21:49:09 +0100

    liblockdep: Fix 'defined but not used' warning for init_utsname()
    We define init_utsname() as static but not inline, resulting
    in a warning for every source file that includes lockdep.h but
    doesn't call it.
    
    Since it is only used by lockdep.c, define it in there.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- tools/lib/lockdep/lockdep.c
+++ tools/lib/lockdep/lockdep.c
@@ -5,4 +5,14 @@
 #define hlist_add_head_rcu		hlist_add_head
 #define hlist_del_rcu			hlist_del
 
+static struct new_utsname *init_utsname(void)
+{
+	static struct new_utsname n = (struct new_utsname) {
+		.release = "liblockdep",
+		.version = LIBLOCKDEP_VERSION,
+	};
+
+	return &n;
+}
+
 #include "../../../kernel/locking/lockdep.c"
