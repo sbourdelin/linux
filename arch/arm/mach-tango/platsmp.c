2090868542735b346787634f7f625db37e20e868
Marc Gonzalez <marc_gonzalez@sigmadesigns.com>
2016-06-15 17:19:47 +0200

    ARM: tango: add HOTPLUG_CPU support
    cpu_die() and cpu_kill() are implemented in firmware.
    
    Signed-off-by: Marc Gonzalez <marc_gonzalez@sigmadesigns.com>

--- arch/arm/mach-tango/platsmp.c
+++ arch/arm/mach-tango/platsmp.c
@@ -9,8 +9,25 @@ static int tango_boot_secondary(unsigned int cpu, struct task_struct *idle)
 	return 0;
 }
 
+#ifdef CONFIG_HOTPLUG_CPU
+static int tango_cpu_kill(unsigned int cpu)
+{
+	return tango_aux_core_kill(cpu);
+}
+
+static void tango_cpu_die(unsigned int cpu)
+{
+	tango_aux_core_die(cpu);
+}
+#else
+#define tango_cpu_kill	NULL
+#define tango_cpu_die	NULL
+#endif
+
 static const struct smp_operations tango_smp_ops __initconst = {
 	.smp_boot_secondary	= tango_boot_secondary,
+	.cpu_kill		= tango_cpu_kill,
+	.cpu_die		= tango_cpu_die,
 };
 
 CPU_METHOD_OF_DECLARE(tango4_smp, "sigma,tango4-smp", &tango_smp_ops);
