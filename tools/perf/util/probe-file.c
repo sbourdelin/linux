a3b98dfa337b921c84971ae3448dcaf1603ba7dd
Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
2016-06-15 12:30:12 +0900

    perf-list: Skip SDTs placed in invalid binaries
    Skip SDTs placed in invalid (non-exist, or older version)
    binaries. Note that perf-probe --cache --list and
    perf-probe --cache --del still handle all the caches
    including invalid binaries.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>

--- tools/perf/util/probe-file.c
+++ tools/perf/util/probe-file.c
@@ -806,7 +806,7 @@ int probe_cache__show_all_caches(struct strfilter *filter)
 	pr_debug("list cache with filter: %s\n", buf);
 	free(buf);
 
-	bidlist = build_id_cache__list_all();
+	bidlist = build_id_cache__list_all(false);
 	if (!bidlist) {
 		pr_debug("Failed to get buildids: %d\n", errno);
 		return -EINVAL;
