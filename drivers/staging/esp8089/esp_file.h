0d65c22c65a50166076aabea4d9f121790a7dd32
Icenowy Zheng <icenowy@outlook.com>
2016-06-19 18:17:55 +0800

    staging: esp8089: add new driver
    This commit adds Espressif ESP8089 driver, the SDIO version.
    
    ESP8089 is a 802.11b/g/n card with SDIO/SPI interface. It is usually used in
    low-price tablets with Allwiner or RockChip SoCs.
    
    Signed-off-by: Icenowy Zheng <icenowy@outlook.com>

---
+++ drivers/staging/esp8089/esp_file.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2010 -2014 Espressif System.
+ *
+ *   file operation in kernel space
+ *
+ */
+
+#ifndef _ESP_FILE_H_
+#define _ESP_FILE_H_
+
+#include <linux/version.h>
+#include <linux/firmware.h>
+
+#define E_ROUND_UP(x, y)  ((((x) + ((y) - 1)) / (y)) * (y))
+
+#define CONF_ATTR_LEN 24
+#define CONF_VAL_LEN 3
+#define MAX_ATTR_NUM 24
+#define MAX_FIX_ATTR_NUM 16
+#define MAX_BUF_LEN ((CONF_ATTR_LEN + CONF_VAL_LEN + 2) * MAX_ATTR_NUM + 2)
+
+struct esp_init_table_elem {
+	char attr[CONF_ATTR_LEN];
+	int offset;
+	short value;
+};
+
+int request_init_conf(void);
+void fix_init_data(u8 * init_data_buf, int buf_size);
+
+#endif				/* _ESP_FILE_H_ */
