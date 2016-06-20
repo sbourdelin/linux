f9b158a706af215d4ba7dd6da382ef17e0731dba
Deepa Dinamani <deepa.kernel@gmail.com>
2016-06-19 17:27:13 -0700

    drivers: staging: lustre: Replace CURRENT_TIME with current_time()
    CURRENT_TIME macro is not appropriate for filesystems as it
    doesn't use the right granularity for filesystem timestamps.
    Use current_time() instead.
    
    This is also in preparation for the patch that transitions
    vfs timestamps to use 64 bit time and hence make them
    y2038 safe. As part of the effort current_time() will be
    extended to do range checks. Hence, it is necessary for all
    file system timestamps to use current_time().
    
    Also change format string for prints so that these are valid
    when vfs is transitioned to use 64 bit timestamps.
    
    Signed-off-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: lustre-devel@lists.lustre.org
    Acked-by: James Simmons <jsimmons@infradead.org>

--- drivers/staging/lustre/lustre/osc/osc_io.c
+++ drivers/staging/lustre/lustre/osc/osc_io.c
@@ -221,7 +221,7 @@ static void osc_page_touch_at(const struct lu_env *env,
 	       kms > loi->loi_kms ? "" : "not ", loi->loi_kms, kms,
 	       loi->loi_lvb.lvb_size);
 
-	attr->cat_mtime = attr->cat_ctime = LTIME_S(CURRENT_TIME);
+	attr->cat_mtime = attr->cat_ctime = ktime_get_real_seconds();
 	valid = CAT_MTIME | CAT_CTIME;
 	if (kms > loi->loi_kms) {
 		attr->cat_kms = kms;
