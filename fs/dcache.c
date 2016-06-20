171dcd6543cf49697d33deb9452e9f58586bd9b4
Oleg Drokin <green@linuxhacker.ru>
2016-06-20 11:22:46 -0400

    Remove stale d_splice_alias comment
    It's not really possible to call d_splice_alias with a hashed
    dentry no matter what, so remove the stale comment indicating
    otherwise. The only user of this functionality was fixed
    long ago according to Al.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2961,7 +2961,7 @@ out_err:
 /**
  * d_splice_alias - splice a disconnected dentry into the tree if one exists
  * @inode:  the inode which may have a disconnected dentry
- * @dentry: a negative dentry which we want to point to the inode.
+ * @dentry: a negative unhashed dentry which we want to point to the inode.
  *
  * If inode is a directory and has an IS_ROOT alias, then d_move that in
  * place of the given dentry and return it, else simply d_add the inode
@@ -2975,11 +2975,6 @@ out_err:
  *
  * If a dentry was found and moved, then it is returned.  Otherwise NULL
  * is returned.  This matches the expected return value of ->lookup.
- *
- * Cluster filesystems may call this function with a negative, hashed dentry.
- * In that case, we know that the inode will be a regular file, and also this
- * will only occur during atomic_open. So we need to check for the dentry
- * being already hashed only in the final case.
  */
 struct dentry *d_splice_alias(struct inode *inode, struct dentry *dentry)
 {
