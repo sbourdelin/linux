6c2995fd58a22a1117301ee2d0f9cc68bb08145f
Chris Wilson <chris@chris-wilson.co.uk>
2016-06-21 09:53:44 +0100

    drm/i915: Move connector registration to driver registration
    Defer connector registration from during construction to the driver
    registration phase. This is important for ordering the action correctly,
    e.g. not using debugfs before it is ready.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -5484,7 +5484,6 @@ intel_dp_init_connector(struct intel_digital_port *intel_dig_port,
 			  edp_panel_vdd_work);
 
 	intel_connector_attach_encoder(intel_connector, intel_encoder);
-	drm_connector_register(connector);
 
 	if (HAS_DDI(dev))
 		intel_connector->get_hw_state = intel_ddi_connector_get_hw_state;
@@ -5560,7 +5559,6 @@ fail:
 		edp_panel_vdd_off_sync(intel_dp);
 		pps_unlock(intel_dp);
 	}
-	drm_connector_unregister(connector);
 	drm_connector_cleanup(connector);
 
 	return false;
