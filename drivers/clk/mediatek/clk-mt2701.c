c3836428040f5d6f7887e7cc51aff944d425cec8
Shunli Wang <shunli.wang@mediatek.com>
2016-06-22 15:40:27 +0800

    reset: mediatek: Add MT2701 reset driver
    In infrasys and perifsys, there are many reset
    control bits for kinds of modules. These bits are
    used as actual reset controllers to be registered
    into kernel's generic reset controller framework.
    
    Signed-off-by: Shunli Wang <shunli.wang@mediatek.com>
    Signed-off-by: James Liao <jamesjj.liao@mediatek.com>
    Signed-off-by: Erin Lo <erin.lo@mediatek.com>
    Tested-by: John Crispin <blogic@openwrt.org>
    Acked-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/clk/mediatek/clk-mt2701.c
+++ drivers/clk/mediatek/clk-mt2701.c
@@ -791,6 +791,8 @@ static void mtk_infrasys_init(struct device_node *node)
 	if (r)
 		pr_err("%s(): could not register clock provider: %d\n",
 			__func__, r);
+
+	mtk_register_reset_controller(node, 2, 0x30);
 }
 
 static const struct mtk_gate_regs peri0_cg_regs = {
@@ -911,6 +913,8 @@ static void mtk_pericfg_init(struct device_node *node)
 	if (r)
 		pr_err("%s(): could not register clock provider: %d\n",
 			__func__, r);
+
+	mtk_register_reset_controller(node, 2, 0x0);
 }
 
 #define MT8590_PLL_FMAX		(2000 * MHZ)
