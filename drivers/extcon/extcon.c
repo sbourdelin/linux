022a5116e7abd816431215e2961fb3b126fe0b1c
Stephen Boyd <stephen.boyd@linaro.org>
2016-06-23 00:53:24 -0700

    extcon: Check for incorrect connection type in notifier register
    If we call extcon_register_notifier() with the wrong cable type,
    it blows up with an oops instead of returning an error code.
    Let's be nice and fail gracefully given that the consumer might
    not know if the cable is supported by the extcon provider.
    
    Signed-off-by: Stephen Boyd <stephen.boyd@linaro.org>

--- drivers/extcon/extcon.c
+++ drivers/extcon/extcon.c
@@ -586,6 +586,8 @@ int extcon_register_notifier(struct extcon_dev *edev, unsigned int id,
 		return -EINVAL;
 
 	idx = find_cable_index_by_id(edev, id);
+	if (idx < 0)
+		return idx;
 
 	spin_lock_irqsave(&edev->lock, flags);
 	ret = raw_notifier_chain_register(&edev->nh[idx], nb);
@@ -611,6 +613,8 @@ int extcon_unregister_notifier(struct extcon_dev *edev, unsigned int id,
 		return -EINVAL;
 
 	idx = find_cable_index_by_id(edev, id);
+	if (idx < 0)
+		return idx;
 
 	spin_lock_irqsave(&edev->lock, flags);
 	ret = raw_notifier_chain_unregister(&edev->nh[idx], nb);
