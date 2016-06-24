a18a4505b966fd43ec084f35502969856ec3c719
Ravi Bangoria <ravi.bangoria@linux.vnet.ibm.com>
2016-06-24 17:23:56 +0530

    perf annotate: Define macro for arch names
    Define macro for each arch name and use them instead of using arch
    name as string.
    
    Signed-off-by: Ravi Bangoria <ravi.bangoria@linux.vnet.ibm.com>

--- tools/perf/arch/common.h
+++ tools/perf/arch/common.h
@@ -5,6 +5,16 @@
 
 extern const char *objdump_path;
 
+#define PERF_ARCH_X86		"x86"
+#define PERF_ARCH_SPARC		"sparc"
+#define PERF_ARCH_ARM64		"arm64"
+#define PERF_ARCH_ARM		"arm"
+#define PERF_ARCH_S390		"s390"
+#define PERF_ARCH_PARISC	"parisc"
+#define PERF_ARCH_POWERPC	"powerpc"
+#define PERF_ARCH_MIPS		"mips"
+#define PERF_ARCH_SH		"sh"
+
 int perf_env__lookup_objdump(struct perf_env *env);
 const char *normalize_arch(char *arch);
 
