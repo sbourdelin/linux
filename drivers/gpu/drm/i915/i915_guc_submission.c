ba8c519afbb298e526efd11c2a5622f824d8df95
Akash Goel <akash.goel@intel.com>
2016-06-27 17:46:56 +0530

    drm/i915: New module param to control the size of buffer used for storing GuC firmware logs
    On recieving the log buffer flush interrupt from GuC firmware, Driver
    stores the snapshot of the log buffer in a local buffer, from which
    Userspace can pull the logs. By default Driver store, up to, 4 snapshots
    of the log buffer in a local buffer (managed by relay).
    Added a new module (read only) param, 'guc_log_size', through which User
    can specify the number of snapshots of log buffer to be stored in local
    buffer. This can be used to ensure capturing of all boot time logs even
    with high verbosity level.
    
    Signed-off-by: Akash Goel <akash.goel@intel.com>

--- drivers/gpu/drm/i915/i915_guc_submission.c
+++ drivers/gpu/drm/i915/i915_guc_submission.c
@@ -999,8 +999,7 @@ static void guc_create_log_relay_file(struct intel_guc *guc)
 
 	/* Keep the size of sub buffers same as shared log buffer */
 	subbuf_size = guc->log_obj->base.size;
-	/* TODO: Decide based on the User's input */
-	n_subbufs = 4;
+	n_subbufs = i915.guc_log_size;
 
 	guc_log_relay_chan = relay_open("guc_log", log_dir,
 			subbuf_size, n_subbufs, &relay_callbacks, dev);
