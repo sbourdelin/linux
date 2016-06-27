c7027aa190560b30c3de4037ecc082aa1432b462
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-06-27 19:39:54 +0300

    mm: oom: deduplicate victim selection code for memcg and global oom
    When selecting an oom victim, we use the same heuristic for both memory
    cgroup and global oom. The only difference is the scope of tasks to
    select the victim from. So we could just export an iterator over all
    memcg tasks and keep all oom related logic in oom_kill.c, but instead we
    duplicate pieces of it in memcontrol.c reusing some initially private
    functions of oom_kill.c in order to not duplicate all of it. That looks
    ugly and error prone, because any modification of select_bad_process
    should also be propagated to mem_cgroup_out_of_memory.
    
    Let's rework this as follows: keep all oom heuristic related code
    private to oom_kill.c and make oom_kill.c use exported memcg functions
    when it's really necessary (like in case of iterating over memcg tasks).
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -374,6 +374,8 @@ struct mem_cgroup *mem_cgroup_iter(struct mem_cgroup *,
 				   struct mem_cgroup *,
 				   struct mem_cgroup_reclaim_cookie *);
 void mem_cgroup_iter_break(struct mem_cgroup *, struct mem_cgroup *);
+int mem_cgroup_scan_tasks(struct mem_cgroup *,
+			  int (*)(struct task_struct *, void *), void *);
 
 static inline unsigned short mem_cgroup_id(struct mem_cgroup *memcg)
 {
@@ -453,6 +455,8 @@ unsigned long mem_cgroup_get_lru_size(struct lruvec *lruvec, enum lru_list lru)
 }
 
 void mem_cgroup_handle_over_high(void);
+
+unsigned long mem_cgroup_get_limit(struct mem_cgroup *memcg);
 
 void mem_cgroup_print_oom_info(struct mem_cgroup *memcg,
 				struct task_struct *p);
@@ -647,6 +651,12 @@ static inline void mem_cgroup_iter_break(struct mem_cgroup *root,
 {
 }
 
+static inline int mem_cgroup_scan_tasks(struct mem_cgroup *memcg,
+		int (*fn)(struct task_struct *, void *), void *arg)
+{
+	return 0;
+}
+
 static inline unsigned short mem_cgroup_id(struct mem_cgroup *memcg)
 {
 	return 0;
@@ -673,6 +683,11 @@ mem_cgroup_get_lru_size(struct lruvec *lruvec, enum lru_list lru)
 static inline unsigned long
 mem_cgroup_node_nr_lru_pages(struct mem_cgroup *memcg,
 			     int nid, unsigned int lru_mask)
+{
+	return 0;
+}
+
+static inline unsigned long mem_cgroup_get_limit(struct mem_cgroup *memcg)
 {
 	return 0;
 }
