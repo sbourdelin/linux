df4dc0ef4705a14dd56282829c279245c29d1fb3
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2016-06-29 17:00:15 -0700

    acpi: battery: Split battery driver for reuse by DPTF power participant
    DPTF power participant device have common objects with ACPI battery device
    "PNP0C0A". For example the main battery objects:
    _BST: returns dynamic battery information
    _BIX: The static battery properties and control method
    Also both can register with power_supply class to present properties
    of a typical battery device.
    
    To avoid code duplication, the existing battery driver is split into two
    parts:
    - A common part
    - ACPI battery driver
    
    battery_common
    Most of the existing battery driver source, except ACPI enumeration part
    moved a separate file. It exposes an interface, which can be used by ACPI
    battery driver and DPTF power participant driver.
    Also the current battery driver code had lots of check_patch errors.
    These check_patch errors are addressed in this battery_common. These are
    the main interface functions:
    acpi_battery_common_add
    acpi_battery_common_remove
    acpi_battery_common_resume
    acpi_battery_common_notify
    
    battery: This is ACPI battery driver, which just enumerate on PNP0C0A
    device and use interface functions offered by battery common part.
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- drivers/acpi/battery.h
+++ drivers/acpi/battery.h
@@ -1,3 +1,18 @@
+/*
+ * battery.h
+ * Copyright (c) 2016, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
 #ifndef __ACPI_BATTERY_H
 #define __ACPI_BATTERY_H
 
@@ -6,5 +21,20 @@
 #define ACPI_BATTERY_NOTIFY_STATUS	0x80
 #define ACPI_BATTERY_NOTIFY_INFO	0x81
 #define ACPI_BATTERY_NOTIFY_THRESHOLD   0x82
+
+extern int battery_bix_broken_package;
+extern int battery_notification_delay_ms;
+extern struct proc_dir_entry *acpi_battery_dir;
+
+int acpi_battery_common_add(struct acpi_device *device);
+int acpi_battery_common_remove(struct acpi_device *device);
+int acpi_battery_common_resume(struct device *dev);
+void acpi_battery_common_notify(struct acpi_device *device, u32 event);
+
+/* Defined in cm_sbs.c */
+#ifdef CONFIG_ACPI_PROCFS_POWER
+struct proc_dir_entry *acpi_lock_battery_dir(void);
+void *acpi_unlock_battery_dir(struct proc_dir_entry *acpi_battery_dir);
+#endif
 
 #endif
