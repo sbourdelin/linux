27a535db81dc1cefd4dd4d6f4e065a8823180521
Yongji Xie <xyjxie@linux.vnet.ibm.com>
2016-06-30 18:53:13 +0800

    PCI: Add a macro to set default alignment for all PCI devices
    Now we can use something like "pci=resource_alignment=*:*:*.*:noresize"
    to enforce the alignment of all MMIO BARs to be at least PAGE_SIZE so
    that we can passthrough sub-page(size < PAGE_SIZE) BARs to guest in
    VFIO module.
    
    But sometimes we may want to enable this feature by default on
    some platforms such as PowerNV platform which would easily see those
    sub-page BARs because of its 64K page. To achieve that, we add a
    macro PCIBIOS_DEFAULT_ALIGNMENT to set default alignment for all
    PCI devices and define it on PowerNV platform.
    
    Signed-off-by: Yongji Xie <xyjxie@linux.vnet.ibm.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -4761,6 +4761,10 @@ static resource_size_t pci_specified_resource_alignment(struct pci_dev *dev,
 	char *p;
 	bool invalid = false;
 
+#ifdef PCIBIOS_DEFAULT_ALIGNMENT
+	align = PCIBIOS_DEFAULT_ALIGNMENT;
+	*resize = false;
+#endif
 	spin_lock(&resource_alignment_lock);
 	p = resource_alignment_param;
 	if (pci_has_flag(PCI_PROBE_ONLY)) {
