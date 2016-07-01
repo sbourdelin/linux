cd017060fded512eec7145c8f5d6c341e82750ac
David Howells <dhowells@redhat.com>
2016-07-01 08:08:07 +0100

    Introduce rb_replace_node_rcu()
    Hi Peter,
    
    How about the attached patch?  Should I also reorder rb_replace_node() whilst
    I'm at it so that the new node is initialised first (it shouldn't make a
    difference, I know)?
    
    David

--- lib/rbtree.c
+++ lib/rbtree.c
@@ -551,6 +551,25 @@ void rb_replace_node(struct rb_node *victim, struct rb_node *new,
 }
 EXPORT_SYMBOL(rb_replace_node);
 
+void rb_replace_node_rcu(struct rb_node *victim, struct rb_node *new,
+			 struct rb_root *root)
+{
+	struct rb_node *parent = rb_parent(victim);
+
+	/* Copy the pointers/colour from the victim to the replacement */
+	*new = *victim;
+
+	/* Set the surrounding nodes to point to the replacement */
+	if (victim->rb_left)
+		rb_set_parent(victim->rb_left, new);
+	if (victim->rb_right)
+		rb_set_parent(victim->rb_right, new);
+
+	/* Set the onward pointer last with an RCU barrier */
+	__rb_change_child_rcu(victim, new, parent, root);
+}
+EXPORT_SYMBOL(rb_replace_node_rcu);
+
 static struct rb_node *rb_left_deepest_node(const struct rb_node *node)
 {
 	for (;;) {
