cd259c66c4ad56ab4136c278b826ca8ffe60436a
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2016-07-01 11:50:11 +0530

    net: ethernet: xilinx: Add gmii2rgmii converter support
    This patch adds support for gmii2rgmii converter.
    
    The GMII to RGMII IP core provides the Reduced Gigabit Media
    Independent Interface (RGMII) between Ethernet physical media
    Devices and the Gigabit Ethernet controller. This core can
    switch dynamically between the three different speed modes of
    Operation.
    MDIO interface is used to set operating speed of Ethernet MAC
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>

---
+++ include/linux/xilinx_gmii2rgmii.h
@@ -0,0 +1,24 @@
+#ifndef _GMII2RGMII_H
+#define _GMII2RGMII_H
+
+#include <linux/of.h>
+#include <linux/phy.h>
+#include <linux/mii.h>
+
+#define XILINX_GMII2RGMII_FULLDPLX		BMCR_FULLDPLX
+#define XILINX_GMII2RGMII_SPEED1000		BMCR_SPEED1000
+#define XILINX_GMII2RGMII_SPEED100		BMCR_SPEED100
+#define XILINX_GMII2RGMII_REG_NUM			0x10
+
+struct gmii2rgmii {
+	struct net_device	*dev;
+	struct mii_bus		*mii_bus;
+	struct phy_device	*gmii2rgmii_phy_dev;
+	void			*platform_data;
+	void (*mdio_write)(struct mii_bus *bus, int mii_id, int reg,
+			   int val);
+	void (*fix_mac_speed)(void *priv, unsigned int speed);
+};
+
+extern int gmii2rgmii_phyprobe(struct gmii2rgmii *xphy);
+#endif
