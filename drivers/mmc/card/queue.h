3e2ba40780ce31eace7a1ee723522f91ae878c08
Adrian Hunter <adrian.hunter@intel.com>
2016-07-08 13:23:07 +0300

    mmc: queue: Add a function to control wake-up on new requests
    Add a function to control wake-up on new requests. This will enable
    Software Command Queuing to choose whether or not to queue new
    requests immediately or wait for the current task to complete.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- drivers/mmc/card/queue.h
+++ drivers/mmc/card/queue.h
@@ -52,6 +52,7 @@ struct mmc_queue {
 	unsigned int		flags;
 #define MMC_QUEUE_SUSPENDED	(1 << 0)
 	bool			asleep;
+	bool			is_new_req;
 
 	int			(*issue_fn)(struct mmc_queue *, struct request *);
 	void			*data;
@@ -81,5 +82,6 @@ extern int mmc_access_rpmb(struct mmc_queue *);
 extern struct mmc_queue_req *mmc_queue_req_find(struct mmc_queue *,
 						struct request *);
 extern void mmc_queue_req_free(struct mmc_queue *, struct mmc_queue_req *);
+extern void mmc_queue_set_wake(struct mmc_queue *, bool);
 
 #endif
