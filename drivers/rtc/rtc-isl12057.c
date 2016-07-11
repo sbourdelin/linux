anobli/linux-next

commit 4e30a585a3434b95317fb57a1e7c89d85d22e65b

rtc: isl12057: let the rtc core interpret the partial alarm
The rtc chip doesn't support triggering on month and year. So just don't
assign the respective fields in .read_alarm and let the rtc core
interpret this accordingly.

Signed-off-by: Uwe Kleine-König <uwe@kleine-koenig.org>
Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

drivers/rtc/rtc-isl12057.c
@@ -264,36 +264,6 @@ static int isl12057_rtc_read_alarm(struct device *dev, struct rtc_wkalrm *alarm)
 	alarm_tm->tm_min  = bcd2bin(regs[1] & 0x7f);
 	alarm_tm->tm_hour = bcd2bin(regs[2] & 0x3f);
 	alarm_tm->tm_mday = bcd2bin(regs[3] & 0x3f);
-	alarm_tm->tm_wday = -1;
-
-	/*
-	 * The alarm section does not store year/month. We use the ones in rtc
-	 * section as a basis and increment month and then year if needed to get
-	 * alarm after current time.
-	 */
-	ret = _isl12057_rtc_read_time(dev, &rtc_tm);
-	if (ret)
-		goto err_unlock;
-
-	alarm_tm->tm_year = rtc_tm.tm_year;
-	alarm_tm->tm_mon = rtc_tm.tm_mon;
-
-	ret = rtc_tm_to_time(&rtc_tm, &rtc_secs);
-	if (ret)
-		goto err_unlock;
-
-	ret = rtc_tm_to_time(alarm_tm, &alarm_secs);
-	if (ret)
-		goto err_unlock;
-
-	if (alarm_secs < rtc_secs) {
-		if (alarm_tm->tm_mon == 11) {
-			alarm_tm->tm_mon = 0;
-			alarm_tm->tm_year += 1;
-		} else {
-			alarm_tm->tm_mon += 1;
-		}
-	}
 
 	ret = regmap_read(data->regmap, ISL12057_REG_INT, &ir);
 	if (ret) {

sbourdelin/linux

commit 784355cfdde40ee2a744681aa237f848ba76e587

rtc: isl12057: remove unused variables
A cleanup patch introduced a harmless warning about now unused local variables:

drivers/rtc/rtc-isl12057.c: In function 'isl12057_rtc_read_alarm':
drivers/rtc/rtc-isl12057.c:249:26: error: unused variable 'alarm_secs' [-Werror=unused-variable]
  unsigned long rtc_secs, alarm_secs;
                          ^~~~~~~~~~
drivers/rtc/rtc-isl12057.c:249:16: error: unused variable 'rtc_secs' [-Werror=unused-variable]
  unsigned long rtc_secs, alarm_secs;
                ^~~~~~~~
drivers/rtc/rtc-isl12057.c:248:18: error: unused variable 'rtc_tm' [-Werror=unused-variable]
  struct rtc_time rtc_tm, *alarm_tm = &alarm->time;
                  ^~~~~~

This removes the variables to shut up those warnings.

Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Fixes: 4e30a585a343 ("rtc: isl12057: let the rtc core interpret the partial alarm")

drivers/rtc/rtc-isl12057.c
@@ -245,8 +245,7 @@ static int isl12057_rtc_update_alarm(struct device *dev, int enable)
 static int isl12057_rtc_read_alarm(struct device *dev, struct rtc_wkalrm *alarm)
 {
 	struct isl12057_rtc_data *data = dev_get_drvdata(dev);
-	struct rtc_time rtc_tm, *alarm_tm = &alarm->time;
-	unsigned long rtc_secs, alarm_secs;
+	struct rtc_time *alarm_tm = &alarm->time;
 	u8 regs[ISL12057_A1_SEC_LEN];
 	unsigned int ir;
 	int ret;