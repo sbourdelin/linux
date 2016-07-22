11a2c368328d156e42118ecb47c9973320e8c914
Waiman Long <Waiman.Long@hpe.com>
2016-07-22 16:35:55 -0400

    vfs: Use dlock list for superblock's inode list
    When many threads are trying to add or delete inode to or from
    a superblock's s_inodes list, spinlock contention on the list can
    become a performance bottleneck.
    
    This patch changes the s_inodes field to become a dlock list which
    is a distributed set of lists with per-list spinlocks.  As a result,
    the following superblock inode list (sb->s_inodes) iteration functions
    in vfs are also being modified:
    
     1. iterate_bdevs()
     2. drop_pagecache_sb()
     3. wait_sb_inodes()
     4. evict_inodes()
     5. invalidate_inodes()
     6. fsnotify_unmount_inodes()
     7. add_dquot_ref()
     8. remove_dquot_ref()
    
    With an exit microbenchmark that creates a large number of threads,
    attachs many inodes to them and then exits. The runtimes of that
    microbenchmark with 1000 threads before and after the patch on a
    4-socket Intel E7-4820 v3 system (40 cores, 80 threads) were as
    follows:
    
      Kernel            Elapsed Time    System Time
      ------            ------------    -----------
      Vanilla 4.5-rc4      65.29s         82m14s
      Patched 4.5-rc4      22.81s         23m03s
    
    Before the patch, spinlock contention at the inode_sb_list_add()
    function at the startup phase and the inode_sb_list_del() function at
    the exit phase were about 79% and 93% of total CPU time respectively
    (as measured by perf). After the patch, the dlock_list_add()
    function consumed only about 0.04% of CPU time at startup phase. The
    dlock_list_del() function consumed about 0.4% of CPU time at exit
    phase. There were still some spinlock contention, but they happened
    elsewhere.
    
    Signed-off-by: Waiman Long <Waiman.Long@hpe.com>
    Reviewed-by: Jan Kara <jack@suse.cz>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -1896,9 +1896,9 @@ EXPORT_SYMBOL(__invalidate_device);
 void iterate_bdevs(void (*func)(struct block_device *, void *), void *arg)
 {
 	struct inode *inode, *old_inode = NULL;
+	DEFINE_DLOCK_LIST_ITER(iter, &blockdev_superblock->s_inodes);
 
-	spin_lock(&blockdev_superblock->s_inode_list_lock);
-	list_for_each_entry(inode, &blockdev_superblock->s_inodes, i_sb_list) {
+	dlist_for_each_entry(inode, &iter, i_sb_list) {
 		struct address_space *mapping = inode->i_mapping;
 
 		spin_lock(&inode->i_lock);
@@ -1909,7 +1909,7 @@ void iterate_bdevs(void (*func)(struct block_device *, void *), void *arg)
 		}
 		__iget(inode);
 		spin_unlock(&inode->i_lock);
-		spin_unlock(&blockdev_superblock->s_inode_list_lock);
+		dlock_list_unlock(&iter);
 		/*
 		 * We hold a reference to 'inode' so it couldn't have been
 		 * removed from s_inodes list while we dropped the
@@ -1923,8 +1923,7 @@ void iterate_bdevs(void (*func)(struct block_device *, void *), void *arg)
 
 		func(I_BDEV(inode), arg);
 
-		spin_lock(&blockdev_superblock->s_inode_list_lock);
+		dlock_list_relock(&iter);
 	}
-	spin_unlock(&blockdev_superblock->s_inode_list_lock);
 	iput(old_inode);
 }
--- fs/drop_caches.c
+++ fs/drop_caches.c
@@ -16,9 +16,9 @@ int sysctl_drop_caches;
 static void drop_pagecache_sb(struct super_block *sb, void *unused)
 {
 	struct inode *inode, *toput_inode = NULL;
+	DEFINE_DLOCK_LIST_ITER(iter, &sb->s_inodes);
 
-	spin_lock(&sb->s_inode_list_lock);
-	list_for_each_entry(inode, &sb->s_inodes, i_sb_list) {
+	dlist_for_each_entry(inode, &iter, i_sb_list) {
 		spin_lock(&inode->i_lock);
 		if ((inode->i_state & (I_FREEING|I_WILL_FREE|I_NEW)) ||
 		    (inode->i_mapping->nrpages == 0)) {
@@ -27,15 +27,14 @@ static void drop_pagecache_sb(struct super_block *sb, void *unused)
 		}
 		__iget(inode);
 		spin_unlock(&inode->i_lock);
-		spin_unlock(&sb->s_inode_list_lock);
+		dlock_list_unlock(&iter);
 
 		invalidate_mapping_pages(inode->i_mapping, 0, -1);
 		iput(toput_inode);
 		toput_inode = inode;
 
-		spin_lock(&sb->s_inode_list_lock);
+		dlock_list_relock(&iter);
 	}
-	spin_unlock(&sb->s_inode_list_lock);
 	iput(toput_inode);
 }
 
