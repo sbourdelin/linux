95e0be4ad134ba9346f083900d152f1fa8f86dde
Andrew Donnellan <andrew.donnellan@au1.ibm.com>
2016-07-22 17:16:35 +1000

    powerpc/powernv: document cxl dependency on special case in pnv_eeh_reset()
    pnv_eeh_reset() has special handling for PEs whose primary bus is the root
    bus or the bus immediately underneath the root port.
    
    The cxl bi-modal card support added in b0b5e5918ad1 ("cxl: Add
    cxl_check_and_switch_mode() API to switch bi-modal cards") relies on this
    behaviour when hot-resetting the CAPI adapter following a mode switch.
    Document this in pnv_eeh_reset() so we don't accidentally break it.
    
    Suggested-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>

--- arch/powerpc/platforms/powernv/eeh-powernv.c
+++ arch/powerpc/platforms/powernv/eeh-powernv.c
@@ -1094,6 +1094,13 @@ static int pnv_eeh_reset(struct eeh_pe *pe, int option)
 	if (pe->type & EEH_PE_VF)
 		return pnv_eeh_reset_vf_pe(pe, option);
 
+	/*
+	 * If dealing with the root bus (or the bus underneath the
+	 * root port), we reset the bus underneath the root port.
+	 *
+	 * The cxl driver depends on this behaviour for bi-modal card
+	 * switching.
+	 */
 	if (pci_is_root_bus(bus) ||
 	    pci_is_root_bus(bus->parent))
 		return pnv_eeh_root_reset(hose, option);
