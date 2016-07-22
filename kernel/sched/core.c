cf179aabe550176c4d9aba36ef84fdeebba180a1
Vegard Nossum <vegard.nossum@oracle.com>
2016-07-22 21:46:02 +0200

    sched/core: add taint on "BUG: sleeping function called from invalid context"
    Seeing this, it occurs to me that we should probably add a taint here:
    
        BUG: sleeping function called from invalid context at mm/slab.h:388
        in_atomic(): 0, irqs_disabled(): 0, pid: 32211, name: trinity-c3
        Preemption disabled at:[<ffffffff811aaa37>] console_unlock+0x2f7/0x930
    
        CPU: 3 PID: 32211 Comm: trinity-c3 Not tainted 4.7.0-rc7+ #19
                                           ^^^^^^^^^^^
        Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS Ubuntu-1.8.2-1ubuntu1 04/01/2014
         0000000000000000 ffff8800b8a17160 ffffffff81971441 ffff88011a3c4c80
         ffff88011a3c4c80 ffff8800b8a17198 ffffffff81158067 0000000000000de6
         ffff88011a3c4c80 ffffffff8390e07c 0000000000000184 0000000000000000
        Call Trace:
        [...]
    
        BUG: sleeping function called from invalid context at arch/x86/mm/fault.c:1309
        in_atomic(): 0, irqs_disabled(): 0, pid: 32211, name: trinity-c3
        Preemption disabled at:[<ffffffff8119db33>] down_trylock+0x13/0x80
    
        CPU: 3 PID: 32211 Comm: trinity-c3 Not tainted 4.7.0-rc7+ #19
                                           ^^^^^^^^^^^
        Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS Ubuntu-1.8.2-1ubuntu1 04/01/2014
         0000000000000000 ffff8800b8a17e08 ffffffff81971441 ffff88011a3c4c80
         ffff88011a3c4c80 ffff8800b8a17e40 ffffffff81158067 0000000000000000
         ffff88011a3c4c80 ffffffff83437b20 000000000000051d 0000000000000000
        Call Trace:
        [...]
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Rusty Russel <rusty@rustcorp.com.au>
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7581,6 +7581,7 @@ void ___might_sleep(const char *file, int line, int preempt_offset)
 	}
 #endif
 	dump_stack();
+	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
 }
 EXPORT_SYMBOL(___might_sleep);
 #endif
