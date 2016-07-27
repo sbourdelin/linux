026902493edc36a3eede1e1ccda8bdf2421fd0ab
Jeff Vander Stoep <jeffv@google.com>
2016-07-27 07:46:23 -0700

    security, perf: Add config option to restrict perf_event_open
    When CONFIG_SECURITY_PERF_EVENTS_RESTRICT=y kernel.perf_event_paranoid
    sysctl will be set to 3 by default, and no unprivileged use of the
    perf_event_open syscall will be permitted unless it is changed.
    
    This new level of restriction is intended to reduce the attack
    surface of the kernel. It allows for a safe default to be set on
    production systems at build time while leaving a simple means for
    developers to grant access.
    
    Signed-off-by: Jeff Vander Stoep <jeffv@google.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -355,7 +355,11 @@ static struct srcu_struct pmus_srcu;
  *   2 - disallow kernel profiling for unpriv
  *   3 - disallow all unpriv perf event use
  */
+#ifdef CONFIG_SECURITY_PERF_EVENTS_RESTRICT
+int sysctl_perf_event_paranoid __read_mostly = 3;
+#else
 int sysctl_perf_event_paranoid __read_mostly = 2;
+#endif
 
 /* Minimum for 512 kiB + 1 user control page */
 int sysctl_perf_event_mlock __read_mostly = 512 + (PAGE_SIZE / 1024); /* 'free' kiB per user */
