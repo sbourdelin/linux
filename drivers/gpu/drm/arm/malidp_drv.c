fb5c2fb6a6470d821a6a1b505273e7c7bfcb5696
Liviu Dudau <Liviu.Dudau@arm.com>
2016-07-29 15:44:16 +0100

    drm: mali-dp: Set the drm->irq_enabled flag to match driver's state.
    Mali DP driver does not use drm_irq_{un,}install() function so the
    drm->irq_enabled flag does not get managed automatically. Among other
    DRM framework functions, drm_wait_vblank() checks the value of the
    flag and return -EINVAL if not set.
    
    Cc: Brian Starkey <Brian.Starkey@arm.com>
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>

--- drivers/gpu/drm/arm/malidp_drv.c
+++ drivers/gpu/drm/arm/malidp_drv.c
@@ -377,6 +377,8 @@ static int malidp_bind(struct device *dev)
 	if (ret < 0)
 		goto irq_init_fail;
 
+	drm->irq_enabled = true;
+
 	ret = drm_vblank_init(drm, drm->mode_config.num_crtc);
 	if (ret < 0) {
 		DRM_ERROR("failed to initialise vblank\n");
@@ -402,6 +404,7 @@ fbdev_fail:
 vblank_fail:
 	malidp_se_irq_fini(drm);
 	malidp_de_irq_fini(drm);
+	drm->irq_enabled = false;
 irq_init_fail:
 	component_unbind_all(dev, drm);
 bind_fail:
@@ -439,6 +442,7 @@ static void malidp_unbind(struct device *dev)
 	drm_kms_helper_poll_fini(drm);
 	malidp_se_irq_fini(drm);
 	malidp_de_irq_fini(drm);
+	drm->irq_enabled = false;
 	drm_vblank_cleanup(drm);
 	component_unbind_all(dev, drm);
 	of_node_put(malidp->crtc.port);
