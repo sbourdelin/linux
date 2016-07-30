9a0a1bcd4e940b514f4283b5a8521d672c6adcb8
zhong jiang <zhongjiang@huawei.com>
2016-07-30 11:51:09 +0800

    fs: fix a bug when new_insert_key is not initialization
    when compile the kenrel code, I happens to the following warn.
    fs/reiserfs/ibalance.c:1156:2: warning: ‘new_insert_key’ may be used
    uninitialized in this function.
    memcpy(new_insert_key_addr, &new_insert_key, KEY_SIZE);
    
    The patch fix it by check the new_insert_ptr. if new_insert_ptr is not
    NULL, we ensure that new_insert_key is assigned. therefore, memcpy will
    saftly exec the operatetion.
    
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>

--- fs/reiserfs/ibalance.c
+++ fs/reiserfs/ibalance.c
@@ -1153,8 +1153,10 @@ int balance_internal(struct tree_balance *tb,
 				       insert_ptr);
 	}
 
-	memcpy(new_insert_key_addr, &new_insert_key, KEY_SIZE);
-	insert_ptr[0] = new_insert_ptr;
+	if (new_insert_ptr) {
+		memcpy(new_insert_key_addr, &new_insert_key, KEY_SIZE);
+		insert_ptr[0] = new_insert_ptr;
+	}
 
 	return order;
 }
