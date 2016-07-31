4bd6907983f395fd8b7170b4d62d59120970b9e8
Cristina Moraru <cristina.moraru09@gmail.com>
2016-07-31 17:33:50 +0200

    Add kconfig_symbol attribute to struct module
    Create additional attribute in struct module
    in order for each module to store its associate
    kconfig CONFIG_* symbol.
    
    The goal is to enable each module to expose in
    /sys its corresponding CONFIG_* option. The value
    of this attribute will be dynamically pegged by
    modpost without requiring extra work from the
    driver developers. Further, this information will
    be used by a hardware interogation tool to extract
    build information about the existent devices.
    
    This patch is part of a research project within
    Google Summer of Code of porting 'make localmodconfig'
    for backported drivers.
    
    Signed-off-by: Cristina Moraru <cristina.moraru09@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -757,6 +757,7 @@ static struct module_attribute modinfo_##field = {                    \
 
 MODINFO_ATTR(version);
 MODINFO_ATTR(srcversion);
+MODINFO_ATTR(kconfig_symbol);
 
 static char last_unloaded_module[MODULE_NAME_LEN+1];
 
