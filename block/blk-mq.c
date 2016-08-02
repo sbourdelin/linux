daf69bc2c7b8e0cccf3d7d6d04517de334968a87
Gabriel Krisman Bertazi <krisman@linux.vnet.ibm.com>
2016-08-02 13:19:31 -0300

    blk-mq: Prevent round-robin from scheduling dead cpus
    Hi,
    
    I'm not completely sure I got the cause for this one completely right.
    Still, it does looks like the correct fix and a good improvement in the
    overall, so I'm making it an RFC for now to gather some feedback.
    
    Let me hear your thoughts.
    
    -- >8 --
    
    When notifying blk-mq about CPU removals while running IO, we risk
    racing the hctx->cpumask update with blk_mq_hctx_next_cpu, and end up
    scheduling a dead cpu to execute hctx->run_{,delayed_}work.  As a
    result, kblockd_schedule_delayed_work_on() may schedule another cpu
    outside of hctx->cpumask, which triggers the following warning at
    __blk_mq_run_hw_queue:
    
    WARN_ON(!cpumask_test_cpu(raw_smp_processor_id(), hctx->cpumask));
    
    This patch makes the issue much more unlikely to happen, as it makes
    blk_mq_hctx_next_cpu aware of dead cpus, and triggers the round-robin
    code, despite of remaining batch processing time.  Thus, in case we
    offline a cpu in the middle of its batch processing time, we no longer
    waste time scheduling it here, and just move through to the next cpu in
    the mask.
    
    The warning may still be triggered, though, since this is not the only
    case that may cause the queue to schedule on a dead cpu.  But this fixes
    the common case, which is the remaining batch processing time of a
    sudden dead cpu, which makes the issue much more unlikely to happen.
    
    Signed-off-by: Gabriel Krisman Bertazi <krisman@linux.vnet.ibm.com>
    Cc: Brian King <brking@linux.vnet.ibm.com>
    Cc: linux-block@vger.kernel.org
    Cc: linux-scsi@vger.kernel.org

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -889,7 +889,8 @@ static int blk_mq_hctx_next_cpu(struct blk_mq_hw_ctx *hctx)
 	if (hctx->queue->nr_hw_queues == 1)
 		return WORK_CPU_UNBOUND;
 
-	if (--hctx->next_cpu_batch <= 0) {
+	if (--hctx->next_cpu_batch <= 0 ||
+	    !cpumask_test_cpu(hctx->next_cpu, cpu_online_mask)) {
 		int cpu = hctx->next_cpu, next_cpu;
 
 		next_cpu = cpumask_next(hctx->next_cpu, hctx->cpumask);
@@ -899,7 +900,8 @@ static int blk_mq_hctx_next_cpu(struct blk_mq_hw_ctx *hctx)
 		hctx->next_cpu = next_cpu;
 		hctx->next_cpu_batch = BLK_MQ_CPU_WORK_BATCH;
 
-		return cpu;
+		return (cpumask_test_cpu(cpu, cpu_online_mask)) ?
+			cpu : blk_mq_hctx_next_cpu(hctx);
 	}
 
 	return hctx->next_cpu;
