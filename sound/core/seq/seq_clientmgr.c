de67a0f56b2ded9550f41b505750df56018e2b77
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-08-07 18:49:14 +0900

    ALSA: seq: optimize query_next_client function to new design
    In former commit, actual operations of each ioctl command get argument
    in kernel space. Copying from/to user space is performed outside of
    the function.
    
    This commit optimizes to the new design.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- sound/core/seq/seq_clientmgr.c
+++ sound/core/seq/seq_clientmgr.c
@@ -1988,32 +1988,26 @@ static int seq_ioctl_query_subs(struct snd_seq_client *client, void *arg)
 /*
  * query next client
  */
-static int seq_ioctl_query_next_client(struct snd_seq_client *client,
-				       void __user *arg)
+static int seq_ioctl_query_next_client(struct snd_seq_client *client, void *arg)
 {
+	struct snd_seq_client_info *info = arg;
 	struct snd_seq_client *cptr = NULL;
-	struct snd_seq_client_info info;
-
-	if (copy_from_user(&info, arg, sizeof(info)))
-		return -EFAULT;
 
 	/* search for next client */
-	info.client++;
-	if (info.client < 0)
-		info.client = 0;
-	for (; info.client < SNDRV_SEQ_MAX_CLIENTS; info.client++) {
-		cptr = snd_seq_client_use_ptr(info.client);
+	info->client++;
+	if (info->client < 0)
+		info->client = 0;
+	for (; info->client < SNDRV_SEQ_MAX_CLIENTS; info->client++) {
+		cptr = snd_seq_client_use_ptr(info->client);
 		if (cptr)
 			break; /* found */
 	}
 	if (cptr == NULL)
 		return -ENOENT;
 
-	get_client_info(cptr, &info);
+	get_client_info(cptr, info);
 	snd_seq_client_unlock(cptr);
 
-	if (copy_to_user(arg, &info, sizeof(info)))
-		return -EFAULT;
 	return 0;
 }
 
