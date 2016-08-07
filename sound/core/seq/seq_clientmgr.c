a868b01f9a6f70c26806b3494c35da366aa5656d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-08-07 18:49:08 +0900

    ALSA: seq: optimize set_queue_client function to new design
    In former commit, actual operations of each ioctl command get argument
    in kernel space. Copying from/to user space is performed outside of
    the function.
    
    This commit optimizes to the new design.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- sound/core/seq/seq_clientmgr.c
+++ sound/core/seq/seq_clientmgr.c
@@ -1773,17 +1773,14 @@ static int seq_ioctl_get_queue_client(struct snd_seq_client *client, void *arg)
 
 
 /* SET_QUEUE_CLIENT ioctl() */
-static int seq_ioctl_set_queue_client(struct snd_seq_client *client,
-				      void __user *arg)
+static int seq_ioctl_set_queue_client(struct snd_seq_client *client, void *arg)
 {
+	struct snd_seq_queue_client *info = arg;
 	int err;
-	struct snd_seq_queue_client info;
 
-	if (copy_from_user(&info, arg, sizeof(info)))
-		return -EFAULT;
-
-	if (info.used >= 0) {
-		err = snd_seq_queue_use(info.queue, client->number, info.used);
+	if (info->used >= 0) {
+		err = snd_seq_queue_use(info->queue, client->number,
+					info->used);
 		if (err < 0)
 			return err;
 	}
