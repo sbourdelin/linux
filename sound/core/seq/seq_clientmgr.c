5c4f3926b7ca0f404417d51dfc8ade07ea284c21
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-08-07 18:49:01 +0900

    ALSA: seq: optimize get_named_queue function to new design
    In former commit, actual operations of each ioctl command get argument
    in kernel space. Copying from/to user space is performed outside of
    the function.
    
    This commit optimizes to the new design.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- sound/core/seq/seq_clientmgr.c
+++ sound/core/seq/seq_clientmgr.c
@@ -1598,25 +1598,18 @@ static int seq_ioctl_set_queue_info(struct snd_seq_client *client, void *arg)
 }
 
 /* GET_NAMED_QUEUE ioctl() */
-static int seq_ioctl_get_named_queue(struct snd_seq_client *client,
-				     void __user *arg)
+static int seq_ioctl_get_named_queue(struct snd_seq_client *client, void *arg)
 {
-	struct snd_seq_queue_info info;
+	struct snd_seq_queue_info *info = arg;
 	struct snd_seq_queue *q;
 
-	if (copy_from_user(&info, arg, sizeof(info)))
-		return -EFAULT;
-
-	q = snd_seq_queue_find_name(info.name);
+	q = snd_seq_queue_find_name(info->name);
 	if (q == NULL)
 		return -EINVAL;
-	info.queue = q->queue;
-	info.owner = q->owner;
-	info.locked = q->locked;
+	info->queue = q->queue;
+	info->owner = q->owner;
+	info->locked = q->locked;
 	queuefree(q);
-
-	if (copy_to_user(arg, &info, sizeof(info)))
-		return -EFAULT;
 
 	return 0;
 }
