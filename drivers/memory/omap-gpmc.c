8cea3c93032f3efe11714d36f2e4fa2702e1a85a
Linus Walleij <linus.walleij@linaro.org>
2016-08-08 10:03:16 +0200

    memory: omap-gpmc: use devm_gpiochip_add_data()
    This saves a few codelines in the driver.
    
    Cc: Roger Quadros <rogerq@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/memory/omap-gpmc.c
+++ drivers/memory/omap-gpmc.c
@@ -2266,18 +2266,13 @@ static int gpmc_gpio_init(struct gpmc_device *gpmc)
 	gpmc->gpio_chip.get = gpmc_gpio_get;
 	gpmc->gpio_chip.base = -1;
 
-	ret = gpiochip_add(&gpmc->gpio_chip);
+	ret = devm_gpiochip_add_data(gpmv->dev, &gpmc->gpio_chip, NULL);
 	if (ret < 0) {
 		dev_err(gpmc->dev, "could not register gpio chip: %d\n", ret);
 		return ret;
 	}
 
 	return 0;
-}
-
-static void gpmc_gpio_exit(struct gpmc_device *gpmc)
-{
-	gpiochip_remove(&gpmc->gpio_chip);
 }
 
 static int gpmc_probe(struct platform_device *pdev)
@@ -2394,7 +2389,6 @@ static int gpmc_remove(struct platform_device *pdev)
 	struct gpmc_device *gpmc = platform_get_drvdata(pdev);
 
 	gpmc_free_irq(gpmc);
-	gpmc_gpio_exit(gpmc);
 	gpmc_mem_exit();
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
