adb67ccdcbc8a02923a521bb7e475bfd76dff826
Fabian Frederick <fabf@skynet.be>
2016-08-12 23:54:47 +0200

    iommu/amd: remove AMD_IOMMU_STATS
    Commit e85e8f69cedb
    ("iommu/amd: Remove statistics code")
    removed that config.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>

--- drivers/iommu/amd_iommu_proto.h
+++ drivers/iommu/amd_iommu_proto.h
@@ -78,11 +78,7 @@ static inline int amd_iommu_create_irq_domain(struct amd_iommu *iommu)
 extern int amd_iommu_complete_ppr(struct pci_dev *pdev, int pasid,
 				  int status, int tag);
 
-#ifndef CONFIG_AMD_IOMMU_STATS
-
 static inline void amd_iommu_stats_init(void) { }
-
-#endif /* !CONFIG_AMD_IOMMU_STATS */
 
 static inline bool is_rd890_iommu(struct pci_dev *pdev)
 {
