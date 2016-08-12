ea89e3b26cc5e6ee8fc411e90eb42b6f8043f224
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-08-12 11:40:13 +0300

    timer: Make msleep(0) a nop
    Thanks to the msecs_to_jiffies()+1 msleep(0) may actually sleep for up
    to one jiffy. Presumably the caller should be satisfied if we "sleep"
    for 0 jiffies instead of 0-1 jiffies, so let's just turn msleep(0)
    into a nop.
    
    This can simplify some callers as they don't have to check for the
    0 msecs case themselves anymore. Or if they're not checking for 0,
    they might avoid a needless sleep occasionally.
    
    A slight concern might be that someone is calling msleep(0) and
    depending on some delay being there. But that can clearly blow up
    even without this change, so I'm not overly worried about it.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1891,7 +1891,7 @@ void __init init_timers(void)
  */
 void msleep(unsigned int msecs)
 {
-	unsigned long timeout = msecs_to_jiffies(msecs) + 1;
+	unsigned long timeout = msecs ? msecs_to_jiffies(msecs) + 1 : 0;
 
 	while (timeout)
 		timeout = schedule_timeout_uninterruptible(timeout);
@@ -1905,7 +1905,7 @@ EXPORT_SYMBOL(msleep);
  */
 unsigned long msleep_interruptible(unsigned int msecs)
 {
-	unsigned long timeout = msecs_to_jiffies(msecs) + 1;
+	unsigned long timeout = msecs ? msecs_to_jiffies(msecs) + 1 : 0;
 
 	while (timeout && !signal_pending(current))
 		timeout = schedule_timeout_interruptible(timeout);
