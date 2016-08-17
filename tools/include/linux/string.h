929c8be48855a48f096a0479d444c7d34f3b3db1
Vineet Gupta <Vineet.Gupta1@synopsys.com>
2016-08-17 13:00:50 -0700

    tools lib: Reinstate strlcpy() header guard with __UCLIBC__
    perf tools build in recent kernels spews splat when cross compiling with uClibc
    
    |   CC       util/alias.o
    | In file included from tools/perf/util/../ui/../util/cache.h:8:0,
    |                 from tools/perf/util/../ui/helpline.h:7,
    |                 from tools/perf/util/debug.h:8,
    |                 from arch/../util/cpumap.h:9,
    |                 from arch/../util/env.h:5,
    |                 from arch/common.h:4,
    |                 from arch/common.c:3:
    | tools/include/linux/string.h:12:15: warning: redundant redeclaration of ‘strlcpy’ [-Wredundant-decls]
    |  extern size_t strlcpy(char *dest, const char *src, size_t size);
                   ^
    This is after commit 61a6445e463a31 ("tools lib: Guard the strlcpy() header with
    __GLIBC__"). While the commit was right in theory, issue is uClibc also
    defines __GLIBC__ for application headers.
    
    Instead of reverting the commit, manually revert with some addeed commentary to
    same effect.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexey Brodkin <Alexey.Brodkin@synopsys.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Cc: Petri Gynther <pgynther@google.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- tools/include/linux/string.h
+++ tools/include/linux/string.h
@@ -8,7 +8,12 @@ void *memdup(const void *src, size_t len);
 
 int strtobool(const char *s, bool *res);
 
-#ifdef __GLIBC__
+/*
+ * Ideally instead of blacklisting libs which define this already (uclibc,
+ * musl..), we could whitelist libs which reuire it (glibc). However uClibc
+ * also defines __GLIBC__ for user application headers
+ */
+#ifndef __UCLIBC__
 extern size_t strlcpy(char *dest, const char *src, size_t size);
 #endif
 
