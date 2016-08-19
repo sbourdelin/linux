13e6b783f99edaad1554b3b50db177876c2f4324
Al Stone <ahs3@redhat.com>
2016-08-19 17:16:34 -0600

    trivial: ACPI: remove extraneous whitespace from a comment
    Signed-off-by: Al Stone <ahs3@redhat.com>

--- drivers/acpi/tables.c
+++ drivers/acpi/tables.c
@@ -416,7 +416,7 @@ int __init acpi_table_parse(char *id, acpi_tbl_table_handler handler)
 		return -ENODEV;
 }
 
-/* 
+/*
  * The BIOS is supposed to supply a single APIC/MADT,
  * but some report two.  Provide a knob to use either.
  * (don't you wish instance 0 and 1 were not the same?)
