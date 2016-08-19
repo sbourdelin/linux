947f52d1898f885e6e85de1c5abdf9052dab157a
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2016-08-19 14:22:37 +0530

    powerpc/64/kexec: Fix MMU cleanup on radix
    Just using the hash ops won't work anymore since radix will have
    NULL in there. Instead create an mmu_cleanup_all() function which
    will do the right thing based on the MMU mode.
    
    For Radix, for now I clear UPRT and the PTCR, effectively switching
    back to Radix with no partition table setup.
    
    Currently set it to NULL on BookE thought it might be a good idea
    to wipe the TLB there (Scott ?)
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- arch/powerpc/kernel/machine_kexec_64.c
+++ arch/powerpc/kernel/machine_kexec_64.c
@@ -30,6 +30,7 @@
 #include <asm/smp.h>
 #include <asm/hw_breakpoint.h>
 #include <asm/asm-prototypes.h>
+#include <asm/firmware.h>
 
 #ifdef CONFIG_PPC_BOOK3E
 int default_machine_kexec_prepare(struct kimage *image)
@@ -54,9 +55,6 @@ int default_machine_kexec_prepare(struct kimage *image)
 	struct device_node *node;
 	const unsigned long *basep;
 	const unsigned int *sizep;
-
-	if (!mmu_hash_ops.hpte_clear_all)
-		return -ENOENT;
 
 	/*
 	 * Since we use the kernel fault handlers and paging code to
@@ -379,13 +377,8 @@ void default_machine_kexec(struct kimage *image)
 	 * a toc is easier in C, so pass in what we can.
 	 */
 	kexec_sequence(&kexec_stack, image->start, image,
-			page_address(image->control_code_page),
-#ifdef CONFIG_PPC_STD_MMU
-			mmu_hash_ops.hpte_clear_all
-#else
-			NULL
-#endif
-	);
+		       page_address(image->control_code_page),
+		       mmu_cleanup_all);
 	/* NOTREACHED */
 }
 
