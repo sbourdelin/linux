0d8c5a501cb78873329fe6208f43dfd2b9675e10
Sagar Arun Kamble <sagar.a.kamble@intel.com>
2016-08-21 11:49:54 +0530

    drm/i915: Remove RPM suspend dependency on rps.enabled and related changes
    For Gen9, RPM suspend is failing if rps.enabled=false. This is needed for
    other platforms as RC6 and RPS enabling is indicated by rps.enabled. RPM
    Suspend depends only on RC6, so we need to remove the check of rps.enabled.
    For Gen9 RC6 and RPS enabling is separated hence do rps.enabled check only
    for non-Gen9 platforms. Once RC6 and RPS enabling is separated for other
    GENs this check can be completely removed.
    Moved setting of rps.enabled to platform level functions as there is case
    of disabling of RPS in gen9_enable_rps.
    
    v2: Changing parameter to dev_priv for IS_GEN9 and HAS_RUNTIME_PM and line
        spacing changes. (David)
        and commit message update for checkpatch issues.
    
    Reviewed-by: David Weinehall <david.weinehall@linux.intel.com>
    Signed-off-by: Sagar Arun Kamble <sagar.a.kamble@intel.com>

--- drivers/gpu/drm/i915/intel_runtime_pm.c
+++ drivers/gpu/drm/i915/intel_runtime_pm.c
@@ -2764,7 +2764,7 @@ void intel_runtime_pm_enable(struct drm_i915_private *dev_priv)
 	 * so the driver's own RPM reference tracking asserts also work on
 	 * platforms without RPM support.
 	 */
-	if (!HAS_RUNTIME_PM(dev)) {
+	if (!HAS_RUNTIME_PM(dev_priv)) {
 		pm_runtime_dont_use_autosuspend(device);
 		pm_runtime_get_sync(device);
 	} else {
