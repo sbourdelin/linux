1d39ae3c3d8e5d1231fb03e5677037f4b419b5af
Nicolai Stange <nicstange@gmail.com>
2016-08-23 01:33:17 +0200

    clockevents: purge ->min_delta_ns and ->max_delta_ns
    The struct clock_event_device ->min_delta_ns and ->max_delta_ns aren't
    used anymore.
    
    Purge them.
    
    In __clockevents_update_bounds(), shortcut the
      ->min_delta_ticks => ->min_delta_ns => ->min_delta_ticks_adjusted
    calculation detour -- it had been made solely for the purpose of ensuring
    that ->min_delta_ticks_adjusted corresponds to something >= 1us.
    
    Signed-off-by: Nicolai Stange <nicstange@gmail.com>

--- include/linux/clockchips.h
+++ include/linux/clockchips.h
@@ -87,8 +87,6 @@ enum clock_event_state {
  * @tick_resume:	resume clkevt device
  * @broadcast:		function to broadcast events
  * @min_delta_ticks:	minimum delta value in ticks stored for reconfiguration
- * @max_delta_ns:	maximum delta value in ns
- * @min_delta_ns:	minimum delta value in ns
  * @name:		ptr to clock event name
  * @rating:		variable to rate clock event devices
  * @irq:		IRQ number (only for non CPU local devices)
@@ -120,8 +118,6 @@ struct clock_event_device {
 	void			(*suspend)(struct clock_event_device *);
 	void			(*resume)(struct clock_event_device *);
 	unsigned long		min_delta_ticks;
-	u64			max_delta_ns;
-	u64			min_delta_ns;
 
 	const char		*name;
 	int			rating;
