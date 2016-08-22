549cd0498fe1322fd7f494a0cc4f77cda035e31a
Mikko Rapeli <mikko.rapeli@iki.fi>
2016-08-22 20:33:11 +0200

    include/uapi/xen/privcmd.h: fix compilation in userspace
    xen/interface/xen.h is not exported from kernel headers so remove the
    dependency and provide needed defines for domid_t and xen_pfn_t if they
    are not already defined by some other e.g. Xen specific headers.
    
    Suggested by Andrew Cooper <andrew.cooper3@citrix.com> on lkml message
    <5569F9C9.8000607@citrix.com>.
    
    The ifdef for ARM is ugly but did not find better solutions for it.
    
    Fixes userspace compilation error:
    
    xen/privcmd.h:38:31: fatal error: xen/interface/xen.h: No such file or directory
    
    Signed-off-by: Mikko Rapeli <mikko.rapeli@iki.fi>
    Cc: David Vrabel <david.vrabel@citrix.com>

--- include/uapi/xen/privcmd.h
+++ include/uapi/xen/privcmd.h
@@ -35,7 +35,17 @@
 
 #include <linux/types.h>
 #include <linux/compiler.h>
-#include <xen/interface/xen.h>
+
+/* Defined by include/xen/interface/xen.h, but it is not part of Linux uapi */
+#ifndef __XEN_PUBLIC_XEN_H__
+typedef __u16 domid_t;
+
+#if (defined __ARMEL__ || defined __ARMEB__)
+typedef __u64 xen_pfn_t;
+#else
+typedef unsigned long xen_pfn_t;
+#endif /* (defined __ARMEL__ || defined __ARMEB__) */
+#endif /* __XEN_PUBLIC_XEN_H__ */
 
 struct privcmd_hypercall {
 	__u64 op;
