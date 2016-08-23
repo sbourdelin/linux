249bd62ce48bf96ea624a6ee9947254e6ff58175
Tom O'Rourke <Tom.O'Rourke@intel.com>
2016-08-23 16:09:36 +0530

    drm/i915/slpc: Add has_slpc capability flag
    Add has_slpc capablity flag to indicate GuC firmware
    supports single loop power control (SLPC).  SLPC is
    a replacement for some host-based power management
    features.
    
    v1: fix whitespace (Sagar)
    
    Reviewed-by: David Weinehall <david.weinehall@linux.intel.com>
    Signed-off-by: Tom O'Rourke <Tom.O'Rourke@intel.com>
    Signed-off-by: Sagar Arun Kamble <sagar.a.kamble@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -654,6 +654,7 @@ struct intel_csr {
 	func(is_kabylake) sep \
 	func(is_preliminary) sep \
 	func(has_fbc) sep \
+	func(has_slpc) sep \
 	func(has_pipe_cxsr) sep \
 	func(has_hotplug) sep \
 	func(cursor_needs_physical) sep \
@@ -2793,6 +2794,7 @@ struct drm_i915_cmd_table {
 #define HAS_GUC(dev)		(IS_GEN9(dev))
 #define HAS_GUC_UCODE(dev)	(HAS_GUC(dev))
 #define HAS_GUC_SCHED(dev)	(HAS_GUC(dev))
+#define HAS_SLPC(dev)		(INTEL_INFO(dev)->has_slpc)
 
 #define HAS_RESOURCE_STREAMER(dev) (IS_HASWELL(dev) || \
 				    INTEL_INFO(dev)->gen >= 8)
