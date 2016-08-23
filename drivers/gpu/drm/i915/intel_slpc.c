4b6f1f4bdce3cc9d7c0d29e24bdf3a88e2b1f370
Sagar Arun Kamble <sagar.a.kamble@intel.com>
2016-08-23 16:09:56 +0530

    drm/i915/slpc: Only Enable GTPERF, Disable DCC, Balancer, IBC, FPS Stall
    v1: Updated tasks and frequency post reset.
        Added DFPS param update for MAX_FPS and FPS Stall.
    
    Signed-off-by: Sagar Arun Kamble <sagar.a.kamble@intel.com>

--- drivers/gpu/drm/i915/intel_slpc.c
+++ drivers/gpu/drm/i915/intel_slpc.c
@@ -301,8 +301,38 @@ void intel_slpc_disable(struct drm_i915_private *dev_priv)
 
 void intel_slpc_enable(struct drm_i915_private *dev_priv)
 {
+	u64 val;
+
 	host2guc_slpc_reset(dev_priv);
 	dev_priv->guc.slpc.enabled = true;
+
+	/* Enable only GTPERF task, Disable others */
+	val = SLPC_PARAM_TASK_ENABLED;
+	slpc_enable_disable_set(&dev_priv->drm, val,
+				SLPC_PARAM_TASK_ENABLE_GTPERF,
+				SLPC_PARAM_TASK_DISABLE_GTPERF);
+
+	val = SLPC_PARAM_TASK_DISABLED;
+	slpc_enable_disable_set(&dev_priv->drm, val,
+				SLPC_PARAM_TASK_ENABLE_BALANCER,
+				SLPC_PARAM_TASK_DISABLE_BALANCER);
+
+	slpc_enable_disable_set(&dev_priv->drm, val,
+				SLPC_PARAM_TASK_ENABLE_DCC,
+				SLPC_PARAM_TASK_DISABLE_DCC);
+
+	intel_slpc_set_param(dev_priv,
+			     SLPC_PARAM_GLOBAL_DISABLE_IA_GT_BALANCING,
+			     1);
+
+	intel_slpc_set_param(dev_priv,
+			     SLPC_PARAM_DFPS_THRESHOLD_MAX_FPS,
+			     0);
+
+	intel_slpc_set_param(dev_priv,
+			     SLPC_PARAM_DFPS_DISABLE_FRAMERATE_STALLING,
+			     1);
+
 }
 
 void intel_slpc_reset(struct drm_i915_private *dev_priv)
