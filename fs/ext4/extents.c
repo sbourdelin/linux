ae7b814400c169cc171f0e84d2f25aa4b7bd7837
Fabian Frederick <fabf@skynet.be>
2016-08-24 22:03:16 +0200

    ext4: remove unneeded test in ext4_alloc_file_blocks()
    ext4_alloc_file_blocks() is called from ext4_zero_range() and
    ext4_fallocate() both already testing EXT4_INODE_EXTENTS
    We can call ext_depth(inode) unconditionnally.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -4693,13 +4693,7 @@ static int ext4_alloc_file_blocks(struct file *file, ext4_lblk_t offset,
 	 * credits to insert 1 extent into extent tree
 	 */
 	credits = ext4_chunk_trans_blocks(inode, len);
-	/*
-	 * We can only call ext_depth() on extent based inodes
-	 */
-	if (ext4_test_inode_flag(inode, EXT4_INODE_EXTENTS))
-		depth = ext_depth(inode);
-	else
-		depth = -1;
+	depth = ext_depth(inode);
 
 retry:
 	while (ret >= 0 && len) {
