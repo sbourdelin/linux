8e1d93fe0039c45e9f2742749c194375bab14b77
Mathieu Poirier <mathieu.poirier@linaro.org>
2016-08-25 14:20:45 -0600

    perf tools: adding sink configuration for cs_etm PMU
    Using the PMU::set_drv_config() callback to enable the CoreSight
    sink that will be used for the trace session.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- tools/perf/arch/arm/util/pmu.c
+++ tools/perf/arch/arm/util/pmu.c
@@ -19,7 +19,9 @@
 #include <linux/coresight-pmu.h>
 #include <linux/perf_event.h>
 
+#include "cs-etm.h"
 #include "../../util/pmu.h"
+
 
 struct perf_event_attr
 *perf_pmu__get_default_config(struct perf_pmu *pmu __maybe_unused)
@@ -28,6 +30,7 @@ struct perf_event_attr
 	if (!strcmp(pmu->name, CORESIGHT_ETM_PMU_NAME)) {
 		/* add ETM default config here */
 		pmu->selectable = true;
+		pmu->set_drv_config = cs_etm_set_drv_config;
 	}
 #endif
 	return NULL;
