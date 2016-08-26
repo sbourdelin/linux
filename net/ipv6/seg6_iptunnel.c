a3f71f956ebee1e71b5f0ed115f9147ed8702130
David Lebrun <david.lebrun@uclouvain.be>
2016-08-26 17:54:10 +0200

    ipv6: sr: add calls to verify and insert HMAC signatures
    This patch enables the verification of the HMAC signature for transiting
    SR-enabled packets, and its insertion on encapsulated/injected SRH.
    
    Signed-off-by: David Lebrun <david.lebrun@uclouvain.be>

--- net/ipv6/seg6_iptunnel.c
+++ net/ipv6/seg6_iptunnel.c
@@ -109,6 +109,14 @@ static int seg6_do_srh_encap(struct sk_buff *skb, struct ipv6_sr_hdr *osrh)
 	hdr->daddr = isrh->segments[isrh->first_segment];
 	set_tun_src(net, skb->dev, &hdr->daddr, &hdr->saddr);
 
+#ifdef CONFIG_IPV6_SEG6_HMAC
+	if (sr_get_flags(isrh) & SR6_FLAG_HMAC) {
+		err = seg6_push_hmac(net, &hdr->saddr, isrh);
+		if (unlikely(err))
+			return err;
+	}
+#endif
+
 	return 0;
 }
 
@@ -118,6 +126,7 @@ static int seg6_do_srh_inline(struct sk_buff *skb, struct ipv6_sr_hdr *osrh)
 	struct ipv6hdr *hdr, *oldhdr;
 	struct ipv6_sr_hdr *isrh;
 	int hdrlen, err;
+	struct net *net = dev_net(skb_dst(skb)->dev);
 
 	hdrlen = (osrh->hdrlen + 1) << 3;
 
@@ -143,6 +152,14 @@ static int seg6_do_srh_inline(struct sk_buff *skb, struct ipv6_sr_hdr *osrh)
 
 	isrh->segments[0] = hdr->daddr;
 	hdr->daddr = isrh->segments[isrh->first_segment];
+
+#ifdef CONFIG_IPV6_SEG6_HMAC
+	if (sr_get_flags(isrh) & SR6_FLAG_HMAC) {
+		err = seg6_push_hmac(net, &hdr->saddr, isrh);
+		if (unlikely(err))
+			return err;
+	}
+#endif
 
 	return 0;
 }
