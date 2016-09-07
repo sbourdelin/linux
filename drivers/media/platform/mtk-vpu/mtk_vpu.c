42201ca5b12b8510b9f8ff3525b65187c56422e9
Colin Ian King <colin.king@canonical.com>
2016-09-07 18:10:27 +0100

    VPU: mediatek: fix null pointer dereference on pdev
    pdev is being null checked, however, prior to that it is being
    dereferenced by platform_get_drvdata.  Move the assignments of
    vpu and run to after the pdev null check to avoid a potential
    null pointer dereference.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- drivers/media/platform/mtk-vpu/mtk_vpu.c
+++ drivers/media/platform/mtk-vpu/mtk_vpu.c
@@ -523,9 +523,9 @@ static int load_requested_vpu(struct mtk_vpu *vpu,
 
 int vpu_load_firmware(struct platform_device *pdev)
 {
-	struct mtk_vpu *vpu = platform_get_drvdata(pdev);
+	struct mtk_vpu *vpu;
 	struct device *dev = &pdev->dev;
-	struct vpu_run *run = &vpu->run;
+	struct vpu_run *run;
 	const struct firmware *vpu_fw = NULL;
 	int ret;
 
@@ -533,6 +533,9 @@ int vpu_load_firmware(struct platform_device *pdev)
 		dev_err(dev, "VPU platform device is invalid\n");
 		return -EINVAL;
 	}
+
+	vpu = platform_get_drvdata(pdev);
+	run = &vpu->run;
 
 	mutex_lock(&vpu->vpu_mutex);
 	if (vpu->fw_loaded) {
