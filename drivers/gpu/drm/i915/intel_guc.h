aa3a26976b26643bcb9a5d3c42494a1532a14a3f
Akash Goel <akash.goel@intel.com>
2016-09-08 16:09:08 +0530

    drm/i915: Add a relay backed debugfs interface for capturing GuC logs
    Added a new debugfs interface '/sys/kernel/debug/dri/guc_log' for the
    User to capture GuC firmware logs. Availed relay framework to implement
    the interface, where Driver will have to just use a relay API to store
    snapshots of the GuC log buffer in the buffer managed by relay.
    The snapshot will be taken when GuC firmware sends a log buffer flush
    interrupt and up to four snapshots could be stored in the relay buffer.
    The relay buffer will be operated in a mode where it will overwrite the
    data not yet collected by User.
    Besides mmap method, through which User can directly access the relay
    buffer contents, relay also supports the 'poll' method. Through the 'poll'
    call on log file, User can come to know whenever a new snapshot of the
    log buffer is taken by Driver, so can run in tandem with the Driver and
    capture the logs in a sustained/streaming manner, without any loss of data.
    
    v2: Defer the creation of relay channel & associated debugfs file, as
        debugfs setup is now done at the end of i915 Driver load. (Chris)
    
    v3:
    - Switch to no-overwrite mode for relay.
    - Fix the relay sub buffer switching sequence.
    
    v4:
    - Update i915 Kconfig to select RELAY config. (TvrtKo)
    - Log a message when there is no sub buffer available to capture
      the GuC log buffer. (Tvrtko)
    - Increase the number of relay sub buffers to 8 from 4, to have
      sufficient buffering for boot time logs
    
    v5:
    - Fix the alignment, indentation issues and some minor cleanup. (Tvrtko)
    - Update the comment to elaborate on why a relay channel has to be
      associated with the debugfs file. (Tvrtko)
    
    v6:
    - Move the write to 'is_global' after the NULL check on parent directory
      dentry pointer. (Tvrtko)
    
    v7: Add a BUG_ON to validate relay buffer allocation size. (Chris)
    
    Testcase: igt/tools/intel_guc_logger
    
    Suggested-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Sourab Gupta <sourab.gupta@intel.com>
    Signed-off-by: Akash Goel <akash.goel@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>

--- drivers/gpu/drm/i915/intel_guc.h
+++ drivers/gpu/drm/i915/intel_guc.h
@@ -127,6 +127,7 @@ struct intel_guc_log {
 	void *buf_addr;
 	struct workqueue_struct *flush_wq;
 	struct work_struct flush_work;
+	struct rchan *relay_chan;
 };
 
 struct intel_guc {
@@ -171,5 +172,7 @@ int i915_guc_wq_check_space(struct drm_i915_gem_request *rq);
 void i915_guc_submission_disable(struct drm_i915_private *dev_priv);
 void i915_guc_submission_fini(struct drm_i915_private *dev_priv);
 void i915_guc_capture_logs(struct drm_i915_private *dev_priv);
+void i915_guc_register(struct drm_i915_private *dev_priv);
+void i915_guc_unregister(struct drm_i915_private *dev_priv);
 
 #endif
