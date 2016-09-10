c01972549461be14ca8db6aa13a0ae10cbe90758
Markus Elfring <elfring@users.sourceforge.net>
2016-09-10 20:04:02 +0200

    DRBD-receiver: Delete an unnecessary initialisation in receive_sizes()
    A local variable was assigned a null pointer despite of the detail
    that it was immediately reassigned by the following statement.
    Thus remove such an unnecessary initialisation.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- drivers/block/drbd/drbd_receiver.c
+++ drivers/block/drbd/drbd_receiver.c
@@ -4083,7 +4083,7 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 		}
 
 		if (my_usize != p_usize) {
-			struct disk_conf *old_disk_conf, *new_disk_conf = NULL;
+			struct disk_conf *old_disk_conf, *new_disk_conf;
 
 			new_disk_conf = kzalloc(sizeof(struct disk_conf), GFP_KERNEL);
 			if (!new_disk_conf) {
