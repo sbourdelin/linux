055d358ac7558404de1a97efe88c435378342f95
Paulo Zanoni <paulo.r.zanoni@intel.com>
2016-09-13 21:38:15 -0300

    drm/i915: introduce intel_has_sagv()
    And use it to move knowledge about the SAGV-supporting platforms from
    the callers to the SAGV code.
    
    We'll add more platforms to intel_has_sagv(), so IMHO it makes more
    sense to move all this to a single function instead of patching all
    the callers every time we add SAGV support to a new platform.
    
    v2: Move I915_SAGV_NOT_CONTROLLED to the new function (Lyude).
    
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -2884,6 +2884,13 @@ skl_wm_plane_id(const struct intel_plane *plane)
 	}
 }
 
+static bool
+intel_has_sagv(struct drm_i915_private *dev_priv)
+{
+	return IS_SKYLAKE(dev_priv) &&
+	       dev_priv->sagv_status != I915_SAGV_NOT_CONTROLLED;
+}
+
 /*
  * SAGV dynamically adjusts the system agent voltage and clock frequencies
  * depending on power and performance requirements. The display engine access
@@ -2900,8 +2907,10 @@ intel_enable_sagv(struct drm_i915_private *dev_priv)
 {
 	int ret;
 
-	if (dev_priv->sagv_status == I915_SAGV_NOT_CONTROLLED ||
-	    dev_priv->sagv_status == I915_SAGV_ENABLED)
+	if (!intel_has_sagv(dev_priv))
+		return 0;
+
+	if (dev_priv->sagv_status == I915_SAGV_ENABLED)
 		return 0;
 
 	DRM_DEBUG_KMS("Enabling the SAGV\n");
@@ -2949,8 +2958,10 @@ intel_disable_sagv(struct drm_i915_private *dev_priv)
 {
 	int ret, result;
 
-	if (dev_priv->sagv_status == I915_SAGV_NOT_CONTROLLED ||
-	    dev_priv->sagv_status == I915_SAGV_DISABLED)
+	if (!intel_has_sagv(dev_priv))
+		return 0;
+
+	if (dev_priv->sagv_status == I915_SAGV_DISABLED)
 		return 0;
 
 	DRM_DEBUG_KMS("Disabling the SAGV\n");
@@ -2990,6 +3001,9 @@ bool intel_can_enable_sagv(struct drm_atomic_state *state)
 	struct drm_crtc *crtc;
 	enum pipe pipe;
 	int level, plane;
+
+	if (!intel_has_sagv(dev_priv))
+		return false;
 
 	/*
 	 * SKL workaround: bspec recommends we disable the SAGV when we have
