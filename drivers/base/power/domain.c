8c63fc2ea9656f22d4c60b59a2136fe95efbd60b
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2016-09-15 12:39:23 +0200

    PM / Domains: Allow holes in genpd_data.domains array
    In platforms such as Rockchip's, the array of domains isn't always
    filled without holes, as which domains are present depend on the
    particular SoC revision.
    
    By allowing holes to be in the array, such SoCs can still use a single
    set of constants to index the array of power domains.
    
    Fixes: 0159ec670763 ("PM / Domains: Verify the PM domain is present when adding a provider")
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Cc: Jon Hunter <jonathanh@nvidia.com>
    Cc: Heiko Stuebner <heiko@sntech.de>

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -1581,6 +1581,8 @@ int of_genpd_add_provider_onecell(struct device_node *np,
 	mutex_lock(&gpd_list_lock);
 
 	for (i = 0; i < data->num_domains; i++) {
+		if (!data->domains[i])
+			continue;
 		if (!pm_genpd_present(data->domains[i]))
 			goto error;
 
