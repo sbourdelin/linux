c7f3b66579df4268b2a92f90dd4a54bb786f20e8
Alexander Shishkin <alexander.shishkin@linux.intel.com>
2016-09-15 16:29:43 +0300

    perf/x86: Tighten up the kernel_ip() check
    The kernel_ip() filter is used mostly by the DS/LBR code to look at the
    branch addresses, but Intel PT also uses it to validate the address
    filter offsets for kernel addresses, for which it is not sufficient:
    supplying something in bits 64:48 that's not a sign extension of the lower
    address bits (like 0xf00d000000000000) throws a #GP.
    
    In the interest of improving everybody's kernel address checks, this
    patch adds address validation to kernel_ip().
    
    Cc: stable@vger.kernel.org # 4.7
    Reported-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -783,7 +783,7 @@ static inline bool kernel_ip(unsigned long ip)
 #ifdef CONFIG_X86_32
 	return ip > PAGE_OFFSET;
 #else
-	return (long)ip < 0;
+	return (long)ip < 0 && virt_addr_valid(ip);
 #endif
 }
 
