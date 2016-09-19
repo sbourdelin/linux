9d01ed32638ec2e0389d81d4c6f9b06f48978152
Kyle Huey <me@kylehuey.com>
2016-09-18 21:17:13 -0700

    x86/arch_prctl/64 Use SYSCALL_DEFINE2 to define sys_arch_prctl
    Signed-off-by: Kyle Huey <khuey@kylehuey.com>

--- arch/x86/um/syscalls_64.c
+++ arch/x86/um/syscalls_64.c
@@ -72,7 +72,7 @@ long arch_prctl(struct task_struct *task, int code, unsigned long __user *addr)
 	return ret;
 }
 
-long sys_arch_prctl(int code, unsigned long addr)
+SYSCALL_DEFINE2(arch_prctl, int, code, unsigned long, addr)
 {
 	return arch_prctl(current, code, (unsigned long __user *) addr);
 }
