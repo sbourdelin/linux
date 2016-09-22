bc305cabc45e2ba83ccc1ee9a1ee70ee0fdabfe3
Matthew Wilcox <mawilcox@microsoft.com>
2016-09-22 11:53:35 -0700

    radix-tree: Fix optimisation problem
    When compiling the radix tree with -O2, GCC thinks it can optimise:
    
    	void *entry = parent->slots[offset];
    	int siboff = entry - parent->slots;
    	void *slot = parent->slots + siboff;
    
    into
    
    	void *slot = entry;
    
    Unfortunately, 'entry' is a tagged pointer, so this optimisation leads
    to getting an unaligned pointer back from radix_tree_lookup_slot().
    The test suite wasn't being compiled with optimisation, so we hadn't
    spotted it before now.  Change the test suite to compile with -O2, and
    fix the optimisation problem by passing 'entry' through entry_to_node()
    so gcc knows this isn't a plain pointer.

--- lib/radix-tree.c
+++ lib/radix-tree.c
@@ -105,7 +105,8 @@ static unsigned int radix_tree_descend(struct radix_tree_node *parent,
 
 #ifdef CONFIG_RADIX_TREE_MULTIORDER
 	if (radix_tree_is_internal_node(entry)) {
-		unsigned long siboff = get_slot_offset(parent, entry);
+		unsigned long siboff = get_slot_offset(parent,
+						(void **)entry_to_node(entry));
 		if (siboff < RADIX_TREE_MAP_SIZE) {
 			offset = siboff;
 			entry = rcu_dereference_raw(parent->slots[offset]);
