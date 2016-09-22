cfc5ce062311c976e8efbd3737bc35a6124b9373
Yannis Damigos <giannis.damigos@gmail.com>
2016-09-22 20:55:21 +0300

    staging:android:io: Fix multiple styling issues
    This patch fixes 1 error, 1 warning and 14 checks found by
    checkpatch.
    
    Signed-off-by: Yannis Damigos <giannis.damigos@gmail.com>

--- drivers/staging/android/ion/ion_of.c
+++ drivers/staging/android/ion/ion_of.c
@@ -26,17 +26,17 @@
 #include "ion_of.h"
 
 int ion_parse_dt_heap_common(struct device_node *heap_node,
-			struct ion_platform_heap *heap,
-			struct ion_of_heap *compatible)
+			     struct ion_platform_heap *heap,
+			     struct ion_of_heap *compatible)
 {
 	int i;
 
-	for (i = 0; compatible[i].name != NULL; i++) {
+	for (i = 0; compatible[i].name; i++) {
 		if (of_device_is_compatible(heap_node, compatible[i].compat))
 			break;
 	}
 
-	if (compatible[i].name == NULL)
+	if (!compatible[i].name)
 		return -ENODEV;
 
 	heap->id = compatible[i].heap_id;
@@ -47,33 +47,33 @@ int ion_parse_dt_heap_common(struct device_node *heap_node,
 	/* Some kind of callback function pointer? */
 
 	pr_info("%s: id %d type %d name %s align %lx\n", __func__,
-			heap->id, heap->type, heap->name, heap->align);
+		heap->id, heap->type, heap->name, heap->align);
 	return 0;
 }
 
 int ion_setup_heap_common(struct platform_device *parent,
-			struct device_node *heap_node,
-			struct ion_platform_heap *heap)
+			  struct device_node *heap_node,
+			  struct ion_platform_heap *heap)
 {
 	int ret = 0;
 
 	switch (heap->type) {
-		case ION_HEAP_TYPE_CARVEOUT:
-		case ION_HEAP_TYPE_CHUNK:
-			if (heap->base && heap->size)
-				return 0;
+	case ION_HEAP_TYPE_CARVEOUT:
+	case ION_HEAP_TYPE_CHUNK:
+		if (heap->base && heap->size)
+			return 0;
 
-			ret = of_reserved_mem_device_init(heap->priv);
-			break;
-		default:
-			break;
+		ret = of_reserved_mem_device_init(heap->priv);
+		break;
+	default:
+		break;
 	}
 
 	return ret;
 }
 
 struct ion_platform_data *ion_parse_dt(struct platform_device *pdev,
-					struct ion_of_heap *compatible)
+				       struct ion_of_heap *compatible)
 {
 	int num_heaps, ret;
 	const struct device_node *dt_node = pdev->dev.of_node;
@@ -88,13 +88,13 @@ struct ion_platform_data *ion_parse_dt(struct platform_device *pdev,
 		return ERR_PTR(-EINVAL);
 
 	heaps = devm_kzalloc(&pdev->dev,
-				sizeof(struct ion_platform_heap)*num_heaps,
-				GFP_KERNEL);
+			     sizeof(struct ion_platform_heap) * num_heaps,
+			     GFP_KERNEL);
 	if (!heaps)
 		return ERR_PTR(-ENOMEM);
 
 	data = devm_kzalloc(&pdev->dev, sizeof(struct ion_platform_data),
-				GFP_KERNEL);
+			    GFP_KERNEL);
 	if (!data)
 		return ERR_PTR(-ENOMEM);
 
@@ -106,7 +106,7 @@ struct ion_platform_data *ion_parse_dt(struct platform_device *pdev,
 			return ERR_PTR(ret);
 
 		heap_pdev = of_platform_device_create(node, heaps[i].name,
-							&pdev->dev);
+						      &pdev->dev);
 		if (!pdev)
 			return ERR_PTR(-ENOMEM);
 		heap_pdev->dev.platform_data = &heaps[i];
@@ -118,7 +118,6 @@ struct ion_platform_data *ion_parse_dt(struct platform_device *pdev,
 			goto out_err;
 		i++;
 	}
-
 
 	data->heaps = heaps;
 	data->nr = num_heaps;
@@ -155,14 +154,13 @@ static int rmem_ion_device_init(struct reserved_mem *rmem, struct device *dev)
 	heap->base = rmem->base;
 	heap->base = rmem->size;
 	pr_debug("%s: heap %s base %pa size %pa dev %p\n", __func__,
-			heap->name, &rmem->base, &rmem->size, dev);
+		 heap->name, &rmem->base, &rmem->size, dev);
 	return 0;
 }
 
 static void rmem_ion_device_release(struct reserved_mem *rmem,
-					struct device *dev)
+				    struct device *dev)
 {
-	return;
 }
 
 static const struct reserved_mem_ops rmem_dma_ops = {
@@ -181,5 +179,6 @@ static int __init rmem_ion_setup(struct reserved_mem *rmem)
 	rmem->ops = &rmem_dma_ops;
 	return 0;
 }
+
 RESERVEDMEM_OF_DECLARE(ion, "ion-region", rmem_ion_setup);
 #endif
