8e9df1674c7882349d3c14bcb264c8a1a502b279
H. Nikolaus Schaller <hns@goldelico.com>
2016-09-23 14:41:10 +0200

    drivers:input:tsc2007: send pendown and penup only once like ads7846(+tsc2046) driver does
    this should reduce unnecessary input events.
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/input/touchscreen/tsc2007.c
+++ drivers/input/touchscreen/tsc2007.c
@@ -94,6 +94,7 @@ struct tsc2007 {
 
 	wait_queue_head_t	wait;
 	bool			stopped;
+	bool			pendown;
 
 	int			(*get_pendown_state)(struct device *);
 	void			(*clear_penirq)(void);
@@ -254,9 +255,13 @@ static irqreturn_t tsc2007_soft_irq(int irq, void *handle)
 
 	dev_dbg(&ts->client->dev, "UP\n");
 
-	input_report_key(input, BTN_TOUCH, 0);
-	input_report_abs(input, ABS_PRESSURE, 0);
-	input_sync(input);
+	if (ts->pendown) {
+		input_report_key(input, BTN_TOUCH, 0);
+		input_report_abs(input, ABS_PRESSURE, 0);
+		input_sync(input);
+
+		ts->pendown = false;
+	}
 
 	if (ts->clear_penirq)
 		ts->clear_penirq();
