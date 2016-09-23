eea9f91d48d90c50eea1ef3d03048d979b8d89e5
Wang Nan <wangnan0@huawei.com>
2016-09-23 12:49:58 +0000

    perf tools: Extract kernel build option detector as utils
    Following commits will use builtin clang to compile BPF script.
    llvm__get_kbuild_opts() is extracted to help it detect kbuild dir
    and include options.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexei Starovoitov <ast@fb.com>
    Cc: He Kuang <hekuang@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/llvm-utils.h
+++ tools/perf/util/llvm-utils.h
@@ -50,4 +50,7 @@ int llvm__compile_bpf(const char *path, void **p_obj_buf, size_t *p_obj_buf_sz);
 
 /* This function is for test__llvm() use only */
 int llvm__search_clang(void);
+
+/* This function is reused by builtin clang support */
+void llvm__get_kbuild_opts(char **kbuild_dir, char **kbuild_include_opts);
 #endif
