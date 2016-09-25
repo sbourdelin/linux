6d8517f5b2138ed2aaba619d8e27f480803f329b
Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
2016-09-25 19:53:48 +0530

    powerpc: Add #defs for paca->soft_enabled flags
    Two #defs IRQ_DISABLE_LEVEL_NONE and IRQ_DISABLE_LEVEL_LINUX
    are added to be used when updating paca->soft_enabled.
    Replace the hardcoded values used when updating
    paca->soft_enabled with IRQ_DISABLE_MASK_* #def.
    No logic change.
    
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>

--- arch/powerpc/include/asm/exception-64s.h
+++ arch/powerpc/include/asm/exception-64s.h
@@ -408,7 +408,7 @@ label##_relon_hv:						\
 
 #define __SOFTEN_TEST(h, vec)						\
 	lbz	r10,PACASOFTIRQEN(r13);					\
-	cmpwi	r10,0;							\
+	cmpwi	r10,IRQ_DISABLE_MASK_LINUX;							\
 	li	r10,SOFTEN_VALUE_##vec;					\
 	beq	masked_##h##interrupt
 #define _SOFTEN_TEST(h, vec)	__SOFTEN_TEST(h, vec)
