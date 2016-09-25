d8eb9f0818898afb7bd38135d23ba481e8031da5
Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
2016-09-25 19:53:56 +0530

    powerpc:Add new kconfig IRQ_DEBUG_SUPPORT
    New Kconfig is added "CONFIG_IRQ_DEBUG_SUPPORT" to add warn_on
    to alert the invalid transitions. Also moved the code under
    the CONFIG_TRACE_IRQFLAGS in arch_local_irq_restore() to new Kconfig.
    
    Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>

--- arch/powerpc/kernel/irq.c
+++ arch/powerpc/kernel/irq.c
@@ -258,7 +258,7 @@ notrace void arch_local_irq_restore(unsigned long en)
 	 */
 	if (unlikely(irq_happened != PACA_IRQ_HARD_DIS))
 		__hard_irq_disable();
-#ifdef CONFIG_TRACE_IRQFLAGS
+#ifdef CONFIG_IRQ_DEBUG_SUPPORT
 	else {
 		/*
 		 * We should already be hard disabled here. We had bugs
@@ -269,7 +269,7 @@ notrace void arch_local_irq_restore(unsigned long en)
 		if (WARN_ON(mfmsr() & MSR_EE))
 			__hard_irq_disable();
 	}
-#endif /* CONFIG_TRACE_IRQFLAGS */
+#endif /* CONFIG_IRQ_DEBUG_SUPPORT */
 
 	soft_enabled_set(IRQ_DISABLE_MASK_LINUX);
 
