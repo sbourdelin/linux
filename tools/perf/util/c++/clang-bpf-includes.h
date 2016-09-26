490b01f3df800d5c036c5bdf2655b48f2a5accff
Wang Nan <wangnan0@huawei.com>
2016-09-26 07:27:09 +0000

    perf clang: Link BPF functions declaration into perf
    Use a shell script to generate BPF functions declarations from kernel
    source code, embedded the generated header into a C string. Following
    commits will utilizes clang's virtual file system to automatically
    include this header to all BPF scripts.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexei Starovoitov <ast@fb.com>
    Cc: He Kuang <hekuang@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>

---
+++ tools/perf/util/c++/clang-bpf-includes.h
@@ -0,0 +1,12 @@
+#ifndef CLANG_BPF_INCLUDS_H
+#define CLANG_BPF_INCLUDS_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char clang_builtin_bpf_funcs_str[];
+
+#ifdef __cplusplus
+}
+#endif
+#endif
