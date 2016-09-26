c4d84ce0bb27b417ce8b6924c9a3d9adf585a17b
Raju Lakkaraju <Raju.Lakkaraju@microsemi.com>
2016-09-26 19:18:25 +0530

    net: phy: Add Edge-rate driver for Microsemi PHYs.
    Hi,
    
    Thank you for review the code and valuable comments.
    Summary of changes:
            V1: Initial version of Edge-rate driver add by using IOCTL.
            V2: Changed edge-rate parameter to Device Tree with magic number.
            V3: Added Device Tree documentatoin and edge-rate parameter table.
                Added probe function initialize the vsc8531 private datastructure.
    Thanks,
    Raju.
    
    From: Raju Lakkaraju <Raju.Lakkaraju@microsemi.com>

---
+++ include/dt-bindings/net/mscc-phy-vsc8531.h
@@ -0,0 +1,23 @@
+/*
+ * Device Tree constants for Microsemi VSC8531 PHY
+ *
+ * Author: Nagaraju Lakkaraju
+ *
+ * License: Dual MIT/GPL
+ * Copyright (c) 2016 Microsemi Corporation
+ */
+
+#ifndef _DT_BINDINGS_MSCC_VSC8531_H
+#define _DT_BINDINGS_MSCC_VSC8531_H
+
+/* MAC interface Edge rate control pad */
+#define MSCC_EDGE_RATE_CNTL_SLOWEST      0
+#define MSCC_EDGE_RATE_CNTL_PLUS_1       1
+#define MSCC_EDGE_RATE_CNTL_PLUS_2       2
+#define MSCC_EDGE_RATE_CNTL_PLUS_3       3
+#define MSCC_EDGE_RATE_CNTL_PLUS_4       4
+#define MSCC_EDGE_RATE_CNTL_PLUS_5       5
+#define MSCC_EDGE_RATE_CNTL_PLUS_6       6
+#define MSCC_EDGE_RATE_CNTL_FASTEST      7
+
+#endif
