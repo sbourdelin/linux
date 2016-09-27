f71c074a486d9d367e92832b4471c5ab2354cda9
Lawrence Brakmo <brakmo@fb.com>
2016-09-27 09:06:29 -0700

    tcp: Change txhash on every SYN and RTO retransmit
    The current code changes txhash (flowlables) on every retransmitted
    SYN/ACK, but only after the 2nd retransmitted SYN and only after
    tcp_retries1 RTO retransmits.
    
    With this patch:
    1) txhash is changed with every SYN retransmist
    2) adds the option for the txhash to be changed before tcp_retries1
       RTO retransmits. A new sysctl tcp_rto_txhash_prob represents the
       probability that txhash will be changed. The default value is 0
       which maintains previous behavior and a value of 100 will always
       change it.
    
    Tested with packetdrill tests
    
    Signed-off-by: Lawrence Brakmo <brakmo@fb.com>

--- net/ipv4/tcp_timer.c
+++ net/ipv4/tcp_timer.c
@@ -192,6 +192,8 @@ static int tcp_write_timeout(struct sock *sk)
 			if (tp->syn_data && icsk->icsk_retransmits == 1)
 				NET_INC_STATS(sock_net(sk),
 					      LINUX_MIB_TCPFASTOPENACTIVEFAIL);
+		} else if (!tp->syn_data && !tp->syn_fastopen) {
+			sk_rethink_txhash(sk);
 		}
 		retry_until = icsk->icsk_syn_retries ? : net->ipv4.sysctl_tcp_syn_retries;
 		syn_set = true;
@@ -213,6 +215,11 @@ static int tcp_write_timeout(struct sock *sk)
 			tcp_mtu_probing(icsk, sk);
 
 			dst_negative_advice(sk);
+		} else if (sk->sk_txhash && sysctl_tcp_rto_txhash_prob > 0) {
+			u32 v = prandom_u32() % 100;
+
+			if (v < sysctl_tcp_rto_txhash_prob)
+				sk_set_txhash(sk);
 		}
 
 		retry_until = net->ipv4.sysctl_tcp_retries2;
