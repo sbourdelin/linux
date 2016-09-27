062546f47c2eae76b1e7fa7576055ec87e1f1381
Markus Elfring <elfring@users.sourceforge.net>
2016-09-27 15:26:51 +0200

    md/bitmap: Replace a kzalloc() call by kcalloc() in bitmap_resize()
    The script "checkpatch.pl" can point information out like the following.
    
    WARNING: Prefer kcalloc over kzalloc with multiply
    
    Thus fix the affected source code place.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- drivers/md/bitmap.c
+++ drivers/md/bitmap.c
@@ -2035,8 +2035,7 @@ int bitmap_resize(struct bitmap *bitmap, sector_t blocks,
 		return ret;
 
 	pages = DIV_ROUND_UP(chunks, PAGE_COUNTER_RATIO);
-
-	new_bp = kzalloc(pages * sizeof(*new_bp), GFP_KERNEL);
+	new_bp = kcalloc(pages, sizeof(*new_bp), GFP_KERNEL);
 	if (!new_bp) {
 		bitmap_file_unmap(&store);
 		return -ENOMEM;
