c0917cc2324a650266f31e6e4c84da580b490eef
Yijing Wang <wangyijing@huawei.com>
2016-09-27 11:16:04 +0800

    libsas: Alloc dynamic work to avoid missing sas events
    Now libsas hotplug work is static, LLDD driver queue
    the hotplug work into shost->work_q. If LLDD driver
    burst post lots hotplug event to libsas, the hotplug
    events may pending in the workqueue like
    
    shost->workq
    tail | PHYE_LOSS_OF_SIGNAL  | PORTE_BYTES_DMAED | head
    
    In this case, if a new PORTE_BYTES_DMAED event coming,
    it would be lost, because we can not queue a work which
    is already pending in the workqueue, also libsas has a
    pending bit to avoid queue the same event.
    
    The lost hotplug event make something confusing, e.g.
    we have sas disks connected hardware, but we can not
    found them in kernel.
    
    This patch remove the static defined hotplug work,
    and use dynamic work to avoid missing hotplug events.
    
    Signed-off-by: Yijing Wang <wangyijing@huawei.com>
    Signed-off-by: Yousong He <heyousong@huawei.com>
    Signed-off-by: Qilin Chen <chenqilin2@huawei.com>

--- drivers/scsi/libsas/sas_port.c
+++ drivers/scsi/libsas/sas_port.c
@@ -261,9 +261,8 @@ void sas_porte_bytes_dmaed(struct work_struct *work)
 	struct asd_sas_event *ev = to_asd_sas_event(work);
 	struct asd_sas_phy *phy = ev->phy;
 
-	clear_bit(PORTE_BYTES_DMAED, &phy->port_events_pending);
-
 	sas_form_port(phy);
+	kfree(ev);
 }
 
 void sas_porte_broadcast_rcvd(struct work_struct *work)
@@ -273,14 +272,13 @@ void sas_porte_broadcast_rcvd(struct work_struct *work)
 	unsigned long flags;
 	u32 prim;
 
-	clear_bit(PORTE_BROADCAST_RCVD, &phy->port_events_pending);
-
 	spin_lock_irqsave(&phy->sas_prim_lock, flags);
 	prim = phy->sas_prim;
 	spin_unlock_irqrestore(&phy->sas_prim_lock, flags);
 
 	SAS_DPRINTK("broadcast received: %d\n", prim);
 	sas_discover_event(phy->port, DISCE_REVALIDATE_DOMAIN);
+	kfree(ev);
 }
 
 void sas_porte_link_reset_err(struct work_struct *work)
@@ -288,9 +286,8 @@ void sas_porte_link_reset_err(struct work_struct *work)
 	struct asd_sas_event *ev = to_asd_sas_event(work);
 	struct asd_sas_phy *phy = ev->phy;
 
-	clear_bit(PORTE_LINK_RESET_ERR, &phy->port_events_pending);
-
 	sas_deform_port(phy, 1);
+	kfree(ev);
 }
 
 void sas_porte_timer_event(struct work_struct *work)
@@ -298,9 +295,8 @@ void sas_porte_timer_event(struct work_struct *work)
 	struct asd_sas_event *ev = to_asd_sas_event(work);
 	struct asd_sas_phy *phy = ev->phy;
 
-	clear_bit(PORTE_TIMER_EVENT, &phy->port_events_pending);
-
 	sas_deform_port(phy, 1);
+	kfree(ev);
 }
 
 void sas_porte_hard_reset(struct work_struct *work)
@@ -308,9 +304,8 @@ void sas_porte_hard_reset(struct work_struct *work)
 	struct asd_sas_event *ev = to_asd_sas_event(work);
 	struct asd_sas_phy *phy = ev->phy;
 
-	clear_bit(PORTE_HARD_RESET, &phy->port_events_pending);
-
 	sas_deform_port(phy, 1);
+	kfree(ev);
 }
 
 /* ---------- SAS port registration ---------- */
@@ -353,3 +348,11 @@ void sas_unregister_ports(struct sas_ha_struct *sas_ha)
 			sas_deform_port(sas_ha->sas_phy[i], 0);
 
 }
+
+const work_func_t sas_port_event_fns[PORT_NUM_EVENTS] = {
+	[PORTE_BYTES_DMAED] = sas_porte_bytes_dmaed,
+	[PORTE_BROADCAST_RCVD] = sas_porte_broadcast_rcvd,
+	[PORTE_LINK_RESET_ERR] = sas_porte_link_reset_err,
+	[PORTE_TIMER_EVENT] = sas_porte_timer_event,
+	[PORTE_HARD_RESET] = sas_porte_hard_reset,
+};
