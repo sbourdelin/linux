a912958e0f8e090820955e814d8383f85444f8c3
Paolo Bonzini <pbonzini@redhat.com>
2016-09-27 23:20:11 +0200

    kvm: x86: avoid atomic operations on APICv vmentry
    On some benchmarks (e.g. netperf with ioeventfd disabled), APICv
    posted interrupts turn out to be slower than interrupt injection via
    KVM_REQ_EVENT.
    
    This patch optimizes a bit the IRR update, avoiding expensive atomic
    operations in the common case where PI.ON=0 at vmentry or the PIR vector
    is mostly zero.  This saves around 20 cycles per vmexit, as
    measured by kvm-unit-tests' inl_from_qemu test (20 runs):
    
                  | enable_apicv=1  |  enable_apicv=0
                  | mean     stdev  |  mean     stdev
        ----------|-----------------|------------------
        before    | 5826     32.65  |  5765     47.09
        after     | 5809     43.42  |  5777     77.02
    
    Of course, any change in the right column is just placebo effect. :)
    The savings are bigger if interrupts are frequent.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -4824,7 +4824,8 @@ static void vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
 
-	if (!pi_test_and_clear_on(&vmx->pi_desc))
+	if (!pi_test_on(&vmx->pi_desc) ||
+	    !pi_test_and_clear_on(&vmx->pi_desc))
 		return;
 
 	kvm_apic_update_irr(vcpu, vmx->pi_desc.pir);
