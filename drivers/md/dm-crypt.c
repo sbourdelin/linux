6af5b2d48a956c68791e247e7d1feafaf4d85d89
Markus Elfring <elfring@users.sourceforge.net>
2016-09-28 14:05:25 +0200

    md/dm-crypt: Reduce the scope for a variable in crypt_alloc_tfms()
    Move the definition for the variable "err" (including its declaration)
    into an if branch (so that the corresponding setting will only be performed
    if a call of the function "crypto_alloc_skcipher" failed as before
    this refactoring).
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1451,7 +1451,6 @@ static void crypt_free_tfms(struct crypt_config *cc)
 static int crypt_alloc_tfms(struct crypt_config *cc, char *ciphermode)
 {
 	unsigned i;
-	int err;
 
 	cc->tfms = kcalloc(cc->tfms_count, sizeof(*cc->tfms), GFP_KERNEL);
 	if (!cc->tfms)
@@ -1460,7 +1459,8 @@ static int crypt_alloc_tfms(struct crypt_config *cc, char *ciphermode)
 	for (i = 0; i < cc->tfms_count; i++) {
 		cc->tfms[i] = crypto_alloc_skcipher(ciphermode, 0, 0);
 		if (IS_ERR(cc->tfms[i])) {
-			err = PTR_ERR(cc->tfms[i]);
+			int err = PTR_ERR(cc->tfms[i]);
+
 			crypt_free_tfms(cc);
 			return err;
 		}
