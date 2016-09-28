eadcc6815dfa036762823eb540f11b4000842b21
Matt Ranostay <mranostay@gmail.com>
2016-09-28 12:55:11 -0700

    power: power_supply: add new property POWER_SUPPLY_PROP_POLL_INTERVAL
    Add new POWER_SUPPLY_PROP_POLL_INTERVAL property to display/set the
    update interval in seconds of the device.
    
    Cc: Sebastian Reichel <sre@kernel.org>
    Cc: Tony Lindgren <tony@atomide.com
    Signed-off-by: Matt Ranostay <matt@ranostay.consulting>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -198,6 +198,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(scope),
 	POWER_SUPPLY_ATTR(charge_term_current),
 	POWER_SUPPLY_ATTR(calibrate),
+	POWER_SUPPLY_ATTR(poll_interval),
 	/* Properties of type `const char *' */
 	POWER_SUPPLY_ATTR(model_name),
 	POWER_SUPPLY_ATTR(manufacturer),
