120093b8aa54a80561a4f236ad5d4b913ec65f3d
Michal Hocko <mhocko@suse.com>
2016-10-04 10:12:15 +0200

    mm, compaction: allow compaction for GFP_NOFS requests
    compaction has been disabled for GFP_NOFS and GFP_NOIO requests since
    the direct compaction was introduced by 56de7263fcf3 ("mm: compaction:
    direct compact when a high-order allocation fails"). The main reason
    is that the migration of page cache pages might recurse back to fs/io
    layer and we could potentially deadlock. This is overly conservative
    because all the anonymous memory is migrateable in the GFP_NOFS context
    just fine.  This might be a large portion of the memory in many/most
    workkloads.
    
    Remove the GFP_NOFS restriction and make sure that we skip all fs pages
    (those with a mapping) while isolating pages to be migrated. We cannot
    consider clean fs pages because they might need a metadata update so
    only isolate pages without any mapping for nofs requests.
    
    The effect of this patch will be probably very limited in many/most
    workloads because higher order GFP_NOFS requests are quite rare,
    although different configurations might lead to very different results
    as GFP_NOFS usage is rather unleashed (e.g. I had hard time to trigger
    any with my setup). But still there shouldn't be any strong reason to
    completely back off and do nothing in that context. In the worst case
    we just skip parts of the block with fs pages. This might be still
    sufficient to make a progress for small orders.
    
    Signed-off-by: Michal Hocko <mhocko@suse.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -834,6 +834,13 @@ isolate_migratepages_block(struct compact_control *cc, unsigned long low_pfn,
 		    page_count(page) > page_mapcount(page))
 			goto isolate_fail;
 
+		/*
+		 * Only allow to migrate anonymous pages in GFP_NOFS context
+		 * because those do not depend on fs locks.
+		 */
+		if (!(cc->gfp_mask & __GFP_FS) && page_mapping(page))
+			goto isolate_fail;
+
 		/* If we already hold the lock, we can skip some rechecking */
 		if (!locked) {
 			locked = compact_trylock_irqsave(zone_lru_lock(zone),
@@ -1696,14 +1703,16 @@ enum compact_result try_to_compact_pages(gfp_t gfp_mask, unsigned int order,
 		unsigned int alloc_flags, const struct alloc_context *ac,
 		enum compact_priority prio)
 {
-	int may_enter_fs = gfp_mask & __GFP_FS;
 	int may_perform_io = gfp_mask & __GFP_IO;
 	struct zoneref *z;
 	struct zone *zone;
 	enum compact_result rc = COMPACT_SKIPPED;
 
-	/* Check if the GFP flags allow compaction */
-	if (!may_enter_fs || !may_perform_io)
+	/*
+	 * Check if the GFP flags allow compaction - GFP_NOIO is really
+	 * tricky context because the migration might require IO and
+	 */
+	if (!may_perform_io)
 		return COMPACT_SKIPPED;
 
 	trace_mm_compaction_try_to_compact_pages(order, gfp_mask, prio);
