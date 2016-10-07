b3027c1ba6db08294b39c04d1d0aa4a745d9775d
Guenter Roeck <groeck@juniper.net>
2016-10-07 18:15:24 +0300

    uapi: jnx: Export board types to user space
    Board type definitions are part of the kernel/user ABI and
    thus need to be exported.
    
    Signed-off-by: Guenter Roeck <groeck@juniper.net>
    Signed-off-by: Mohammad Kamil <mkamil@juniper.net>
    [Ported from Juniper kernel]
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

---
+++ include/uapi/linux/jnx/jnx-subsys.h
@@ -0,0 +1,38 @@
+/*
+ * Juniper board type definitions
+ *
+ * These definitions are needed by user-space applications.
+ *
+ * Copyright (C) 2012-2014 Juniper Networks. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _UAPI_JNX_SUBSYS_H
+#define _UAPI_JNX_SUBSYS_H
+
+/*
+ * Juniper Board Type Definitions
+ */
+#define JNX_BOARD_TYPE_UNKNOWN	0
+#define JNX_BOARD_TYPE_RE	1
+#define JNX_BOARD_TYPE_FPC	2
+#define JNX_BOARD_TYPE_SPMB	3
+#define JNX_BOARD_TYPE_CB	4
+#define JNX_BOARD_TYPE_PS	5
+#define JNX_BOARD_TYPE_FAN	6
+#define JNX_BOARD_TYPE_FPM	7
+#define JNX_BOARD_TYPE_CG	8
+#define JNX_BOARD_TYPE_MIDPLANE	9
+#define JNX_BOARD_TYPE_PIC	10
+#define JNX_BOARD_TYPE_SIB	11
+
+#endif /* _UAPI_JNX_SUBSYS_H */
