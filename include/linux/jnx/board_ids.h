4a0ce31b1a9026ac9c5f09bb1a1e16644a5af7ea
Rajat Jain <rajatjain@juniper.net>
2016-10-07 18:15:25 +0300

    jnx: Introduce board_ids.h
    Introduce header file to contain the Juniper Assembly IDs.
    
    Signed-off-by: Mohammad Kamil <mkamil@juniper.net>
    Signed-off-by: Rajat Jain <rajatjain@juniper.net>
    Signed-off-by: Debjit Ghosh <dghosh@juniper.net>
    Signed-off-by: Guenter Roeck <groeck@juniper.net>
    [Ported from Juniper kernel]
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

---
+++ include/linux/jnx/board_ids.h
@@ -0,0 +1,31 @@
+/*
+ * Juniper Assembly ID(s) - for Juniper Boards
+ *
+ * Rajat Jain <rajatjain@juniper.net>
+ * Copyright (c) 2014 Juniper Networks
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __JNX_BOARD_IDS__
+#define __JNX_BOARD_IDS__
+
+#define JNX_ID_SNG_CB		0x09B3	/* Sangria Control Board */
+#define JNX_ID_SNG_PMB		0x09B7	/* Sangria CB/FPC PMB Board */
+#define JNX_ID_SNG_VDV_BASE_P2	0x09BC	/* Sangria Vaudville FPC Base P2 */
+#define JNX_ID_HENDRICKS_FPC_P2	0x0B9B	/* Hendricks FPC (new Serdes added) */
+#define JNX_ID_HENDRICKS_CB	0x0BA8  /* Hendricks Control Board */
+#define JNX_ID_GLD_2T_FPC	0x0BF9	/* Gladiator 2T FPC */
+#define JNX_ID_GLD_3T_FPC	0x0BFA	/* Gladiator 3T FPC */
+#define JNX_ID_POLARIS_RCB	0x0C09	/* Polaris RCB */
+#define JNX_ID_POLARIS_MLC	0x0C0A	/* Polaris Mono Line Card */
+#define JNX_ID_POLARIS_RCB_P2	0x0C51	/* Polaris RCB P2 */
+#define JNX_ID_OMEGA_RCB	0x0C6B	/* Omega RCB */
+
+#endif /* __JNX_BOARD_IDS__ */
