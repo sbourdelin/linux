edbcc92d1699c17ea331afeecba8a3106c6f59e2
Michael Braun <michael-dev@fami-braun.de>
2016-10-10 19:12:22 +0200

    cfg80211: configure multicast to unicast for AP interfaces
    This add a userspace toggle to configure multicast to unicast.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>
    
    --
    v6:
     - clarify documentation
     - fix policy for NL80211_ATTR_MULTICAST_TO_UNICAST_ENABLED

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -599,6 +599,17 @@
  *
  * @NL80211_CMD_SET_WDS_PEER: Set the MAC address of the peer on a WDS interface.
  *
+ * @NL80211_CMD_SET_MULTICAST_TO_UNICAST: Configure if this AP should perform
+ *      multicast to unicast conversion. When enabled, all multicast packets
+ *      with ethertype ARP, IPv4 or IPv6 (possibly within an 802.1q header)
+ *      will be sent out to each station once with the destination (multicast)
+ *      mac address replaced by the stations mac address.
+ *      This can only be toggled per BSS. Configure this on an interface of
+ *      type %NL80211_IFTYPE_AP. It applies to all its vlans interfaces
+ *      (%NL80211_IFTYPE_AP_VLAN), except for those in 4addr (WDS) mode.
+ *      If %NL80211_ATTR_MULTICAST_TO_UNICAST_ENABLED is not present with this
+ *      command, the feature is disabled.
+ *
  * @NL80211_CMD_JOIN_MESH: Join a mesh. The mesh ID must be given, and initial
  *	mesh config parameters may be given.
  * @NL80211_CMD_LEAVE_MESH: Leave the mesh network -- no special arguments, the
@@ -1025,6 +1036,8 @@ enum nl80211_commands {
 	NL80211_CMD_WIPHY_REG_CHANGE,
 
 	NL80211_CMD_ABORT_SCAN,
+
+	NL80211_CMD_SET_MULTICAST_TO_UNICAST,
 
 	/* add new commands above here */
 
@@ -1867,6 +1880,9 @@ enum nl80211_commands {
  * @NL80211_ATTR_MESH_PEER_AID: Association ID for the mesh peer (u16). This is
  *	used to pull the stored data for mesh peer in power save state.
  *
+ * @NL80211_ATTR_MULTICAST_TO_UNICAST_ENABLED: Multicast packets should be
+ *      send out as unicast to all stations.
+ *
  * @NUM_NL80211_ATTR: total number of nl80211_attrs available
  * @NL80211_ATTR_MAX: highest attribute number currently defined
  * @__NL80211_ATTR_AFTER_LAST: internal use
@@ -2260,6 +2276,8 @@ enum nl80211_attrs {
 	NL80211_ATTR_MEASUREMENT_DURATION_MANDATORY,
 
 	NL80211_ATTR_MESH_PEER_AID,
+
+	NL80211_ATTR_MULTICAST_TO_UNICAST_ENABLED,
 
 	/* add attributes here, update the policy in nl80211.c */
 
