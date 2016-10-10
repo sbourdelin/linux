5683d5866be77638da285cc23c72c3e48d4fbf2a
Michael Braun <michael-dev@fami-braun.de>
2016-10-10 19:12:23 +0200

    mac80211: multicast to unicast conversion
    This patch adds support for sending multicast data packets with ARP, IPv4
    and IPv6 payload (possible 802.1q tagged) as 802.11 unicast frames to all
    stations.
    
    IEEE 802.11 multicast has well known issues, among them:
     1. packets are not acked and hence not retransmitted, resulting in
        decreased reliablity
     2. packets are send at low rate, increasing time required on air
    
    When used with AP_VLAN, there is another disadvantage:
     3. all stations in the BSS are woken up, regardsless of their AP_VLAN
        assignment.
    
    By doing multicast to unicast conversion, all three issus are solved.
    
    IEEE802.11-2012 proposes directed multicast service (DMS) using A-MSDU
    frames and a station initiated control protocol. It has the advantage that
    the station can recover the destination multicast mac address, but it is
    not backward compatible with non QOS stations and does not enable the
    administrator of a BSS to force this mode of operation within a BSS.
    Additionally, it would require both the ap and the station to implement
    the control protocol, which is optional on both ends. Furthermore, I've
    seen a few mobile phone stations locally that indicate qos support but
    won't complete DHCP if their broadcasts are encapsulated as A-MSDU. Though
    they work fine with this series approach.
    
    This patch therefore does not opt to implement DMS but instead just
    replicates the packet and changes the destination address. As this works
    fine with ARP, IPv4 and IPv6, it is limited to these protocols and normal
    802.11 multicast frames are send out for all other payload protocols.
    
    There is a runtime toggle to enable multicast conversion in a per-bss
    fashion.
    
    When there is only a single station assigned to the AP_VLAN interface, no
    packet replication will occur. 4addr mode of operation is unchanged.
    
    This change opts for iterating all BSS stations for finding the stations
    assigned to this AP/AP_VLAN interface, as there currently is no per
    AP_VLAN list to iterate and multicast packets are expected to be few.
    If needed, such a list could be added later.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>
    
    --
    v5:
      - rename bss->unicast to bss->multicast_to_unicast
      - access sdata->bss only after checking iftype
    v4:
      - rename MULTICAST_TO_UNICAST to MULTICAST_TO_UNICAST
    v3: fix compile error for trace.h
    v2: add nl80211 toggle
        rename tx_dnat to change_da
        change int to bool unicast

--- net/mac80211/debugfs_netdev.c
+++ net/mac80211/debugfs_netdev.c
@@ -487,6 +487,8 @@ static ssize_t ieee80211_if_fmt_num_buffered_multicast(
 }
 IEEE80211_IF_FILE_R(num_buffered_multicast);
 
+IEEE80211_IF_FILE(multicast_to_unicast, u.ap.multicast_to_unicast, HEX);
+
 /* IBSS attributes */
 static ssize_t ieee80211_if_fmt_tsf(
 	const struct ieee80211_sub_if_data *sdata, char *buf, int buflen)
@@ -642,6 +644,7 @@ static void add_ap_files(struct ieee80211_sub_if_data *sdata)
 	DEBUGFS_ADD(dtim_count);
 	DEBUGFS_ADD(num_buffered_multicast);
 	DEBUGFS_ADD_MODE(tkip_mic_test, 0200);
+	DEBUGFS_ADD_MODE(multicast_to_unicast, 0600);
 }
 
 static void add_vlan_files(struct ieee80211_sub_if_data *sdata)
