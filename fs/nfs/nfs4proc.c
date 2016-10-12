f3a360610c4de1131ba461dbd3b2b19f47bbee1c
NeilBrown <neilb@suse.com>
2016-10-12 13:39:18 +1100

    NFSv4: change nfs4_select_rw_stateid to take a lock_context inplace of lock_owner
    The only time that a lock_context is not available is in setattr.
    In this case, we want to find a lock context relevant to the process if there is one.
    The fallback can easily be handled at a lower level.
    
    So change nfs4_select_rw_stateid to take a lock_context, passing NULL from _nfs4_do_setattr.
    nfs4_copy_lock_state() also now takes a lock_context, and falls back to searching
    for "owner == current->files" if not lock_context is given.
    
    Note that nfs4_set_rw_stateid is *always* passed a non-NULL l_ctx, so the
    fact that we remove the NULL test there does not change correctness.
    
    This change is preparation for correctly support flock stateids.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -2764,12 +2764,9 @@ static int _nfs4_do_setattr(struct inode *inode,
 	if (nfs4_copy_delegation_stateid(inode, fmode, &arg->stateid, &delegation_cred)) {
 		/* Use that stateid */
 	} else if (truncate && state != NULL) {
-		struct nfs_lockowner lockowner = {
-			.l_owner = current->files,
-		};
 		if (!nfs4_valid_open_stateid(state))
 			return -EBADF;
-		if (nfs4_select_rw_stateid(state, FMODE_WRITE, &lockowner,
+		if (nfs4_select_rw_stateid(state, FMODE_WRITE, NULL,
 				&arg->stateid, &delegation_cred) == -EIO)
 			return -EBADF;
 	} else
@@ -4362,11 +4359,7 @@ int nfs4_set_rw_stateid(nfs4_stateid *stateid,
 		const struct nfs_lock_context *l_ctx,
 		fmode_t fmode)
 {
-	const struct nfs_lockowner *lockowner = NULL;
-
-	if (l_ctx != NULL)
-		lockowner = &l_ctx->lockowner;
-	return nfs4_select_rw_stateid(ctx->state, fmode, lockowner, stateid, NULL);
+	return nfs4_select_rw_stateid(ctx->state, fmode, l_ctx, stateid, NULL);
 }
 EXPORT_SYMBOL_GPL(nfs4_set_rw_stateid);
 
