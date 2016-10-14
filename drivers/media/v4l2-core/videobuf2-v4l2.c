da8745793a369dfc334880e0552381f797117a24
Pawel Osciak <posciak@chromium.org>
2016-10-14 14:08:14 +0200

    vb2: Add support for use_dma_bidirectional queue flag
    When this flag is set for CAPTURE queues by the driver on calling
    vb2_queue_init(), it forces the buffers on the queue to be
    allocated/mapped with DMA_BIDIRECTIONAL direction flag, instead of
    DMA_FROM_DEVICE. This allows the device not only to write to the
    buffers, but also read out from them. This may be useful e.g. for codec
    hardware, which may be using CAPTURE buffers as reference to decode
    other buffers.
    
    This flag is ignored for OUTPUT queues, as we don't want to allow HW to
    be able to write to OUTPUT buffers.
    
    Signed-off-by: Pawel Osciak <posciak@chromium.org>
    Tested-by: Pawel Osciak <posciak@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Signed-off-by: Thierry Escande <thierry.escande@collabora.com>

--- drivers/media/v4l2-core/videobuf2-v4l2.c
+++ drivers/media/v4l2-core/videobuf2-v4l2.c
@@ -659,8 +659,12 @@ int vb2_queue_init(struct vb2_queue *q)
 	 * queues will always initialize waiting_for_buffers to false.
 	 */
 	q->quirk_poll_must_check_waiting_for_buffers = true;
-	q->dma_dir = V4L2_TYPE_IS_OUTPUT(q->type)
-		   ? DMA_TO_DEVICE : DMA_FROM_DEVICE;
+
+	if (V4L2_TYPE_IS_OUTPUT(q->type))
+		q->dma_dir = DMA_TO_DEVICE;
+	else
+		q->dma_dir = q->use_dma_bidirectional
+			   ? DMA_BIDIRECTIONAL : DMA_FROM_DEVICE;
 
 	return vb2_core_queue_init(q);
 }
