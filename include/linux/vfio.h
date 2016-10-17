475e91b14013f98fe2c0cc8f447825a4fc533d81
Kirti Wankhede <kwankhede@nvidia.com>
2016-10-18 02:52:07 +0530

    vfio: Introduce vfio_set_irqs_validate_and_prepare()
    Vendor driver using mediated device framework would use same mechnism to
    validate and prepare IRQs. Introducing this function to reduce code
    replication in multiple drivers.
    
    Signed-off-by: Kirti Wankhede <kwankhede@nvidia.com>
    Signed-off-by: Neo Jia <cjia@nvidia.com>
    Change-Id: Ie201f269dda0713ca18a07dc4852500bd8b48309

--- include/linux/vfio.h
+++ include/linux/vfio.h
@@ -112,6 +112,10 @@ extern int vfio_info_add_capability(struct vfio_region_info *info,
 				    struct vfio_info_cap *caps,
 				    int cap_type_id, void *cap_type);
 
+extern int vfio_set_irqs_validate_and_prepare(struct vfio_irq_set *hdr,
+					      int num_irqs, int max_irq_type,
+					      size_t *data_size);
+
 struct pci_dev;
 #ifdef CONFIG_EEH
 extern void vfio_spapr_pci_eeh_open(struct pci_dev *pdev);
