78ba05f1e55e02c8eac6c57139ea6a9436f272b1
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2016-10-19 00:40:41 +0900

    printk: rename nmi.c and exported api
    A preparation patch for printk_safe work. No functional change.
    - rename nmi.c to print_safe.c
    - rename exported functions to have `safe' prefix.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -644,11 +644,11 @@ void handle_IPI(int ipinr, struct pt_regs *regs)
 		break;
 
 	case IPI_CPU_BACKTRACE:
-		printk_nmi_enter();
+		printk_safe_nmi_enter();
 		irq_enter();
 		nmi_cpu_backtrace(regs);
 		irq_exit();
-		printk_nmi_exit();
+		printk_safe_nmi_exit();
 		break;
 
 	default:
