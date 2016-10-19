c5932c5f8386768140a1425f04a1469054b3c5d3
Tom Herbert <tom@herbertland.com>
2016-10-19 16:03:06 -0700

    udp: UDP flow dissector
    Add infrastructure for performing per protocol flow dissection and
    support flow dissection in UDP payloads (e.g. flow dissection on a
    UDP encapsulated tunnel.
    
    The per protocol flow dissector is called by flow_dissect function
    in the offload_callbacks of a protocol. The arguments of this function
    include the necessary information to do flow dissection as derived
    from __skb_flow_dissect which is where the callback is intended to be
    called from. There are return codes from the callback in the form
    FLOW_DIS_RET_* that indicate the result. FLOW_DIS_RET_IPPROTO
    means that the payload should be dissected as an IP proto, the
    specific protocol is returned in a pointer argument. Likewise,
    FLOW_DIS_RET_PROTO indicate the payload should be processed as
    an ethertype which is returned in another argument.
    
    A case for IPPROTO_UDP was added to __skb_flow_dissect. Since
    UDP flow dissector involves a relatively expensive socket lookup
    there is a static key check first to see if there are any sockets
    that have enabled flow dissection. After this check, the offload
    ops for UDP for either IPv4 or IPv6 is considered. If the
    flow_dissect function is it is called. Upon return the result
    is processed (pass, out_bad, process as IP protocol, process
    as ethertype). Note that if the result indicates a protocol must
    be processed it is expected that nhoff has been updated to the
    encapsulated protocol header.
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2197,6 +2197,11 @@ struct offload_callbacks {
 	struct sk_buff		**(*gro_receive)(struct sk_buff **head,
 						 struct sk_buff *skb);
 	int			(*gro_complete)(struct sk_buff *skb, int nhoff);
+	int			(*flow_dissect)(const struct sk_buff *skb,
+		void *data, int hlen,
+		int *nhoff, u8 *ip_proto,
+		__be16 *proto,
+		const struct flow_dissector_key_addrs *key_addrs);
 };
 
 struct packet_offload {
