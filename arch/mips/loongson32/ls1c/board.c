465694ffe6c11d64c283dd90f41dc45693bdfd73
Yang Ling <gnaygnil@gmail.com>
2016-10-21 13:53:58 +0800

    MIPS: loongson1: Add watchdog support for Loongson1 board
    The patch adds watchdog support for Loongson1 board.
    
    Signed-off-by: Yang Ling <gnaygnil@gmail.com>

--- arch/mips/loongson32/ls1c/board.c
+++ arch/mips/loongson32/ls1c/board.c
@@ -13,6 +13,7 @@ static struct platform_device *ls1c_platform_devices[] __initdata = {
 	&ls1x_uart_pdev,
 	&ls1x_eth0_pdev,
 	&ls1x_rtc_pdev,
+	&ls1x_wdt_pdev,
 };
 
 static int __init ls1c_platform_init(void)
