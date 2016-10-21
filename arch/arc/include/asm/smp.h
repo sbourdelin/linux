9430056860be2518e915f67418b015440db2d952
Yuriy Kolerov <yuriy.kolerov@synopsys.com>
2016-10-22 01:15:27 +0300

    ARC: SMP: Use "unsigned virq" in smp_ipi_irq_setup instead of "signed irq"
    This function takes a cpu number and a virq number and registers an
    appropriate handler per cpu. However smp_ipi_irq_setup is incorrectly
    used in several places of ARC platform code - hwirq is passed instead
    of virq. This patch is intended to clarify declaration of virq argument
    to prevent passing of hwirq instead of virq in future.
    
    Signed-off-by: Yuriy Kolerov <yuriy.kolerov@synopsys.com>

--- arch/arc/include/asm/smp.h
+++ arch/arc/include/asm/smp.h
@@ -37,9 +37,9 @@ extern const char *arc_platform_smp_cpuinfo(void);
  * API expected BY platform smp code (FROM arch smp code)
  *
  * smp_ipi_irq_setup:
- *	Takes @cpu and @irq to which the arch-common ISR is hooked up
+ *	Takes @cpu and @virq to which the arch-common ISR is hooked up
  */
-extern int smp_ipi_irq_setup(int cpu, int irq);
+extern int smp_ipi_irq_setup(int cpu, unsigned int virq);
 
 /*
  * struct plat_smp_ops	- SMP callbacks provided by platform to ARC SMP
