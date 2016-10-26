c498b9b3b6f3bf7a4aaf9dd489fc3421df00d8dd
Rodrigo Vivi <rodrigo.vivi@intel.com>
2016-10-26 11:22:00 -0700

    drm/i915/fbc: Assume maximum 8mb of stolen is used for gen8+
    Since Broxton has same FBC block as BDW+ let's assume it also
    don't have access to the stolen usable range.
    
    FBC is currently not saving power on Broxton and I believe
    the compression threshold is limited to 1x.
    
    Cc: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Cc: Marc Herbert <marc.herbert@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/intel_fbc.c
+++ drivers/gpu/drm/i915/intel_fbc.c
@@ -530,12 +530,11 @@ static int find_compression_threshold(struct drm_i915_private *dev_priv,
 	int ret;
 	u64 end;
 
-	/* The FBC hardware for BDW/SKL doesn't have access to the stolen
+	/* The FBC hardware for gen8+ doesn't have access to the stolen
 	 * reserved range size, so it always assumes the maximum (8mb) is used.
 	 * If we enable FBC using a CFB on that memory range we'll get FIFO
 	 * underruns, even if that range is not reserved by the BIOS. */
-	if (IS_BROADWELL(dev_priv) ||
-	    IS_SKYLAKE(dev_priv) || IS_KABYLAKE(dev_priv))
+	if (INTEL_INFO(dev_priv)->gen <= 8)
 		end = ggtt->stolen_size - 8 * 1024 * 1024;
 	else
 		end = ggtt->stolen_usable_size;
