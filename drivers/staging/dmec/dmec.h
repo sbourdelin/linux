d1dbc0e8147a59533b8c3dabcfef4fef09a8dfc4
Zahari Doychev <zahari.doychev@linux.com>
2016-10-27 12:47:13 +0200

    i2c-dmec: add i2c bus support for dmec
    This is support for the i2c bus functionality of the Data Modul embedded
    controllers.
    
    Signed-off-by: Zahari Doychev <zahari.doychev@linux.com>

--- drivers/staging/dmec/dmec.h
+++ drivers/staging/dmec/dmec.h
@@ -1,6 +1,15 @@
 #ifndef _LINUX_MFD_DMEC_H
 #define _LINUX_MFD_DMEC_H
 
+struct dmec_i2c_platform_data {
+	u32 reg_shift; /* register offset shift value */
+	u32 reg_io_width; /* register io read/write width */
+	u32 clock_khz; /* input clock in kHz */
+	bool big_endian; /* registers are big endian */
+	u8 num_devices; /* number of devices in the devices list */
+	struct i2c_board_info const *devices; /* devices connected to the bus */
+};
+
 struct regmap *dmec_get_regmap(struct device *dev);
 
 #endif
