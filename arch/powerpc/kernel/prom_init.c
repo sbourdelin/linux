dbad1e8ee474a6f0266b92bf89b1139e4f213697
Michael Bringmann <mwb@linux.vnet.ibm.com>
2016-10-27 09:25:55 -0500

    powerpc: Enable support for new DRC devtree properties
    prom_init.c: Enable support for new DRC device tree properties
    "ibm,drc-info" and "ibm,dynamic-memory-v2" in initial handshake
    between the Linux kernel and the front end processor.
    
    [V2: Revise constant names.]
    [V3: No change.]
    [V4: Update comments]
    [V5: Resynchronize/resubmit]
    [V6: Resynchronize to latest kernel commit code]
    [V7: Correct mail threading]
    [v8: Insert more useful variable names]
    
    Signed-off-by: Michael Bringmann <mwb@linux.vnet.ibm.com>

--- arch/powerpc/kernel/prom_init.c
+++ arch/powerpc/kernel/prom_init.c
@@ -696,7 +696,7 @@ unsigned char ibm_architecture_vec[] = {
 	OV4_MIN_ENT_CAP,		/* minimum VP entitled capacity */
 
 	/* option vector 5: PAPR/OF options */
-	VECTOR_LENGTH(21),		/* length */
+	VECTOR_LENGTH(22),		/* length */
 	0,				/* don't ignore, don't halt */
 	OV5_FEAT(OV5_LPAR) | OV5_FEAT(OV5_SPLPAR) | OV5_FEAT(OV5_LARGE_PAGES) |
 	OV5_FEAT(OV5_DRCONF_MEMORY) | OV5_FEAT(OV5_DONATE_DEDICATE_CPU) |
@@ -732,6 +732,7 @@ unsigned char ibm_architecture_vec[] = {
 	0,							/* Byte 19 */
 	0,							/* Byte 20 */
 	OV5_FEAT(OV5_SUB_PROCESSORS),				/* Byte 21 */
+	OV5_FEAT(OV5_DYN_MEM_V2) | OV5_FEAT(OV5_DRC_INFO),	/* Byte 22 */
 
 	/* option vector 6: IBM PAPR hints */
 	VECTOR_LENGTH(3),		/* length */
