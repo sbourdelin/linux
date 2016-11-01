8e30d472a59779758885baf6d951886e76b4d413
Christopher Covington <cov@codeaurora.org>
2016-11-01 11:10:55 -0600

    mm: Provide generic VDSO unmap and remap functions
    When Address Space Layout Randomization (ASLR, randmaps) is enabled, the
    address of the VDSO fluctuates from one process to the next. If
    Checkpoint/Restore In Userspace (CRIU) is to fully replicate the memory map
    of a previous process, it must be able to remap the VDSO of a new process
    to the address used by the previous process. Historically this has been
    implemented in architecture-specific code for PowerPC and x86. In order to
    support 32-bit and 64-bit ARM without further duplication of code, copy
    Laurent Dufour's implementation for PowerPC with slight modifications to a
    generic location. This is hopefully the beginning of a long process of VDSO
    code de-duplication between architectures.
    
    Signed-off-by: Christopher Covington <cov@codeaurora.org>

--- include/asm-generic/mm_hooks.h
+++ include/asm-generic/mm_hooks.h
@@ -1,7 +1,17 @@
 /*
- * Define generic no-op hooks for arch_dup_mmap, arch_exit_mmap
- * and arch_unmap to be included in asm-FOO/mmu_context.h for any
- * arch FOO which doesn't need to hook these.
+ * Define generic hooks for arch_dup_mmap, arch_exit_mmap and arch_unmap to be
+ * included in asm-FOO/mmu_context.h for any arch FOO which doesn't need to
+ * specially hook these.
+ *
+ * arch_remap originally from include/linux-mm-arch-hooks.h
+ * arch_unmap originally from arch/powerpc/include/asm/mmu_context.h
+ * Copyright (C) 2015, IBM Corporation
+ * Author: Laurent Dufour <ldufour@linux.vnet.ibm.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
  */
 #ifndef _ASM_GENERIC_MM_HOOKS_H
 #define _ASM_GENERIC_MM_HOOKS_H
@@ -19,6 +29,25 @@ static inline void arch_unmap(struct mm_struct *mm,
 			struct vm_area_struct *vma,
 			unsigned long start, unsigned long end)
 {
+#ifdef CONFIG_GENERIC_VDSO
+	if (start <= mm->context.vdso && mm->context.vdso < end)
+		mm->context.vdso = 0;
+#endif /* CONFIG_GENERIC_VDSO */
+}
+
+static inline void arch_remap(struct mm_struct *mm,
+			      unsigned long old_start, unsigned long old_end,
+			      unsigned long new_start, unsigned long new_end)
+{
+#ifdef CONFIG_GENERIC_VDSO
+	/*
+	 * mremap() doesn't allow moving multiple vmas so we can limit the
+	 * check to old_addr == vdso.
+	 */
+	if (old_addr == mm->context.vdso)
+		mm->context.vdso = new_addr;
+
+#endif /* CONFIG_GENERIC_VDSO */
 }
 
 static inline void arch_bprm_mm_init(struct mm_struct *mm,
