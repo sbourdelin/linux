df912b4649d36fd194ceacb1e3f1ad66990c8f31
Tom Lendacky <thomas.lendacky@amd.com>
2016-11-03 08:30:36 -0500

    amd-xgbe: Add I2C support for determining SFP media types
    Add support to initialize and use the I2C controller within the hardware
    in order to determine the SFP media type that is installed.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- drivers/net/ethernet/amd/xgbe/xgbe-pci.c
+++ drivers/net/ethernet/amd/xgbe/xgbe-pci.c
@@ -461,6 +461,7 @@ static const struct xgbe_version_data xgbe_v2a = {
 	.rx_max_fifo_size		= 229376,
 	.tx_tstamp_workaround		= 1,
 	.ecc_support			= 1,
+	.i2c_support			= 1,
 };
 
 static const struct xgbe_version_data xgbe_v2b = {
@@ -471,6 +472,7 @@ static const struct xgbe_version_data xgbe_v2b = {
 	.rx_max_fifo_size		= 65536,
 	.tx_tstamp_workaround		= 1,
 	.ecc_support			= 1,
+	.i2c_support			= 1,
 };
 
 static const struct pci_device_id xgbe_pci_table[] = {
