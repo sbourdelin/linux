c60cd7742bbaa25eb0a75dde1a5328a4baf56490
Felix Fietkau <nbd@nbd.name>
2016-11-04 10:27:53 +0100

    mac80211: remove bogus skb vif assignment
    The call to ieee80211_txq_enqueue overwrites the vif pointer with the
    codel enqueue time, so setting it just before that call makes no sense.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- net/mac80211/tx.c
+++ net/mac80211/tx.c
@@ -1500,7 +1500,6 @@ static bool ieee80211_queue_skb(struct ieee80211_local *local,
 				struct sta_info *sta,
 				struct sk_buff *skb)
 {
-	struct ieee80211_tx_info *info = IEEE80211_SKB_CB(skb);
 	struct fq *fq = &local->fq;
 	struct ieee80211_vif *vif;
 	struct txq_info *txqi;
@@ -1524,8 +1523,6 @@ static bool ieee80211_queue_skb(struct ieee80211_local *local,
 
 	if (!txqi)
 		return false;
-
-	info->control.vif = vif;
 
 	spin_lock_bh(&fq->lock);
 	ieee80211_txq_enqueue(local, txqi, skb);
