7402a196129d88b150409cb3fffd649b8ba21b37
zhichang.yuan <yuanzhichang@hisilicon.com>
2016-11-08 11:47:07 +0800

    ARM64 LPC: Indirect ISA port IO introduced
    For arm64, there is no I/O space as other architectural platforms, such as
    X86. Most I/O accesses are achieved based on MMIO. But for some arm64 SoCs,
    such as Hip06, when accessing some legacy ISA devices connected to LPC, those
    known port addresses are used to control the corresponding target devices, for
    example, 0x2f8 is for UART, 0xe4 is for ipmi-bt. It is different from the
    normal MMIO mode in using.
    
    To drive these devices, this patch introduces a method named indirect-IO.
    In this method the in/out pair in arch/arm64/include/asm/io.h will be
    redefined. When upper layer drivers call in/out with those known legacy port
    addresses to access the peripherals, the hooking functions corrresponding to
    those target peripherals will be called. Through this way, those upper layer
    drivers which depend on in/out can run on Hip06 without any changes.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: zhichang.yuan <yuanzhichang@hisilicon.com>
    Signed-off-by: Gabriele Paoloni <gabriele.paoloni@huawei.com>

---
+++ arch/arm64/kernel/extio.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2016 Hisilicon Limited, All Rights Reserved.
+ * Author: Zhichang Yuan <yuanzhichang@hisilicon.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/io.h>
+
+struct extio_ops *arm64_extio_ops;
+
+
+BUILD_EXTIO(b, u8)
+
+BUILD_EXTIO(w, u16)
+
+BUILD_EXTIO(l, u32)
