75837f660a0b9da07326bc386d51712847b19a63
Ramesh Shanmugasundaram <ramesh.shanmugasundaram@bp.renesas.com>
2016-11-09 15:44:42 +0000

    media: Add new SDR formats SC16, SC18 & SC20
    This patch adds support for the three new SDR formats. These formats
    were prefixed with "sliced" indicating I data constitutes the top half and
    Q data constitutes the bottom half of the received buffer.
    
    V4L2_SDR_FMT_SCU16BE - 14-bit complex (I & Q) unsigned big-endian sample
    inside 16-bit. V4L2 FourCC: SC16
    
    V4L2_SDR_FMT_SCU18BE - 16-bit complex (I & Q) unsigned big-endian sample
    inside 18-bit. V4L2 FourCC: SC18
    
    V4L2_SDR_FMT_SCU20BE - 18-bit complex (I & Q) unsigned big-endian sample
    inside 20-bit. V4L2 FourCC: SC20
    
    Signed-off-by: Ramesh Shanmugasundaram <ramesh.shanmugasundaram@bp.renesas.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -666,6 +666,9 @@ struct v4l2_pix_format {
 #define V4L2_SDR_FMT_CS8          v4l2_fourcc('C', 'S', '0', '8') /* complex s8 */
 #define V4L2_SDR_FMT_CS14LE       v4l2_fourcc('C', 'S', '1', '4') /* complex s14le */
 #define V4L2_SDR_FMT_RU12LE       v4l2_fourcc('R', 'U', '1', '2') /* real u12le */
+#define V4L2_SDR_FMT_SCU16BE	  v4l2_fourcc('S', 'C', '1', '6') /* sliced complex u16be */
+#define V4L2_SDR_FMT_SCU18BE	  v4l2_fourcc('S', 'C', '1', '8') /* sliced complex u18be */
+#define V4L2_SDR_FMT_SCU20BE	  v4l2_fourcc('S', 'C', '2', '0') /* sliced complex u20be */
 
 /* Touch formats - used for Touch devices */
 #define V4L2_TCH_FMT_DELTA_TD16	v4l2_fourcc('T', 'D', '1', '6') /* 16-bit signed deltas */
