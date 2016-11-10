1e3704e954481168b1c3616f047fa1f8b4f353bb
Oleg Drokin <oleg.drokin@intel.com>
2016-11-10 12:30:58 -0500

    staging: lustre: osc: Remove remains of osc_ast_guard
    osc_ast_guard has been removed by the clio simplification.
    Remove the last lock class definition.
    
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-7148
    Reviewed-on: http://review.whamcloud.com/16392
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Bobi Jam <bobijam@hotmail.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- drivers/staging/lustre/lustre/osc/osc_dev.c
+++ drivers/staging/lustre/lustre/osc/osc_dev.c
@@ -88,8 +88,6 @@ struct lu_kmem_descr osc_caches[] = {
 	}
 };
 
-struct lock_class_key osc_ast_guard_class;
-
 /*****************************************************************************
  *
  * Type conversions.
