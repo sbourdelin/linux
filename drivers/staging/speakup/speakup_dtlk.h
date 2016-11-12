6b290d073739871b83c8b9a9d60f0c052727dfc8
Walt Feasel <waltfeasel@gmail.com>
2016-11-11 20:13:54 -0500

    staging:speakup:speakup_dtlk.h Modify block comments
    Made suggested modifications from checkpatch in reference
    to WARNING: Block comments should align the * on each line
       Modified block text alignment for consistency
    
    Signed-off-by: Walt Feasel <waltfeasel@gmail.com>

--- drivers/staging/speakup/speakup_dtlk.h
+++ drivers/staging/speakup/speakup_dtlk.h
@@ -1,8 +1,8 @@
 /* speakup_dtlk.h - header file for speakups DoubleTalk driver. */
 
 #define SYNTH_IO_EXTENT	0x02
-#define SYNTH_CLEAR	0x18		/* stops speech */
-	/* TTS Port Status Flags */
+#define SYNTH_CLEAR	0x18	/* stops speech */
+				/* TTS Port Status Flags */
 #define TTS_READABLE	0x80	/* mask for bit which is nonzero if a
 				 * byte can be read from the TTS port
 				 */
@@ -23,12 +23,12 @@
 				 * writing, and goes 1 again 180-190
 				 * usec later.
 				 */
-#define TTS_ALMOST_FULL	0x08	/* mask for AF bit: When set to 1,
-					 * indicates that less than 300 bytes
-					 * are available in the TTS input
-					 * buffer. AF is always 0 in the PCM,
-					 * TGN and CVSD modes.
-					 */
+#define TTS_ALMOST_FULL 0x08	/* mask for AF bit: When set to 1,
+				 * indicates that less than 300 bytes
+				 * are available in the TTS input
+				 * buffer. AF is always 0 in the PCM,
+				 * TGN and CVSD modes.
+				 */
 #define TTS_ALMOST_EMPTY 0x04	/* mask for AE bit: When set to 1,
 				 * indicates that less than 300 bytes
 				 * are remaining in DoubleTalk's input
