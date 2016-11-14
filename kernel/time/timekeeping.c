470ba30a73ffc6d52b6efc74a18f091e3288b8b4
Chris Metcalf <cmetcalf@mellanox.com>
2016-11-14 14:42:49 -0500

    time: Avoid signed overflow in timekeeping_delta_to_ns()
    This bugfix was originally made in commit 35a4933a8959 ("time:
    Avoid signed overflow in timekeeping_get_ns()").  When the code was
    refactored in commit 6bd58f09e1d8 ("time: Add cycles to nanoseconds
    translation") the signed overflow fix was lost.  Re-introduce it.
    
    Signed-off-by: Chris Metcalf <cmetcalf@mellanox.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -304,8 +304,7 @@ static inline s64 timekeeping_delta_to_ns(struct tk_read_base *tkr,
 {
 	s64 nsec;
 
-	nsec = delta * tkr->mult + tkr->xtime_nsec;
-	nsec >>= tkr->shift;
+	nsec = (delta * tkr->mult + tkr->xtime_nsec) >> tkr->shift;
 
 	/* If arch requires, add in get_arch_timeoffset() */
 	return nsec + arch_gettimeoffset();
