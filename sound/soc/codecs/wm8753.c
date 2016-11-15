329866090923a5ff919a4a957750bb2c539eddc0
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2016-11-15 05:12:42 +0000

    ASoC: wm8753: Convert to module_i2c/spi_driver()
    Let's use module_i2c/spi_driver()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/codecs/wm8753.c
+++ sound/soc/codecs/wm8753.c
@@ -1555,6 +1555,7 @@ static struct spi_driver wm8753_spi_driver = {
 	.probe		= wm8753_spi_probe,
 	.remove		= wm8753_spi_remove,
 };
+module_spi_driver(wm8753_spi_driver);
 #endif /* CONFIG_SPI_MASTER */
 
 #if IS_ENABLED(CONFIG_I2C)
@@ -1608,39 +1609,8 @@ static struct i2c_driver wm8753_i2c_driver = {
 	.remove =   wm8753_i2c_remove,
 	.id_table = wm8753_i2c_id,
 };
+module_i2c_driver(wm8753_i2c_driver);
 #endif
-
-static int __init wm8753_modinit(void)
-{
-	int ret = 0;
-#if IS_ENABLED(CONFIG_I2C)
-	ret = i2c_add_driver(&wm8753_i2c_driver);
-	if (ret != 0) {
-		printk(KERN_ERR "Failed to register wm8753 I2C driver: %d\n",
-		       ret);
-	}
-#endif
-#if defined(CONFIG_SPI_MASTER)
-	ret = spi_register_driver(&wm8753_spi_driver);
-	if (ret != 0) {
-		printk(KERN_ERR "Failed to register wm8753 SPI driver: %d\n",
-		       ret);
-	}
-#endif
-	return ret;
-}
-module_init(wm8753_modinit);
-
-static void __exit wm8753_exit(void)
-{
-#if IS_ENABLED(CONFIG_I2C)
-	i2c_del_driver(&wm8753_i2c_driver);
-#endif
-#if defined(CONFIG_SPI_MASTER)
-	spi_unregister_driver(&wm8753_spi_driver);
-#endif
-}
-module_exit(wm8753_exit);
 
 MODULE_DESCRIPTION("ASoC WM8753 driver");
 MODULE_AUTHOR("Liam Girdwood");
