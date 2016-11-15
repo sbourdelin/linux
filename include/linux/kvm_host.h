663cf39878465ce41c9be464a72b48c794d93dbe
Bandan Das <bsd@redhat.com>
2016-11-15 01:27:58 -0500

    kvm: x86: don't print warning messages for unimplemented msrs
    Change unimplemented msrs messages to use pr_debug.
    If CONFIG_DYNAMIC_DEBUG is set, then these messages can be
    enabled at run time or else -DDEBUG can be used at compile
    time to enable them. These messages will still be printed if
    ignore_msrs=1.
    
    Signed-off-by: Bandan Das <bsd@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -439,6 +439,9 @@ struct kvm {
 	pr_info("kvm [%i]: " fmt, task_pid_nr(current), ## __VA_ARGS__)
 #define kvm_debug(fmt, ...) \
 	pr_debug("kvm [%i]: " fmt, task_pid_nr(current), ## __VA_ARGS__)
+#define kvm_debug_ratelimited(fmt, ...) \
+	pr_debug_ratelimited("kvm [%i]: " fmt, task_pid_nr(current), \
+			     ## __VA_ARGS__)
 #define kvm_pr_unimpl(fmt, ...) \
 	pr_err_ratelimited("kvm [%i]: " fmt, \
 			   task_tgid_nr(current), ## __VA_ARGS__)
@@ -449,6 +452,8 @@ struct kvm {
 			(vcpu)->vcpu_id, kvm_rip_read(vcpu), ## __VA_ARGS__)
 
 #define vcpu_debug(vcpu, fmt, ...)					\
+	kvm_debug("vcpu%i " fmt, (vcpu)->vcpu_id, ## __VA_ARGS__)
+#define vcpu_debug_ratelimited(vcpu, fmt, ...)				\
 	kvm_debug("vcpu%i " fmt, (vcpu)->vcpu_id, ## __VA_ARGS__)
 #define vcpu_err(vcpu, fmt, ...)					\
 	kvm_err("vcpu%i " fmt, (vcpu)->vcpu_id, ## __VA_ARGS__)
