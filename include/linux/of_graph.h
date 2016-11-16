5e47a756905b96c27de441912e165f12ea94703f
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2016-11-16 02:19:50 +0000

    of_graph: add of_graph_get_port/endpoint_count()
    OF graph want to count its port/endpoint number, same as
    of_get_child_count(). This patch adds these functions.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/linux/of_graph.h
+++ include/linux/of_graph.h
@@ -54,6 +54,8 @@ struct of_endpoint {
 #ifdef CONFIG_OF
 int of_graph_parse_endpoint(const struct device_node *node,
 				struct of_endpoint *endpoint);
+int of_graph_get_port_count(const struct device_node *np);
+int of_graph_get_endpoint_count(const struct device_node *np);
 struct device_node *of_graph_get_port_by_id(struct device_node *node, u32 id);
 struct device_node *of_graph_get_top_port(struct device *dev);
 struct device_node *of_graph_get_next_port(const struct device_node *parent,
@@ -77,6 +79,12 @@ static inline int of_graph_parse_endpoint(const struct device_node *node,
 					struct of_endpoint *endpoint)
 {
 	return -ENOSYS;
+}
+
+static inline int of_graph_get_endpoint_count(const struct device_node *np,
+					      char *type)
+{
+	return 0;
 }
 
 static inline struct device_node *of_graph_get_port_by_id(
