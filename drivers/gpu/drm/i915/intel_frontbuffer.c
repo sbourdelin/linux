51683fbbec8e9a3814c6206fe898d658e8014592
Chris Wilson <chris@chris-wilson.co.uk>
2016-11-17 15:32:30 +0000

    drm/i915: Remove misinformation about mark_busy/mark_idle
    Nowadays these are solely for managing the GT wakelock. Don't confuse
    the reader by referring to how they used to work, nor suggest they are
    deprecated simply because they are no longer relevant for frontbuffer
    tracking.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_frontbuffer.c
+++ drivers/gpu/drm/i915/intel_frontbuffer.c
@@ -53,11 +53,6 @@
  * busyness. There is no direct way to detect idleness. Instead an idle timer
  * work delayed work should be started from the flush and flip functions and
  * cancelled as soon as busyness is detected.
- *
- * Note that there's also an older frontbuffer activity tracking scheme which
- * just tracks general activity. This is done by the various mark_busy and
- * mark_idle functions. For display power management features using these
- * functions is deprecated and should be avoided.
  */
 
 #include <drm/drmP.h>
