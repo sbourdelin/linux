004178125a2cf3c9c8a9e0f70cc38f2f7187eae0
Hemant Kumar <hemant@linux.vnet.ibm.com>
2016-11-21 11:40:05 +0530

    powerpc/perf: IMA pmu cpumask and cpu hotplug support
    Adds cpumask attribute to be used by each IMA pmu. Only one cpu (any
    online CPU) from each chip for nest PMUs is designated to read counters.
    
    On CPU hotplug, dying CPU is checked to see whether it is one of the
    designated cpus, if yes, next online cpu from the same chip (for nest
    units) is designated as new cpu to read counters.
    
    Cc: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Anton Blanchard <anton@samba.org>
    Cc: Sukadev Bhattiprolu <sukadev@linux.vnet.ibm.com>
    Cc: Michael Neuling <mikey@neuling.org>
    Cc: Stewart Smith <stewart@linux.vnet.ibm.com>
    Cc: Stephane Eranian <eranian@google.com>
    Signed-off-by: Hemant Kumar <hemant@linux.vnet.ibm.com>

--- arch/powerpc/include/asm/opal.h
+++ arch/powerpc/include/asm/opal.h
@@ -229,6 +229,8 @@ int64_t opal_pci_tce_kill(uint64_t phb_id, uint32_t kill_type,
 int64_t opal_rm_pci_tce_kill(uint64_t phb_id, uint32_t kill_type,
 			     uint32_t pe_num, uint32_t tce_size,
 			     uint64_t dma_addr, uint32_t npages);
+int64_t opal_nest_ima_counters_control(uint64_t mode, uint64_t value1,
+				  uint64_t value2, uint64_t value3);
 
 /* Internal functions */
 extern int early_init_dt_scan_opal(unsigned long node, const char *uname,
