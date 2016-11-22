b2e6b8928b140b2971803d6a39a434b2ac6a5a66
Stefan Eichenberger <eichest@gmail.com>
2016-11-22 11:39:44 +0100

    net: dsa: mv88e6xxx: egress all frames
    Egress multicast and egress unicast is only enabled for CPU/DSA ports
    but for switching operation it seems it should be enabled for all ports.
    Do I miss something here?
    
    I did the following test:
    brctl addbr br0
    brctl addif br0 lan0
    brctl addif br0 lan1
    
    In this scenario the unicast and multicast packets were not forwarded,
    therefore ARP requests were not resolved, and no connection could be
    established.
    
    If no bridge is configured we do not forward unicast and multicast
    packets because the VLAN mapping is active.
    
    Signed-off-by: Stefan Eichenberger <stefan.eichenberger@netmodule.com>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -2456,15 +2456,14 @@ static int mv88e6xxx_setup_port(struct mv88e6xxx_chip *chip, int port)
 	    mv88e6xxx_6185_family(chip) || mv88e6xxx_6320_family(chip))
 		reg = PORT_CONTROL_IGMP_MLD_SNOOP |
 		PORT_CONTROL_USE_TAG | PORT_CONTROL_USE_IP |
-		PORT_CONTROL_STATE_FORWARDING;
+		PORT_CONTROL_STATE_FORWARDING |
+		PORT_CONTROL_FORWARD_UNKNOWN_MC | PORT_CONTROL_FORWARD_UNKNOWN;
 	if (dsa_is_cpu_port(ds, port)) {
 		if (mv88e6xxx_has(chip, MV88E6XXX_FLAG_EDSA))
-			reg |= PORT_CONTROL_FRAME_ETHER_TYPE_DSA |
-				PORT_CONTROL_FORWARD_UNKNOWN_MC;
+			reg |= PORT_CONTROL_FRAME_ETHER_TYPE_DSA;
 		else
 			reg |= PORT_CONTROL_DSA_TAG;
-		reg |= PORT_CONTROL_EGRESS_ADD_TAG |
-			PORT_CONTROL_FORWARD_UNKNOWN;
+		reg |= PORT_CONTROL_EGRESS_ADD_TAG;
 	}
 	if (dsa_is_dsa_port(ds, port)) {
 		if (mv88e6xxx_6095_family(chip) ||
