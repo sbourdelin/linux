62a8b9683f07f38836f565e8a8bb101f57a9327d
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2016-11-24 15:34:16 +0100

    net: dt-bindings: add RGMII TX delay configuration to meson8b-dwmac
    This allows configuring the RGMII TX clock delay. This clock is
    generated by the Meson 8b / GXBB DWMAC glue. The configuration depends
    on the actual hardware (no delay may be needed due to the design of the
    actual circuit, the PHY might add this delay, etc.).
    The configuration values are provided as preprocessor macros to make the
    devicetree files easier to read.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ include/dt-bindings/net/dwmac-meson8b.h
@@ -0,0 +1,18 @@
+/*
+ * Devicetree constants for the Amlogic Meson8b and GXBB DWMAC glue layer
+ *
+ * Copyright (C) 2016 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* TX delay configuration */
+#define DWMAC_MESON8B_TXDLY_OFF				0x0
+#define DWMAC_MESON8B_TXDLY_QUARTER_CYCLE		0x1
+#define DWMAC_MESON8B_TXDLY_HALF_CYCLE			0x2
+#define DWMAC_MESON8B_TXDLY_THREE_QUARTER_CYCLE		0x3
