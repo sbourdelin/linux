7eb7f5185184a01e085a39fec04e9034d05c83cf
Adrian Hunter <adrian.hunter@intel.com>
2016-11-25 12:07:19 +0200

    mmc: block: Add Software Command Queuing
    eMMC Command Queuing is a feature added in version 5.1.  The card maintains
    a queue of up to 32 data transfers.  Commands CMD45/CMD45 are sent to queue
    up transfers in advance, and then one of the transfers is selected to
    "execute" by CMD46/CMD47 at which point data transfer actually begins.
    
    The advantage of command queuing is that the card can prepare for transfers
    in advance. That makes a big difference in the case of random reads because
    the card can start reading into its cache in advance.
    
    A v5.1 host controller can manage the command queue itself, but it is also
    possible for software to manage the queue using an non-v5.1 host controller
    - that is what Software Command Queuing is.
    
    Refer to the JEDEC (http://www.jedec.org/) eMMC v5.1 Specification for more
    information about Command Queuing.
    
    Two important aspects of Command Queuing that affect the implementation
    are:
     - only read/write requests are queued
     - the queue must be empty to send other commands, including re-tuning
    
    To support Software Command Queuing a separate function is provided to
    issue read/write requests (i.e. mmc_swcmdq_issue_rw_rq()) and the
    mmc_blk_request structure amended to cater for additional commands CMD44
    and CMD45. There is a separate function (mmc_swcmdq_prep()) to prepare the
    needed commands, but transfers are started by mmc_start_req() like normal.
    
    mmc_swcmdq_issue_rw_rq() enqueues the new request and then executes tasks
    until the queue is empty or mmc_swcmdq_execute() asks for a new request.
    This puts mmc_swcmdq_execute() in control of the decision whether to queue
    more requests or wait for the active one.
    
    Recovery is invoked if anything goes wrong. Recovery has 2 options:
     1. Discard the queue and re-queue all requests. If that fails, fall back
        to option 2.
     2. Reset and re-queue all requests. If that fails, error out all the
        requests.
    In either case, re-tuning will be done if needed after the queue becomes
    empty because re-tuning is released at that point.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- drivers/mmc/card/queue.h
+++ drivers/mmc/card/queue.h
@@ -15,9 +15,13 @@ struct mmc_blk_data;
 
 struct mmc_blk_request {
 	struct mmc_request	mrq;
-	struct mmc_command	sbc;
+	union {
+		struct mmc_command	sbc;
+		struct mmc_command	cmd44;
+	};
 	struct mmc_command	cmd;
 	struct mmc_command	stop;
+	struct mmc_command	cmd45;
 	struct mmc_data		data;
 	int			retune_retry_done;
 };
@@ -50,6 +54,7 @@ struct mmc_queue_req {
 	enum mmc_packed_type	cmd_type;
 	struct mmc_packed	*packed;
 	int			task_id;
+	unsigned int		retry_cnt;
 };
 
 struct mmc_queue {
@@ -66,6 +71,10 @@ struct mmc_queue {
 	int			qdepth;
 	int			qcnt;
 	unsigned long		qslots;
+	/* Following are defined for Software Command Queuing */
+	unsigned long		qsr;
+	struct mmc_async_req	*prepared_areq;
+	bool			qsr_err;
 };
 
 extern int mmc_init_queue(struct mmc_queue *, struct mmc_card *, spinlock_t *,
