924ee491d66b58eeb65b907d53c444b7aeb570d2
Adrian Hunter <adrian.hunter@intel.com>
2016-11-25 12:07:22 +0200

    mmc: sdhci-acpi: Enable Software Command Queuing for some Intel controllers
    Set MMC_CAP_SWCMDQ for Intel BYT and related eMMC host controllers.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- drivers/mmc/host/sdhci-acpi.c
+++ drivers/mmc/host/sdhci-acpi.c
@@ -274,7 +274,7 @@ static int sdhci_acpi_sd_probe_slot(struct platform_device *pdev,
 static const struct sdhci_acpi_slot sdhci_acpi_slot_int_emmc = {
 	.chip    = &sdhci_acpi_chip_int,
 	.caps    = MMC_CAP_8_BIT_DATA | MMC_CAP_NONREMOVABLE |
-		   MMC_CAP_HW_RESET | MMC_CAP_1_8V_DDR |
+		   MMC_CAP_HW_RESET | MMC_CAP_1_8V_DDR | MMC_CAP_SWCMDQ |
 		   MMC_CAP_CMD_DURING_TFR | MMC_CAP_WAIT_WHILE_BUSY,
 	.caps2   = MMC_CAP2_HC_ERASE_SZ,
 	.flags   = SDHCI_ACPI_RUNTIME_PM,
