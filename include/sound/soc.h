8cace4330b67222c4df007d71cfb2bb7d75a08da
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2016-11-28 17:32:26 +0000

    ASoC: core: Add component pin control functions
    It's often the case that a codec driver will need to control its
    own pins. However, if a name_prefix has been applied to this codec it
    must be included in the name passed to any of the snd_soc_dapm_x_pin()
    functions.
    
    The behaviour of the existing pin control functions is reasonable, since
    you may want to search for a fully-specified name within the scope of an
    entire card. This means that we can't apply the prefix in these functions
    because it will break card-scope searches.
    
    Constructing a prefixed string "manually" in codec drivers leads to a lot
    of repetition of the same code.
    
    To make this tidier in codec drivers this patch adds a new set of
    equivalent functions that take a struct snd_soc_component instead of a
    dapm context and automatically add the component's name_prefix to the
    given name. This makes it a simple change in codec drivers to be
    prefix-safe.
    
    The new functions are not quite trivial enough to be inlines and the
    compiler won't be able to compile-away any part of them.
    
    Although it looks somewhat inefficient to have to allocate a temporary
    buffer and combine strings, the current design of the widget list
    doesn't lend itself to a more optimized implementation - it's a single
    list of all widgets on a card and is searched linearly for a matching
    string. As pin state changes are generally low-frequency events it's
    unlikely to be a significant issue - at least not enough to rewrite the
    widget list handling just for this.
    
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1718,4 +1718,29 @@ static inline void snd_soc_dapm_mutex_unlock(struct snd_soc_dapm_context *dapm)
 	mutex_unlock(&dapm->card->dapm_mutex);
 }
 
+extern int snd_soc_component_enable_pin(struct snd_soc_component *component,
+					const char *pin);
+extern int snd_soc_component_enable_pin_unlocked(
+					struct snd_soc_component *component,
+					const char *pin);
+extern int snd_soc_component_disable_pin(struct snd_soc_component *component,
+					 const char *pin);
+extern int snd_soc_component_disable_pin_unlocked(
+					struct snd_soc_component *component,
+					const char *pin);
+extern int snd_soc_component_nc_pin(struct snd_soc_component *component,
+				    const char *pin);
+extern int snd_soc_component_nc_pin_unlocked(
+					struct snd_soc_component *component,
+					const char *pin);
+
+extern int snd_soc_component_get_pin_status(struct snd_soc_component *component,
+					    const char *pin);
+extern int snd_soc_component_force_enable_pin(
+					struct snd_soc_component *component,
+					const char *pin);
+extern int snd_soc_component_force_enable_pin_unlocked(
+					struct snd_soc_component *component,
+					const char *pin);
+
 #endif
