c0f1e000bc04ad4a95606e96b53115e1bb8435a0
Florian Westphal <fw@strlen.de>
2016-11-30 13:28:50 +0100

    tcp: allow to turn tcp timestamp randomization off
    Eric says: "By looking at tcpdump, and TS val of xmit packets of multiple
    flows, we can deduct the relative qdisc delays (think of fq pacing).
    This should work even if we have one flow per remote peer."
    
    Having random per flow (or host) offsets doesn't allow that anymore so add
    a way to turn this off.
    
    Suggested-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -76,7 +76,7 @@
 #include <asm/unaligned.h>
 #include <linux/errqueue.h>
 
-int sysctl_tcp_timestamps __read_mostly = 1;
+int sysctl_tcp_timestamps __read_mostly = 2;
 int sysctl_tcp_window_scaling __read_mostly = 1;
 int sysctl_tcp_sack __read_mostly = 1;
 int sysctl_tcp_fack __read_mostly = 1;
@@ -85,6 +85,7 @@ int sysctl_tcp_dsack __read_mostly = 1;
 int sysctl_tcp_app_win __read_mostly = 31;
 int sysctl_tcp_adv_win_scale __read_mostly = 1;
 EXPORT_SYMBOL(sysctl_tcp_adv_win_scale);
+EXPORT_SYMBOL(sysctl_tcp_timestamps);
 
 /* rfc5961 challenge ack rate limiting */
 int sysctl_tcp_challenge_ack_limit = 1000;
