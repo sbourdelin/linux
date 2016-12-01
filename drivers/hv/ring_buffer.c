726af0a0f4bf64e5af1b8e3300c72c73e7ef1b0e
K. Y. Srinivasan <kys@microsoft.com>
2016-12-01 09:28:41 -0800

    Drivers: hv: vmbus: Prevent sending data on a rescinded channel
    After the channel is rescinded, the host does not read from the rescinded channel.
    Fail writes to a channel that has already been rescinded
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>

--- drivers/hv/ring_buffer.c
+++ drivers/hv/ring_buffer.c
@@ -298,6 +298,9 @@ int hv_ringbuffer_write(struct vmbus_channel *channel,
 	unsigned long flags = 0;
 	struct hv_ring_buffer_info *outring_info = &channel->outbound;
 
+	if (channel->rescind)
+		return -ENODEV;
+
 	for (i = 0; i < kv_count; i++)
 		totalbytes_towrite += kv_list[i].iov_len;
 
