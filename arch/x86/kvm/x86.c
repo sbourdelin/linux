bbe1a6a80f06680991e514fa726f3ef7614d57a0
Roman Kagan <rkagan@virtuozzo.com>
2016-12-02 11:47:50 +0300

    kvm/x86: fix inversed check for async_pf MSR
    Signed-off-by: Roman Kagan <rkagan@virtuozzo.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -8421,7 +8421,7 @@ void kvm_arch_async_page_present(struct kvm_vcpu *vcpu,
 bool kvm_arch_can_inject_async_page_present(struct kvm_vcpu *vcpu)
 {
 	if (!(vcpu->arch.apf.msr_val & KVM_ASYNC_PF_ENABLED))
-		return true;
+		return false;
 	else
 		return !kvm_event_needs_reinjection(vcpu) &&
 			kvm_x86_ops->interrupt_allowed(vcpu);
