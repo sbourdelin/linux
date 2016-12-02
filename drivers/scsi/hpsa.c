623a8868fee792aed1350d4db2dc5557d02cc755
Hannes Reinecke <hare@suse.de>
2016-12-02 11:32:59 +0100

    hpsa: fallback to use legacy REPORT PHYS command
    Older SmartArray controllers (eg SmartArray 64xx) do
    not support the extended REPORT PHYS command, so
    fallback to use the legacy version here.
    
    Signed-off-by: Hannes Reinecke <hare@suse.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -3426,7 +3426,7 @@ static u64 hpsa_get_sas_address_from_report_physical(struct ctlr_info *h,
 		kfree(physdev);
 		return 0;
 	}
-	nphysicals = get_unaligned_be32(physdev->LUNListLength) / 24;
+	nphysicals = get_unaligned_be32(physdev->LUNListLength) / 8;
 
 	for (i = 0; i < nphysicals; i++)
 		if (!memcmp(&physdev->LUN[i].lunid[0], scsi3addr, 8)) {
@@ -3628,8 +3628,32 @@ out:
 static inline int hpsa_scsi_do_report_phys_luns(struct ctlr_info *h,
 		struct ReportExtendedLUNdata *buf, int bufsize)
 {
-	return hpsa_scsi_do_report_luns(h, 0, buf, bufsize,
-						HPSA_REPORT_PHYS_EXTENDED);
+	int rc;
+	struct ReportLUNdata *lbuf;
+
+	rc = hpsa_scsi_do_report_luns(h, 0, buf, bufsize,
+				      HPSA_REPORT_PHYS_EXTENDED);
+	if (!rc || !hpsa_allow_any)
+		return rc;
+
+	/* REPORT PHYS EXTENDED is not supported */
+	lbuf = kzalloc(sizeof(*lbuf), GFP_KERNEL);
+	if (!lbuf)
+		return -ENOMEM;
+
+	rc = hpsa_scsi_do_report_luns(h, 0, lbuf, sizeof(*lbuf), 0);
+	if (!rc) {
+		int i;
+		u32 nphys;
+
+		/* Copy ReportLUNdata header */
+		memcpy(buf, lbuf, 8);
+		nphys = be32_to_cpu(*((__be32 *)lbuf->LUNListLength)) / 24;
+		for (i = 0; i < nphys; i++)
+			memcpy(buf->LUN[i].lunid, lbuf->LUN[i], 8);
+	}
+	kfree(lbuf);
+	return rc;
 }
 
 static inline int hpsa_scsi_do_report_log_luns(struct ctlr_info *h,
