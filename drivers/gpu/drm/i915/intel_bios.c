81c02db856c2d920854cc838078a4992ba820417
Vidya Srinivas <vidya.srinivas@intel.com>
2016-12-07 20:28:19 +0530

    FOR_UPSTREAM [VPG]: drm/i915: Parse panel BL controller from VBT
    Currently the backlight controller is taken as 0. It needs to derive
    value from the VBT. Adding the necessary changes.
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Vidya Srinivas <vidya.srinivas@intel.com>

--- drivers/gpu/drm/i915/intel_bios.c
+++ drivers/gpu/drm/i915/intel_bios.c
@@ -330,6 +330,8 @@ parse_lfp_backlight(struct drm_i915_private *dev_priv,
 
 		method = &backlight_data->backlight_control[panel_type];
 		dev_priv->vbt.backlight.type = method->type;
+		dev_priv->vbt.backlight.controller = 0;
+		dev_priv->vbt.backlight.controller = method->controller;
 	}
 
 	dev_priv->vbt.backlight.pwm_freq_hz = entry->pwm_freq_hz;
@@ -341,6 +343,9 @@ parse_lfp_backlight(struct drm_i915_private *dev_priv,
 		      dev_priv->vbt.backlight.active_low_pwm ? "low" : "high",
 		      dev_priv->vbt.backlight.min_brightness,
 		      backlight_data->level[panel_type]);
+
+	DRM_DEBUG_KMS("VBT BL controller %u\n",
+			dev_priv->vbt.backlight.controller);
 }
 
 /* Try to find sdvo panel data */
