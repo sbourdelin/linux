0a361165cd116cc6c3b7ae09b243db090ad0a35c
Nayna Jain <nayna@linux.vnet.ibm.com>
2016-12-11 00:35:33 -0500

    tpm: add securityfs support for TPM 2.0 firmware event log
    Unlike the device driver support for TPM 1.2, the TPM 2.0 does
    not support the securityfs pseudo files for displaying the
    firmware event log.
    
    This patch enables support for providing the TPM 2.0 event log in
    binary form. TPM 2.0 event log supports a crypto agile format that
    records multiple digests, which is different from TPM 1.2. This
    patch enables the tpm_bios_log_setup for TPM 2.0  and adds the
    event log parser which understand the TPM 2.0 crypto agile format.
    
    Signed-off-by: Nayna Jain <nayna@linux.vnet.ibm.com>

--- drivers/char/tpm/tpm_eventlog.h
+++ drivers/char/tpm/tpm_eventlog.h
@@ -2,9 +2,12 @@
 #ifndef __TPM_EVENTLOG_H__
 #define __TPM_EVENTLOG_H__
 
+#include <crypto/hash_info.h>
+
 #define TCG_EVENT_NAME_LEN_MAX	255
 #define MAX_TEXT_EVENT		1000	/* Max event string length */
 #define ACPI_TCPA_SIG		"TCPA"	/* 0x41504354 /'TCPA' */
+#define TPM2_ACTIVE_PCR_BANKS	3
 
 #ifdef CONFIG_PPC64
 #define do_endian_conversion(x) be32_to_cpu(x)
@@ -72,6 +75,73 @@ enum tcpa_pc_event_ids {
 	POST_CONTENTS,
 	HOST_TABLE_OF_DEVICES,
 };
+
+/*
+ * All the structures related to TPM 2.0 Event Log are taken from TCG EFI
+ * Protocol Specification, Family "2.0". Document is available on link
+ * http://www.trustedcomputinggroup.org/tcg-efi-protocol-specification/ .
+ * Information is also available on TCG PC Client Platform Firmware Profile
+ * Specification, Family "2.0".
+ * Detailed digest structures for TPM 2.0 are defined in document
+ * Trusted Platform Module Library Part 2: Structures, Family "2.0".
+ */
+
+/* TPM 2.0 Event log header algorithm spec. */
+struct tcg_efi_specid_event_algs {
+	u16 alg_id;
+	u16 digest_size;
+} __packed;
+
+/* TPM 2.0 Event log header data. */
+struct tcg_efi_specid_event {
+	u8 signature[16];
+	u32 platform_class;
+	u8 spec_version_minor;
+	u8 spec_version_major;
+	u8 spec_errata;
+	u8 uintnsize;
+	u32 num_algs;
+	struct tcg_efi_specid_event_algs digest_sizes[TPM2_ACTIVE_PCR_BANKS];
+	u8 vendor_info_size;
+	u8 vendor_info[0];
+} __packed;
+
+/* TPM 2.0 Event Log Header. */
+struct tcg_pcr_event {
+	u32 pcr_idx;
+	u32 event_type;
+	u8 digest[20];
+	u32 event_size;
+	u8 event[0];
+} __packed;
+
+/* TPM 2.0 Crypto agile algorithm and respective digest. */
+struct tpmt_ha {
+	u16 alg_id;
+	u8 digest[SHA384_DIGEST_SIZE];
+} __packed;
+
+/* TPM 2.0 Crypto agile digests list. */
+struct tpml_digest_values {
+	u32 count;
+	struct tpmt_ha digests[TPM2_ACTIVE_PCR_BANKS];
+} __packed;
+
+/* TPM 2.0 Event field structure. */
+struct tcg_event_field {
+	u32 event_size;
+	u8 event[0];
+} __packed;
+
+/* TPM 2.0 Crypto agile log entry format. */
+struct tcg_pcr_event2 {
+	u32 pcr_idx;
+	u32 event_type;
+	struct tpml_digest_values digests;
+	struct tcg_event_field event;
+} __packed;
+
+extern const struct seq_operations tpm2_binary_b_measurements_seqops;
 
 #if defined(CONFIG_ACPI)
 int tpm_read_log_acpi(struct tpm_chip *chip);
