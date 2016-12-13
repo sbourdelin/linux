f1ad0502457a9c5d98d5e028087bb03ebee5b0c2
Baolin Wang <baolin.wang@linaro.org>
2016-12-13 17:22:21 +0800

    usb: host: xhci: Clean up commands when stop endpoint command is timeout
    Since the stop endpoint command is timeout, we will halt the xHCI
    controller and issuing the usb_hc_died() to report abnormal shutdown
    of a host controller, but before that, we should clean up the command
    queue to free commands memory and complete the pending commands.
    
    Signed-off-by: Baolin Wang <baolin.wang@linaro.org>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -902,6 +902,7 @@ void xhci_stop_endpoint_command_watchdog(unsigned long arg)
 			xhci_kill_endpoint_urbs(xhci, i, j);
 	}
 	spin_unlock_irqrestore(&xhci->lock, flags);
+	xhci_cleanup_command_queue(xhci);
 	xhci_dbg_trace(xhci, trace_xhci_dbg_cancel_urb,
 			"Calling usb_hc_died()");
 	usb_hc_died(xhci_to_hcd(xhci));
