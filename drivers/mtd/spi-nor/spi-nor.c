6f8f844532f0ffc42cedd564f80c6be54f0e8410
Larry D. Pinney <ldpinney@gmail.com>
2016-12-20 21:41:21 +0100

    mtd: spi-nor: add support for ESMT_f25l32qa and ESMT_f25l64qa
    Add Support for the ESMT_F25L32QA and ESMT_F25L64QA
    These are 4MB and 8MB SPI NOR Chips from Elite Semiconductor Memory
    Technology
    
    Signed-off-by: John Crispin <john@phrozen.org>
    Signed-off-by: Larry D. Pinney <ldpinney@gmail.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -822,6 +822,8 @@ static const struct flash_info spi_nor_ids[] = {
 
 	/* ESMT */
 	{ "f25l32pa", INFO(0x8c2016, 0, 64 * 1024, 64, SECT_4K) },
+	{ "f25l32qa", INFO(0x8c4116, 0, 64 * 1024, 64, SECT_4K) },
+	{ "f25l64qa", INFO(0x8c4117, 0, 64 * 1024, 128, SECT_4K) },
 
 	/* Everspin */
 	{ "mr25h256", CAT25_INFO( 32 * 1024, 1, 256, 2, SPI_NOR_NO_ERASE | SPI_NOR_NO_FR) },
