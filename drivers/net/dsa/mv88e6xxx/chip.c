98ec67555ab9f5d4a5eff5d4d7eacfcaa2204ed1
Romain Perier <romain.perier@free-electrons.com>
2016-12-20 09:51:36 +0100

    net: dsa: mv88e6xxx: Don't forbid MDIO I/Os for PHY addr >= num_of_ports
    Some Marvell ethernet switches have internal ethernet transceivers with
    hardcoded phy addresses. These addresses can be greater than the number
    of ports or its value might be different than the associated port number.
    This is for example the case for MV88E6341 that has 6 ports and internal
    Port 1 to Port4 PHYs mapped at SMI addresses from 0x11 to 0x14.
    
    This commits fixes the issue by removing the condition in MDIO callbacks.
    
    Signed-off-by: Romain Perier <romain.perier@free-electrons.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -2881,9 +2881,6 @@ static int mv88e6xxx_mdio_read(struct mii_bus *bus, int phy, int reg)
 	u16 val;
 	int err;
 
-	if (phy >= mv88e6xxx_num_ports(chip))
-		return 0xffff;
-
 	mutex_lock(&chip->reg_lock);
 	err = mv88e6xxx_phy_read(chip, phy, reg, &val);
 	mutex_unlock(&chip->reg_lock);
@@ -2895,9 +2892,6 @@ static int mv88e6xxx_mdio_write(struct mii_bus *bus, int phy, int reg, u16 val)
 {
 	struct mv88e6xxx_chip *chip = bus->priv;
 	int err;
-
-	if (phy >= mv88e6xxx_num_ports(chip))
-		return 0xffff;
 
 	mutex_lock(&chip->reg_lock);
 	err = mv88e6xxx_phy_write(chip, phy, reg, val);
