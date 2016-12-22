30d736c5c1bc440ad263e256c25f9bb50d96e421
Francis Laniel <laniel_francis@inventati.org>
2016-12-22 17:25:24 +0100

    drivers/staging/dgnc/dgnc_mgmt.c : add some goto statements for error handling
    I follow Dan Carpenter's advices and I update my patch.
    I just add goto statements for error handling.
    
    Good bye.
    
    Signed-off-by: Francis Laniel <laniel_francis@inventati.org>

--- drivers/staging/dgnc/dgnc_mgmt.c
+++ drivers/staging/dgnc/dgnc_mgmt.c
@@ -42,6 +42,7 @@ int dgnc_mgmt_open(struct inode *inode, struct file *file)
 {
 	unsigned long flags;
 	unsigned int minor = iminor(inode);
+	int rc = 0;
 
 	spin_lock_irqsave(&dgnc_global_lock, flags);
 
@@ -49,18 +50,19 @@ int dgnc_mgmt_open(struct inode *inode, struct file *file)
 	if (minor < MAXMGMTDEVICES) {
 		/* Only allow 1 open at a time on mgmt device */
 		if (dgnc_mgmt_in_use[minor]) {
-			spin_unlock_irqrestore(&dgnc_global_lock, flags);
-			return -EBUSY;
+			goto unlock;
+			rc = -EBUSY;
 		}
 		dgnc_mgmt_in_use[minor]++;
 	} else {
-		spin_unlock_irqrestore(&dgnc_global_lock, flags);
-		return -ENXIO;
+		goto unlock;
+		rc = -ENXIO;
 	}
 
+unlock:
 	spin_unlock_irqrestore(&dgnc_global_lock, flags);
 
-	return 0;
+	return rc;
 }
 
 /*
