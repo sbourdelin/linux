d625207a37a296f321a1c4d627b55c2348fa6869
Chen Yu <yu.c.chen@intel.com>
2016-12-23 16:41:53 +0800

    VM: x86: Return ealier if clocksource has not changed
    Currently the notifier of pvclock_gtod_notify() get invoked
    frequently due to the periodic update_wall_time(). This might
    slow down the system a little bit as there might be redundant
    execution code path and unnecessary lock contention
    in update_pvclock_gtod(), which was found when I was doing
    suspend/resume speed testings. As pvclock_gtod_notify()
    should be invoked only when clocksource has changed, according to
    Commit 16e8d74d2da9 ("KVM: x86: notifier for clocksource changes")
    , either we can add a new notifier for clocksource switch,
    or we can simply bypass the following code in pvclock_gtod_notify()
    earlier if there is no clocksource switch.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: "Radim Krcmar" <rkrcmar@redhat.com>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Wanpeng Li <wanpeng.li@hotmail.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -5961,13 +5961,14 @@ static int pvclock_gtod_notify(struct notifier_block *nb, unsigned long unused,
 	struct pvclock_gtod_data *gtod = &pvclock_gtod_data;
 	struct timekeeper *tk = priv;
 
+	if (likely(gtod->clock.vclock_mode == VCLOCK_TSC))
+		return 0;
 	update_pvclock_gtod(tk);
 
 	/* disable master clock if host does not trust, or does not
 	 * use, TSC clocksource
 	 */
-	if (gtod->clock.vclock_mode != VCLOCK_TSC &&
-	    atomic_read(&kvm_guest_has_master_clock) != 0)
+	if (atomic_read(&kvm_guest_has_master_clock) != 0)
 		queue_work(system_long_wq, &pvclock_gtod_work);
 
 	return 0;
