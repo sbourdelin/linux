8ee3f7dd4a96ddccee9ce41fee8688c7607f0896
Nicholas Mc Guire <hofrat@osadl.org>
2016-12-23 11:01:36 +0100

    x86, boot: add missing declaration of string functions
    Add the missing declarations of basic string functions to string.h to allow
    a clean build.
    
    Fixes: commit 5be865661516 ("String-handling functions for the new x86 setup code.")
    Signed-off-by: Nicholas Mc Guire <hofrat@osadl.org>

--- arch/x86/boot/string.h
+++ arch/x86/boot/string.h
@@ -18,4 +18,13 @@ int memcmp(const void *s1, const void *s2, size_t len);
 #define memset(d,c,l) __builtin_memset(d,c,l)
 #define memcmp	__builtin_memcmp
 
+int strcmp(const char *str1, const char *str2);
+int strncmp(const char *cs, const char *ct, size_t count);
+size_t strlen(const char *s);
+char *strstr(const char *s1, const char *s2);
+size_t strnlen(const char *s, size_t maxlen);
+unsigned int atou(const char *s);
+unsigned long long simple_strtoull(const char *cp,
+				    char **endp, unsigned int base);
+
 #endif /* BOOT_STRING_H */
