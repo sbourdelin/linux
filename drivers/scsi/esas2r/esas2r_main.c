5ffbccf9d502b8e825ad3b8fb0e7d915766522b8
Shyam Saini <mayhs11saini@gmail.com>
2016-12-23 17:09:35 +0530

    drivers: scsi: esas2r: Use setup_timer() and mod_timer()
    Use setup_timer instead of structure field assignments
    to initialize a timer.
    
    mod_timer is a more efficient way to update the expire field of
    an active timer (if the timer is inactive it will be activated)
    
    Signed-off-by: Shyam Saini <mayhs11saini@gmail.com>

--- drivers/scsi/esas2r/esas2r_main.c
+++ drivers/scsi/esas2r/esas2r_main.c
@@ -1635,14 +1635,8 @@ static void esas2r_timer_callback(unsigned long context);
 
 void esas2r_kickoff_timer(struct esas2r_adapter *a)
 {
-	init_timer(&a->timer);
-
-	a->timer.function = esas2r_timer_callback;
-	a->timer.data = (unsigned long)a;
-	a->timer.expires = jiffies +
-			   msecs_to_jiffies(100);
-
-	add_timer(&a->timer);
+	setup_timer(&a->timer, esas2r_timer_callback, (unsigned long)a);
+	mod_timer(&a->timer, jiffies + msecs_to_jiffies(100));
 }
 
 static void esas2r_timer_callback(unsigned long context)
