580fac2f9a4f43e86b516a76a769451b44d57377
Fabio Estevam <fabio.estevam@nxp.com>
2016-12-27 11:00:40 -0200

    thermal: thermal_hwmon: Convert to hwmon_device_register_with_info()
    Booting Linux on a mx6q based board leads to the following warning:
    
    (NULL device *): hwmon_device_register() is deprecated. Please convert the
    driver to use hwmon_device_register_with_info().
    
    ,so do as suggested.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>

--- drivers/thermal/thermal_hwmon.c
+++ drivers/thermal/thermal_hwmon.c
@@ -165,7 +165,8 @@ int thermal_add_hwmon_sysfs(struct thermal_zone_device *tz)
 
 	INIT_LIST_HEAD(&hwmon->tz_list);
 	strlcpy(hwmon->type, tz->type, THERMAL_NAME_LENGTH);
-	hwmon->device = hwmon_device_register(NULL);
+	hwmon->device = hwmon_device_register_with_info(NULL, NULL, NULL,
+							NULL, NULL);
 	if (IS_ERR(hwmon->device)) {
 		result = PTR_ERR(hwmon->device);
 		goto free_mem;
