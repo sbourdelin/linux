3692099ce264145b40dfa348ca4d8bdb7fa3b344
vathsala nagaraju <vathsala.nagaraju@intel.com>
2016-12-30 10:55:23 +0530

    drm/i915/psr: report live PSR2 State
    Reports  live state of PSR2 form PSR2_STATUS register.
    bit field 31:28 gives the live state of PSR2.
    It can be used to check if system is in deep sleep,
    selective update or selective update standby.
    During video play back, we can use this to check
    if system is entering SU mode or not.
    when system is in idle state, DEEP_SLEEP(8) must be entered.
    When video playback is happening, system must be in
    SLEEP(3 / selective update) or SU_STANDBY( 6 / selective update standby)
    
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Jim Bride <jim.bride@linux.intel.com>
    Signed-off-by: Vathsala Nagaraju <vathsala.nagaraju@intel.com>
    Signed-off-by: Patil Deepti <deepti.patil@intel.com>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -3624,6 +3624,8 @@ enum {
 
 #define EDP_PSR2_STATUS_CTL            _MMIO(0x6f940)
 #define EDP_PSR2_STATUS_STATE_MASK     (0xf<<28)
+#define EDP_PSR2_STATUS_STATE_SHIFT    28
+#define EDP_PSR2_STATUS_TG_ON          0xa
 
 /* VGA port control */
 #define ADPA			_MMIO(0x61100)
