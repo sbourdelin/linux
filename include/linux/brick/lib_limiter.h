8131aadffa81cb2e07b078a79ead77b04f9b1c82
Thomas Schoebel-Theuer <tst@schoebel-theuer.de>
2016-12-30 23:57:36 +0100

    mars: add new module lib_limiter
    Signed-off-by: Thomas Schoebel-Theuer <tst@schoebel-theuer.de>

---
+++ include/linux/brick/lib_limiter.h
@@ -0,0 +1,52 @@
+/*
+ * MARS Long Distance Replication Software
+ *
+ * Copyright (C) 2010-2014 Thomas Schoebel-Theuer
+ * Copyright (C) 2011-2014 1&1 Internet AG
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef LIB_LIMITER_H
+#define LIB_LIMITER_H
+
+#include <linux/utsname.h>
+
+struct rate_limiter {
+	/* hierarchy tree */
+	struct rate_limiter *lim_father;
+
+	/* tunables */
+	int lim_max_ops_rate;
+	int lim_max_amount_rate;
+	int lim_max_delay;
+	int lim_min_window;
+	int lim_max_window;
+
+	/* readable */
+	int lim_ops_rate;
+	int lim_amount_rate;
+	int lim_ops_cumul;
+	int lim_amount_cumul;
+	int lim_total_ops;
+	int lim_total_amount;
+	long long lim_stamp;
+
+	/* internal */
+	long long lim_ops_accu;
+	long long lim_amount_accu;
+};
+
+extern int rate_limit(struct rate_limiter *lim, int amount);
+
+extern void rate_limit_sleep(struct rate_limiter *lim, int amount);
+
+#endif
