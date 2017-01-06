84155204606c1de926e7344c9a5d4322a2f8e5e4
Sainath Grandhi <sainath.grandhi@intel.com>
2017-01-06 14:33:25 -0800

    TAP: Tap character device creation/destroy API
    This patch provides tap device create/destroy APIs in tap.c.
    
    Signed-off-by: Sainath Grandhi <sainath.grandhi@intel.com>
    Tested-by: Sainath Grandhi <sainath.grandhi@intel.com>

--- include/linux/if_tap.h
+++ include/linux/if_tap.h
@@ -6,6 +6,9 @@ void tap_del_queues(struct net_device *dev);
 int tap_get_minor(struct macvlan_dev *vlan);
 void tap_free_minor(struct macvlan_dev *vlan);
 int tap_queue_resize(struct macvlan_dev *vlan);
+int tap_create_cdev(struct cdev *tap_cdev,
+		     dev_t *tap_major, const char *device_name);
+void tap_destroy_cdev(dev_t major, struct cdev *tap_cdev);
 struct socket *tap_get_socket(struct file *file);
 
 #endif /*_LINUX_IF_TAP_H_*/
