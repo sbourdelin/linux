a7363d2a15a83ef37381b35319b1941026d587c7
Liam Breck <kernel@networkimprov.net>
2017-01-11 16:41:50 -0800

    power: bq24190_charger: Fix irq triggering to IRQF_TRIGGER_FALLING
    The interrupt signal is TRIGGER_FALLING. This is is specified
    in the data sheet "PIN FUNCTIONS":
    
    "The INT pin sends active low, 256 us pulse to hos to report charger
     device status and fault."
    
    Also the direction can be seen in the data sheet "Figure 1. bq24190
    with D+/D- Detection and USB On-The-Go (OTG)" that shows a 10k pull-up
    resistor installed for the sample configurations.
    
    Fixes: d7bf353fd0aa3 ("bq24190_charger: Add support for TI BQ24190
    Battery Charger")
    Cc: Mark A. Greer <mgreer@animalcreek.com>
    Cc: Matt Ranostay <matt@ranostay.consulting>
    Signed-off-by: Liam Breck <kernel@networkimprov.net>
    [tony@atomide.com: updated patch description]
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/power/supply/bq24190_charger.c
+++ drivers/power/supply/bq24190_charger.c
@@ -1395,7 +1395,7 @@ static int bq24190_probe(struct i2c_client *client,
 	irq_set_status_flags(bdi->irq, IRQ_NOAUTOEN);
 	ret = devm_request_threaded_irq(dev, bdi->irq, NULL,
 			bq24190_irq_handler_thread,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+			IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
 			"bq24190-charger", bdi);
 	if (ret < 0) {
 		dev_err(dev, "Can't set up irq handler\n");
