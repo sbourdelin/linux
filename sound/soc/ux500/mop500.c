acf842f6ebe92acc1e039198c4a7064350f6bf3a
Linus Walleij <linus.walleij@linaro.org>
2017-01-20 14:20:11 +0100

    ASoC: ux500: do not moan on deferrals
    This removes the "error" print on probe deferral, it's
    unnecessary and confusing. Also cut a few debug prints we do
    not need.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- sound/soc/ux500/mop500.c
+++ sound/soc/ux500/mop500.c
@@ -103,8 +103,6 @@ static int mop500_probe(struct platform_device *pdev)
 	struct device_node *np = pdev->dev.of_node;
 	int ret;
 
-	dev_dbg(&pdev->dev, "%s: Enter.\n", __func__);
-
 	mop500_card.dev = &pdev->dev;
 
 	if (np) {
@@ -113,22 +111,12 @@ static int mop500_probe(struct platform_device *pdev)
 			return ret;
 	}
 
-	dev_dbg(&pdev->dev, "%s: Card %s: Set platform drvdata.\n",
-		__func__, mop500_card.name);
 	platform_set_drvdata(pdev, &mop500_card);
 
 	snd_soc_card_set_drvdata(&mop500_card, NULL);
 
-	dev_dbg(&pdev->dev, "%s: Card %s: num_links = %d\n",
-		__func__, mop500_card.name, mop500_card.num_links);
-	dev_dbg(&pdev->dev, "%s: Card %s: DAI-link 0: name = %s\n",
-		__func__, mop500_card.name, mop500_card.dai_link[0].name);
-	dev_dbg(&pdev->dev, "%s: Card %s: DAI-link 0: stream_name = %s\n",
-		__func__, mop500_card.name,
-		mop500_card.dai_link[0].stream_name);
-
 	ret = snd_soc_register_card(&mop500_card);
-	if (ret)
+	if (ret && ret != -EPROBE_DEFER)
 		dev_err(&pdev->dev,
 			"Error: snd_soc_register_card failed (%d)!\n", ret);
 
@@ -138,8 +126,6 @@ static int mop500_probe(struct platform_device *pdev)
 static int mop500_remove(struct platform_device *pdev)
 {
 	struct snd_soc_card *mop500_card = platform_get_drvdata(pdev);
-
-	pr_debug("%s: Enter.\n", __func__);
 
 	snd_soc_unregister_card(mop500_card);
 	mop500_ab8500_remove(mop500_card);
