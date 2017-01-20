4c190f5adfbef8c30512a6748adf8aee755bf755
Ryder Lee <ryder.lee@mediatek.com>
2017-01-20 15:28:33 +0800

    macsec: fix validation failed in asynchronous operation.
    Add missing "macsec_skb_cb(skb)->valid = true" in callback
    function macsec_decrypt_done(), this fixes packet validation
    failed while decrypting asynchronously.
    
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>

--- drivers/net/macsec.c
+++ drivers/net/macsec.c
@@ -880,6 +880,7 @@ static void macsec_decrypt_done(struct crypto_async_request *base, int err)
 	aead_request_free(macsec_skb_cb(skb)->req);
 
 	rcu_read_lock_bh();
+	macsec_skb_cb(skb)->valid = true;
 	pn = ntohl(macsec_ethhdr(skb)->packet_number);
 	if (!macsec_post_decrypt(skb, &macsec->secy, pn)) {
 		rcu_read_unlock_bh();
