3b66565a89393e1078f699ed255f5dfbb286385f
Chris Wilson <chris@chris-wilson.co.uk>
2017-01-21 14:50:45 +0000

    drm/i915: Reset the gpu on takeover
    The GPU may be in an unknown state following resume and module load. The
    previous occupant may have left contexts loaded, or other dangerous
    state, which can cause an immediate GPU hang for us. The only save
    course of action is to reset the GPU prior to using it - similarly to
    how we reset the GPU prior to unload (before a second user may be
    affected by our leftover state).
    
    We need to reset the GPU very early in our load/resume sequence so that
    any stale HW pointers are revoked prior to any resource allocations we
    make (that may conflict).
    
    A reset should only be a couple of milliseconds on a slow device, a cost
    we should easily be able to absorb into our initialisation times.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -950,6 +950,7 @@ static int i915_driver_init_mmio(struct drm_i915_private *dev_priv)
 		goto put_bridge;
 
 	intel_uncore_init(dev_priv);
+	i915_gem_init_mmio(dev_priv);
 
 	return 0;
 
@@ -1579,6 +1580,7 @@ static int i915_drm_resume(struct drm_device *dev)
 
 	disable_rpm_wakeref_asserts(dev_priv);
 	intel_sanitize_gt_powersave(dev_priv);
+	i915_gem_sanitize(dev_priv);
 
 	ret = i915_ggtt_enable_hw(dev_priv);
 	if (ret)
