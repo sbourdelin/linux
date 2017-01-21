93114604be7d1e8c4b79390b8b7e470db2a7ce73
Markus Elfring <elfring@users.sourceforge.net>
2017-01-21 18:48:45 +0100

    s390/pci: Move two assignments for the variable "ret" in get_pfn()
    A local variable was set to an error code in two cases before a concrete
    error situation was detected. Thus move the corresponding assignments into
    if branches to indicate a software failure there.
    
    This issue was detected by using the Coccinelle software.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- arch/s390/pci/pci_mmio.c
+++ arch/s390/pci/pci_mmio.c
@@ -18,13 +18,15 @@ static long get_pfn(unsigned long user_addr, unsigned long access,
 	long ret;
 
 	down_read(&current->mm->mmap_sem);
-	ret = -EINVAL;
 	vma = find_vma(current->mm, user_addr);
-	if (!vma)
+	if (!vma) {
+		ret = -EINVAL;
 		goto out;
-	ret = -EACCES;
-	if (!(vma->vm_flags & access))
+	}
+	if (!(vma->vm_flags & access)) {
+		ret = -EACCES;
 		goto out;
+	}
 	ret = follow_pfn(vma, user_addr, pfn);
 out:
 	up_read(&current->mm->mmap_sem);
