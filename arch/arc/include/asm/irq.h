b0cdca7b2485f09ba65efe460208f046c7ebc64a
Yuriy Kolerov <yuriy.kolerov@synopsys.com>
2017-01-28 03:01:41 +0300

    ARCv2: IRQ: Remove option for setting number of interrupts
    When you set a value of ARC_NUMBER_OF_INTERRUPTS option
    it affects only a size of the interrupts table but macros
    for number of virtual interrupts (NR_IRQS) and for number
    of hardware interrupts (NR_CPU_IRQS) remain unchanged.
    Moreover usage of ARC_NUMBER_OF_INTERRUPTS is bad for
    portability since it is not possible to change size
    of the interrupts table after linkage.
    
    This patch makes these changes in IRQ subsystem:
    
      * NR_CPU_IRQS defines a maximum number of hardware interrupts.
      * Remove ARC_NUMBER_OF_INTERRUPTS option and create interrupts
        table for all possible hardware interrupts.
      * Increase a maximum number of virtual IRQs to 512. ARCv2 can
        support 240 interrupts in the core interrupts controllers
        and 128 interrupts in IDU. Thus 512 virtual IRQs must be
        enough for most configurations of boards.
    
    Signed-off-by: Yuriy Kolerov <yuriy.kolerov@synopsys.com>

--- arch/arc/include/asm/irq.h
+++ arch/arc/include/asm/irq.h
@@ -9,19 +9,41 @@
 #ifndef __ASM_ARC_IRQ_H
 #define __ASM_ARC_IRQ_H
 
-#define NR_CPU_IRQS	32  /* number of interrupt lines of ARC770 CPU */
-#define NR_IRQS		128 /* allow some CPU external IRQ handling */
+#ifdef CONFIG_ISA_ARCV2
+
+/*
+ * A maximum number of supported interrupts in the core interrupt controller.
+ * This number is not equal to the maximum interrupt number (256) because
+ * first 16 lines are reserved for exceptions and are not configurable.
+ */
+#define NR_CPU_IRQS	240
+
+/*
+ * ARCv2 can support 240 interrupts in the core interrupts controllers and
+ * 128 interrupts in IDU. Thus 512 virtual IRQs must be enough for most
+ * configurations of boards.
+ */
+#define NR_IRQS		512
 
 /* Platform Independent IRQs */
-#ifdef CONFIG_ISA_ARCV2
 #define IPI_IRQ		19
 #define SOFTIRQ_IRQ	21
 #define FIRST_EXT_IRQ	24
+
+#else
+
+#define NR_CPU_IRQS	32  /* number of interrupt lines of ARC770 CPU */
+#define NR_IRQS		128 /* allow some CPU external IRQ handling */
+
 #endif
+
+#ifndef __ASSEMBLY__
 
 #include <linux/interrupt.h>
 #include <asm-generic/irq.h>
 
 extern void arc_init_IRQ(void);
+
+#endif
 
 #endif
