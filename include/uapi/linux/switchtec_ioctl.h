a093df3e8a70524096aa678e1abc28886bed7efe
Logan Gunthorpe <logang@deltatee.com>
2017-01-31 10:03:24 -0700

    MicroSemi Switchtec management interface driver
    Microsemi's "Switchtec" line of PCI switch devices is already well
    supported by the kernel with standard PCI switch drivers. However, the
    Switchtec device advertises a special management endpoint with a separate
    PCI function address and class code. This endpoint enables some additional
    functionality which includes:
    
     * Packet and Byte Counters
     * Switch Firmware Upgrades
     * Event and Error logs
     * Querying port link status
     * Custom user firmware commands
    
    This patch introduces the switchtec kernel module which provides
    PCI driver that exposes a char device. The char device provides
    userspace access to this interface through read, write and (optionally)
    poll calls. A couple of special IOCTLs are provided to:
    
    * Inform userspace of firmware partition locations
    * Pass event counts and allow userspace to wait on events
    
    A short text file is provided which documents the switchtec driver,
    outlines the semantics of using the char device and describes the
    IOCTLs.
    
    The device also exposes a few read-only sysfs attributes which provide
    some device information component names and versions which is provided
    by the hardware. These are documented in
    Documentation/ABI/testing/sysfs-class-switchtec
    
    A userspace tool and library which utilizes this interface is available
    at [1]. This tool takes inspiration (and borrows some code) from
    nvme-cli [2]. The tool is largely complete at this time but additional
    features may be added in the future.
    
    [1] https://github.com/sbates130272/switchtec-user
    [2] https://github.com/linux-nvme/nvme-cli
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
    Signed-off-by: Stephen Bates <stephen.bates@microsemi.com>

---
+++ include/uapi/linux/switchtec_ioctl.h
@@ -0,0 +1,129 @@
+/*
+ * Microsemi Switchtec PCIe Driver
+ * Copyright (c) 2017, Microsemi Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#ifndef _UAPI_LINUX_SWITCHTEC_IOCTL_H
+#define _UAPI_LINUX_SWITCHTEC_IOCTL_H
+
+#include <linux/types.h>
+
+enum switchtec_ioctl_partition {
+	SWITCHTEC_IOCTL_PART_CFG0,
+	SWITCHTEC_IOCTL_PART_CFG1,
+	SWITCHTEC_IOCTL_PART_IMG0,
+	SWITCHTEC_IOCTL_PART_IMG1,
+	SWITCHTEC_IOCTL_PART_NVLOG,
+	SWITCHTEC_IOCTL_PART_VENDOR0,
+	SWITCHTEC_IOCTL_PART_VENDOR1,
+	SWITCHTEC_IOCTL_PART_VENDOR2,
+	SWITCHTEC_IOCTL_PART_VENDOR3,
+	SWITCHTEC_IOCTL_PART_VENDOR4,
+	SWITCHTEC_IOCTL_PART_VENDOR5,
+	SWITCHTEC_IOCTL_PART_VENDOR6,
+	SWITCHTEC_IOCTL_PART_VENDOR7,
+	SWITCHTEC_IOCTL_NUM_PARTITIONS,
+};
+
+struct switchtec_ioctl_fw_info {
+	__u32 flash_length;
+
+	struct {
+		__u32 address;
+		__u32 length;
+		__u32 active;
+	} partition[SWITCHTEC_IOCTL_NUM_PARTITIONS];
+};
+
+struct switchtec_ioctl_event_summary {
+	__u64 global;
+	__u64 part_bitmap;
+	__u32 local_part;
+	__u32 part[48];
+	__u32 pff[48];
+};
+
+enum switchtec_ioctl_event {
+	SWITCHTEC_IOCTL_EVENT_STACK_ERROR,
+	SWITCHTEC_IOCTL_EVENT_PPU_ERROR,
+	SWITCHTEC_IOCTL_EVENT_ISP_ERROR,
+	SWITCHTEC_IOCTL_EVENT_SYS_RESET,
+	SWITCHTEC_IOCTL_EVENT_FW_EXC,
+	SWITCHTEC_IOCTL_EVENT_FW_NMI,
+	SWITCHTEC_IOCTL_EVENT_FW_NON_FATAL,
+	SWITCHTEC_IOCTL_EVENT_FW_FATAL,
+	SWITCHTEC_IOCTL_EVENT_TWI_MRPC_COMP,
+	SWITCHTEC_IOCTL_EVENT_TWI_MRPC_COMP_ASYNC,
+	SWITCHTEC_IOCTL_EVENT_CLI_MRPC_COMP,
+	SWITCHTEC_IOCTL_EVENT_CLI_MRPC_COMP_ASYNC,
+	SWITCHTEC_IOCTL_EVENT_GPIO_INT,
+	SWITCHTEC_IOCTL_EVENT_PART_RESET,
+	SWITCHTEC_IOCTL_EVENT_MRPC_COMP,
+	SWITCHTEC_IOCTL_EVENT_MRPC_COMP_ASYNC,
+	SWITCHTEC_IOCTL_EVENT_DYN_PART_BIND_COMP,
+	SWITCHTEC_IOCTL_EVENT_AER_IN_P2P,
+	SWITCHTEC_IOCTL_EVENT_AER_IN_VEP,
+	SWITCHTEC_IOCTL_EVENT_DPC,
+	SWITCHTEC_IOCTL_EVENT_CTS,
+	SWITCHTEC_IOCTL_EVENT_HOTPLUG,
+	SWITCHTEC_IOCTL_EVENT_IER,
+	SWITCHTEC_IOCTL_EVENT_THRESH,
+	SWITCHTEC_IOCTL_EVENT_POWER_MGMT,
+	SWITCHTEC_IOCTL_EVENT_TLP_THROTTLING,
+	SWITCHTEC_IOCTL_EVENT_FORCE_SPEED,
+	SWITCHTEC_IOCTL_EVENT_CREDIT_TIMEOUT,
+	SWITCHTEC_IOCTL_EVENT_LINK_STATE,
+	SWITCHTEC_IOCTL_MAX_EVENTS,
+};
+
+#define SWITCHTEC_IOCTL_EVENT_LOCAL_PART_IDX -1
+#define SWITCHTEC_IOCTL_EVENT_IDX_ALL -2
+
+#define SWITCHTEC_IOCTL_EVENT_FLAG_CLEAR     (1 << 0)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_EN_POLL   (1 << 1)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_EN_LOG    (1 << 2)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_EN_CLI    (1 << 3)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_EN_FATAL  (1 << 4)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_DIS_POLL  (1 << 5)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_DIS_LOG   (1 << 6)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_DIS_CLI   (1 << 7)
+#define SWITCHTEC_IOCTL_EVENT_FLAG_DIS_FATAL (1 << 8)
+
+struct switchtec_ioctl_event_ctl {
+	__u32 event_id;
+	__s32 index;
+	__u32 flags;
+	__u16 occurred;
+	__u16 count;
+	__u32 data[5];
+};
+
+#define SWITCHTEC_IOCTL_PFF_VEP 100
+struct switchtec_ioctl_pff_port {
+	__u32 pff;
+	__u32 partition;
+	__u32 port;
+};
+
+#define SWITCHTEC_IOCTL_FW_INFO \
+	_IOR('W', 0x40, struct switchtec_ioctl_fw_info)
+#define SWITCHTEC_IOCTL_EVENT_SUMMARY \
+	_IOR('W', 0x41, struct switchtec_ioctl_event_summary)
+#define SWITCHTEC_IOCTL_EVENT_CTL \
+	_IOWR('W', 0x42, struct switchtec_ioctl_event_ctl)
+#define SWITCHTEC_IOCTL_PFF_TO_PORT \
+	_IOWR('W', 0x43, struct switchtec_ioctl_pff_port)
+#define SWITCHTEC_IOCTL_PORT_TO_PFF \
+	_IOWR('W', 0x44, struct switchtec_ioctl_pff_port)
+
+#endif
