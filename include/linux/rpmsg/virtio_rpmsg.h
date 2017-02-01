52c19d879fb5ccabb1098189a7096451b0c22000
Loic Pallardy <loic.pallardy@st.com>
2017-02-01 16:07:09 +0100

    include: virtio_rpmsg: add virtio rpmsg configuration structure
    Rpmsg channel configuration should be identical on host and
    coprocessor side.
    This patch proposes a new structure named struct virtio_rpmsg_cfg
    to gather all configuration information to characterize the
    communication link and.
    This structure will be exchanged with coprocessor via the resource
    table, expanding struct fw_rsc_vdev. It will guarantee that host
    and coprocessor will share the same information.
    virtio_rpmsg will access it thanks to virtio get and set features.
    Presence of struct virtio_rpmsg_cfg is optional.
    
    Signed-off-by: Loic Pallardy <loic.pallardy@st.com>

---
+++ include/linux/rpmsg/virtio_rpmsg.h
@@ -0,0 +1,32 @@
+
+#ifndef _LINUX_VIRTIO_RPMSG_H
+#define _LINUX_VIRTIO_RPMSG_H
+
+/* Offset in struct fw_rsc_vdev */
+#define RPMSG_CONFIG_OFFSET	0
+
+/**
+ * struct virtio_rpmsg_cfg - optional configuration field for virtio rpmsg
+ * provided at probe time by virtio (get/set)
+ * @id: virtio cfg id (as in virtio_ids.h)
+ * @version: virtio_rpmsg_cfg structure version number
+ * @va: virtual address (used when buffer allocated by low level driver)
+ * @da: device address
+ * @pa: physical address
+ * @len: length (in bytes)
+ * @buf_size: size of rpmsg buffer size (defined by firmware else default value
+ *	      used)
+ * @reserved: reserved (must be zero)
+ */
+struct virtio_rpmsg_cfg {
+	u32 id;
+	u32 version;
+	u64 va;
+	u32 da;
+	u32 pa;
+	u32 len;
+	u32 buf_size;
+	u32 reserved;
+} __packed;
+
+#endif
