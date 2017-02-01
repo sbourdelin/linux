3ed7c2f8c0c026eb2daa01d485645937cba7cef3
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2017-01-31 18:11:28 -0800

    device property: allow to constify properties
    There is no reason why statically defined properties should be modifiable,
    so let's make device_add_properties() and the rest of pset_*() functions to
    take const pointers to properties.
    
    This will allow us to mark properties as const/__initconst at definition
    sites.
    
    Reviewed-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/linux/property.h
+++ include/linux/property.h
@@ -247,7 +247,7 @@ property_entries_dup(const struct property_entry *properties);
 void property_entries_free(const struct property_entry *properties);
 
 int device_add_properties(struct device *dev,
-			  struct property_entry *properties);
+			  const struct property_entry *properties);
 void device_remove_properties(struct device *dev);
 
 bool device_dma_supported(struct device *dev);
