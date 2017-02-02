0e4beebee9bb384a664fd2559248dffee3f0db35
Richard Leitner <richard.leitner@skidata.com>
2017-02-02 14:44:29 +0100

    usb: misc: add USB251xB/xBi Hi-Speed Hub Controller Driver
    This patch adds a driver for configuration of the Microchip USB251xB/xBi
    USB 2.0 hub controller series with USB 2.0 upstream connectivity, SMBus
    configuration interface and two to four USB 2.0 downstream ports.
    
    Furthermore add myself as a maintainer for this driver.
    
    The datasheet can be found at the manufacturers website, see [1]. All
    device-tree exposed configuration features have been tested on a i.MX6
    platform with a USB2512B hub.
    
    [1] http://ww1.microchip.com/downloads/en/DeviceDoc/00001692C.pdf
    
    Signed-off-by: Richard Leitner <richard.leitner@skidata.com>

---
+++ include/linux/platform_data/usb251xb.h
@@ -0,0 +1,33 @@
+#ifndef __USB251XB_H__
+#define __USB251XB_H__
+
+struct usb251xb_platform_data {
+	int gpio_reset;
+	u16 vendor_id;
+	u16 product_id;
+	u16 device_id;
+	u8 conf_data1;
+	u8 conf_data2;
+	u8 conf_data3;
+	u8 non_rem_dev;
+	u8 port_disable_sp;
+	u8 port_disable_bp;
+	u8 max_power_sp;
+	u8 max_power_bp;
+	u8 max_current_sp;
+	u8 max_current_bp;
+	u8 power_on_time;
+	u16 lang_id;
+	char manufacturer[62];
+	char product[62];
+	char serial[62];
+	u8 bat_charge_en;
+	u8 boost_up;
+	u8 boost_x;
+	u8 port_swap;
+	u8 port_map12;
+	u8 port_map34;
+	u8 status;
+};
+
+#endif
