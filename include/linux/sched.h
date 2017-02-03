09a6f14904582add2b1ddffbe733ec9120eeb930
Ingo Molnar <mingo@kernel.org>
2017-02-03 23:15:21 +0100

    sched/headers: Move the 'root_task_group' declaration to <linux/sched/autogroup.h>
    Also remove the duplicate declaration from <linux/init_task.h>.
    
    ( That declaration was originally duplicated for dependency hell reasons,
      but there's no problem including the much smaller <linux/sched/autogroup.h>
      header now, to pick up the right prototype. )
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1708,10 +1708,6 @@ static inline void set_task_cpu(struct task_struct *p, unsigned int cpu)
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
 
-#ifdef CONFIG_CGROUP_SCHED
-extern struct task_group root_task_group;
-#endif /* CONFIG_CGROUP_SCHED */
-
 #ifndef TASK_SIZE_OF
 #define TASK_SIZE_OF(tsk)	TASK_SIZE
 #endif
