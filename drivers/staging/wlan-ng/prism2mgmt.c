9d44cfdad262932380206335bf427fe0f3ba3850
Maksymilian Piechota <maksymilianpiechota@gmail.com>
2017-02-13 09:32:13 -0500

    staging: wlan-ng: realign else if continuation
    realign else if continuation
    
    Signed-off-by: Maksymilian Piechota <maksymilianpiechota@gmail.com>

--- drivers/staging/wlan-ng/prism2mgmt.c
+++ drivers/staging/wlan-ng/prism2mgmt.c
@@ -1298,7 +1298,7 @@ int prism2mgmt_wlansniff(struct wlandevice *wlandev, void *msgp)
 			hw->sniffhdr = 0;
 			wlandev->netdev->type = ARPHRD_IEEE80211_PRISM;
 		} else if ((msg->wlanheader.status == P80211ENUM_msgitem_status_data_ok) &&
-			(msg->wlanheader.data == P80211ENUM_truth_true)) {
+			   (msg->wlanheader.data == P80211ENUM_truth_true)) {
 			hw->sniffhdr = 1;
 			wlandev->netdev->type = ARPHRD_IEEE80211_PRISM;
 		} else {
