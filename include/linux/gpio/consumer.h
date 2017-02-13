3c688a5d43d95afb5580211ddabffe12aa5fa7e2
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2017-02-12 17:13:55 -0800

    gpio: return NULL from gpiod_get_optional when GPIOLIB is disabled
    Given the intent behind gpiod_get_optional() and friends it does not make
    sense to return -ENOSYS when GPIOLIB is disabled: the driver is expected to
    work just fine without gpio so let's behave as if gpio was not found.
    Otherwise we have to special-case -ENOSYS in drivers.
    
    Note that there was objection that someone might forget to enable GPIOLIB
    when dealing with a platform that has device that actually specifies
    optional gpio and we'll break it. I find this unconvincing as that would
    have to be the *only GPIO* in the system, which is extremely unlikely.
    
    Suggested-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/linux/gpio/consumer.h
+++ include/linux/gpio/consumer.h
@@ -169,14 +169,14 @@ static inline struct gpio_desc *__must_check
 gpiod_get_optional(struct device *dev, const char *con_id,
 		   enum gpiod_flags flags)
 {
-	return ERR_PTR(-ENOSYS);
+	return NULL;
 }
 
 static inline struct gpio_desc *__must_check
 gpiod_get_index_optional(struct device *dev, const char *con_id,
 			 unsigned int index, enum gpiod_flags flags)
 {
-	return ERR_PTR(-ENOSYS);
+	return NULL;
 }
 
 static inline struct gpio_descs *__must_check
@@ -190,7 +190,7 @@ static inline struct gpio_descs *__must_check
 gpiod_get_array_optional(struct device *dev, const char *con_id,
 			 enum gpiod_flags flags)
 {
-	return ERR_PTR(-ENOSYS);
+	return NULL;
 }
 
 static inline void gpiod_put(struct gpio_desc *desc)
@@ -230,14 +230,14 @@ static inline struct gpio_desc *__must_check
 devm_gpiod_get_optional(struct device *dev, const char *con_id,
 			  enum gpiod_flags flags)
 {
-	return ERR_PTR(-ENOSYS);
+	return NULL;
 }
 
 static inline struct gpio_desc *__must_check
 devm_gpiod_get_index_optional(struct device *dev, const char *con_id,
 				unsigned int index, enum gpiod_flags flags)
 {
-	return ERR_PTR(-ENOSYS);
+	return NULL;
 }
 
 static inline struct gpio_descs *__must_check
@@ -251,7 +251,7 @@ static inline struct gpio_descs *__must_check
 devm_gpiod_get_array_optional(struct device *dev, const char *con_id,
 			      enum gpiod_flags flags)
 {
-	return ERR_PTR(-ENOSYS);
+	return NULL;
 }
 
 static inline void devm_gpiod_put(struct device *dev, struct gpio_desc *desc)
