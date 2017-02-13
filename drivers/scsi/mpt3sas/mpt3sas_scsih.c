06d1308163ddf0f298597a33607d944d37a8c341
Damien Le Moal <damien.lemoal@wdc.com>
2017-02-13 14:11:52 +0900

    mpt3sas: Ignore unaligned completion length for ZBC_IN
    The ZBC_IN command (REPORT ZONES) reply length is always a multiple of
    64B and thus may not be aligned on the device LBA size.
    For this command, retry due to the unaligned completion length is
    incorrect so do not check alignment of the reply length.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

--- drivers/scsi/mpt3sas/mpt3sas_scsih.c
+++ drivers/scsi/mpt3sas/mpt3sas_scsih.c
@@ -4748,8 +4748,9 @@ _scsih_io_done(struct MPT3SAS_ADAPTER *ioc, u16 smid, u8 msix_index, u32 reply)
 	 * then scsi-ml does not need to handle this misbehavior.
 	 */
 	sector_sz = scmd->device->sector_size;
-	if (unlikely(scmd->request->cmd_type == REQ_TYPE_FS && sector_sz &&
-		     xfer_cnt % sector_sz)) {
+	if (scmd->request->cmd_type == REQ_TYPE_FS &&
+	    scmd->cmnd[0] != ZBC_IN &&
+	    sector_sz && xfer_cnt % sector_sz) {
 		sdev_printk(KERN_INFO, scmd->device,
 		    "unaligned partial completion avoided (xfer_cnt=%u, sector_sz=%u)\n",
 			    xfer_cnt, sector_sz);
