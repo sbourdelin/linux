6ace963f0bb79e5048f5ade1ed8a0f2f2f2ab7e6
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2017-02-12 16:36:18 -0800

    driver core: emit uevents when device is bound to a driver
    Majority of standard for a subsystem device attributes are created at the
    same time devices are created, before KOBJECT_ADD uevent is emitted by the
    driver core. This means that attributes are there when userspace is
    notified about new device appearance.
    
    However many drivers create additional driver-specific device attributes
    when binding to the device, to provide userspace with additional controls,
    and such attributes may not be there yet when userpsace receives
    KOBJECT_ADD event. Changing the drivers to introduce intermediate "dummy"
    device as a container for such attributes would be wasteful, and in many
    cases, braking our sysfs ABI. Let's add a new event, KOBJECT_BIND (and its
    counterpart, KOBJECT_UNBIND) that is emitted after a driver is bound to a
    device. It can be used by userspace wishing to use driver-specific
    attributes of a device.
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -258,6 +258,8 @@ static void driver_bound(struct device *dev)
 	if (dev->bus)
 		blocking_notifier_call_chain(&dev->bus->p->bus_notifier,
 					     BUS_NOTIFY_BOUND_DRIVER, dev);
+
+	kobject_uevent(&dev->kobj, KOBJ_BIND);
 }
 
 static int driver_sysfs_add(struct device *dev)
@@ -839,6 +841,8 @@ static void __device_release_driver(struct device *dev, struct device *parent)
 			blocking_notifier_call_chain(&dev->bus->p->bus_notifier,
 						     BUS_NOTIFY_UNBOUND_DRIVER,
 						     dev);
+
+		kobject_uevent(&dev->kobj, KOBJ_UNBIND);
 	}
 }
 
