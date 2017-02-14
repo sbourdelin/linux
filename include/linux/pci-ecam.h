e0b5730b618e7b4c510b506798bc54a5cff44d7f
Arnd Bergmann <arnd@arndb.de>
2017-02-14 22:42:56 +0100

    PCI: always build pci-host-common on ARM64
    The Thunder PCI host driver is always built, but it relies on pci_host_common_probe,
    so we get a link error here:
    
    drivers/pci/host/pci-thunder-pem.c: In function 'thunder_pem_probe':
    drivers/pci/host/pci-thunder-pem.c:412:9: error: implicit declaration of function 'pci_host_common_probe';did you mean 'pci_host_bridge_priv'? [-Werror=implicit-function-declaration]
    
    This adds the common file to the existing hack.
    
    Fixes: ca5ab37b19df ("PCI: Explain ARM64 ACPI/MCFG quirk Kconfig and build strategy")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/pci-ecam.h
+++ include/linux/pci-ecam.h
@@ -68,9 +68,8 @@ extern struct pci_ecam_ops xgene_v1_pcie_ecam_ops; /* APM X-Gene PCIe v1 */
 extern struct pci_ecam_ops xgene_v2_pcie_ecam_ops; /* APM X-Gene PCIe v2.x */
 #endif
 
-#ifdef CONFIG_PCI_HOST_GENERIC
 /* for DT-based PCI controllers that support ECAM */
 int pci_host_common_probe(struct platform_device *pdev,
 			  struct pci_ecam_ops *ops);
-#endif
+
 #endif
