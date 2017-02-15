82080f27c145ff98b6262053c856021324c11acd
Alan Tull <atull@kernel.org>
2017-02-15 10:14:16 -0600

    fpga-bridge: add non-dt support
    Add functions that will support using FPGA bridges without
    device tree.
    
    * fpga_bridge_get
      Get the bridge given the device.
    
    * of_fpga_bridges_get_to_list
      Given the device node, get the bridge and add it to a list.
      (renamed from priviously existing fpga_bridges_get_to_list)
    
    * fpga_bridges_get_to_list
      Given the device, get the bridge and add it to a list.
    
    Signed-off-by: Alan Tull <atull@kernel.org>

--- drivers/fpga/fpga-region.c
+++ drivers/fpga/fpga-region.c
@@ -200,9 +200,9 @@ static int fpga_region_get_bridges(struct fpga_region *region,
 	int i, ret;
 
 	/* If parent is a bridge, add to list */
-	ret = fpga_bridge_get_to_list(region_np->parent,
-				      reg_ovl->image_info,
-				      &region->bridge_list);
+	ret = of_fpga_bridge_get_to_list(region_np->parent,
+					 reg_ovl->image_info,
+					 &region->bridge_list);
 	if (ret == -EBUSY)
 		return ret;
 
@@ -225,8 +225,8 @@ static int fpga_region_get_bridges(struct fpga_region *region,
 			continue;
 
 		/* If node is a bridge, get it and add to list */
-		ret = fpga_bridge_get_to_list(br, reg_ovl->image_info,
-					      &region->bridge_list);
+		ret = of_fpga_bridge_get_to_list(br, reg_ovl->image_info,
+						 &region->bridge_list);
 
 		/* If any of the bridges are in use, give up */
 		if (ret == -EBUSY) {
