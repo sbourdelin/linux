8bc0c9affaab3c30cd59c8317a12f72d85bc282d
Sagi Grimberg <sagi@grimberg.me>
2017-02-16 19:43:34 +0200

    net/utils: generic inet_pton_with_scope helper
    Several locations in the stack need to handle ipv4/ipv6
    (with scope) and port strings conversion to sockaddr.
    Add a helper that takes either AF_INET, AF_INET6 or
    AF_UNSPEC (for wildcard) to centralize this handling.
    
    Suggested-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>

--- net/core/utils.c
+++ net/core/utils.c
@@ -26,9 +26,11 @@
 #include <linux/percpu.h>
 #include <linux/init.h>
 #include <linux/ratelimit.h>
+#include <linux/socket.h>
 
 #include <net/sock.h>
 #include <net/net_ratelimit.h>
+#include <net/ipv6.h>
 
 #include <asm/byteorder.h>
 #include <linux/uaccess.h>
@@ -299,6 +301,95 @@ out:
 	return ret;
 }
 EXPORT_SYMBOL(in6_pton);
+
+/**
+ * inet_pton_with_scope - convert an IPv4/IPv6 and port to socket address
+ * @net: net namespace (used for scope handling)
+ * @af: address family, AF_INET, AF_INET6 or AF_UNSPEC for either
+ * @src: the start of the address string
+ * @port: the start of the port string (or NULL for none)
+ * @addr: output socket address
+ *
+ * Return zero on success, return errno when any error occurs.
+ */
+int inet_pton_with_scope(struct net *net, __kernel_sa_family_t af,
+		const char *src, const char *port, struct sockaddr_storage *addr)
+{
+	struct sockaddr_in *addr4;
+	struct sockaddr_in6 *addr6;
+	const char *scope_delim;
+	bool unspec = false;
+	int srclen = strlen(src);
+	u16 port_num;
+
+	if (port) {
+		if (kstrtou16(port, 0, &port_num)) {
+			pr_err("failed port_num %s\n", port);
+			return -EINVAL;
+		}
+	} else {
+		port_num = 0;
+	}
+
+	switch (af) {
+	case AF_UNSPEC:
+		unspec = true;
+		/* FALLTHRU */
+	case AF_INET:
+		if (srclen <= INET_ADDRSTRLEN) {
+			addr4 = (struct sockaddr_in *)addr;
+			if (in4_pton(src, srclen, (u8 *) &addr4->sin_addr.s_addr,
+				     '\n', NULL) > 0) {
+				addr4->sin_family = AF_INET;
+				addr4->sin_port = htons(port_num);
+				return 0;
+			}
+			pr_err("failed in4_pton %s\n", src);
+		}
+		if (!unspec)
+			break;
+	case AF_INET6:
+		if (srclen <= INET6_ADDRSTRLEN) {
+			addr6 = (struct sockaddr_in6 *)addr;
+			if (in6_pton(src, srclen, (u8 *) &addr6->sin6_addr.s6_addr,
+				     '%', &scope_delim) == 0) {
+				pr_err("failed in6_pton %s\n", src);
+				return -EINVAL;
+			}
+
+			if (ipv6_addr_type(&addr6->sin6_addr) & IPV6_ADDR_LINKLOCAL &&
+			    src + srclen != scope_delim && *scope_delim == '%') {
+				struct net_device *dev;
+				char scope_id[16];
+				size_t scope_len = min_t(size_t, sizeof scope_id,
+							 src + srclen - scope_delim - 1);
+
+				memcpy(scope_id, scope_delim + 1, scope_len);
+				scope_id[scope_len] = '\0';
+
+				dev = dev_get_by_name(net, scope_id);
+				if (dev) {
+					addr6->sin6_scope_id = dev->ifindex;
+					dev_put(dev);
+				} else if (kstrtouint(scope_id, 0, &addr6->sin6_scope_id)) {
+					pr_err("failed dev_get_by_name %s\n", scope_id);
+					return -EINVAL;
+				}
+			}
+
+			addr6->sin6_family = AF_INET6;
+			addr6->sin6_port = htons(port_num);
+
+			return 0;
+		}
+		break;
+	default:
+		pr_err("unexpected address family %d\n", af);
+	};
+
+	return -EINVAL;
+}
+EXPORT_SYMBOL(inet_pton_with_scope);
 
 void inet_proto_csum_replace4(__sum16 *sum, struct sk_buff *skb,
 			      __be32 from, __be32 to, bool pseudohdr)
