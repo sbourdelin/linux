8f4604bde81bf89f2019b42edf8f3f97bbf8681c
Lakshmipathi.G <Lakshmipathi.G@giis.co.in>
2017-02-17 04:01:59 +0530

    btrfs-progs: misc-tests: Superblock corruption and recovery using backup.
    Test script to recover damaged primary superblock using backup superblock.
    
    Signed-off-by: Lakshmipathi.G <Lakshmipathi.G@giis.co.in>

---
+++ tests/misc-tests/019-fix-superblock-corruption/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Corrupt primary superblock and restore it using backup superblock.
+#
+
+source $TOP/tests/common
+
+check_prereq btrfs-select-super
+check_prereq btrfs
+
+setup_root_helper
+prepare_test_dev 512M
+
+FIRST_SUPERBLOCK_OFFSET=65536
+
+test_superblock_restore()
+{
+	run_check $SUDO_HELPER $TOP/mkfs.btrfs -f $TEST_DEV
+
+	# Corrupt superblock checksum
+        dd if=/dev/zero of=$TEST_DEV seek=$FIRST_SUPERBLOCK_OFFSET bs=1 \
+        count=4  conv=notrunc &> /dev/null
+	# Run btrfs check to detect corruption
+	$TOP/btrfs check $TEST_DEV >& /dev/null && \
+		_fail "btrfs check should detect corruption"
+	# Copy backup superblock to primary
+	run_check $TOP/btrfs-select-super -s 1 $TEST_DEV
+
+	echo "Performing btrfs check" &>> $RESULTS
+	$TOP/btrfs check $TEST_DEV &>> $RESULTS
+        if [ $? -ne 0 ]; then
+		_fail "Failed to fix superblock."
+        fi
+}
+
+test_superblock_restore
