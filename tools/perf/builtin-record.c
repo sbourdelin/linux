c77b6a9c7c41509d1ba65d959fc7e14269a3cdbc
Jiri Olsa <jolsa@kernel.org>
2017-02-17 15:00:58 +0100

    perf record: Add -a as a default target
    Adding system wide (-a) option as a default target
    if non is specified.
    
      # perf record
      Warning: No target specified, setting system-wide collection (-a).
      ...
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/n/tip-tq11of2qlz8kxpxzva05d54l@git.kernel.org

--- tools/perf/builtin-record.c
+++ tools/perf/builtin-record.c
@@ -1677,8 +1677,10 @@ int cmd_record(int argc, const char **argv, const char *prefix __maybe_unused)
 
 	argc = parse_options(argc, argv, record_options, record_usage,
 			    PARSE_OPT_STOP_AT_NON_OPTION);
-	if (!argc && target__none(&rec->opts.target))
-		usage_with_options(record_usage, record_options);
+	if (!argc && target__none(&rec->opts.target)) {
+		pr_warning("Warning: No target specified, setting system-wide collection (-a).\n");
+		rec->opts.target.system_wide = true;
+	}
 
 	if (nr_cgroups && !rec->opts.target.system_wide) {
 		usage_with_options_msg(record_usage, record_options,
