674e1f54078a647ccb8330751ecceaf683fa4e4e
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2017-02-23 17:54:50 +0000

    pinctrl: samsung: Calculate GPIO base for pinctrl_add_gpio_range
    As the pinctrl is now added before the GPIOs are registered we need to
    manually calculate what the GPIO base will be, otherwise the base for
    each gpio_range will be set to zero. Fortunately the driver
    already assigns a GPIO base, in samsung_gpiolib_register, and uses the
    same calculation it does for the pin_base. Meaning the two will always
    be the same and allowing us to reuse the pinbase and avoid the issue.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- drivers/pinctrl/samsung/pinctrl-samsung.c
+++ drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -884,7 +884,7 @@ static int samsung_pinctrl_register(struct platform_device *pdev,
 		pin_bank->grange.id = bank;
 		pin_bank->grange.pin_base = drvdata->pin_base
 						+ pin_bank->pin_base;
-		pin_bank->grange.base = pin_bank->gpio_chip.base;
+		pin_bank->grange.base = pin_bank->grange.pin_base;
 		pin_bank->grange.npins = pin_bank->gpio_chip.ngpio;
 		pin_bank->grange.gc = &pin_bank->gpio_chip;
 		pinctrl_add_gpio_range(drvdata->pctl_dev, &pin_bank->grange);
