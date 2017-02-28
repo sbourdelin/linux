c9a3230994934d56d9000755555a2dfbce1fb62d
Alexander Kapshuk <alexander.kapshuk@gmail.com>
2017-02-28 21:52:44 +0200

    staging/fbtft: Hush checkpatch.pl warning about unnecessary line continuations.
    Use a single fmt string with appropriate verbs as conversion specifiers,
    followed by the original string literals and the integer argument
    instead of using a backslash to escape a new line embedded inbetween
    quoted string literals passed as fmt arguments to dev_err() invoked in
    drivers/staging/fbtft/fb_ssd1331.c.
    
    Signed-off-by: Alexander Kapshuk <alexander.kapshuk@gmail.com>

--- drivers/staging/fbtft/fb_ssd1331.c
+++ drivers/staging/fbtft/fb_ssd1331.c
@@ -129,17 +129,19 @@ static int set_gamma(struct fbtft_par *par, u32 *curves)
 
 	for (i = 0; i < 63; i++) {
 		if (i > 0 && curves[i] < 2) {
-			dev_err(par->info->device,
-				"Illegal value in Grayscale Lookup Table at index %d. " \
-				"Must be greater than 1\n", i);
+			dev_err(par->info->device, "%s %d. %s\n",
+				"Illegal value in Grayscale Lookup Table at index",
+				i,
+				"Must be greater than 1");
 			return -EINVAL;
 		}
 		acc += curves[i];
 		tmp[i] = acc;
 		if (acc > 180) {
-			dev_err(par->info->device,
-				"Illegal value(s) in Grayscale Lookup Table. " \
-				"At index=%d, the accumulated value has exceeded 180\n", i);
+			dev_err(par->info->device, "%s%d, %s\n",
+				"Illegal value(s) in Grayscale Lookup Table. At index=",
+				i,
+				"the accumulated value has exceeded 180");
 			return -EINVAL;
 		}
 	}
