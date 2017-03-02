0ba4de3c05ac1434e8d1f123a688c39dc4f9c03f
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2017-03-02 11:01:32 +0100

    vfio: type1: conditionally check MSI remapping at irq domain level
    Check only if irq domains are available.
    
    Signed-off-by: Mian Yousaf Kaukab <yousaf.kaukab@suse.com>

--- drivers/vfio/vfio_iommu_type1.c
+++ drivers/vfio/vfio_iommu_type1.c
@@ -1287,8 +1287,9 @@ static int vfio_iommu_type1_attach_group(void *iommu_data,
 	INIT_LIST_HEAD(&domain->group_list);
 	list_add(&group->next, &domain->group_list);
 
-	msi_remap = resv_msi ? irq_domain_check_msi_remap() :
-				iommu_capable(bus, IOMMU_CAP_INTR_REMAP);
+	msi_remap = resv_msi && IS_ENABLED(CONFIG_IRQ_DOMAIN) ?
+			irq_domain_check_msi_remap() :
+			iommu_capable(bus, IOMMU_CAP_INTR_REMAP);
 
 	if (!allow_unsafe_interrupts && !msi_remap) {
 		pr_warn("%s: No interrupt remapping support.  Use the module param \"allow_unsafe_interrupts\" to enable VFIO IOMMU support on this platform\n",
