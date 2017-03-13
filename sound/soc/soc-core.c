b79f99e54831a38c40681d35541f89fbd0584780
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2017-03-13 05:53:30 +0000

    ASoC: soc-core: enable "dai-name" on snd_soc_of_parse_daifmt()
    Current snd_soc_of_parse_daifmt() detects [prefix]format, but
    "format" was unclear. This patch expand it to [prefix]dai-format, too
    This patch update simple-xx-card.txt which uses this feature
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -3951,11 +3951,16 @@ unsigned int snd_soc_of_parse_daifmt(struct device_node *np,
 		prefix = "";
 
 	/*
-	 * check "[prefix]format = xxx"
+	 * check "[prefix]dai-format = xxx"
+	 * or    "[prefix]format = xxx"
 	 * SND_SOC_DAIFMT_FORMAT_MASK area
 	 */
-	snprintf(prop, sizeof(prop), "%sformat", prefix);
+	snprintf(prop, sizeof(prop), "%sdai-format", prefix);
 	ret = of_property_read_string(np, prop, &str);
+	if (ret < 0) {
+		snprintf(prop, sizeof(prop), "%sformat", prefix);
+		ret = of_property_read_string(np, prop, &str);
+	}
 	if (ret == 0) {
 		for (i = 0; i < ARRAY_SIZE(of_fmt_table); i++) {
 			if (strcmp(str, of_fmt_table[i].name) == 0) {
