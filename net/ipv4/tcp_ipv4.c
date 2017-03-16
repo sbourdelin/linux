df8b2b0e243ca4ded4ce5d10d2007f59481f260e
Kevin Xu <kaiwen.xu@hulu.com>
2017-03-16 01:08:30 -0700

    net: Do not hold the reference for the same sk_rx_dst.
    In some rare cases, inet_sk_rx_dst_set() may be called multiple times
    on the same dst, causing reference count leakage. Eventually, it
    prevents net_device to be destroyed. The bug then manifested as
    
    unregister_netdevice: waiting for lo to become free. Usage count = 1
    
    in the kernel log, preventing new network namespace creation.
    
    The patch works around the issue by checking whether the socket already
    has the same dst set.
    
    Signed-off-by: Kevin Xu <kaiwen.xu@hulu.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1806,9 +1806,14 @@ void inet_sk_rx_dst_set(struct sock *sk, const struct sk_buff *skb)
 {
 	struct dst_entry *dst = skb_dst(skb);
 
-	if (dst && dst_hold_safe(dst)) {
-		sk->sk_rx_dst = dst;
-		inet_sk(sk)->rx_dst_ifindex = skb->skb_iif;
+	if (dst) {
+		if (unlikely(dst == sk->sk_rx_dst))
+			return;
+
+		if (dst_hold_safe(dst)) {
+			sk->sk_rx_dst = dst;
+			inet_sk(sk)->rx_dst_ifindex = skb->skb_iif;
+		}
 	}
 }
 EXPORT_SYMBOL(inet_sk_rx_dst_set);
