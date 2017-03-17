aec0585e6ef141ceefc72b2d24eab0e5368c15fb
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2017-03-17 15:08:20 +0100

    IIO: consumer: allow to set buffer sizes
    Add iio consumer API to set buffer size and watermark according
    to sysfs API.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

--- include/linux/iio/consumer.h
+++ include/linux/iio/consumer.h
@@ -134,6 +134,19 @@ struct iio_cb_buffer *iio_channel_get_all_cb(struct device *dev,
 						       void *private),
 					     void *private);
 /**
+ * iio_channel_cb_set_buffer_size() - set the buffer length.
+ * @cb_buffer:		The callback buffer from whom we want the channel
+ *			information.
+ * @length: buffer length in bytes
+ * @watermark: buffer watermark in bytes
+ *
+ * This function allows to configure the buffer length. The watermark if
+ * forced to half of the buffer.
+ */
+int iio_channel_cb_set_buffer_params(struct iio_cb_buffer *cb_buffer,
+				     size_t length, size_t watermark);
+
+/**
  * iio_channel_release_all_cb() - release and unregister the callback.
  * @cb_buffer:		The callback buffer that was allocated.
  */
