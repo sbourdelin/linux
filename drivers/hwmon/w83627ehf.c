6147fcd305f90cf7808ae3a5add4995088fceff1
Peter Huewe <peterhuewe@gmx.de>
2017-03-23 14:03:07 +0100

    w83627ehf: Drop FSFE template and replace with SPDX License information
    As indicated by checkpatch it makes sense to not use the FSFE Template
    about GPLv2+
    
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>

--- drivers/hwmon/w83627ehf.c
+++ drivers/hwmon/w83627ehf.c
@@ -18,19 +18,8 @@
  *  This driver also supports the W83627EHG, which is the lead-free
  *  version of the W83627EHF.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Released under the GPLv2 or later.
+ *  SPDX-License-Identifier: GPL-2.0+
  *
  *  Supports the following chips:
  *
