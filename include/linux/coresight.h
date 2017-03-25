1d44ecb11cfe84a85fabf3265d2e217fb7e0ef4e
Leo Yan <leo.yan@linaro.org>
2017-03-26 02:23:12 +0800

    coresight: refactor with function of_coresight_get_cpu
    This is refactor to add function of_coresight_get_cpu(), so it's used to
    retrieve CPU id for coresight component. Finally can use it as a common
    function for multiple places.
    
    Suggested-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- include/linux/coresight.h
+++ include/linux/coresight.h
@@ -263,9 +263,11 @@ static inline int coresight_timeout(void __iomem *addr, u32 offset,
 #endif
 
 #ifdef CONFIG_OF
+extern int of_coresight_get_cpu(struct device_node *node);
 extern struct coresight_platform_data *of_get_coresight_platform_data(
 				struct device *dev, struct device_node *node);
 #else
+static inline int of_coresight_get_cpu(struct device_node *node) { return 0; }
 static inline struct coresight_platform_data *of_get_coresight_platform_data(
 	struct device *dev, struct device_node *node) { return NULL; }
 #endif
