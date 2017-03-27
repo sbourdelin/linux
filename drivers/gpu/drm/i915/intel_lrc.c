6934f3391e2f781f74f1559482f3b3ea559c3f6d
Chris Wilson <chris@chris-wilson.co.uk>
2017-03-27 04:28:14 +0100

    drm/i915/execlists: Wrap tail pointer after reset tweaking
    If the request->wa_tail is 0 (because it landed exactly on the end of
    the ringbuffer), when we reconstruct request->tail following a reset we
    fill in an illegal value (-8 or 0x001ffff8). As a result, RING_HEAD is
    never able to catch up with RING_TAIL and the GPU spins endlessly. If
    the ring contains a couple of breadcrumbs, even our hangcheck is unable
    to catch the busy-looping as the ACTHD and seqno continually advance.
    
    Fixes: a3aabe86a340 ("drm/i915/execlists: Reinitialise context image after GPU hang")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@intel.com>
    Cc: <stable@vger.kernel.org> # v4.10+

--- drivers/gpu/drm/i915/intel_lrc.c
+++ drivers/gpu/drm/i915/intel_lrc.c
@@ -1282,6 +1282,7 @@ static void reset_common_ring(struct intel_engine_cs *engine,
 
 	/* Reset WaIdleLiteRestore:bdw,skl as well */
 	request->tail = request->wa_tail - WA_TAIL_DWORDS * sizeof(u32);
+	request->tail &= request->ring->size - 1;
 	GEM_BUG_ON(!IS_ALIGNED(request->tail, 8));
 }
 
