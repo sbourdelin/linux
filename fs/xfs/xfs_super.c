26766a99e484b9a4269dccd295f0dd4c16597cd9
Darrick J. Wong <darrick.wong@oracle.com>
2017-03-27 13:46:11 -0700

    xfs: don't let suspend freeze the buffer workqueue
    Suspend has this annoying behavior in XFS where it freezes workqueues in
    some arbitrary order.  This is a problem because the sync call can cause
    an AIL push, which may have to perform a RMW cycle on an inode cluster
    buffer if that buffer has been reclaimed.  When this happens, the AIL
    issues a buffer read for which the io completion ends up on the xfs_buf
    workqueue.  If /that/ workqueue has already been frozen, the suspend
    will timeout because we froze the workqueues in the wrong order.
    
    It seems suspicious to be freezing IO helper threads[1], so let's just
    not do that anymore.  Prior to this patch, 4.10 fails to suspend on my
    laptop about 80% of the time; afterwards, it works every time.  I've not
    done much suspend-and-crash testing on it though.
    
    [1] https://lwn.net/Articles/705269/
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -837,7 +837,7 @@ xfs_init_mount_workqueues(
 	struct xfs_mount	*mp)
 {
 	mp->m_buf_workqueue = alloc_workqueue("xfs-buf/%s",
-			WQ_MEM_RECLAIM|WQ_FREEZABLE, 1, mp->m_fsname);
+			WQ_MEM_RECLAIM, 1, mp->m_fsname);
 	if (!mp->m_buf_workqueue)
 		goto out;
 
