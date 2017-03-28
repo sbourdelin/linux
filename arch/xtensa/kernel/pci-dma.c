b9485735580482dc2355489206e955c38a90d9d7
Boris Brezillon <boris.brezillon@free-electrons.com>
2017-03-28 10:20:42 +0200

    xtensa: Fix mmap() support
    The xtensa architecture does not implement the dma_map_ops->mmap() hook,
    thus relying on the default dma_common_mmap() implementation.
    This implementation is only safe on DMA-coherent architecture (hence the
    !defined(CONFIG_ARCH_NO_COHERENT_DMA_MMAP) condition), but xtensa does not
    fall in this case.
    This lead to bad pfn calculation when someone tries to mmap() one or
    several pages that are not part of the identity mapping because the
    dma_common_mmap() extract the pfn value from the virt address using
    virt_to_page() which is only applicable on DMA-coherent platforms (on
    other platforms, DMA coherent pages are mapped in a different region).
    
    Implement xtensa_dma_mmap() (loosely based on __arm_dma_mmap()) in which
    pfn is extracted from the DMA address using PFN_DOWN().
    
    While we're at it, select ARCH_NO_COHERENT_DMA_MMAP from the XTENSA
    entry so that we never silently fallback to dma_common_mmap() if someone
    decides to drop the xtensa_dma_mmap() implementation.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- arch/xtensa/kernel/pci-dma.c
+++ arch/xtensa/kernel/pci-dma.c
@@ -233,9 +233,32 @@ int xtensa_dma_mapping_error(struct device *dev, dma_addr_t dma_addr)
 	return 0;
 }
 
+static int xtensa_dma_mmap(struct device *dev, struct vm_area_struct *vma,
+			   void *cpu_addr, dma_addr_t dma_addr, size_t size,
+			   unsigned long attrs)
+{
+	int ret = -ENXIO;
+#ifdef CONFIG_MMU
+	unsigned long nr_vma_pages = (vma->vm_end - vma->vm_start) >> PAGE_SHIFT;
+	unsigned long nr_pages = PAGE_ALIGN(size) >> PAGE_SHIFT;
+	unsigned long pfn = PFN_DOWN(dma_addr);
+	unsigned long off = vma->vm_pgoff;
+
+	if (dma_mmap_from_coherent(dev, vma, cpu_addr, size, &ret))
+		return ret;
+
+	if (off < nr_pages && nr_vma_pages <= (nr_pages - off))
+		ret = remap_pfn_range(vma, vma->vm_start, pfn + off,
+				      vma->vm_end - vma->vm_start,
+				      vma->vm_page_prot);
+#endif  /* CONFIG_MMU */
+	return ret;
+}
+
 struct dma_map_ops xtensa_dma_map_ops = {
 	.alloc = xtensa_dma_alloc,
 	.free = xtensa_dma_free,
+	.mmap = xtensa_dma_mmap,
 	.map_page = xtensa_map_page,
 	.unmap_page = xtensa_unmap_page,
 	.map_sg = xtensa_map_sg,
