11097a5921556f0166fe2f71353c0e35e5a60a0d
Wu Hao <hao.wu@intel.com>
2017-03-30 20:08:14 +0800

    fpga: intel: afu add FPGA_GET_API_VERSION/CHECK_EXTENSION ioctls support
    FPGA_GET_API_VERSION and FPGA_CHECK_EXTENSION ioctls are common ones which
    need to be supported by all feature devices drivers including FME and AFU.
    This patch implements above 2 ioctls in Intel FPGA Accelerated Function
    Unit (AFU) driver.
    
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>
    Signed-off-by: Enno Luebbers <enno.luebbers@intel.com>
    Signed-off-by: Shiva Rao <shiva.rao@intel.com>
    Signed-off-by: Christopher Rauer <christopher.rauer@intel.com>
    Signed-off-by: Xiao Guangrong <guangrong.xiao@linux.intel.com>
    Signed-off-by: Wu Hao <hao.wu@intel.com>

--- drivers/fpga/intel/afu-main.c
+++ drivers/fpga/intel/afu-main.c
@@ -124,6 +124,13 @@ static int afu_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+static long afu_ioctl_check_extension(struct feature_platform_data *pdata,
+				     unsigned long arg)
+{
+	/* No extension support for now */
+	return 0;
+}
+
 static long afu_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	struct platform_device *pdev = filp->private_data;
@@ -134,6 +141,10 @@ static long afu_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	dev_dbg(&pdev->dev, "%s cmd 0x%x\n", __func__, cmd);
 
 	switch (cmd) {
+	case FPGA_GET_API_VERSION:
+		return FPGA_API_VERSION;
+	case FPGA_CHECK_EXTENSION:
+		return afu_ioctl_check_extension(pdata, arg);
 	default:
 		/*
 		 * Let sub-feature's ioctl function to handle the cmd
