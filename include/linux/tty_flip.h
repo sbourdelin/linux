028860847ac71331835d5df310a4552d6f4c5b45
Nicolas Pitre <nicolas.pitre@linaro.org>
2017-04-01 18:21:19 -0400

    minitty: minimal TTY support alternative for serial ports
    This is a minimal TTY layer alternative for embedded systems with limited
    capabilities. This supports only serial ports, supports only a subset of
    the default line discipline, and dispense with anything that is of no use
    for a small embedded system.
    
    The goal here is to minimize memory footprint. The code size is more than 5x
    smaller than the regular code providing the same functionalities. Runtime
    memory usage is greatly reduced as well.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>

--- include/linux/tty_flip.h
+++ include/linux/tty_flip.h
@@ -13,6 +13,8 @@ extern int tty_prepare_flip_string(struct tty_port *port,
 extern void tty_flip_buffer_push(struct tty_port *port);
 void tty_schedule_flip(struct tty_port *port);
 
+#ifndef CONFIG_MINITTY_SERIAL
+
 static inline int tty_insert_flip_char(struct tty_port *port,
 					unsigned char ch, char flag)
 {
@@ -34,6 +36,13 @@ static inline int tty_insert_flip_string(struct tty_port *port,
 {
 	return tty_insert_flip_string_fixed_flag(port, chars, TTY_NORMAL, size);
 }
+
+#else
+extern int tty_insert_flip_char(struct tty_port *port, unsigned char ch,
+				char flag);
+extern int tty_insert_flip_string(struct tty_port *port,
+				  const unsigned char *chars, size_t size);
+#endif
 
 extern void tty_buffer_lock_exclusive(struct tty_port *port);
 extern void tty_buffer_unlock_exclusive(struct tty_port *port);
