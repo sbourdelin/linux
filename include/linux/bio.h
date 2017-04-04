5a4a55226303a6d4435891fca54f72a296a57fa8
Dmitry Monakhov <dmonakhov@openvz.org>
2017-04-04 22:56:40 +0400

    bio: add bvec_iter rewind API
    Some ->bi_end_io handlers (for example: pi_verify or decrypt handlers)
    need to know original data vector, but after bio traverse io-stack it may
    be advanced, splited and relocated many times so it is hard to guess
    original iterator. Let's add 'bi_done' conter which accounts number
    of bytes iterator was advanced during it's evolution. Later end_io handler
    may easily restore original iterator by rewinding iterator to
    iter->bi_done.
    
    Note: this change makes sizeof (struct bvec_iter) multiple to 8
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- include/linux/bio.h
+++ include/linux/bio.h
@@ -166,11 +166,28 @@ static inline void bio_advance_iter(struct bio *bio, struct bvec_iter *iter,
 {
 	iter->bi_sector += bytes >> 9;
 
-	if (bio_no_advance_iter(bio))
+	if (bio_no_advance_iter(bio)) {
 		iter->bi_size -= bytes;
-	else {
+		iter->bi_done += bytes;
+	} else {
 		int err;
 		err = bvec_iter_advance(bio->bi_io_vec, iter, bytes);
+		if (unlikely(err))
+			bio->bi_error = err;
+	}
+}
+
+static inline void bio_rewind_iter(struct bio *bio, struct bvec_iter *iter,
+				    unsigned bytes)
+{
+	iter->bi_sector -= bytes >> 9;
+
+	if (bio_no_advance_iter(bio)) {
+		iter->bi_size += bytes;
+		iter->bi_done -= bytes;
+	} else {
+		int err;
+		err = bvec_iter_rewind(bio->bi_io_vec, iter, bytes);
 		if (unlikely(err))
 			bio->bi_error = err;
 	}
