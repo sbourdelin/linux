2864bff08c37790b6056c5c4cfe6740be33ff79f
Richard Guy Briggs <rgb@redhat.com>
2017-04-04 05:21:23 -0400

    audit: show fstype:pathname for entries with anonymous parents
    Tracefs or debugfs were causing hundreds to thousands of null PATH
    records to be associated with the init_module and finit_module SYSCALL
    records on a few modules when the following rule was in place for
    startup:
            -a always,exit -F arch=x86_64 -S init_module -F key=mod-load
    
    This happens because the parent inode is not found in the task's
    audit_names list and hence treats it as anonymous.  This gives us no
    information other than a numerical device number that may no longer be
    visible upon log inspeciton, and an inode number.
    
    Fill in the filesystem type, filesystem magic number and full pathname
    from the filesystem mount point on previously null PATH records from
    entries that have an anonymous parent from the child dentry using
    dentry_path_raw().
    
    Make the dentry argument of __audit_inode_child() non-const so that we
    can take a reference to it in the case of an anonymous parent with
    dget() and dget_parent() to be able to later print a partial path from
    the host filesystem rather than null.
    
    Since all we are given is an inode of the parent and the dentry of the
    child, finding the path from the mount point to the root of the
    filesystem is more challenging that would involve searching all
    vfsmounts from "/" until a matching dentry is found for that
    filesystem's root dentry.  Even if one is found, there may be more than
    one mount point.  At this point the gain seems marginal since
    knowing the filesystem type and path are a significant help in tracking
    down the source of the PATH records and being to address them.
    
    Sample output:
    type=PROCTITLE msg=audit(1488317694.446:143): proctitle=2F7362696E2F6D6F6470726F6265002D71002D2D006E66737634
    type=PATH msg=audit(1488317694.446:143): item=797 name=tracefs(74726163):/events/nfs4/nfs4_setclientid/format inode=15969 dev=00:09 mode=0100444 ouid=0 ogid=0 rdev=00:00 obj=system_u:object_r:tracefs_t:s0 nametype=CREATE
    type=PATH msg=audit(1488317694.446:143): item=796 name=tracefs(74726163):/events/nfs4/nfs4_setclientid inode=15964 dev=00:09 mode=040755 ouid=0 ogid=0 rdev=00:00 obj=system_u:object_r:tracefs_t:s0 nametype=PARENT
    ...
    type=PATH msg=audit(1488317694.446:143): item=1 name=tracefs(74726163):/events/nfs4 inode=15571 dev=00:09 mode=040755 ouid=0 ogid=0 rdev=00:00 obj=system_u:object_r:tracefs_t:s0 nametype=CREATE
    type=PATH msg=audit(1488317694.446:143): item=0 name=tracefs(74726163):/events inode=119 dev=00:09 mode=040755 ouid=0 ogid=0 rdev=00:00 obj=system_u:object_r:tracefs_t:s0 nametype=PARENT
    type=UNKNOWN[1330] msg=audit(1488317694.446:143): name="nfsv4"
    type=SYSCALL msg=audit(1488317694.446:143): arch=c000003e syscall=313 success=yes exit=0 a0=1 a1=55d5a35ce106 a2=0 a3=1 items=798 ppid=6 pid=528 auid=4294967295 uid=0 gid=0 euid=0 suid=0 fsuid=0 egid=0 sgid=0 fsgid=0 tty=(none) ses=4294967295 comm="modprobe" exe="/usr/bin/kmod" subj=system_u:system_r:insmod_t:s0 key="mod-load"
    
    See: https://github.com/linux-audit/audit-kernel/issues/8
    Test case: https://github.com/linux-audit/audit-testsuite/issues/42
    
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -74,6 +74,7 @@
 #include <linux/string.h>
 #include <linux/uaccess.h>
 #include <uapi/linux/limits.h>
+#include <linux/dcache.h>
 
 #include "audit.h"
 
@@ -881,6 +882,8 @@ static inline void audit_free_names(struct audit_context *context)
 		list_del(&n->list);
 		if (n->name)
 			putname(n->name);
+		if (n->dentry)
+			dput(n->dentry);
 		if (n->should_free)
 			kfree(n);
 	}
@@ -1858,7 +1861,7 @@ void __audit_file(const struct file *file)
  * unsuccessful attempts.
  */
 void __audit_inode_child(struct inode *parent,
-			 const struct dentry *dentry,
+			 struct dentry *dentry,
 			 const unsigned char type)
 {
 	struct audit_context *context = current->audit_context;
@@ -1914,6 +1917,7 @@ void __audit_inode_child(struct inode *parent,
 		if (!n)
 			return;
 		audit_copy_inode(n, NULL, parent);
+		n->dentry = dget_parent(dentry);
 	}
 
 	if (!found_child) {
@@ -1935,6 +1939,8 @@ void __audit_inode_child(struct inode *parent,
 		audit_copy_inode(found_child, dentry, inode);
 	else
 		found_child->ino = AUDIT_INO_UNSET;
+	if (!found_parent)
+		found_child->dentry = dget(dentry);
 }
 EXPORT_SYMBOL_GPL(__audit_inode_child);
 
