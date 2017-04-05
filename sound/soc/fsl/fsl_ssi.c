490512e86d0e88f3bc150ec330b94720f419612c
Fabio Estevam <fabio.estevam@nxp.com>
2017-04-05 13:40:10 -0300

    ASoC: fsl_ssi: Remove an outdated comment
    The comment for the FSLSSI_I2S_RATES definition states that the
    driver currently only supports I2S slave mode, which is no longer
    correct.
    
    Remove such outdated comment.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>

--- sound/soc/fsl/fsl_ssi.c
+++ sound/soc/fsl/fsl_ssi.c
@@ -54,14 +54,6 @@
 #include "fsl_ssi.h"
 #include "imx-pcm.h"
 
-/**
- * FSLSSI_I2S_RATES: sample rates supported by the I2S
- *
- * This driver currently only supports the SSI running in I2S slave mode,
- * which means the codec determines the sample rate.  Therefore, we tell
- * ALSA that we support all rates and let the codec driver decide what rates
- * are really supported.
- */
 #define FSLSSI_I2S_RATES SNDRV_PCM_RATE_CONTINUOUS
 
 /**
