eb59b0215bc3ef98675afa499b1f6bd2db6edf32
Juergen Borleis <jbe@pengutronix.de>
2017-04-07 10:15:00 +0200

    net: dsa: add new DSA switch driver for the SMSC-LAN9303
    The SMSC/Microchip LAN9303 is an ethernet switch device with one CPU port
    and two external ethernet ports with built-in phys.
    
    This driver uses the DSA framework, but is currently only capable of
    separating the two external ports. There is no offload support yet.
    
    Signed-off-by: Juergen Borleis <jbe@pengutronix.de>

---
+++ drivers/net/dsa/lan9303.h
@@ -0,0 +1,24 @@
+#include <linux/regmap.h>
+#include <linux/device.h>
+#include <net/dsa.h>
+
+struct lan9303 {
+	struct device *dev;
+	struct regmap *regmap;
+	struct regmap_irq_chip_data *irq_data;
+	struct gpio_desc *reset_gpio;
+	u32 reset_duration; /* in [ms] */
+	bool phy_addr_sel_strap;
+	struct dsa_switch *ds;
+	struct mutex indirect_mutex; /* protect indexed register access */
+};
+
+extern const struct regmap_access_table lan9303_register_set;
+
+static inline struct lan9303 *ds_to_lan9303(struct dsa_switch *ds)
+{
+	return (struct lan9303 *)ds->priv;
+}
+
+int lan9303_probe(struct lan9303 *chip, struct device_node *np);
+int lan9303_remove(struct lan9303 *chip);
