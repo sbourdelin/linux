38cde6a13108b993a60a65200695186273959761
Dan Carpenter <dan.carpenter@oracle.com>
2017-04-14 22:54:25 +0300

    drm/i915: set "ret" correctly on error paths
    If "crtc" is NULL, then my static checker complains that "ret" isn't
    initialized on that path.  It doesn't really cause a problem unless
    "ret" is somehow set to -EDEADLK which is not likely.
    
    Chris Wilson also noticed another error path where "ret" isn't set
    correctly.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -11202,6 +11202,7 @@ retry:
 	 */
 	if (!crtc) {
 		DRM_DEBUG_KMS("no pipe available for load-detect\n");
+		ret = -ENODEV;
 		goto fail;
 	}
 
@@ -11258,6 +11259,7 @@ found:
 		DRM_DEBUG_KMS("reusing fbdev for load-detection framebuffer\n");
 	if (IS_ERR(fb)) {
 		DRM_DEBUG_KMS("failed to allocate framebuffer for load-detection\n");
+		ret = PTR_ERR(fb);
 		goto fail;
 	}
 
