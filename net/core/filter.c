4522a7842f914d1301184356c60783ae669251fd
Chenbo Feng <fengc@google.com>
2017-04-14 16:12:14 -0700

    Add uid and cookie bpf helper to cg_skb_func_proto
    BPF helper functions get_socket_cookie and get_socket_uid can be
    used for network traffic classifications, among others. Expose
    them also to programs of type BPF_PROG_TYPE_CGROUP_SKB. As of
    commit 8f917bba0042 ("bpf: pass sk to helper functions") the required
    skb->sk function is available at both cgroup bpf ingress and egress
    hooks.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -2718,6 +2718,10 @@ cg_skb_func_proto(enum bpf_func_id func_id)
 	switch (func_id) {
 	case BPF_FUNC_skb_load_bytes:
 		return &bpf_skb_load_bytes_proto;
+	case BPF_FUNC_get_socket_cookie:
+		return &bpf_get_socket_cookie_proto;
+	case BPF_FUNC_get_socket_uid:
+		return &bpf_get_socket_uid_proto;
 	default:
 		return bpf_base_func_proto(func_id);
 	}
