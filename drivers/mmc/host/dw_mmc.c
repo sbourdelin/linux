5565d01d984d93f0a6f7eec9972c4eeedd118c5f
Ulf Hansson <ulf.hansson@linaro.org>
2017-04-19 23:17:10 +0200

    mmc: dw_mmc: Prevent runtime PM suspend when SDIO IRQs are enabled
    To be able to handle SDIO IRQs the dw_mmc device needs to be powered and
    providing clock to the SDIO card. Therefore, we must not allow the device
    to be runtime PM suspended while SDIO IRQs are enabled.
    
    To fix this, let's increase the runtime PM usage count while the mmc core
    enables SDIO IRQs. Later when the mmc core tells dw_mmc to disable SDIO
    IRQs, we drop the usage count to again allow runtime PM suspend.
    
    This now becomes the default behaviour for dw_mmc. In cases where SDIO IRQs
    can be re-routed as GPIO wake-ups during runtime PM suspend, one could
    potentially allow runtime PM suspend. However, that will have to be
    addressed as a separate change on top of this one.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -39,6 +39,7 @@
 #include <linux/of.h>
 #include <linux/of_gpio.h>
 #include <linux/mmc/slot-gpio.h>
+#include <linux/pm_runtime.h>
 
 #include "dw_mmc.h"
 
@@ -1664,8 +1665,15 @@ static void __dw_mci_enable_sdio_irq(struct dw_mci_slot *slot, int enb)
 static void dw_mci_enable_sdio_irq(struct mmc_host *mmc, int enb)
 {
 	struct dw_mci_slot *slot = mmc_priv(mmc);
+	struct dw_mci *host = slot->host;
 
 	__dw_mci_enable_sdio_irq(slot, enb);
+
+	/* Avoid runtime suspending the device when SDIO IRQ is enabled */
+	if (enb)
+		pm_runtime_get_noresume(host->dev);
+	else
+		pm_runtime_put_noidle(host->dev);
 }
 
 static void dw_mci_ack_sdio_irq(struct mmc_host *mmc)
