df714f960b6feaeb700ba1053d60950774d0264c
Aurelien Aptel <aaptel@suse.com>
2017-04-21 13:37:58 +0200

    fs/inode.c: spelling in inode_owner_or_capable doc
    CC: trivial@kernel.org
    Signed-off-by: Aurelien Aptel <aaptel@suse.com>

--- fs/inode.c
+++ fs/inode.c
@@ -2014,7 +2014,7 @@ EXPORT_SYMBOL(inode_init_owner);
  * inode_owner_or_capable - check current task permissions to inode
  * @inode: inode being checked
  *
- * Return true if current either has CAP_FOWNER in a namespace with the
+ * Return true if current task has either CAP_FOWNER in a namespace with the
  * inode owner uid mapped, or owns the file.
  */
 bool inode_owner_or_capable(const struct inode *inode)
