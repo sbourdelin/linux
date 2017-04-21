a0af36b059ae2a4c9bd31e764836bef0b59f663a
surenderpolsani <surenderpolsani@gmail.com>
2017-04-21 09:24:57 +0530

    staging : rtl8188eu : remove void function return
    kernel coding style doesn't allow the return statement
    in void function.
    
    Signed-off-by: Surenderp <surenderpolsani@gmail.com>

--- drivers/staging/rtl8188eu/hal/rtl8188e_dm.c
+++ drivers/staging/rtl8188eu/hal/rtl8188e_dm.c
@@ -165,7 +165,7 @@ void rtw_hal_dm_watchdog(struct adapter *Adapter)
 skip_dm:
 	/*  Check GPIO to determine current RF on/off and Pbc status. */
 	/*  Check Hardware Radio ON/OFF or not */
-	return;
+	;
 }
 
 void rtw_hal_dm_init(struct adapter *Adapter)
