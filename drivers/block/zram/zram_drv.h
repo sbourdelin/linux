039b276dfee6b789914bddd2d7c5cd2ec3798835
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2017-04-21 10:14:50 +0900

    zram: make deduplication feature optional
    Benefit of deduplication is dependent on the workload so it's not
    preferable to always enable. Therefore, make it optional in Kconfig
    and device param. Default is 'off'. This option will be beneficial
    for users who use the zram as blockdev and stores build output to it.
    
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -134,7 +134,12 @@ struct zram {
 	 * zram is claimed so open request will be failed
 	 */
 	bool claim; /* Protected by bdev->bd_mutex */
+	bool use_dedup;
 };
 
+static inline bool zram_dedup_enabled(struct zram *zram)
+{
+	return zram->use_dedup;
+}
 void zram_entry_free(struct zram *zram, struct zram_entry *entry);
 #endif
