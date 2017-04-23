e99037ba342d2ca73048be6c0b302d38d5b39e17
Pan Bian <bianpan2016@163.com>
2017-04-23 19:22:49 +0800

    usb: hub: check return value
    Function usb_autopm_get_interface() will return a negative errno on
    failure, and function usb_autopm_put_interface() should not be called if
    it fails. However, in function usb_remove_device(), the return value of
    function usb_autopm_get_interface() is not validated before calling
    usb_autopm_put_interface().
    
    Signed-off-by: Pan Bian <bianpan2016@163.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -940,16 +940,18 @@ int usb_remove_device(struct usb_device *udev)
 {
 	struct usb_hub *hub;
 	struct usb_interface *intf;
+	int if_err;
 
 	if (!udev->parent)	/* Can't remove a root hub */
 		return -EINVAL;
 	hub = usb_hub_to_struct_hub(udev->parent);
 	intf = to_usb_interface(hub->intfdev);
 
-	usb_autopm_get_interface(intf);
+	if_err = usb_autopm_get_interface(intf);
 	set_bit(udev->portnum, hub->removed_bits);
 	hub_port_logical_disconnect(hub, udev->portnum);
-	usb_autopm_put_interface(intf);
+	if (!if_err)
+		usb_autopm_put_interface(intf);
 	return 0;
 }
 
