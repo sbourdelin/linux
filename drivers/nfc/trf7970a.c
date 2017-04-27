cddf2f47c4e831ad7b82e65a790a8c4988ad5602
Geoff Lansberry <geoff@kuvee.com>
2017-04-26 21:41:48 -0400

    NFC: trf7970a: Correct register settings for 27MHz clock
    In prior commits the selected clock frequency does not propagate
    correctly to what is written the the TRF7970A_MODULATOR_SYS_CLK_CTRL
    register.
    
    Signed-off-by: Geoff Lansberry <geoff@kuvee.com>

--- drivers/nfc/trf7970a.c
+++ drivers/nfc/trf7970a.c
@@ -2079,6 +2079,13 @@ static int trf7970a_probe(struct spi_device *spi)
 		return -EINVAL;
 	}
 
+	if (clk_freq == TRF7970A_27MHZ_CLOCK_FREQUENCY) {
+		trf->modulator_sys_clk_ctrl = TRF7970A_MODULATOR_27MHZ;
+		dev_dbg(trf->dev, "trf7970a configured for 27MHz crystal\n");
+	} else {
+		trf->modulator_sys_clk_ctrl = 0;
+	}
+
 	if (of_property_read_bool(np, "en2-rf-quirk"))
 		trf->quirks |= TRF7970A_QUIRK_EN2_MUST_STAY_LOW;
 
