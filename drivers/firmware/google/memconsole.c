5714a480554af23b977b22fc985f978589565166
Julius Werner <jwerner@chromium.org>
2017-04-27 13:59:17 -0700

    firmware: google: memconsole: Adapt to new coreboot ring buffer format
    This patch needs to be applied on top of the other memconsole patches
    currently queued up in char-misc-next (ending in 049a59db34e).
    
    -------------------------8<--------------------------------------------
    
    The upstream coreboot implementation of memconsole was enhanced from a
    single-boot console to a persistent ring buffer
    (https://review.coreboot.org/#/c/18301). This patch changes the kernel
    memconsole driver to be able to read the new format.
    
    In addition, it is desirable for the driver to always return the current
    state of the console, so that it may be used by runtime firmware to log
    further messages after the kernel is already initialized. The existing
    implementation would already re-read the buffer on every access, but
    only measured the total size of the console once during initialization.
    Thus, the generic console interface was redesigned to allow the
    implementation full control over how to process an access, and the
    coreboot implementation will re-read the console size every time.
    
    Finally, add a new /sys/firmware/rawlog node and change the existing
    /sys/firmware/log to sanitize non-printable characters from the output.
    With the new persistent console it becomes more likely that bytes might
    get slightly corrupted (due to DRAM degradation during a reboot), and
    it's usually undesirable to get stray control characters in the console
    dump because a bit in a letter flipped.
    
    Signed-off-by: Julius Werner <jwerner@chromium.org>

--- drivers/firmware/google/memconsole.c
+++ drivers/firmware/google/memconsole.c
@@ -15,6 +15,7 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/ctype.h>
 #include <linux/init.h>
 #include <linux/sysfs.h>
 #include <linux/kobject.h>
@@ -22,39 +23,58 @@
 
 #include "memconsole.h"
 
-static char *memconsole_baseaddr;
-static size_t memconsole_length;
+static ssize_t (*memconsole_read_func)(char *, loff_t, size_t);
 
-static ssize_t memconsole_read(struct file *filp, struct kobject *kobp,
+static ssize_t memconsole_read_raw(struct file *filp, struct kobject *kobp,
 			       struct bin_attribute *bin_attr, char *buf,
 			       loff_t pos, size_t count)
 {
-	return memory_read_from_buffer(buf, count, &pos, memconsole_baseaddr,
-				       memconsole_length);
+	if (WARN_ON_ONCE(!memconsole_read_func))
+		return -EIO;
+	return memconsole_read_func(buf, pos, count);
 }
 
-static struct bin_attribute memconsole_bin_attr = {
+static ssize_t memconsole_read_log(struct file *filp, struct kobject *kobp,
+			       struct bin_attribute *bin_attr, char *buf,
+			       loff_t pos, size_t count)
+{
+	ssize_t i;
+	ssize_t rv = memconsole_read_raw(filp, kobp, bin_attr, buf, pos, count);
+
+	if (rv > 0)
+		for (i = 0; i < rv; i++)
+			if (!isprint(buf[i]) && !isspace(buf[i]))
+				buf[i] = '?';
+	return rv;
+}
+
+/* Memconsoles may be much longer than 4K, so need to use binary attributes. */
+static struct bin_attribute memconsole_log_attr = {
 	.attr = {.name = "log", .mode = 0444},
-	.read = memconsole_read,
+	.read = memconsole_read_log,
+};
+static struct bin_attribute memconsole_raw_attr = {
+	.attr = {.name = "rawlog", .mode = 0444},
+	.read = memconsole_read_raw,
 };
 
-void memconsole_setup(void *baseaddr, size_t length)
+void memconsole_setup(ssize_t (*read_func)(char *, loff_t, size_t))
 {
-	memconsole_baseaddr = baseaddr;
-	memconsole_length = length;
+	memconsole_read_func = read_func;
 }
 EXPORT_SYMBOL(memconsole_setup);
 
 int memconsole_sysfs_init(void)
 {
-	memconsole_bin_attr.size = memconsole_length;
-	return sysfs_create_bin_file(firmware_kobj, &memconsole_bin_attr);
+	return sysfs_create_bin_file(firmware_kobj, &memconsole_log_attr) ||
+		sysfs_create_bin_file(firmware_kobj, &memconsole_raw_attr);
 }
 EXPORT_SYMBOL(memconsole_sysfs_init);
 
 void memconsole_exit(void)
 {
-	sysfs_remove_bin_file(firmware_kobj, &memconsole_bin_attr);
+	sysfs_remove_bin_file(firmware_kobj, &memconsole_log_attr);
+	sysfs_remove_bin_file(firmware_kobj, &memconsole_raw_attr);
 }
 EXPORT_SYMBOL(memconsole_exit);
 
