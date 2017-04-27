ddf30c11e71d02c1dd50dfae42650cb0f4d5b791
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-04-27 13:27:49 +0300

    drm/omap: add new connector types
    We have been using DRM_MODE_CONNECTOR_Unknown for many of our outputs
    because there has not been a proper connector type for them.
    
    We now have connector type for DPI so let's take it into use. At the
    same time, add better connector types for the remaining outputs too.
    
    This patch sets the following outputs to use the following connector
    types:
    
    DPI -> DPI
    DBI -> DPI (MIPI DBI is very similar to DPI at the bus level)
    SDI -> LVDS (SDI is a type of LVDS)
    VENC -> SVIDEO (it could also be composite, but we don't have that
            information here, so svideo should be quite good match)
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/omap_drv.c
+++ drivers/gpu/drm/omapdrm/omap_drv.c
@@ -214,6 +214,13 @@ static int get_connector_type(struct omap_dss_device *dssdev)
 		return DRM_MODE_CONNECTOR_DVID;
 	case OMAP_DISPLAY_TYPE_DSI:
 		return DRM_MODE_CONNECTOR_DSI;
+	case OMAP_DISPLAY_TYPE_DPI:
+	case OMAP_DISPLAY_TYPE_DBI:
+		return DRM_MODE_CONNECTOR_DPI;
+	case OMAP_DISPLAY_TYPE_VENC:
+		return DRM_MODE_CONNECTOR_SVIDEO;
+	case OMAP_DISPLAY_TYPE_SDI:
+		return DRM_MODE_CONNECTOR_LVDS;
 	default:
 		return DRM_MODE_CONNECTOR_Unknown;
 	}
