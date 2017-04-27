c5d82969be308edd1e38bf78ec2912cfdd5ef58d
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-04-27 13:27:51 +0300

    drm/omap: remove ovl_set_channel_out
    At the moment we have ovl_set_channel_out() to configure the output
    channel of an overlay. It makes sense to have this configuration as part
    of the reset of overlay configuration, and in DSS6+ we need the output
    channel when doing the other overlay configuration.
    
    This patch adds 'channel' parameter to ovl_setup(), so that all overlay
    configuration is done via the same function, and removes the
    ovl_set_channel_out().
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/dss/omapdss.h
+++ drivers/gpu/drm/omapdrm/dss/omapdss.h
@@ -917,7 +917,8 @@ struct dispc_ops {
 			enum omap_channel channel);
 	int (*ovl_setup)(enum omap_plane_id plane,
 			 const struct omap_overlay_info *oi,
-			const struct videomode *vm, bool mem_to_mem);
+			const struct videomode *vm, bool mem_to_mem,
+			enum omap_channel channel);
 
 	enum omap_color_mode (*ovl_get_color_modes)(enum omap_plane_id plane);
 };
