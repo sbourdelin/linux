62df5ba677851e42a66433d6c121760570222b24
Bogdan Mirea <Bogdan-Stefan_mirea@mentor.com>
2017-04-28 16:26:00 +0300

    usb: gadget: gserial: check if console kthread exists
    In the current implementation we get a Kernel Oops when calling rmmod
    for g_serial driver. This happens because the function that creates the
    kthread (gs_console_setup) which should be called at driver probing is
    never called for ttyGS* resulting in a null info->console_thread
    pointer.
    
    Noticed this behaviour on Renesas Rcar-H3 Salvator-X (R8A7795).
    
    Signed-off-by: Bogdan Mirea <Bogdan-Stefan_mirea@mentor.com>

--- drivers/usb/gadget/function/u_serial.c
+++ drivers/usb/gadget/function/u_serial.c
@@ -1256,7 +1256,8 @@ static void gserial_console_exit(void)
 	struct gscons_info *info = &gscons_info;
 
 	unregister_console(&gserial_cons);
-	kthread_stop(info->console_thread);
+	if (info->console_thread && !IS_ERR(info->console_thread))
+		kthread_stop(info->console_thread);
 	gs_buf_free(&info->con_buf);
 }
 
