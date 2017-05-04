6740a294f0f72f968e578dbc2d2d89266ac45a4d
Mathias Rav <mathiasrav@gmail.com>
2017-05-04 12:13:38 -0400

    staging: lustre: lprocfs: Use kstrtouint_from_user
    Prefer kstrtouint_from_user to copy_from_user+simple_strtoul.
    
    The helper function lprocfs_wr_uint() is only used to implement
    "dump_granted_max" in debugfs.
    
    Note the slight change in semantics: The previous implementation using
    simple_strtoul allows garbage after the number, whereas kstrtox only allows
    a trailing line break. The previous implementation allowed a write of zero
    bytes whereas kstrtox will return -EINVAL. Since this only affects a single
    debugfs endpoint, this should be a permissible slight change of semantics
    in exchange for 18 fewer lines of code.
    
    Signed-off-by: Mathias Rav <mathiasrav@gmail.com>

--- drivers/staging/lustre/lustre/obdclass/lprocfs_status.c
+++ drivers/staging/lustre/lustre/obdclass/lprocfs_status.c
@@ -399,27 +399,7 @@ EXPORT_SYMBOL(lprocfs_rd_uint);
 int lprocfs_wr_uint(struct file *file, const char __user *buffer,
 		    unsigned long count, void *data)
 {
-	unsigned *p = data;
-	char dummy[MAX_STRING_SIZE + 1], *end;
-	unsigned long tmp;
-
-	if (count >= sizeof(dummy))
-		return -EINVAL;
-
-	if (count == 0)
-		return 0;
-
-	if (copy_from_user(dummy, buffer, count))
-		return -EFAULT;
-
-	dummy[count] = '\0';
-
-	tmp = simple_strtoul(dummy, &end, 0);
-	if (dummy == end)
-		return -EINVAL;
-
-	*p = (unsigned int)tmp;
-	return count;
+	return kstrtouint_from_user(buffer, count, 0, (unsigned int *)data);
 }
 EXPORT_SYMBOL(lprocfs_wr_uint);
 
