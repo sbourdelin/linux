ee285fc90b9b62f028f1cf0432446a3451014bcf
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2017-05-08 09:46:40 +0200

    drm/nouveau/secboot: fix some error handling in 'ls_ucode_img_load_gr'
    The last goto looks spurious because it releases less resources than the
    previous one.
    Add a new label in order to free the memory allocated by the 'kmemdup'
    call.
    
    Fixes: 9d896f3e41a6 ("drm/nouveau/secboot: abstract LS firmware loading functions")
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>

--- drivers/gpu/drm/nouveau/nvkm/subdev/secboot/ls_ucode_gr.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/secboot/ls_ucode_gr.c
@@ -116,6 +116,7 @@ ls_ucode_img_load_gr(const struct nvkm_subdev *subdev, struct ls_ucode_img *img,
 	ret = nvkm_firmware_get(subdev->device, f, &sig);
 	if (ret)
 		goto free_data;
+
 	img->sig = kmemdup(sig->data, sig->size, GFP_KERNEL);
 	if (!img->sig) {
 		ret = -ENOMEM;
@@ -127,10 +128,12 @@ ls_ucode_img_load_gr(const struct nvkm_subdev *subdev, struct ls_ucode_img *img,
 					     &img->ucode_desc);
 	if (IS_ERR(img->ucode_data)) {
 		ret = PTR_ERR(img->ucode_data);
-		goto free_data;
+		goto free_img_sig;
 	}
 	img->ucode_size = img->ucode_desc.image_size;
 
+free_img_sig:
+	kfree(img->sig);
 free_sig:
 	nvkm_firmware_put(sig);
 free_data:
