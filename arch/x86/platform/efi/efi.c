e183a914c5e969ee8857fae72d0b3bcb3e9845a1
Sai Praneeth <sai.praneeth.prakhya@intel.com>
2017-05-10 15:49:05 -0700

    x86/efi: Add EFI_PGT_DUMP support for x86_32, kexec and efi=old_map
    EFI_PGT_DUMP, as the name suggests dumps efi page tables to dmesg during
    kernel boot. This feature is very useful while debugging page
    faults/null pointer dereferences to efi related addresses. Presently,
    this feature is limited only to x86_64, so let's extend it to other efi
    configurations like kexec kernel, efi=old_map and to x86_32 as well.
    This doesn't effect normal boot path because this config option should
    be used only for debug purposes.
    
    Signed-off-by: Sai Praneeth Prakhya <sai.praneeth.prakhya@intel.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Ricardo Neri <ricardo.neri@intel.com>
    Cc: Matt Fleming <matt@codeblueprint.co.uk>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Ravi Shankar <ravi.v.shankar@intel.com>

--- arch/x86/platform/efi/efi.c
+++ arch/x86/platform/efi/efi.c
@@ -829,6 +829,8 @@ static void __init kexec_enter_virtual_mode(void)
 	if (efi_enabled(EFI_OLD_MEMMAP) && (__supported_pte_mask & _PAGE_NX))
 		runtime_code_page_mkexec();
 
+	efi_dump_pagetable();
+
 	/* clean DUMMY object */
 	efi_delete_dummy_variable();
 #endif
