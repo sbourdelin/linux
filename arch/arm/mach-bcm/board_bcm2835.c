e9ab9fc63a0a2709cc90a169d0a052763d08ff5d
Marc Zyngier <marc.zyngier@arm.com>
2017-05-10 14:26:20 +0100

    irqchip/bcm2836: Move SMP startup code to arch/arm
    One of the RPi-2/3 irqchip's key features is that it contains some
    SMP startup code for the 32bit ARM architecture version. The only
    reason I can imagine for this is "RPi is special".
    
    Let's move this code where it belongs (in the platform support code),
    creating a shared include file for this purpose.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/mach-bcm/board_bcm2835.c
+++ arch/arm/mach-bcm/board_bcm2835.c
@@ -16,9 +16,42 @@
 #include <linux/irqchip.h>
 #include <linux/of_address.h>
 #include <linux/clk/bcm2835.h>
+#include <linux/irqchip/irq-bcm2836.h>
 
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
+
+#ifdef CONFIG_SMP
+static const struct of_device_id bcm2836_intc[] = {
+	{ .compatible = "brcm,bcm2836-l1-intc" },
+	{ },
+};
+
+static int bcm2836_smp_boot_secondary(unsigned int cpu, struct task_struct *idle)
+{
+	struct device_node *np;
+	void __iomem *base;
+
+	np = of_find_matching_node(NULL, bcm2836_intc);
+	if (!np)
+		return -ENODEV;
+
+	base = of_iomap(np, 0);
+	if (!base)
+		return -ENOMEM;
+
+	writel(virt_to_phys(secondary_startup),
+	       base + LOCAL_MAILBOX3_SET0 + 16 * cpu);
+
+	iounmap(base);
+
+	return 0;
+}
+
+static const struct smp_operations bcm2836_smp_ops = {
+	.smp_boot_secondary	= bcm2836_smp_boot_secondary,
+};
+#endif
 
 static void __init bcm2835_init(void)
 {
@@ -37,5 +70,6 @@ static const char * const bcm2835_compat[] = {
 
 DT_MACHINE_START(BCM2835, "BCM2835")
 	.init_machine = bcm2835_init,
-	.dt_compat = bcm2835_compat
+	.dt_compat = bcm2835_compat,
+	.smp = smp_ops(bcm2836_smp_ops),
 MACHINE_END
