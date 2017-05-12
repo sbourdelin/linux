1145f30659aa2a638bc9dcc94946e2b65a9090ec
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2017-05-12 10:59:44 -0300

    docs-rst: convert kernel-hacking to ReST
    Use pandoc to convert documentation to ReST by calling
    Documentation/sphinx/tmplcvt script.
    
    - Manually adjusted to use ..note and ..warning
    - Minor fixes for it to be parsed without errors
    - Use **bold** for emphasis.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

---
+++ Documentation/kernel-hacking/conf.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8; mode: python -*-
+
+project = "Unreliable Guide To Hacking The Linux Kernel"
+
+tags.add("subproject")
+
+latex_documents = [
+    ('index', 'kernel-hacking.tex', project,
+     'The kernel development community', 'manual'),
+]
