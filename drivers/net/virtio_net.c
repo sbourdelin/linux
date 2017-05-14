3c733494a93966ed625dbb9f5a4ad67b09a403a2
Maciej Fijalkowski <macfij7@wp.pl>
2017-05-14 19:51:28 +0200

    Fix ERROR: trailing statements should be on next line
    Signed-off-by: Maciej Fijalkowski <macfij7@wp.pl>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -224,7 +224,8 @@ static void give_pages(struct receive_queue *rq, struct page *page)
 	struct page *end;
 
 	/* Find end of list, sew whole thing into vi->rq.pages. */
-	for (end = page; end->private; end = (struct page *)end->private);
+	for (end = page; end->private; end = (struct page *)end->private)
+		;
 	end->private = (unsigned long)rq->pages;
 	rq->pages = page;
 }
