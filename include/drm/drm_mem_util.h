803d4fab37c8dbb2ad2860066f2700728c216e43
Michal Hocko <mhocko@suse.com>
2017-05-16 11:06:06 +0200

    drm: use kvmalloc_array for drm_malloc*
    drm_malloc* has grown their own kmalloc with vmalloc fallback
    implementations. MM has grown kvmalloc* helpers in the meantime. Let's
    use those because it a) reduces the code and b) MM has a better idea
    how to implement fallbacks (e.g. do not vmalloc before kmalloc is tried
    with __GFP_NORETRY).
    
    Signed-off-by: Michal Hocko <mhocko@suse.com>

--- include/drm/drm_mem_util.h
+++ include/drm/drm_mem_util.h
@@ -43,31 +43,12 @@ static __inline__ void *drm_calloc_large(size_t nmemb, size_t size)
 /* Modeled after cairo's malloc_ab, it's like calloc but without the zeroing. */
 static __inline__ void *drm_malloc_ab(size_t nmemb, size_t size)
 {
-	if (size != 0 && nmemb > SIZE_MAX / size)
-		return NULL;
-
-	if (size * nmemb <= PAGE_SIZE)
-	    return kmalloc(nmemb * size, GFP_KERNEL);
-
-	return vmalloc(size * nmemb);
+	return kvmalloc_array(nmemb, size, GFP_KERNEL);
 }
 
 static __inline__ void *drm_malloc_gfp(size_t nmemb, size_t size, gfp_t gfp)
 {
-	if (size != 0 && nmemb > SIZE_MAX / size)
-		return NULL;
-
-	if (size * nmemb <= PAGE_SIZE)
-		return kmalloc(nmemb * size, gfp);
-
-	if (gfp & __GFP_RECLAIMABLE) {
-		void *ptr = kmalloc(nmemb * size,
-				    gfp | __GFP_NOWARN | __GFP_NORETRY);
-		if (ptr)
-			return ptr;
-	}
-
-	return __vmalloc(size * nmemb, gfp, PAGE_KERNEL);
+	return kvmalloc_array(nmemb, size, gfp);
 }
 
 static __inline void drm_free_large(void *ptr)
