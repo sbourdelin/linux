76fa55841ea28a5da42252009ee93f5f9c0c0fd2
Wei Yang <richard.weiyang@gmail.com>
2017-05-24 18:03:47 +0800

    mm/vmalloc: a slight change of compare target in __insert_vmap_area()
    The vmap RB tree store the elements in order and no overlap between any of
    them. The comparison in __insert_vmap_area() is to decide which direction
    the search should follow and make sure the new vmap_area is not overlap
    with any other.
    
    Current implementation fails to do the overlap check.
    
    When first "if" is not true, it means
    
        va->va_start >= tmp_va->va_end
    
    And with the truth
    
        xxx->va_end > xxx->va_start
    
    The deduction is
    
        va->va_end > tmp_va->va_start
    
    which is the condition in second "if".
    
    This patch changes a little of the comparison in __insert_vmap_area() to
    make sure it forbids the overlapped vmap_area.
    
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -360,9 +360,9 @@ static void __insert_vmap_area(struct vmap_area *va)
 
 		parent = *p;
 		tmp_va = rb_entry(parent, struct vmap_area, rb_node);
-		if (va->va_start < tmp_va->va_end)
+		if (va->va_end <= tmp_va->va_start)
 			p = &(*p)->rb_left;
-		else if (va->va_end > tmp_va->va_start)
+		else if (va->va_start >= tmp_va->va_end)
 			p = &(*p)->rb_right;
 		else
 			BUG();
