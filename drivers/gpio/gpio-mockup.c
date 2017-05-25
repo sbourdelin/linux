20bcea08dbb20088be9c6b0d584684f4f9303382
Bartosz Golaszewski <brgl@bgdev.pl>
2017-05-25 10:33:45 +0200

    gpio: mockup: don't return magic numbers from probe()
    When the requested number of GPIO lines is 0, return -EINVAL, not
    -1 which is -EPERM.
    
    Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>

--- drivers/gpio/gpio-mockup.c
+++ drivers/gpio/gpio-mockup.c
@@ -381,7 +381,7 @@ static int gpio_mockup_probe(struct platform_device *pdev)
 			ret = gpio_mockup_add(dev, &chips[i],
 					      chip_name, base, ngpio);
 		} else {
-			ret = -1;
+			ret = -EINVAL;
 		}
 
 		if (ret) {
