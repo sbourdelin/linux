fb18d5d186631f74e70711ce1f097d2dc36bf817
Rodrigo Vivi <rodrigo.vivi@intel.com>
2017-05-30 15:42:57 -0700

    drm/i915/cfl: Introduce Coffee Lake platform definition.
    Coffee Lake is a Intel® Processor containing Intel® HD Graphics
    following Kabylake.
    
    It is Gen9 graphics based platform on top of CNP PCH.
    
    Let's start by adding the platform definition based on previous
    platforms but yet as preliminary_hw_support.
    
    On following patches we will start adding PCI IDs and the
    platform specific changes.
    
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -425,6 +425,14 @@ static const struct intel_device_info intel_kabylake_gt3_info = {
 	.ring_mask = RENDER_RING | BSD_RING | BLT_RING | VEBOX_RING | BSD2_RING,
 };
 
+static const struct intel_device_info intel_coffeelake_info = {
+	BDW_FEATURES,
+	.is_alpha_support = 1,
+	.platform = INTEL_COFFEELAKE,
+	.gen = 9,
+	.ddb_size = 896,
+};
+
 /*
  * Make sure any device matches here are from most specific to most
  * general.  For example, since the Quanta match is based on the subsystem
