91679e89c0969d64f2b11e9ed062e1f7862957f8
Maor Gottlieb <maorg@mellanox.com>
2017-05-30 10:29:13 +0300

    IB/mlx5: Add support to dropless RQ
    RQs that were configured for "delay drop" will prevent packet drops
    when their WQEs are depleted.
    Marking an RQ to be drop-less is done by setting delay_drop_en in RQ
    context using CREATE_RQ command.
    
    Since this feature is globally activated/deactivated by using the
    SET_DELAY_DROP command on all the marked RQs, we activated/deactivated
    it according to the number of RQs with 'delay_drop' enabled.
    
    When timeout is expired, then the feature is deactivated. Therefore
    the driver handles the delay drop timeout event and reactivate it.
    
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>
    Reviewed-by: Yishai Hadas <yishaih@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- drivers/infiniband/hw/mlx5/mlx5_ib.h
+++ drivers/infiniband/hw/mlx5/mlx5_ib.h
@@ -247,6 +247,10 @@ struct mlx5_ib_wq {
 	void		       *qend;
 };
 
+enum mlx5_ib_wq_flags {
+	MLX5_IB_WQ_FLAGS_DELAY_DROP = 0x1,
+};
+
 struct mlx5_ib_rwq {
 	struct ib_wq		ibwq;
 	struct mlx5_core_qp	core_qp;
@@ -264,6 +268,7 @@ struct mlx5_ib_rwq {
 	u32			wqe_count;
 	u32			wqe_shift;
 	int			wq_sig;
+	u32			create_flags; /* Use enum mlx5_ib_wq_flags */
 };
 
 enum {
@@ -618,6 +623,19 @@ struct mlx5_roce {
 	atomic_t		next_port;
 };
 
+enum {
+	MLX5_MAX_DELAY_DROP_TIMEOUT_MS = 100,
+};
+
+struct mlx5_ib_delay_drop {
+	struct mlx5_ib_dev     *dev;
+	struct work_struct	delay_drop_work;
+	/* serialize setting of delay drop */
+	struct mutex		lock;
+	u32			timeout;
+	bool			activate;
+};
+
 struct mlx5_ib_dev {
 	struct ib_device		ib_dev;
 	struct mlx5_core_dev		*mdev;
@@ -654,6 +672,7 @@ struct mlx5_ib_dev {
 	struct mlx5_ib_port	*port;
 	struct mlx5_sq_bfreg     bfreg;
 	struct mlx5_sq_bfreg     fp_bfreg;
+	struct mlx5_ib_delay_drop delay_drop;
 };
 
 static inline struct mlx5_ib_cq *to_mibcq(struct mlx5_core_cq *mcq)
