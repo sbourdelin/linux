a046e0740e5ac3c048b4d45751c5e5065d4a223c
Anshuman Khandual <khandual@linux.vnet.ibm.com>
2017-06-01 17:16:54 +0900

    mm: hugetlb: soft-offline: dissolve source hugepage after successful migration
    Currently hugepage migrated by soft-offline (i.e. due to correctable
    memory errors) is contained as a hugepage, which means many non-error
    pages in it are unreusable, i.e. wasted.
    
    This patch solves this issue by dissolving source hugepages into buddy.
    As done in previous patch, PageHWPoison is set only on a head page of
    the error hugepage. Then in dissoliving we move the PageHWPoison flag to
    the raw error page so that all healthy subpages return back to buddy.
    
    Signed-off-by: Anshuman Khandual <khandual@linux.vnet.ibm.com>
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -1251,6 +1251,8 @@ put_anon:
 out:
 	if (rc != -EAGAIN)
 		putback_active_hugepage(hpage);
+	if (reason == MR_MEMORY_FAILURE && !test_set_page_hwpoison(hpage))
+		num_poisoned_pages_inc();
 
 	/*
 	 * If migration was not successful and there's a freeing callback, use
