5ffde27ebd083f2f75b278796f9f2a2925795fce
Sergey Dyasli <sergey.dyasli@citrix.com>
2017-06-05 09:41:08 +0100

    xen: fix HYPERVISOR_dm_op() prototype
    Change the third parameter to be the required struct xen_dm_op_buf *
    instead of a generic void * (which blindly accepts any pointer).
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- arch/x86/include/asm/xen/hypercall.h
+++ arch/x86/include/asm/xen/hypercall.h
@@ -49,6 +49,7 @@
 #include <xen/interface/physdev.h>
 #include <xen/interface/platform.h>
 #include <xen/interface/xen-mca.h>
+#include <xen/interface/hvm/dm_op.h>
 
 /*
  * The hypercall asms have to meet several constraints:
@@ -474,7 +475,7 @@ HYPERVISOR_xenpmu_op(unsigned int op, void *arg)
 
 static inline int
 HYPERVISOR_dm_op(
-	domid_t dom, unsigned int nr_bufs, void *bufs)
+	domid_t dom, unsigned int nr_bufs, struct xen_dm_op_buf *bufs)
 {
 	return _hypercall3(int, dm_op, dom, nr_bufs, bufs);
 }
