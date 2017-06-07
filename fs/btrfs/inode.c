5319de820a7986bcb6feb698d879a86851eddc8a
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-07 03:58:39 +0300

    Btrfs: backref_comp decrease max compare count
    In worst case code do 6 comparison,
    just add some new checks to switch check branch faster
    now in worst case code do 4 comparison
    
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -2263,20 +2263,23 @@ struct new_sa_defrag_extent {
 static int backref_comp(struct sa_defrag_extent_backref *b1,
 			struct sa_defrag_extent_backref *b2)
 {
-	if (b1->root_id < b2->root_id)
-		return -1;
-	else if (b1->root_id > b2->root_id)
+	if (b1->root_id != b2->root_id) {
+		if (b1->root_id < b2->root_id)
+			return -1;
 		return 1;
+	}
 
-	if (b1->inum < b2->inum)
-		return -1;
-	else if (b1->inum > b2->inum)
+	if (b1->inum != b2->inum) {
+		if (b1->inum < b2->inum)
+			return -1;
 		return 1;
+	}
 
-	if (b1->file_pos < b2->file_pos)
-		return -1;
-	else if (b1->file_pos > b2->file_pos)
+	if (b1->file_pos != b2->file_pos) {
+		if (b1->file_pos < b2->file_pos)
+			return -1;
 		return 1;
+	}
 
 	/*
 	 * [------------------------------] ===> (a range of space)
