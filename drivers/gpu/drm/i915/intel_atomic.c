08a9c1d9bb10d7440fd2f2e68cd7fb930a2c2f44
Chandra Konduru <chandra.konduru@intel.com>
2017-06-07 10:21:21 +0530

    drm/i915: Set scaler mode for NV12
    This patch sets appropriate scaler mode for NV12 format.
    In this mode, skylake scaler does either chroma-upsampling or
    chroma-upsampling and resolution scaling
    
    Link: https://patchwork.kernel.org/patch/6426161/
    Signed-off-by: Chandra Konduru <chandra.konduru@intel.com>
    Signed-off-by: Nabendu Maiti <nabendu.bikash.maiti@intel.com>
    Signed-off-by: Vidya Srinivas <vidya.srinivas@intel.com>

--- drivers/gpu/drm/i915/intel_atomic.c
+++ drivers/gpu/drm/i915/intel_atomic.c
@@ -250,6 +250,12 @@ int intel_atomic_setup_scalers(struct drm_i915_private *dev_priv,
 		/* set scaler mode */
 		if (IS_GEMINILAKE(dev_priv)) {
 			scaler_state->scalers[*scaler_id].mode = 0;
+		} else if (plane_state && plane_state->base.fb &&
+			plane_state->base.fb->format->format ==
+			DRM_FORMAT_NV12) {
+			DRM_ERROR("NV12 format setting scaler mode\n");
+			scaler_state->scalers[*scaler_id].mode =
+				PS_SCALER_MODE_NV12;
 		} else if (num_scalers_need == 1 && intel_crtc->pipe != PIPE_C) {
 			/*
 			 * when only 1 scaler is in use on either pipe A or B,
