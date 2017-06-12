46b85ab7d6bb0f5e1bc1734def113321431005d6
Salvatore Mesoraca <s.mesoraca16@gmail.com>
2017-06-12 18:56:57 +0200

    Creation of "pagefault_handler_x86" LSM hook
    Creation of a new hook to let LSM modules handle user-space pagefaults on
    x86.
    It can be used to avoid segfaulting the originating process.
    If it's the case it can modify process registers before returning.
    
    Signed-off-by: Salvatore Mesoraca <s.mesoraca16@gmail.com>
    Cc: x86@kernel.org
    Cc: Ingo Molnar <mingo@redhat.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -487,6 +487,11 @@
  *	Check if the requested @vmflags are allowed.
  *	@vmflags contains requested the vmflags.
  *	Return 0 if the operation is allowed to continue.
+ * @pagefault_handler_x86:
+ *	Handle pagefaults on x86.
+ *	@regs contains process' registers.
+ *	@error_code contains error code for the pagefault.
+ *	@address contains the address that caused the pagefault.
  * @file_lock:
  *	Check permission before performing file locking operations.
  *	Note: this hook mediates both flock and fcntl style locks.
@@ -1487,6 +1492,9 @@ union security_list_options {
 	int (*file_mprotect)(struct vm_area_struct *vma, unsigned long reqprot,
 				unsigned long prot);
 	int (*check_vmflags)(vm_flags_t vmflags);
+	int (*pagefault_handler_x86)(struct pt_regs *regs,
+				     unsigned long error_code,
+				     unsigned long address);
 	int (*file_lock)(struct file *file, unsigned int cmd);
 	int (*file_fcntl)(struct file *file, unsigned int cmd,
 				unsigned long arg);
@@ -1759,6 +1767,7 @@ struct security_hook_heads {
 	struct list_head mmap_file;
 	struct list_head file_mprotect;
 	struct list_head check_vmflags;
+	struct list_head pagefault_handler_x86;
 	struct list_head file_lock;
 	struct list_head file_fcntl;
 	struct list_head file_set_fowner;
