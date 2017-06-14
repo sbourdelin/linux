c1f3257752422df0b9d88a69bec3ed45e832286b
Jacob Pan <jacob.jun.pan@linux.intel.com>
2017-06-14 15:22:55 -0700

    iommu: Introduce bind_pasid_table API function
    Virtual IOMMU was proposed to support Shared Virtual Memory (SVM) use
    case in the guest:
    https://lists.gnu.org/archive/html/qemu-devel/2016-11/msg05311.html
    
    As part of the proposed architecture, when a SVM capable PCI
    device is assigned to a guest, nested mode is turned on. Guest owns the
    first level page tables (request with PASID) and performs GVA->GPA
    translation. Second level page tables are owned by the host for GPA->HPA
    translation for both request with and without PASID.
    
    A new IOMMU driver interface is therefore needed to perform tasks as
    follows:
    * Enable nested translation and appropriate translation type
    * Assign guest PASID table pointer (in GPA) and size to host IOMMU
    
    This patch introduces new functions called iommu_(un)bind_pasid_table()
    to IOMMU APIs. Architecture specific IOMMU function can be added later
    to perform the specific steps for binding pasid table of assigned devices.
    
    This patch also adds model definition in iommu.h. It would be used to
    check if the bind request is from a compatible entity. e.g. a bind
    request from an intel_iommu emulator may not be supported by an ARM SMMU
    driver.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Liu, Yi L <yi.l.liu@linux.intel.com>
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>

---
+++ include/uapi/linux/iommu.h
@@ -0,0 +1,33 @@
+/*
+ * IOMMU user API definitions
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _UAPI_IOMMU_H
+#define _UAPI_IOMMU_H
+
+/**
+ * PASID table data used to bind guest PASID table to the host IOMMU. This will
+ * enable guest managed first level page tables.
+ * @ptr		PASID table pointer in GPA
+ * @size	size of the guest PASID table, must be <= host table size
+ * @model	magic number tells vendor apart
+ * @length	length of the opaque data
+ * @opaque	architecture specific IOMMU data
+ */
+struct pasid_table_info {
+	__u64	ptr;
+	__u64	size;
+	__u32	model;
+#define INTEL_IOMMU	(1 << 0)
+#define ARM_SMMU	(1 << 1)
+	__u32	length;
+	__u8	opaque[];
+};
+
+
+#endif /* _UAPI_IOMMU_H */
