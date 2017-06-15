b2e0a42e32d4eb9d5ee99264616dbba8fc288944
Mark Brown <broonie@kernel.org>
2017-06-15 20:13:33 +0100

    ASoC: rockchip: Fix build
    Reported-by: Christophe Jaillet <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/rockchip/rockchip_i2s.c
+++ sound/soc/rockchip/rockchip_i2s.c
@@ -641,7 +641,7 @@ static int rockchip_i2s_probe(struct platform_device *pdev)
 			goto err_pm_disable;
 	}
 
-	soc_dai = devm_kmemdup(&pdev->dev, &rockchip_i2s_dai
+	soc_dai = devm_kmemdup(&pdev->dev, &rockchip_i2s_dai,
 			       sizeof(*soc_dai), GFP_KERNEL);
 	if (!soc_dai) {
 		err = -ENOMEM;
