c022b5de8e8071c8b97008359bedab9944490401
Gustavo Padovan <gustavo.padovan@collabora.com>
2017-06-16 16:39:07 +0900

    uvc: enable subscriptions to other events
    Call v4l2_ctrl_subscribe_event to subscribe to more events supported by
    v4l.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>

--- drivers/media/usb/uvc/uvc_v4l2.c
+++ drivers/media/usb/uvc/uvc_v4l2.c
@@ -1240,7 +1240,7 @@ static int uvc_ioctl_subscribe_event(struct v4l2_fh *fh,
 	case V4L2_EVENT_CTRL:
 		return v4l2_event_subscribe(fh, sub, 0, &uvc_ctrl_sub_ev_ops);
 	default:
-		return -EINVAL;
+		return v4l2_ctrl_subscribe_event(fh, sub);
 	}
 }
 
