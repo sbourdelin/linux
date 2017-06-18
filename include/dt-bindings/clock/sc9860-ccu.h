0017c8358af72b693ed6d07e2578bbd924182a6b
Chunyan Zhang <chunyan.zhang@spreadtrum.com>
2017-06-18 09:58:54 +0800

    clk: sprd: add clocks support for SC9860
    Add the list of clocks in SC9860 CCU.
    
    Original-by: Xiaolong Zhang <xiaolong.zhang@spreadtrum.com>
    Signed-off-by: Chunyan Zhang <zhang.chunyan@linaro.org>

---
+++ include/dt-bindings/clock/sc9860-ccu.h
@@ -0,0 +1,19 @@
+/*
+ * Spreadtrum SC9860 platform clocks
+ *
+ * Copyright (C) 2017, Spreadtrum Communications Inc.
+ *
+ * SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+ */
+
+#ifndef _DT_BINDINGS_CLK_SC9860_CCU_H_
+#define _DT_BINDINGS_CLK_SC9860_CCU_H_
+
+#define CLK_FAC_1M	2
+#define CLK_EMMC_2X_EN	29
+#define CLK_L0_409M6	60
+#define CLK_EMMC_2X	88
+#define CLK_EMMC_EB	158
+
+
+#endif /* _DT_BINDINGS_CLK_SC9860_CCU_H_ */
