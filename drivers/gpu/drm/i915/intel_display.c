d5e045acc471546d42a25ba4dcd456f56273dae0
Shashank Sharma <shashank.sharma@intel.com>
2017-06-19 21:38:19 +0530

    drm/i915: prepare pipe for YCBCR output
    To get HDMI YCBCR420 output, the PIPEMISC register should be
    programmed to:
    - Generate YCBCR output (bit 11)
    - In case of YCBCR420 outputs, it should be programmed in full
      blend mode to use the scaler in 5x3 ratio (bits 26 and 27)
    
    This patch:
    - Adds definition of these bits.
    - Programs PIPEMISC for YCBCR outputs.
    
    V2: rebase
    V3: rebase
    V4: rebase
    
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Cc: Ander Conselvan De Oliveira <ander.conselvan.de.oliveira@intel.com>
    Cc: Daniel Vetter <daniel.vetter@intel.com>
    Signed-off-by: Shashank Sharma <shashank.sharma@intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -8103,6 +8103,15 @@ static void haswell_set_pipemisc(struct drm_crtc *crtc)
 		if (intel_crtc->config->dither)
 			val |= PIPEMISC_DITHER_ENABLE | PIPEMISC_DITHER_TYPE_SP;
 
+		if (hdmi_out > DRM_HDMI_OUTPUT_DEFAULT_RGB) {
+			val |= PIPEMISC_OUTPUT_YCBCR;
+
+			if (hdmi_out == DRM_HDMI_OUTPUT_YCBCR420) {
+				val |= PIPEMISC_YCBCR420_ENABLE |
+				       PIPEMISC_YCBCR420_MODE_BLEND;
+			}
+		}
+
 		I915_WRITE(PIPEMISC(intel_crtc->pipe), val);
 	}
 }
