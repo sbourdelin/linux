c180376cafd16b13756bfb0f1993b8000e6d4ddf
Sumit Semwal <sumit.semwal@linaro.org>
2017-06-19 19:14:11 +0530

    selftests: lib: Skip tests on missing test modules
    With older kernels, printf.sh and bitmap.sh fail because they can't find
    the respective test modules they are looking for.
    
    Add the skip portion on missing the respective test_XXX module. Error out
    with the same error code as prime_numbers.sh.
    
    v2: Per Shuah's review, search for the module rather than do modprobe.
    
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>

--- tools/testing/selftests/lib/bitmap.sh
+++ tools/testing/selftests/lib/bitmap.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # Runs bitmap infrastructure tests using test_bitmap kernel module
 
+if ! find /lib/modules/$(uname -r) -type f -name test_bitmap.ko | grep -q .;
+then
+	echo "bitmap: test_bitmap.ko not found [SKIP]"
+	exit 77
+fi
+
 if /sbin/modprobe -q test_bitmap; then
 	/sbin/modprobe -q -r test_bitmap
 	echo "bitmap: ok"
--- tools/testing/selftests/lib/printf.sh
+++ tools/testing/selftests/lib/printf.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # Runs printf infrastructure using test_printf kernel module
 
+if ! find /lib/modules/$(uname -r) -type f -name test_printf.ko | grep -q .;
+then
+        echo "printf: test_printf.ko not found [SKIP]"
+        exit 77
+fi
+
 if /sbin/modprobe -q test_printf; then
 	/sbin/modprobe -q -r test_printf
 	echo "printf: ok"
