a51558512dfeb4b61a6f50a7e7ce9d0097dac3d8
Kees Cook <keescook@chromium.org>
2017-06-20 22:58:35 -0700

    binfmt_elf: Use ELF_ET_DYN_BASE only for PIE
    The ELF_ET_DYN_BASE position was originally intended to keep loaders
    away from ET_EXEC binaries. (For example, running "/lib/ld-linux.so.2
    /bin/cat" might cause the subsequent load of /bin/cat into where the
    loader had been loaded.) With the advent of PIE (ET_DYN binaries with
    an INTERP Program Header), ELF_ET_DYN_BASE continued to be used since
    the kernel was only looking at ET_DYN. However, since ELF_ET_DYN_BASE
    is traditionally set at the top 1/3rd of the TASK_SIZE, a substantial
    portion of the address space is unused.
    
    For 32-bit tasks when RLIMIT_STACK is set to RLIM_INFINITY, programs
    are loaded below the mmap region. This means they can be made to collide
    (CVE-2017-1000370) or nearly collide (CVE-2017-1000371) with pathological
    stack regions. Lowering ELF_ET_DYN_BASE solves both by moving programs
    above the mmap region in all cases, and will now additionally avoid
    programs falling back to the mmap region by enforcing MAP_FIXED for
    program loads (i.e. if it would have collided with the stack, now it
    will fail to load instead of falling back to the mmap region).
    
    To allow for a lower ELF_ET_DYN_BASE, loaders (ET_DYN without INTERP)
    are loaded into the mmap region, leaving space available for either an
    ET_EXEC binary with a fixed location or PIE being loaded into mmap by the
    loader. Only PIE programs are loaded offset from ELF_ET_DYN_BASE, which
    means architectures can now safely lower their values without risk of
    loaders colliding with their subsequently loaded programs.
    
    Thanks go to PaX for inspiration on how to approach this solution.
    
    Fixes: d1fd836dcf00 ("mm: split ET_DYN ASLR from mmap ASLR")
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- arch/x86/include/asm/elf.h
+++ arch/x86/include/asm/elf.h
@@ -245,12 +245,8 @@ extern int force_personality32;
 #define CORE_DUMP_USE_REGSET
 #define ELF_EXEC_PAGESIZE	4096
 
-/* This is the location that an ET_DYN program is loaded if exec'ed.  Typical
-   use of this is to invoke "./ld.so someprog" to test out a new version of
-   the loader.  We need to make sure that it is out of the way of the program
-   that it will "exec", and that there is sufficient room for the brk.  */
-
-#define ELF_ET_DYN_BASE		(TASK_SIZE / 3 * 2)
+/* This is the base location for PIE (ET_DYN with INTERP) loads. */
+#define ELF_ET_DYN_BASE		0x400000UL
 
 /* This yields a mask that user programs can use to figure out what
    instruction set this CPU supports.  This could be done in user space,
