c1e4eab225bcdadb60f720f2560dc2b1e9370f91
Matthias Schiffer <mschiffer@universe-factory.net>
2017-06-25 23:56:02 +0200

    net: add netlink_ext_ack argument to rtnl_link_ops.slave_changelink
    Add support for extended error reporting.
    
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -2629,7 +2629,8 @@ replay:
 					return -EOPNOTSUPP;
 
 				err = m_ops->slave_changelink(master_dev, dev,
-							      tb, slave_data);
+							      tb, slave_data,
+							      extack);
 				if (err < 0)
 					return err;
 				status |= DO_SETLINK_NOTIFY;
