174ad0890724b2664dd1b92a6a40e6c39c829e88
Matthias Schiffer <mschiffer@universe-factory.net>
2017-06-25 23:56:00 +0200

    net: add netlink_ext_ack argument to rtnl_link_ops.changelink
    Add support for extended error reporting.
    
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -2618,7 +2618,7 @@ replay:
 				    !ops->changelink)
 					return -EOPNOTSUPP;
 
-				err = ops->changelink(dev, tb, data);
+				err = ops->changelink(dev, tb, data, extack);
 				if (err < 0)
 					return err;
 				status |= DO_SETLINK_NOTIFY;
