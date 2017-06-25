3ac3dad725c993200703297fc570e5d46876f41d
Matthias Schiffer <mschiffer@universe-factory.net>
2017-06-25 23:56:03 +0200

    net: add netlink_ext_ack argument to rtnl_link_ops.slave_validate
    Add support for extended error reporting.
    
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -2599,7 +2599,8 @@ replay:
 				slave_data = slave_attr;
 			}
 			if (m_ops->slave_validate) {
-				err = m_ops->slave_validate(tb, slave_data);
+				err = m_ops->slave_validate(tb, slave_data,
+							    extack);
 				if (err < 0)
 					return err;
 			}
