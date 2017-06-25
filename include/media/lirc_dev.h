c4d57f136758af0cb6014aa233fa67b69cc97a3a
David Härdeman <david@hardeman.nu>
2017-06-25 14:31:40 +0200

    lirc_dev: make better use of file->private_data
    By making better use of file->private_data in lirc_dev we can avoid
    digging around in the irctls[] array, thereby simplifying the code.
    
    External drivers need to use lirc_get_pdata() instead of mucking around
    in file->private_data.
    
    The newly introduced lirc_init_pdata() function isn't very elegant, but
    it's a stopgap measure which can be removed once lirc_zilog is converted
    to rc-core.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>

--- include/media/lirc_dev.h
+++ include/media/lirc_dev.h
@@ -179,6 +179,9 @@ extern int lirc_register_driver(struct lirc_driver *d);
 
 extern void lirc_unregister_driver(struct lirc_driver *d);
 
+/* Must be called in the open fop before lirc_get_pdata() can be used */
+void lirc_init_pdata(struct inode *inode, struct file *file);
+
 /* Returns the private data stored in the lirc_driver
  * associated with the given device file pointer.
  */
