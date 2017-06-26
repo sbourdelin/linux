2731307382570c841b828fe7ce23820c72d7b1e9
Ming Lei <ming.lei@redhat.com>
2017-06-26 20:10:22 +0800

    fs/mpage: convert to bio_for_each_segment_all_sp()
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- fs/mpage.c
+++ fs/mpage.c
@@ -46,9 +46,10 @@
 static void mpage_end_io(struct bio *bio)
 {
 	struct bio_vec *bv;
+	struct bvec_iter_all bia;
 	int i;
 
-	bio_for_each_segment_all(bv, bio, i) {
+	bio_for_each_segment_all_sp(bv, bio, i, bia) {
 		struct page *page = bv->bv_page;
 		page_endio(page, op_is_write(bio_op(bio)),
 				blk_status_to_errno(bio->bi_status));
