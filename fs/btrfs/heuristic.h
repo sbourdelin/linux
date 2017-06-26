88006c3229b336c77f45e413ecf3160b9fb98e7c
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-26 04:24:56 +0300

    Btrfs: add heuristic method for make decision compress or not compress
    Add a heuristic computation before compression,
    for avoiding load resource heavy compression workspace,
    if data are probably can't be compressed.
    
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

---
+++ fs/btrfs/heuristic.h
@@ -0,0 +1,13 @@
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+
+enum compression_advice {
+	COMPRESS_NONE,
+	COMPRESS_COST_EASY,
+	COMPRESS_COST_MEDIUM,
+	COMPRESS_COST_HARD
+};
+
+enum compression_advice btrfs_compress_heuristic(struct inode *inode,
+	u64 start, u64 end);
