bf475c22f5969831fa839f07b9783eac38a9b42a
Wanpeng Li <wanpeng.li@hotmail.com>
2017-06-26 18:47:01 -0700

    KVM: async_pf: Let host know whether the guest support delivery async_pf as #PF vmexit
    Adds another flag bit (bit 2) to MSR_KVM_ASYNC_PF_EN. If bit 2 is 1, async
    page faults are delivered to L1 as #PF vmexits; if bit 2 is 0, kvm_can_do_async_pf
    returns 0 if in guest mode.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -330,7 +330,8 @@ static void kvm_guest_cpu_init(void)
 #ifdef CONFIG_PREEMPT
 		pa |= KVM_ASYNC_PF_SEND_ALWAYS;
 #endif
-		wrmsrl(MSR_KVM_ASYNC_PF_EN, pa | KVM_ASYNC_PF_ENABLED);
+		pa |= KVM_ASYNC_PF_DELIVERY_AS_PF_VMEXIT | KVM_ASYNC_PF_ENABLED;
+		wrmsr_safe(MSR_KVM_ASYNC_PF_EN, pa & 0xffffffff, pa >> 32);
 		__this_cpu_write(apf_reason.enabled, 1);
 		printk(KERN_INFO"KVM setup async PF for cpu %d\n",
 		       smp_processor_id());
