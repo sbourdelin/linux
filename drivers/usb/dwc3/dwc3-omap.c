a363dc1d84a0aaacce1f89ed6b803cf30bdff6ab
Grygorii Strashko <grygorii.strashko@ti.com>
2017-06-28 18:31:31 -0500

    usb: dwc3: omap: remove IRQ_NOAUTOEN used with shared irq
    IRQ_NOAUTOEN can't be used with shared IRQs and Kernel now will triggers
    warning if it happns, since commit 04c848d39879 ("genirq: Warn when
    IRQ_NOAUTOEN is used with shared interrupts"). And this is the case for
    OMAP DWC 3 driver.
    
    Hence, remove IRQ_NOAUTOEN flag and instead call disable_irq() before
    disabling PM runtime in probe error path handling.
    
    Fixes: 12a7f17fac5b ("usb: dwc3: omap: fix race of pm runtime with...")
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>

--- drivers/usb/dwc3/dwc3-omap.c
+++ drivers/usb/dwc3/dwc3-omap.c
@@ -512,7 +512,6 @@ static int dwc3_omap_probe(struct platform_device *pdev)
 
 	/* check the DMA Status */
 	reg = dwc3_omap_readl(omap->base, USBOTGSS_SYSCONFIG);
-	irq_set_status_flags(omap->irq, IRQ_NOAUTOEN);
 	ret = devm_request_threaded_irq(dev, omap->irq, dwc3_omap_interrupt,
 					dwc3_omap_interrupt_thread, IRQF_SHARED,
 					"dwc3-omap", omap);
@@ -533,10 +532,10 @@ static int dwc3_omap_probe(struct platform_device *pdev)
 	}
 
 	dwc3_omap_enable_irqs(omap);
-	enable_irq(omap->irq);
 	return 0;
 
 err1:
+	disable_irq(omap->irq);
 	pm_runtime_put_sync(dev);
 	pm_runtime_disable(dev);
 
