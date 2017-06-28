daf774c256de2a8d33a74071cbf2db9ce03e88a5
Pieter Jansen van Vuuren <pieter.jansenvanvuuren@netronome.com>
2017-06-28 22:29:56 +0200

    nfp: provide infrastructure for offloading flower based TC filters
    Adds a flower based TC offload handler for representor devices, this
    is in addition to the bpf based offload handler. The changes in this
    patch will be used in a follow-up patch to add tc flower offload to
    the NFP.
    
    The flower app enables tc offloads on representors by default.
    
    Signed-off-by: Pieter Jansen van Vuuren <pieter.jansenvanvuuren@netronome.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

--- drivers/net/ethernet/netronome/nfp/nfp_port.h
+++ drivers/net/ethernet/netronome/nfp/nfp_port.h
@@ -37,6 +37,7 @@
 #include <net/devlink.h>
 #include <net/switchdev.h>
 
+struct tc_to_netdev;
 struct net_device;
 struct nfp_app;
 struct nfp_pf;
@@ -108,6 +109,10 @@ struct nfp_port {
 };
 
 extern const struct switchdev_ops nfp_port_switchdev_ops;
+
+int
+nfp_port_setup_tc(struct net_device *netdev, u32 handle, u32 chain_index,
+		  __be16 proto, struct tc_to_netdev *tc);
 
 struct nfp_port *nfp_port_from_netdev(struct net_device *netdev);
 struct nfp_port *
