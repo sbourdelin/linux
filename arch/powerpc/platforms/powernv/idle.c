67562da70cb5b183795b0339a5472f715aaff8b4
Joel Stanley <joel@jms.id.au>
2017-07-03 13:31:52 +0930

    powerpc/powernv/idle: Put pnv_cpu_offline behind HOTPLUG_CPU
    In commit 900612315788 ("powerpc/powernv/smp: Add busy-wait loop as fall back
    for CPU-Hotplug") the idle code uses generic_check_cpu_restart(), but that
    function is not available when CONFIG_HOTPLUG_CPU is disabled.
    
    arch/powerpc/platforms/powernv/idle.c: In function ‘pnv_cpu_offline’:
    arch/powerpc/platforms/powernv/idle.c:286:11: error: implicit declaration of
    function ‘generic_check_cpu_restart’ [-Werror=implicit-function-declaration]
       while (!generic_check_cpu_restart(cpu)) {
               ^~~~~~~~~~~~~~~~~~~~~~~~~
    
    The callers of pnv_cpu_offline are behind CONFIG_HOTPLUG_CPU, so fix the build
    error by putting this code behind the same gard.
    
    Fixes: 900612315788 ("powerpc/powernv/smp: Add busy-wait loop as fall back for CPU-Hotplug").
    Cc: <stable@vger.kernel.org> # 4.12
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- arch/powerpc/platforms/powernv/idle.c
+++ arch/powerpc/platforms/powernv/idle.c
@@ -261,6 +261,8 @@ static u64 pnv_deepest_stop_psscr_val;
 static u64 pnv_deepest_stop_psscr_mask;
 static bool deepest_stop_found;
 
+#ifdef CONFIG_HOTPLUG_CPU
+
 /*
  * pnv_cpu_offline: A function that puts the CPU into the deepest
  * available platform idle state on a CPU-Offline.
@@ -293,6 +295,8 @@ unsigned long pnv_cpu_offline(unsigned int cpu)
 
 	return srr1;
 }
+
+#endif /* CONFIG_HOTPLUG_CPU */
 
 /*
  * Power ISA 3.0 idle initialization.
