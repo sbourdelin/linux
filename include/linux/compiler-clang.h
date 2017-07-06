9c4f5c855ce464e06255701923f7baf094242b14
Greg Hackmann <ghackmann@google.com>
2017-07-06 15:01:14 -0700

    kasan: add compiler support for clang
    For now we can hard-code ASAN ABI level 5, since historical clang builds
    can't build the kernel anyway.  We also need to emulate gcc's
    __SANITIZE_ADDRESS__ flag, or memset() calls won't be instrumented.
    
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -23,3 +23,13 @@
  */
 #undef inline
 #define inline inline __attribute__((unused)) notrace
+
+/* all clang versions usable with the kernel support KASAN ABI version 5
+ */
+#define KASAN_ABI_VERSION 5
+
+/* emulate gcc's __SANITIZE_ADDRESS__ flag
+ */
+#if __has_feature(address_sanitizer)
+#define __SANITIZE_ADDRESS__
+#endif
