5abcd47fd63b9e8f48ff7c40d84a5c48e795a6d7
Ludovic Barre <ludovic.barre@st.com>
2017-07-07 09:26:28 +0200

    irqchip: stm32: fix initial values
    -after cold boot, imr default value depend of hw configuration
    -after hot reboot the registers must be cleared to avoid residue
    
    Signed-off-by: Ludovic Barre <ludovic.barre@st.com>

--- drivers/irqchip/irq-stm32-exti.c
+++ drivers/irqchip/irq-stm32-exti.c
@@ -246,7 +246,16 @@ static int __init stm32_exti_init(struct stm32_exti_bank **stm32_exti_banks,
 		irqs_mask = readl_relaxed(base + stm32_bank->rtsr_ofst);
 		stm32_bank->irqs_mask = irqs_mask;
 		nr_exti = fls(readl_relaxed(base + stm32_bank->rtsr_ofst));
+
+		/*
+		 * This IP has no reset, so after hot reboot we should
+		 * clear registers to avoid residue
+		 */
+		writel_relaxed(0, base + stm32_bank->imr_ofst);
+		writel_relaxed(0, base + stm32_bank->emr_ofst);
 		writel_relaxed(0, base + stm32_bank->rtsr_ofst);
+		writel_relaxed(0, base + stm32_bank->ftsr_ofst);
+		writel_relaxed(~0UL, base + stm32_bank->pr_ofst);
 
 		pr_info("%s: bank%d, External IRQs available:%#x\n",
 			node->full_name, i, irqs_mask);
