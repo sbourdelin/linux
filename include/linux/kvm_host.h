f71184b68e7e103eb025edeb1938fffc96d83396
Christian Borntraeger <borntraeger@de.ibm.com>
2017-07-07 15:50:37 +0200

    KVM: use correct accessor function for __kvm_memslots
    kvm memslots are protected by srcu and not by rcu. We must use
    srcu_dereference_check instead of rcu_dereference_check.
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -562,7 +562,7 @@ void kvm_put_kvm(struct kvm *kvm);
 
 static inline struct kvm_memslots *__kvm_memslots(struct kvm *kvm, int as_id)
 {
-	return rcu_dereference_check(kvm->memslots[as_id],
+	return srcu_dereference_check(kvm->memslots[as_id], &kvm->srcu,
 			srcu_read_lock_held(&kvm->srcu)
 			|| lockdep_is_held(&kvm->slots_lock));
 }
