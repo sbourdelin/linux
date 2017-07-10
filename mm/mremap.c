f73c267864e4cfa5aa34bdf92ac522d3175ba9b5
Anshuman Khandual <khandual@linux.vnet.ibm.com>
2017-07-10 16:40:59 +0530

    mm/mremap: Remove redundant checks inside vma_expandable()
    As 'delta' is an unsigned long, 'end' (vma->vm_end + delta) cannot
    be less than 'vma->vm_end'. Checking for availability of virtual
    address range at the end of the VMA for the incremental size is
    also reduntant at this point. Hence drop them both.
    
    Signed-off-by: Anshuman Khandual <khandual@linux.vnet.ibm.com>

--- mm/mremap.c
+++ mm/mremap.c
@@ -487,12 +487,9 @@ out:
 static int vma_expandable(struct vm_area_struct *vma, unsigned long delta)
 {
 	unsigned long end = vma->vm_end + delta;
-	if (end < vma->vm_end) /* overflow */
-		return 0;
-	if (vma->vm_next && vma->vm_next->vm_start < end) /* intersection */
-		return 0;
-	if (get_unmapped_area(NULL, vma->vm_start, end - vma->vm_start,
-			      0, MAP_FIXED) & ~PAGE_MASK)
+
+	/* Intersection with next VMA */
+	if (vma->vm_next && vma->vm_next->vm_start < end)
 		return 0;
 	return 1;
 }
