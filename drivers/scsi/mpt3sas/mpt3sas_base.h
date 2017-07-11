2d16a79eea8646fef28ffbf4cc866b4dc2e62b86
Suganath Prabu S <suganath-prabu.subramani@broadcom.com>
2017-07-11 14:25:10 +0530

    mpt3sas: NVMe drive support for BTDHMAPPING ioctl command and log info
    * Added debug prints for pcie devices in ioctl debug path. Which
    will be helpful for debugging.
    * Added PCIe device support for ioctl BTDHMAPPING ioctl.
    
    Signed-off-by: Chaitra P B <chaitra.basappa@broadcom.com>
    Signed-off-by: Suganath Prabu S <suganath-prabu.subramani@broadcom.com>

--- drivers/scsi/mpt3sas/mpt3sas_base.h
+++ drivers/scsi/mpt3sas/mpt3sas_base.h
@@ -452,6 +452,7 @@ struct _internal_cmd {
 	struct completion done;
 	void	*reply;
 	void	*sense;
+	u64	*nvme_error_response;
 	u16	status;
 	u16	smid;
 };
@@ -1615,7 +1616,7 @@ void
 mpt3sas_scsi_direct_io_set(struct MPT3SAS_ADAPTER *ioc, u16 smid, u8 direct_io);
 void
 mpt3sas_setup_direct_io(struct MPT3SAS_ADAPTER *ioc, struct scsi_cmnd *scmd,
-	struct _raid_device *raid_device, Mpi2SCSIIORequest_t *mpi_request,
+	struct _raid_device *raid_device, Mpi25SCSIIORequest_t *mpi_request,
 	u16 smid);
 
 /* NCQ Prio Handling Check */
