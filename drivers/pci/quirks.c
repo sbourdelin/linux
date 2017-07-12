69471f623565aa98609a5375d422a0131707bcb4
Sinan Kaya <okaya@codeaurora.org>
2017-07-12 00:04:14 -0400

    PCI: Add Extended Tags quirk for Broadcom HT2100 Root Port
    All PCIe devices are expected to be able to handle 8-bit tags.
    'commit 60db3a4d8cc9 ("PCI: Enable PCIe Extended Tags if supported")'
    enabled extended tags for all devices based on the spec direction.
    
    The Broadcom HT2100 seems to be having issues with handling
    8-bit tags. Mark it as broken.
    
    If a device with extended tags quirk is found, disable extended tags for
    all devices in the tree assuming peer-to-peer is possible.
    
    The pci_walk_bus() in the quirk handles all devices we've already
    enumerated, and all devices we'll enumerate in the future are handled in
    pci_configure_device().
    
    Reported-by: Wim ten Have <wim.ten.have@oracle.com>
    Link: https://bugzilla.redhat.com/show_bug.cgi?id=1467674
    Fixes: 60db3a4d8cc9 ("PCI: Enable PCIe Extended Tags if supported")
    Signed-off-by: Sinan Kaya <okaya@codeaurora.org>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -4681,3 +4681,22 @@ static void quirk_intel_no_flr(struct pci_dev *dev)
 }
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_INTEL, 0x1502, quirk_intel_no_flr);
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_INTEL, 0x1503, quirk_intel_no_flr);
+
+static void quirk_exttags_completer(struct pci_dev *pdev)
+{
+	struct pci_host_bridge *bridge = pci_find_host_bridge(pdev->bus);
+
+	if (!bridge)
+		return;
+
+	bridge->broken_ext_tags = 1;
+	dev_info(&pdev->dev, "Extended Tag handling is broken\n");
+
+	pci_walk_bus(bridge->bus, pci_configure_extended_tags, NULL);
+}
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_SERVERWORKS, 0x0140,
+			quirk_exttags_completer);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_SERVERWORKS, 0x0142,
+			quirk_exttags_completer);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_SERVERWORKS, 0x0144,
+			quirk_exttags_completer);
