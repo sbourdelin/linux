9d22948bf40dc4ce80d3e7f0f04228c060ec59bf
David Sterba <dsterba@suse.com>
2017-07-13 15:18:44 +0200

    btrfs: allow defrag compress to override NOCOMPRESS attribute
    Currently, the BTRFS_INODE_NOCOMPRESS will prevent any compression on a
    given file, except when the mount is force-compress. As users have
    reported on IRC, this will also prevent compression when requested by
    defrag (btrfs fi defrag -c file).
    
    The nocompress flag is set automatically by filesystem when the ratios
    are bad and the user would have to manually drop the bit in order to
    make defrag -c work. This is not good from the usability perspective.
    
    This patch will raise priority for the defrag -c over nocompress, ie.
    any file with NOCOMPRESS bit set will get defragmented. The bit will
    remain untouched.
    
    Alternate option was to also drop the nocompress bit and keep the
    decision logic as is, but I think this is not the right solution.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -399,12 +399,14 @@ static inline int inode_need_compress(struct inode *inode)
 	/* force compress */
 	if (btrfs_test_opt(fs_info, FORCE_COMPRESS))
 		return 1;
+	/* force compress when called from defrag */
+	if (BTRFS_I(inode)->force_compress)
+		return 1;
 	/* bad compression ratios */
 	if (BTRFS_I(inode)->flags & BTRFS_INODE_NOCOMPRESS)
 		return 0;
 	if (btrfs_test_opt(fs_info, COMPRESS) ||
-	    BTRFS_I(inode)->flags & BTRFS_INODE_COMPRESS ||
-	    BTRFS_I(inode)->force_compress)
+	    BTRFS_I(inode)->flags & BTRFS_INODE_COMPRESS)
 		return 1;
 	return 0;
 }
