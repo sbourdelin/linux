d2c18a7fc3297358eadc2285ed278022c429e728
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2017-07-13 16:52:40 -0700

    mux: consumer: Add dummy functions for !CONFIG_MULTIPLEXER case
    Add dummy functions to avoid compile time issues when CONFIG_MULTIPLEXER
    is not enabled.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- include/linux/mux/consumer.h
+++ include/linux/mux/consumer.h
@@ -16,6 +16,8 @@
 struct device;
 struct mux_control;
 
+#if IS_ENABLED(CONFIG_MULTIPLEXER)
+
 unsigned int mux_control_states(struct mux_control *mux);
 int __must_check mux_control_select(struct mux_control *mux,
 				    unsigned int state);
@@ -28,5 +30,45 @@ void mux_control_put(struct mux_control *mux);
 
 struct mux_control *devm_mux_control_get(struct device *dev,
 					 const char *mux_name);
+
+#else
+
+static inline unsigned int mux_control_states(struct mux_control *mux)
+{
+	return 0;
+}
+
+static inline int __must_check mux_control_select(struct mux_control *mux,
+						  unsigned int state)
+{
+	return -EINVAL;
+}
+
+static inline int __must_check mux_control_try_select(struct mux_control *mux,
+						      unsigned int state)
+{
+	return -EINVAL;
+}
+
+static inline int mux_control_deselect(struct mux_control *mux)
+{
+	return -EINVAL;
+}
+
+static inline struct mux_control *mux_control_get(struct device *dev,
+						  const char *mux_name)
+{
+	return ERR_PTR(-ENODEV);
+}
+
+static inline void mux_control_put(struct mux_control *mux) {}
+
+static inline struct mux_control *devm_mux_control_get(struct device *dev,
+						       const char *mux_name)
+{
+	return ERR_PTR(-ENODEV);
+}
+
+#endif /* CONFIG_MULTIPLEXER */
 
 #endif /* _LINUX_MUX_CONSUMER_H */
