9a68664975aedac28d1ba5abe94f2a139ae27e48
Bartosz Golaszewski <brgl@bgdev.pl>
2017-07-19 14:20:12 +0200

    irq/irq_sim: add a simple interrupt simulator framework
    Implement a simple, irq_work-based framework for simulating
    interrupts. Currently the API exposes routines for initializing and
    deinitializing the simulator object, enqueueing the interrupts and
    retrieving the allocated interrupt numbers based on the offset of the
    dummy interrupt in the simulator struct.
    
    Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>

---
+++ include/linux/irq_sim.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017 Bartosz Golaszewski <brgl@bgdev.pl>
+ *
+ * Provides a framework for allocating simulated interrupts which can be
+ * requested like normal irqs and enqueued from process context.
+ */
+
+#ifndef _LINUX_IRQ_SIM_H
+#define _LINUX_IRQ_SIM_H
+
+#include <linux/irq_work.h>
+
+struct irq_sim_work_ctx {
+	struct irq_work work;
+	int irq;
+};
+
+struct irq_sim_irq_ctx {
+	int irqnum;
+	bool enabled;
+};
+
+struct irq_sim {
+	struct irq_sim_work_ctx work_ctx;
+	int irq_base;
+	unsigned int irq_count;
+	struct irq_sim_irq_ctx *irqs;
+};
+
+int irq_sim_init(struct irq_sim *sim, unsigned int num_irqs);
+void irq_sim_fini(struct irq_sim *sim);
+
+void irq_sim_fire(struct irq_sim *sim, unsigned int offset);
+
+int irq_sim_irqnum(struct irq_sim *sim, unsigned int offset);
+
+#endif /* _LINUX_IRQ_SIM_H */
