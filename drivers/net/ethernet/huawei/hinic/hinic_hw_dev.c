3706f382ff3053f9fbd260505d4e3ddaad1f8f0d
Aviad Krawczyk <aviad.krawczyk@huawei.com>
2017-07-19 17:19:11 +0800

    net-next/hinic: Set qp context
    Update the nic about the resources of the queue pairs.
    
    Signed-off-by: Aviad Krawczyk <aviad.krawczyk@huawei.com>
    Signed-off-by: Zhao Chen <zhaochen6@huawei.com>

--- drivers/net/ethernet/huawei/hinic/hinic_hw_dev.c
+++ drivers/net/ethernet/huawei/hinic/hinic_hw_dev.c
@@ -24,6 +24,7 @@
 #include "hinic_hw_if.h"
 #include "hinic_hw_eqs.h"
 #include "hinic_hw_mgmt.h"
+#include "hinic_hw_qp_ctxt.h"
 #include "hinic_hw_qp.h"
 #include "hinic_hw_io.h"
 #include "hinic_hw_dev.h"
@@ -75,6 +76,9 @@ static int get_capability(struct hinic_hwdev *hwdev,
 
 	/* Each QP has its own (SQ + RQ) interrupts */
 	num_qps = (num_irqs - (num_aeqs + num_ceqs)) / 2;
+
+	if (num_qps > HINIC_Q_CTXT_MAX)
+		num_qps = HINIC_Q_CTXT_MAX;
 
 	/* num_qps must be power of 2 */
 	num_qps = BIT(fls(num_qps) - 1);
