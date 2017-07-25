b2bab1020d70c53a5e9fdc453d662ca768534f76
Andi Kleen <ak@linux.intel.com>
2017-07-24 17:43:49 -0700

    x86/xsave: Move xsave initialization to after parsing early parameters
    Move the XSAVE initialization code to be after parsing early parameters.
    I don't see any reason why the FPU code needs to be initialized that
    early, nothing else in the initialization phase uses XSAVE.
    This is useful to be able to handle command line parameters in the
    XSAVE initialization code.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -92,6 +92,7 @@
 #include <asm/processor.h>
 #include <asm/bugs.h>
 #include <asm/kasan.h>
+#include <asm/fpu/internal.h>
 
 #include <asm/vsyscall.h>
 #include <asm/cpu.h>
@@ -990,6 +991,8 @@ void __init setup_arch(char **cmdline_p)
 	x86_configure_nx();
 
 	parse_early_param();
+
+	fpu__init_system(&boot_cpu_data);
 
 #ifdef CONFIG_MEMORY_HOTPLUG
 	/*
