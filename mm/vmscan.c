b6603bf0aa94fa2345ee2e6efac74003ab5c6952
Johannes Weiner <hannes@cmpxchg.org>
2017-07-27 11:30:09 -0400

    mm: workingset: tell cache transitions from workingset thrashing
    Refaults happen during transitions between workingsets as well as
    in-place thrashing. Knowing the difference between the two has a range
    of applications, including measuring the impact of memory shortage on
    the system performance, as well as the ability to smarter balance
    pressure between the filesystem cache and the swap-backed workingset.
    
    During workingset transitions, inactive cache refaults and pushes out
    established active cache. When that active cache isn't stale, however,
    and also ends up refaulting, that's bonafide thrashing.
    
    Introduce a new page flag that tells on eviction whether the page has
    been active or not in its lifetime. This bit is then stored in the
    shadow entry, to classify refaults as transitioning or thrashing.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1978,6 +1978,7 @@ static void shrink_active_list(unsigned long nr_to_scan,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
+		SetPageWorkingset(page);
 		list_add(&page->lru, &l_inactive);
 	}
 
