592858c8462362994ddddcbbbed4b28d442f6fdd
Noralf Trønnes <noralf@tronnes.org>
2017-07-31 20:42:14 +0200

    drm: Add GEM backed framebuffer library
    This library provides helpers for drivers that don't subclass
    drm_framebuffer and are backed by drm_gem_object. The code is
    taken from drm_fb_cma_helper.
    
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>

--- include/drm/drm_framebuffer.h
+++ include/drm/drm_framebuffer.h
@@ -190,6 +190,10 @@ struct drm_framebuffer {
 	 * @filp_head: Placed on &drm_file.fbs, protected by &drm_file.fbs_lock.
 	 */
 	struct list_head filp_head;
+	/**
+	 * @obj: GEM objects backing the framebuffer, one per plane (optional).
+	 */
+	struct drm_gem_object *obj[4];
 };
 
 #define obj_to_fb(x) container_of(x, struct drm_framebuffer, base)
