d7ba60e4c7a8af3dde9630b16b852b8c6dc56e0c
Punit Agrawal <punit.agrawal@arm.com>
2017-08-02 10:49:01 +0100

    arm64: hugetlb: Override huge_pte_clear() to support contiguous hugepages
    The default huge_pte_clear() implementation does not clear contiguous
    page table entries when it encounters contiguous hugepages that are
    supported on arm64.
    
    Fix this by overriding the default implementation to clear all the
    entries associated with contiguous hugepages.
    
    Signed-off-by: Punit Agrawal <punit.agrawal@arm.com>
    Cc: David Woods <dwoods@mellanox.com>

--- arch/arm64/mm/hugetlbpage.c
+++ arch/arm64/mm/hugetlbpage.c
@@ -68,6 +68,30 @@ static int find_num_contig(struct mm_struct *mm, unsigned long addr,
 	return CONT_PTES;
 }
 
+static inline int num_contig_ptes(unsigned long size, size_t *pgsize)
+{
+	int contig_ptes = 0;
+
+	*pgsize = size;
+
+	switch (size) {
+	case PUD_SIZE:
+	case PMD_SIZE:
+		contig_ptes = 1;
+		break;
+	case CONT_PMD_SIZE:
+		*pgsize = PMD_SIZE;
+		contig_ptes = CONT_PMDS;
+		break;
+	case CONT_PTE_SIZE:
+		*pgsize = PAGE_SIZE;
+		contig_ptes = CONT_PTES;
+		break;
+	}
+
+	return contig_ptes;
+}
+
 /*
  * Changing some bits of contiguous entries requires us to follow a
  * Break-Before-Make approach, breaking the whole contiguous set
@@ -243,6 +267,18 @@ pte_t arch_make_huge_pte(pte_t entry, struct vm_area_struct *vma,
 			__func__, pagesize);
 	}
 	return entry;
+}
+
+void huge_pte_clear(struct mm_struct *mm, unsigned long addr,
+		    pte_t *ptep, unsigned long sz)
+{
+	int i, ncontig;
+	size_t pgsize;
+
+	ncontig = num_contig_ptes(sz, &pgsize);
+
+	for (i = 0; i < ncontig; i++, addr += pgsize, ptep++)
+		pte_clear(mm, addr, ptep);
 }
 
 pte_t huge_ptep_get_and_clear(struct mm_struct *mm,
