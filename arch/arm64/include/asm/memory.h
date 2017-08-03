c587fffa0895a98c688fc7a56f04302c726b9976
Nick Desaulniers <ndesaulniers@google.com>
2017-08-03 10:11:16 -0700

    arm64: avoid overflow in VA_START and PAGE_OFFSET
    The bitmask used to define these values produces overflow, as seen by
    this compiler warning:
    
    arch/arm64/kernel/head.S:47:8: warning:
          integer overflow in preprocessor expression
      #elif (PAGE_OFFSET & 0x1fffff) != 0
             ^~~~~~~~~~~
    arch/arm64/include/asm/memory.h:52:46: note:
          expanded from macro 'PAGE_OFFSET'
      #define PAGE_OFFSET             (UL(0xffffffffffffffff) << (VA_BITS -
    1))
                                          ~~~~~~~~~~~~~~~~~~  ^
    
    It would be preferrable to use GENMASK_ULL() instead, but it's not set
    up to be used from assembly (the UL() macro token pastes UL suffixes
    when not included in assembly sources).
    
    Suggested-by: Yury Norov <ynorov@caviumnetworks.com>
    Suggested-by: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -64,8 +64,9 @@
  * TASK_UNMAPPED_BASE - the lower boundary of the mmap VM area.
  */
 #define VA_BITS			(CONFIG_ARM64_VA_BITS)
-#define VA_START		(UL(0xffffffffffffffff) << VA_BITS)
-#define PAGE_OFFSET		(UL(0xffffffffffffffff) << (VA_BITS - 1))
+#define VA_START		((UL(0xffffffffffffffff) >> VA_BITS) << VA_BITS)
+#define PAGE_OFFSET		((UL(0xffffffffffffffff) >> (VA_BITS - 1)) \
+	<< (VA_BITS - 1))
 #define KIMAGE_VADDR		(MODULES_END)
 #define MODULES_END		(MODULES_VADDR + MODULES_VSIZE)
 #define MODULES_VADDR		(VA_START + KASAN_SHADOW_SIZE)
