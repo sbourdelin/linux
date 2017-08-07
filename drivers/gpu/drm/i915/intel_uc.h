6972a2441e1c48cb6fae829902eed04e8b72fc9d
Michal Wajdeczko <michal.wajdeczko@intel.com>
2017-08-07 16:14:27 +0000

    drm/i915/guc: Handle default action received over CT
    With enabled CT, instead of programming SCRATCH 15 register with the
    Guc to host message, Guc will send us unsolicited CT message.
    Content of the first payload dword of this request has the same
    format as data expected in the above scratch register.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Oscar Mateo <oscar.mateo@intel.com>

--- drivers/gpu/drm/i915/intel_uc.h
+++ drivers/gpu/drm/i915/intel_uc.h
@@ -230,6 +230,7 @@ void intel_uc_fini_fw(struct drm_i915_private *dev_priv);
 int intel_uc_init_hw(struct drm_i915_private *dev_priv);
 void intel_uc_fini_hw(struct drm_i915_private *dev_priv);
 void intel_guc_notification_handler(struct intel_guc *guc);
+void intel_guc_process_default_action(struct intel_guc *guc, u32 msg);
 int intel_guc_sample_forcewake(struct intel_guc *guc);
 int intel_guc_send_nop(struct intel_guc *guc, const u32 *action, u32 len, u32 *response);
 int intel_guc_send_mmio(struct intel_guc *guc, const u32 *action, u32 len, u32 *response);
