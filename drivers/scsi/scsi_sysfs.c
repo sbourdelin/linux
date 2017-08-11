005471beea7dec6dae3ffd197959dabe52bab6c9
Hannes Reinecke <hare@suse.de>
2017-08-11 16:23:18 +0200

    scsi: Export blacklist flags to sysfs
    Each scsi device is scanned according to the found blacklist flags,
    but this information is never presented to sysfs.
    This makes it quite hard to figure out if blacklisting worked as
    expected.
    With this patch we're exporting an additional attribute 'blacklist'
    containing the blacklist flags for this device.
    
    Signed-off-by: Hannes Reinecke <hare@suse.com>

--- drivers/scsi/scsi_sysfs.c
+++ drivers/scsi/scsi_sysfs.c
@@ -20,6 +20,7 @@
 #include <scsi/scsi_dh.h>
 #include <scsi/scsi_transport.h>
 #include <scsi/scsi_driver.h>
+#include <scsi/scsi_devinfo.h>
 
 #include "scsi_priv.h"
 #include "scsi_logging.h"
@@ -109,6 +110,50 @@ static const char *scsi_access_state_name(unsigned char state)
 	return name;
 }
 #endif
+
+static const struct {
+	unsigned int	value;
+	char		*name;
+} sdev_bflags[] = {
+	{ BLIST_NOLUN, "NOLUN" },
+	{ BLIST_FORCELUN, "FORCELUN" },
+	{ BLIST_BORKEN, "BORKEN" },
+	{ BLIST_KEY, "KEY" },
+	{ BLIST_SINGLELUN, "SINGLELUN" },
+	{ BLIST_NOTQ, "NOTQ" },
+	{ BLIST_SPARSELUN, "SPARSELUN" },
+	{ BLIST_MAX5LUN, "MAX5LUN" },
+	{ BLIST_ISROM, "ISROM" },
+	{ BLIST_LARGELUN, "LARGELUN" },
+	{ BLIST_INQUIRY_36, "INQUIRY_36" },
+	{ BLIST_NOSTARTONADD, "NOSTARTONADD" },
+	{ BLIST_REPORTLUN2, "REPORTLUN2" },
+	{ BLIST_NOREPORTLUN, "NOREPORTLUN" },
+	{ BLIST_NOT_LOCKABLE, "NOT_LOCKABLE" },
+	{ BLIST_NO_ULD_ATTACH, "NO_ULD_ATTACH" },
+	{ BLIST_SELECT_NO_ATN, "SELECT_NO_ATN" },
+	{ BLIST_RETRY_HWERROR, "RETRY_HWERROR" },
+	{ BLIST_MAX_512, "MAX_512" },
+	{ BLIST_NO_DIF, "NO_DIF" },
+	{ BLIST_SKIP_VPD_PAGES, "SKIP_VPD_PAGES" },
+	{ BLIST_TRY_VPD_PAGES, "TRY_VPD_PAGES" },
+	{ BLIST_NO_RSOC, "NO_RSOC" },
+	{ BLIST_MAX_1024, "MAX_1024" },
+};
+
+static const char *sdev_bflags_name(unsigned int bflags)
+{
+	int i;
+	const char *name = NULL;
+
+	for (i = 0; i < ARRAY_SIZE(sdev_bflags); i++) {
+		if (sdev_bflags[i].value == bflags) {
+			name = sdev_bflags[i].name;
+			break;
+		}
+	}
+	return name;
+}
 
 static int check_set(unsigned long long *val, char *src)
 {
@@ -953,6 +998,43 @@ sdev_show_wwid(struct device *dev, struct device_attribute *attr,
 }
 static DEVICE_ATTR(wwid, S_IRUGO, sdev_show_wwid, NULL);
 
+static ssize_t
+sdev_show_blacklist(struct device *dev, struct device_attribute *attr,
+		    char *buf)
+{
+	struct scsi_device *sdev = to_scsi_device(dev);
+	int i;
+	char *ptr = buf;
+	ssize_t len = 0;
+
+	for (i = 0; i < sizeof(unsigned int) * 8; i++) {
+		unsigned int bflags = (unsigned int)1 << i;
+		ssize_t blen;
+		const char *name = NULL;
+
+		if (!(bflags & sdev->sdev_bflags))
+			continue;
+
+		if (ptr != buf) {
+			blen = snprintf(ptr, 2, " ");
+			ptr += blen;
+			len += blen;
+		}
+		name = sdev_bflags_name(bflags);
+		if (name)
+			blen = snprintf(ptr, strlen(name) + 1,
+					"%s", name);
+		else
+			blen = snprintf(ptr, 67, "0x%X", bflags);
+		ptr += blen;
+		len += blen;
+	}
+	if (len)
+		len += snprintf(ptr, 2, "\n");
+	return len;
+}
+static DEVICE_ATTR(blacklist, S_IRUGO, sdev_show_blacklist, NULL);
+
 #ifdef CONFIG_SCSI_DH
 static ssize_t
 sdev_show_dh_state(struct device *dev, struct device_attribute *attr,
@@ -1138,6 +1220,7 @@ static struct attribute *scsi_sdev_attrs[] = {
 	&dev_attr_queue_depth.attr,
 	&dev_attr_queue_type.attr,
 	&dev_attr_wwid.attr,
+	&dev_attr_blacklist.attr,
 #ifdef CONFIG_SCSI_DH
 	&dev_attr_dh_state.attr,
 	&dev_attr_access_state.attr,
