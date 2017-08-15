ddaf37bf42c1e167a85b2a1e4a9bca9611a0d41c
Gilad Ben-Yossef <gilad@benyossef.com>
2017-08-15 09:26:34 +0300

    staging: ccree: simplify resource release on error
    The resource release on probe/init error was being handled
    in an awkward manner and possibly leaking memory on certain
    (unlikely) error path.
    
    Fix it by simplifying the error resource release and making
    it easier to track.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>

--- drivers/staging/ccree/ssi_hash.c
+++ drivers/staging/ccree/ssi_hash.c
@@ -2229,6 +2229,7 @@ int ssi_hash_alloc(struct ssi_drvdata *drvdata)
 		goto fail;
 	}
 
+	INIT_LIST_HEAD(&hash_handle->hash_list);
 	drvdata->hash_handle = hash_handle;
 
 	sram_size_to_alloc = sizeof(digest_len_init) +
@@ -2258,8 +2259,6 @@ int ssi_hash_alloc(struct ssi_drvdata *drvdata)
 		SSI_LOG_ERR("Init digest CONST failed (rc=%d)\n", rc);
 		goto fail;
 	}
-
-	INIT_LIST_HEAD(&hash_handle->hash_list);
 
 	/* ahash registration */
 	for (alg = 0; alg < ARRAY_SIZE(driver_hash); alg++) {
