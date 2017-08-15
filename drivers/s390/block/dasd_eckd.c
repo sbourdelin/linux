f343d2198f51f027b6983761dcaa216dc88f44e9
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-08-15 16:05:33 +0530

    s390: dasd_eckd: constify ccw_device_id
    ccw_device_id are not supposed to change at runtime. All functions
    working with ccw_device_id provided by <asm/ccwdev.h> work with
    const ccw_device_id. So mark the non-const structs as const.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>

--- drivers/s390/block/dasd_eckd.c
+++ drivers/s390/block/dasd_eckd.c
@@ -70,7 +70,7 @@ static struct dasd_discipline dasd_eckd_discipline;
 
 /* The ccw bus type uses this table to find devices that it sends to
  * dasd_eckd_probe */
-static struct ccw_device_id dasd_eckd_ids[] = {
+static const struct ccw_device_id dasd_eckd_ids[] = {
 	{ CCW_DEVICE_DEVTYPE (0x3990, 0, 0x3390, 0), .driver_info = 0x1},
 	{ CCW_DEVICE_DEVTYPE (0x2105, 0, 0x3390, 0), .driver_info = 0x2},
 	{ CCW_DEVICE_DEVTYPE (0x3880, 0, 0x3380, 0), .driver_info = 0x3},
