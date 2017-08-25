19951ac8f684e4c431fe7d68a93caead699f6692
Christian Gmeiner <christian.gmeiner@gmail.com>
2017-08-25 11:07:06 +0200

    drm/etnaviv: add performance monitor request validation
    Check if the selected domain and signal combination exists.
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Reviewed-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/gpu/drm/etnaviv/etnaviv_perfmon.h
+++ drivers/gpu/drm/etnaviv/etnaviv_perfmon.h
@@ -40,4 +40,6 @@ int etnaviv_pm_query_dom(struct etnaviv_gpu *gpu,
 int etnaviv_pm_query_sig(struct etnaviv_gpu *gpu,
 	struct drm_etnaviv_pm_signal *signal);
 
+int etnaviv_pm_req_validate(const struct drm_etnaviv_gem_submit_pmr *r);
+
 #endif /* __ETNAVIV_PERFMON_H__ */
