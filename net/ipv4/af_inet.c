61c1308a734ed102c5047ca53dda397d173a9010
Michael Witten <mfwitten@gmail.com>
2017-09-08 00:47:49 +0000

    net: inet_recvmsg(): Remove unnecessary bitwise operation
    The flag `MSG_DONTWAIT' is handled by passing an argument through
    the dedicated parameter `nonblock' of the function `tcp_recvmsg()'.
    
    Presumably because `MSG_DONTWAIT' is handled so explicitly, it is
    unset in the collection of flags that are passed to `tcp_recvmsg()';
    yet, this unsetting appears to be unnecessary, and so this commit
    removes the bitwise operation that performs the unsetting.
    
    Signed-off-by: Michael Witten <mfwitten@gmail.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -791,7 +791,7 @@ int inet_recvmsg(struct socket *sock, struct msghdr *msg, size_t size,
 	sock_rps_record_flow(sk);
 
 	err = sk->sk_prot->recvmsg(sk, msg, size, flags & MSG_DONTWAIT,
-				   flags & ~MSG_DONTWAIT, &addr_len);
+				   flags, &addr_len);
 	if (err >= 0)
 		msg->msg_namelen = addr_len;
 	return err;
