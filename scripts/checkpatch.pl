8d99b7ff68fbe3b296448470d843fa24e7f359c0
Stafford Horne <shorne@gmail.com>
2017-09-09 18:04:06 +0900

    checkpatch: Fix ignoring cover-letter logic
    Currently running checkpatch on a directory with a cover-letter.patch
    file reports the following error:
    
      -----------------------------------------
      patches/smp-v2/v2-0000-cover-letter.patch
      -----------------------------------------
    
      ERROR: Does not appear to be a unified-diff format patch
    
    The logic to suppress the unified-diff check for cover letters is there
    but is checking $file instead of $filename.  Fix the variable to use the
    correct one.
    
    Signed-off-by: Stafford Horne <shorne@gmail.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -6365,7 +6365,7 @@ sub process {
 		exit(0);
 	}
 
-	if (!$is_patch && $file !~ /cover-letter\.patch$/) {
+	if (!$is_patch && $filename !~ /cover-letter\.patch$/) {
 		ERROR("NOT_UNIFIED_DIFF",
 		      "Does not appear to be a unified-diff format patch\n");
 	}
