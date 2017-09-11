2225c3fd013ec8f940776938ff13d0b0a9c8d8b4
Thiebaud Weksteen <tweek@google.com>
2017-09-11 12:00:21 +0200

    efi: call get_event_log before ExitBootServices
    With TPM 2.0 specification, the event logs may only be accessible by
    calling an EFI Boot Service. Modify the EFI stub to copy the log area to
    a new Linux-specific EFI configuration table so it remains accessible
    once booted.
    
    When calling this service, it is possible to specify the expected format
    of the logs: TPM 1.2 (SHA1) or TPM 2.0 ("Crypto Agile"). For now, only the
    first format is retrieved.
    
    Signed-off-by: Thiebaud Weksteen <tweek@google.com>

--- drivers/firmware/efi/efi.c
+++ drivers/firmware/efi/efi.c
@@ -532,6 +532,8 @@ int __init efi_config_parse_tables(void *config_tables, int count, int sz,
 	if (efi_enabled(EFI_MEMMAP))
 		efi_memattr_init();
 
+	efi_tpm_eventlog_init();
+
 	/* Parse the EFI Properties table if it exists */
 	if (efi.properties_table != EFI_INVALID_TABLE_ADDR) {
 		efi_properties_table_t *tbl;
