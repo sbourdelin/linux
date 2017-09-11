e875144fe38231d4337d60b906ba4ba534077204
Michał Winiarski <michal.winiarski@intel.com>
2017-09-11 16:17:31 +0200

    drm/i915/guc: Submit GuC workitems containing coalesced requests
    To create an upper bound on number of GuC workitems, we need to change
    the way that requests are being submitted. Rather than submitting each
    request as an individual workitem, we can do coalescing in a similar way
    we're handlig execlist submission ports. We also need to stop pretending
    that we're doing "lite-restore" in GuC submission (we would create a
    workitem each time we hit this condition). This allows us to completely
    remove the reservation, replacing it with a compile time check.
    
    v2: Also coalesce when replaying on reset (Daniele)
    v3: Consistent wq_resv - per-request (Daniele)
    v4: Squash removing wq_resv
    
    References: https://bugs.freedesktop.org/show_bug.cgi?id=101873
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Jeff McGee <jeff.mcgee@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Oscar Mateo <oscar.mateo@intel.com>
    Signed-off-by: Michał Winiarski <michal.winiarski@intel.com>

--- drivers/gpu/drm/i915/intel_uc.h
+++ drivers/gpu/drm/i915/intel_uc.h
@@ -55,10 +55,6 @@ struct drm_i915_gem_request;
  *
  * We also keep a few statistics on failures. Ideally, these should all
  * be zero!
- *   no_wq_space: times that the submission pre-check found no space was
- *                available in the work queue (note, the queue is shared,
- *                not per-engine). It is OK for this to be nonzero, but
- *                it should not be huge!
  *   b_fail: failed to ring the doorbell. This should never happen, unless
  *           somehow the hardware misbehaves, or maybe if the GuC firmware
  *           crashes? We probably need to reset the GPU to recover.
@@ -83,8 +79,6 @@ struct i915_guc_client {
 	uint32_t wq_offset;
 	uint32_t wq_size;
 	uint32_t wq_tail;
-	uint32_t wq_rsvd;
-	uint32_t no_wq_space;
 
 	/* Per-engine counts of GuC submissions */
 	uint64_t submissions[I915_NUM_ENGINES];
@@ -250,8 +244,6 @@ u32 intel_guc_wopcm_size(struct drm_i915_private *dev_priv);
 /* i915_guc_submission.c */
 int i915_guc_submission_init(struct drm_i915_private *dev_priv);
 int i915_guc_submission_enable(struct drm_i915_private *dev_priv);
-int i915_guc_wq_reserve(struct drm_i915_gem_request *rq);
-void i915_guc_wq_unreserve(struct drm_i915_gem_request *request);
 void i915_guc_submission_disable(struct drm_i915_private *dev_priv);
 void i915_guc_submission_fini(struct drm_i915_private *dev_priv);
 struct i915_vma *intel_guc_allocate_vma(struct intel_guc *guc, u32 size);
