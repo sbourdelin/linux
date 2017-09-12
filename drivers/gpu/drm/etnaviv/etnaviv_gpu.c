a5aac7437cb90954aa362b245deea9e26b19b0cc
Christian Gmeiner <christian.gmeiner@gmail.com>
2017-09-12 17:11:42 +0200

    drm/etnaviv: add 'sync point' support
    In order to support performance counters in a sane way we need to provide
    a method to sync the GPU with the CPU. The GPU can process multpile command
    buffers/events per irq. With the help of a 'sync point' we can trigger an event
    and stop the GPU/FE immediately. When the CPU is done with is processing it
    simply needs to restart the FE and the GPU will process the command stream.
    
    Changes from v1 -> v2:
    - process sync point with a work item to keep irq as fast as possible
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- drivers/gpu/drm/etnaviv/etnaviv_gpu.c
+++ drivers/gpu/drm/etnaviv/etnaviv_gpu.c
@@ -25,6 +25,7 @@
 #include "etnaviv_gpu.h"
 #include "etnaviv_gem.h"
 #include "etnaviv_mmu.h"
+#include "etnaviv_perfmon.h"
 #include "common.xml.h"
 #include "state.xml.h"
 #include "state_hi.xml.h"
@@ -1359,6 +1360,7 @@ int etnaviv_gpu_submit(struct etnaviv_gpu *gpu,
 	}
 
 	gpu->event[event].fence = fence;
+	gpu->event[event].sync_point = NULL;
 	submit->fence = dma_fence_get(fence);
 	gpu->active_fence = submit->fence->seqno;
 
@@ -1402,6 +1404,23 @@ out_pm_put:
 	etnaviv_gpu_pm_put(gpu);
 
 	return ret;
+}
+
+static void etnaviv_process_sync_point(struct etnaviv_gpu *gpu,
+	struct etnaviv_event *event)
+{
+	u32 addr = gpu_read(gpu, VIVS_FE_DMA_ADDRESS);
+
+	event->sync_point(gpu, event);
+	etnaviv_gpu_start_fe(gpu, addr + 2, 2);
+}
+
+static void pmrs_worker(struct work_struct *work)
+{
+	struct etnaviv_gpu *gpu = container_of(work, struct etnaviv_gpu,
+					       pmrs_work);
+
+	etnaviv_process_sync_point(gpu, &gpu->event[gpu->pmrs_event]);
 }
 
 /*
@@ -1449,6 +1468,11 @@ static irqreturn_t irq_handler(int irq, void *data)
 			intr &= ~(1 << event);
 
 			dev_dbg(gpu->dev, "event %u\n", event);
+
+			if (gpu->event[event].sync_point) {
+				gpu->pmrs_event = event;
+				etnaviv_queue_work(gpu->drm, &gpu->pmrs_work);
+			}
 
 			fence = gpu->event[event].fence;
 			gpu->event[event].fence = NULL;
@@ -1655,6 +1679,7 @@ static int etnaviv_gpu_bind(struct device *dev, struct device *master,
 
 	INIT_LIST_HEAD(&gpu->active_cmd_list);
 	INIT_WORK(&gpu->retire_work, retire_worker);
+	INIT_WORK(&gpu->pmrs_work, pmrs_worker);
 	INIT_WORK(&gpu->recover_work, recover_worker);
 	init_waitqueue_head(&gpu->fence_event);
 
