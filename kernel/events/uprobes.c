25e5108757b14af734f0b4e2411e00048cd7fa15
Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
2017-09-14 01:58:01 +0530

    kernel/uprobes: Ratelimit messages
    Ratelimit warnings from uprobes so as not to flood the kernel log.
    
    Suggested-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -114,8 +114,8 @@ struct xol_area {
 
 static void uprobe_warn(struct task_struct *t, const char *msg)
 {
-	pr_warn("uprobe: %s:%d failed to %s\n",
-			current->comm, current->pid, msg);
+	pr_warn_ratelimited("uprobe: %s:%d failed to %s\n",
+				current->comm, current->pid, msg);
 }
 
 /*
