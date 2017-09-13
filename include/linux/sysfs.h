ec8dd87e79b9a2c1f81647f28a50c2997e5a1a3d
Himanshi Jain <himshijain.hj@gmail.com>
2017-09-13 14:14:07 +0530

    include: linux: sysfs: Add __ATTR_NAMED macro
    Add __ATTR_NAMED macro similar to __ATTR but taking name as a
    string instead of implicit conversion of argument to string using
    the macro _stringify(_name).
    
    Signed-off-by: Himanshi Jain <himshijain.hj@gmail.com>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -104,6 +104,13 @@ struct attribute_group {
 	.store	= _store,						\
 }
 
+#define __ATTR_NAMED(_name, _mode, _show, _store) {			\
+	.attr = {.name = _name,						\
+		 .mode = VERIFY_OCTAL_PERMISSIONS(_mode) },		\
+	.show = _show,							\
+	.store = _store,						\
+}
+
 #define __ATTR_PREALLOC(_name, _mode, _show, _store) {			\
 	.attr = {.name = __stringify(_name),				\
 		 .mode = SYSFS_PREALLOC | VERIFY_OCTAL_PERMISSIONS(_mode) },\
