817e960ddbc4cd1fe424ce00deeabc886e045610
Jérôme de Bretagne <jerome.debretagne@gmail.com>
2017-09-18 00:57:12 +0200

    platform/x86: intel-hid: Power button suspend on Dell Latitude 7275
    On Dell Latitude 7275 the 5-button array is not exposed in the
    ACPI tables, but still notifies are sent to the Intel HID device
    object (device ID INT33D5) in response to power button actions.
    They were ignored as the intel-hid driver was not prepared to
    take care of them until recently.
    
    Power button wakeup from suspend-to-idle was added in:
    635173a17b03 ("intel-hid: Wake up Dell Latitude 7275 from
    suspend-to-idle"). However power button suspend doesn't work
    yet on this platform so it would be good to add it also.
    
    On the affected platform (for which priv->array is NULL), add
    a new upfront check against the power button press notification
    (0xCE) to notify_handler(), outside the wakeup mode this time,
    which allows to report the power button press event and
    trigger the suspend. Also catch and ignore the corresponding
    power button release notification (0xCF) to stop it from being
    reported as an "unknown event" in the logs.
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=196115
    Tested-by: Jérôme de Bretagne <jerome.debretagne@gmail.com>
    Signed-off-by: Jérôme de Bretagne <jerome.debretagne@gmail.com>

--- drivers/platform/x86/intel-hid.c
+++ drivers/platform/x86/intel-hid.c
@@ -215,6 +215,22 @@ static void notify_handler(acpi_handle handle, u32 event, void *context)
 		return;
 	}
 
+	/*
+	 * Needed for suspend to work on some platforms that don't expose
+	 * the 5-button array, but still send notifies with power button
+	 * event code to this device object on power button actions.
+	 *
+	 * Report the power button press; catch and ignore the button release.
+	 */
+	if (!priv->array) {
+		if (event == 0xce) {
+			input_report_key(priv->input_dev, KEY_POWER, 1);
+			input_sync(priv->input_dev);
+			return;
+		} else if (event == 0xcf)
+			return;
+	}
+
 	/* 0xC0 is for HID events, other values are for 5 button array */
 	if (event != 0xc0) {
 		if (!priv->array ||
