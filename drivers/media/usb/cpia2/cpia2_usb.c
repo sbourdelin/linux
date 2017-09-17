cf7bcbdd93e193d45401c79b560769a1e3be09eb
Markus Elfring <elfring@users.sourceforge.net>
2017-09-17 13:23:47 +0200

    cpia2: Delete unnecessary null pointer checks in free_sbufs()
    The script "checkpatch.pl" pointed information out like the following.
    
    WARNING: kfree(NULL) is safe and this check is probably not required
    
    Thus fix the affected source code place.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- drivers/media/usb/cpia2/cpia2_usb.c
+++ drivers/media/usb/cpia2/cpia2_usb.c
@@ -519,15 +519,11 @@ static void free_sbufs(struct camera_data *cam)
 	int i;
 
 	for (i = 0; i < NUM_SBUF; i++) {
-		if(cam->sbuf[i].urb) {
-			usb_kill_urb(cam->sbuf[i].urb);
-			usb_free_urb(cam->sbuf[i].urb);
-			cam->sbuf[i].urb = NULL;
-		}
-		if(cam->sbuf[i].data) {
-			kfree(cam->sbuf[i].data);
-			cam->sbuf[i].data = NULL;
-		}
+		usb_kill_urb(cam->sbuf[i].urb);
+		usb_free_urb(cam->sbuf[i].urb);
+		cam->sbuf[i].urb = NULL;
+		kfree(cam->sbuf[i].data);
+		cam->sbuf[i].data = NULL;
 	}
 }
 
