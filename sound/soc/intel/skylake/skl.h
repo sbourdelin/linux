26adbae2cefdca9863931ec34215b9a97af0b52f
Jaikrishna Nemallapudi <jaikrishnax.nemallapudi@intel.com>
2017-09-18 10:26:45 +0530

    ASoC: Intel: Skylake: Parse nhlt to populate clock information
    NHLT is parsed to fill the SSP clock information. If an endpoint is present
    for a SSP, then the corresponding SSP clocks are created.
    
    MCLK is consistent across endpoints and configuration for an SSP. So NHLT
    is queried for the first endpoint for an SSP and MCLK information for that
    SSP is populated.
    
    For SCLK/SCLKFS, the best fit is queried from the NHLT configurations which
    matches the clock rate requested. Best fit is decided based on below:
    
    1. If rate matches with multiple configurations, then the first
    configuration is selected.
    
    2. If for a selected fs and bits_per_sample, there are multiple endpoint
    configuration match, then the configuration with max number of channels is
    selected. So, the user has to set the rate which fits max number of
    channels
    
    Max of 18 clocks created, 3 for each SSP and there are 6 SSPs. There are
    also 3 parent clocks XTAL, Cardinal and PLL. This patch populates the all
    the clock information.
    
    Cardinal/PLL are fixed for all platforms. For XTAL, APL/GLK uses 19.2MHz,
    whereas for SKL/APL it is 24MHz.
    
    Signed-off-by: Jaikrishna Nemallapudi <jaikrishnax.nemallapudi@intel.com>
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>

--- sound/soc/intel/skylake/skl.h
+++ sound/soc/intel/skylake/skl.h
@@ -25,6 +25,7 @@
 #include <sound/hdaudio_ext.h>
 #include <sound/soc.h>
 #include "skl-nhlt.h"
+#include "skl-ssp-clk.h"
 
 #define SKL_SUSPEND_DELAY 2000
 
@@ -123,6 +124,8 @@ const struct skl_dsp_ops *skl_get_dsp_ops(int pci_id);
 void skl_update_d0i3c(struct device *dev, bool enable);
 int skl_nhlt_create_sysfs(struct skl *skl);
 void skl_nhlt_remove_sysfs(struct skl *skl);
+void skl_get_clks(struct skl *skl, struct skl_ssp_clk *ssp_clks);
+struct skl_clk_parent_src *skl_get_parent_clk(u8 clk_id);
 
 struct skl_module_cfg;
 
