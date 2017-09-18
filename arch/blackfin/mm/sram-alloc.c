9c30a90e12251c6ab54c30e2176b0a5df0ac8407
Corentin Labbe <clabbe.montjoie@gmail.com>
2017-09-18 19:22:01 +0200

    blackfin: sram-alloc.c: remove unneeded linux/miscdevice.h include
    arch/blackfin/mm/sram-alloc.c does not use any miscdevice so the
    inclusion of linux/miscdevice.h is unnecessary.
    This patch remove this inclusion.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>

--- arch/blackfin/mm/sram-alloc.c
+++ arch/blackfin/mm/sram-alloc.c
@@ -9,7 +9,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
-#include <linux/miscdevice.h>
 #include <linux/ioport.h>
 #include <linux/fcntl.h>
 #include <linux/init.h>
