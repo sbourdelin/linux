sbourdelin/linux

commit 36ae3c0a36b7456432fedce38ae2f7bd3e01a563

KVM: Don't accept obviously wrong gsi values via KVM_IRQFD
We cannot add routes for gsi values >= KVM_MAX_IRQ_ROUTES -- see
kvm_set_irq_routing(). Hence, there is no sense in accepting them
via KVM_IRQFD. Prevent them from entering the system in the first
place.

Signed-off-by: Jan H. Schönherr <jschoenh@amazon.de>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

virt/kvm/eventfd.c
@@ -565,6 +565,8 @@ kvm_irqfd(struct kvm *kvm, struct kvm_irqfd *args)
 {
 	if (args->flags & ~(KVM_IRQFD_FLAG_DEASSIGN | KVM_IRQFD_FLAG_RESAMPLE))
 		return -EINVAL;
+	if (args->gsi >= KVM_MAX_IRQ_ROUTES)
+		return -EINVAL;
 
 	if (args->flags & KVM_IRQFD_FLAG_DEASSIGN)
 		return kvm_irqfd_deassign(kvm, args);

commit d55f6a3e9638a776def36226a53dc631a829a4ca

KVM: KVM_MAX_IRQ_ROUTES only defined with CONFIG_HAVE_KVM_IRQ_ROUTING
Commit 36ae3c0a36b7 introduced code using KVM_MAX_IRQ_ROUTES. This
define isn't available on all platforms. As it is dependent on
CONFIG_HAVE_KVM_IRQ_ROUTING this define is used to enable the code
in 36ae3c0a36b7.

Fixes: 36ae3c0a36b7 ("KVM: Don't accept obviously wrong gsi values via KVM_IRQFD")
Reported-by: Abdul Haleem <abdhalee@linux.vnet.ibm.com>
Suggested-by: Jan H. Schönherr <jschoenh@amazon.de>
Signed-off-by: Daniel Black <daniel.black@au.ibm.com>

virt/kvm/eventfd.c
@@ -565,8 +565,10 @@ kvm_irqfd(struct kvm *kvm, struct kvm_irqfd *args)
 {
 	if (args->flags & ~(KVM_IRQFD_FLAG_DEASSIGN | KVM_IRQFD_FLAG_RESAMPLE))
 		return -EINVAL;
+#ifdef CONFIG_HAVE_KVM_IRQ_ROUTING
 	if (args->gsi >= KVM_MAX_IRQ_ROUTES)
 		return -EINVAL;
+#endif
 
 	if (args->flags & KVM_IRQFD_FLAG_DEASSIGN)
 		return kvm_irqfd_deassign(kvm, args);