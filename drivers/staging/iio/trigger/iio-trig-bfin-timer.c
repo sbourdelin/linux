bf1e1e46ab89c9258d5f6ebfcc380323e770589d
Harsha Sharma <harshasharmaiitr@gmail.com>
2017-09-26 23:56:15 +0530

    staging: iio: trigger: Move header file content to source file
    The contents of the header file are used only by this single source file.
    Moved content into iio-trig-bfin-timer.c and removed iio-trig-bfin-timer.h
    
    Signed-off-by: Harsha Sharma <harshasharmaiitr@gmail.com>

--- drivers/staging/iio/trigger/iio-trig-bfin-timer.c
+++ drivers/staging/iio/trigger/iio-trig-bfin-timer.c
@@ -19,7 +19,25 @@
 #include <linux/iio/iio.h>
 #include <linux/iio/trigger.h>
 
-#include "iio-trig-bfin-timer.h"
+/**
+ * struct iio_bfin_timer_trigger_pdata - timer trigger platform data
+ * @output_enable: Enable external trigger pulse generation.
+ * @active_low: Whether the trigger pulse is active low.
+ * @duty_ns: Length of the trigger pulse in nanoseconds.
+ *
+ * This struct is used to configure the output pulse generation of the blackfin
+ * timer trigger. If output_enable is set to true an external trigger signal
+ * will generated on the pin corresponding to the timer. This is useful for
+ * converters which needs an external signal to start conversion. active_low and
+ * duty_ns are used to configure the type of the trigger pulse. If output_enable
+ * is set to false no external trigger pulse will be generated and active_low
+ * and duty_ns are ignored.
+ **/
+struct iio_bfin_timer_trigger_pdata {
+	 bool output_enable;
+	 bool active_low;
+	 unsigned int duty_ns;
+};
 
 struct bfin_timer {
 	unsigned short id, bit;
