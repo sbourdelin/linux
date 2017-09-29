c0fbf0ed9029035de08fce6bf6592060ee1f390d
Alexandru Moise <00moses.alexander00@gmail.com>
2017-09-29 17:13:39 +0200

    mm, hugetlb: fix "treat_as_movable" condition in htlb_alloc_mask
    If hugepage_migration_supported() returns true, this renders the
    hugepages_treat_as_movable sysctl completely pointless.
    
    Let's keep this behavior optional by switching the if() condition
    from || to &&.
    
    Signed-off-by: Alexandru Moise <00moses.alexander00@gmail.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -926,7 +926,7 @@ retry_cpuset:
 /* Movability of hugepages depends on migration support. */
 static inline gfp_t htlb_alloc_mask(struct hstate *h)
 {
-	if (hugepages_treat_as_movable || hugepage_migration_supported(h))
+	if (hugepages_treat_as_movable && hugepage_migration_supported(h))
 		return GFP_HIGHUSER_MOVABLE;
 	else
 		return GFP_HIGHUSER;
