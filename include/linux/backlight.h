32a01d600552b1ab0a62e689128740ecf98ee9cf
Meghana Madhyastha <meghana.madhyastha@gmail.com>
2017-10-01 22:56:12 +0530

    backlight: Add IS_ENABLED(CONFIG_BACKLIGHT_CLASS_DEVICE)
    Add IS_ENABLED(CONFIG_BACKLIGHT_CLASS_DEVICE) as part of the
    if directive for the function declaration of
    of_find_backlight_by_node in order to avoid module dependency
    errors.
    
    Signed-off-by: Meghana Madhyastha <meghana.madhyastha@gmail.com>

--- include/linux/backlight.h
+++ include/linux/backlight.h
@@ -162,7 +162,7 @@ struct generic_bl_info {
 	void (*kick_battery)(void);
 };
 
-#ifdef CONFIG_OF
+#if defined CONFIG_OF && IS_ENABLED(CONFIG_BACKLIGHT_CLASS_DEVICE)
 struct backlight_device *of_find_backlight_by_node(struct device_node *node);
 #else
 static inline struct backlight_device *
