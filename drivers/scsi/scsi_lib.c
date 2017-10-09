f246f66ae5dc876cb46693afaa91f4d21dc06c69
Bart Van Assche <bart.vanassche@wdc.com>
2017-10-09 16:14:00 -0700

    block, scsi: Make SCSI quiesce and resume work reliably
    The contexts from which a SCSI device can be quiesced or resumed are:
    * Writing into /sys/class/scsi_device/*/device/state.
    * SCSI parallel (SPI) domain validation.
    * The SCSI device power management methods. See also scsi_bus_pm_ops.
    
    It is essential during suspend and resume that neither the filesystem
    state nor the filesystem metadata in RAM changes. This is why while
    the hibernation image is being written or restored that SCSI devices
    are quiesced. The SCSI core quiesces devices through scsi_device_quiesce()
    and scsi_device_resume(). In the SDEV_QUIESCE state execution of
    non-preempt requests is deferred. This is realized by returning
    BLKPREP_DEFER from inside scsi_prep_state_check() for quiesced SCSI
    devices. Avoid that a full queue prevents power management requests
    to be submitted by deferring allocation of non-preempt requests for
    devices in the quiesced state. This patch has been tested by running
    the following commands and by verifying that after resume the fio job
    is still running:
    
    for d in /sys/class/block/sd*[a-z]; do
      hcil=$(readlink "$d/device")
      hcil=${hcil#../../../}
      echo 4 > "$d/queue/nr_requests"
      echo 1 > "/sys/class/scsi_device/$hcil/device/queue_depth"
    done
    bdev=$(readlink /dev/disk/by-uuid/5217d83f-213e-4b42-b86e-20013325ba6c)
    bdev=${bdev#../../}
    hcil=$(readlink "/sys/block/$bdev/device")
    hcil=${hcil#../../../}
    fio --name="$bdev" --filename="/dev/$bdev" --buffered=0 --bs=512 --rw=randread \
      --ioengine=libaio --numjobs=4 --iodepth=16 --iodepth_batch=1 --thread \
      --loops=$((2**31)) &
    pid=$!
    sleep 1
    systemctl hibernate
    sleep 10
    kill $pid
    
    Reported-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    References: "I/O hangs after resuming from suspend-to-ram" (https://marc.info/?l=linux-block&m=150340235201348).
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Cc: Martin K. Petersen <martin.petersen@oracle.com>
    Cc: Ming Lei <ming.lei@redhat.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Hannes Reinecke <hare@suse.com>
    Cc: Johannes Thumshirn <jthumshirn@suse.de>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -2926,21 +2926,29 @@ static void scsi_wait_for_queuecommand(struct scsi_device *sdev)
 int
 scsi_device_quiesce(struct scsi_device *sdev)
 {
+	struct request_queue *q = sdev->request_queue;
 	int err;
+
+	/* If the SCSI device already has been quiesced, do nothing. */
+	if (blk_set_preempt_only(q))
+		return 0;
+
+	/*
+	 * Since blk_mq_freeze_queue() calls synchronize_rcu() indirectly the
+	 * effect of blk_set_preempt_only() will be visible for
+	 * percpu_ref_tryget() callers that occur after the queue
+	 * unfreeze. See also https://lwn.net/Articles/573497/.
+	 */
+	blk_mq_freeze_queue(q);
+	blk_mq_unfreeze_queue(q);
 
 	mutex_lock(&sdev->state_mutex);
 	err = scsi_device_set_state(sdev, SDEV_QUIESCE);
+	if (err)
+		blk_clear_preempt_only(q);
 	mutex_unlock(&sdev->state_mutex);
 
-	if (err)
-		return err;
-
-	scsi_run_queue(sdev->request_queue);
-	while (atomic_read(&sdev->device_busy)) {
-		msleep_interruptible(200);
-		scsi_run_queue(sdev->request_queue);
-	}
-	return 0;
+	return err;
 }
 EXPORT_SYMBOL(scsi_device_quiesce);
 
@@ -2962,7 +2970,7 @@ void scsi_device_resume(struct scsi_device *sdev)
 	mutex_lock(&sdev->state_mutex);
 	if (sdev->sdev_state == SDEV_QUIESCE &&
 	    scsi_device_set_state(sdev, SDEV_RUNNING) == 0)
-		scsi_run_queue(sdev->request_queue);
+		blk_clear_preempt_only(sdev->request_queue);
 	mutex_unlock(&sdev->state_mutex);
 }
 EXPORT_SYMBOL(scsi_device_resume);
