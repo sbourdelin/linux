22df9c6be0f3041af9ea3f90b3d67523589991a8
Chris Packham <chris.packham@alliedtelesis.co.nz>
2017-10-10 11:46:55 +1300

    uio: uio_pdrv_genirq: Add "marvell,88e2040" to dt compatible list
    The marvell,88e2040 is a N-BaseT Ethernet PHY which requires a userspace
    driver. uio_pdrv_genirq can be used to provide interrupts to such a
    userspace driver.
    
    Signed-off-by: Chris Packham <chris.packham@alliedtelesis.co.nz>

--- drivers/uio/uio_pdrv_genirq.c
+++ drivers/uio/uio_pdrv_genirq.c
@@ -254,11 +254,12 @@ static const struct dev_pm_ops uio_pdrv_genirq_dev_pm_ops = {
 #ifdef CONFIG_OF
 static struct of_device_id uio_of_genirq_match[] = {
 	{ .compatible = "st,stm32f100" },
+	{ .compatible = "marvell,88e2040" },
 	{ /* This is filled with module_parm */ },
 	{ /* Sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, uio_of_genirq_match);
-module_param_string(of_id, uio_of_genirq_match[1].compatible, 128, 0);
+module_param_string(of_id, uio_of_genirq_match[2].compatible, 128, 0);
 MODULE_PARM_DESC(of_id, "Openfirmware id of the device to be handled by uio");
 #endif
 
