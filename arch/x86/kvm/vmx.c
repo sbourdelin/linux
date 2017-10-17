a36cbe9b09f256b5b41ab4c05be8812869632363
Wanpeng Li <wanpeng.li@hotmail.com>
2017-10-16 21:03:57 -0700

    KVM: VMX: Don't advertise EPT switching if EPT itself is not exposed
    I can use vmxcap tool to observe "EPTP Switching   yes" even if EPT is not
    exposed to L1.
    
    EPT switching is advertised unconditionally since it is emulated, however,
    it can be treated as an extended feature for EPT and it should not be
    advertised if EPT itself is not exposed. This patch fixes it.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Jim Mattson <jmattson@google.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -2841,8 +2841,12 @@ static void nested_vmx_setup_ctls_msrs(struct vcpu_vmx *vmx)
 		 * Advertise EPTP switching unconditionally
 		 * since we emulate it
 		 */
-		vmx->nested.nested_vmx_vmfunc_controls =
-			VMX_VMFUNC_EPTP_SWITCHING;
+		if (enable_ept) {
+			vmx->nested.nested_vmx_vmfunc_controls =
+				VMX_VMFUNC_EPTP_SWITCHING;
+		} else
+			vmx->nested.nested_vmx_secondary_ctls_high &=
+				~SECONDARY_EXEC_ENABLE_VMFUNC;
 	}
 
 	/*
