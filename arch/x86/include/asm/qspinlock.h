039453a0a52822c58ecc623f918557842fa0841c
Eduardo Valentin <eduval@amazon.com>
2017-10-23 17:44:27 -0700

    locking/qspinlock/x86: Avoid test-and-set when PV_DEDICATED is set
    Currently, the existing qspinlock implementation will fallback to
    test-and-set if the hypervisor has not set the PV_UNHALT flag.
    
    This patch gives the opportunity to guest kernels to select
    between test-and-set and the regular queueu fair lock implementation
    based on the PV_DEDICATED KVM feature flag. When the PV_DEDICATED
    flag is not set, the code will still fall back to test-and-set,
    but when the PV_DEDICATED flag is set, the code will use
    the regular queue spinlock implementation.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: "Radim Krčmář" <rkrcmar@redhat.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: x86@kernel.org
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: kvm@vger.kernel.org
    Cc: linux-doc@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: Jan H. Schoenherr <jschoenh@amazon.de>
    Cc: Anthony Liguori <aliguori@amazon.com>
    Suggested-by: Matt Wilson <msw@amazon.com>
    Signed-off-by: Eduardo Valentin <eduval@amazon.com>

--- arch/x86/include/asm/qspinlock.h
+++ arch/x86/include/asm/qspinlock.h
@@ -1,6 +1,8 @@
 #ifndef _ASM_X86_QSPINLOCK_H
 #define _ASM_X86_QSPINLOCK_H
 
+#include <linux/kvm_para.h>
+
 #include <asm/cpufeature.h>
 #include <asm-generic/qspinlock_types.h>
 #include <asm/paravirt.h>
@@ -52,6 +54,8 @@ static inline bool virt_spin_lock(struct qspinlock *lock)
 	if (!static_cpu_has(X86_FEATURE_HYPERVISOR))
 		return false;
 
+	if (kvm_para_has_feature(KVM_FEATURE_PV_DEDICATED))
+		return false;
 	/*
 	 * On hypervisors without PARAVIRT_SPINLOCKS support we fall
 	 * back to a Test-and-Set spinlock, because fair locks have
