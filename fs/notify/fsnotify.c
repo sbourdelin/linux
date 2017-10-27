b66428fd9313e0d3280e37efaba50d11d6707107
Yang Shi <yang.s@alibaba-inc.com>
2017-10-28 02:22:18 +0800

    fs: fsnotify: account fsnotify metadata to kmemcg
    If some process generates events into a huge or unlimit event queue, but no
    listener read them, they may consume significant amount of memory silently
    until oom happens or some memory pressure issue is raised.
    It'd better to account those slab caches in memcg so that we can get heads
    up before the problematic process consume too much memory silently.
    
    But, the accounting might be heuristic if the producer is in the different
    memcg from listener if the listener doesn't read the events. Due to the
    current design of kmemcg, who does the allocation, who gets the accounting.
    
    Signed-off-by: Yang Shi <yang.s@alibaba-inc.com>

--- fs/notify/fsnotify.c
+++ fs/notify/fsnotify.c
@@ -386,7 +386,7 @@ static __init int fsnotify_init(void)
 		panic("initializing fsnotify_mark_srcu");
 
 	fsnotify_mark_connector_cachep = KMEM_CACHE(fsnotify_mark_connector,
-						    SLAB_PANIC);
+						    SLAB_PANIC|SLAB_ACCOUNT);
 
 	return 0;
 }
--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -785,7 +785,7 @@ static int __init inotify_user_setup(void)
 
 	BUG_ON(hweight32(ALL_INOTIFY_BITS) != 21);
 
-	inotify_inode_mark_cachep = KMEM_CACHE(inotify_inode_mark, SLAB_PANIC);
+	inotify_inode_mark_cachep = KMEM_CACHE(inotify_inode_mark, SLAB_PANIC|SLAB_ACCOUNT);
 
 	inotify_max_queued_events = 16384;
 	init_user_ns.ucount_max[UCOUNT_INOTIFY_INSTANCES] = 128;
