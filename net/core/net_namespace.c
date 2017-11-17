2860305330b75ffe70e65a61071eaaf674ff5815
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-17 21:29:31 +0300

    net: Move net_defaults_ops to pernet_sys list
    According to net/core/Makefile, net/core/net_namespace.o
    core initcalls execute right after net/core/sock.o.
    
    net_defaults_ops introduces only net_defaults_init_net method,
    and it acts on net::core::sysctl_somaxconn, which
    is not interested the rest of pernet_subsys and pernet_device
    lists. Then, move it to pernet_sys.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -371,7 +371,7 @@ static struct pernet_operations net_defaults_ops = {
 
 static __init int net_defaults_init(void)
 {
-	if (register_pernet_subsys(&net_defaults_ops))
+	if (register_pernet_sys(&net_defaults_ops))
 		panic("Cannot initialize net default settings");
 
 	return 0;
