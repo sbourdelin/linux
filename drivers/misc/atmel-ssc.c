0f8c62ac54ecd6f42dc988ca2b72798662b4f621
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-11-19 10:04:01 +0530

    misc: atmel-ssc: Fix platform_get_irq's error checking
    The platform_get_irq() function returns negative if an error occurs.
    zero or positive number on success. platform_get_irq() error checking
    for zero is not correct.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>

--- drivers/misc/atmel-ssc.c
+++ drivers/misc/atmel-ssc.c
@@ -235,7 +235,7 @@ static int ssc_probe(struct platform_device *pdev)
 	clk_disable_unprepare(ssc->clk);
 
 	ssc->irq = platform_get_irq(pdev, 0);
-	if (!ssc->irq) {
+	if (ssc->irq <= 0) {
 		dev_dbg(&pdev->dev, "could not get irq\n");
 		return -ENXIO;
 	}
