bc3d510e7e0dc642677554449703a3dfe5f3d7fb
Michal Hocko <mhocko@kernel.org>
2017-11-20 17:04:22 +0100

    arch, mm: introduce arch_tlb_gather_mmu_lazy
    On Mon 20-11-17 14:24:44, Will Deacon wrote:
    > On Thu, Nov 16, 2017 at 10:20:42AM +0100, Michal Hocko wrote:
    > > On Wed 15-11-17 17:33:32, Will Deacon wrote:
    [...]
    > > > > diff --git a/arch/arm64/include/asm/tlb.h b/arch/arm64/include/asm/tlb.h
    > > > > index ffdaea7954bb..7adde19b2bcc 100644
    > > > > --- a/arch/arm64/include/asm/tlb.h
    > > > > +++ b/arch/arm64/include/asm/tlb.h
    > > > > @@ -43,7 +43,7 @@ static inline void tlb_flush(struct mmu_gather *tlb)
    > > > >  	 * The ASID allocator will either invalidate the ASID or mark
    > > > >  	 * it as used.
    > > > >  	 */
    > > > > -	if (tlb->fullmm)
    > > > > +	if (tlb->lazy)
    > > > >  		return;
    > > >
    > > > This looks like the right idea, but I'd rather make this check:
    > > >
    > > > 	if (tlb->fullmm && tlb->lazy)
    > > >
    > > > since the optimisation doesn't work for anything than tearing down the
    > > > entire address space.
    > >
    > > OK, that makes sense.
    > >
    > > > Alternatively, I could actually go check MMF_UNSTABLE in tlb->mm, which
    > > > would save you having to add an extra flag in the first place, e.g.:
    > > >
    > > > 	if (tlb->fullmm && !test_bit(MMF_UNSTABLE, &tlb->mm->flags))
    > > >
    > > > which is a nice one-liner.
    > >
    > > But that would make it oom_reaper specific. What about the softdirty
    > > case Minchan has mentioned earlier?
    >
    > We don't (yet) support that on arm64, so we're ok for now. If we do grow
    > support for it, then I agree that we want a flag to identify the case where
    > the address space is going away and only elide the invalidation then.
    
    What do you think about the following patch instead? I have to confess
    I do not really understand the fullmm semantic so I might introduce some
    duplication by this flag. If you think this is a good idea, I will post
    it in a separate thread.

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -527,6 +527,8 @@ static inline cpumask_t *mm_cpumask(struct mm_struct *mm)
 struct mmu_gather;
 extern void tlb_gather_mmu(struct mmu_gather *tlb, struct mm_struct *mm,
 				unsigned long start, unsigned long end);
+extern void tlb_gather_mmu_lazy(struct mmu_gather *tlb, struct mm_struct *mm,
+				unsigned long start, unsigned long end);
 extern void tlb_finish_mmu(struct mmu_gather *tlb,
 				unsigned long start, unsigned long end);
 
