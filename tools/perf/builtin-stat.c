sbourdelin/linux

commit 7b60a7e3a687481553d2b6ec7e6390a6e82f1849

perf stat record: Synthesize event update events
Synthesize other events stuff not carried within attr event - unit,
scale, name.

Reported-by: Kan Liang <kan.liang@intel.com>
Signed-off-by: Jiri Olsa <jolsa@kernel.org>
Cc: David Ahern <dsahern@gmail.com>
Cc: Namhyung Kim <namhyung@kernel.org>
Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
Link: http://lkml.kernel.org/r/1446734469-11352-11-git-send-email-jolsa@kernel.org
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

tools/perf/builtin-stat.c
@@ -351,8 +351,19 @@ static void workload_exec_failed_signal(int signo __maybe_unused, siginfo_t *inf
 	workload_exec_errno = info->si_value.sival_int;
 }
 
+static bool has_unit(struct perf_evsel *counter)
+{
+	return counter->unit && *counter->unit;
+}
+
+static bool has_scale(struct perf_evsel *counter)
+{
+	return counter->scale != 1;
+}
+
 static int perf_stat_synthesize_config(bool is_pipe)
 {
+	struct perf_evsel *counter;
 	int err;
 
 	if (is_pipe) {
@@ -361,6 +372,54 @@ static int perf_stat_synthesize_config(bool is_pipe)
 		if (err < 0) {
 			pr_err("Couldn't synthesize attrs.\n");
 			return err;
+		}
+	}
+
+	/*
+	 * Synthesize other events stuff not carried within
+	 * attr event - unit, scale, name
+	 */
+	evlist__for_each(evsel_list, counter) {
+		if (!counter->supported)
+			continue;
+
+		/*
+		 * Synthesize unit and scale only if it's defined.
+		 */
+		if (has_unit(counter)) {
+			err = perf_event__synthesize_event_update_unit(NULL, counter, process_synthesized_event);
+			if (err < 0) {
+				pr_err("Couldn't synthesize evsel unit.\n");
+				return err;
+			}
+		}
+
+		if (has_scale(counter)) {
+			err = perf_event__synthesize_event_update_scale(NULL, counter, process_synthesized_event);
+			if (err < 0) {
+				pr_err("Couldn't synthesize evsel scale.\n");
+				return err;
+			}
+		}
+
+		if (counter->own_cpus) {
+			err = perf_event__synthesize_event_update_cpus(NULL, counter, process_synthesized_event);
+			if (err < 0) {
+				pr_err("Couldn't synthesize evsel scale.\n");
+				return err;
+			}
+		}
+
+		/*
+		 * Name is needed only for pipe output,
+		 * perf.data carries event names.
+		 */
+		if (is_pipe) {
+			err = perf_event__synthesize_event_update_name(NULL, counter, process_synthesized_event);
+			if (err < 0) {
+				pr_err("Couldn't synthesize evsel name.\n");
+				return err;
+			}
 		}
 	}
 

commit 5707979226092f8ed48740f99641e1d4a5c8681f

perf stat: Fix duplicate error messages in perf_stat_synthesize_config()
Duplicate error messages were used so far in this function implementation.
Make them unique instead.

This issue was detected by using the Coccinelle software.

Fixes: 7b60a7e3a687481553d2b6ec7e6390a6e82f1849 ("perf stat record: Synthesize event update events")
Fixes: 8b99b1a4e0b082ea6a277766982dac84483d4d3c ("perf stat record: Synthesize stat record data")
Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

tools/perf/builtin-stat.c
@@ -512,7 +512,7 @@ static int perf_stat_synthesize_config(bool is_pipe)
 		if (counter->own_cpus) {
 			err = perf_event__synthesize_event_update_cpus(NULL, counter, process_synthesized_event);
 			if (err < 0) {
-				pr_err("Couldn't synthesize evsel scale.\n");
+				pr_err("Couldn't synthesize evsel CPUs.\n");
 				return err;
 			}
 		}
@@ -541,7 +541,7 @@ static int perf_stat_synthesize_config(bool is_pipe)
 	err = perf_event__synthesize_cpu_map(NULL, evsel_list->cpus,
 					     process_synthesized_event, NULL);
 	if (err < 0) {
-		pr_err("Couldn't synthesize thread map.\n");
+		pr_err("Couldn't synthesize CPU map.\n");
 		return err;
 	}
 