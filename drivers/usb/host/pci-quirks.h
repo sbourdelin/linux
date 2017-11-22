f2ed1cde943fcef28c0d36d765a7f984675668ed
Joe Lee <asmt.swfae@gmail.com>
2017-11-22 17:45:14 +0800

    xhci : AMD Promontory USB disable port support
    For AMD Promontory xHCI host,
    although you can disable USB 2.0 ports in BIOSsettings,
    those ports will be enabled anyway after you remove a device on
    that port and re-plug it in again. It's a known limitation of the chip.
    As a workaround we can clear the PORT_WAKE_BITS.
    
    Signed-off-by: Joe Lee <asmt.swfae@gmail.com>

--- drivers/usb/host/pci-quirks.h
+++ drivers/usb/host/pci-quirks.h
@@ -10,6 +10,7 @@ int usb_hcd_amd_remote_wakeup_quirk(struct pci_dev *pdev);
 bool usb_amd_hang_symptom_quirk(void);
 bool usb_amd_prefetch_quirk(void);
 void usb_amd_dev_put(void);
+int usb_amd_pt_check_port(struct device *device, int port);
 void usb_amd_quirk_pll_disable(void);
 void usb_amd_quirk_pll_enable(void);
 void usb_asmedia_modifyflowcontrol(struct pci_dev *pdev);
@@ -23,6 +24,7 @@ static inline void usb_amd_quirk_pll_disable(void) {}
 static inline void usb_amd_quirk_pll_enable(void) {}
 static inline void usb_asmedia_modifyflowcontrol(struct pci_dev *pdev) {}
 static inline void usb_amd_dev_put(void) {}
+static inline int usb_amd_pt_check_port(struct device *device, int port) {}
 static inline void usb_disable_xhci_ports(struct pci_dev *xhci_pdev) {}
 static inline void sb800_prefetch(struct device *dev, int on) {}
 #endif  /* CONFIG_USB_PCI */
