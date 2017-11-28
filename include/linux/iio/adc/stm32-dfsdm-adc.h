2a873ed31eaa60e42182ff9e6cec6f0b6476c482
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2017-11-28 16:05:54 +0100

    IIO: ADC: add stm32 DFSDM support for PDM microphone
    This code offers a way to handle PDM audio microphones in
    ASOC framework. Audio driver should use consumer API.
    A specific management is implemented for DMA, with a
    callback, to allows to handle audio buffers efficiently.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

---
+++ include/linux/iio/adc/stm32-dfsdm-adc.h
@@ -0,0 +1,28 @@
+/*
+ * This file discribe the STM32 DFSDM IIO driver API for audio part
+ *
+ * Copyright (C) 2017, STMicroelectronics - All Rights Reserved
+ * Author(s): Arnaud Pouliquen <arnaud.pouliquen@st.com>.
+ *
+ * License terms: GPL V2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+#ifndef STM32_DFSDM_ADC_H
+#define STM32_DFSDM_ADC_H
+
+int stm32_dfsdm_get_buff_cb(struct iio_dev *iio_dev,
+			    int (*cb)(const void *data, size_t size,
+				      void *private),
+			    void *private);
+int stm32_dfsdm_release_buff_cb(struct iio_dev *iio_dev);
+
+#endif
