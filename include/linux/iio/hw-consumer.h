593eddd2fd80e4b3ea5f7a4d1a11f9ebd48fc95d
Lars-Peter Clausen <lars@metafoo.de>
2017-11-28 16:05:45 +0100

    iio: Add hardware consumer buffer support
    Hardware consumer interface can be used when one IIO device has
    a direct connection to another device in hardware.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

---
+++ include/linux/iio/hw-consumer.h
@@ -0,0 +1,20 @@
+/*
+ * Industrial I/O in kernel hardware consumer interface
+ *
+ * Copyright 2017 Analog Devices Inc.
+ *  Author: Lars-Peter Clausen <lars@metafoo.de>
+ *
+ * Licensed under the GPL-2 or later.
+ */
+
+#ifndef LINUX_IIO_HW_CONSUMER_H
+#define LINUX_IIO_HW_CONSUMER_H
+
+struct iio_hw_consumer;
+
+struct iio_hw_consumer *iio_hw_consumer_alloc(struct device *dev);
+void iio_hw_consumer_free(struct iio_hw_consumer *hwc);
+int iio_hw_consumer_enable(struct iio_hw_consumer *hwc);
+void iio_hw_consumer_disable(struct iio_hw_consumer *hwc);
+
+#endif
