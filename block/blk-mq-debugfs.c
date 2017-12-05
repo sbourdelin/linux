f6786010506c87a64e478aef25c55ac7f6b7b546
Bart Van Assche <bart.vanassche@wdc.com>
2017-12-04 16:38:09 -0800

    blk-mq-debugfs: Also show requests that have not yet been started
    When debugging e.g. the SCSI timeout handler it is important that
    requests that have not yet been started or that already have
    completed are also reported through debugfs.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Cc: Ming Lei <ming.lei@redhat.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Hannes Reinecke <hare@suse.com>
    Cc: Johannes Thumshirn <jthumshirn@suse.de>
    Cc: Martin K. Petersen <martin.petersen@oracle.com>

--- block/blk-mq-debugfs.c
+++ block/blk-mq-debugfs.c
@@ -409,7 +409,7 @@ static void hctx_show_busy_rq(struct request *rq, void *data, bool reserved)
 	const struct show_busy_params *params = data;
 
 	if (blk_mq_map_queue(rq->q, rq->mq_ctx->cpu) == params->hctx &&
-	    test_bit(REQ_ATOM_STARTED, &rq->atomic_flags))
+	    list_empty(&rq->queuelist))
 		__blk_mq_debugfs_rq_show(params->m,
 					 list_entry_rq(&rq->queuelist));
 }
