66db533d719f4dac2e735f5d7b9cef04645baaa8
Anthony Kim <anthony.kim@hideep.com>
2017-12-06 10:41:15 +0900

    Input: include "linux/gpio/consumer.h" header file.
    An error occurred during compile because there is no header file
    at x86 configure.
    
    Signed-off-by: Anthony Kim <anthony.kim@hideep.com>

--- drivers/input/touchscreen/hideep.c
+++ drivers/input/touchscreen/hideep.c
@@ -12,6 +12,7 @@
 #include <linux/delay.h>
 #include <linux/gpio.h>
 #include <linux/gpio/machine.h>
+#include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
 #include <linux/acpi.h>
 #include <linux/interrupt.h>
