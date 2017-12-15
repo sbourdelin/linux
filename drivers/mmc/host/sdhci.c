5ed6be825f4d13d11e4956f683159ef805728010
Zhoujie Wu <zjwu@marvell.com>
2017-12-15 11:28:56 -0800

    mmc: sdhci: wait 5ms after set 1.8V signal enable
    According to SD spec 3.00 3.6.1 signal voltage switch
    procedure step 6~8,
    (6) Set 1.8V Signal Enable in the Host Control 2 register.
    (7) Wait 5ms. 1.8V voltage regulator shall be stable within this period.
    (8) If 1.8V Signal Enable is cleared by Host Controller, go to step (12).
    
    Host should wait 5ms after set 1.8V signal enable bit in
    Host Control 2 register and check if 1.8V is stable or not.
    
    But current code checks if this bit is cleared by controller
    right after set it. On some platforms found the bit is not
    cleared right away and host reports "1.8V regulator output
    did not became stable" and 5ms delay can help.
    
    Follow the spec and add 5ms delay to make sure the 1.8V Signal Enable
    bit is cleared.
    
    Signed-off-by: Zhoujie Wu <zjwu@marvell.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1933,6 +1933,9 @@ int sdhci_start_signal_voltage_switch(struct mmc_host *mmc,
 		if (host->ops->voltage_switch)
 			host->ops->voltage_switch(host);
 
+		/* Wait for 5ms */
+		usleep_range(5000, 5500);
+
 		/* 1.8V regulator output should be stable within 5 ms */
 		ctrl = sdhci_readw(host, SDHCI_HOST_CONTROL2);
 		if (ctrl & SDHCI_CTRL_VDD_180)
