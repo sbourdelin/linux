af03c2287662c50823539c8b25d64132c211761a
Khan M Rashedun-Naby <naby88@gmail.com>
2017-12-20 22:23:55 +0600

    Block: blk-flush: removed an unnecessary else statement
    As both of the if and else statement block is returning something then
    there is no need of the else statement. Thus this else statement
    has been removed.
    
    Signed-off-by: Khan M Rashedun-Naby <naby88@gmail.com>

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -137,13 +137,14 @@ static bool blk_flush_queue_rq(struct request *rq, bool add_front)
 	if (rq->q->mq_ops) {
 		blk_mq_add_to_requeue_list(rq, add_front, true);
 		return false;
-	} else {
-		if (add_front)
-			list_add(&rq->queuelist, &rq->q->queue_head);
-		else
-			list_add_tail(&rq->queuelist, &rq->q->queue_head);
-		return true;
 	}
+
+	if (add_front)
+		list_add(&rq->queuelist, &rq->q->queue_head);
+	else
+		list_add_tail(&rq->queuelist, &rq->q->queue_head);
+
+	return true;
 }
 
 /**
