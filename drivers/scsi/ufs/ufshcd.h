244893a30c15d89d071e5694ab6b9043b0688f29
Stanislav Nijnikov <stanislav.nijnikov@wdc.com>
2017-12-25 10:34:35 +0200

    ufs: sysfs: string descriptors
    Signed-off-by: Stanislav Nijnikov <stanislav.nijnikov@wdc.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -848,6 +848,9 @@ int ufshcd_query_descriptor_retry(struct ufs_hba *hba,
 				  u8 *desc_buf, int *buf_len);
 int ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode,
 	enum flag_idn idn, bool *flag_res);
+int ufshcd_read_string_desc(struct ufs_hba *hba, int desc_index,
+			    u8 *buf, u32 size, bool ascii);
+
 int ufshcd_hold(struct ufs_hba *hba, bool async);
 void ufshcd_release(struct ufs_hba *hba);
 
