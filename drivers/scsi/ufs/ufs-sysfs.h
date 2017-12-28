9d2784ffb541b74bc9ca13d10072bb02512a7703
Stanislav Nijnikov <stanislav.nijnikov@wdc.com>
2017-12-28 15:29:06 +0200

    ufs: sysfs: device descriptor
    This patch introduces a sysfs group entry for the UFS device descriptor
    parameters. The group adds "device_descriptor" folder under the UFS driver
    sysfs entry (/sys/bus/platform/drivers/ufshcd/*). The parameters are shown
    as hexadecimal numbers. The full information about the parameters could be
    found at UFS specifications 2.1.
    
    Signed-off-by: Stanislav Nijnikov <stanislav.nijnikov@wdc.com>

---
+++ drivers/scsi/ufs/ufs-sysfs.h
@@ -0,0 +1,25 @@
+/*
+* UFS Device Management sysfs
+*
+* Copyright (C) 2017 Western Digital Corporation
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License version
+* 2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+*/
+#ifndef __UFS_SYSFS_H__
+#define __UFS_SYSFS_H__
+
+#include <linux/sysfs.h>
+
+#include "ufshcd.h"
+
+void ufs_sysfs_add_device_management(struct ufs_hba *hba);
+void ufs_sysfs_remove_device_management(struct ufs_hba *hba);
+#endif
