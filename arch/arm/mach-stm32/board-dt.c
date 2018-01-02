945af6d6ba0155cd21e90649894d6566f3e644a8
Ludovic Barre <ludovic.barre@st.com>
2018-01-02 16:01:46 +0100

    ARM: stm32: prepare stm32 family to welcome armv7 architecture
    This patch prepares the STM32 machine for the integration of Cortex-A
    based microprocessor (MPU), on top of the existing Cortex-M
    microcontroller family (MCU). Since both MCUs and MPUs are sharing
    common hardware blocks we can keep using ARCH_STM32 flag for most of
    them. If a hardware block is specific to one family we can use either
    ARM_SINGLE_ARMV7M or ARCH_MULTI_V7 flag.
    
    Signed-off-by: Ludovic Barre <ludovic.barre@st.com>

--- arch/arm/mach-stm32/board-dt.c
+++ arch/arm/mach-stm32/board-dt.c
@@ -1,11 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) Maxime Coquelin 2015
  * Author:  Maxime Coquelin <mcoquelin.stm32@gmail.com>
- * License terms:  GNU General Public License (GPL), version 2
  */
 
 #include <linux/kernel.h>
-#include <asm/v7m.h>
 #include <asm/mach/arch.h>
 
 static const char *const stm32_compat[] __initconst = {
@@ -18,5 +17,4 @@ static const char *const stm32_compat[] __initconst = {
 
 DT_MACHINE_START(STM32DT, "STM32 (Device Tree Support)")
 	.dt_compat = stm32_compat,
-	.restart = armv7m_restart,
 MACHINE_END
