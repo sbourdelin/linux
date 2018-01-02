4ce71b197e20edcbcba15b75d569add3b9668cd9
Fugang Duan <fugang.duan@nxp.com>
2018-01-02 17:57:44 +0800

    net: fec: defer probe if regulator is not ready
    Defer probe if regulator is not ready. E.g. some regulator is fixed
    regulator controlled by i2c expander gpio, the i2c device may be probed
    after the driver, then it should handle the case of defer probe error.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3483,6 +3483,10 @@ fec_probe(struct platform_device *pdev)
 			goto failed_regulator;
 		}
 	} else {
+		if (PTR_ERR(fep->reg_phy) == -EPROBE_DEFER) {
+			ret = -EPROBE_DEFER;
+			goto failed_regulator;
+		}
 		fep->reg_phy = NULL;
 	}
 
@@ -3570,6 +3574,7 @@ failed_phy:
 	of_node_put(phy_node);
 failed_ioremap:
 	free_netdev(ndev);
+	dev_id--;
 
 	return ret;
 }
