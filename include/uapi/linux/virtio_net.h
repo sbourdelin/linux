dc3ece21d9476e0097089b0446adfa2acd0b2ee0
Sridhar Samudrala <sridhar.samudrala@intel.com>
2018-01-02 16:35:37 -0800

    virtio_net: Introduce VIRTIO_NET_F_MASTER feature bit
    This feature bit can be used by hypervisor to indicate virtio_net device to
    act as a master for a directly attached passthru device with the same MAC
    address.
    
    Signed-off-by: Sridhar Samudrala <sridhar.samudrala@intel.com>

--- include/uapi/linux/virtio_net.h
+++ include/uapi/linux/virtio_net.h
@@ -56,6 +56,7 @@
 #define VIRTIO_NET_F_MQ	22	/* Device supports Receive Flow
 					 * Steering */
 #define VIRTIO_NET_F_CTRL_MAC_ADDR 23	/* Set MAC address */
+#define VIRTIO_NET_F_MASTER	62	/* act as master for a VF device */
 
 #ifndef VIRTIO_NET_NO_LEGACY
 #define VIRTIO_NET_F_GSO	6	/* Host handles pkts w/ any GSO type */
