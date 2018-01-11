cc2f5332581cecd987e7619a44ef81b19dfff354
Oliver Neukum <oneukum@suse.com>
2018-01-11 16:01:49 +0100

    usbnet: silence an unnecessary warning
    That a kevent could not be scheduled is not an error.
    Such handlers must be able to deal with multiple events anyway.
    As the successful scheduling of a work is a debug event, make
    the failure debug priority, too.
    
    Signed-off-by: Oliver Neukum <oneukum@suse.com>
    Reported-by: Cristian Caravena <caravena@gmail.com>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -458,8 +458,7 @@ void usbnet_defer_kevent (struct usbnet *dev, int work)
 {
 	set_bit (work, &dev->flags);
 	if (!schedule_work (&dev->kevent)) {
-		if (net_ratelimit())
-			netdev_err(dev->net, "kevent %d may have been dropped\n", work);
+		netdev_dbg(dev->net, "kevent %d may have been dropped\n", work);
 	} else {
 		netdev_dbg(dev->net, "kevent %d scheduled\n", work);
 	}
