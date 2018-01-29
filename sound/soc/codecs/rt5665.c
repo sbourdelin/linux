e5e46474c8b04a49d39e2280195f1ac41e9c0fb5
Bard Liao <bardliao@realtek.com>
2018-01-29 19:15:38 +0800

    ASoC: rt5665: remove rt5668 support
    Remove rt5668 support from rt5665 codec driver since rt5668 will
    not go into mass production.
    
    Signed-off-by: Bard Liao <bardliao@realtek.com>

--- sound/soc/codecs/rt5665.c
+++ sound/soc/codecs/rt5665.c
@@ -4828,9 +4828,6 @@ static int rt5665_i2c_probe(struct i2c_client *i2c,
 	case 0x0:
 		rt5665->id = CODEC_5666;
 		break;
-	case 0x6:
-		rt5665->id = CODEC_5668;
-		break;
 	case 0x3:
 	default:
 		rt5665->id = CODEC_5665;
@@ -4963,7 +4960,6 @@ static void rt5665_i2c_shutdown(struct i2c_client *client)
 static const struct of_device_id rt5665_of_match[] = {
 	{.compatible = "realtek,rt5665"},
 	{.compatible = "realtek,rt5666"},
-	{.compatible = "realtek,rt5668"},
 	{},
 };
 MODULE_DEVICE_TABLE(of, rt5665_of_match);
@@ -4973,7 +4969,6 @@ MODULE_DEVICE_TABLE(of, rt5665_of_match);
 static const struct acpi_device_id rt5665_acpi_match[] = {
 	{"10EC5665", 0,},
 	{"10EC5666", 0,},
-	{"10EC5668", 0,},
 	{},
 };
 MODULE_DEVICE_TABLE(acpi, rt5665_acpi_match);
