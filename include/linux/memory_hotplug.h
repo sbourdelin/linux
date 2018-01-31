bcddbf66c023648fda8aaadb202b19e6dea11af0
Pavel Tatashin <pasha.tatashin@oracle.com>
2018-01-31 00:42:43 -0500

    mm: optimize memory hotplug
    This patch was inspired by the discussion of this problem:
    http://lkml.kernel.org/r/20180130083006.GB1245@in.ibm.com
    
    Currently, during memory hotplugging we traverse struct pages several
    times:
    
    1. memset(0) in sparse_add_one_section()
    2. loop in __add_section() to set do: set_page_node(page, nid); and
       SetPageReserved(page);
    3. loop in pages_correctly_reserved() to check that SetPageReserved is set.
    4. loop in memmap_init_zone() to call __init_single_pfn()
    
    This patch removes loops 1, 2, and 3 and only leaves the loop 4, where all
    struct page fields are initialized in one go, the same as it is now done
    during boot.
    
    The benefits:
    - We improve the memory hotplug performance because we are not evicting
      cache several times and also reduce loop branching overheads.
    
    - Remove condition from hotpath in __init_single_pfn(), that was added in
      order to fix the problem that was reported by Bharata in the above email
      thread, thus also improve the performance during normal boot.
    
    - Make memory hotplug more similar to boot memory initialization path
      because we zero and initialize struct pages only in one function.
    
    - Simplifies memory hotplug strut page initialization code, and thus
      enables future improvements, such as multi-threading the initialization
      of struct pages in order to improve the hotplug performance even further
      on larger machines.
    
    Signed-off-by: Pavel Tatashin <pasha.tatashin@oracle.com>

--- include/linux/memory_hotplug.h
+++ include/linux/memory_hotplug.h
@@ -234,6 +234,8 @@ void put_online_mems(void);
 void mem_hotplug_begin(void);
 void mem_hotplug_done(void);
 
+int get_section_nid(unsigned long section_nr);
+
 #else /* ! CONFIG_MEMORY_HOTPLUG */
 #define pfn_to_online_page(pfn)			\
 ({						\
