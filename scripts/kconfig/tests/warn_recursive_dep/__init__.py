3723cfc369f2480ba7193350e6c58cbff133eeb7
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-02-06 09:34:53 +0900

    kconfig: test: check if recursive dependencies are detected
    Recursive dependency should be detected and warned.  Test this.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

---
+++ scripts/kconfig/tests/warn_recursive_dep/__init__.py
@@ -0,0 +1,10 @@
+"""
+Warn recursive inclusion
+========================
+
+Recursive dependency should be warned.
+"""
+
+def test(conf):
+    assert conf.oldaskconfig() == 0
+    assert conf.stderr_contains('expected_stderr')
