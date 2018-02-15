ace95d4f63540a354747085740aa0b49d8f24708
Oscar Mateo <oscar.mateo@intel.com>
2018-02-15 14:46:43 -0800

    drm/i915: Move WA BB stuff to the workarounds file as well
    Since we are trying to put all WA stuff together, do not forget about the BB WAs.
    
    v2: s/intel_bb_workarounds_init/intel_engine_init_bb_workarounds (Chris)
    v3: Rebased to before the WAs are stored
    v4: Rebased
    
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk> (v2)
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>

--- drivers/gpu/drm/i915/intel_workarounds.h
+++ drivers/gpu/drm/i915/intel_workarounds.h
@@ -13,4 +13,7 @@ void intel_gt_workarounds_apply(struct drm_i915_private *dev_priv);
 
 int intel_whitelist_workarounds_apply(struct intel_engine_cs *engine);
 
+int intel_engine_init_bb_workarounds(struct intel_engine_cs *engine);
+void intel_engine_fini_bb_workarounds(struct intel_engine_cs *engine);
+
 #endif
