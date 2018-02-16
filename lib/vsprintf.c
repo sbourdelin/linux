e0ca0e483eef7f415d59b0ef4ec966eb18648b3d
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-02-16 23:07:11 +0200

    lib/vsprintf: Mark expected switch fall-through
    In preparation to enabling -Wimplicit-fallthrough, mark switch cases
    where we are expecting to fall through.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -2081,6 +2081,7 @@ qualifier:
 
 	case 'x':
 		spec->flags |= SMALL;
+		/* fall through */
 
 	case 'X':
 		spec->base = 16;
@@ -3035,8 +3036,10 @@ int vsscanf(const char *buf, const char *fmt, va_list args)
 			break;
 		case 'i':
 			base = 0;
+			/* fall through */
 		case 'd':
 			is_sign = true;
+			/* fall through */
 		case 'u':
 			break;
 		case '%':
