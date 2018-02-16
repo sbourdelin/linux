7a2cb763f5b7f1f6ee34c0535cae219ca57408b1
Sebastian Gottschall <s.gottschall@newmedia-net.de>
2018-02-16 18:20:42 +0100

    v4 ath10k: add LED and GPIO controlling support for various chipsets
    Adds LED and GPIO Control support for 988x, 9887, 9888, 99x0, 9984 and ipq4019 based chipsets with on chipset connected led's
    using WMI Firmware API.
    The LED device will get available named as "ath10k-phyX" at sysfs and can be controlled with various triggers.
    adds also debugfs interface for gpio control.
    
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -24,6 +24,8 @@
 #include <linux/pci.h>
 #include <linux/uuid.h>
 #include <linux/time.h>
+#include <linux/gpio.h>
+#include <linux/leds.h>
 
 #include "htt.h"
 #include "htc.h"
@@ -769,6 +771,19 @@ struct ath10k_per_peer_tx_stats {
 	u32	reserved2;
 };
 
+struct ath10k_gpiocontrol {
+	struct ath10k *ar;
+	u32 gpio_set_num, gpio_num, gpio_input, gpio_pull_type, gpio_intr_mode, gpio_set; /* since we have no gpio read method, these are the state variables for debugfs.  */
+	struct gpio_led wifi_led;
+	struct gpio_chip gchip;
+#ifdef CONFIG_LEDS_CLASS
+	struct led_classdev cdev;
+#endif
+	char label[48];
+	u32 gpio_state_dir; /* same as for debugfs, but for gpiochip implementation */
+	u32 gpio_state_pin;
+};
+
 struct ath10k {
 	struct ath_common ath_common;
 	struct ieee80211_hw *hw;
@@ -797,7 +812,10 @@ struct ath10k {
 	u32 low_5ghz_chan;
 	u32 high_5ghz_chan;
 	bool ani_enabled;
-
+#ifdef CONFIG_GPIOLIB
+	struct ath10k_gpiocontrol *gpio;
+	int gpio_attached;
+#endif
 	bool p2p;
 
 	struct {
