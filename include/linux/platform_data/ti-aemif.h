bc744a84567964271d72413c24a5b5cdb33a6f9c
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2018-02-16 17:47:10 +0100

    memory: aemif: add support for board files
    Currently aemif is supported in two places separately. By the platform
    driver in drivers/memory and by a hand crafted driver in mach-davinci.
    
    We want to drop the latter but also keep the legacy mode. Add support
    for board files to the aemif driver.
    
    The new structure in platform data currently only contains the chip
    select number, since currently existing users don't require anything
    else, but it can be extended in the future.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

--- include/linux/platform_data/ti-aemif.h
+++ include/linux/platform_data/ti-aemif.h
@@ -16,8 +16,17 @@
 
 #include <linux/of_platform.h>
 
+struct aemif_abus_data {
+	u32 cs;
+};
+
 struct aemif_platform_data {
 	struct of_dev_auxdata *dev_lookup;
+	u32 cs_offset;
+	struct aemif_abus_data *abus_data;
+	size_t num_abus_data;
+	struct platform_device *sub_devices;
+	size_t num_sub_devices;
 };
 
 #endif /* __TI_DAVINCI_AEMIF_DATA_H__ */
