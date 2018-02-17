6e0e9fbd6cb48f9c06c0f51d10b9d84682dc8a60
Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
2018-02-17 20:10:35 +0100

    Support the nonstring variable attribute (gcc >= 8)
    From the GCC manual:
    
    The nonstring variable attribute specifies that an object or member
    declaration with type array of char or pointer to char is intended to
    store character arrays that do not necessarily contain a terminating NUL
    character. This is useful in detecting uses of such arrays or pointers
    with functions that expect NUL-terminated strings, and to avoid warnings
    when such an array or pointer is used as an argument to a bounded string
    manipulation function such as strncpy.
    
      https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html
    
    Some reports are already coming to the LKML regarding these
    warnings. When they are false positives, we can use __nonstring to let
    gcc know a NUL character is not required; like in this case:
    
      https://lkml.org/lkml/2018/1/16/135
    
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: David Rientjes <rientjes@google.com>

--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -271,4 +271,8 @@ struct ftrace_likely_data {
 # define __native_word(t) (sizeof(t) == sizeof(char) || sizeof(t) == sizeof(short) || sizeof(t) == sizeof(int) || sizeof(t) == sizeof(long))
 #endif
 
+#ifndef __nonstring
+# define __nonstring
+#endif
+
 #endif /* __LINUX_COMPILER_TYPES_H */
