b18c9741327a37acdab876790f66e55e21c2215c
Geert Uytterhoeven <geert+renesas@glider.be>
2018-02-20 10:40:16 +0100

    serial: arc_uart: Fix out-of-bounds access through DT alias
    The arc_uart_ports[] array is indexed using a value derived from the
    "serialN" alias in DT, which may lead to an out-of-bounds access.
    
    Fix this by adding a range check.
    
    Note that the array size is defined by a Kconfig symbol
    (CONFIG_SERIAL_ARC_NR_PORTS), so this can even be triggered using a
    legitimate DTB.
    
    Fixes: 10640deb04b7949a ("serial: arc_uart: Fix out-of-bounds access through DT alias")
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/tty/serial/arc_uart.c
+++ drivers/tty/serial/arc_uart.c
@@ -593,6 +593,11 @@ static int arc_serial_probe(struct platform_device *pdev)
 	if (dev_id < 0)
 		dev_id = 0;
 
+	if (dev_id >= CONFIG_SERIAL_ARC_NR_PORTS) {
+		dev_err(&pdev->dev, "serial%d out of range\n", dev_id);
+		return -EINVAL;
+	}
+
 	uart = &arc_uart_ports[dev_id];
 	port = &uart->port;
 
