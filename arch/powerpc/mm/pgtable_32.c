12f438ed01a38fdcee7befaaf41b95b7c31d6367
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2018-02-20 17:14:19 +0100

    powerpc/mm/32: Use pfn_valid to check if pointer is in RAM
    The Nintendo Wii has a memory layout that places two chunks of RAM at
    non-adjacent addresses, and MMIO between them. Currently, the allocation
    of these MMIO areas is made possible by declaring the MMIO hole as
    reserved memory and allowing reserved memory to be allocated (cf.
    wii_memory_fixups).
    
    This patch is the first step towards proper support for discontiguous
    memory on PPC32 by using pfn_valid to check if a pointer points into
    RAM, rather than open-coding the check. It should result in no
    functional difference.
    
    Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>

--- arch/powerpc/mm/pgtable_32.c
+++ arch/powerpc/mm/pgtable_32.c
@@ -147,7 +147,7 @@ __ioremap_caller(phys_addr_t addr, unsigned long size, unsigned long flags,
 	 * Don't allow anybody to remap normal RAM that we're using.
 	 * mem_init() sets high_memory so only do the check after that.
 	 */
-	if (slab_is_available() && (p < virt_to_phys(high_memory)) &&
+	if (slab_is_available() && pfn_valid(__phys_to_pfn(p)) &&
 	    !(__allow_ioremap_reserved && memblock_is_region_reserved(p, size))) {
 		printk("__ioremap(): phys addr 0x%llx is RAM lr %ps\n",
 		       (unsigned long long)p, __builtin_return_address(0));
