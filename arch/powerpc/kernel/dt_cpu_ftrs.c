7cdd7aade96a018d72c25d98f82058deef28320f
Nicholas Piggin <npiggin@gmail.com>
2018-02-21 05:08:27 +1000

    powerpc/64s: dt_cpu_ftrs fix POWER9 DD2.2 and above
    The CPU_FTR_POWER9_DD2_1 flag is intended to be set for DD2.1 and
    above (which is what the cputable setup does). Fix dt_cpu_ftrs setup
    to match.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

--- arch/powerpc/kernel/dt_cpu_ftrs.c
+++ arch/powerpc/kernel/dt_cpu_ftrs.c
@@ -707,7 +707,9 @@ static __init void cpufeatures_cpu_quirks(void)
 	 */
 	if ((version & 0xffffff00) == 0x004e0100)
 		cur_cpu_spec->cpu_features |= CPU_FTR_POWER9_DD1;
-	else if ((version & 0xffffefff) == 0x004e0201)
+	else if ((version & 0xffffefff) == 0x004e0200)
+		; /* DD2.0 has no feature flag */
+	else /* DD2.1 and up have DD2_1 */
 		cur_cpu_spec->cpu_features |= CPU_FTR_POWER9_DD2_1;
 }
 
