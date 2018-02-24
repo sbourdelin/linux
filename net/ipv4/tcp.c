ba4f5b62a2565718cf1b85e736df12da68bbb114
Stephen Hemminger <stephen@networkplumber.org>
2018-02-24 11:04:54 -0800

    net: mark slab's used by ss as UAPI
    The iproute2 ss command reads /proc/slabinfo as way to get estimates
    for number of open sockets etc. This has been broken since slab
    merging went in 3.17.
    
    Mark those kmem caches's as non mergeable with new flag.
    The TCP caches's are already not mergeable because of the RCU
    flags, but someone might change that and cause surprise later.
    
    Reported-by: Igor Komyagin <ikomyagin@gmail.com>
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3617,7 +3617,8 @@ void __init tcp_init(void)
 	tcp_hashinfo.bind_bucket_cachep =
 		kmem_cache_create("tcp_bind_bucket",
 				  sizeof(struct inet_bind_bucket), 0,
-				  SLAB_HWCACHE_ALIGN|SLAB_PANIC, NULL);
+				  SLAB_HWCACHE_ALIGN|SLAB_PANIC|SLAB_VISIBLE_UAPI,
+				  NULL);
 
 	/* Size and allocate the main established and bind bucket
 	 * hash tables.
--- net/socket.c
+++ net/socket.c
@@ -286,9 +286,9 @@ static void init_inodecache(void)
 	sock_inode_cachep = kmem_cache_create("sock_inode_cache",
 					      sizeof(struct socket_alloc),
 					      0,
-					      (SLAB_HWCACHE_ALIGN |
-					       SLAB_RECLAIM_ACCOUNT |
-					       SLAB_MEM_SPREAD | SLAB_ACCOUNT),
+					      SLAB_HWCACHE_ALIGN | SLAB_VISIBLE_UAPI |
+					      SLAB_RECLAIM_ACCOUNT |
+					      SLAB_MEM_SPREAD | SLAB_ACCOUNT,
 					      init_once);
 	BUG_ON(sock_inode_cachep == NULL);
 }
