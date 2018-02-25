f759dfe362abd34412524975080d537a663502d9
Ben Caradoc-Davies <ben@transient.nz>
2018-02-26 10:15:41 +1300

    Revert "mac80211: use QoS NDP for AP probing"
    This reverts commit 7b6ddeaf27eca72795ceeae2f0f347db1b5f9a30.
    
    The above commit causes an Atheros AR9271 ath9k_htc USB WiFi adapter
    connected to an AP with QoS/WME enabled to lose all IP connectivity after
    something like 10 to 90 minutes. The adapter remains up and associated
    and "iw dev wlan0 station dump" shows byte and packet counters that keep
    increasing, but all IP connectivity fails, including ping, DNS, and web.
    The host cannot be pinged by other hosts on the WLAN. Network can be
    restored by unloading and reloading the ath9k_htc module, or physically
    unplugging and replugging the adapter, triggering NetworkManager to
    reconnect.
    
    The problematic commit is on torvalds/master and linux-stable/linux-4.15.y.
    On linux-stable/linux-4.14.y: e23090a7d8f05f03cf564148472130286f5ca9bf.
    
    Problem confirmed on Debian linux-image-4.14.0-3-amd64 4.14.17-1 and
    Debian linux-image-4.15.0-1-amd64 4.15.4-1 and vanilla 4.14.16
    (git e23090a7d8f0 from linux-stable/linux-4.14.y) and vanilla 4.16.0-rc2
    (git 3664ce2d9309 from torvalds/master).
    
    Fix tested by reverting the commit on vanilla 4.16.0-rc2 (git 3664ce2d9309
    from torvalds/master) and applying the patch to Debian
    linux-image-4.15.0-1-amd64 4.15.4-1. Both tests resulted in stable IP
    connectivity.
    
    See also Debian Bug#891060:
    Atheros AR9271 ath9k_htc USB WiFi connected but IP traffic stops
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=891060
    
    Signed-off-by: Ben Caradoc-Davies <ben@transient.nz>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -896,7 +896,7 @@ void ieee80211_send_nullfunc(struct ieee80211_local *local,
 	struct ieee80211_hdr_3addr *nullfunc;
 	struct ieee80211_if_managed *ifmgd = &sdata->u.mgd;
 
-	skb = ieee80211_nullfunc_get(&local->hw, &sdata->vif, true);
+	skb = ieee80211_nullfunc_get(&local->hw, &sdata->vif);
 	if (!skb)
 		return;
 
