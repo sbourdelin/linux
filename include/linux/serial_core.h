450a6f10b615a188202b448192cfede29e5f6008
Mathieu Malaterre <malat@debian.org>
2018-02-25 18:22:18 +0100

    powerpc: Mark the variable earlycon_acpi_spcr_enable maybe_unused
    Re-use the object-like macro EARLYCON_USED_OR_UNUSED to mark
    `earlycon_acpi_spcr_enable` as maybe_unused.
    
    Fix the following warning (treated as error in W=1)
    
      CC      arch/powerpc/kernel/setup-common.o
    In file included from ./include/linux/serial_8250.h:14:0,
                     from arch/powerpc/kernel/setup-common.c:33:
    ./include/linux/serial_core.h:382:19: error: ‘earlycon_acpi_spcr_enable’ defined but not used [-Werror=unused-const-variable=]
     static const bool earlycon_acpi_spcr_enable;
                       ^~~~~~~~~~~~~~~~~~~~~~~~~
    cc1: all warnings being treated as errors
    
    Signed-off-by: Mathieu Malaterre <malat@debian.org>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -379,6 +379,7 @@ extern int of_setup_earlycon(const struct earlycon_id *match,
 extern bool earlycon_acpi_spcr_enable __initdata;
 int setup_earlycon(char *buf);
 #else
+EARLYCON_USED_OR_UNUSED
 static const bool earlycon_acpi_spcr_enable;
 static inline int setup_earlycon(char *buf) { return 0; }
 #endif
