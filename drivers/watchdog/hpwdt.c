ec65d0c69fcb62c43b3c8b4d8189e35abc7eca9d
Jerry Hoemann <jerry.hoemann@hpe.com>
2018-02-25 14:32:53 -0700

    watchdog/hpwdt: Update Module info.
    Update Module description to reflect branding changes.
    
    Signed-off-by: Jerry Hoemann <jerry.hoemann@hpe.com>

--- drivers/watchdog/hpwdt.c
+++ drivers/watchdog/hpwdt.c
@@ -429,7 +429,7 @@ static struct pci_driver hpwdt_driver = {
 };
 
 MODULE_AUTHOR("Tom Mingarelli");
-MODULE_DESCRIPTION("hp watchdog driver");
+MODULE_DESCRIPTION("hpe watchdog driver");
 MODULE_LICENSE("GPL");
 MODULE_VERSION(HPWDT_VERSION);
 
