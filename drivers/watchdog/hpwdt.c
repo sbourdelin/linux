95e2dd9a7a3692db1f07a597e2e8b83acd137147
Jerry Hoemann <jerry.hoemann@hpe.com>
2018-02-25 14:32:55 -0700

    watchdog/hpwdt: condition early return of NMI handler on iLO5
    Modify prior change to not claim an NMI unless originated
    from iLO to apply only to iLO5 and later going forward.
    This restores hpwdt traditional behavior of calling panic
    if the NMI is NMI_IO_CHECK, NMI_SERR, or NMI_UNKNOWN for
    legacy hardware.
    
    Signed-off-by: Jerry Hoemann <jerry.hoemann@hpe.com>

--- drivers/watchdog/hpwdt.c
+++ drivers/watchdog/hpwdt.c
@@ -32,6 +32,7 @@
 #define HPWDT_MAX_TIMER			TICKS_TO_SECS(65535)
 #define DEFAULT_MARGIN			30
 
+static bool iLO5;
 static unsigned int soft_margin = DEFAULT_MARGIN;	/* in seconds */
 static unsigned int reload;			/* the computed soft_margin */
 static bool nowayout = WATCHDOG_NOWAYOUT;
@@ -121,7 +122,7 @@ static int hpwdt_pretimeout(unsigned int ulReason, struct pt_regs *regs)
 		"3. OA Forward Progress Log\n"
 		"4. iLO Event Log";
 
-	if ((ulReason == NMI_UNKNOWN) && mynmi)
+	if (iLO5 && ulReason == NMI_UNKNOWN && mynmi)
 		return NMI_DONE;
 
 	if (allow_kdump)
@@ -278,6 +279,9 @@ static int hpwdt_init_one(struct pci_dev *dev,
 	dev_info(&dev->dev, "HPE Watchdog Timer Driver: %s"
 			", timer margin: %d seconds (nowayout=%d).\n",
 			HPWDT_VERSION, hpwdt_dev.timeout, nowayout);
+
+	if (dev->subsystem_vendor == PCI_VENDOR_ID_HP_3PAR)
+		iLO5 = 1;
 
 	return 0;
 
