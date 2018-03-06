15b7e5ced521e2746d3757d09cb6d58833794bdc
Peter Maydell <peter.maydell@linaro.org>
2018-03-06 16:26:46 +0000

    KVM: arm: reserve bit in KVM_REG_ARM encoding for secure/nonsecure
    We have a KVM_REG_ARM encoding that we use to expose KVM guest registers
    to userspace. Define that bit 28 in this encoding indicates secure vs
    nonsecure, so we can distinguish the secure and nonsecure banked versions
    of a banked AArch32 register.
    
    For KVM currently, all guest registers are nonsecure, but defining
    the bit is useful for userspace. In particular, QEMU uses this
    encoding as part of its on-the-wire migration format, and needs to be
    able to describe secure-bank registers when it is migrating (fully
    emulated) EL3-enabled CPUs.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- arch/arm/include/uapi/asm/kvm.h
+++ arch/arm/include/uapi/asm/kvm.h
@@ -135,6 +135,11 @@ struct kvm_arch_memory_slot {
 #define KVM_REG_ARM_CRM_SHIFT		7
 #define KVM_REG_ARM_32_CRN_MASK		0x0000000000007800
 #define KVM_REG_ARM_32_CRN_SHIFT	11
+/* For KVM currently all guest registers are nonsecure, but we reserve a bit
+ * in the encoding to distinguish secure from nonsecure for banked registers.
+ */
+#define KVM_REG_ARM_SECURE_MASK	0x0000000010000000
+#define KVM_REG_ARM_SECURE_SHIFT	28
 
 #define ARM_CP15_REG_SHIFT_MASK(x,n) \
 	(((x) << KVM_REG_ARM_ ## n ## _SHIFT) & KVM_REG_ARM_ ## n ## _MASK)
