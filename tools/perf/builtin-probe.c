011407b2a5360cf1201307f07ed17e705fe59a24
Holger Hans Peter Freyther <holgar+kernel@google.com>
2018-05-14 12:19:35 +0800

    perf probe: Do not exclude mangled C++ funcs
    Using --funcs --no-demangle on a C++ binary does not list any of the C++
    functions. Change the default filter to not exclude the Common C++ ABI
    symbols.
    
     $ ./perf probe -x ./cxx-example --funcs --no-demangle
     ...
     _ZN9__gnu_cxx13new_allocatorIiEC1Ev
     ...
    
    Signed-off-by: Holger Hans Peter Freyther <holgar+kernel@google.com>

--- tools/perf/builtin-probe.c
+++ tools/perf/builtin-probe.c
@@ -43,7 +43,7 @@
 #include "util/probe-file.h"
 
 #define DEFAULT_VAR_FILTER "!__k???tab_* & !__crc_*"
-#define DEFAULT_FUNC_FILTER "!_*"
+#define DEFAULT_FUNC_FILTER "!_* | _Z*"
 #define DEFAULT_LIST_FILTER "*"
 
 /* Session management structure */
