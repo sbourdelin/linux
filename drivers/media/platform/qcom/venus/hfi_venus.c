b16d5a51e13f98400eb313c2f8f780a131685c8b
Stanimir Varbanov <stanimir.varbanov@linaro.org>
2018-05-15 10:58:39 +0300

    venus: hfi_venus: move set of default properties to core init
    This moves setting of default properties (firmware debug, idle
    indicator and low power mode) from session init to core init.
    All of those properties are need to be enabled/disabled early
    so that they could be used before the clients are even initialized.
    
    The other reason is to set idle indicator property early before
    we enter into venus_suspend function where we need to check for
    ARM9 WFI.
    
    Signed-off-by: Stanimir Varbanov <stanimir.varbanov@linaro.org>

--- drivers/media/platform/qcom/venus/hfi_venus.c
+++ drivers/media/platform/qcom/venus/hfi_venus.c
@@ -1090,6 +1090,10 @@ static int venus_core_init(struct venus_core *core)
 	if (ret)
 		dev_warn(dev, "failed to send image version pkt to fw\n");
 
+	ret = venus_sys_set_default_properties(hdev);
+	if (ret)
+		return ret;
+
 	return 0;
 }
 
@@ -1133,10 +1137,6 @@ static int venus_session_init(struct venus_inst *inst, u32 session_type,
 	struct venus_hfi_device *hdev = to_hfi_priv(inst->core);
 	struct hfi_session_init_pkt pkt;
 	int ret;
-
-	ret = venus_sys_set_default_properties(hdev);
-	if (ret)
-		return ret;
 
 	ret = pkt_session_init(&pkt, inst, session_type, codec);
 	if (ret)
