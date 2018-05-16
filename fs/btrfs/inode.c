2836fdfe34feeba0be0d43db751fdffa4eac1bab
Ethan Lien <ethanlien@synology.com>
2018-05-16 16:28:54 +0800

    btrfs: Skip some btrfs_cross_ref_exist() check in nocow path
    In nocow path, we check if the extent is snapshotted in
    btrfs_cross_ref_exist(). We can do the similar check earlier and avoid
    unnecessary search into extent tree.
    
    Signed-off-by: Ethan Lien <ethanlien@synology.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -1382,6 +1382,9 @@ next_slot:
 			    btrfs_file_extent_encryption(leaf, fi) ||
 			    btrfs_file_extent_other_encoding(leaf, fi))
 				goto out_check;
+			if (btrfs_file_extent_generation(leaf, fi) <=
+			    btrfs_root_last_snapshot(&root->root_item))
+				goto out_check;
 			if (extent_type == BTRFS_FILE_EXTENT_REG && !force)
 				goto out_check;
 			if (btrfs_extent_readonly(fs_info, disk_bytenr))
@@ -7488,6 +7491,10 @@ noinline int can_nocow_extent(struct inode *inode, u64 offset, u64 *len,
 	if (btrfs_file_extent_compression(leaf, fi) ||
 	    btrfs_file_extent_encryption(leaf, fi) ||
 	    btrfs_file_extent_other_encoding(leaf, fi))
+		goto out;
+
+	if (btrfs_file_extent_generation(leaf, fi) <=
+	    btrfs_root_last_snapshot(&root->root_item))
 		goto out;
 
 	backref_offset = btrfs_file_extent_offset(leaf, fi);
