2bd846608407514b7fcaf6da297a254c7b403482
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-05-18 13:52:25 +0900

    kconfig: refactor ncurses package checks for building mconf
    The mconf (or its infrastructure, lxdiaglog) depends on ncurses.
    check-lxdialog.sh has additional checks in case pkg-config is not
    available.  However, qconf and gconf already rely on pkg-config to
    check necessary packages.  For simplification, drop the fallback
    code from check-lxdialog.sh and move/rename to mconf-cfg.sh to
    make it work in the same way as the other GUI frontends.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

---
+++ scripts/kconfig/mconf-cfg.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+
+PKG="ncursesw"
+PKG2="ncurses"
+
+if pkg-config --exists $PKG; then
+	echo cflags=\"-DNCURSES_WIDECHAR=1 $(pkg-config --cflags $PKG)\"
+	echo libs=\"$(pkg-config --libs $PKG)\"
+	exit 0
+fi
+
+if pkg-config --exists $PKG2; then
+	echo cflags=\"$(pkg-config --cflags $PKG2)\"
+	echo libs=\"$(pkg-config --libs $PKG2)\"
+	exit 0
+fi
+
+echo >&2 "*"
+echo >&2 "* Unable to find the ncurses."
+echo >&2 "* Install ncurses (ncurses-devel or libncurses-dev"
+echo >&2 "* depending on your distribution)"
+echo >&2 "*"
+exit 1
