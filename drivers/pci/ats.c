2bcac1578edd417d9c9c5cb7df013b8f78dc26c0
Sinan Kaya <okaya@codeaurora.org>
2018-05-19 12:52:49 -0400

    PCI: Do not enable PASID when End-to-End TLP is not supported
    A PCIe endpoint carries the process address space identifier (PASID) in
    the TLP prefix as part of the memory read/write transaction. The address
    information in the TLP is relevant only for a given PASID context.
    
    A translation agent takes PASID value and the address information from the
    TLP to look up the physical address in the system.
    
    If a bridge drops the TLP prefix, the translation agent can resolve the
    address to an incorrect location and cause data corruption. Prevent
    this condition by requiring End-to-End TLP prefix to be supported on the
    entire data path between the endpoint and the root port.
    
    Signed-off-by: Sinan Kaya <okaya@codeaurora.org>

--- drivers/pci/ats.c
+++ drivers/pci/ats.c
@@ -268,7 +268,9 @@ EXPORT_SYMBOL_GPL(pci_reset_pri);
 int pci_enable_pasid(struct pci_dev *pdev, int features)
 {
 	u16 control, supported;
+	struct pci_dev *bridge;
 	int pos;
+	u32 cap;
 
 	if (WARN_ON(pdev->pasid_enabled))
 		return -EBUSY;
@@ -276,6 +278,20 @@ int pci_enable_pasid(struct pci_dev *pdev, int features)
 	pos = pci_find_ext_capability(pdev, PCI_EXT_CAP_ID_PASID);
 	if (!pos)
 		return -EINVAL;
+
+	bridge = pci_upstream_bridge(pdev);
+	while (bridge) {
+		if (!pci_find_capability(bridge, PCI_CAP_ID_EXP))
+			return -EINVAL;
+
+		if (pcie_capability_read_dword(bridge, PCI_EXP_DEVCAP2, &cap))
+			return -EINVAL;
+
+		if (!(cap & PCI_EXP_DEVCAP2_E2ETLP))
+			return -EINVAL;
+
+		bridge = pci_upstream_bridge(bridge);
+	}
 
 	pci_read_config_word(pdev, pos + PCI_PASID_CAP, &supported);
 	supported &= PCI_PASID_CAP_EXEC | PCI_PASID_CAP_PRIV;
