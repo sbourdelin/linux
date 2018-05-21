73491a8a1906ba56cc8467342bc9b17745ac43df
Heiner Kallweit <hkallweit1@gmail.com>
2018-05-21 19:01:19 +0200

    r8169: perform reset synchronously in __rtl8169_resume
    The driver uses pm_runtime_get_sync() in few places and relies on the
    device being fully runtime-resumed after this call. So far however
    the runtime resume callback triggers an asynchronous reset.
    Avoid this and perform the reset synchronously.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>

--- drivers/net/ethernet/realtek/r8169.c
+++ drivers/net/ethernet/realtek/r8169.c
@@ -7327,9 +7327,9 @@ static void __rtl8169_resume(struct net_device *dev)
 	rtl_lock_work(tp);
 	napi_enable(&tp->napi);
 	set_bit(RTL_FLAG_TASK_ENABLED, tp->wk.flags);
+	if (netif_running(dev))
+		rtl_reset_work(tp);
 	rtl_unlock_work(tp);
-
-	rtl_schedule_task(tp, RTL_FLAG_TASK_RESET_PENDING);
 }
 
 static int rtl8169_resume(struct device *device)
