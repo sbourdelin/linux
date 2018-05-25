8e4ce14d84b87f88a87c9bd1acdcdde4a50befee
Ming Lei <ming.lei@redhat.com>
2018-05-25 11:45:57 +0800

    fs/buffer.c: use bvec iterator to truncate the bio
    Once multipage bvec is enabled, the last bvec may include more than one
    page, this patch use segment_last_page() to truncate the bio.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- fs/buffer.c
+++ fs/buffer.c
@@ -3021,7 +3021,10 @@ void guard_bio_eod(int op, struct bio *bio)
 
 	/* ..and clear the end of the buffer for reads */
 	if (op == REQ_OP_READ) {
-		zero_user(bvec->bv_page, bvec->bv_offset + bvec->bv_len,
+		struct bio_vec bv;
+
+		segment_last_page(bvec, &bv);
+		zero_user(bv.bv_page, bv.bv_offset + bv.bv_len,
 				truncated_bytes);
 	}
 }
