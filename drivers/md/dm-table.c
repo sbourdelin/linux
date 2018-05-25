d5a08c2326070088420a96c650c496065d7cb2e1
Ross Zwisler <ross.zwisler@linux.intel.com>
2018-05-25 16:00:51 -0600

    dm: fix test for DAX device support
    Currently device_supports_dax() just checks to see if the QUEUE_FLAG_DAX
    flag is set on the device's request queue to decide whether or not the
    device supports filesystem DAX.  This is insufficient because there are
    devices like PMEM namespaces in raw mode which have QUEUE_FLAG_DAX set but
    which don't actually support DAX.
    
    This means that you could create a dm-linear device, for example, where the
    first part of the dm-linear device was a PMEM namespace in fsdax mode and
    the second part was a PMEM namespace in raw mode.  Both DM and the
    filesystem you put on that dm-linear device would think the whole device
    supports DAX, which would lead to bad behavior once your raw PMEM namespace
    part using DAX needed struct page for something.
    
    Fix this by using bdev_dax_supported() like filesystems do at mount time.
    This checks for raw mode and also performs other tests like checking to
    make sure the dax_direct_access() path works.
    
    Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Fixes: commit 545ed20e6df6 ("dm: add infrastructure for DAX support")

--- drivers/md/dm-table.c
+++ drivers/md/dm-table.c
@@ -885,9 +885,7 @@ EXPORT_SYMBOL_GPL(dm_table_set_type);
 static int device_supports_dax(struct dm_target *ti, struct dm_dev *dev,
 			       sector_t start, sector_t len, void *data)
 {
-	struct request_queue *q = bdev_get_queue(dev->bdev);
-
-	return q && blk_queue_dax(q);
+	return bdev_dax_supported(dev->bdev, PAGE_SIZE);
 }
 
 static bool dm_table_supports_dax(struct dm_table *t)
