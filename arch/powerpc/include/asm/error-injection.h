7443d919e510ddcc783cfb51319c8253da548dac
linux-kernel-owner@vger.kernel.org <linux-kernel-owner@vger.kernel.org>
2018-05-29 18:06:03 +0530

    powerpc: Add support for function error injection
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>

---
+++ arch/powerpc/include/asm/error-injection.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_ERROR_INJECTION_H
+#define _ASM_ERROR_INJECTION_H
+
+#include <asm-generic/error-injection.h>
+
+#define ARCH_FUNC_RET	"blr"
+
+#endif /* _ASM_ERROR_INJECTION_H */
