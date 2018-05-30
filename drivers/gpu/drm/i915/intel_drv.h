7981c0e4d7c286626fe3a48c45329ae29e47a678
Ville Syrjälä <ville.syrjala@linux.intel.com>
2018-05-30 19:59:29 +0300

    drm/i915: Introduce intel_plane_alloc()
    Pull the common plane+plane_state allocation into a small helper.
    Reduces the amount of boilerplate in the plane initialization
    functions.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -2085,6 +2085,9 @@ bool skl_plane_has_ccs(struct drm_i915_private *dev_priv,
 bool intel_format_is_yuv(uint32_t format);
 bool skl_plane_has_planar(struct drm_i915_private *dev_priv,
 			  enum pipe pipe, enum plane_id plane_id);
+struct intel_plane *intel_plane_alloc(void);
+void intel_plane_free(struct intel_plane *plane);
+
 
 /* intel_tv.c */
 void intel_tv_init(struct drm_i915_private *dev_priv);
