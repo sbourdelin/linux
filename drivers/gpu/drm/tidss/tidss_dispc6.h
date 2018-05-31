9a8e9a26d32fad4d218973cbfc7e1274a84fcfd5
Tomi Valkeinen <tomi.valkeinen@ti.com>
2018-05-31 15:41:06 +0300

    drm/tidss: add new driver for TI Keystone platforms
    This patch adds a new DRM driver for Texas Instruments DSS6 IP used on
    Texas Instruments Keystone K2G SoC. The DSS6 IP is a major change to the
    older DSS IP versions, which are supported by the omapdrm driver, and
    while on higher level the DSS6 resembles the older DSS versions, the
    registers and the internal pipelines differ a lot.
    
    DSS6 IP on K2G is a "ultra-light" version, and has only a single plane
    and a single output. The driver will also support future DSS versions,
    which support multiple planes and outputs, so the driver already has
    support for those.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

---
+++ drivers/gpu/drm/tidss/tidss_dispc6.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+ * Author: Tomi Valkeinen <tomi.valkeinen@ti.com>
+ */
+
+#ifndef __TIDSS_DISPC6_H__
+#define __TIDSS_DISPC6_H__
+
+#include "tidss_dispc.h"
+
+#endif
