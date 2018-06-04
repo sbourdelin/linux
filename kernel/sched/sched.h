e0369b2b9cd8932a0282eb7dde7fad3abbb981f8
Srikar Dronamraju <srikar@linux.vnet.ibm.com>
2018-06-04 15:30:18 +0530

    sched/numa: Modify migrate_swap to accept additional params
    There are checks in migrate_swap_stop that check if the task/cpu
    combination is as per migrate_swap_arg before migrating.
    
    However atleast one of the two tasks to be swapped by migrate_swap could
    have migrated to a completely different cpu before updating the
    migrate_swap_arg. The new cpu where the task is currently running could
    be a different node too. If the task has migrated, numa balancer might
    end up placing a task in a wrong node.  Instead of achieving node
    consolidation, it may end up spreading the load across nodes.
    
    To avoid that pass the cpus as additional parameters.
    
    While here, place migrate_swap under CONFIG_NUMA_BALANCING.
    
    Signed-off-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1068,7 +1068,8 @@ enum numa_faults_stats {
 };
 extern void sched_setnuma(struct task_struct *p, int node);
 extern int migrate_task_to(struct task_struct *p, int cpu);
-extern int migrate_swap(struct task_struct *, struct task_struct *);
+extern int migrate_swap(struct task_struct *p, struct task_struct *t,
+			int cpu, int scpu);
 #endif /* CONFIG_NUMA_BALANCING */
 
 #ifdef CONFIG_SMP
