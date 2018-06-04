c5ceeef2841d63235a2f7360b4f7894ec06eca5b
Piotr Piorkowski <piotr.piorkowski@intel.com>
2018-06-04 16:19:46 +0200

    drm/i915/guc: Move defines with size of GuC logs to intel_guc_log.h
    At this moment, we have defined GuC logs sizes in intel_guc_fwif.h, but
    as these values are related directly to the GuC logs, and not to API of
    GuC parameters, we should move these defines to intel_guc_log.h.
    
    v2:
    - change buffers size to more friendly (Michał Wajdeczko)
    - remove GUC_LOG_SIZE define (Michał Wajdeczko)
    
    Signed-off-by: Piotr Piórkowski <piotr.piorkowski@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Michał Winiarski <michal.winiarski@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Michal Wajdeczko <michal.wajdeczko@intel.com>

--- drivers/gpu/drm/i915/intel_guc_log.h
+++ drivers/gpu/drm/i915/intel_guc_log.h
@@ -34,12 +34,9 @@
 
 struct intel_guc;
 
-/*
- * The first page is to save log buffer state. Allocate one
- * extra page for others in case for overlap
- */
-#define GUC_LOG_SIZE	((1 + GUC_LOG_DPC_PAGES + 1 + GUC_LOG_ISR_PAGES + \
-			  1 + GUC_LOG_CRASH_PAGES + 1) << PAGE_SHIFT)
+#define CRASH_BUFFER_SIZE	(8 * 1024)
+#define DPC_BUFFER_SIZE		(32 * 1024)
+#define ISR_BUFFER_SIZE		(32 * 1024)
 
 /*
  * While we're using plain log level in i915, GuC controls are much more...
