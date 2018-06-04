1a39381d70000f0097ec6e2ceb75812d6c00b2f1
Nadav Amit <namit@vmware.com>
2018-06-04 04:21:26 -0700

    x86: alternatives: macrofy locks for better inlining
    GCC considers the number of statements in inlined assembly blocks,
    according to new-lines and semicolons, as an indication to the cost of
    the block in time and space. This data is distorted by the kernel code,
    which puts information in alternative sections. As a result, the
    compiler may perform incorrect inlining and branch optimizations.
    
    The solution is to set an assembly macro and call it from the inlined
    assembly block. As a result GCC considers the inline assembly block as
    a single instruction.
    
    This patch handles the LOCK prefix, allowing more aggresive inlining.
    
       text	   data	    bss	    dec	    hex	filename
    18140140 10225284 2957312 31322736 1ddf270 ./vmlinux before
    18146889 10225380 2957312 31329581 1de0d2d ./vmlinux after (+6845)
    
    Static text symbols:
    Before:	40286
    After:	40218	(-68)
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: x86@kernel.org
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    
    Signed-off-by: Nadav Amit <namit@vmware.com>

--- arch/x86/include/asm/alternative.h
+++ arch/x86/include/asm/alternative.h
@@ -30,20 +30,8 @@
  * and size information.  That keeps the table sizes small.
  */
 
-#ifdef CONFIG_SMP
-#define LOCK_PREFIX_HERE \
-		".pushsection .smp_locks,\"a\"\n"	\
-		".balign 4\n"				\
-		".long 671f - .\n" /* offset */		\
-		".popsection\n"				\
-		"671:"
-
-#define LOCK_PREFIX LOCK_PREFIX_HERE "\n\tlock; "
-
-#else /* ! CONFIG_SMP */
-#define LOCK_PREFIX_HERE ""
-#define LOCK_PREFIX ""
-#endif
+#define LOCK_PREFIX_HERE "LOCK_PREFIX_HERE\n\t"
+#define LOCK_PREFIX "LOCK_PREFIX "
 
 struct alt_instr {
 	s32 instr_offset;	/* original instruction */
