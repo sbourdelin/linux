57652dd849f527e28d31bbfa489487829020971c
Marek Vasut <marek.vasut@gmail.com>
2018-06-06 01:10:04 +0200

    mfd: da9063: Use PLATFORM_DEVID_NONE
    Use PLATFORM_DEVID_NONE instead of -1 in mfd_add_devices.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Cc: Geert Uytterhoeven <geert+renesas@glider.be>
    Cc: Lee Jones <lee.jones@linaro.org>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Steve Twiss <stwiss.opensource@diasemi.com>
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Cc: linux-renesas-soc@vger.kernel.org

--- drivers/mfd/da9063-core.c
+++ drivers/mfd/da9063-core.c
@@ -226,7 +226,7 @@ int da9063_device_init(struct da9063 *da9063, unsigned int irq)
 
 	da9063->irq_base = regmap_irq_chip_get_base(da9063->regmap_irq);
 
-	ret = mfd_add_devices(da9063->dev, -1, da9063_devs,
+	ret = mfd_add_devices(da9063->dev, PLATFORM_DEVID_NONE, da9063_devs,
 			      ARRAY_SIZE(da9063_devs), NULL, da9063->irq_base,
 			      NULL);
 	if (ret) {
