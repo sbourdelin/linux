e8da27b5ca056098976b6aa604914bbefd5f1853
Brett T. Warden <brett.t.warden@intel.com>
2018-06-05 17:43:06 -0700

    module: Implement sig_unenforce parameter
    When CONFIG_MODULE_SIG_FORCE is enabled, also provide a boot-time-only
    parameter, module.sig_unenforce, to disable signature enforcement. This
    allows distributions to ship with signature verification enforcement
    enabled by default, but for users to elect to disable it without
    recompiling, to support building and loading out-of-tree modules.
    
    Signed-off-by: Brett T. Warden <brett.t.warden@intel.com>

--- kernel/module.c
+++ kernel/module.c
@@ -64,6 +64,7 @@
 #include <linux/bsearch.h>
 #include <linux/dynamic_debug.h>
 #include <linux/audit.h>
+#include <linux/efi.h>
 #include <uapi/linux/module.h>
 #include "module-internal.h"
 
@@ -274,9 +275,13 @@ static void module_assert_mutex_or_preempt(void)
 }
 
 static bool sig_enforce = IS_ENABLED(CONFIG_MODULE_SIG_FORCE);
-#ifndef CONFIG_MODULE_SIG_FORCE
+#ifdef CONFIG_MODULE_SIG_FORCE
+/* Allow disabling module signature requirement by adding boot param */
+static bool sig_unenforce;
+module_param(sig_unenforce, bool_enable_only, 0444);
+#else /* !CONFIG_MODULE_SIG_FORCE */
 module_param(sig_enforce, bool_enable_only, 0644);
-#endif /* !CONFIG_MODULE_SIG_FORCE */
+#endif /* CONFIG_MODULE_SIG_FORCE */
 
 /*
  * Export sig_enforce kernel cmdline parameter to allow other subsystems rely
@@ -414,6 +419,8 @@ extern const struct kernel_symbol __stop___ksymtab_unused_gpl[];
 extern const s32 __start___kcrctab_unused[];
 extern const s32 __start___kcrctab_unused_gpl[];
 #endif
+
+extern struct boot_params boot_params;
 
 #ifndef CONFIG_MODVERSIONS
 #define symversion(base, idx) NULL
@@ -4243,6 +4250,20 @@ static const struct file_operations proc_modules_operations = {
 static int __init proc_modules_init(void)
 {
 	proc_create("modules", 0, NULL, &proc_modules_operations);
+
+#ifdef CONFIG_MODULE_SIG_FORCE
+	switch (boot_params.secure_boot) {
+	case efi_secureboot_mode_unset:
+	case efi_secureboot_mode_unknown:
+	case efi_secureboot_mode_disabled:
+		/*
+		 * sig_unenforce is only applied if SecureBoot is not
+		 * enabled.
+		 */
+		sig_enforce = !sig_unenforce;
+	}
+#endif
+
 	return 0;
 }
 module_init(proc_modules_init);
