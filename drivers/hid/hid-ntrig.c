6c4895ecaed2b936801aeab602c3df2a83d29bc0
linux-kernel-owner@vger.kernel.org <linux-kernel-owner@vger.kernel.org>
2018-06-11 16:54:56 +0800

    HID: hid-ntrig: add error handling for sysfs_create_group
    When sysfs_create_group fails, the lack of error-handling code may
    cause unexpected results.
    
    This patch adds error-handling code after calling sysfs_create_group.
    
    Signed-off-by: Zhouyang Jia <jiazhouyang09@gmail.com>

--- drivers/hid/hid-ntrig.c
+++ drivers/hid/hid-ntrig.c
@@ -955,6 +955,10 @@ static int ntrig_probe(struct hid_device *hdev, const struct hid_device_id *id)
 
 	ret = sysfs_create_group(&hdev->dev.kobj,
 			&ntrig_attribute_group);
+	if (ret) {
+		hid_err(hdev, "cannot create sysfs group\n");
+		goto err_free;
+	}
 
 	return 0;
 err_free:
