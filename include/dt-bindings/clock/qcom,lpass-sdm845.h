c6cf0d77c5b4fdec4dfbd1b9f0d959707137de42
Taniya Das <tdas@codeaurora.org>
2018-06-14 13:09:02 +0530

    dt-bindings: clock: Introduce QCOM LPASS clock bindings
    Add device tree bindings for Low Power Audio subsystem clock controller for
    Qualcomm Technology Inc's SDM845 SoCs.
    
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

---
+++ include/dt-bindings/clock/qcom,lpass-sdm845.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_SDM_LPASS_SDM845_H
+#define _DT_BINDINGS_CLK_SDM_LPASS_SDM845_H
+
+#define GCC_LPASS_Q6_AXI_CLK				0
+#define GCC_LPASS_SWAY_CLK				1
+#define LPASS_AUDIO_WRAPPER_AON_CLK			2
+#define LPASS_Q6SS_AHBM_AON_CLK				3
+#define LPASS_Q6SS_AHBS_AON_CLK				4
+#define LPASS_QDSP6SS_XO_CLK				5
+#define LPASS_QDSP6SS_SLEEP_CLK				6
+#define LPASS_QDSP6SS_CORE_CLK				7
+
+#endif
