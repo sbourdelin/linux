08a94ca2bb3fa5455ba40daa5a8a343d6e5cac09
Sig Shen <sigshen@synology.com>
2018-06-15 18:51:08 +0800

    vfs: Support FALLOC_FL_NO_HIDE_STALE flag with fallocate
    FALLOC_FL_NO_HIDE_STALE flag must be set if user want to issue
    a discard request for block devices. But vfs_fallocate() will
    return with an error -EOPNOTSUPP indicating lack of support
    if this flag is set.
    
    fix it by allowing FALLOC_FL_NO_HIDE_STALE flag in vfs_fallocate
    
    Fixes: 25f4c41415e5 ("block: implement (some of) fallocate for block devices")
    Signed-off-by: Sig Shen <sigshen@synology.com>

--- include/linux/falloc.h
+++ include/linux/falloc.h
@@ -27,6 +27,7 @@ struct space_resv {
 					 FALLOC_FL_COLLAPSE_RANGE |	\
 					 FALLOC_FL_ZERO_RANGE |		\
 					 FALLOC_FL_INSERT_RANGE |	\
+					 FALLOC_FL_NO_HIDE_STALE |	\
 					 FALLOC_FL_UNSHARE_RANGE)
 
 #endif /* _FALLOC_H_ */
