9a12c19a44b2c732be8573123e3de15ffa0196af
Arnd Bergmann <arnd@arndb.de>
2018-06-19 18:02:08 +0200

    hfsplus: stop using timespec based interfaces
    The native HFS+ timestamps overflow in year 2040, two years after the Unix
    y2038 overflow. However, the way that the conversion between on-disk
    timestamps and in-kernel timestamps was implemented, 64-bit machines
    actually ended up converting negative UTC timestamps (1902 through 1969)
    into times between 2038 and 2106.
    
    Rather than making all machines faithfully represent timestamps in the
    ancient past but break after 2040, this changes the file system to
    always use the unsigned UTC interpretation, reading back times between
    1970 and 2106.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- fs/hfsplus/hfsplus_fs.h
+++ fs/hfsplus/hfsplus_fs.h
@@ -530,13 +530,14 @@ int hfsplus_submit_bio(struct super_block *sb, sector_t sector, void *buf,
 		       void **data, int op, int op_flags);
 int hfsplus_read_wrapper(struct super_block *sb);
 
-/* time macros */
+/* time macros: convert between 1904-2040 and 1970-2106 range,
+ * pre-1970 timestamps are interpreted as post-2038 times after wrap-around */
 #define __hfsp_mt2ut(t)		(be32_to_cpu(t) - 2082844800U)
 #define __hfsp_ut2mt(t)		(cpu_to_be32(t + 2082844800U))
 
 /* compatibility */
-#define hfsp_mt2ut(t)		(struct timespec){ .tv_sec = __hfsp_mt2ut(t) }
+#define hfsp_mt2ut(t)		(struct timespec64){ .tv_sec = __hfsp_mt2ut(t) }
 #define hfsp_ut2mt(t)		__hfsp_ut2mt((t).tv_sec)
-#define hfsp_now2mt()		__hfsp_ut2mt(get_seconds())
+#define hfsp_now2mt()		__hfsp_ut2mt(ktime_get_real_seconds())
 
 #endif
