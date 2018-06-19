71f840a490aca9a2d4e9609641a929c6936c20e2
Rafał Miłecki <rafal@milecki.pl>
2018-06-19 17:48:09 +0200

    brcmfmac: handle monitor mode marked msgbuf packets
    New Broadcom firmwares mark monitor mode packets using a newly defined
    bit in the flags field. Use it to filter them out and pass to the
    monitor interface. These defines were found in bcmmsgbuf.h from SDK.
    
    As not every firmware generates radiotap header this commit introduces
    BRCMF_FEAT_MONITOR_FMT_RADIOTAP flag. It has to be has based on firmware
    capabilities. If not present brcmf_netif_mon_rx() will assume packet is
    a raw 802.11 frame and will prepend it with an empty radiotap header.
    
    This new code is limited to the msgbuf protocol at this point. Adding
    support for SDIO/USB devices will require some extra work (possibly a
    new firmware release).
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.h
@@ -34,6 +34,7 @@
  * GSCAN: enhanced scan offload feature.
  * FWSUP: Firmware supplicant.
  * MONITOR: monitor interface
+ * MONITOR_FMT_RADIOTAP: monitor packets include radiotap header
  */
 #define BRCMF_FEAT_LIST \
 	BRCMF_FEAT_DEF(MBSS) \
@@ -50,7 +51,8 @@
 	BRCMF_FEAT_DEF(MFP) \
 	BRCMF_FEAT_DEF(GSCAN) \
 	BRCMF_FEAT_DEF(FWSUP) \
-	BRCMF_FEAT_DEF(MONITOR)
+	BRCMF_FEAT_DEF(MONITOR) \
+	BRCMF_FEAT_DEF(MONITOR_FMT_RADIOTAP)
 
 /*
  * Quirks:
