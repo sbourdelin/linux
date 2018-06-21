1b90e6c3e095bd3e010c0cd5cf481362e8480a23
Lorne Li <lorneli.cs@gmail.com>
2018-06-21 18:29:33 +0800

    vfs: get inode after passing previous check in rw_verify_area()
    Move file_inode function to the front of line actually using inode.
    
    Signed-off-by: Lorne Li <lorneli.cs@gmail.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -368,7 +368,6 @@ int rw_verify_area(int read_write, struct file *file, const loff_t *ppos, size_t
 	loff_t pos;
 	int retval = -EINVAL;
 
-	inode = file_inode(file);
 	if (unlikely((ssize_t) count < 0))
 		return retval;
 	pos = *ppos;
@@ -382,6 +381,7 @@ int rw_verify_area(int read_write, struct file *file, const loff_t *ppos, size_t
 			return retval;
 	}
 
+	inode = file_inode(file);
 	if (unlikely(inode->i_flctx && mandatory_lock(inode))) {
 		retval = locks_mandatory_area(inode, file, pos, pos + count - 1,
 				read_write == READ ? F_RDLCK : F_WRLCK);
