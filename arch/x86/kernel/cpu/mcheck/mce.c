ff331cfae235c2948f18723d8c6f0f4251940db2
Borislav Petkov <bp@suse.de>
2018-06-21 14:40:20 +0200

    x86/mce: Do not overwrite MCi_STATUS in mce_no_way_out()
    mce_no_way_out() does a quick check during #MC to see whether some of
    the MCEs logged would require the kernel to panic immediately. And it
    passes a struct mce where MCi_STATUS gets written.
    
    However, after having saved a valid status value, the next iteration
    of the loop which goes over the MCA banks on the CPU, overwrites the
    valid status value because we're using struct mce as storage instead of
    a temporary variable.
    
    Which leads to MCE records with an empty status value:
    
      mce: [Hardware Error]: CPU 0: Machine Check Exception: 6 Bank 0: 0000000000000000
      mce: [Hardware Error]: RIP 10:<ffffffffbd42fbd7> {trigger_mce+0x7/0x10}
    
    Change it to use a temporary variable and save to mce.status only when
    valid. It will still overwrite the old status value if more than one
    MCEs are logged, which warrants the second change in this patch:
    
    Return immediately when severity is a panic one so that we can panic
    immediately with the first fatal MCE logged.
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- arch/x86/kernel/cpu/mcheck/mce.c
+++ arch/x86/kernel/cpu/mcheck/mce.c
@@ -756,12 +756,15 @@ EXPORT_SYMBOL_GPL(machine_check_poll);
 static int mce_no_way_out(struct mce *m, char **msg, unsigned long *validp,
 			  struct pt_regs *regs)
 {
+	u64 mci_status;
 	int i, ret = 0;
 	char *tmp;
 
 	for (i = 0; i < mca_cfg.banks; i++) {
-		m->status = mce_rdmsrl(msr_ops.status(i));
-		if (m->status & MCI_STATUS_VAL) {
+		mci_status = mce_rdmsrl(msr_ops.status(i));
+		if (mci_status & MCI_STATUS_VAL) {
+			m->status = mci_status;
+
 			__set_bit(i, validp);
 			if (quirk_no_way_out)
 				quirk_no_way_out(i, m, regs);
@@ -769,7 +772,7 @@ static int mce_no_way_out(struct mce *m, char **msg, unsigned long *validp,
 
 		if (mce_severity(m, mca_cfg.tolerant, &tmp, true) >= MCE_PANIC_SEVERITY) {
 			*msg = tmp;
-			ret = 1;
+			return 1;
 		}
 	}
 	return ret;
