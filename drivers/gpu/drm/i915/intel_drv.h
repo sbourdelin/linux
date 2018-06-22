1da2636a6719d6fce6a307fde86b48074cc7565a
Mahesh Kumar <mahesh1.kumar@intel.com>
2018-06-22 16:11:32 +0530

    drm/i915/crc: implement get_crc_sources callback
    This patch implements get_crc_sources callback, which returns list of
    all the valid crc sources supported by driver in current platform.
    
    Signed-off-by: Mahesh Kumar <mahesh1.kumar@intel.com>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -2151,11 +2151,13 @@ int intel_crtc_set_crc_source(struct drm_crtc *crtc, const char *source_name,
 			      size_t *values_cnt);
 int intel_crtc_verify_crc_source(struct drm_crtc *crtc,
 				 const char *source_name, size_t *values_cnt);
+void intel_crtc_get_crc_sources(struct seq_file *m, struct drm_crtc *crtc);
 void intel_crtc_disable_pipe_crc(struct intel_crtc *crtc);
 void intel_crtc_enable_pipe_crc(struct intel_crtc *crtc);
 #else
 #define intel_crtc_set_crc_source NULL
 #define intel_crtc_verify_crc_source NULL
+#define intel_crtc_get_crc_sources NULL
 static inline void intel_crtc_disable_pipe_crc(struct intel_crtc *crtc)
 {
 }
