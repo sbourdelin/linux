ccf00a08b8c514e1150a3e850a30861653aef603
Colin Ian King <colin.king@canonical.com>
2018-06-23 23:42:08 +0100

    afs: make afs_volume_record_life static and remove afs_volume_gc_delay
    Variable afs_volume_record_life is local to the source and does not need
    to be in global scope, so make it static.  Variable afs_volume_gc_delay
    is not referenced at all in the source, it is unused and can be removed.
    
    Cleans up sparse warnings:
    symbol 'afs_volume_gc_delay' was not declared. Should it be static?
    symbol 'afs_volume_record_life' was not declared. Should it be static?
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- fs/afs/volume.c
+++ fs/afs/volume.c
@@ -13,8 +13,7 @@
 #include <linux/slab.h>
 #include "internal.h"
 
-unsigned __read_mostly afs_volume_gc_delay = 10;
-unsigned __read_mostly afs_volume_record_life = 60 * 60;
+static unsigned __read_mostly afs_volume_record_life = 60 * 60;
 
 static const char *const afs_voltypes[] = { "R/W", "R/O", "BAK" };
 
