f4a30de892f9ab6486683ff6c132a3782f18fc65
Sasha Neftin <sasha.neftin@intel.com>
2018-06-24 11:44:58 +0300

    igc: Add skeletal frame for Intel(R) 2.5G Ethernet Controller support.
    This patch adds the beginning framework onto which I am going to add
    the igc driver which supports the Intel(R) I225-LM/I225-V 2.5G
    Ethernet Controller.
    
    Sasha Neftin (v2):
    update module author, copyright and licence header
    cosmetic changes
    
    Sasha Neftin (v3):
    Removed unused header files
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>

---
+++ drivers/net/ethernet/intel/igc/e1000_hw.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c)  2018 Intel Corporation */
+
+#ifndef _E1000_HW_H_
+#define _E1000_HW_H_
+
+#define E1000_DEV_ID_I225_LM			0x15F2
+#define E1000_DEV_ID_I225_V			0x15F3
+
+#endif /* _E1000_HW_H_ */
