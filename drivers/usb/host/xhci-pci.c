2fe3d57336b9c045f66b0a525c61f16d3ec8d02f
Guy Chronister <guylovesbritt@gmail.com>
2018-06-27 11:04:27 -0500

    Fixed typo on USB device ID number.
    Signed-off-by: Guy Chronister <guylovesbritt@gmail.com>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -196,7 +196,7 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 		xhci->quirks |= XHCI_BROKEN_STREAMS;
 	}
 	if (pdev->vendor == PCI_VENDOR_ID_RENESAS &&
-	    pdev->device == 0x0014) {
+	    pdev->device == 0x0015) {
 		xhci->quirks |= XHCI_TRUST_TX_LENGTH;
 		xhci->quirks |= XHCI_ZERO_64B_REGS;
 	}
