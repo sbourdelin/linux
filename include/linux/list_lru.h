dd4eb5cca4db083951dffcea79ffb96e834ecf5c
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-07-03 18:08:56 +0300

    mm: Introduce CONFIG_MEMCG_KMEM as combination of CONFIG_MEMCG && !CONFIG_SLOB
    This patch introduces new config option, which is used
    to replace repeating CONFIG_MEMCG && !CONFIG_SLOB pattern.
    Next patches add a little more memcg+kmem related code,
    so let's keep the defines more clearly.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Vladimir Davydov <vdavydov.dev@gmail.com>
    Tested-by: Shakeel Butt <shakeelb@google.com>

--- include/linux/list_lru.h
+++ include/linux/list_lru.h
@@ -42,7 +42,7 @@ struct list_lru_node {
 	spinlock_t		lock;
 	/* global list, used for the root cgroup in cgroup aware lrus */
 	struct list_lru_one	lru;
-#if defined(CONFIG_MEMCG) && !defined(CONFIG_SLOB)
+#ifdef CONFIG_MEMCG_KMEM
 	/* for cgroup aware lrus points to per cgroup lists, otherwise NULL */
 	struct list_lru_memcg	__rcu *memcg_lrus;
 #endif
@@ -52,7 +52,7 @@ struct list_lru_node {
 struct list_lru {
 	struct list_lru_node	*node;
 	bool			lock_irq;
-#if defined(CONFIG_MEMCG) && !defined(CONFIG_SLOB)
+#ifdef CONFIG_MEMCG_KMEM
 	struct list_head	list;
 #endif
 };
