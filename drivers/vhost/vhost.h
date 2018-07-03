7b681e385b6f8d7b60ea17ea40140588f5f8e8d1
Jason Wang <jasowang@redhat.com>
2018-07-03 13:37:58 +0800

    vhost: hide used ring layout from device
    We used to return descriptor head by vhost_get_vq_desc() to device and
    pass it back to vhost_add_used() and its friends. This exposes the
    internal used ring layout to device which makes it hard to be extended for
    e.g packed ring layout.
    
    So this patch tries to hide the used ring layout by
    
    - letting vhost_get_vq_desc() return pointer to struct vring_used_elem
    - accepting pointer to struct vring_used_elem in vhost_add_used() and
      vhost_add_used_and_signal()
    
    This could help to hide used ring layout and make it easier to
    implement packed ring on top.
    
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -182,6 +182,7 @@ bool vhost_vq_access_ok(struct vhost_virtqueue *vq);
 bool vhost_log_access_ok(struct vhost_dev *);
 
 int vhost_get_vq_desc(struct vhost_virtqueue *,
+		      struct vring_used_elem *used_elem,
 		      struct iovec iov[], unsigned int iov_count,
 		      unsigned int *out_num, unsigned int *in_num,
 		      struct vhost_log *log, unsigned int *log_num);
@@ -191,15 +192,17 @@ int vhost_get_bufs(struct vhost_virtqueue *vq,
 		   unsigned *iovcount,
 		   struct vhost_log *log,
 		   unsigned *log_num,
-		   unsigned int quota);
+		   unsigned int quota,
+		   s16 *count);
 void vhost_discard_vq_desc(struct vhost_virtqueue *, int n);
 
 int vhost_vq_init_access(struct vhost_virtqueue *);
-int vhost_add_used(struct vhost_virtqueue *, unsigned int head, int len);
+int vhost_add_used(struct vhost_virtqueue *vq,
+		   struct vring_used_elem *elem, int len);
 int vhost_add_used_n(struct vhost_virtqueue *, struct vring_used_elem *heads,
 		     unsigned count);
 void vhost_add_used_and_signal(struct vhost_dev *, struct vhost_virtqueue *,
-			       unsigned int id, int len);
+			       struct vring_used_elem *, int len);
 void vhost_add_used_and_signal_n(struct vhost_dev *, struct vhost_virtqueue *,
 			       struct vring_used_elem *heads, unsigned count);
 void vhost_signal(struct vhost_dev *, struct vhost_virtqueue *);
