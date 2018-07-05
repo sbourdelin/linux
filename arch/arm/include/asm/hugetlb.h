a8f1e32e728152c6e8e692ce55ed7ee382efc1ee
Alexandre Ghiti <alex@ghiti.fr>
2018-07-05 11:07:07 +0000

    hugetlb: Introduce generic version of hugetlb_free_pgd_range
    arm, arm64, mips, parisc, sh, x86 architectures use the
    same version of hugetlb_free_pgd_range, so move this generic
    implementation into asm-generic/hugetlb.h.
    
    Signed-off-by: Alexandre Ghiti <alex@ghiti.fr>

--- arch/arm/include/asm/hugetlb.h
+++ arch/arm/include/asm/hugetlb.h
@@ -23,18 +23,8 @@
 #define _ASM_ARM_HUGETLB_H
 
 #include <asm/page.h>
-#include <asm-generic/hugetlb.h>
 
 #include <asm/hugetlb-3level.h>
-
-static inline void hugetlb_free_pgd_range(struct mmu_gather *tlb,
-					  unsigned long addr, unsigned long end,
-					  unsigned long floor,
-					  unsigned long ceiling)
-{
-	free_pgd_range(tlb, addr, end, floor, ceiling);
-}
-
 
 static inline int is_hugepage_only_range(struct mm_struct *mm,
 					 unsigned long addr, unsigned long len)
@@ -67,5 +57,7 @@ static inline void arch_clear_hugepage_flags(struct page *page)
 {
 	clear_bit(PG_dcache_clean, &page->flags);
 }
+
+#include <asm-generic/hugetlb.h>
 
 #endif /* _ASM_ARM_HUGETLB_H */
