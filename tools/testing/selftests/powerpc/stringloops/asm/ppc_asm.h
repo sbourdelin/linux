146a8e593384e9e938f1e846fb9df4ee6caaf6cf
Christophe Leroy <christophe.leroy@c-s.fr>
2018-07-05 08:29:04 +0000

    selftests/powerpc: update strlen() test to test the new assembly function for PPC32
    This patch adds a test for testing the new assembly strlen() for PPC32
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>

--- tools/testing/selftests/powerpc/stringloops/asm/ppc_asm.h
+++ tools/testing/selftests/powerpc/stringloops/asm/ppc_asm.h
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+
 #include <ppc-asm.h>
 
 #ifndef r1
@@ -6,3 +7,12 @@
 #endif
 
 #define _GLOBAL(A) FUNC_START(test_ ## A)
+
+#ifndef __powerpc64__
+#define SZL		4
+#define PPC_LLU		lwzu
+#define PPC_LCMPI	cmplwi
+#define PPC_ROTLI	rotlwi
+#define PPC_CNTLZL	cntlzw
+#define PPC_SRLI	srwi
+#endif
