c5a5778db30f6d182732d2edb3b0f6f39350bf58
Deepa Dinamani <deepa.kernel@gmail.com>
2018-07-06 22:42:44 -0700

    timex: prepare compat helpers for y2038 changes
    Move struct compat_timex to compat_time.h.
    This is in line with other compat time structures that are planned
    to be deprecated eventually.
    
    Also enable compat_get/put_timex helpers unconditionally.
    
    Signed-off-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Cc: linux-arch@vger.kernel.org

--- include/asm-generic/compat.h
+++ include/asm-generic/compat.h
@@ -1,3 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-/* This is an empty stub for 32-bit-only architectures */
+/* This is a stub for 32-bit-only architectures */
+
+#include <linux/types.h>
+
+typedef s32		compat_int_t;
+typedef s32		compat_long_t;
+typedef u32		compat_uint_t;
