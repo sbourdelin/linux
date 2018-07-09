96743403c66b83e73855b2e6e0d2242d868b8bc1
Yandong Zhao <yandong77520@gmail.com>
2018-07-09 16:45:35 +0800

    arm64: neon: Add preemption protection for kernel_neon_busy
    may_use_simd() can be called in any case and access kernel_neon_busy,
    for example: BUG_ON(!may_use_simd()).  This patch ensures that
    migration will not occur during program access to kernel_neon_busy.
    
    Signed-off-by: Yandong Zhao <yandong77520@gmail.com>

--- arch/arm64/include/asm/simd.h
+++ arch/arm64/include/asm/simd.h
@@ -29,20 +29,12 @@ DECLARE_PER_CPU(bool, kernel_neon_busy);
 static __must_check inline bool may_use_simd(void)
 {
 	/*
-	 * The raw_cpu_read() is racy if called with preemption enabled.
-	 * This is not a bug: kernel_neon_busy is only set when
-	 * preemption is disabled, so we cannot migrate to another CPU
-	 * while it is set, nor can we migrate to a CPU where it is set.
-	 * So, if we find it clear on some CPU then we're guaranteed to
-	 * find it clear on any CPU we could migrate to.
-	 *
-	 * If we are in between kernel_neon_begin()...kernel_neon_end(),
-	 * the flag will be set, but preemption is also disabled, so we
-	 * can't migrate to another CPU and spuriously see it become
-	 * false.
+	 * Operations for contexts where we do not want to do any checks for
+	 * preemptions.  Unless strictly necessary, always use this_cpu_*()
+	 * instead.
 	 */
 	return !in_irq() && !irqs_disabled() && !in_nmi() &&
-		!raw_cpu_read(kernel_neon_busy);
+		!this_cpu_read(kernel_neon_busy);
 }
 
 #else /* ! CONFIG_KERNEL_MODE_NEON */
