2f48c705df77c59daf2c38232239d62a2689733d
Gu Jinxiang <gujx@cn.fujitsu.com>
2018-07-10 18:27:20 +0800

    btrfs: make fs_devices to be a local variable
    fs_devices is always passed to btrfs_scan_one_device which
    overrides it. And in the call stack below fs_devices is passed to
    btrfs_scan_one_device from btrfs_mount_root.
    And in btrfs_mount_root the output fs_devices of this call stack
    is not used.
    btrfs_mount_root
    	-> btrfs_parse_early_options
    		->btrfs_scan_one_device
    So, there is no necessary to pass fs_devices from btrfs_mount_root,
    use a local variable in btrfs_parse_early_options is enough.
    
    Signed-off-by: Gu Jinxiang <gujx@cn.fujitsu.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -886,11 +886,12 @@ out:
  * only when we need to allocate a new super block.
  */
 static int btrfs_parse_early_options(const char *options, fmode_t flags,
-		void *holder, struct btrfs_fs_devices **fs_devices)
+		void *holder)
 {
 	substring_t args[MAX_OPT_ARGS];
 	char *device_name, *opts, *orig, *p;
 	int error = 0;
+	struct btrfs_fs_devices *fs_devices = NULL;
 
 	if (!options)
 		return 0;
@@ -918,7 +919,7 @@ static int btrfs_parse_early_options(const char *options, fmode_t flags,
 				goto out;
 			}
 			error = btrfs_scan_one_device(device_name,
-					flags, holder, fs_devices);
+					flags, holder, &fs_devices);
 			kfree(device_name);
 			if (error)
 				goto out;
@@ -1526,8 +1527,7 @@ static struct dentry *btrfs_mount_root(struct file_system_type *fs_type,
 	if (!(flags & SB_RDONLY))
 		mode |= FMODE_WRITE;
 
-	error = btrfs_parse_early_options(data, mode, fs_type,
-					  &fs_devices);
+	error = btrfs_parse_early_options(data, mode, fs_type);
 	if (error) {
 		return ERR_PTR(error);
 	}
