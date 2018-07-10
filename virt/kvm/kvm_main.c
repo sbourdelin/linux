0b6fc4b14375c87d72c014cdee4bc1abd766e251
Zhang Yi <yi.z.zhang@linux.intel.com>
2018-07-11 01:01:33 +0800

    kvm: remove redundant reserved page check
    PageReserved() is already checked inside kvm_is_reserved_pfn(),
    remove it from kvm_set_pfn_dirty().
    
    Signed-off-by: Zhang Yi <yi.z.zhang@linux.intel.com>
    Signed-off-by: Zhang Yu <yu.c.zhang@linux.intel.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1690,12 +1690,8 @@ EXPORT_SYMBOL_GPL(kvm_release_pfn_dirty);
 
 void kvm_set_pfn_dirty(kvm_pfn_t pfn)
 {
-	if (!kvm_is_reserved_pfn(pfn)) {
-		struct page *page = pfn_to_page(pfn);
-
-		if (!PageReserved(page))
-			SetPageDirty(page);
-	}
+	if (!kvm_is_reserved_pfn(pfn))
+		SetPageDirty(pfn_to_page(pfn));
 }
 EXPORT_SYMBOL_GPL(kvm_set_pfn_dirty);
 
