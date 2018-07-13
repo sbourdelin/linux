fc81e412ca48950b9caa1cac1b926ca709e3163d
Patrick Steinhardt <ps@pks.im>
2018-07-13 15:29:57 +0200

    block: fix NPE when resuming SCSI devices using blk-mq
    When power management for SCSI is enabled and if a device uses blk-mq,
    it is possible to trigger a `NULL` pointer exception when resuming that
    device. The NPE is triggered when trying to dereference the `request_fn`
    function pointer of the device's `request_queue`:
    
        __blk_run_queue_uncond:470
        __blk_run_queue:490
        blk_post_runtime_resume:3889
        sdev_runtime_resume:263
        scsi_runtime_resume:275
    
    When the SCSI device is being allocated by `scsi_alloc_sdev`, the
    device's request queue will either be initialized via
    `scsi_mq_alloc_queue` or `scsi_old_alloc_queue`. But the `request_fn`
    member of the request queue is in fact only being set in
    `scsi_old_alloc_queue`, which will then later cause the mentioned NPE.
    
    Fix the issue by checking whether the `request_fn` is set in
    `__blk_run_queue_uncond`. In case it is unset, we'll silently return and
    not try to invoke the callback, thus fixing the NPE.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>

--- block/blk-core.c
+++ block/blk-core.c
@@ -456,7 +456,7 @@ inline void __blk_run_queue_uncond(struct request_queue *q)
 	lockdep_assert_held(q->queue_lock);
 	WARN_ON_ONCE(q->mq_ops);
 
-	if (unlikely(blk_queue_dead(q)))
+	if (unlikely(!q->request_fn) || unlikely(blk_queue_dead(q)))
 		return;
 
 	/*
