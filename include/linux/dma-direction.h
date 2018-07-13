3a9a2dbba7fe68aa0264324befa29605d83cdc53
Shunyong Yang <shunyong.yang@hxt-semitech.com>
2018-07-13 14:37:20 +0800

    PCI: Unify pci and normal dma direction definition
    Current DMA direction definitions in pci-dma-compat.h and dma-direction.h
    are mirrored in value. Unify them to enhance readability and avoid
    possible inconsistency.
    
    Cc: Joey Zheng <yu.zheng@hxt-semitech.com>
    Signed-off-by: Shunyong Yang <shunyong.yang@hxt-semitech.com>

--- include/linux/dma-direction.h
+++ include/linux/dma-direction.h
@@ -2,7 +2,7 @@
 #ifndef _LINUX_DMA_DIRECTION_H
 #define _LINUX_DMA_DIRECTION_H
 /*
- * These definitions mirror those in pci.h, so they can be used
+ * These definitions mirror those in pci-dma-compat.h, so they can be used
  * interchangeably with their PCI_ counterparts.
  */
 enum dma_data_direction {
