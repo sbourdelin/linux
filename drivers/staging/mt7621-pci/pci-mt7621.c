3a5b990463b977f5ec644ca09af9b48ce18ef81d
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2018-07-16 17:53:17 +0200

    staging: mt7621-pci: remove unused includes
    Some includes are not needed at all. Remove them.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>

--- drivers/staging/mt7621-pci/pci-mt7621.c
+++ drivers/staging/mt7621-pci/pci-mt7621.c
@@ -46,8 +46,6 @@
 #include <linux/slab.h>
 #include <linux/version.h>
 #include <linux/pci.h>
-#include <linux/io.h>
-#include <asm/mips-cm.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/delay.h>
