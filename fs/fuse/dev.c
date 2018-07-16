0fa3faaffe23c168678a9390c8d93d407704cf0b
Andrey Ryabinin <aryabinin@virtuozzo.com>
2018-07-16 19:03:43 +0300

    fs/fuse, splice: use kvmalloc to allocate array of pipe_buffer structs.
    The amount of pipe->buffers is basically controlled by userspace by
    fcntl(... F_SETPIPE_SZ ...) so it could be large. High order allocations
    could be slow (if memory is heavily fragmented) or may fail if the order
    is larger than PAGE_ALLOC_COSTLY_ORDER.
    
    Since the 'bufs' doesn't need to be physically contiguous, use
    the kvmalloc_array() to allocate memory. If high order
    page isn't available, the kvamalloc*() will fallback to 0-order.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -1362,8 +1362,8 @@ static ssize_t fuse_dev_splice_read(struct file *in, loff_t *ppos,
 	if (!fud)
 		return -EPERM;
 
-	bufs = kmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
-			     GFP_KERNEL);
+	bufs = kvmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
+			      GFP_KERNEL);
 	if (!bufs)
 		return -ENOMEM;
 
@@ -1396,7 +1396,7 @@ out:
 	for (; page_nr < cs.nr_segs; page_nr++)
 		put_page(bufs[page_nr].page);
 
-	kfree(bufs);
+	kvfree(bufs);
 	return ret;
 }
 
@@ -1944,8 +1944,8 @@ static ssize_t fuse_dev_splice_write(struct pipe_inode_info *pipe,
 	if (!fud)
 		return -EPERM;
 
-	bufs = kmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
-			     GFP_KERNEL);
+	bufs = kvmalloc_array(pipe->buffers, sizeof(struct pipe_buffer),
+			      GFP_KERNEL);
 	if (!bufs)
 		return -ENOMEM;
 
@@ -2003,7 +2003,7 @@ static ssize_t fuse_dev_splice_write(struct pipe_inode_info *pipe,
 		pipe_buf_release(pipe, &bufs[idx]);
 
 out:
-	kfree(bufs);
+	kvfree(bufs);
 	return ret;
 }
 
