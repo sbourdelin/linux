6bed6b5537e36bf36b702e7561aa16b7ac1fe42c
James Morse <james.morse@arm.com>
2018-07-19 16:24:25 +0100

    KVM: arm: Add 32bit get/set events support
    arm64's new use of KVMs get_events/set_events API calls isn't just
    or RAS, it allows an SError that has been made pending by KVM as
    part of its device emulation to be migrated.
    
    Wire this up for 32bit too.
    
    We only need to read/write the HCR_VA bit, and check that no esr has
    been provided, as we don't yet support VDFSR.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Reviewed-by: Dongjiu Geng <gengdongjiu@huawei.com>

--- virt/kvm/arm/arm.c
+++ virt/kvm/arm/arm.c
@@ -1044,7 +1044,6 @@ static int kvm_arm_vcpu_has_attr(struct kvm_vcpu *vcpu,
 	return ret;
 }
 
-#ifdef __KVM_HAVE_VCPU_EVENTS	/* temporary: until 32bit is wired up */
 static int kvm_arm_vcpu_get_events(struct kvm_vcpu *vcpu,
 				   struct kvm_vcpu_events *events)
 {
@@ -1070,7 +1069,6 @@ static int kvm_arm_vcpu_set_events(struct kvm_vcpu *vcpu,
 
 	return __kvm_arm_vcpu_set_events(vcpu, events);
 }
-#endif /* __KVM_HAVE_VCPU_EVENTS */
 
 long kvm_arch_vcpu_ioctl(struct file *filp,
 			 unsigned int ioctl, unsigned long arg)
@@ -1152,7 +1150,6 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 		r = kvm_arm_vcpu_has_attr(vcpu, &attr);
 		break;
 	}
-#ifdef __KVM_HAVE_VCPU_EVENTS
 	case KVM_GET_VCPU_EVENTS: {
 		struct kvm_vcpu_events events;
 
@@ -1172,7 +1169,6 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 
 		return kvm_arm_vcpu_set_events(vcpu, &events);
 	}
-#endif
 	default:
 		r = -EINVAL;
 	}
