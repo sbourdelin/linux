66b979ee79a5cabb385c751df67944b9f42ca62e
Nick Desaulniers <ndesaulniers@google.com>
2018-07-30 10:06:20 -0700

    kernel.h: Disable -Wreturn-stack-address for _THIS_IP_
    Starting with Clang-7.0, _THIS_IP_ generates -Wreturn-stack-address
    warnings for almost every translation unit. In general, I'd prefer to
    leave this on (returning the address of a stack allocated variable is in
    general a bad idea) and disable it only at whitelisted call sites.
    
    We can't do something like:
     #pragma clang diagnostic push
     #pragma clang diagnostic ignored "-Wreturn-stack-address"
     <code>
     #pragma clang diagnostic pop
    
    in a GNU Statement Expression or macro, hence we use _Pragma, which is
    its raison d'être: https://gcc.gnu.org/onlinedocs/cpp/Pragmas.html
    
    Cc: stable@vger.kernel.org # 4.17, 4.14, 4.9, 4.4
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -168,7 +168,15 @@
 
 
 #define _RET_IP_		(unsigned long)__builtin_return_address(0)
-#define _THIS_IP_  ({ __label__ __here; __here: (unsigned long)&&__here; })
+#define _THIS_IP_  (							\
+{									\
+	_Pragma("clang diagnostic push")				\
+	_Pragma("clang diagnostic ignored \"-Wreturn-stack-address\"")	\
+	__label__ __here;						\
+__here: (unsigned long)&&__here;					\
+	_Pragma("clang diagnostic pop")					\
+}									\
+)
 
 #ifdef CONFIG_LBDAF
 # include <asm/div64.h>
