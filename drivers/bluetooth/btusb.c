812bddb86d3fc5ae26c26cf6ae2e207222df770a
Amit K Bag <amit.k.bag@intel.com>
2018-08-01 11:53:42 +0530

    Bluetooth: btusb: Add HCI reset command on BT shutdown
    In case of BT turn off from UI it is safe to add HCI reset command
    which will maintain the controller state.
    
    Signed-off-by: Amit K Bag <amit.k.bag@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -2351,6 +2351,18 @@ static int btusb_shutdown_intel(struct hci_dev *hdev)
 	struct sk_buff *skb;
 	long ret;
 
+	/* In case of BT off from UI it is safe to do HCI reset.
+	 * This will maintain the controller state.
+	 */
+	skb = __hci_cmd_sync(hdev, HCI_OP_RESET, 0, NULL, HCI_INIT_TIMEOUT);
+        if (IS_ERR(skb)) {
+		ret = PTR_ERR(skb);
+		BT_ERR("%s: BT controller  HCI reset failed (%ld)",
+		       hdev->name, ret);
+		return ret;
+	}
+	kfree_skb(skb);
+
 	/* Some platforms have an issue with BT LED when the interface is
 	 * down or BT radio is turned off, which takes 5 seconds to BT LED
 	 * goes off. This command turns off the BT LED immediately.
