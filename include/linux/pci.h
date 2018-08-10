4b835578cc3d59f369c28c0bad84709d3b08de80
Bharat Kumar Gogada <bharat.kumar.gogada@xilinx.com>
2018-08-10 21:09:37 +0530

    PCI: Add setup_platform_service_irq hook to struct pci_host_bridge
    Add setup_platform_service_irq hook to struct pci_host_bridge.
    Some platforms have dedicated interrupt line from root complex to
    interrupt controller for PCIe services like AER/PME etc.
    This hook is to register platform IRQ's to PCIe port services.
    
    Signed-off-by: Bharat Kumar Gogada <bharat.kumar.gogada@xilinx.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -472,6 +472,7 @@ struct pci_host_bridge {
 	u8 (*swizzle_irq)(struct pci_dev *, u8 *); /* Platform IRQ swizzler */
 	int (*map_irq)(const struct pci_dev *, u8, u8);
 	void (*release_fn)(struct pci_host_bridge *);
+	int (*setup_platform_service_irq)(struct pci_host_bridge *, int *, int);
 	void		*release_data;
 	struct msi_controller *msi;
 	unsigned int	ignore_reset_delay:1;	/* For entire hierarchy */
