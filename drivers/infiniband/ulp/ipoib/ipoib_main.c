989e40670d337d9095e1b13c339c92b1a88d28df
Arseny Maslennikov <ar@cs.msu.ru>
2018-08-13 14:42:23 +0300

    IB/ipoib: Stop using dev_id to expose port numbers
    Some InfiniBand network devices have multiple ports on the same PCI
    function. Prior to this the kernel erroneously used the `dev_id' sysfs
    field of those network interfaces to convey the port number to userspace.
    
    `dev_id' is currently reserved for distinguishing stacked ifaces
    (e.g: VLANs) with the same hardware address as their parent device.
    
    Similar fixes to net/mlx4_en and many other drivers, which started
    exporting this information through `dev_id' before 3.15, were accepted
    into the kernel 4 years ago.
    See 76a066f2a2a0268b565459c417b59724b5a3197b, commit message:
    `net/mlx4_en: Expose port number through sysfs'.
    
    I would be OK with this commit not being backported to stable, since
    it might break admin-supplied udev rules and the likes.
    
    Signed-off-by: Arseny Maslennikov <ar@cs.msu.ru>

--- drivers/infiniband/ulp/ipoib/ipoib_main.c
+++ drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -2252,7 +2252,6 @@ static struct net_device *ipoib_add_port(const char *format,
 	}
 
 	SET_NETDEV_DEV(priv->dev, hca->dev.parent);
-	priv->dev->dev_id = port - 1;
 	priv->dev->dev_port = port - 1;
 
 	result = ib_query_port(hca, port, &attr);
