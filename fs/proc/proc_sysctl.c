2688e4f3b5c00315c1fafa4fc62cd4006cfe3dc7
Srikanth K H <srikanth.h@samsung.com>
2018-08-16 15:04:13 +0530

    Preventive patch in the proc file-system to handle NULL check.
    If the make directory for "sys" interface fail's then its
    dereferenced without even checking for its validity which
    will lead to crash, hence added preventive code to check
    for NULL and accordingly dereference.
    
    Signed-off-by: Srikanth K H <srikanth.h@samsung.com>

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -1692,6 +1692,8 @@ int __init proc_sys_init(void)
 	struct proc_dir_entry *proc_sys_root;
 
 	proc_sys_root = proc_mkdir("sys", NULL);
+	if (!proc_sys_root)
+		return -ENOMEM;
 	proc_sys_root->proc_iops = &proc_sys_dir_operations;
 	proc_sys_root->proc_fops = &proc_sys_dir_file_operations;
 	proc_sys_root->nlink = 0;
