a76a26d39732882b2f6540f8cf82a9e78acf672a
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2018-08-18 15:24:29 +0200

    seq_file: use seq_open_data in __seq_open_private
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -620,18 +620,15 @@ void *__seq_open_private(struct file *f, const struct seq_operations *ops,
 {
 	int rc;
 	void *private;
-	struct seq_file *seq;
 
 	private = kzalloc(psize, GFP_KERNEL_ACCOUNT);
 	if (private == NULL)
 		goto out;
 
-	rc = seq_open(f, ops);
+	rc = seq_open_data(f, ops, private);
 	if (rc < 0)
 		goto out_free;
 
-	seq = f->private_data;
-	seq->private = private;
 	return private;
 
 out_free:
