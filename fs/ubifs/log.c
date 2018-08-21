7f32b29b1c53491fa5559b82a72fa861358cba98
Liu Song <liu.song11@zte.com.cn>
2018-08-21 11:17:22 +0800

    ubifs: remove unnecessary check in ubifs_log_start_commit
    The value of c->lhead_offs cannot exceed max_len which much
    smaller than c->leb_size. So the check will never be true.
    Just remove it.
    
    Signed-off-by: Liu Song <liu.song11@zte.com.cn>
    Reviewed-by: Jiang Biao <jiang.biao2@zte.com.cn>

--- fs/ubifs/log.c
+++ fs/ubifs/log.c
@@ -427,10 +427,6 @@ int ubifs_log_start_commit(struct ubifs_info *c, int *ltail_lnum)
 	*ltail_lnum = c->lhead_lnum;
 
 	c->lhead_offs += len;
-	if (c->lhead_offs == c->leb_size) {
-		c->lhead_lnum = ubifs_next_log_lnum(c, c->lhead_lnum);
-		c->lhead_offs = 0;
-	}
 
 	remove_buds(c);
 
