0f49aa150313c932db9e6024846f1b1d92da9b28
Robert Walker <robert.walker@arm.com>
2018-08-22 17:03:57 +0100

    perf: Support for Arm A32/T32 instruction sets in CoreSight trace
    This patch adds support for generating instruction samples from trace of
    AArch32 programs using the A32 and T32 instruction sets.
    
    T32 has variable 2 or 4 byte instruction size, so the conversion between
    addresses and instruction counts requires extra information from the trace
    decoder, requiring version 0.9.1 of OpenCSD.  A check for the new struct
    member has been added to the feature check for OpenCSD.
    
    Signed-off-by: Robert Walker <robert.walker@arm.com>

--- tools/build/feature/test-libopencsd.c
+++ tools/build/feature/test-libopencsd.c
@@ -3,6 +3,13 @@
 
 int main(void)
 {
+	/*
+	 * Requires ocsd_generic_trace_elem.num_instr_range introduced in
+	 * OpenCSD 0.9
+	 */
+	ocsd_generic_trace_elem elem;
+	(void)elem.num_instr_range;
+
 	(void)ocsd_get_version();
 	return 0;
 }
