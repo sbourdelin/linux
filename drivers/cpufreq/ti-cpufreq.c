b51a253287adc295693d369371296a62d7694bef
Dave Gerlach <d-gerlach@ti.com>
2018-08-23 14:20:35 -0500

    cpufreq: ti-cpufreq: Only register platform_device when supported
    Currently the ti-cpufreq driver blindly registers a 'ti-cpufreq' to force
    the driver to probe on any platforms where the driver is built in.
    However, this should only happen on platforms that actually can make use
    of the driver. There is already functionality in place to match the
    SoC compatible so let's factor this out into a separate call and
    make sure we find a match before creating the ti-cpufreq platform device.
    
    Reviewed-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

--- drivers/cpufreq/ti-cpufreq.c
+++ drivers/cpufreq/ti-cpufreq.c
@@ -201,19 +201,28 @@ static const struct of_device_id ti_cpufreq_of_match[] = {
 	{},
 };
 
+static const struct of_device_id *ti_cpufreq_match_node(void)
+{
+	struct device_node *np;
+	const struct of_device_id *match;
+
+	np = of_find_node_by_path("/");
+	match = of_match_node(ti_cpufreq_of_match, np);
+	of_node_put(np);
+
+	return match;
+}
+
 static int ti_cpufreq_probe(struct platform_device *pdev)
 {
 	u32 version[VERSION_COUNT];
-	struct device_node *np;
 	const struct of_device_id *match;
 	struct opp_table *ti_opp_table;
 	struct ti_cpufreq_data *opp_data;
 	const char * const reg_names[] = {"vdd", "vbb"};
 	int ret;
 
-	np = of_find_node_by_path("/");
-	match = of_match_node(ti_cpufreq_of_match, np);
-	of_node_put(np);
+	match = ti_cpufreq_match_node();
 	if (!match)
 		return -ENODEV;
 
@@ -290,7 +299,10 @@ fail_put_node:
 
 static int ti_cpufreq_init(void)
 {
-	platform_device_register_simple("ti-cpufreq", -1, NULL, 0);
+	/* Check to ensure we are on a compatible platform */
+	if (ti_cpufreq_match_node())
+		platform_device_register_simple("ti-cpufreq", -1, NULL, 0);
+
 	return 0;
 }
 module_init(ti_cpufreq_init);
