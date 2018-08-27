143d701c5f7642ffbb9fa90340d79ffdb82c6ae9
Alexey Dobriyan <adobriyan@gmail.com>
2018-08-28 02:14:51 +0300

    seq_file: rewrite seq_puts() in terms of seq_write()
    Space savings -- 42 bytes!
    
    	seq_puts	71      29     [-42]
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -653,14 +653,7 @@ EXPORT_SYMBOL(seq_putc);
 
 void seq_puts(struct seq_file *m, const char *s)
 {
-	int len = strlen(s);
-
-	if (m->count + len >= m->size) {
-		seq_set_overflow(m);
-		return;
-	}
-	memcpy(m->buf + m->count, s, len);
-	m->count += len;
+	seq_write(m, s, strlen(s));
 }
 EXPORT_SYMBOL(seq_puts);
 
