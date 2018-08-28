d3a44aab64704d223ea5c9fc0d3723c765ea86b2
vsrini4 <vidya.srinivas@intel.com>
2018-08-29 04:41:49 +0530

    drm/i915/icl: GEN11 supports 6 sprite planes
    GEN11 supports 6 sprite planes. Making the change
    in intel_device_info.c accordingly
    
    Signed-off-by: vsrini4 <vidya.srinivas@intel.com>

--- drivers/gpu/drm/i915/intel_device_info.c
+++ drivers/gpu/drm/i915/intel_device_info.c
@@ -761,7 +761,10 @@ void intel_device_info_runtime_init(struct intel_device_info *info)
 	 * we don't expose the topmost plane at all to prevent ABI breakage
 	 * down the line.
 	 */
-	if (IS_GEN10(dev_priv) || IS_GEMINILAKE(dev_priv))
+	if (IS_GEN11(dev_priv))
+		for_each_pipe(dev_priv, pipe)
+			info->num_sprites[pipe] = 6;
+	else if (IS_GEN10(dev_priv) || IS_GEMINILAKE(dev_priv))
 		for_each_pipe(dev_priv, pipe)
 			info->num_sprites[pipe] = 3;
 	else if (IS_BROXTON(dev_priv)) {
