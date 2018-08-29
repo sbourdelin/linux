46a929c341a7c539761823fc9454fa33649d997f
Fan Wu <wufan@codeaurora.org>
2018-08-29 18:33:52 +0000

    EDAC, ghes: use CPER module handles to locate DIMMs
    The current ghes_edac driver does not update per-dimm error
    counters when reporting memory errors, because there is no
    platform-independent way to find DIMMs based on the error
    information provided by firmware. This patch offers a solution
    for platforms whose firmwares provide valid module handles
    (SMBIOS type 17) in error records. In this case ghes_edac will
    use the module handles to locate DIMMs and thus makes per-dimm
    error reporting possible.
    
    Signed-off-by: Fan Wu <wufan@codeaurora.org>

--- drivers/edac/ghes_edac.c
+++ drivers/edac/ghes_edac.c
@@ -74,6 +74,26 @@ static void ghes_edac_count_dimms(const struct dmi_header *dh, void *arg)
 		(*num_dimm)++;
 }
 
+static int ghes_edac_dimm_index(u16 handle)
+{
+	struct mem_ctl_info *mci;
+	int i;
+
+	if (!ghes_pvt)
+		return -1;
+
+	mci = ghes_pvt->mci;
+
+	if (!mci)
+		return -1;
+
+	for (i = 0; i < mci->tot_dimms; i++) {
+		if (mci->dimms[i]->smbios_handle == handle)
+			return i;
+	}
+	return -1;
+}
+
 static void ghes_edac_dmidecode(const struct dmi_header *dh, void *arg)
 {
 	struct ghes_edac_dimm_fill *dimm_fill = arg;
@@ -160,6 +180,8 @@ static void ghes_edac_dmidecode(const struct dmi_header *dh, void *arg)
 				entry->memory_type, entry->type_detail,
 				entry->total_width, entry->data_width);
 		}
+
+		dimm->smbios_handle = entry->handle;
 
 		dimm_fill->count++;
 	}
@@ -306,12 +328,20 @@ void ghes_edac_report_mem_error(struct ghes *ghes, int sev,
 		p += sprintf(p, "bit_pos:%d ", mem_err->bit_pos);
 	if (mem_err->validation_bits & CPER_MEM_VALID_MODULE_HANDLE) {
 		const char *bank = NULL, *device = NULL;
+		int index = -1;
+
 		dmi_memdev_name(mem_err->mem_dev_handle, &bank, &device);
+		p += sprintf(p, "DIMM DMI handle: 0x%.4x ",
+			     mem_err->mem_dev_handle);
 		if (bank != NULL && device != NULL)
 			p += sprintf(p, "DIMM location:%s %s ", bank, device);
-		else
-			p += sprintf(p, "DIMM DMI handle: 0x%.4x ",
-				     mem_err->mem_dev_handle);
+
+		index = ghes_edac_dimm_index(mem_err->mem_dev_handle);
+		if (index >= 0) {
+			e->top_layer = index;
+			e->enable_per_layer_report = true;
+		}
+
 	}
 	if (p > e->location)
 		*(p - 1) = '\0';
