2bf54433529cd2d99021bde5f58ba2120aff45db
Chengguang Xu <cgxu519@gmx.com>
2018-08-29 22:04:04 +0800

    virtio_scsi: remove unnecessary condition check
    kmem_cache_destroy()/mempool_destroy() can handle NULL pointer
    correctly, so there is no need to check NULL pointer before calling
    kmem_cache_destroy()/mempool_destroy(). Meanwhile, because this
    is in the error case of init function, also remove unnecessary
    NULL assignment.
    
    Signed-off-by: Chengguang Xu <cgxu519@gmx.com>

--- drivers/scsi/virtio_scsi.c
+++ drivers/scsi/virtio_scsi.c
@@ -1012,14 +1012,8 @@ static int __init init(void)
 	return 0;
 
 error:
-	if (virtscsi_cmd_pool) {
-		mempool_destroy(virtscsi_cmd_pool);
-		virtscsi_cmd_pool = NULL;
-	}
-	if (virtscsi_cmd_cache) {
-		kmem_cache_destroy(virtscsi_cmd_cache);
-		virtscsi_cmd_cache = NULL;
-	}
+	mempool_destroy(virtscsi_cmd_pool);
+	kmem_cache_destroy(virtscsi_cmd_cache);
 	return ret;
 }
 
