effc30a4f88c6a183a6cdaced58fcc509b0fc92e
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2018-08-30 23:38:43 +0200

    mmc: renesas_sdhi_internal_dmac: set scatter/gather max segment size
    Fix warning when running with CONFIG_DMA_API_DEBUG_SG=y by allocating a
    device_dma_parameters structure and filling in the max segment size. The
    size used is the result of a discussion with Renesas hardware engineers
    and unfortunately not found in the datasheet.
    
      renesas_sdhi_internal_dmac ee140000.sd: DMA-API: mapping sg segment
      longer than device claims to support [len=126976] [max=65536A]
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reported-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/mmc/host/renesas_sdhi_internal_dmac.c
+++ drivers/mmc/host/renesas_sdhi_internal_dmac.c
@@ -308,11 +308,22 @@ static const struct soc_device_attribute gen3_soc_whitelist[] = {
 static int renesas_sdhi_internal_dmac_probe(struct platform_device *pdev)
 {
 	const struct soc_device_attribute *soc = soc_device_match(gen3_soc_whitelist);
+	struct device *dev = &pdev->dev;
 
 	if (!soc)
 		return -ENODEV;
 
 	global_flags |= (unsigned long)soc->data;
+
+	if (!dev->dma_parms) {
+		dev->dma_parms = devm_kzalloc(dev, sizeof(*dev->dma_parms),
+					      GFP_KERNEL);
+		if (!dev->dma_parms)
+			return -ENOMEM;
+	}
+
+	if (dma_set_max_seg_size(dev, 0xffffffff))
+		dev_warn(dev, "Unable to set seg size\n");
 
 	return renesas_sdhi_probe(pdev, &renesas_sdhi_internal_dmac_dma_ops);
 }
