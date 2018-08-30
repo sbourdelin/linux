a0e8959e9c69173979f970124d946a660fa351c9
Lu Baolu <baolu.lu@linux.intel.com>
2018-08-30 09:35:23 +0800

    iommu/vt-d: Shared virtual address in scalable mode
    This patch enables the current SVA (Shared Virtual Address)
    implementation to work in the scalable mode.
    
    Cc: Ashok Raj <ashok.raj@intel.com>
    Cc: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Cc: Kevin Tian <kevin.tian@intel.com>
    Cc: Liu Yi L <yi.l.liu@intel.com>
    Signed-off-by: Sanjay Kumar <sanjay.k.kumar@intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Ashok Raj <ashok.raj@intel.com>

--- drivers/iommu/intel-pasid.h
+++ drivers/iommu/intel-pasid.h
@@ -50,7 +50,6 @@ void intel_pasid_free_table(struct device *dev);
 struct pasid_table *intel_pasid_get_table(struct device *dev);
 int intel_pasid_get_dev_max_id(struct device *dev);
 struct pasid_entry *intel_pasid_get_entry(struct device *dev, int pasid);
-void intel_pasid_clear_entry(struct device *dev, int pasid);
 int intel_pasid_setup_first_level(struct intel_iommu *iommu,
 				  struct mm_struct *mm,
 				  struct device *dev,
