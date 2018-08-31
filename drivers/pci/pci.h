6801db2625b06289fc15b8feac7f0e5a489f5225
Keith Busch <keith.busch@intel.com>
2018-08-31 15:26:27 -0600

    PCI/ERR: Use slot reset if available
    The secondary bus reset may have link side effects that a hot plug
    capable port may incorrectly react to. This patch will use the slot
    specific reset for hotplug ports, fixing the undesirable link down-up
    handling during error recovering.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -35,6 +35,7 @@ int pci_mmap_fits(struct pci_dev *pdev, int resno, struct vm_area_struct *vmai,
 
 int pci_probe_reset_function(struct pci_dev *dev);
 int pci_bridge_secondary_bus_reset(struct pci_dev *dev);
+int pci_bus_error_reset(struct pci_dev *dev);
 
 /**
  * struct pci_platform_pm_ops - Firmware PM callbacks
