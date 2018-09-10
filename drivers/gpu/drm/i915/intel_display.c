59257b5d0f19fc90058f779741daab97422c3728
P Raviraj Sitaram <raviraj.p.sitaram@intel.com>
2018-09-10 18:31:22 +0530

    drm/i915/chv: Update csc coefficient matrix during modeset
    During modeset, previously configured csc coefficient matrix,if any, will
    not persist. This can result in blank screen as csc mode will be programmed
    while loading LUT but csc coefficient matrix remains unprogrammed.
    
    Signed-off-by: P Raviraj Sitaram <raviraj.p.sitaram@intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -6005,6 +6005,9 @@ static void valleyview_crtc_enable(struct intel_crtc_state *pipe_config,
 
 	i9xx_set_pipeconf(intel_crtc);
 
+	if (IS_CHERRYVIEW(dev_priv))
+		intel_color_set_csc(&pipe_config->base);
+
 	intel_crtc->active = true;
 
 	intel_set_cpu_fifo_underrun_reporting(dev_priv, pipe, true);
