0e8c8b3c26acd80230ffa8eadce57584bbb73033
YueHaibing <yuehaibing@huawei.com>
2018-09-11 02:15:37 +0000

    drm/omap: dss: Use PTR_ERR_OR_ZERO in hdmi_audio_register()
    Use PTR_ERR_OR_ZERO rather than if(IS_ERR(...)) + PTR_ERR
    
    Generated by: scripts/coccinelle/api/ptr_ret.cocci
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>

--- drivers/gpu/drm/omapdrm/dss/hdmi4.c
+++ drivers/gpu/drm/omapdrm/dss/hdmi4.c
@@ -617,10 +617,7 @@ static int hdmi_audio_register(struct omap_hdmi *hdmi)
 		&hdmi->pdev->dev, "omap-hdmi-audio", PLATFORM_DEVID_AUTO,
 		&pdata, sizeof(pdata));
 
-	if (IS_ERR(hdmi->audio_pdev))
-		return PTR_ERR(hdmi->audio_pdev);
-
-	return 0;
+	return PTR_ERR_OR_ZERO(hdmi->audio_pdev);
 }
 
 /* -----------------------------------------------------------------------------
