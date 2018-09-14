d6fd1f4ac0b5cc99d077cdc44af8312ce4d3110e
Christophe Leroy <christophe.leroy@c-s.fr>
2018-09-14 13:32:01 +0000

    watchdog: mpc8xxx: provide boot status
    mpc8xxx watchdog driver supports the following platforms:
    - mpc8xx
    - mpc83xx
    - mpc86xx
    
    Those three platforms have a 32 bits register which provides the
    reason of the last boot, including whether it was caused by the
    watchdog.
    
    mpc8xx: Register RSR, bit SWRS (bit 3)
    mpc83xx: Register RSR, bit SWRS (bit 28)
    mpc86xx: Register RSTRSCR, bit WDT_RR (bit 11)
    
    This patch maps the register as defined in the device tree and updates
    wdt.bootstatus based on the value of the watchdog related bit. Then
    the information can be retrieved via the WDIOC_GETBOOTSTATUS ioctl.
    
    Hereunder is an example of devicetree for mpc8xx,
    the Reset Status Register being at offset 0x288:
    
    		WDT: watchdog@0 {
    			compatible = "fsl,mpc823-wdt";
    			reg = <0x0 0x10 0x288 0x4>;
    		};
    
    On the mpc83xx, RSR is at offset 0x910
    On the mpc86xx, RSTRSCR is at offset 0xe0094
    
    Suggested-by: Radu Rendec <radu.rendec@gmail.com>
    Tested-by: Christophe Leroy <christophe.leroy@c-s.fr> # On mpc885
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>

--- drivers/watchdog/mpc8xxx_wdt.c
+++ drivers/watchdog/mpc8xxx_wdt.c
@@ -47,6 +47,7 @@ struct mpc8xxx_wdt {
 struct mpc8xxx_wdt_type {
 	int prescaler;
 	bool hw_enabled;
+	u32 rsr_mask;
 };
 
 struct mpc8xxx_wdt_ddata {
@@ -136,6 +137,7 @@ static int mpc8xxx_wdt_probe(struct platform_device *ofdev)
 	u32 freq = fsl_get_sys_freq();
 	bool enabled;
 	struct device *dev = &ofdev->dev;
+	u32 __iomem *rsr = NULL;
 
 	wdt_type = of_device_get_match_data(dev);
 	if (!wdt_type)
@@ -157,6 +159,21 @@ static int mpc8xxx_wdt_probe(struct platform_device *ofdev)
 	if (!enabled && wdt_type->hw_enabled) {
 		dev_info(dev, "could not be enabled in software\n");
 		return -ENODEV;
+	}
+
+	res = platform_get_resource(ofdev, IORESOURCE_MEM, 1);
+	if (res)
+		rsr = ioremap(res->start, resource_size(res));
+	if (rsr) {
+		bool status = in_be32(rsr) & wdt_type->rsr_mask;
+
+		ddata->wdd.bootstatus = status ? WDIOF_CARDRESET : 0;
+		 /* clear reset status bits related to watchdog timer */
+		out_be32(rsr, wdt_type->rsr_mask);
+		iounmap(rsr);
+
+		dev_info(dev, "Last boot was %scaused by watchdog\n",
+			 status ? "" : "not ");
 	}
 
 	spin_lock_init(&ddata->lock);
@@ -216,6 +233,7 @@ static const struct of_device_id mpc8xxx_wdt_match[] = {
 		.compatible = "mpc83xx_wdt",
 		.data = &(struct mpc8xxx_wdt_type) {
 			.prescaler = 0x10000,
+			.rsr_mask = BIT(3), /* RSR Bit SWRS */
 		},
 	},
 	{
@@ -223,6 +241,7 @@ static const struct of_device_id mpc8xxx_wdt_match[] = {
 		.data = &(struct mpc8xxx_wdt_type) {
 			.prescaler = 0x10000,
 			.hw_enabled = true,
+			.rsr_mask = BIT(20), /* RSTRSCR Bit WDT_RR */
 		},
 	},
 	{
@@ -230,6 +249,7 @@ static const struct of_device_id mpc8xxx_wdt_match[] = {
 		.data = &(struct mpc8xxx_wdt_type) {
 			.prescaler = 0x800,
 			.hw_enabled = true,
+			.rsr_mask = BIT(28), /* RSR Bit SWRS */
 		},
 	},
 	{},
