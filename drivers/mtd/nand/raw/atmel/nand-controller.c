ee34d520746307addc5708a693713a3fdc590c24
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-09-18 08:33:17 -0500

    mtd: rawnand: atmel: Fix potential NULL pointer dereference
    There is a potential execution path in which function
    of_find_compatible_node() returns NULL. In such a case,
    we end up having a NULL pointer dereference when accessing
    pointer *nfc_np* in function of_clk_get().
    
    So, we better don't take any chances and fix this by null
    checking pointer *nfc_np* before calling of_clk_get().
    
    Addresses-Coverity-ID: 1473052 ("Dereference null return value")
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>

--- drivers/mtd/nand/raw/atmel/nand-controller.c
+++ drivers/mtd/nand/raw/atmel/nand-controller.c
@@ -2077,6 +2077,11 @@ atmel_hsmc_nand_controller_legacy_init(struct atmel_hsmc_nand_controller *nc)
 	nfc_np = of_find_compatible_node(dev->of_node, NULL,
 					 "atmel,sama5d3-nfc");
 
+	if (!nfc_np) {
+		dev_err(dev, "Could not find device node for sama5d3-nfc\n");
+		return -ENODEV;
+	}
+
 	nc->clk = of_clk_get(nfc_np, 0);
 	if (IS_ERR(nc->clk)) {
 		ret = PTR_ERR(nc->clk);
