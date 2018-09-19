62ab1a819e2b85598d35decd1543ff4d9fc9303d
Dmitry Safonov <dima@arista.com>
2018-09-19 21:50:31 +0100

    timens: Add align for timens_offsets
    Align offsets so that Time Namespace will work for ia32 applications on
    x86_64 host.
    
    Signed-off-by: Dmitry Safonov <dima@arista.com>

--- include/linux/timens_offsets.h
+++ include/linux/timens_offsets.h
@@ -2,9 +2,16 @@
 #ifndef _LINUX_TIME_OFFSETS_H
 #define _LINUX_TIME_OFFSETS_H
 
+/*
+ * Time offsets need align as they're placed on vvar page,
+ * which should have tail paddings on ia32 vdso.
+ * Otherwise as u64 has align(4), vvar offsets will differ.
+ * On 64-bit big-endian systems vdso should convert to timespec64
+ * to timespec because of a padding occuring between the fields.
+ */
 struct timens_offsets {
-	struct timespec64  monotonic_time_offset;
-	struct timespec64  monotonic_boottime_offset;
+	struct timespec64  monotonic_time_offset __aligned(8);
+	struct timespec64  monotonic_boottime_offset __aligned(8);
 };
 
 #endif
