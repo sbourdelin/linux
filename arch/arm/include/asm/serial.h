7a19be4cda0483f002bda6ed1bbbdc869976b879
Rafał Miłecki <rafal@milecki.pl>
2018-09-20 22:13:57 +0200

    ARM: add serial.h and set BASE_BAUD to 0
    For years arm has been using serial.h from asm-generic which sets
    BASE_BAUD value to the (1843200 / 16). This is incorrect as:
    1) This value obviously isn't correct for all devices
    2) There are no device specific serial.h with CONFIG_ARCH_MULTIPLATFORM
    
    That value breaks early serial console e.g. on Broadcom Northstar boards
    (CONFIG_ARCH_BCM_5301X) which need BASE_BAUD = (99532800 / 16).
    
    The problem remained unnoticed until 4.14 and the commit 31cb9a8575ca0
    ("earlycon: initialise baud field of earlycon device structure").
    
    Above commit started filling "baud" in the "struct earlycon_device" (it
    seems to be a correct behavior) and changed behavior of the 8250/16550
    early console driver. Starting with 4.14 kernel's 8250_early.c sets UART
    divisor if both: "uartclk" and "baud" are set. Having wrong value for
    BASE_BAUD (and so "uartclk") results in broken console setup.
    
    Solution to that has been introduced in commit 0ff3ab701963 ("serial:
    8250_early: Only set divisor if valid clk & baud") and requires setting
    BASE_BAUD (and so "uartclk") to 0.
    
    A change similar to this action has been pushed for MIPS in the commit
    c8ec2041f549 ("MIPS: Add custom serial.h with BASE_BAUD override for
    generic kernel"). Another platform using 0 for BASE_BAUD is s390.
    
    Fixes: 31cb9a8575ca ("earlycon: initialise baud field of earlycon device structure")
    Cc: stable@vger.kernel.org # v4.14+
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

---
+++ arch/arm/include/asm/serial.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_ARM_SERIAL_H
+#define _ASM_ARM_SERIAL_H
+
+/* ARM kernels support multiple devices so there isn't a single valid value. */
+#define BASE_BAUD 0
+
+#endif /* _ASM_ARM_SERIAL_H */
