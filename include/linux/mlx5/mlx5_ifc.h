5dc30e31d79dddb85704430ae2626b7c3356fb68
Yishai Hadas <yishaih@mellanox.com>
2018-09-20 21:35:20 +0300

    net/mlx5: Set uid as part of CQ commands
    Set uid as part of CQ commands so that the firmware can manage the CQ
    object in a secured way.
    
    The firmware should mark this CQ with the given uid so that it can
    be used later on only by objects with the same uid.
    
    Upon DEVX flows that use this CQ (e.g. create QP command), the
    pointed CQ must have the same uid as of the issuer uid command.
    
    When a command is issued with uid=0 it means that the issuer of the
    command is trusted (i.e. kernel), in that case any pointed object
    can be used regardless of its uid.
    
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -5611,7 +5611,7 @@ enum {
 
 struct mlx5_ifc_modify_cq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
@@ -6386,7 +6386,7 @@ struct mlx5_ifc_destroy_cq_out_bits {
 
 struct mlx5_ifc_destroy_cq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
@@ -7146,7 +7146,7 @@ struct mlx5_ifc_create_cq_out_bits {
 
 struct mlx5_ifc_create_cq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
