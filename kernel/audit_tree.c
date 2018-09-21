e413a8a14a938a9e68523897c51a108839d1e6fe
Przemysław Głębocki <glebocki.przemyslaw@gmail.com>
2018-09-22 01:38:22 +0200

    audit: fixed a braces coding style issue
    Fixed a coding style issue.
    
    Signed-off-by: Przemysław Głębocki <glebocki.przemyslaw@gmail.com>

--- kernel/audit_tree.c
+++ kernel/audit_tree.c
@@ -112,10 +112,9 @@ static void free_chunk(struct audit_chunk *chunk)
 {
 	int i;
 
-	for (i = 0; i < chunk->count; i++) {
+	for (i = 0; i < chunk->count; i++)
 		if (chunk->owners[i].owner)
 			put_tree(chunk->owners[i].owner);
-	}
 	kfree(chunk);
 }
 
