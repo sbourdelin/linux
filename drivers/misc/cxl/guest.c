1805433393261ed1b8f41e257c9f65e60d742320
zhong jiang <zhongjiang@huawei.com>
2018-09-26 19:41:12 +0800

    misc: cxl: Move a deference below a NULL test
    It is safe to move a deference below a NULL test.
    
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>

--- drivers/misc/cxl/guest.c
+++ drivers/misc/cxl/guest.c
@@ -1018,10 +1018,10 @@ err1:
 
 void cxl_guest_remove_afu(struct cxl_afu *afu)
 {
-	pr_devel("in %s - AFU(%d)\n", __func__, afu->slice);
-
 	if (!afu)
 		return;
+
+	pr_devel("in %s - AFU(%d)\n", __func__, afu->slice);
 
 	/* flush and stop pending job */
 	afu->guest->handle_err = false;
