d131035b0b46c07545d7ec3b9022c0326a510cbc
Zhu Yanjun <yanjun.zhu@oracle.com>
2018-09-25 23:11:18 -0400

    IB/rxe: replace kvfree with vfree
    The buf is allocated by vmalloc_user in the function rxe_queue_init.
    So it is better to free it by vfree.
    
    Fixes: 8700e3e7c485 ("Soft RoCE driver")
    Signed-off-by: Zhu Yanjun <yanjun.zhu@oracle.com>

--- drivers/infiniband/sw/rxe/rxe_cq.c
+++ drivers/infiniband/sw/rxe/rxe_cq.c
@@ -97,7 +97,7 @@ int rxe_cq_from_init(struct rxe_dev *rxe, struct rxe_cq *cq, int cqe,
 	err = do_mmap_info(rxe, uresp ? &uresp->mi : NULL, context,
 			   cq->queue->buf, cq->queue->buf_size, &cq->queue->ip);
 	if (err) {
-		kvfree(cq->queue->buf);
+		vfree(cq->queue->buf);
 		kfree(cq->queue);
 		return err;
 	}
