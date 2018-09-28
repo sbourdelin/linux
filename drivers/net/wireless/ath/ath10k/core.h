f06ff9746f4a7305d755200bc19649ef800a258a
Rajkumar Manoharan <rmanohar@codeaurora.org>
2018-09-28 16:16:14 -0700

    ath10k: reporting estimated tx airtime for fairness
    Transmit airtime will be estimated from last tx rate used.
    Firmware report tx rate by peer stats. Airtime is computed
    on tx path and the same will be reported to mac80211 upon
    tx completion.
    
    Signed-off-by: Kan Yan <kyan@google.com>
    Signed-off-by: Rajkumar Manoharan <rmanohar@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -123,6 +123,7 @@ struct ath10k_skb_cb {
 	u8 flags;
 	u8 eid;
 	u16 msdu_id;
+	u16 airtime_est;
 	struct ieee80211_vif *vif;
 	struct ieee80211_txq *txq;
 } __packed;
@@ -493,6 +494,7 @@ struct ath10k_sta {
 	u32 smps;
 	u16 peer_id;
 	struct rate_info txrate;
+	u32 last_tx_bitrate;
 
 	struct work_struct update_wk;
 	u64 rx_duration;
