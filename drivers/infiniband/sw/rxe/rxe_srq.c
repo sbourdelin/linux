20f1e59f1139c979b87ca10b031264cbf1554b5c
Zhu Yanjun <yanjun.zhu@oracle.com>
2018-09-29 23:50:22 -0400

    IB: rxe: avoid srq memory leak
    In rxe_queue_init, q and q->buf are allocated. In do_mmap_info,
    q->ip is allocated. When error occurs, rxe_srq_from_init and
    the later error handler do not free these allocated memories.
    This will make memory leak.
    
    CC: Srinivas Eeda <srinivas.eeda@oracle.com>
    CC: Junxiao Bi <junxiao.bi@oracle.com>
    Signed-off-by: Zhu Yanjun <yanjun.zhu@oracle.com>

--- drivers/infiniband/sw/rxe/rxe_srq.c
+++ drivers/infiniband/sw/rxe/rxe_srq.c
@@ -129,13 +129,18 @@ int rxe_srq_from_init(struct rxe_dev *rxe, struct rxe_srq *srq,
 
 	err = do_mmap_info(rxe, uresp ? &uresp->mi : NULL, context, q->buf,
 			   q->buf_size, &q->ip);
-	if (err)
+	if (err) {
+		vfree(q->buf);
+		kfree(q);
 		return err;
+	}
 
 	if (uresp) {
 		if (copy_to_user(&uresp->srq_num, &srq->srq_num,
-				 sizeof(uresp->srq_num)))
+				 sizeof(uresp->srq_num))) {
+			rxe_queue_cleanup(q);
 			return -EFAULT;
+		}
 	}
 
 	return 0;
