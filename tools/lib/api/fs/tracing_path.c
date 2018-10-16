6007d34b8348700131740e75d6b3d7a3e976d130
Jiri Olsa <jolsa@kernel.org>
2018-10-16 13:48:18 +0200

    perf tools: Fix tracing_path_mount proper path
    If there's no tracefs (RHEL7) support the tracing_path_mount
    returns debugfs path which results in following fail:
    
      # perf probe sys_write
      kprobe_events file does not exist - please rebuild kernel with CONFIG_KPROBE_EVENTS.
      Error: Failed to add events.
    
    In tracing_path_debugfs_mount function we need to return the
    'tracing' path instead of just the mount to make it work:
    
      # perf probe sys_write
      Added new event:
        probe:sys_write      (on sys_write)
    
      You can now use it in all perf tools, such as:
    
              perf record -e probe:sys_write -aR sleep 1
    
    Adding the 'return tracing_path;' also to tracing_path_tracefs_mount
    function just for consistency with tracing_path_debugfs_mount.
    
    Upstream keeps working, because it has the tracefs support.
    
    Link: http://lkml.kernel.org/n/tip-yiwkzexq9fk1ey1xg3gnjlw4@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/lib/api/fs/tracing_path.c
+++ tools/lib/api/fs/tracing_path.c
@@ -38,7 +38,7 @@ static const char *tracing_path_tracefs_mount(void)
 
 	__tracing_path_set("", mnt);
 
-	return mnt;
+	return tracing_path;
 }
 
 static const char *tracing_path_debugfs_mount(void)
@@ -51,7 +51,7 @@ static const char *tracing_path_debugfs_mount(void)
 
 	__tracing_path_set("tracing/", mnt);
 
-	return mnt;
+	return tracing_path;
 }
 
 const char *tracing_path_mount(void)
