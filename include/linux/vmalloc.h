8dab1f5c1e6a0b2cb17237035efaf019553d79ea
Uladzislau Rezki (Sony) <urezki@gmail.com>
2018-10-19 19:35:37 +0200

    mm/vmalloc: keep track of free blocks for allocation
    Currently an allocation of the new VA area is done over
    busy list iteration until a suitable hole is found between
    two busy areas. Therefore each new allocation causes the
    list being grown. Due to long list and different permissive
    parameters an allocation can take a long time on embedded
    devices(milliseconds).
    
    This patch organizes the vmalloc memory layout into free
    areas of the VMALLOC_START-VMALLOC_END range. It uses a
    red-black tree that keeps blocks sorted by their offsets
    in pair with linked list keeping the free space in order
    of increasing addresses.
    
    Allocation: to allocate a new block a search is done over
    free list areas until a suitable block is large enough to
    encompass the requested size. If the block is bigger than
    requested size - it is split.
    
    De-allocation: red-black tree allows efficiently find a
    spot in the tree whereas a linked list allows fast merge
    of de-allocated memory chunks with existing free blocks
    creating large coalesced areas.
    
    model name: Intel(R) Core(TM) i5-3320M CPU @ 2.60GHz
    
    test_1:
    <measure this loop time>
    for (n = 0; n < 1000000; n++) {
        void *ptr_1 = vmalloc(3 * PAGE_SIZE);
        *((__u8 *)ptr_1) = 0; /* Pretend we used the mem */
        vfree(ptr_1);
    }
    <measure this loop time>
    
    1218459(us) vs 1146597(us) 5%
    1219721(us) vs 1145212(us) 6%
    1226255(us) vs 1142134(us) 6%
    1239828(us) vs 1144809(us) 7%
    1232131(us) vs 1144775(us) 7%
    
    test_2:
    for (n = 0; n < 15000; n++)
        ptr[n] = vmalloc(1 * PAGE_SIZE);
    
    <measure this loop time>
    for (n = 0; n < 1000000; n++) {
        void *ptr_1 = vmalloc(100 * PAGE_SIZE);
        void *ptr_2 = vmalloc(1 * PAGE_SIZE);
        *((__u8 *)ptr_1) = 0; /* Pretend we used the mem */
        *((__u8 *)ptr_2) = 1; /* Pretend we used the mem */
    
        vfree(ptr_1);
        vfree(ptr_2);
    }
    <measure this loop time>
    
    55866315(us) vs 15037680(us) 73%
    57601435(us) vs 14809454(us) 74%
    52612371(us) vs 14550292(us) 72%
    48894648(us) vs 14769538(us) 69%
    55718063(us) vs 14727350(us) 73%
    
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>

--- include/linux/vmalloc.h
+++ include/linux/vmalloc.h
@@ -45,11 +45,11 @@ struct vmap_area {
 	unsigned long va_start;
 	unsigned long va_end;
 	unsigned long flags;
+	unsigned long align;
 	struct rb_node rb_node;         /* address sorted rbtree */
 	struct list_head list;          /* address sorted list */
 	struct llist_node purge_list;    /* "lazy purge" list */
 	struct vm_struct *vm;
-	struct rcu_head rcu_head;
 };
 
 /*
