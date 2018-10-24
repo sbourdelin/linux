53b2763793862a5ef6d28f15495e11cc5bc08a0d
Olga Kornievskaia <kolga@netapp.com>
2018-10-24 15:58:34 -0400

    NFS inter ssc open
    NFSv4.2 inter server to server copy requires the destination server to
    READ the data from the source server using the provided stateid and
    file handle.
    
    Given an NFSv4 stateid and filehandle from the COPY operaion, provide the
    destination server with an NFS client function to create a struct file
    suitable for the destiniation server to READ the data to be copied.
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Olga Kornievskaia <kolga@netapp.com>

--- fs/nfs/nfs4_fs.h
+++ fs/nfs/nfs4_fs.h
@@ -307,6 +307,13 @@ extern int nfs4_set_rw_stateid(nfs4_stateid *stateid,
 		const struct nfs_open_context *ctx,
 		const struct nfs_lock_context *l_ctx,
 		fmode_t fmode);
+extern int nfs4_proc_getattr(struct nfs_server *server, struct nfs_fh *fhandle,
+			     struct nfs_fattr *fattr, struct nfs4_label *label,
+			     struct inode *inode);
+extern int update_open_stateid(struct nfs4_state *state,
+				const nfs4_stateid *open_stateid,
+				const nfs4_stateid *deleg_stateid,
+				fmode_t fmode);
 
 #if defined(CONFIG_NFS_V4_1)
 extern int nfs41_sequence_done(struct rpc_task *, struct nfs4_sequence_res *);
