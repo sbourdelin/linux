64dafd93f2deb35739b11cfa946e508770d9346a
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2018-10-29 19:24:08 +0100

    media: mt9m111: allow to setup pixclk polarity
    The chip can be configured to output data transitions on the
    rising or falling edge of PIXCLK (Datasheet R58:1[9]), default is on the
    falling edge.
    
    Parsing the fw-node is made in a subfunction to bundle all (future)
    dt-parsing / fw-parsing stuff.
    
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
    (m.grzeschik@pengutronix.de: Fix inverting clock. INV_PIX_CLOCK bit is set
    per default. Set bit to 0 (enable mask bit without value) to enable
    falling edge sampling.)
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    (m.felsch@pengutronix.de: use fwnode helpers)
    (m.felsch@pengutronix.de: mv fw parsing into own function)
    (m.felsch@pengutronix.de: adapt commit msg)
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>

--- drivers/media/i2c/mt9m111.c
+++ drivers/media/i2c/mt9m111.c
@@ -15,12 +15,14 @@
 #include <linux/delay.h>
 #include <linux/v4l2-mediabus.h>
 #include <linux/module.h>
+#include <linux/property.h>
 
 #include <media/v4l2-async.h>
 #include <media/v4l2-clk.h>
 #include <media/v4l2-common.h>
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-device.h>
+#include <media/v4l2-fwnode.h>
 
 /*
  * MT9M111, MT9M112 and MT9M131:
@@ -239,6 +241,8 @@ struct mt9m111 {
 	const struct mt9m111_datafmt *fmt;
 	int lastpage;	/* PageMap cache value */
 	bool is_streaming;
+	/* user point of view - 0: falling 1: rising edge */
+	unsigned int pclk_sample:1;
 #ifdef CONFIG_MEDIA_CONTROLLER
 	struct media_pad pad;
 #endif
@@ -590,6 +594,10 @@ static int mt9m111_set_pixfmt(struct mt9m111 *mt9m111,
 		dev_err(&client->dev, "Pixel format not handled: %x\n", code);
 		return -EINVAL;
 	}
+
+	/* receiver samples on falling edge, chip-hw default is rising */
+	if (mt9m111->pclk_sample == 0)
+		mask_outfmt2 |= MT9M111_OUTFMT_INV_PIX_CLOCK;
 
 	ret = mt9m111_reg_mask(client, context_a.output_fmt_ctrl2,
 			       data_outfmt2, mask_outfmt2);
@@ -1051,9 +1059,15 @@ static int mt9m111_s_stream(struct v4l2_subdev *sd, int enable)
 static int mt9m111_g_mbus_config(struct v4l2_subdev *sd,
 				struct v4l2_mbus_config *cfg)
 {
-	cfg->flags = V4L2_MBUS_MASTER | V4L2_MBUS_PCLK_SAMPLE_RISING |
+	struct mt9m111 *mt9m111 = container_of(sd, struct mt9m111, subdev);
+
+	cfg->flags = V4L2_MBUS_MASTER |
 		V4L2_MBUS_HSYNC_ACTIVE_HIGH | V4L2_MBUS_VSYNC_ACTIVE_HIGH |
 		V4L2_MBUS_DATA_ACTIVE_HIGH;
+
+	cfg->flags |= mt9m111->pclk_sample ? V4L2_MBUS_PCLK_SAMPLE_RISING :
+		V4L2_MBUS_PCLK_SAMPLE_FALLING;
+
 	cfg->type = V4L2_MBUS_PARALLEL;
 
 	return 0;
@@ -1123,6 +1137,32 @@ done:
 	return ret;
 }
 
+static int mt9m111_probe_fw(struct i2c_client *client, struct mt9m111 *mt9m111)
+{
+	struct v4l2_fwnode_endpoint *bus_cfg;
+	struct fwnode_handle *np;
+	int ret = 0;
+
+	np = fwnode_graph_get_next_endpoint(dev_fwnode(&client->dev), NULL);
+	if (!np)
+		return -EINVAL;
+
+	bus_cfg = v4l2_fwnode_endpoint_alloc_parse(np);
+	if (IS_ERR(bus_cfg)) {
+		ret = PTR_ERR(bus_cfg);
+		goto out_put_fw;
+	}
+
+	mt9m111->pclk_sample = !!(bus_cfg->bus.parallel.flags &
+				  V4L2_MBUS_PCLK_SAMPLE_RISING);
+
+	v4l2_fwnode_endpoint_free(bus_cfg);
+
+out_put_fw:
+	fwnode_handle_put(np);
+	return ret;
+}
+
 static int mt9m111_probe(struct i2c_client *client,
 			 const struct i2c_device_id *did)
 {
@@ -1146,6 +1186,10 @@ static int mt9m111_probe(struct i2c_client *client,
 
 	/* Default HIGHPOWER context */
 	mt9m111->ctx = &context_b;
+
+	ret = mt9m111_probe_fw(client, mt9m111);
+	if (ret)
+		return ret;
 
 	v4l2_i2c_subdev_init(&mt9m111->subdev, client, &mt9m111_subdev_ops);
 	mt9m111->subdev.flags |= V4L2_SUBDEV_FL_HAS_DEVNODE;
