6be62687b514b196d5048ceecb285d2ad135251a
Shayenne da Luz Moura <shayenneluzmoura@gmail.com>
2018-10-31 11:44:45 -0300

    drm: Remove 80-column line in drm_mode_object.c
    Add a new variable to make the drm_mode_object comparison before
    idr_alloc and decrease line size.
    
    Signed-off-by: Shayenne da Luz Moura <shayenneluzmoura@gmail.com>

--- drivers/gpu/drm/drm_mode_object.c
+++ drivers/gpu/drm/drm_mode_object.c
@@ -36,9 +36,10 @@ int __drm_mode_object_add(struct drm_device *dev, struct drm_mode_object *obj,
 			  void (*obj_free_cb)(struct kref *kref))
 {
 	int ret;
+	struct drm_mode_object *obj_tmp = register_obj ? obj : NULL;
 
 	mutex_lock(&dev->mode_config.idr_mutex);
-	ret = idr_alloc(&dev->mode_config.crtc_idr, register_obj ? obj : NULL, 1, 0, GFP_KERNEL);
+	ret = idr_alloc(&dev->mode_config.crtc_idr, obj_tmp, 1, 0, GFP_KERNEL);
 	if (ret >= 0) {
 		/*
 		 * Set up the object linking under the protection of the idr
