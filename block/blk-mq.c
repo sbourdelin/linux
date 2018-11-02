b7afafb5449e5d8f98a90f15d08460db99b1c5a8
Jianchao Wang <jianchao.w.wang@oracle.com>
2018-11-02 15:08:55 +0800

    blk-mq: issue directly with bypass 'false' in blk_mq_sched_insert_requests
    It is not necessary to issue request directly with bypass 'true'
    in blk_mq_sched_insert_requests and insert the non-issued requests
    itself. Just set bypass to 'false' and let blk_mq_try_issue_directly
    handle them totally.
    
    Signed-off-by: Jianchao Wang <jianchao.w.wang@oracle.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1832,20 +1832,11 @@ void blk_mq_try_issue_list_directly(struct blk_mq_hw_ctx *hctx,
 		struct list_head *list)
 {
 	while (!list_empty(list)) {
-		blk_status_t ret;
 		struct request *rq = list_first_entry(list, struct request,
 				queuelist);
 
 		list_del_init(&rq->queuelist);
-		ret = blk_mq_request_issue_directly(rq);
-		if (ret != BLK_STS_OK) {
-			if (ret == BLK_STS_RESOURCE ||
-					ret == BLK_STS_DEV_RESOURCE) {
-				list_add(&rq->queuelist, list);
-				break;
-			}
-			blk_mq_end_request(rq, ret);
-		}
+		blk_mq_try_issue_directly(hctx, rq, NULL, false);
 	}
 }
 
