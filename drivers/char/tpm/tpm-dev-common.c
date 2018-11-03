ee8e7d423ebae92b398d68aecc9255434483eb14
Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
2018-11-03 19:08:04 +0200

    tpm: move TPM space code out of tpm_transmit()
    Prepare and commit TPM space before and after calling tpm_transmit()
    instead of doing that inside tpm_transmit(). After this change we can
    remove TPM_TRANSMIT_NESTED flag from tpm2_prepare_space() and
    tpm2_commit_space() and replace it with TPM_TRANSMIT_UNLOCKED.
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

--- drivers/char/tpm/tpm-dev-common.c
+++ drivers/char/tpm/tpm-dev-common.c
@@ -30,13 +30,38 @@ static DEFINE_MUTEX(tpm_dev_wq_lock);
 static ssize_t tpm_dev_transmit(struct tpm_chip *chip, struct tpm_space *space,
 				u8 *buf, size_t bufsiz)
 {
-	ssize_t ret;
+	struct tpm_output_header *header = (void *)buf;
+	ssize_t ret, len;
 
 	mutex_lock(&chip->tpm_mutex);
-	ret = tpm_transmit(chip, space, buf, bufsiz, TPM_TRANSMIT_UNLOCKED);
-	mutex_unlock(&chip->tpm_mutex);
+	ret = tpm2_prepare_space(chip, space, buf, bufsiz);
+	/* If the command is not implemented by the TPM, synthesize a
+	 * response with a TPM2_RC_COMMAND_CODE return for user-space.
+	 */
+	if (ret == -EOPNOTSUPP) {
+		header->length = cpu_to_be32(sizeof(*header));
+		header->tag = cpu_to_be16(TPM2_ST_NO_SESSIONS);
+		header->return_code = cpu_to_be32(TPM2_RC_COMMAND_CODE |
+						  TSS2_RESMGR_TPM_RC_LAYER);
+		ret = sizeof(*header);
+		goto out;
+	}
+	if (ret)
+		goto out;
 
-	return ret;
+	len = tpm_transmit(chip, space, buf, bufsiz, TPM_TRANSMIT_UNLOCKED);
+	if (len < 0) {
+		ret = len;
+		goto out;
+	}
+
+	ret = tpm2_commit_space(chip, space, buf, &len);
+out:
+	mutex_unlock(&chip->tpm_mutex);
+	if (ret)
+		return ret;
+
+	return len;
 }
 
 static void tpm_dev_async_work(struct work_struct *work)
