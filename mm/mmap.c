42a811602382a09d55d944039c5870dee93bf026
Yangtao Li <tiny.windzz@gmail.com>
2018-11-04 07:44:56 -0500

    mm: mmap: remove unnecessary unlikely()
    WARN_ON() already contains an unlikely(), so it's not necessary to use
    unlikely.
    
    Signed-off-by: Yangtao Li <tiny.windzz@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2965,10 +2965,8 @@ out:
 static inline void verify_mm_writelocked(struct mm_struct *mm)
 {
 #ifdef CONFIG_DEBUG_VM
-	if (unlikely(down_read_trylock(&mm->mmap_sem))) {
-		WARN_ON(1);
+	if (WARN_ON(down_read_trylock(&mm->mmap_sem)))
 		up_read(&mm->mmap_sem);
-	}
 #endif
 }
 
