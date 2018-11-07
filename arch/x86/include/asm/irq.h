f76927fef9a5cf047dffbdfb2b94977b3cd53960
Yi Wang <wang.yi59@zte.com.cn>
2018-11-07 11:32:37 +0800

    x86/irq: fix build warning in irq.c
    This fixes the following warnings:
    arch/x86/kernel/irq.c:270:28: warning: no previous prototype for ‘smp_x86_platform_ipi’ [-Wmissing-prototypes]
     __visible void __irq_entry smp_x86_platform_ipi(struct pt_regs *regs)
                                ^
    arch/x86/kernel/irq.c:301:16: warning: no previous prototype for ‘smp_kvm_posted_intr_ipi’ [-Wmissing-prototypes]
     __visible void smp_kvm_posted_intr_ipi(struct pt_regs *regs)
                    ^
    arch/x86/kernel/irq.c:314:16: warning: no previous prototype for ‘smp_kvm_posted_intr_wakeup_ipi’ [-Wmissing-prototypes]
     __visible void smp_kvm_posted_intr_wakeup_ipi(struct pt_regs *regs)
                    ^
    arch/x86/kernel/irq.c:328:16: warning: no previous prototype for ‘smp_kvm_posted_intr_nested_ipi’ [-Wmissing-prototypes]
     __visible void smp_kvm_posted_intr_nested_ipi(struct pt_regs *regs)
                    ^
    
    Signed-off-by: Yi Wang <wang.yi59@zte.com.cn>

--- arch/x86/include/asm/irq.h
+++ arch/x86/include/asm/irq.h
@@ -30,6 +30,9 @@ extern void fixup_irqs(void);
 
 #ifdef CONFIG_HAVE_KVM
 extern void kvm_set_posted_intr_wakeup_handler(void (*handler)(void));
+extern __visible void smp_kvm_posted_intr_ipi(struct pt_regs *regs);
+extern __visible void smp_kvm_posted_intr_wakeup_ipi(struct pt_regs *regs);
+extern __visible void smp_kvm_posted_intr_nested_ipi(struct pt_regs *regs);
 #endif
 
 extern void (*x86_platform_ipi_callback)(void);
@@ -44,6 +47,8 @@ extern void init_ISA_irqs(void);
 #ifdef CONFIG_X86_LOCAL_APIC
 void arch_trigger_cpumask_backtrace(const struct cpumask *mask,
 				    bool exclude_self);
+
+extern __visible void smp_x86_platform_ipi(struct pt_regs *regs);
 #define arch_trigger_cpumask_backtrace arch_trigger_cpumask_backtrace
 #endif
 
