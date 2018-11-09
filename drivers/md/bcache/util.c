95646257af8a65bdcec7d4841f71f9188c601ccf
Ming Lei <ming.lei@redhat.com>
2018-11-10 00:26:03 +0800

    bcache: avoid to use bio_for_each_segment_all() in bch_bio_alloc_pages()
    bch_bio_alloc_pages() is always called on one new bio, so it is safe
    to access the bvec table directly. Given it is the only kind of this
    case, open code the bvec table access since bio_for_each_segment_all()
    will be changed to support for iterating over multipage bvec.
    
    Acked-by: Coly Li <colyli@suse.de>
    Cc: Coly Li <colyli@suse.de>
    Cc: linux-bcache@vger.kernel.org
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/md/bcache/util.c
+++ drivers/md/bcache/util.c
@@ -270,7 +270,7 @@ int bch_bio_alloc_pages(struct bio *bio, gfp_t gfp_mask)
 	int i;
 	struct bio_vec *bv;
 
-	bio_for_each_segment_all(bv, bio, i) {
+	for (i = 0, bv = bio->bi_io_vec; i < bio->bi_vcnt; bv++) {
 		bv->bv_page = alloc_page(gfp_mask);
 		if (!bv->bv_page) {
 			while (--bv >= bio->bi_io_vec)
