63743d4d3fb88681087400cbe9a5ef27cb78685a
Thiago Jung Bauermann <bauerman@linux.ibm.com>
2018-11-16 18:07:03 -0200

    integrity: Introduce integrity_keyring_from_id()
    IMA will need to obtain the keyring used to verify file signatures so that
    it can verify the module-style signature appended to files.
    
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/integrity.h
+++ security/integrity/integrity.h
@@ -153,12 +153,18 @@ extern struct dentry *integrity_dir;
 
 #ifdef CONFIG_INTEGRITY_SIGNATURE
 
+struct key *integrity_keyring_from_id(const unsigned int id);
 int integrity_digsig_verify(const unsigned int id, const char *sig, int siglen,
 			    const char *digest, int digestlen);
 
 int __init integrity_init_keyring(const unsigned int id);
 int __init integrity_load_x509(const unsigned int id, const char *path);
 #else
+
+static inline struct key *integrity_keyring_from_id(const unsigned int id)
+{
+	return ERR_PTR(-EINVAL);
+}
 
 static inline int integrity_digsig_verify(const unsigned int id,
 					  const char *sig, int siglen,
