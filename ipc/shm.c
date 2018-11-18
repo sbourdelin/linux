b1b790ed93b2828d25e282696d596e4889e5ec7c
Daniel Robson <danielijrobson@gmail.com>
2018-11-18 23:49:11 +0000

    ipc/shm.c: removed 'int err' from shm_get_policy()
    removed unneeded variable in shm_get_policy()
    
    Signed-off-by: Daniel Robson <danielijrobson@gmail.com>

--- ipc/shm.c
+++ ipc/shm.c
@@ -432,11 +432,10 @@ static int shm_set_policy(struct vm_area_struct *vma, struct mempolicy *new)
 {
 	struct file *file = vma->vm_file;
 	struct shm_file_data *sfd = shm_file_data(file);
-	int err = 0;
 
 	if (sfd->vm_ops->set_policy)
-		err = sfd->vm_ops->set_policy(vma, new);
-	return err;
+		return sfd->vm_ops->set_policy(vma, new);
+	return 0;
 }
 
 static struct mempolicy *shm_get_policy(struct vm_area_struct *vma,
