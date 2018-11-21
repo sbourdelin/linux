41c889fd962d49e4d010a77ed32c62ced376353c
Pan Bian <bianpan2013@163.com>
2018-11-21 14:57:48 +0800

    xfrm_user: use xfrm_state_put to free xfrm_state_alloc return value
    The memory chunk allocated by xfrm_state_alloc() should be released with
    xfrm_state_put(), not kfree.
    
    Signed-off-by: Pan Bian <bianpan2013@163.com>

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -2288,13 +2288,13 @@ static int xfrm_add_acquire(struct sk_buff *skb, struct nlmsghdr *nlh,
 
 	}
 
-	kfree(x);
+	xfrm_state_put(x);
 	kfree(xp);
 
 	return 0;
 
 free_state:
-	kfree(x);
+	xfrm_state_put(x);
 nomem:
 	return err;
 }
