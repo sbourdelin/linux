2d94a057e89034cf75c88081518ef3001634ade4
Huaisheng Ye <yehs1@lenovo.com>
2018-11-21 11:27:02 +0800

    dm: expand valid types for dm-ioctl
    If use DAX way to mount the origin device of dm_snapshot, when
    merging back snapshot to origin, system call table_load during the
    construction of snapshot-merge will try to detect new dm_table's type
    equals to the existed md's type or not.
    The existed type equals to DM_TYPE_DAX_BIO_BASED, but the new created
    type belongs to DM_TYPE_BIO_BASED. So, we need to expand valid_type in
    function is_valid_type.
    
    Signed-off-by: Huaisheng Ye <yehs1@lenovo.com>

--- drivers/md/dm-ioctl.c
+++ drivers/md/dm-ioctl.c
@@ -1295,7 +1295,8 @@ static int populate_table(struct dm_table *table,
 static bool is_valid_type(enum dm_queue_mode cur, enum dm_queue_mode new)
 {
 	if (cur == new ||
-	    (cur == DM_TYPE_BIO_BASED && new == DM_TYPE_DAX_BIO_BASED))
+	    (cur == DM_TYPE_BIO_BASED && new == DM_TYPE_DAX_BIO_BASED) ||
+	    (cur == DM_TYPE_DAX_BIO_BASED && new == DM_TYPE_BIO_BASED))
 		return true;
 
 	return false;
