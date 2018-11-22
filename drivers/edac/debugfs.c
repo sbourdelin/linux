d9ac151e051cb9d581931b95772824bbfe464d19
Jan Luebbe <jlu@pengutronix.de>
2018-11-22 13:11:40 +1300

    EDAC: Add missing debugfs_create_x32 wrapper
    We already have wrappers for x8 and x16, so add the missing x32 one.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Chris Packham <chris.packham@alliedtelesis.co.nz>

--- drivers/edac/debugfs.c
+++ drivers/edac/debugfs.c
@@ -161,3 +161,14 @@ struct dentry *edac_debugfs_create_x16(const char *name, umode_t mode,
 	return debugfs_create_x16(name, mode, parent, value);
 }
 EXPORT_SYMBOL_GPL(edac_debugfs_create_x16);
+
+/* Wrapper for debugfs_create_x32() */
+struct dentry *edac_debugfs_create_x32(const char *name, umode_t mode,
+				       struct dentry *parent, u32 *value)
+{
+	if (!parent)
+		parent = edac_debugfs;
+
+	return debugfs_create_x32(name, mode, parent, value);
+}
+EXPORT_SYMBOL_GPL(edac_debugfs_create_x32);
