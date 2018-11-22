3054438e6b1c75b4bc41adb0598f81151651ae17
Paul Gortmaker <paul.gortmaker@windriver.com>
2018-11-21 23:32:58 -0500

    mfd: wm831x-core: drop unused MODULE_ tags from non-modular code
    The Kconfig currently controlling compilation of this code is:
    
    drivers/mfd/Kconfig:config MFD_WM831X
    drivers/mfd/Kconfig:    bool
    
    ...meaning that it currently is not being built as a module by anyone.
    
    Lets remove the couple traces of modular infrastructure use, so that
    when reading the driver there is no doubt it is builtin-only.
    
    We delete the MODULE_LICENSE tag etc. since all that information
    is already contained at the top of the file in the comments.
    
    Cc: Lee Jones <lee.jones@linaro.org>
    Cc: patches@opensource.cirrus.com
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/mfd/wm831x-core.c
+++ drivers/mfd/wm831x-core.c
@@ -13,7 +13,8 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/export.h>
 #include <linux/bcd.h>
 #include <linux/delay.h>
 #include <linux/mfd/core.h>
@@ -1944,7 +1945,3 @@ void wm831x_device_shutdown(struct wm831x *wm831x)
 	}
 }
 EXPORT_SYMBOL_GPL(wm831x_device_shutdown);
-
-MODULE_DESCRIPTION("Core support for the WM831X AudioPlus PMIC");
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Mark Brown");
