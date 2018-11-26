563e77edc93643fff3060697254c88d63e2e45d0
Steven Toth <stoth@kernellabs.com>
2018-11-26 19:09:37 +0100

    media: hdcapm: add support for usb2hdcapm hdmi2usb framegrabber from startech
    This patch is based on the work of Steven Toth. He reverse engineered
    the driver by tracing the windows driver.
    
    https://github.com/stoth68000/hdcapm/
    
    Signed-off-by: Steven Toth <stoth@kernellabs.com>
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>

---
+++ drivers/media/usb/hdcapm/hdcapm-buffer.c
@@ -0,0 +1,230 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Driver for the Startech USB2HDCAPM USB capture device
+ *
+ * Copyright (c) 2017 Steven Toth <stoth@kernellabs.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * GNU General Public License for more details.
+ */
+
+#include "hdcapm.h"
+
+struct hdcapm_buffer *hdcapm_buffer_alloc(struct hdcapm_dev *dev,
+					  u32 nr, u32 maxsize)
+{
+	struct hdcapm_buffer *buf;
+
+	buf = kzalloc(sizeof(*buf), GFP_KERNEL);
+	if (!buf)
+		return NULL;
+
+	buf->nr = nr;
+	buf->dev = dev;
+	buf->maxsize = maxsize;
+	buf->ptr = kzalloc(maxsize, GFP_KERNEL);
+	if (!buf->ptr) {
+		kfree(buf);
+		return NULL;
+	}
+
+	return buf;
+}
+
+void hdcapm_buffer_free(struct hdcapm_buffer *buf)
+{
+	kfree(buf->ptr);
+	buf->ptr = NULL;
+
+	usb_free_urb(buf->urb);
+	buf->urb = NULL;
+
+	kfree(buf);
+}
+
+/* Helper macro for moving all buffers to another list.
+ * We WILL take the mutex in this func.
+ */
+void hdcapm_buffers_move_all(struct hdcapm_dev *dev,
+			     struct list_head *to, struct list_head *from)
+{
+	struct hdcapm_buffer *buf;
+
+	mutex_lock(&dev->dmaqueue_lock);
+	while (!list_empty(from)) {
+		buf = list_first_entry(from, struct hdcapm_buffer, list);
+		if (buf)
+			list_move_tail(&buf->list, to);
+	}
+	mutex_unlock(&dev->dmaqueue_lock);
+}
+
+/* Helper macro to free all buffers from a list.
+ * We WILL take the mutex in this func.
+ */
+void hdcapm_buffers_free_all(struct hdcapm_dev *dev, struct list_head *head)
+{
+	struct hdcapm_buffer *buf;
+
+	mutex_lock(&dev->dmaqueue_lock);
+	while (!list_empty(head)) {
+		buf = list_first_entry(head, struct hdcapm_buffer, list);
+		if (buf) {
+			list_del(&buf->list);
+			hdcapm_buffer_free(buf);
+		}
+	}
+	mutex_unlock(&dev->dmaqueue_lock);
+}
+
+/* Helper macros for managing the device lists.
+ * We WILL take the mutex in this func.
+ * Return a reference to the top most used buffer, we're going to
+ * read some or all of it (probably). Don't delete it from the list.
+ */
+struct hdcapm_buffer *hdcapm_buffer_peek_used(struct hdcapm_dev *dev)
+{
+	struct hdcapm_buffer *buf = NULL;
+
+	mutex_lock(&dev->dmaqueue_lock);
+	if (!list_empty(&dev->list_buf_used)) {
+		buf = list_first_entry(&dev->list_buf_used,
+				       struct hdcapm_buffer, list);
+	}
+	mutex_unlock(&dev->dmaqueue_lock);
+
+	v4l2_dbg(3, hdcapm_debug, dev->sd, "%s() returns %p\n", __func__, buf);
+
+	return buf;
+}
+
+static struct hdcapm_buffer *hdcapm_buffer_next_used(struct hdcapm_dev *dev)
+{
+	struct hdcapm_buffer *buf = NULL;
+
+	mutex_lock(&dev->dmaqueue_lock);
+	if (!list_empty(&dev->list_buf_used)) {
+		buf = list_first_entry(&dev->list_buf_used,
+				       struct hdcapm_buffer, list);
+		list_del(&buf->list);
+	}
+	mutex_unlock(&dev->dmaqueue_lock);
+
+	v4l2_dbg(3, hdcapm_debug, dev->sd, "%s() returns %p\n", __func__, buf);
+
+	return buf;
+}
+
+/* Pull the top buffer from the free list, but don't specifically remove
+ * it from the list. If no buffer exists, steal one from the used list.
+ * We WILL take the mutex in this func. Return the buffer at the top of
+ * the free list, delete the list node. We're probably going to fill it
+ * and move it to the used list. IF no free buffers exist, steal one
+ * from the used list and flag an internal data loss statistic.
+ */
+struct hdcapm_buffer *hdcapm_buffer_next_free(struct hdcapm_dev *dev)
+{
+	struct hdcapm_buffer *buf = NULL;
+
+	mutex_lock(&dev->dmaqueue_lock);
+	if (!list_empty(&dev->list_buf_free)) {
+		buf = list_first_entry(&dev->list_buf_free,
+				       struct hdcapm_buffer, list);
+		list_del(&buf->list);
+	}
+	mutex_unlock(&dev->dmaqueue_lock);
+
+	if (!buf) {
+		v4l2_err(dev->sd,
+			 "%s() No empty buffers. Increase buffer_count.\n",
+			 __func__);
+		buf = hdcapm_buffer_next_used(dev);
+		if (!buf)
+			v4l2_err(dev->sd,
+				 "%s() No free or empty buffers.\n", __func__);
+		dev->stats->buffer_overrun++;
+	}
+
+	v4l2_dbg(3, hdcapm_debug, dev->sd, "%s() returns %p\n", __func__, buf);
+
+	return buf;
+}
+
+static void hdcapm_buffer_add_to_list(struct hdcapm_dev *dev,
+				      struct hdcapm_buffer *buf,
+				      struct list_head *list)
+{
+	mutex_lock(&dev->dmaqueue_lock);
+	list_add_tail(&buf->list, list);
+	mutex_unlock(&dev->dmaqueue_lock);
+}
+
+inline void hdcapm_buffer_add_to_free(struct hdcapm_dev *dev,
+				      struct hdcapm_buffer *buf)
+{
+	hdcapm_buffer_add_to_list(dev, buf, &dev->list_buf_free);
+}
+
+inline void hdcapm_buffer_add_to_used(struct hdcapm_dev *dev,
+				      struct hdcapm_buffer *buf)
+{
+	hdcapm_buffer_add_to_list(dev, buf, &dev->list_buf_used);
+}
+
+static inline void hdcapm_buffer_move(struct hdcapm_dev *dev,
+				      struct hdcapm_buffer *buf,
+				      struct list_head *list)
+{
+	mutex_lock(&dev->dmaqueue_lock);
+	list_move_tail(&buf->list, list);
+	mutex_unlock(&dev->dmaqueue_lock);
+}
+
+/* Helper macros for moving a buffer to the free list.
+ * We WILL take the mutex in this func.
+ */
+void hdcapm_buffer_move_to_free(struct hdcapm_dev *dev,
+				struct hdcapm_buffer *buf)
+{
+	hdcapm_buffer_move(dev, buf, &dev->list_buf_free);
+}
+
+/* Helper macros for moving a buffer to the used list.
+ * We WILL take the mutex in this func.
+ */
+void hdcapm_buffer_move_to_used(struct hdcapm_dev *dev,
+				struct hdcapm_buffer *buf)
+{
+	hdcapm_buffer_move(dev, buf, &dev->list_buf_used);
+}
+
+/* For debugging. Lock the buffer queue and measure how much data (in bytes)
+ * and how many items are on the list.
+ */
+int hdcapm_buffer_used_queue_stats(struct hdcapm_dev *dev,
+				   u64 *bytes, u64 *items)
+{
+	struct hdcapm_buffer *buf = NULL;
+	struct list_head *p = NULL, *q = NULL;
+
+	*bytes = 0;
+	*items = 0;
+
+	mutex_lock(&dev->dmaqueue_lock);
+	list_for_each_safe(p, q, &dev->list_buf_used) {
+		buf = list_entry(p, struct hdcapm_buffer, list);
+		(*bytes) += (buf->actual_size - buf->readpos);
+		(*items)++;
+	}
+	mutex_unlock(&dev->dmaqueue_lock);
+
+	return 0;
+}
---
+++ drivers/media/usb/hdcapm/hdcapm-video.c
@@ -0,0 +1,665 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Driver for the Startech USB2HDCAPM USB capture device
+ *
+ * Copyright (c) 2017 Steven Toth <stoth@kernellabs.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * GNU General Public License for more details.
+ */
+
+#include "hdcapm.h"
+
+#define ENCODER_MIN_BITRATE  2000000
+#define ENCODER_MAX_BITRATE 20000000
+#define ENCODER_DEF_BITRATE ENCODER_MAX_BITRATE
+
+#define ENCODER_MIN_GOP_SIZE  1
+#define ENCODER_MAX_GOP_SIZE 60
+#define ENCODER_DEF_GOP_SIZE ENCODER_MAX_GOP_SIZE
+
+static int s_ctrl(struct v4l2_ctrl *ctrl)
+{
+	struct hdcapm_dev *dev = container_of(ctrl->handler,
+					      struct hdcapm_dev, ctrl_handler);
+	struct hdcapm_encoder_parameters *p = &dev->encoder_parameters;
+	int ret = 0;
+
+	switch (ctrl->id) {
+	case V4L2_CID_MPEG_AUDIO_MUTE:
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_AUDIO_MUTE) = %d\n",
+			 __func__, ctrl->val);
+		p->audio_mute = ctrl->val;
+		break;
+	case V4L2_CID_BRIGHTNESS:
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_BRIGHTNESS) = %d\n", __func__, ctrl->val);
+		p->brightness = ctrl->val;
+		break;
+	case V4L2_CID_MPEG_VIDEO_BITRATE:
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_VIDEO_BITRATE) = %d\n",
+			 __func__, ctrl->val);
+		p->bitrate_bps = ctrl->val;
+		break;
+	case V4L2_CID_MPEG_VIDEO_BITRATE_PEAK:
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_BITRATE_PEAK) = %d\n",
+			 __func__, ctrl->val);
+		p->bitrate_peak_bps = ctrl->val;
+		break;
+	case V4L2_CID_MPEG_VIDEO_BITRATE_MODE:
+		switch (ctrl->val) {
+		case V4L2_MPEG_VIDEO_BITRATE_MODE_VBR:
+			p->h264_mode = 1;
+			break;
+		case V4L2_MPEG_VIDEO_BITRATE_MODE_CBR:
+			p->h264_mode = 0;
+			break;
+		default:
+			v4l2_err(dev->sd,
+				 "failed to handle ctrl->id 0x%x, value = %d\n",
+				 ctrl->id, ctrl->val);
+			ret = -EINVAL;
+		}
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_VIDEO_BITRATE_MODE) = %d\n",
+			 __func__, ctrl->val);
+		break;
+	case V4L2_CID_MPEG_VIDEO_GOP_SIZE:
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_VIDEO_GOP_SIZE) = %d\n",
+			 __func__, ctrl->val);
+		p->gop_size = ctrl->val;
+
+		/* If we're in VBR mode GOP 1 looks bad,
+		 * force a change to CBR.
+		 */
+		if (p->gop_size == 1 && p->h264_mode == 1) {
+			v4l2_info(dev->sd,
+				  "GOP size 1 produces poor quality, switching from VBR to CBR\n");
+			p->h264_mode = 0;
+		}
+		break;
+	case V4L2_CID_MPEG_VIDEO_H264_LEVEL:
+		if (ctrl->val > V4L2_MPEG_VIDEO_H264_LEVEL_5_1) {
+			v4l2_err(dev->sd,
+				 "failed to handle ctrl->id 0x%x, value = %d\n",
+				 ctrl->id, ctrl->val);
+			ret = -EINVAL;
+		}
+		p->h264_level = ctrl->val;
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_VIDEO_H264_LEVEL) = %d\n",
+			 __func__, ctrl->val);
+		break;
+	case V4L2_CID_MPEG_VIDEO_H264_ENTROPY_MODE:
+		switch (ctrl->val) {
+		case V4L2_MPEG_VIDEO_H264_ENTROPY_MODE_CABAC:
+			p->h264_entropy_mode = 1;
+			break;
+		case V4L2_MPEG_VIDEO_H264_ENTROPY_MODE_CAVLC:
+			p->h264_entropy_mode = 0;
+			break;
+		default:
+			v4l2_err(dev->sd,
+				 "failed to handle ctrl->id 0x%x, value = %d\n",
+				 ctrl->id, ctrl->val);
+			ret = -EINVAL;
+		}
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_VIDEO_H264_ENTROPY_MODE) = %d\n",
+			 __func__, ctrl->val);
+		break;
+	case V4L2_CID_MPEG_VIDEO_H264_PROFILE:
+		switch (ctrl->val) {
+		case V4L2_MPEG_VIDEO_H264_PROFILE_BASELINE:
+			p->h264_profile = 0;
+			break;
+		case V4L2_MPEG_VIDEO_H264_PROFILE_MAIN:
+			p->h264_profile = 1;
+			break;
+		case V4L2_MPEG_VIDEO_H264_PROFILE_HIGH:
+			p->h264_profile = 2;
+			break;
+		default:
+			v4l2_err(dev->sd,
+				 "failed to handle ctrl->id 0x%x, value = %d\n",
+				 ctrl->id, ctrl->val);
+			ret = -EINVAL;
+		}
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_VIDEO_H264_PROFILE) = %d\n",
+			 __func__, ctrl->val);
+		break;
+	case V4L2_CID_MPEG_STREAM_TYPE:
+		v4l2_dbg(1, hdcapm_debug, dev->sd,
+			 "%s(V4L2_CID_MPEG_STREAM_TYPE) = %d\n",
+			 __func__, ctrl->val);
+		break;
+	default:
+		v4l2_err(dev->sd,
+			 "failed to handle ctrl->id 0x%x, value = %d\n",
+			 ctrl->id, ctrl->val);
+		ret = -EINVAL;
+	}
+
+	return ret;
+}
+
+static const struct v4l2_ctrl_ops ctrl_ops = {
+	.s_ctrl = s_ctrl,
+};
+
+static int vidioc_enum_input(struct file *file, void *priv_fh,
+			     struct v4l2_input *i)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+
+	if (i->index > 0)
+		return -EINVAL;
+
+	snprintf(i->name, sizeof(i->name), "HDMI / DVI");
+	i->type = V4L2_INPUT_TYPE_CAMERA;
+	i->capabilities = V4L2_IN_CAP_DV_TIMINGS;
+
+	return v4l2_subdev_call(dev->sd, video, g_input_status, &i->status);
+}
+
+static int vidioc_querycap(struct file *file, void *priv,
+			   struct v4l2_capability *cap)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+
+	strcpy(cap->driver, KBUILD_MODNAME);
+	strlcpy(cap->card, dev->name, sizeof(cap->card));
+	usb_make_path(dev->udev, cap->bus_info, sizeof(cap->bus_info));
+
+	cap->device_caps = V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_READWRITE;
+	cap->capabilities = cap->device_caps | V4L2_CAP_DEVICE_CAPS;
+
+	return 0;
+}
+
+static int vidioc_log_status(struct file *file, void *priv)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+	struct hdcapm_statistics *s = dev->stats;
+	u64 q_used_bytes, q_used_items;
+	struct hdcapm_encoder_parameters *p = &dev->encoder_parameters;
+
+	v4l2_info(dev->sd, "device_state:           %s\n",
+		  dev->state == STATE_START ? "START" :
+		  dev->state == STATE_STARTED ? "STARTED" :
+		  dev->state == STATE_STOP ? "STOP" :
+		  dev->state == STATE_STOPPED ? "STOPPED" : "UNDEFINED");
+
+	v4l2_info(dev->sd, "device_context:         0x%p\n", dev);
+	v4l2_info(dev->sd, "codec_buffers_received: %llu\n",
+		  s->codec_buffers_received);
+	v4l2_info(dev->sd, "codec_bytes_received:   %llu\n",
+		  s->codec_bytes_received);
+	v4l2_info(dev->sd, "codec_ts_not_yet_ready: %llu\n",
+		  s->codec_ts_not_yet_ready);
+	v4l2_info(dev->sd, "buffer_overrun:         %llu\n", s->buffer_overrun);
+
+	if (p->output_width && p->output_height)
+		v4l2_info(dev->sd, "video_scaler_output:    %dx%d\n",
+			  p->output_width, p->output_height);
+	else
+		v4l2_info(dev->sd, "video_scaler_output:    [native 1:1]\n");
+
+	if (hdcapm_buffer_used_queue_stats(dev,
+					   &q_used_bytes, &q_used_items) == 0) {
+		v4l2_info(dev->sd, "q_used_bytes:           %llu\n",
+			  q_used_bytes);
+		v4l2_info(dev->sd, "q_used_items:           %llu\n",
+			  q_used_items);
+	}
+
+	return v4l2_subdev_call(dev->sd, core, log_status);
+}
+
+static int vidioc_g_input(struct file *file, void *priv, unsigned int *i)
+{
+	*i = 0;
+
+	return 0;
+}
+
+static int vidioc_s_input(struct file *file, void *priv, unsigned int i)
+{
+	if (i > 0)
+		return -EINVAL;
+
+	return 0;
+}
+
+static int vidioc_g_dv_timings(struct file *file, void *priv,
+			       struct v4l2_dv_timings *timings)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+
+	return v4l2_subdev_call(dev->sd, video, g_dv_timings, timings);
+}
+
+static int vidioc_enum_dv_timings(struct file *file, void *priv,
+				  struct v4l2_enum_dv_timings *timings)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+
+	timings->pad = 0;
+	timings->reserved[0] = 0;
+	timings->reserved[1] = 0;
+
+	return v4l2_subdev_call(dev->sd, pad, enum_dv_timings, timings);
+}
+
+static int vidioc_dv_timings_cap(struct file *file, void *priv,
+				 struct v4l2_dv_timings_cap *cap)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+
+	cap->pad = 0;
+
+	return v4l2_subdev_call(dev->sd, pad, dv_timings_cap, cap);
+}
+
+static int vidioc_enum_fmt_vid_cap(struct file *file, void *priv,
+				   struct v4l2_fmtdesc *f)
+{
+	if (f->index != 0)
+		return -EINVAL;
+
+	strlcpy(f->description, "MPEG", sizeof(f->description));
+	f->pixelformat = V4L2_PIX_FMT_MPEG;
+	f->flags = V4L2_FMT_FLAG_COMPRESSED;
+	return 0;
+}
+
+static int vidioc_g_fmt_vid_cap(struct file *file, void *priv,
+				struct v4l2_format *f)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+	struct hdcapm_encoder_parameters *p = &dev->encoder_parameters;
+	struct v4l2_dv_timings timings;
+
+	if (v4l2_subdev_call(dev->sd, video, g_dv_timings, &timings) < 0)
+		return -EINVAL;
+
+	f->fmt.pix.pixelformat    = V4L2_PIX_FMT_MPEG;
+	f->fmt.pix.bytesperline   = 0;
+	f->fmt.pix.sizeimage      = 188 * 312;
+	f->fmt.pix.colorspace     = V4L2_COLORSPACE_SMPTE170M;
+
+	if (p->output_width)
+		f->fmt.pix.width  = p->output_width;
+	else
+		f->fmt.pix.width  = timings.bt.width;
+
+	if (p->output_height)
+		f->fmt.pix.height = p->output_height;
+	else
+		f->fmt.pix.height = timings.bt.width;
+
+	f->fmt.pix.height         = timings.bt.height;
+	f->fmt.pix.field          = V4L2_FIELD_NONE;
+
+	return 0;
+}
+
+static int vidioc_s_fmt_vid_cap(struct file *file, void *priv,
+				struct v4l2_format *f)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+	struct hdcapm_encoder_parameters *p = &dev->encoder_parameters;
+	struct v4l2_dv_timings timings;
+
+	if (v4l2_subdev_call(dev->sd, video, g_dv_timings, &timings) < 0)
+		return -EINVAL;
+
+	/* Its not clear to me if the input resolution changes, if we're
+	 * required to preserve the users requested width and height, or
+	 * default it back to 1:1 with the input signal.
+	 */
+	p->output_width = f->fmt.pix.width;
+	p->output_height = f->fmt.pix.height;
+
+	f->fmt.pix.pixelformat  = V4L2_PIX_FMT_MPEG;
+	f->fmt.pix.bytesperline = 0;
+	f->fmt.pix.sizeimage    = 188 * 312;
+	f->fmt.pix.colorspace   = V4L2_COLORSPACE_SMPTE170M;
+	f->fmt.pix.width        = timings.bt.width;
+	f->fmt.pix.height       = timings.bt.height;
+	f->fmt.pix.field        = V4L2_FIELD_NONE;
+
+	return 0;
+}
+
+static int vidioc_try_fmt_vid_cap(struct file *file, void *priv,
+				  struct v4l2_format *f)
+{
+	return vidioc_s_fmt_vid_cap(file, priv, f);
+}
+
+static int vidioc_subscribe_event(struct v4l2_fh *fh,
+				  const struct v4l2_event_subscription *sub)
+{
+	switch (sub->type) {
+	case V4L2_EVENT_SOURCE_CHANGE:
+		return v4l2_event_subscribe(fh, sub, 16, NULL);
+	default:
+		v4l2_warn(fh->vdev->v4l2_dev,
+			  "event sub->type = 0x%x (UNKNOWN)\n", sub->type);
+	}
+	return v4l2_ctrl_subscribe_event(fh, sub);
+}
+
+static int vidioc_query_dv_timings(struct file *file,
+				   void *priv_fh,
+				   struct v4l2_dv_timings *timings)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+
+	return v4l2_subdev_call(dev->sd, video, query_dv_timings, timings);
+}
+
+static const struct v4l2_ioctl_ops mpeg_ioctl_ops = {
+	.vidioc_enum_input        = vidioc_enum_input,
+	.vidioc_querycap          = vidioc_querycap,
+	.vidioc_log_status        = vidioc_log_status,
+	.vidioc_g_input           = vidioc_g_input,
+	.vidioc_s_input           = vidioc_s_input,
+	.vidioc_g_dv_timings      = vidioc_g_dv_timings,
+	.vidioc_query_dv_timings  = vidioc_query_dv_timings,
+	.vidioc_enum_dv_timings   = vidioc_enum_dv_timings,
+	.vidioc_dv_timings_cap    = vidioc_dv_timings_cap,
+	.vidioc_enum_fmt_vid_cap  = vidioc_enum_fmt_vid_cap,
+	.vidioc_g_fmt_vid_cap     = vidioc_g_fmt_vid_cap,
+	.vidioc_s_fmt_vid_cap     = vidioc_s_fmt_vid_cap,
+	.vidioc_try_fmt_vid_cap   = vidioc_try_fmt_vid_cap,
+	.vidioc_subscribe_event   = vidioc_subscribe_event,
+	.vidioc_unsubscribe_event = v4l2_event_unsubscribe,
+};
+
+static int fops_open(struct file *file)
+{
+	struct hdcapm_dev *dev;
+	struct hdcapm_fh *fh;
+
+	dev = (struct hdcapm_dev *)video_get_drvdata(video_devdata(file));
+	if (!dev)
+		return -ENODEV;
+
+	v4l2_dbg(2, hdcapm_debug, dev->sd, "%s()\n", __func__);
+
+	/* allocate + initialize per filehandle data */
+	fh = kzalloc(sizeof(*fh), GFP_KERNEL);
+	if (!fh)
+		return -ENOMEM;
+
+	fh->dev = dev;
+	v4l2_fh_init(&fh->fh, video_devdata(file));
+	file->private_data = &fh->fh;
+	v4l2_fh_add(&fh->fh);
+
+	return 0;
+}
+
+static int fops_release(struct file *file)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+
+	v4l2_dbg(2, hdcapm_debug, dev->sd, "%s()\n", __func__);
+
+	/* Shut device down on last close */
+	if ((atomic_cmpxchg(&fh->v4l_reading, 1, 0) == 1) &&
+	    (atomic_dec_return(&dev->v4l_reader_count) == 0))
+		/* stop mpeg capture then cancel buffers */
+		hdcapm_core_stop_streaming(dev);
+
+	v4l2_fh_del(&fh->fh);
+	v4l2_fh_exit(&fh->fh);
+	kfree(fh);
+
+	return 0;
+}
+
+static ssize_t fops_read(struct file *file, char __user *buffer,
+			 size_t count, loff_t *pos)
+{
+	struct hdcapm_fh *fh = file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+	struct hdcapm_buffer *ubuf = NULL;
+	int ret = 0;
+	int rem, cnt;
+	u8 *p;
+
+	if (*pos) {
+		v4l2_err(dev->sd, "%s() ESPIPE\n", __func__);
+		return -ESPIPE;
+	}
+
+	if ((atomic_cmpxchg(&fh->v4l_reading, 0, 1) == 0) &&
+	    (atomic_inc_return(&dev->v4l_reader_count) == 1))
+		hdcapm_core_start_streaming(dev);
+
+	/* blocking wait for buffer */
+	if ((file->f_flags & O_NONBLOCK) == 0) {
+		if (wait_event_interruptible(dev->wait_read,
+					     hdcapm_buffer_peek_used(dev))) {
+			v4l2_err(dev->sd,
+				 "%s() ERESTARTSYS\n", __func__);
+			//return -ERESTARTSYS;
+			return -EINVAL;
+		}
+	}
+
+	/* Pull the first buffer from the used list */
+	ubuf = hdcapm_buffer_peek_used(dev);
+
+	while ((count > 0) && ubuf) {
+		/* set remaining bytes to copy */
+		rem = ubuf->actual_size - ubuf->readpos;
+		cnt = rem > count ? count : rem;
+
+		p = ubuf->ptr + ubuf->readpos;
+
+		v4l2_dbg(3, hdcapm_debug, dev->sd,
+			 "%s() nr=%d count=%d cnt=%d rem=%d buf=%p buf->readpos=%d\n",
+			 __func__, ubuf->nr, (int)count,
+			 cnt, rem, ubuf, ubuf->readpos);
+
+		if (copy_to_user(buffer, p, cnt)) {
+			v4l2_err(dev->sd,
+				 "%s() copy_to_user failed\n", __func__);
+			if (!ret) {
+				v4l2_err(dev->sd, "%s() EFAULT\n", __func__);
+				ret = -EFAULT;
+			}
+			goto err;
+		}
+
+		ubuf->readpos += cnt;
+		count -= cnt;
+		buffer += cnt;
+		ret += cnt;
+
+		if (ubuf->readpos > ubuf->actual_size)
+			v4l2_err(dev->sd, "read() pos > actual, huh?\n");
+
+		if (ubuf->readpos == ubuf->actual_size) {
+			/* finished with current buffer, take next buffer */
+
+			/* Requeue the buffer on the free list */
+			ubuf->readpos = 0;
+
+			hdcapm_buffer_move_to_free(dev, ubuf);
+
+			/* Dequeue next */
+			if ((file->f_flags & O_NONBLOCK) == 0) {
+				if (wait_event_interruptible(dev->wait_read,
+						hdcapm_buffer_peek_used(dev)))
+					break;
+			}
+			ubuf = hdcapm_buffer_peek_used(dev);
+		}
+	}
+err:
+	if (!ret && !ubuf)
+		ret = -EAGAIN;
+
+	return ret;
+}
+
+static unsigned int fops_poll(struct file *file, poll_table *wait)
+{
+	unsigned long req_events = poll_requested_events(wait);
+	struct hdcapm_fh *fh = (struct hdcapm_fh *)file->private_data;
+	struct hdcapm_dev *dev = fh->dev;
+	unsigned int mask = v4l2_ctrl_poll(file, wait);
+
+	if (!(req_events & (POLLIN | POLLRDNORM)))
+		return mask;
+
+	if (atomic_cmpxchg(&fh->v4l_reading, 0, 1) == 0) {
+		if (atomic_inc_return(&dev->v4l_reader_count) == 1)
+			hdcapm_core_start_streaming(dev);
+	}
+
+	/* Pull the first buffer from the used list */
+	if (!list_empty(&dev->list_buf_used))
+		mask |= POLLIN | POLLRDNORM;
+
+	return mask;
+}
+
+static const struct v4l2_file_operations mpeg_fops = {
+	.owner          = THIS_MODULE,
+	.open           = fops_open,
+	.release        = fops_release,
+	.read           = fops_read,
+	.poll           = fops_poll,
+	.unlocked_ioctl = video_ioctl2,
+};
+
+static struct video_device mpeg_template = {
+	.name          = "hdcapm",
+	.fops          = &mpeg_fops,
+	.ioctl_ops     = &mpeg_ioctl_ops,
+	.minor         = -1,
+};
+
+int hdcapm_video_register(struct hdcapm_dev *dev)
+{
+	struct v4l2_ctrl_handler *hdl = &dev->ctrl_handler;
+	int ret;
+
+	v4l2_dbg(1, hdcapm_debug, dev->sd, "%s()\n", __func__);
+
+	/* Any video controls. */
+
+	dev->v4l_device = video_device_alloc();
+	if (!dev->v4l_device)
+		return -EINVAL;
+
+	/* Configure the V4L2 device properties */
+	*dev->v4l_device = mpeg_template;
+	snprintf(dev->v4l_device->name, sizeof(dev->v4l_device->name),
+		 "%s %s (%s)", dev->name, "mpeg", dev->name);
+	dev->v4l_device->v4l2_dev = &dev->v4l2_dev;
+	dev->v4l_device->release = video_device_release;
+
+	v4l2_ctrl_handler_init(hdl, 14);
+	dev->v4l_device->ctrl_handler = hdl;
+
+	v4l2_ctrl_new_std(hdl, &ctrl_ops, V4L2_CID_MPEG_AUDIO_MUTE, 0, 1, 1, 0);
+	v4l2_ctrl_new_std(hdl, &ctrl_ops, V4L2_CID_BRIGHTNESS, 0, 255, 1, 127);
+	v4l2_ctrl_new_std(hdl, &ctrl_ops, V4L2_CID_MPEG_VIDEO_GOP_SIZE,
+			  ENCODER_MIN_GOP_SIZE,
+			  ENCODER_MAX_GOP_SIZE, 1, ENCODER_DEF_GOP_SIZE);
+	v4l2_ctrl_new_std(hdl, &ctrl_ops, V4L2_CID_MPEG_VIDEO_BITRATE,
+			  ENCODER_MIN_BITRATE,
+			  ENCODER_MAX_BITRATE, 100000, ENCODER_DEF_BITRATE);
+	v4l2_ctrl_new_std(hdl, &ctrl_ops, V4L2_CID_MPEG_VIDEO_BITRATE_PEAK,
+			  ENCODER_MIN_BITRATE,
+			  ENCODER_MAX_BITRATE, 100000, ENCODER_DEF_BITRATE);
+
+	v4l2_ctrl_new_std_menu(hdl, &ctrl_ops,
+			       V4L2_CID_MPEG_VIDEO_BITRATE_MODE,
+			       V4L2_MPEG_VIDEO_BITRATE_MODE_CBR, 0,
+			       V4L2_MPEG_VIDEO_BITRATE_MODE_VBR);
+
+	v4l2_ctrl_new_std_menu(hdl, &ctrl_ops, V4L2_CID_MPEG_VIDEO_H264_LEVEL,
+			       V4L2_MPEG_VIDEO_H264_LEVEL_5_1,
+			       0, V4L2_MPEG_VIDEO_H264_LEVEL_4_0);
+
+	v4l2_ctrl_new_std_menu(hdl, &ctrl_ops,
+			       V4L2_CID_MPEG_VIDEO_H264_ENTROPY_MODE,
+			       V4L2_MPEG_VIDEO_H264_ENTROPY_MODE_CABAC,
+			       0, V4L2_MPEG_VIDEO_H264_ENTROPY_MODE_CAVLC);
+
+	v4l2_ctrl_new_std_menu(hdl, &ctrl_ops,
+			       V4L2_CID_MPEG_VIDEO_H264_PROFILE,
+			       V4L2_MPEG_VIDEO_H264_PROFILE_HIGH,
+			       ~((1 << V4L2_MPEG_VIDEO_H264_PROFILE_BASELINE) |
+				 (1 << V4L2_MPEG_VIDEO_H264_PROFILE_MAIN) |
+				 (1 << V4L2_MPEG_VIDEO_H264_PROFILE_HIGH)),
+			       V4L2_MPEG_VIDEO_H264_PROFILE_HIGH);
+
+	v4l2_ctrl_new_std_menu(hdl, &ctrl_ops,
+			       V4L2_CID_MPEG_STREAM_TYPE,
+			       V4L2_MPEG_STREAM_TYPE_MPEG2_TS,
+			       ~(1 << V4L2_MPEG_STREAM_TYPE_MPEG2_TS),
+			       V4L2_MPEG_STREAM_TYPE_MPEG2_TS);
+
+	/* Establish all default control values. */
+	v4l2_ctrl_handler_setup(hdl);
+
+	video_set_drvdata(dev->v4l_device, dev);
+	ret = video_register_device(dev->v4l_device, VFL_TYPE_GRABBER, -1);
+	if (ret < 0)
+		goto fail1;
+
+	v4l2_info(dev->sd,
+		  "registered device video%d [mpeg]\n", dev->v4l_device->num);
+
+	ret = 0; /* Success */
+
+fail1:
+	return ret;
+}
+
+void hdcapm_video_unregister(struct hdcapm_dev *dev)
+{
+	v4l2_dbg(1, hdcapm_debug, dev->sd, "%s()\n", __func__);
+
+	if (dev->v4l_device) {
+		if (dev->v4l_device->minor != -1)
+			video_unregister_device(dev->v4l_device);
+		else
+			video_device_release(dev->v4l_device);
+
+		dev->v4l_device = NULL;
+	}
+	v4l2_ctrl_handler_free(&dev->ctrl_handler);
+}
