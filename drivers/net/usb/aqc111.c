ea99ca7b1a84e4c4e3f45dd5c1e9c61ac292d56f
Nathan Chancellor <natechancellor@gmail.com>
2018-11-28 22:18:09 -0700

    net: usb: aqc111: Properly initialize wol_cfg in aqc111_suspend
    Clang warns:
    
    drivers/net/usb/aqc111.c:1326:37: warning: suggest braces around
    initialization of subobject [-Wmissing-braces]
                    struct aqc111_wol_cfg wol_cfg = { 0 };
                                                      ^
                                                      {}
    1 warning generated.
    
    Add another set of braces to initialize the char subobjects as well.
    
    Fixes: e58ba4544c77 ("net: usb: aqc111: Add support for wake on LAN by MAGIC packet")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/net/usb/aqc111.c
+++ drivers/net/usb/aqc111.c
@@ -1323,7 +1323,7 @@ static int aqc111_suspend(struct usb_interface *intf, pm_message_t message)
 			      1, 1, &reg8);
 
 	if (aqc111_data->wol_flags) {
-		struct aqc111_wol_cfg wol_cfg = { 0 };
+		struct aqc111_wol_cfg wol_cfg = { { 0 } };
 
 		aqc111_data->phy_cfg |= AQ_WOL;
 		ether_addr_copy(wol_cfg.hw_addr, dev->net->dev_addr);
