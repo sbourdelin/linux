7ba098be979711fb3f2bd1d3889f98b07feb135c
Matteo Croce <mcroce@redhat.com>
2018-12-04 18:05:42 +0100

    macvlan: remove duplicate check
    Following commit 59f997b088d2 ("macvlan: return correct error value"),
    there is a duplicate check for mac addresses both in macvlan_sync_address()
    and macvlan_set_mac_address().
    As the former calls the latter, remove the one in macvlan_set_mac_address()
    and move the one in macvlan_sync_address() before any other check.
    
    Signed-off-by: Matteo Croce <mcroce@redhat.com>

--- drivers/net/macvlan.c
+++ drivers/net/macvlan.c
@@ -700,14 +700,14 @@ static int macvlan_sync_address(struct net_device *dev, unsigned char *addr)
 	struct macvlan_port *port = vlan->port;
 	int err;
 
+	if (macvlan_addr_busy(vlan->port, addr))
+		return -EADDRINUSE;
+
 	if (!(dev->flags & IFF_UP)) {
 		/* Just copy in the new address */
 		ether_addr_copy(dev->dev_addr, addr);
 	} else {
 		/* Rehash and update the device filters */
-		if (macvlan_addr_busy(vlan->port, addr))
-			return -EADDRINUSE;
-
 		if (!macvlan_passthru(port)) {
 			err = dev_uc_add(lowerdev, addr);
 			if (err)
@@ -746,9 +746,6 @@ static int macvlan_set_mac_address(struct net_device *dev, void *p)
 		macvlan_set_addr_change(vlan->port);
 		return dev_set_mac_address(vlan->lowerdev, addr);
 	}
-
-	if (macvlan_addr_busy(vlan->port, addr->sa_data))
-		return -EADDRINUSE;
 
 	return macvlan_sync_address(dev, addr->sa_data);
 }
