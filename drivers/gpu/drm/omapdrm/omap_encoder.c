52971ac182b33fbb38aafadcb97e11000b001209
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-12-05 17:00:01 +0200

    drm/omap: Remove enable checks from display .enable() and .remove()
    The displays (connectors, panels and encoders) bail out from their
    .enable() and .disable() handlers if the dss device is already enabled
    or disabled. Those safety checks are not needed when the functions are
    called through the omapdss_device_ops, as the .enable() and .disable()
    handlers are called from the DRM atomic helpers that already guarantee
    that no double enabling or disabling can occur.
    
    However, the handlers are also called directly from the .remove()
    handler. While this shouldn't be needed either as the modules can't be
    removed as long as the device is in use, it's still a good practice to
    disable the device explicitly. There is currently a safety check in
    .remove() in some drivers but not all of them.
    
    Remove the safety checks from the .enable() and .disable() handlers, and
    add missing ones in the .remove() handler.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/gpu/drm/omapdrm/omap_encoder.c
+++ drivers/gpu/drm/omapdrm/omap_encoder.c
@@ -143,9 +143,6 @@ static void omap_encoder_disable(struct drm_encoder *encoder)
 
 	dev_dbg(dev->dev, "disable(%s)\n", dssdev->name);
 
-	if (!omapdss_device_is_enabled(dssdev))
-		return;
-
 	dssdev->ops->disable(dssdev);
 
 	dssdev->state = OMAP_DSS_DISPLAY_DISABLED;
@@ -159,9 +156,6 @@ static void omap_encoder_enable(struct drm_encoder *encoder)
 	int r;
 
 	dev_dbg(dev->dev, "enable(%s)\n", dssdev->name);
-
-	if (omapdss_device_is_enabled(dssdev))
-		return;
 
 	r = dssdev->ops->enable(dssdev);
 	if (r) {
