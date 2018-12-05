7c3279c385e04dfd4856a3bb35c5aff9af18c49e
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-12-05 17:00:07 +0200

    drm/omap: Add a dss device operation flag for .get_modes()
    Instead of manually iterating over the dss devices in the pipeline to
    find the first one that implements the .get_modes() operation, add a new
    operation flag for .get_modes() and use the omap_connector_find_device()
    helper function to locate the right dss device.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/gpu/drm/omapdrm/displays/panel-dpi.c
+++ drivers/gpu/drm/omapdrm/displays/panel-dpi.c
@@ -152,6 +152,7 @@ static int panel_dpi_probe(struct platform_device *pdev)
 	dssdev->type = OMAP_DISPLAY_TYPE_DPI;
 	dssdev->owner = THIS_MODULE;
 	dssdev->of_ports = BIT(0);
+	dssdev->ops_flags = OMAP_DSS_DEVICE_OP_MODES;
 
 	omapdss_display_init(dssdev);
 	omapdss_device_register(dssdev);
