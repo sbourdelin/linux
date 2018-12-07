c53cefa50b2c5f49c16496f2982fd58be665952a
Pingfan Liu <kernelfans@gmail.com>
2018-12-07 12:53:08 +0800

    mm/pageblock: throw compiling time error if pageblock_bits can not hold MIGRATE_TYPES
    Currently, NR_PAGEBLOCK_BITS and MIGRATE_TYPES are not associated by code.
    If someone adds extra migrate type, then he may forget to enlarge the
    NR_PAGEBLOCK_BITS. Hence it requires some way to fix.
    NR_PAGEBLOCK_BITS depends on MIGRATE_TYPES, while these macro
    spread on two different .h file with reverse dependency, it is a little
    hard to refer to MIGRATE_TYPES in pageblock-flag.h. This patch tries to
    remind such relation in compiling-time.
    
    Signed-off-by: Pingfan Liu <kernelfans@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Pavel Tatashin <pavel.tatashin@microsoft.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Alexander Duyck <alexander.h.duyck@linux.intel.com>

--- include/linux/pageblock-flags.h
+++ include/linux/pageblock-flags.h
@@ -25,11 +25,12 @@
 
 #include <linux/types.h>
 
+#define PB_migratetype_bits 3
 /* Bit indices that affect a whole block of pages */
 enum pageblock_bits {
 	PB_migrate,
-	PB_migrate_end = PB_migrate + 3 - 1,
-			/* 3 bits required for migrate types */
+	PB_migrate_end = PB_migrate + PB_migratetype_bits - 1,
+			/* n bits required for migrate types */
 	PB_migrate_skip,/* If set the block is skipped by compaction */
 
 	/*
