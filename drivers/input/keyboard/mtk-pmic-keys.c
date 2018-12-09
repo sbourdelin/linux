4d9e837e777437042a481278aa8d9be89d86dda5
YueHaibing <yuehaibing@huawei.com>
2018-12-09 14:34:40 +0800

    input: keyboard: remove duplicated include from mtk-pmic-keys.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>

--- drivers/input/keyboard/mtk-pmic-keys.c
+++ drivers/input/keyboard/mtk-pmic-keys.c
@@ -19,7 +19,6 @@
 #include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
-#include <linux/kernel.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/regmap.h>
