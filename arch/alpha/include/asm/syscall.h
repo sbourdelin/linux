4b349c4e3e5dd277c5ef1951bd2a1a8270b83132
Dmitry V. Levin <ldv@altlinux.org>
2018-12-10 07:27:54 +0300

    alpha: define remaining syscall_get_* functions
    syscall_get_* functions are required to be implemented on all
    architectures in order to extend the generic ptrace API with
    PTRACE_GET_SYSCALL_INFO request.
    
    This adds remaining 4 syscall_get_* functions as documented
    in asm-generic/syscall.h: syscall_get_nr, syscall_get_arguments,
    syscall_get_error, and syscall_get_return_value.
    
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: Matt Turner <mattst88@gmail.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Elvira Khabirova <lineprinter@altlinux.org>
    Cc: Eugene Syromyatnikov <esyr@redhat.com>
    Cc: linux-alpha@vger.kernel.org
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>

--- arch/alpha/include/asm/syscall.h
+++ arch/alpha/include/asm/syscall.h
@@ -4,7 +4,34 @@
 
 #include <uapi/linux/audit.h>
 
-static inline int syscall_get_arch(void)
+static inline int
+syscall_get_nr(struct task_struct *task, struct pt_regs *regs)
+{
+	return regs->r0;
+}
+
+static inline void
+syscall_get_arguments(struct task_struct *task, struct pt_regs *regs,
+		      unsigned int i, unsigned int n, unsigned long *args)
+{
+	BUG_ON(i + n > 6);
+	memcpy(args, &regs->r16 + i, n * sizeof(args[0]));
+}
+
+static inline long
+syscall_get_error(struct task_struct *task, struct pt_regs *regs)
+{
+	return regs->r19 ? -regs->r0 : 0;
+}
+
+static inline long
+syscall_get_return_value(struct task_struct *task, struct pt_regs *regs)
+{
+	return regs->r0;
+}
+
+static inline int
+syscall_get_arch(void)
 {
 	return AUDIT_ARCH_ALPHA;
 }
