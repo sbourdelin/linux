8dd39f111e1006b8f47fbaf352d1e80523b7201f
Dmitry V. Levin <ldv@altlinux.org>
2018-12-10 07:30:21 +0300

    parisc: define syscall_get_error()
    syscall_get_error() is required to be implemented on all
    architectures in addition to already implemented syscall_get_nr(),
    syscall_get_arguments(), syscall_get_return_value(), and
    syscall_get_arch() functions in order to extend the generic
    ptrace API with PTRACE_GET_SYSCALL_INFO request.
    
    Cc: Helge Deller <deller@gmx.de>
    Cc: James E.J. Bottomley <jejb@parisc-linux.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Elvira Khabirova <lineprinter@altlinux.org>
    Cc: Eugene Syromyatnikov <esyr@redhat.com>
    Cc: linux-parisc@vger.kernel.org
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>

--- arch/parisc/include/asm/syscall.h
+++ arch/parisc/include/asm/syscall.h
@@ -43,6 +43,13 @@ static inline void syscall_get_arguments(struct task_struct *tsk,
 	}
 }
 
+static inline long syscall_get_error(struct task_struct *task,
+				     struct pt_regs *regs)
+{
+	unsigned long error = regs->gr[28];
+	return IS_ERR_VALUE(error) ? error : 0;
+}
+
 static inline long syscall_get_return_value(struct task_struct *task,
 						struct pt_regs *regs)
 {
