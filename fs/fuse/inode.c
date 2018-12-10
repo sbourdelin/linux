6dd24bd6cc468f679c802f72510b78b82568290c
Vivek Goyal <vgoyal@redhat.com>
2018-12-10 12:12:59 -0500

    fuse, dax: Take ->i_mmap_sem lock during dax page fault
    We need some kind of locking mechanism here. Normal file systems like
    ext4 and xfs seems to take their own semaphore to protect agains
    truncate while fault is going on.
    
    We have additional requirement to protect against fuse dax memory range
    reclaim. When a range has been selected for reclaim, we need to make sure
    no other read/write/fault can try to access that memory range while
    reclaim is in progress. Once reclaim is complete, lock will be released
    and read/write/fault will trigger allocation of fresh dax range.
    
    Taking inode_lock() is not an option in fault path as lockdep complains
    about circular dependencies. So define a new fuse_inode->i_mmap_sem.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -85,6 +85,7 @@ static struct inode *fuse_alloc_inode(struct super_block *sb)
 	fi->state = 0;
 	fi->nr_dmaps = 0;
 	mutex_init(&fi->mutex);
+	init_rwsem(&fi->i_mmap_sem);
 	init_rwsem(&fi->i_dmap_sem);
 	fi->forget = fuse_alloc_forget();
 	if (!fi->forget) {
