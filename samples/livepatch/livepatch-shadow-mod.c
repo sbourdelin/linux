8964c8b0cf0388db91b32c526c9980fd3fabd114
Nicholas Mc Guire <hofrat@osadl.org>
2018-12-13 12:09:50 +0100

    livepatch: handle kzalloc failure
    kzalloc() return should be checked. On dummy_alloc() failing
    in kzalloc() NULL should be returned.
    
    Signed-off-by: Nicholas Mc Guire <hofrat@osadl.org>

--- samples/livepatch/livepatch-shadow-mod.c
+++ samples/livepatch/livepatch-shadow-mod.c
@@ -118,6 +118,8 @@ noinline struct dummy *dummy_alloc(void)
 
 	/* Oops, forgot to save leak! */
 	leak = kzalloc(sizeof(int), GFP_KERNEL);
+	if (!leak)
+		return NULL;
 
 	pr_info("%s: dummy @ %p, expires @ %lx\n",
 		__func__, d, d->jiffies_expire);
