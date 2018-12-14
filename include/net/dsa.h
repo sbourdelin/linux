2e89d85215755d4c873f9a8c171584b29b2c336c
Frank Wunderlich <frank-w@public-files.de>
2018-12-14 17:48:40 +0100

    net: dsa: adding fields for holding information about upstream-port
    for multiple cpu-Ports aech port needs storing the the cpu-port to be used
    this Patch adds the needed fields for this
    
    based on
    https://github.com/openwrt/openwrt/blob/master/target/linux/mediatek/patches-4.14/0033-dsa-multi-cpu.patch
    
    Signed-off-by: Frank Wunderlich <frank-w@public-files.de>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -203,6 +203,10 @@ struct dsa_port {
 	struct net_device	*bridge_dev;
 	struct devlink_port	devlink_port;
 	struct phylink		*pl;
+
+	struct net_device	*ethernet;
+	int			upstream;
+
 	/*
 	 * Original copy of the master netdev ethtool_ops
 	 */
