c5ab33e6f234b42533aafd9c383fe63fb087e6f2
Thara Gopinath <thara.gopinath@linaro.org>
2018-12-14 15:22:26 +0100

    PM/runtime:Replace jiffies based accouting with ktime based accounting
    This patch replaces jiffies based accoutning for runtime_active_time
    and runtime_suspended_time with ktime base accounting. This makes the
    runtime debug counters inline with genpd and other pm subsytems which
    uses ktime based accounting.
    
    Signed-off-by: Thara Gopinath <thara.gopinath@linaro.org>
    [move from ktime to raw nsec]
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>

--- drivers/base/power/runtime.c
+++ drivers/base/power/runtime.c
@@ -66,8 +66,8 @@ static int rpm_suspend(struct device *dev, int rpmflags);
  */
 void update_pm_runtime_accounting(struct device *dev)
 {
-	unsigned long now = jiffies;
-	unsigned long delta;
+	u64 now = ktime_to_ns(ktime_get());
+	u64 delta;
 
 	delta = now - dev->power.accounting_timestamp;
 
@@ -77,9 +77,9 @@ void update_pm_runtime_accounting(struct device *dev)
 		return;
 
 	if (dev->power.runtime_status == RPM_SUSPENDED)
-		dev->power.suspended_jiffies += delta;
+		dev->power.suspended_time += delta;
 	else
-		dev->power.active_jiffies += delta;
+		dev->power.active_time += delta;
 }
 
 static void __update_runtime_status(struct device *dev, enum rpm_status status)
@@ -1491,7 +1491,7 @@ void pm_runtime_init(struct device *dev)
 	dev->power.request_pending = false;
 	dev->power.request = RPM_REQ_NONE;
 	dev->power.deferred_resume = false;
-	dev->power.accounting_timestamp = jiffies;
+	dev->power.accounting_timestamp = ktime_to_ns(ktime_get());
 	INIT_WORK(&dev->power.work, pm_runtime_work);
 
 	dev->power.timer_expires = 0;
