2c4f71954aa7efb02d4d1a75218b3a2f29ee2381
Filipe Manana <fdmanana@suse.com>
2018-12-14 19:44:59 +0000

    Btrfs: do not overwrite scrub error with fault error in scrub ioctl
    If scrub returned an error and then the copy_to_user() call did not
    succeed, we would overwrite the error returned by scrub with -EFAULT.
    Fix that by removing the call to copy_to_user() as there is no need
    to do it at all.
    
    Signed-off-by: Filipe Manana <fdmanana@suse.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -4232,9 +4232,6 @@ static long btrfs_ioctl_scrub(struct file *file, void __user *arg)
 			      &sa->progress, sa->flags & BTRFS_SCRUB_READONLY,
 			      0);
 
-	if (copy_to_user(arg, sa, sizeof(*sa)))
-		ret = -EFAULT;
-
 	if (!(sa->flags & BTRFS_SCRUB_READONLY))
 		mnt_drop_write_file(file);
 out:
