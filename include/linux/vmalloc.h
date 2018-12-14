07d7e7e8bd5f8ce9557255081b37b10f914a2ed5
Roman Gushchin <guroan@gmail.com>
2018-12-14 10:07:20 -0800

    mm: show number of vmalloc pages in /proc/meminfo
    Vmalloc() is getting more and more used these days (kernel stacks,
    bpf and percpu allocator are new top users), and the total %
    of memory consumed by vmalloc() can be pretty significant
    and changes dynamically.
    
    /proc/meminfo is the best place to display this information:
    its top goal is to show top consumers of the memory.
    
    Since the VmallocUsed field in /proc/meminfo is not in use
    for quite a long time (it has been defined to 0 by the
    commit a5ad88ce8c7f ("mm: get rid of 'vmalloc_info' from
    /proc/meminfo")), let's reuse it for showing the actual
    physical memory consumption of vmalloc().
    
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>

--- include/linux/vmalloc.h
+++ include/linux/vmalloc.h
@@ -64,10 +64,12 @@ extern void vm_unmap_aliases(void);
 
 #ifdef CONFIG_MMU
 extern void __init vmalloc_init(void);
+extern unsigned long vmalloc_nr_pages(void);
 #else
 static inline void vmalloc_init(void)
 {
 }
+static inline unsigned long vmalloc_nr_pages(void) { return 0; }
 #endif
 
 extern void *vmalloc(unsigned long size);
@@ -83,6 +85,7 @@ extern void *__vmalloc_node_range(unsigned long size, unsigned long align,
 			unsigned long start, unsigned long end, gfp_t gfp_mask,
 			pgprot_t prot, unsigned long vm_flags, int node,
 			const void *caller);
+
 #ifndef CONFIG_MMU
 extern void *__vmalloc_node_flags(unsigned long size, int node, gfp_t flags);
 static inline void *__vmalloc_node_flags_caller(unsigned long size, int node,
