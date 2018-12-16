3dea99047149760bab53ed9bc1ed933c76b2b2b9
Christian Gromm <christian.gromm@microchip.com>
2018-12-16 13:36:20 +0100

    staging: most: sound: correct label name
    This patch fixes the lable name that is used to jump to error
    handling section of function audio_probe_channel() in case
    something went wrong.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>

--- drivers/staging/most/sound/sound.c
+++ drivers/staging/most/sound/sound.c
@@ -649,7 +649,7 @@ skip_adpt_alloc:
 	channel = kzalloc(sizeof(*channel), GFP_KERNEL);
 	if (!channel) {
 		ret = -ENOMEM;
-		goto err_free_card;
+		goto err_free_adpt;
 	}
 	channel->card = adpt->card;
 	channel->cfg = cfg;
@@ -661,13 +661,13 @@ skip_adpt_alloc:
 	ret = audio_set_hw_params(&channel->pcm_hardware, ch_num, sample_res,
 				  cfg);
 	if (ret)
-		goto err_free_card;
+		goto err_free_adpt;
 
 	ret = snd_pcm_new(adpt->card, card_name, adpt->pcm_dev_idx,
 			  playback_count, capture_count, &pcm);
 
 	if (ret < 0)
-		goto err_free_card;
+		goto err_free_adpt;
 
 	pcm->private_data = channel;
 	snprintf(pcm->name, sizeof(pcm->name), card_name);
@@ -676,12 +676,12 @@ skip_adpt_alloc:
 	if (create) {
 		ret = snd_card_register(adpt->card);
 		if (ret < 0)
-			goto err_free_card;
+			goto err_free_adpt;
 		adpt->registered = true;
 	}
 	return 0;
 
-err_free_card:
+err_free_adpt:
 	release_adapter(adpt);
 	return ret;
 }
