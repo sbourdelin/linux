e1771860c5a19f7cc588600309d9c0c6b790691a
Claudiu Beznea <claudiu.beznea@microchip.com>
2018-12-17 14:13:40 +0000

    pwm: extend PWM framework with PWM modes
    Add basic PWM modes: normal and complementary. These modes should
    differentiate the single output PWM channels from two outputs PWM
    channels. These modes could be set as follow:
    1. PWM channels with one output per channel:
    - normal mode
    2. PWM channels with two outputs per channel:
    - normal mode
    - complementary mode
    Since users could use a PWM channel with two output as one output PWM
    channel, the PWM normal mode is allowed to be set for PWM channels with
    two outputs; in fact PWM normal mode should be supported by all PWMs.
    
    The PWM capabilities were implemented per PWM channel. Every PWM controller
    will register a function to get PWM capabilities. If this is not explicitly
    set by the driver a default function will be used to retrieve the PWM
    capabilities (in this case the PWM capabilities will contain only PWM
    normal mode). To retrieve capabilities the pwm_get_caps() function could
    be used.
    
    Every PWM channel have associated a mode in the PWM state. Proper
    support was added to get/set PWM mode. Only modes supported by PWM channel
    could be set.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- drivers/pwm/sysfs.c
+++ drivers/pwm/sysfs.c
@@ -223,11 +223,71 @@ static ssize_t capture_show(struct device *child,
 	return sprintf(buf, "%u %u\n", result.period, result.duty_cycle);
 }
 
+static ssize_t mode_show(struct device *child,
+			 struct device_attribute *attr,
+			 char *buf)
+{
+	struct pwm_device *pwm = child_to_pwm_device(child);
+	struct pwm_state state;
+	unsigned long modebit;
+	enum pwm_mode mode;
+	int len = 0;
+
+	pwm_get_state(pwm, &state);
+
+	for (mode = PWM_MODE_NORMAL; mode < PWM_MODE_CNT; mode++) {
+		modebit = BIT(mode);
+		if (pwm_supports_mode(pwm, modebit)) {
+			if (state.modebit == modebit)
+				len += scnprintf(buf + len,
+						 PAGE_SIZE - len, "[%s] ",
+						 pwm_get_mode_name(modebit));
+			else
+				len += scnprintf(buf + len,
+						 PAGE_SIZE - len, "%s ",
+						 pwm_get_mode_name(modebit));
+		}
+	}
+
+	len += scnprintf(buf + len, PAGE_SIZE - len, "\n");
+	return len;
+}
+
+static ssize_t mode_store(struct device *child,
+			  struct device_attribute *attr,
+			  const char *buf, size_t size)
+{
+	struct pwm_export *export = child_to_pwm_export(child);
+	struct pwm_device *pwm = export->pwm;
+	struct pwm_state state;
+	unsigned long modebit;
+	enum pwm_mode mode;
+	int ret;
+
+	for (mode = PWM_MODE_NORMAL; mode < PWM_MODE_CNT; mode++) {
+		modebit = BIT(mode);
+		if (sysfs_streq(buf, pwm_get_mode_name(modebit)))
+			break;
+	}
+
+	if (mode == PWM_MODE_CNT)
+		return -EINVAL;
+
+	mutex_lock(&export->lock);
+	pwm_get_state(pwm, &state);
+	state.modebit = modebit;
+	ret = pwm_apply_state(pwm, &state);
+	mutex_unlock(&export->lock);
+
+	return ret ? : size;
+}
+
 static DEVICE_ATTR_RW(period);
 static DEVICE_ATTR_RW(duty_cycle);
 static DEVICE_ATTR_RW(enable);
 static DEVICE_ATTR_RW(polarity);
 static DEVICE_ATTR_RO(capture);
+static DEVICE_ATTR_RW(mode);
 
 static struct attribute *pwm_attrs[] = {
 	&dev_attr_period.attr,
@@ -235,6 +295,7 @@ static struct attribute *pwm_attrs[] = {
 	&dev_attr_enable.attr,
 	&dev_attr_polarity.attr,
 	&dev_attr_capture.attr,
+	&dev_attr_mode.attr,
 	NULL
 };
 ATTRIBUTE_GROUPS(pwm);
