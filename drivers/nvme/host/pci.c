dd4096eeca5fa87c1469512d73c8424856c81778
yupeng <yupeng0921@gmail.com>
2018-12-17 00:06:05 -0800

    trace nvme submit queue status
    export nvme disk name, queue id, sq_head, sq_tail to trace event
    usage example:
    go to the event directory:
    cd /sys/kernel/debug/tracing/events/nvme/nvme_sq
    filter by disk name:
    echo 'disk=="nvme1n1"' > filter
    enable the event:
    echo 1 > enable
    check results from trace_pipe:
    cat /sys/kernel/debug/tracing/trace_pipe
    In practice, this patch help me debug hardware related
    performant issue.
    
    Signed-off-by: yupeng <yupeng0921@gmail.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -32,6 +32,9 @@
 #include <linux/sed-opal.h>
 #include <linux/pci-p2pdma.h>
 
+#define CREATE_TRACE_POINTS
+#include "trace_pci.h"
+
 #include "nvme.h"
 
 #define SQ_SIZE(depth)		(depth * sizeof(struct nvme_command))
@@ -899,6 +902,10 @@ static inline void nvme_handle_cqe(struct nvme_queue *nvmeq, u16 idx)
 	}
 
 	req = blk_mq_tag_to_rq(*nvmeq->tags, cqe->command_id);
+	trace_nvme_sq(req->rq_disk,
+		nvmeq->qid,
+		le16_to_cpu(cqe->sq_head),
+		nvmeq->sq_tail);
 	nvme_end_request(req, cqe->status, cqe->result);
 }
 
