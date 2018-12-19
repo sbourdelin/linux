49da4ebea2158c01e662e145c9352fafb416c89a
Song Liu <songliubraving@fb.com>
2018-12-18 22:02:27 -0800

    perf tools: synthesize PERF_RECORD_* for loaded BPF programs
    This patch synthesize PERF_RECORD_KSYMBOL and PERF_RECORD_BPF_EVENT for
    BPF programs loaded before perf-record. This is achieved by gathering
    information about all BPF programs via sys_bpf.
    
    Signed-off-by: Song Liu <songliubraving@fb.com>

--- tools/perf/util/bpf-event.h
+++ tools/perf/util/bpf-event.h
@@ -8,4 +8,9 @@ int machine__process_bpf_event(struct machine *machine,
 			       union perf_event *event,
 			       struct perf_sample *sample);
 
+int perf_event__synthesize_bpf_events(struct perf_tool *tool,
+				      perf_event__handler_t process,
+				      struct machine *machine,
+				      struct record_opts *opts);
+
 #endif
