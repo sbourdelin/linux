2f9eaa07385ab04e0f227fc425400f649ed4aae5
Wolfram Sang <wsa+renesas@sang-engineering.com>
2018-12-19 17:48:18 +0100

    i2c: reject new transfers when adapters are suspended
    Using the new 'is_suspended' flag, we now reject new transfers if the
    adapter is already marked suspended.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -1866,6 +1866,8 @@ int __i2c_transfer(struct i2c_adapter *adap, struct i2c_msg *msgs, int num)
 
 	if (WARN_ON(!msgs || num < 1))
 		return -EINVAL;
+	if (WARN_ON(test_bit(I2C_ALF_IS_SUSPENDED, &adap->locked_flags)))
+		return -ESHUTDOWN;
 
 	if (adap->quirks && i2c_check_for_quirks(adap, msgs, num))
 		return -EOPNOTSUPP;
