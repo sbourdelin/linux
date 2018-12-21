e5f0acee069c8b0c7ac5bb5210bb45171bfb6e01
Nikolay Borisov <nborisov@suse.com>
2018-12-21 14:53:14 +0200

    mm: Define VM_(MAX|MIN)_READAHEAD via sizes.h constants
    All users of the aformentioned macros convert them to kbytes by
    multplying. Instead, directly define the macros via the aptly named
    SZ_16K/SZ_128K ones. Also remove the now redundant comments explaining
    that VM_* are defined in kbytes it's obvious. No functional changes.
    
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -26,6 +26,7 @@
 #include <linux/page_ref.h>
 #include <linux/memremap.h>
 #include <linux/overflow.h>
+#include <linux/sizes.h>
 
 struct mempolicy;
 struct anon_vma;
@@ -2396,8 +2397,8 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	SZ_128K
+#define VM_MIN_READAHEAD	SZ_16K	/* includes current page */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
