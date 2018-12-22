402fa3b1360646efd9284f6d3a1fee6be8635c50
yangerkun <yangerkun@huawei.com>
2018-12-22 16:45:36 +0800

    debugfs: remove inc_nlink in debugfs_create_automount
    Remove inc_nlink in debugfs_create_automount, or this inode will never
    be free.
    
    Signed-off-by: yangerkun <yangerkun@huawei.com>

--- fs/debugfs/inode.c
+++ fs/debugfs/inode.c
@@ -551,12 +551,11 @@ struct dentry *debugfs_create_automount(const char *name,
 	if (unlikely(!inode))
 		return failed_creating(dentry);
 
+	/* directory inodes start off with i_nlink == 2 (for "." entry) */
 	make_empty_dir_inode(inode);
 	inode->i_flags |= S_AUTOMOUNT;
 	inode->i_private = data;
 	dentry->d_fsdata = (void *)f;
-	/* directory inodes start off with i_nlink == 2 (for "." entry) */
-	inc_nlink(inode);
 	d_instantiate(dentry, inode);
 	inc_nlink(d_inode(dentry->d_parent));
 	fsnotify_mkdir(d_inode(dentry->d_parent), dentry);
