cb1f3bf7aed84bf84e6fedad7238dab7009a40f9
Arshiya Hayatkhan Pathan <arshiya.hayatkhan.pathan@intel.com>
2018-12-21 16:20:34 -0800

    selftests/resctrl: Add MBM test
    MBM (Memory Bandwidth Monitoring) test is the first implemented selftest.
    It starts a stressful memory bandwidth benchmark and assigns the
    bandwidth pid in a resctrl monitoring group. Read and compare perf IMC
    counter and MBM total bytes for the benchmark. The numbers should be
    close enough to pass the test.
    
    Default benchmark is built-in fill_buf. But users can specify their
    own benchmark by option "-b".
    
    We can add memory bandwidth monitoring for multiple processes in the
    future.
    
    Signed-off-by: Arshiya Hayatkhan Pathan <arshiya.hayatkhan.pathan@intel.com>
    Signed-off-by: Sai Praneeth Prakhya <sai.praneeth.prakhya@intel.com>,
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>

--- tools/testing/selftests/resctrl/resctrl.h
+++ tools/testing/selftests/resctrl/resctrl.h
@@ -70,5 +70,8 @@ int perf_event_open(struct perf_event_attr *hw_event, pid_t pid, int cpu,
 		    int group_fd, unsigned long flags);
 int run_fill_buf(int span, int malloc_and_init_memory, int memflush, int op);
 int membw_val(char **benchmark_cmd, struct resctrl_val_param *param);
+int mbm_bw_change(int span, int core_id, char *bw_report, char **benchmark_cmd);
+void tests_cleanup(void);
+void mbm_test_cleanup(void);
 
 #endif /* RESCTRL_H */
