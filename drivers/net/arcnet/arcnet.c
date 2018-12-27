99911c9e0110d1bf193791de3d74b73bec9d29b3
Jia-Ju Bai <baijiaju1990@gmail.com>
2018-12-27 10:01:42 +0800

    net: arcnet: Fix a possible concurrency use-after-free bug in arcnet_reply_tasklet()
    In drivers/net/arcnet/arcnet.c, the functions arcnet_reply_tasklet() and
    arcnet_send_packet() may be concurrently executed.
    
    arcnet_reply_tasklet()
      line 430: dev_kfree_skb(lp->outgoing.skb);
    
    arcnet_send_packet()
      line 682: spin_lock_irqsave();
      line 690: lp->outgoing.skb = skb;
      line 691: proto->prepare_tx(..., skb->len, ...)
    
    Thus, a possible concurrency use-after-free bugs may occur.
    
    To fix this bug, the calls to spin_lock_irqsave() and
    spin_unlock_irqrestore() are added in arcnet_reply_tasklet() to protect
    dev_kfree_skb(lp->outgoing.skb).
    
    Signed-off-by: Jia-Ju Bai <baijiaju1990@gmail.com>

--- drivers/net/arcnet/arcnet.c
+++ drivers/net/arcnet/arcnet.c
@@ -401,6 +401,7 @@ static void arcnet_reply_tasklet(unsigned long data)
 	struct sock_exterr_skb *serr;
 	struct sock *sk;
 	int ret;
+	unsigned long flags;
 
 	local_irq_disable();
 	skb = lp->outgoing.skb;
@@ -426,9 +427,13 @@ static void arcnet_reply_tasklet(unsigned long data)
 	serr->ee.ee_data = skb_shinfo(skb)->tskey;
 	serr->ee.ee_info = lp->reply_status;
 
+	spin_lock_irqsave(&lp->lock, flags);
+
 	/* finally erasing outgoing skb */
 	dev_kfree_skb(lp->outgoing.skb);
 	lp->outgoing.skb = NULL;
+
+	spin_unlock_irqrestore(&lp->lock, flags);
 
 	ackskb->dev = lp->dev;
 
