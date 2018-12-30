83a4fba2929be89ae9adea8c7203dfabd15dfc7e
Yang Shi <yang.shi@linux.alibaba.com>
2018-12-30 12:49:35 +0800

    mm: swap: add comment for swap_vma_readahead
    swap_vma_readahead()'s comment is missed, just add it.
    
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Tim Chen <tim.c.chen@intel.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Yang Shi <yang.shi@linux.alibaba.com>

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -698,6 +698,23 @@ static void swap_ra_info(struct vm_fault *vmf,
 	pte_unmap(orig_pte);
 }
 
+/**
+ * swap_vm_readahead - swap in pages in hope we need them soon
+ * @entry: swap entry of this memory
+ * @gfp_mask: memory allocation flags
+ * @vmf: fault information
+ *
+ * Returns the struct page for entry and addr, after queueing swapin.
+ *
+ * Primitive swap readahead code. We simply read in a few pages whoes
+ * virtual addresses are around the fault address in the same vma.
+ *
+ * This has been extended to use the NUMA policies from the mm triggering
+ * the readahead.
+ *
+ * Caller must hold down_read on the vma->vm_mm if vmf->vma is not NULL.
+ *
+ */
 static struct page *swap_vma_readahead(swp_entry_t fentry, gfp_t gfp_mask,
 				       struct vm_fault *vmf)
 {
