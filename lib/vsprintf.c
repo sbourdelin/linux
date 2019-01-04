ac8a67e034d9ef0288eb9fb2966d6c3c53179b2f
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-01-04 21:30:06 +0200

    lib/vsprintf: Print time64_t in human readable format
    There are users which print time and date represented by content of
    time64_t type in human readable format.
    
    Instead of open coding that each time introduce %ptT[dt][r] specifier.
    
    Few test cases for %ptT specifier has been added as well.
    
    Cc: Hans Verkuil <hverkuil@xs4all.nl>
    Cc: Mathias Nyman <mathias.nyman@intel.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: Jonathan Hunter <jonathanh@nvidia.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -31,6 +31,7 @@
 #include <linux/dcache.h>
 #include <linux/cred.h>
 #include <linux/rtc.h>
+#include <linux/time.h>
 #include <linux/uuid.h>
 #include <linux/of.h>
 #include <net/addrconf.h>
@@ -1634,12 +1635,47 @@ char *rtc_str(char *buf, char *end, const struct rtc_time *tm, const char *fmt)
 }
 
 static noinline_for_stack
+void time64_to_rtc_time(time64_t time, struct rtc_time *rtc_time)
+{
+#ifdef CONFIG_RTC_LIB
+	rtc_time64_to_tm(time, rtc_time);
+#else
+	struct tm tm;
+
+	time64_to_tm(time, 0, &tm);
+
+	rtc_time->tm_sec = tm.tm_sec;
+	rtc_time->tm_min = tm.tm_min;
+	rtc_time->tm_hour = tm.tm_hour;
+	rtc_time->tm_mday = tm.tm_mday;
+	rtc_time->tm_mon = tm.tm_mon;
+	rtc_time->tm_year = tm.tm_year;
+	rtc_time->tm_wday = tm.tm_wday;
+	rtc_time->tm_yday = tm.tm_yday;
+
+	rtc_time->tm_isdst = 0;
+#endif
+}
+
+static noinline_for_stack
+char *time64_str(char *buf, char *end, const time64_t *t, const char *fmt)
+{
+	struct rtc_time tm;
+
+	time64_to_rtc_time(*t, &tm);
+
+	return rtc_str(buf, end, &tm, fmt);
+}
+
+static noinline_for_stack
 char *time_and_date(char *buf, char *end, void *ptr, struct printf_spec spec,
 		    const char *fmt)
 {
 	switch (fmt[1]) {
 	case 'R':
 		return rtc_str(buf, end, (const struct rtc_time *)ptr, fmt);
+	case 'T':
+		return time64_str(buf, end, (const time64_t *)ptr, fmt);
 	default:
 		return ptr_to_id(buf, end, ptr, spec);
 	}
@@ -1924,8 +1960,9 @@ char *device_node_string(char *buf, char *end, struct device_node *dn,
  * - 'd[234]' For a dentry name (optionally 2-4 last components)
  * - 'D[234]' Same as 'd' but for a struct file
  * - 'g' For block_device name (gendisk + partition number)
- * - 't[R][dt][r]' For time and date as represented:
+ * - 't[RT][dt][r]' For time and date as represented by:
  *      R    struct rtc_time
+ *      T    time64_t
  * - 'C' For a clock, it prints the name (Common Clock Framework) or address
  *       (legacy clock framework) of the clock
  * - 'Cn' For a clock, it prints the name (Common Clock Framework) or address
