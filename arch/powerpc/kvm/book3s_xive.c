c64896757bed21d5e1ddf1b4203e36e2aa61c4f5
Cédric Le Goater <clg@kaod.org>
2019-01-07 19:43:15 +0100

    KVM: PPC: Book3S HV: check the IRQ controller type
    We will have different KVM devices for interrupts, one for the
    XICS-over-XIVE mode and one for the XIVE native exploitation
    mode. Let's add some checks to make sure we are not mixing the
    interfaces in KVM.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- arch/powerpc/kvm/book3s_xive.c
+++ arch/powerpc/kvm/book3s_xive.c
@@ -819,6 +819,9 @@ u64 kvmppc_xive_get_icp(struct kvm_vcpu *vcpu)
 {
 	struct kvmppc_xive_vcpu *xc = vcpu->arch.xive_vcpu;
 
+	if (!kvmppc_xics_enabled(vcpu))
+		return -EPERM;
+
 	if (!xc)
 		return 0;
 
@@ -834,6 +837,9 @@ int kvmppc_xive_set_icp(struct kvm_vcpu *vcpu, u64 icpval)
 	struct kvmppc_xive *xive = vcpu->kvm->arch.xive;
 	u8 cppr, mfrr;
 	u32 xisr;
+
+	if (!kvmppc_xics_enabled(vcpu))
+		return -EPERM;
 
 	if (!xc || !xive)
 		return -ENOENT;
