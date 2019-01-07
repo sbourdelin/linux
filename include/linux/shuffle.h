a97d2d57f3207a53d2146f6be060315ce205a7ee
Dan Williams <dan.j.williams@intel.com>
2019-01-07 15:21:21 -0800

    mm: Maintain randomization of page free lists
    When freeing a page with an order >= shuffle_page_order randomly select
    the front or back of the list for insertion.
    
    While the mm tries to defragment physical pages into huge pages this can
    tend to make the page allocator more predictable over time. Inject the
    front-back randomness to preserve the initial randomness established by
    shuffle_free_memory() when the kernel was booted.
    
    The overhead of this manipulation is constrained by only being applied
    for MAX_ORDER sized pages by default.
    
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- include/linux/shuffle.h
+++ include/linux/shuffle.h
@@ -30,6 +30,13 @@ static inline void shuffle_zone(struct zone *z, unsigned long start_pfn,
 		return;
 	__shuffle_zone(z, start_pfn, end_pfn);
 }
+
+static inline bool is_shuffle_order(int order)
+{
+	if (!static_branch_unlikely(&page_alloc_shuffle_key))
+                return false;
+	return order >= CONFIG_SHUFFLE_PAGE_ORDER;
+}
 #else
 static inline void shuffle_free_memory(pg_data_t *pgdat, unsigned long start_pfn,
 		unsigned long end_pfn)
@@ -43,6 +50,11 @@ static inline void shuffle_zone(struct zone *z, unsigned long start_pfn,
 
 static inline void page_alloc_shuffle(enum mm_shuffle_ctl ctl)
 {
+}
+
+static inline bool is_shuffle_order(int order)
+{
+	return false;
 }
 #endif
 #endif /* _MM_SHUFFLE_H */
