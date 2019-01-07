b649b6a4c09e046d98cee6f7184a00cc612e2c2a
Dmitry V. Levin <ldv@altlinux.org>
2019-01-07 05:54:04 +0300

    m68k: add asm/syscall.h
    syscall_get_* functions are required to be implemented on all
    architectures in order to extend the generic ptrace API with
    PTRACE_GET_SYSCALL_INFO request.
    
    This introduces asm/syscall.h on m68k implementing all 5 syscall_get_*
    functions as documented in asm-generic/syscall.h: syscall_get_nr,
    syscall_get_arguments, syscall_get_error, syscall_get_return_value,
    and syscall_get_arch.
    
    Acked-by: Paul Moore <paul@paul-moore.com> # audit related parts
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Elvira Khabirova <lineprinter@altlinux.org>
    Cc: Eugene Syromyatnikov <esyr@redhat.com>
    Cc: linux-m68k@lists.linux-m68k.org
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>

---
+++ arch/m68k/include/asm/syscall.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_M68K_SYSCALL_H
+#define _ASM_M68K_SYSCALL_H
+
+#include <asm/ptrace.h>
+#include <asm-generic/syscall.h>
+#include <linux/err.h>
+#include <uapi/linux/audit.h>
+
+static inline int
+syscall_get_nr(struct task_struct *task, struct pt_regs *regs)
+{
+	return regs->orig_d0;
+}
+
+static inline void
+__syscall_get_arguments(struct task_struct *task, struct pt_regs *regs,
+			unsigned int i, unsigned int n, unsigned long *args)
+{
+	BUILD_BUG_ON(sizeof(regs->d1) != sizeof(args[0]));
+	memcpy(args, &regs->d1 + i, n * sizeof(args[0]));
+}
+
+static inline long
+syscall_get_error(struct task_struct *task, struct pt_regs *regs)
+{
+	return IS_ERR_VALUE(regs->d0) ? regs->d0 : 0;
+}
+
+static inline long
+syscall_get_return_value(struct task_struct *task, struct pt_regs *regs)
+{
+	return regs->d0;
+}
+
+static inline int
+syscall_get_arch(void)
+{
+	return AUDIT_ARCH_M68K;
+}
+
+#endif	/* _ASM_M68K_SYSCALL_H */
