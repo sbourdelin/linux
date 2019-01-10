fdb459a252fa8114e0f9c6cf2a731a4d8bb9c375
Eric Biggers <ebiggers@google.com>
2019-01-10 12:41:43 -0800

    fs/direct-io.c: include fs/internal.h for sb_init_dio_done_wq()
    sb_init_dio_done_wq() is defined in fs/direct-io.c and declared in
    fs/internal.h, but the declaration isn't included at the point of the
    definition.  Include the header to enforce that the definition matches
    the declaration.
    
    This addresses a gcc warning when -Wmissing-prototypes is enabled.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- fs/direct-io.c
+++ fs/direct-io.c
@@ -37,6 +37,7 @@
 #include <linux/uio.h>
 #include <linux/atomic.h>
 #include <linux/prefetch.h>
+#include "internal.h"
 
 /*
  * How many user pages to map in one call to get_user_pages().  This determines
