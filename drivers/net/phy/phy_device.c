3143c8f634eb0b8a4e36af59062eea5ae07d40c9
Heiner Kallweit <hkallweit1@gmail.com>
2019-01-15 21:33:52 +0100

    net: phy: check return code when requesting PHY driver module
    When requesting the PHY driver module fails we'll bind the genphy
    driver later. This isn't obvious to the user and may cause, depending
    on the PHY, different types of issues. Therefore check the return code
    of request_module() and inform the user in case of failure.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -548,6 +548,17 @@ static const struct device_type mdio_bus_phy_type = {
 	.pm = MDIO_BUS_PHY_PM_OPS,
 };
 
+static void phy_request_driver_module(struct phy_device *dev, int phy_id)
+{
+	int ret;
+
+	ret = request_module(MDIO_MODULE_PREFIX MDIO_ID_FMT,
+			     MDIO_ID_ARGS(phy_id));
+	if (IS_ENABLED(CONFIG_MODULES) && ret < 0)
+		phydev_err(dev, "error %d loading PHY driver module for ID 0x%08x\n",
+			   ret, phy_id);
+}
+
 struct phy_device *phy_device_create(struct mii_bus *bus, int addr, int phy_id,
 				     bool is_c45,
 				     struct phy_c45_device_ids *c45_ids)
@@ -610,12 +621,10 @@ struct phy_device *phy_device_create(struct mii_bus *bus, int addr, int phy_id,
 			if (!(c45_ids->devices_in_package & (1 << i)))
 				continue;
 
-			request_module(MDIO_MODULE_PREFIX MDIO_ID_FMT,
-				       MDIO_ID_ARGS(c45_ids->device_ids[i]));
+			phy_request_driver_module(dev, c45_ids->device_ids[i]);
 		}
 	} else {
-		request_module(MDIO_MODULE_PREFIX MDIO_ID_FMT,
-			       MDIO_ID_ARGS(phy_id));
+		phy_request_driver_module(dev, phy_id);
 	}
 
 	device_initialize(&mdiodev->dev);
