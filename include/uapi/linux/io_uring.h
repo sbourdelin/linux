b24f71bcb2c8592a7297c2b47c9d34019a1d60dc
Jens Axboe <axboe@kernel.dk>
2019-01-14 19:55:30 -0700

    io_uring: add file registration
    We normally have to fget/fput for each IO we do on a file. Even with
    the batching we do, this atomic inc/dec cost adds up.
    
    This adds IORING_REGISTER_FILES, and IORING_UNREGISTER_FILES opcodes
    for the io_uring_register(2) system call. Pass in an array of fds
    that are in use by the application, and we'll fget these for the
    duration of the io_uring context.
    
    When used, the application must set IOSQE_FIXED_FILE in the sqe->flags
    member. Then, instead of setting sqe->fd to the real fd, it sets sqe->fd
    to the index in the array passed in to IORING_REGISTER_FILES.
    
    Files are automatically unregistered when the io_uring context is
    torn down. An application need only unregister if it wishes to
    register a few set of fds.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/uapi/linux/io_uring.h
+++ include/uapi/linux/io_uring.h
@@ -16,7 +16,7 @@
  */
 struct io_uring_sqe {
 	__u8	opcode;		/* type of operation for this sqe */
-	__u8	flags;		/* as of now unused */
+	__u8	flags;		/* IOSQE_ flags */
 	__u16	ioprio;		/* ioprio for the request */
 	__s32	fd;		/* file descriptor to do IO on */
 	__u64	off;		/* offset into file */
@@ -35,6 +35,11 @@ struct io_uring_sqe {
 		__u64	__pad2[3];
 	};
 };
+
+/*
+ * sqe->flags
+ */
+#define IOSQE_FIXED_FILE	(1 << 0)	/* use fixed fileset */
 
 /*
  * io_uring_setup() flags
@@ -123,6 +128,8 @@ struct io_uring_params {
  */
 #define IORING_REGISTER_BUFFERS		0
 #define IORING_UNREGISTER_BUFFERS	1
+#define IORING_REGISTER_FILES		2
+#define IORING_UNREGISTER_FILES		3
 
 struct io_uring_register_buffers {
 	union {
@@ -130,6 +137,14 @@ struct io_uring_register_buffers {
 		__u64 pad;
 	};
 	__u32 nr_iovecs;
+};
+
+struct io_uring_register_files {
+	union {
+		__s32 *fds;
+		__u64 pad;
+	};
+	__u32 nr_fds;
 };
 
 #endif
