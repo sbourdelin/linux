e84aeb4ee538cde26cd0534b74b84c6b5c8bdf84
Ajay Gupta <ajayg@nvidia.com>
2019-01-17 17:09:03 -0800

    usb: typec: ucsi: add get_fw_info function
    Function is to get the details of ccg firmware and device version.
    
    Signed-off-by: Ajay Gupta <ajayg@nvidia.com>

--- drivers/usb/typec/ucsi/ucsi_ccg.c
+++ drivers/usb/typec/ucsi/ucsi_ccg.c
@@ -17,15 +17,46 @@
 #include <asm/unaligned.h>
 #include "ucsi.h"
 
+struct ccg_dev_info {
+	u8 fw_mode:2;
+	u8 two_pd_ports:2;
+	u8 row_size_256:2;
+	u8:1; /* reserved */
+	u8 hpi_v2_mode:1;
+	u8 bl_mode:1;
+	u8 cfgtbl_invalid:1;
+	u8 fw1_invalid:1;
+	u8 fw2_invalid:1;
+	u8:4; /* reserved */
+	u16 silicon_id;
+	u16 bl_last_row;
+} __packed;
+
+struct version_format {
+	u16 build;
+	u8 patch;
+	u8 min:4;
+	u8 maj:4;
+};
+
+struct version_info {
+	struct version_format base;
+	struct version_format app;
+};
+
 struct ucsi_ccg {
 	struct device *dev;
 	struct ucsi *ucsi;
 	struct ucsi_ppm ppm;
 	struct i2c_client *client;
+	struct ccg_dev_info info;
 };
 
-#define CCGX_RAB_INTR_REG			0x06
-#define CCGX_RAB_UCSI_CONTROL			0x39
+#define CCGX_RAB_DEVICE_MODE			0x0000
+#define CCGX_RAB_INTR_REG			0x0006
+#define CCGX_RAB_READ_ALL_VER			0x0010
+#define CCGX_RAB_READ_FW2_VER			0x0020
+#define CCGX_RAB_UCSI_CONTROL			0x0039
 #define CCGX_RAB_UCSI_CONTROL_START		BIT(0)
 #define CCGX_RAB_UCSI_CONTROL_STOP		BIT(1)
 #define CCGX_RAB_UCSI_DATA_BLOCK(offset)	(0xf000 | ((offset) & 0xff))
@@ -220,6 +251,41 @@ static irqreturn_t ccg_irq_handler(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
+static int get_fw_info(struct ucsi_ccg *uc)
+{
+	struct device *dev = uc->dev;
+	struct version_info version[3];
+	struct version_info *v;
+	int err, i;
+
+	err = ccg_read(uc, CCGX_RAB_READ_ALL_VER, (u8 *)(&version),
+		       sizeof(version));
+	if (err < 0)
+		return err;
+
+	for (i = 1; i < ARRAY_SIZE(version); i++) {
+		v = &version[i];
+		dev_dbg(dev,
+			"FW%d Version: %c%c v%x.%x%x, [Base %d.%d.%d.%d]\n",
+			i, (v->app.build >> 8), (v->app.build & 0xFF),
+			v->app.patch, v->app.maj, v->app.min,
+			v->base.maj, v->base.min, v->base.patch,
+			v->base.build);
+	}
+
+	err = ccg_read(uc, CCGX_RAB_DEVICE_MODE, (u8 *)(&uc->info),
+		       sizeof(uc->info));
+	if (err < 0)
+		return err;
+
+	dev_dbg(dev, "fw_mode: %d\n", uc->info.fw_mode);
+	dev_dbg(dev, "fw1_invalid: %d\n", uc->info.fw1_invalid);
+	dev_dbg(dev, "fw2_invalid: %d\n", uc->info.fw2_invalid);
+	dev_dbg(dev, "silicon_id: 0x%04x\n", uc->info.silicon_id);
+
+	return 0;
+}
+
 static int ucsi_ccg_probe(struct i2c_client *client,
 			  const struct i2c_device_id *id)
 {
@@ -245,6 +311,12 @@ static int ucsi_ccg_probe(struct i2c_client *client,
 	status = ucsi_ccg_init(uc);
 	if (status < 0) {
 		dev_err(uc->dev, "ucsi_ccg_init failed - %d\n", status);
+		return status;
+	}
+
+	status = get_fw_info(uc);
+	if (status < 0) {
+		dev_err(uc->dev, "get_fw_info failed - %d\n", status);
 		return status;
 	}
 
