10d2978f7a975879fc356676c16f6cdfe8dd162b
Heiner Kallweit <hkallweit1@gmail.com>
2019-01-19 10:43:07 +0100

    net: phy: improve phy_init_hw
    Currently the soft reset (if defined) is done only if the driver also
    implements the config_init callback. I think this dependency is a
    mistake, so let's remove it.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -1041,7 +1041,7 @@ int phy_init_hw(struct phy_device *phydev)
 	/* Deassert the reset signal */
 	phy_device_reset(phydev, 0);
 
-	if (!phydev->drv || !phydev->drv->config_init)
+	if (!phydev->drv)
 		return 0;
 
 	if (phydev->drv->soft_reset)
@@ -1054,7 +1054,10 @@ int phy_init_hw(struct phy_device *phydev)
 	if (ret < 0)
 		return ret;
 
-	return phydev->drv->config_init(phydev);
+	if (phydev->drv->config_init)
+		ret = phydev->drv->config_init(phydev);
+
+	return ret;
 }
 EXPORT_SYMBOL(phy_init_hw);
 
