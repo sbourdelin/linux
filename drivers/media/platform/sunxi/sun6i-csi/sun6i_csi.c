cce516d2d63091247716fc6f3e94ed74120a7bd8
Chen-Yu Tsai <wens@csie.org>
2019-02-04 00:03:58 +0800

    media: sun6i: Add support for JPEG media bus format
    The CSI controller can take raw data from the data bus and output it
    directly to capture buffers. This can be used to support the JPEG media
    bus format.
    
    While the controller can report minimum and maximum bytes per line, it
    has no way to report how many lines were captured in the last frame.
    Thus, even when the on-bus data is framed correctly, we have no way to
    accertain the actual amount of data captured, unless we scan the buffer
    for JPEG EOI markers, or sequential zeros. For now we leave bytesused
    alone, and leave it up to userspace applications to parse the data.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>

--- drivers/media/platform/sunxi/sun6i-csi/sun6i_csi.c
+++ drivers/media/platform/sunxi/sun6i-csi/sun6i_csi.c
@@ -149,6 +149,9 @@ bool sun6i_csi_is_format_supported(struct sun6i_csi *csi,
 	case V4L2_PIX_FMT_RGB565X:
 		return (mbus_code == MEDIA_BUS_FMT_RGB565_2X8_BE);
 
+	case V4L2_PIX_FMT_JPEG:
+		return (mbus_code == MEDIA_BUS_FMT_JPEG_1X8);
+
 	default:
 		dev_dbg(sdev->dev, "Unsupported pixformat: 0x%x\n", pixformat);
 		break;
@@ -278,6 +281,9 @@ static enum csi_output_fmt get_csi_output_format(struct sun6i_csi_dev *sdev,
 	case V4L2_PIX_FMT_RGB565:
 	case V4L2_PIX_FMT_RGB565X:
 		return buf_interlaced ? CSI_FRAME_RGB565 : CSI_FIELD_RGB565;
+
+	case V4L2_PIX_FMT_JPEG:
+		return buf_interlaced ? CSI_FRAME_RAW_8 : CSI_FIELD_RAW_8;
 
 	default:
 		dev_warn(sdev->dev, "Unsupported pixformat: 0x%x\n", pixformat);
