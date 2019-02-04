66303309ece7951677204a534ebb7fa0774697a8
Jiri Olsa <jolsa@kernel.org>
2019-02-04 15:18:07 +0100

    perf tools: Add perf_evsel__output_resort_cb function
    Adding perf_evsel__output_resort_cb so we have an interface
    with callback on each hist entry. It will be used in following
    patch.
    
    Link: http://lkml.kernel.org/n/tip-oakdwgxijwhxrlp9405qz612@git.kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -1773,7 +1773,8 @@ static void output_resort(struct hists *hists, struct ui_progress *prog,
 	}
 }
 
-void perf_evsel__output_resort(struct perf_evsel *evsel, struct ui_progress *prog)
+void perf_evsel__output_resort_cb(struct perf_evsel *evsel, struct ui_progress *prog,
+				  hists__resort_cb_t cb, void *cb_arg)
 {
 	bool use_callchain;
 
@@ -1784,7 +1785,12 @@ void perf_evsel__output_resort(struct perf_evsel *evsel, struct ui_progress *pro
 
 	use_callchain |= symbol_conf.show_branchflag_count;
 
-	output_resort(evsel__hists(evsel), prog, use_callchain, NULL, NULL);
+	output_resort(evsel__hists(evsel), prog, use_callchain, cb, cb_arg);
+}
+
+void perf_evsel__output_resort(struct perf_evsel *evsel, struct ui_progress *prog)
+{
+	return perf_evsel__output_resort_cb(evsel, prog, NULL, NULL);
 }
 
 void hists__output_resort(struct hists *hists, struct ui_progress *prog)
