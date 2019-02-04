23904065c0d0542cce7dd4b59780952afd8ff238
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-02-04 20:45:23 +0200

    drm/i915: W/A for underruns with WM1+ disabled on icl
    Disabling WM1+ on ICL causes tons of underruns with
    linear/X-tiled framebuffers. We can avoid this by flipping
    on a chicken bit affecting the way the hw fill the FIFO.
    This may not be the final solution but should hopefully
    avoid some underruns in the meantime.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3912,6 +3912,13 @@ static void skl_set_pipe_chicken(struct intel_crtc *crtc)
 	if (INTEL_GEN(dev_priv) >= 11)
 		tmp |= PER_PIXEL_ALPHA_BYPASS_EN;
 
+	/*
+	 * W/A for underruns with linear/X-tiled with
+	 * WM1+ disabled.
+	 */
+	if (INTEL_GEN(dev_priv) >= 11)
+		tmp |= PM_FILL_MAINTAIN_DBUF_FULLNESS;
+
 	I915_WRITE(PIPE_CHICKEN(pipe), tmp);
 }
 
