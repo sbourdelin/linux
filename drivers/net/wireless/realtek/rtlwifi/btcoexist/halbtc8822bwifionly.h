03ee17cc660f5e896c9f20a31e8ed4f5658bac5d
Larry Finger <Larry.Finger@lwfinger.net>
2019-02-08 11:21:58 -0600

    rtlwifi: btcoex: Replace old-style license information
    The old-style license information is replaced by the SPDX form. There
    are no code changes.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- drivers/net/wireless/realtek/rtlwifi/btcoexist/halbtc8822bwifionly.h
+++ drivers/net/wireless/realtek/rtlwifi/btcoexist/halbtc8822bwifionly.h
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/* Copyright(c) 2016-2017  Realtek Corporation.*/
+
 /******************************************************************************
  *
  * Copyright(c) 2016-2017  Realtek Corporation.
