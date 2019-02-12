bdbf1638880b32f60017fe1b0213a65969f8db07
Myungho Jung <mhjungk@gmail.com>
2019-02-12 00:36:49 -0800

    Bluetooth: hci_ldisc: Add NULL check for tiocmget() and tiocmset()
    tiocmget() and tiocmset() operations are optional and some tty drivers
    like pty miss the operations. H4 spec requires the operations to set
    flow control. Return -EOPNOTSUPP in hci_uart_tty_open() if the oprations
    are not supported.
    
    Signed-off-by: Myungho Jung <mhjungk@gmail.com>

--- drivers/bluetooth/hci_ldisc.c
+++ drivers/bluetooth/hci_ldisc.c
@@ -474,10 +474,11 @@ static int hci_uart_tty_open(struct tty_struct *tty)
 
 	BT_DBG("tty %p", tty);
 
-	/* Error if the tty has no write op instead of leaving an exploitable
-	 * hole
+	/* Error if the tty has no write or tiocmget or tiocmset op instead of
+	 * leaving an exploitable hole
 	 */
-	if (tty->ops->write == NULL)
+	if (tty->ops->write == NULL || tty->ops->tiocmget == NULL ||
+	    tty->ops->tiocmset == NULL)
 		return -EOPNOTSUPP;
 
 	hu = kzalloc(sizeof(struct hci_uart), GFP_KERNEL);
